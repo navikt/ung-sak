b08aed6c5e915591af0658d2ef1af0aa492f0ff8
Frode Carlsen <frode.odde.carlsen@nav.no>
2019-12-11 12:55:36 +0100

    fix: test som feilet. locked maven plugin versjoner

--- domenetjenester/behandlingskontroll/src/test/java/no/nav/foreldrepenger/behandlingskontroll/FagsakYtelseTypeRefTest.java
+++ domenetjenester/behandlingskontroll/src/test/java/no/nav/foreldrepenger/behandlingskontroll/FagsakYtelseTypeRefTest.java
@@ -1,8 +1,9 @@
 package no.nav.foreldrepenger.behandlingskontroll;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import javax.enterprise.context.ApplicationScoped;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -22,27 +23,27 @@ public class FagsakYtelseTypeRefTest {
     public void skal_få_duplikat_instans_av_cdi_bean() throws Exception {
         expected.expect(IllegalStateException.class);
         expected.expectMessage("Har flere matchende instanser");
-        FagsakYtelseTypeRef.Lookup.find(Bokstav.class, FagsakYtelseType.FORELDREPENGER);
+        FagsakYtelseTypeRef.Lookup.find(Bokstav.class, FagsakYtelseType.PLEIEPENGER_SYKT_BARN);
     }
 
     @Test
     public void skal_få_unik_instans_av_cdi_bean() throws Exception {
         var instans = FagsakYtelseTypeRef.Lookup.find(Bokstav.class, FagsakYtelseType.SVANGERSKAPSPENGER);
-        Assertions.assertThat(instans).isNotNull();
+        assertThat(instans).isNotNull();
+        assertThat(instans).isEmpty();
     }
 
     public interface Bokstav {
     }
 
     @ApplicationScoped
-    @FagsakYtelseTypeRef
-
+    @FagsakYtelseTypeRef("PSB")
     public static class A implements Bokstav{
 
     }
 
     @ApplicationScoped
-    @FagsakYtelseTypeRef
+    @FagsakYtelseTypeRef("PSB")
     public static class B implements Bokstav{
 
     }
