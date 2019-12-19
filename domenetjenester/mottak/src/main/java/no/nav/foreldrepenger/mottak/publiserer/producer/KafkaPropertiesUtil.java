9d4a067ad75ba5486f7412144506222154e798f0
Marius Glittum <marius.glittum@nav.no>
2019-12-19 14:22:33 +0100

    Setter timeout på delivery

--- domenetjenester/mottak/src/main/java/no/nav/foreldrepenger/mottak/publiserer/producer/KafkaPropertiesUtil.java
+++ domenetjenester/mottak/src/main/java/no/nav/foreldrepenger/mottak/publiserer/producer/KafkaPropertiesUtil.java
@@ -12,6 +12,7 @@ class KafkaPropertiesUtil {
         properties.setProperty("bootstrap.servers", bootstrapServers);
         properties.setProperty("schema.registry.url", schemaRegistryUrl);
         properties.setProperty("client.id", clientId);
+        properties.setProperty("delivery.timeout.ms", "3000");
 
         setSecurity(username, properties);
         setUsernameAndPassword(username, password, properties);
