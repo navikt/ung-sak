ad4427a839b3977dd9ef04f5ca867fbd9b529c00
Stian Grenborgen <stian.grenborgen@nav.no>
2020-01-30 13:02:19 +0100

    Oppsett for deploy til Nais-testmiljø
    Inkluderer automatisk deploy til testmiljø ved bygg av master.

--- build/run-java.sh
+++ build/run-java.sh
@@ -4,6 +4,9 @@ set -eu
 export JAVA_OPTS="${JAVA_OPTS:-} -Xmx2048m -Xms256m -Djava.security.egd=file:/dev/./urandom -Duser.timezone='Europe/Oslo'"
 export STARTUP_CLASS=${STARTUP_CLASS:-"no.nav.foreldrepenger.web.server.jetty.JettyServer"}
 
+export SYSTEMBRUKER_USERNAME="$(cat /var/run/secrets/nais.io/serviceuser/username)"
+export SYSTEMBRUKER_PASSWORD="$(cat /var/run/secrets/nais.io/serviceuser/password)"
+
 exec java -cp "app.jar:lib/*"${EXTRA_CLASS_PATH:-""} ${DEFAULT_JAVA_OPTS:-} ${JAVA_OPTS} \
     -Dwebapp=${WEBAPP:-"./webapp"} \
     -Dapplication.name=${APP_NAME} ${STARTUP_CLASS?} $@
