e791db937c1924a6fec982c5f893145da26dfdb2
Stian Grenborgen <stian.grenborgen@nav.no>
2020-02-05 14:17:24 +0100

    Exporterer riktige miljøvariabler for LDAP-vault. (#63)

--- build/run-java.sh
+++ build/run-java.sh
@@ -11,6 +11,12 @@ then
     export SYSTEMBRUKER_PASSWORD="$(cat /var/run/secrets/nais.io/serviceuser/password)"
 fi
 
+if test -f /var/run/secrets/nais.io/ldap/username
+then
+    export LDAP_USERNAME="$(cat /var/run/secrets/nais.io/ldap/username)"
+    export LDAP_PASSWORD="$(cat /var/run/secrets/nais.io/ldap/password)"
+fi
+
 exec java -cp "app.jar:lib/*"${EXTRA_CLASS_PATH:-""} ${DEFAULT_JAVA_OPTS:-} ${JAVA_OPTS} \
     -Dwebapp=${WEBAPP:-"./webapp"} \
     -Dapplication.name=${APP_NAME} ${STARTUP_CLASS?} $@
