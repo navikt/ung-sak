9ba5418d33adc80e8d95c718e802ee12330aa156
Bjornar Askevold <bjornar.askevold@nav.no>
2020-02-12 16:20:47 +0100

    Lagt til JsonCreator på AvklartPersonstatus

--- kontrakt/src/main/java/no/nav/k9/sak/kontrakt/person/AvklartPersonstatus.java
+++ kontrakt/src/main/java/no/nav/k9/sak/kontrakt/person/AvklartPersonstatus.java
@@ -2,6 +2,8 @@ package no.nav.k9.sak.kontrakt.person;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 
@@ -15,7 +17,9 @@ public class AvklartPersonstatus {
     private PersonstatusType orginalPersonstatus;
     private PersonstatusType overstyrtPersonstatus;
 
-    public AvklartPersonstatus(PersonstatusType orginalPersonstatus, PersonstatusType overstyrtPersonstatus) {
+    @JsonCreator
+    public AvklartPersonstatus(@JsonProperty("orginalPersonstatus") PersonstatusType orginalPersonstatus,
+                               @JsonProperty("overstyrtPersonstatus") PersonstatusType overstyrtPersonstatus) {
         this.orginalPersonstatus = orginalPersonstatus;
         this.overstyrtPersonstatus = overstyrtPersonstatus;
     }
