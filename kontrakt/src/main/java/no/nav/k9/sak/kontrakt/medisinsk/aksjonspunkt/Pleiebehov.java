0565286dd165e129d0c72b6e35055458a89f1388
Hallvard Andreas Stark <hallvard@scel.to>
2020-02-12 10:07:40 +0100

    Håndterer at listene kan være null i Pleiebehov

--- kontrakt/src/main/java/no/nav/k9/sak/kontrakt/medisinsk/aksjonspunkt/Pleiebehov.java
+++ kontrakt/src/main/java/no/nav/k9/sak/kontrakt/medisinsk/aksjonspunkt/Pleiebehov.java
@@ -1,15 +1,14 @@
 package no.nav.k9.sak.kontrakt.medisinsk.aksjonspunkt;
 
-import java.util.Collections;
-import java.util.List;
-
-import javax.validation.Valid;
-import javax.validation.constraints.Size;
-
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Size;
+import java.util.Collections;
+import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
@@ -35,10 +34,16 @@ public class Pleiebehov {
     }
 
     public List<PeriodeMedTilsyn> getPerioderMedTilsynOgPleie() {
+        if (perioderMedTilsynOgPleie == null) {
+            return List.of();
+        }
         return Collections.unmodifiableList(perioderMedTilsynOgPleie);
     }
 
     public List<PeriodeMedTilsyn> getPerioderMedUtvidetTilsynOgPleie() {
+        if (perioderMedUtvidetTilsynOgPleie == null) {
+            return List.of();
+        }
         return Collections.unmodifiableList(perioderMedUtvidetTilsynOgPleie);
     }
 
