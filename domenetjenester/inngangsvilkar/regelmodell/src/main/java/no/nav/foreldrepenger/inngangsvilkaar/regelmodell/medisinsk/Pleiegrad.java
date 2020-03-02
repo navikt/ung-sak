71483e88eed8f30b584afb6e2cd7bb2d6bde180c
Marius Bækken Glittum <marius.glittum@nav.no>
2020-03-02 14:55:37 +0100

    Update Pleiegrad.java

--- domenetjenester/inngangsvilkar/regelmodell/src/main/java/no/nav/foreldrepenger/inngangsvilkaar/regelmodell/medisinsk/Pleiegrad.java
+++ domenetjenester/inngangsvilkar/regelmodell/src/main/java/no/nav/foreldrepenger/inngangsvilkaar/regelmodell/medisinsk/Pleiegrad.java
@@ -3,6 +3,6 @@ package no.nav.foreldrepenger.inngangsvilkaar.regelmodell.medisinsk;
 public enum Pleiegrad {
     INGEN,
     KONTINUERLIG_TILSYN,
-    UTVIDET_TILSYN,
+    UTVIDET_KONTINUERLIG_TILSYN,
     INNLEGGELSE
 }
