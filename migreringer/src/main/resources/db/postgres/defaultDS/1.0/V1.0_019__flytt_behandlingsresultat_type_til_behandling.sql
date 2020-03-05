4929df4da2dd01e52e418ee15d3e571f0775bf85
Frode Carlsen <frode.odde.carlsen@nav.no>
2020-03-05 11:50:46 +0100

    fix: endrer sql i V1.0_019 ta hensyn til eksisterende rader

--- migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_019__flytt_behandlingsresultat_type_til_behandling.sql
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_019__flytt_behandlingsresultat_type_til_behandling.sql
@@ -1,4 +1,4 @@
 alter table behandling add column BEHANDLING_RESULTAT_TYPE VARCHAR(100) DEFAULT 'IKKE_FASTSATT' NOT NULL;
-update behandling b set behandling_resultat_type = (select behandling_resultat_type from behandling_resultat br where br.behandling_id = b.id and br.behandling_resultat_type is not null);
+update behandling b set behandling_resultat_type = coalesce((select behandling_resultat_type from behandling_resultat br where br.behandling_id = b.id and br.behandling_resultat_type is not null), 'IKKE_FASTSATT');
 alter table behandling_resultat drop column behandling_resultat_type;
 drop table BEHANDLING_RESULTAT_YT_KONSEK cascade;
