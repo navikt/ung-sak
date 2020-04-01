12582b088bef16095209e8dc9d90060b037d18df
Frode Carlsen <frode.odde.carlsen@nav.no>
2020-04-01 14:19:51 +0200

    fix: introduserer INIT_PERIODER, INIT_VILKÅR steg (flyttet tidligere startsteg logikk til INIT_VILKÅR. FLyttet tidligere KOFAKUT logikk til INIT_PERIODER). Behold opprinnelig steg som placeholdere (#481)

---
+++ behandlingsprosess/src/main/java/no/nav/k9/sak/domene/behandling/steg/InitierVilkårSteg.java
@@ -0,0 +1,73 @@
+package no.nav.k9.sak.domene.behandling.steg;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Any;
+import javax.enterprise.inject.Instance;
+import javax.inject.Inject;
+
+import no.nav.k9.sak.behandlingskontroll.BehandleStegResultat;
+import no.nav.k9.sak.behandlingskontroll.BehandlingSteg;
+import no.nav.k9.sak.behandlingskontroll.BehandlingStegRef;
+import no.nav.k9.sak.behandlingskontroll.BehandlingTypeRef;
+import no.nav.k9.sak.behandlingskontroll.BehandlingskontrollKontekst;
+import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
+import no.nav.k9.sak.behandlingslager.behandling.Behandling;
+import no.nav.k9.sak.behandlingslager.behandling.repository.BehandlingRepository;
+import no.nav.k9.sak.behandlingslager.behandling.vilkår.VilkårResultatBuilder;
+import no.nav.k9.sak.behandlingslager.behandling.vilkår.VilkårResultatRepository;
+import no.nav.k9.sak.behandlingslager.behandling.vilkår.Vilkårene;
+import no.nav.k9.sak.inngangsvilkår.perioder.VilkårsPerioderTilVurderingTjeneste;
+
+@BehandlingStegRef(kode = "INIT_VILKÅR")
+@BehandlingTypeRef
+@FagsakYtelseTypeRef
+@ApplicationScoped
+public class InitierVilkårSteg implements BehandlingSteg {
+
+    private BehandlingRepository behandlingRepository;
+    private VilkårResultatRepository vilkårResultatRepository;
+    private Instance<VilkårsPerioderTilVurderingTjeneste> vilkårsPerioderTilVurderingTjenester;
+
+    InitierVilkårSteg() {
+        // for CDI proxy
+    }
+
+    @Inject
+    public InitierVilkårSteg(BehandlingRepository behandlingRepository,
+                     VilkårResultatRepository vilkårResultatRepository,
+                     @Any Instance<VilkårsPerioderTilVurderingTjeneste> vilkårsPerioderTilVurderingTjenester) {
+        this.behandlingRepository = behandlingRepository;
+        this.vilkårResultatRepository = vilkårResultatRepository;
+        this.vilkårsPerioderTilVurderingTjenester = vilkårsPerioderTilVurderingTjenester;
+    }
+
+    @Override
+    public BehandleStegResultat utførSteg(BehandlingskontrollKontekst kontekst) {
+
+        // Utleder vilkår med en gang
+        utledVilkår(kontekst);
+        return BehandleStegResultat.utførtUtenAksjonspunkter();
+    }
+
+    private void utledVilkår(BehandlingskontrollKontekst kontekst) {
+        Behandling behandling = behandlingRepository.hentBehandling(kontekst.getBehandlingId());
+        opprettVilkår(behandling);
+    }
+
+    private void opprettVilkår(Behandling behandling) {
+        // Opprett Vilkårsresultat med vilkårne som som skal vurderes, og sett dem som ikke vurdert
+        var eksisterendeVilkår = vilkårResultatRepository.hentHvisEksisterer(behandling.getId());
+        VilkårResultatBuilder vilkårBuilder = Vilkårene.builderFraEksisterende(eksisterendeVilkår.orElse(null));
+        var perioderTilVurderingTjeneste = FagsakYtelseTypeRef.Lookup.find(vilkårsPerioderTilVurderingTjenester, behandling.getFagsakYtelseType()).orElseThrow();
+        var vilkårPeriodeMap = perioderTilVurderingTjeneste.utled(behandling.getId());
+        var utledetAvstand = perioderTilVurderingTjeneste.maksMellomliggendePeriodeAvstand();
+        vilkårPeriodeMap.forEach((key, value) -> vilkårBuilder
+            .medMaksMellomliggendePeriodeAvstand(utledetAvstand)
+            .leggTilIkkeVurderteVilkår(new ArrayList<>(value), List.of(key)));
+        var vilkårResultat = vilkårBuilder.build();
+        vilkårResultatRepository.lagre(behandling.getId(), vilkårResultat);
+    }
+}
--- ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/uttak/FaktaOmUttakSteg.java
+++ ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/uttak/FaktaOmUttakSteg.java
@@ -1,8 +1,5 @@
 package no.nav.k9.sak.ytelse.pleiepengerbarn.uttak;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
@@ -12,10 +9,7 @@ import no.nav.k9.sak.behandlingskontroll.BehandlingStegRef;
 import no.nav.k9.sak.behandlingskontroll.BehandlingTypeRef;
 import no.nav.k9.sak.behandlingskontroll.BehandlingskontrollKontekst;
 import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
-import no.nav.k9.sak.domene.uttak.repo.UttakAktivitet;
-import no.nav.k9.sak.domene.uttak.repo.UttakAktivitetPeriode;
 import no.nav.k9.sak.domene.uttak.repo.UttakRepository;
-import no.nav.k9.sak.typer.InternArbeidsforholdRef;
 
 @ApplicationScoped
 @BehandlingStegRef(kode = "KOFAKUT")
@@ -34,28 +28,18 @@ public class FaktaOmUttakSteg implements BehandlingSteg {
         this.uttakRepository = uttakRepository;
     }
 
+    @SuppressWarnings("unused")
     @Override
     public BehandleStegResultat utførSteg(BehandlingskontrollKontekst kontekst) {
         Long behandlingId = kontekst.getBehandlingId();
         var oppgittUttak = uttakRepository.hentOppgittUttak(behandlingId);
 
         // TODO (FC): åpne aksjonspunkter når følgende
-        // 1. sjekk mismatch mellom uttakaktiviteter og godkjente arbeidsforhold (ta utgangspunkt i godkjent for beregningsgrunnlag?)
+        // 1. sjekk mismatch mellom uttakaktiviteter og godkjente arbeidsforhold fra opptjening/kontroller arbeidsforhold (ta utgangspunkt i
+        // godkjent for beregningsgrunnlag?)
         // 2. sjekk om flere arbeisforhold godkjent per arbeidsgiver
-
-        // tar rå kopi av dataene i første omgang
-        List<UttakAktivitetPeriode> mappedPerioder = oppgittUttak.getPerioder().stream().map(this::mapUttakAktivitetPeriode).collect(Collectors.toList());
-        var fastsattUttak = new UttakAktivitet(mappedPerioder);
-        uttakRepository.lagreOgFlushFastsattUttak(behandlingId, fastsattUttak);
 
         return BehandleStegResultat.utførtUtenAksjonspunkter();
     }
 
-    private UttakAktivitetPeriode mapUttakAktivitetPeriode(UttakAktivitetPeriode p) {
-        return new UttakAktivitetPeriode(p.getPeriode().getFomDato(), p.getPeriode().getTomDato(),
-            p.getAktivitetType(), p.getArbeidsgiver(),
-            p.getArbeidsforholdRef() != null ? p.getArbeidsforholdRef() : InternArbeidsforholdRef.nullRef(), // legger på null objekt her for sammenligne enklere senre
-            p.getJobberNormaltPerUke(),
-            p.getSkalJobbeProsent());
-    }
 }
