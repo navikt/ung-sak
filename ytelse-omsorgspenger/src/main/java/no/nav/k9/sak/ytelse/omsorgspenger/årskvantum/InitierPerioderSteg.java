2710aba9e3d49fae364473ec0e10e5a1220fd6ae
Frode Carlsen <frode.odde.carlsen@nav.no>
2020-04-01 14:01:04 +0200

    fix: introduserer INIT_PERIODER, INIT_VILKÅR steg (flyttet tidligere startsteg logikk til INIT_VILKÅR. FLyttet tidligere KOFAKUT logikk til INIT_PERIODER). Behold opprinnelig steg som placeholdere

---
+++ ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/årskvantum/InitierPerioderSteg.java
@@ -0,0 +1,91 @@
+package no.nav.k9.sak.ytelse.omsorgspenger.årskvantum;
+
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import no.nav.k9.sak.behandlingskontroll.BehandleStegResultat;
+import no.nav.k9.sak.behandlingskontroll.BehandlingSteg;
+import no.nav.k9.sak.behandlingskontroll.BehandlingStegRef;
+import no.nav.k9.sak.behandlingskontroll.BehandlingTypeRef;
+import no.nav.k9.sak.behandlingskontroll.BehandlingskontrollKontekst;
+import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
+import no.nav.k9.sak.domene.arbeidsforhold.InntektArbeidYtelseTjeneste;
+import no.nav.k9.sak.domene.arbeidsforhold.InntektsmeldingTjeneste;
+import no.nav.k9.sak.domene.iay.modell.Inntektsmelding;
+import no.nav.k9.sak.skjæringstidspunkt.SkjæringstidspunktTjeneste;
+import no.nav.k9.sak.typer.AktørId;
+import no.nav.k9.sak.ytelse.omsorgspenger.inntektsmelding.InntektsmeldingFravær;
+import no.nav.k9.sak.ytelse.omsorgspenger.repo.OmsorgspengerGrunnlagRepository;
+import no.nav.k9.sak.ytelse.omsorgspenger.repo.OppgittFravær;
+import no.nav.k9.sak.ytelse.omsorgspenger.repo.OppgittFraværPeriode;
+
+/** Samle sammen fakta for fravær. */
+@ApplicationScoped
+@BehandlingStegRef(kode = "INIT_PERIODER")
+@BehandlingTypeRef
+@FagsakYtelseTypeRef("OMP")
+public class InitierPerioderSteg implements BehandlingSteg {
+
+    private InntektsmeldingTjeneste inntektsmeldingTjeneste;
+    private OmsorgspengerGrunnlagRepository grunnlagRepository;
+    private InntektArbeidYtelseTjeneste iayTjeneste;
+    private SkjæringstidspunktTjeneste skjæringstidspunktTjeneste;
+
+    protected InitierPerioderSteg() {
+        // for proxy
+    }
+
+    @Inject
+    public InitierPerioderSteg(OmsorgspengerGrunnlagRepository grunnlagRepository,
+                                       InntektArbeidYtelseTjeneste iayTjeneste,
+                                       InntektsmeldingTjeneste inntektsmeldingTjeneste,
+                                       SkjæringstidspunktTjeneste skjæringstidspunktTjeneste) {
+        this.grunnlagRepository = grunnlagRepository;
+        this.iayTjeneste = iayTjeneste;
+        this.inntektsmeldingTjeneste = inntektsmeldingTjeneste;
+        this.skjæringstidspunktTjeneste = skjæringstidspunktTjeneste;
+    }
+
+    @Override
+    public BehandleStegResultat utførSteg(BehandlingskontrollKontekst kontekst) {
+        Long behandlingId = kontekst.getBehandlingId();
+        AktørId aktørId = kontekst.getAktørId();
+
+        var samletFravær = samleSammenOppgittFravær(behandlingId, aktørId);
+        grunnlagRepository.lagreOgFlushOppgittFravær(behandlingId, samletFravær);
+
+        return BehandleStegResultat.utførtUtenAksjonspunkter();
+    }
+
+    private OppgittFravær samleSammenOppgittFravær(Long behandlingId, AktørId aktørId) {
+        Set<OppgittFraværPeriode> fravær = new LinkedHashSet<>();
+        var oppgittOpt = annetOppgittFravær(behandlingId);
+        if (oppgittOpt.isPresent()) {
+            fravær.addAll(oppgittOpt.get().getPerioder());
+        }
+        var fraværFraInntektsmeldinger = fraværFraInntektsmeldinger(behandlingId, aktørId);
+        fravær.addAll(fraværFraInntektsmeldinger);
+        return new OppgittFravær(fravær);
+    }
+
+    private Optional<OppgittFravær> annetOppgittFravær(Long behandlingId) {
+        return grunnlagRepository.hentOppgittFraværHvisEksisterer(behandlingId);
+    }
+
+    private List<OppgittFraværPeriode> fraværFraInntektsmeldinger(Long behandlingId, AktørId aktørId) {
+        var skjæringstidspunkt = skjæringstidspunktTjeneste.getSkjæringstidspunkter(behandlingId).getUtledetSkjæringstidspunkt();
+        var iayGrunnlag = iayTjeneste.hentGrunnlag(behandlingId);
+        var inntektsmeldinger = inntektsmeldingTjeneste.hentInntektsmeldinger(aktørId, skjæringstidspunkt, iayGrunnlag);
+        return trekkUtFravær(inntektsmeldinger);
+    }
+
+    private List<OppgittFraværPeriode> trekkUtFravær(List<Inntektsmelding> inntektsmeldinger) {
+        return new InntektsmeldingFravær().trekkUtAlleFraværOgValiderOverlapp(inntektsmeldinger);
+    }
+
+}
---
+++ ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/uttak/InitierPerioderSteg.java
@@ -0,0 +1,61 @@
+package no.nav.k9.sak.ytelse.pleiepengerbarn.uttak;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import no.nav.k9.sak.behandlingskontroll.BehandleStegResultat;
+import no.nav.k9.sak.behandlingskontroll.BehandlingSteg;
+import no.nav.k9.sak.behandlingskontroll.BehandlingStegRef;
+import no.nav.k9.sak.behandlingskontroll.BehandlingTypeRef;
+import no.nav.k9.sak.behandlingskontroll.BehandlingskontrollKontekst;
+import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
+import no.nav.k9.sak.domene.uttak.repo.UttakAktivitet;
+import no.nav.k9.sak.domene.uttak.repo.UttakAktivitetPeriode;
+import no.nav.k9.sak.domene.uttak.repo.UttakRepository;
+import no.nav.k9.sak.typer.InternArbeidsforholdRef;
+
+@ApplicationScoped
+@BehandlingStegRef(kode = "INIT_PERIODER")
+@BehandlingTypeRef
+@FagsakYtelseTypeRef("PSB")
+public class InitierPerioderSteg implements BehandlingSteg {
+
+    private UttakRepository uttakRepository;
+
+    protected InitierPerioderSteg() {
+        // for proxy
+    }
+
+    @Inject
+    public InitierPerioderSteg(UttakRepository uttakRepository) {
+        this.uttakRepository = uttakRepository;
+    }
+
+    @Override
+    public BehandleStegResultat utførSteg(BehandlingskontrollKontekst kontekst) {
+        Long behandlingId = kontekst.getBehandlingId();
+        var oppgittUttak = uttakRepository.hentOppgittUttak(behandlingId);
+
+        // TODO (FC): åpne aksjonspunkter når følgende
+        // 1. sjekk mismatch mellom uttakaktiviteter og godkjente arbeidsforhold (ta utgangspunkt i godkjent for beregningsgrunnlag?)
+        // 2. sjekk om flere arbeisforhold godkjent per arbeidsgiver
+
+        // tar rå kopi av dataene i første omgang
+        List<UttakAktivitetPeriode> mappedPerioder = oppgittUttak.getPerioder().stream().map(this::mapUttakAktivitetPeriode).collect(Collectors.toList());
+        var fastsattUttak = new UttakAktivitet(mappedPerioder);
+        uttakRepository.lagreOgFlushFastsattUttak(behandlingId, fastsattUttak);
+
+        return BehandleStegResultat.utførtUtenAksjonspunkter();
+    }
+
+    private UttakAktivitetPeriode mapUttakAktivitetPeriode(UttakAktivitetPeriode p) {
+        return new UttakAktivitetPeriode(p.getPeriode().getFomDato(), p.getPeriode().getTomDato(),
+            p.getAktivitetType(), p.getArbeidsgiver(),
+            p.getArbeidsforholdRef() != null ? p.getArbeidsforholdRef() : InternArbeidsforholdRef.nullRef(), // legger på null objekt her for sammenligne enklere senre
+            p.getJobberNormaltPerUke(),
+            p.getSkalJobbeProsent());
+    }
+}
