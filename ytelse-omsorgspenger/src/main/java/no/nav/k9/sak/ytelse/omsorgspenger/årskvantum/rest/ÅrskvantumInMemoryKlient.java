7ee2d62111117d4ae01ea2411d2edda868c6880f
Henning Solberg <henning.solberg@nav.no>
2020-04-07 14:30:45 +0200

    fikset ÅrskvantumInMemoryKlient

--- ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/årskvantum/rest/ÅrskvantumInMemoryKlient.java
+++ ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/årskvantum/rest/ÅrskvantumInMemoryKlient.java
@@ -1,10 +1,11 @@
 package no.nav.k9.sak.ytelse.omsorgspenger.årskvantum.rest;
 
+import no.nav.k9.sak.ytelse.omsorgspenger.årskvantum.api.ÅrskvantumRequest;
+import no.nav.k9.sak.ytelse.omsorgspenger.årskvantum.api.ÅrskvantumRest;
+import no.nav.k9.sak.ytelse.omsorgspenger.årskvantum.api.ÅrskvantumResultat;
+
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Alternative;
-
-import no.nav.k9.sak.ytelse.omsorgspenger.årskvantum.api.ÅrskvantumRequest;
-import no.nav.k9.sak.ytelse.omsorgspenger.årskvantum.api.ÅrskvantumResultat;
 
 @RequestScoped
 @Alternative
@@ -15,6 +16,26 @@ public class ÅrskvantumInMemoryKlient implements ÅrskvantumKlient {
     @Override
     public ÅrskvantumResultat hentÅrskvantumUttak(ÅrskvantumRequest årskvantumRequest) {
         return årskvantumResultat;
+    }
+
+    @Override
+    public void avbrytÅrskvantumForBehandling(String behandlingId) {
+
+    }
+
+    @Override
+    public ÅrskvantumResultat hentÅrskvantumForBehandling(String behandlingId) {
+        return null;
+    }
+
+    @Override
+    public ÅrskvantumResultat hentÅrskvantumForFagsak(String fagsakId) {
+        return null;
+    }
+
+    @Override
+    public ÅrskvantumRest hentResterendeKvantum(String aktørId) {
+        return null;
     }
 
     public void setÅrskvantumResultat(ÅrskvantumResultat årskvantumResultat) {
