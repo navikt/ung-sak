90a5422347014ff7f27a5977e9ae417d0a6354f3
olovholm <ola.lovholm@nav.no>
2020-04-09 14:57:45 +0200

    Toggler sjekk av harÅpenTilbakekrevingsbehandling mot FPTILBAKE

--- domenetjenester/okonomistotte/src/main/java/no/nav/k9/sak/økonomi/tilbakekreving/klient/FptilbakeRestKlientImpl.java
+++ domenetjenester/okonomistotte/src/main/java/no/nav/k9/sak/økonomi/tilbakekreving/klient/FptilbakeRestKlientImpl.java
@@ -1,22 +1,25 @@
 package no.nav.k9.sak.økonomi.tilbakekreving.klient;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-
-import org.apache.http.client.utils.URIBuilder;
-
 import no.nav.k9.sak.typer.Saksnummer;
 import no.nav.vedtak.felles.integrasjon.rest.OidcRestClient;
 import no.nav.vedtak.konfig.KonfigVerdi;
+import org.apache.http.client.utils.URIBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @ApplicationScoped
 public class FptilbakeRestKlientImpl implements FptilbakeRestKlient {
 
+    private static final Logger log = LoggerFactory.getLogger(FptilbakeRestKlientImpl.class);
+
     private OidcRestClient restClient;
     private URI uriHarÅpenTilbakekrevingsbehandling;
+    private boolean fptilbakeAktivert;
 
     public FptilbakeRestKlientImpl() {
         // for CDI proxy
@@ -24,15 +27,22 @@ public class FptilbakeRestKlientImpl implements FptilbakeRestKlient {
 
     @Inject
     public FptilbakeRestKlientImpl(OidcRestClient restClient,
-                                   @KonfigVerdi(value = "URL_FPTILBAKE_SJEKK_AAPEN_BEHANDLING", defaultVerdi = "http://fptilbake/fptilbake/api/behandlinger/tilbakekreving/aapen") String urlSjekkÅpenBehandling) {
+                                   @KonfigVerdi(value = "URL_FPTILBAKE_SJEKK_AAPEN_BEHANDLING", defaultVerdi = "http://fptilbake/fptilbake/api/behandlinger/tilbakekreving/aapen") String urlSjekkÅpenBehandling,
+                                   @KonfigVerdi(value = "FPTILBAKE_AKTIVERT", defaultVerdi = "false", required = false) boolean fptilbakeAktivert) {
         this.restClient = restClient;
         this.uriHarÅpenTilbakekrevingsbehandling = tilUri(urlSjekkÅpenBehandling, "URL_FPTILBAKE_SJEKK_AAPEN_BEHANDLING");
+        this.fptilbakeAktivert = fptilbakeAktivert;
     }
 
     @Override
     public boolean harÅpenTilbakekrevingsbehandling(Saksnummer saksnummer) {
         URI uri = leggTilParameter(uriHarÅpenTilbakekrevingsbehandling, "saksnummer", saksnummer.getVerdi());
-        return restClient.get(uri, Boolean.class);
+        if(fptilbakeAktivert){
+            return restClient.get(uri, Boolean.class);
+        } else {
+            log.info("Fptilbake er ikke aktivert - antar at sak {} ikke har tilbakekrevingsbehandling", saksnummer);
+            return false;
+        }
     }
 
     private static URI leggTilParameter(URI uri, String parameterNavn, String parameterVerdi) {
