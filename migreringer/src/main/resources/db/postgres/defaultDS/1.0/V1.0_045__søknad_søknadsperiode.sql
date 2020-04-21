ad557eddf2e1ab6514da145a711c0f24f0941f37
Frode Carlsen <frode.odde.carlsen@nav.no>
2020-04-21 09:43:32 +0200

    fix: manglende aktiv=true sjekk i migrering 45 (#688)

--- migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_045__søknad_søknadsperiode.sql
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_045__søknad_søknadsperiode.sql
@@ -2,7 +2,6 @@ alter table SO_SOEKNAD add column fom date,
                        add column tom date;
 alter table so_soeknad add constraint chk_so_soeknad_fom_tom check ((fom is null and tom is null) OR fom <= tom);
 
-update SO_SOEKNAD s set fom =
-  (select usp.fom from UT_SOEKNADSPERIODE usp INNER JOIN GR_UTTAK gr ON gr.soeknadsperioder_id = usp.soeknadsperioder_id INNER JOIN GR_SOEKNAD grs ON grs.behandling_id=gr.behandling_id where grs.aktiv=true and grs.soeknad_id=s.id);
-update SO_SOEKNAD s set tom = 
-  (select usp.tom from UT_SOEKNADSPERIODE usp INNER JOIN GR_UTTAK gr ON gr.soeknadsperioder_id = usp.soeknadsperioder_id INNER JOIN GR_SOEKNAD grs ON grs.behandling_id=gr.behandling_id where grs.aktiv=true and grs.soeknad_id=s.id);
+update SO_SOEKNAD s set 
+	fom = (select usp.fom from UT_SOEKNADSPERIODE usp INNER JOIN GR_UTTAK gr ON gr.soeknadsperioder_id = usp.soeknadsperioder_id INNER JOIN GR_SOEKNAD grs ON grs.behandling_id=gr.behandling_id where grs.aktiv=true and gr.aktiv = true and grs.soeknad_id=s.id),
+  	tom = (select usp.tom from UT_SOEKNADSPERIODE usp INNER JOIN GR_UTTAK gr ON gr.soeknadsperioder_id = usp.soeknadsperioder_id INNER JOIN GR_SOEKNAD grs ON grs.behandling_id=gr.behandling_id where grs.aktiv=true and gr.aktiv = true and grs.soeknad_id=s.id);
