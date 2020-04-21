750fbf834e4ccaa5b78d10c99ba1ff4ff05359d3
Marius Bækken Glittum <marius.glittum@nav.no>
2020-04-21 07:34:53 +0200

    Henter ut refusjonsbeløpet fra inntektsmeldingene (#678)
    * Henter ut refusjonsbeløpet fra inntektsmeldingene
    * Korrigerer matching av periode da fraværsperioden vil kunne være splittet opp etter matching mot vilkår

--- ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/årskvantum/tjenester/ÅrskvantumTjenesteImpl.java
+++ ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/årskvantum/tjenester/ÅrskvantumTjenesteImpl.java
@@ -1,6 +1,9 @@
 package no.nav.k9.sak.ytelse.omsorgspenger.årskvantum.tjenester;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.stream.Collectors;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Default;
@@ -12,12 +15,16 @@ import no.nav.k9.sak.behandlingslager.aktør.Familierelasjon;
 import no.nav.k9.sak.behandlingslager.behandling.vilkår.VilkårResultatRepository;
 import no.nav.k9.sak.domene.arbeidsforhold.InntektArbeidYtelseTjeneste;
 import no.nav.k9.sak.domene.iay.modell.Inntektsmelding;
+import no.nav.k9.sak.domene.iay.modell.InntektsmeldingAggregat;
 import no.nav.k9.sak.domene.person.tps.TpsTjeneste;
+import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
 import no.nav.k9.sak.kontrakt.uttak.FraværPeriodeOmsorgspenger;
 import no.nav.k9.sak.kontrakt.uttak.OmsorgspengerUtfall;
 import no.nav.k9.sak.kontrakt.uttak.Periode;
 import no.nav.k9.sak.kontrakt.uttak.UttakArbeidsforhold;
 import no.nav.k9.sak.typer.Arbeidsgiver;
+import no.nav.k9.sak.typer.Beløp;
+import no.nav.k9.sak.typer.InternArbeidsforholdRef;
 import no.nav.k9.sak.typer.Saksnummer;
 import no.nav.k9.sak.ytelse.omsorgspenger.repo.OmsorgspengerGrunnlagRepository;
 import no.nav.k9.sak.ytelse.omsorgspenger.årskvantum.api.Barn;
@@ -74,8 +81,12 @@ public class ÅrskvantumTjenesteImpl implements ÅrskvantumTjeneste {
 
         var inntektArbeidYtelseGrunnlag = inntektArbeidYtelseTjeneste.hentGrunnlag(ref.getBehandlingId());
 
-        LocalDateTime datoForSisteInntektsmelding = inntektArbeidYtelseGrunnlag.getInntektsmeldinger()
-            .get().getInntektsmeldingerSomSkalBrukes().stream().map(Inntektsmelding::getInnsendingstidspunkt).max(LocalDateTime::compareTo).get();
+        var inntektsmeldingAggregat = inntektArbeidYtelseGrunnlag.getInntektsmeldinger().orElseThrow();
+        var datoForSisteInntektsmelding = inntektsmeldingAggregat.getInntektsmeldingerSomSkalBrukes()
+            .stream()
+            .map(Inntektsmelding::getInnsendingstidspunkt)
+            .max(LocalDateTime::compareTo)
+            .orElseThrow();
 
         årskvantumRequest.setBehandlingUUID(ref.getBehandlingUuid().toString());
         årskvantumRequest.setSaksnummer(ref.getSaksnummer().getVerdi());
@@ -100,6 +111,7 @@ public class ÅrskvantumTjenesteImpl implements ÅrskvantumTjeneste {
                 uttaksperiodeOmsorgspenger.setUttakArbeidsforhold(new UttakArbeidsforhold(null, null, fraværPeriode.getAktivitetType(), null));
             } else {
                 String arbeidsforholdId = fraværPeriode.getArbeidsforholdRef() == null ? null : fraværPeriode.getArbeidsforholdRef().getReferanse();
+                var refusjonsbeløp = utledRefusjonsbeløp(inntektsmeldingAggregat, arb, fraværPeriode.getArbeidsforholdRef(), fraværPeriode.getPeriode());
                 uttaksperiodeOmsorgspenger.setUttakArbeidsforhold(new UttakArbeidsforhold(arb.getOrgnr(),
                     arb.getAktørId(),
                     fraværPeriode.getAktivitetType(),
@@ -108,6 +120,35 @@ public class ÅrskvantumTjenesteImpl implements ÅrskvantumTjeneste {
             årskvantumRequest.getUttaksperioder().add(uttaksperiodeOmsorgspenger);
         }
         return årskvantumKlient.hentÅrskvantumUttak(årskvantumRequest);
+    }
+
+    private BigDecimal utledRefusjonsbeløp(InntektsmeldingAggregat inntektsmeldingAggregat,
+                                           Arbeidsgiver arbeidsgiver,
+                                           InternArbeidsforholdRef arbeidsforholdRef,
+                                           DatoIntervallEntitet periode) {
+        var inntektsmeldinger = inntektsmeldingAggregat.getInntektsmeldingerFor(arbeidsgiver);
+        var inntektsmeldingSomMatcherUttak = inntektsmeldinger.stream()
+            .filter(it -> it.getArbeidsforholdRef().gjelderFor(arbeidsforholdRef))
+            .filter(it -> it.getOppgittFravær().stream()
+                .anyMatch(fravære -> periode.overlapper(DatoIntervallEntitet.fraOgMedTilOgMed(fravære.getFom(), fravære.getTom()))))
+            .map(Inntektsmelding::getRefusjonBeløpPerMnd)
+            .collect(Collectors.toSet());
+
+        if (inntektsmeldingSomMatcherUttak.isEmpty()) {
+            return null;
+        } else if (inntektsmeldingSomMatcherUttak.size() == 1) {
+            return inntektsmeldingSomMatcherUttak.iterator().next().getVerdi();
+        } else {
+            // Tar nyeste
+            return inntektsmeldinger.stream()
+                .filter(it -> it.getArbeidsforholdRef().gjelderFor(arbeidsforholdRef))
+                .filter(it -> it.getOppgittFravær().stream()
+                    .anyMatch(fravære -> periode.overlapper(DatoIntervallEntitet.fraOgMedTilOgMed(fravære.getFom(), fravære.getTom()))))
+                .max(Comparator.comparing(Inntektsmelding::getInnsendingstidspunkt))
+                .map(Inntektsmelding::getRefusjonBeløpPerMnd)
+                .map(Beløp::getVerdi)
+                .orElse(null);
+        }
     }
 
     @Override
