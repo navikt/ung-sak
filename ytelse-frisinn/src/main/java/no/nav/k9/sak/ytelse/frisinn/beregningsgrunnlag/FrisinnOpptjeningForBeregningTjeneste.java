e0bd882ae08cbda5a16cfc796080c758613ad53b
Frode Carlsen <frode.odde.carlsen@nav.no>
2020-04-22 19:13:18 +0200

    fix: legger på debug dump (#723)

--- ytelse-frisinn/src/main/java/no/nav/k9/sak/ytelse/frisinn/beregningsgrunnlag/FrisinnOpptjeningForBeregningTjeneste.java
+++ ytelse-frisinn/src/main/java/no/nav/k9/sak/ytelse/frisinn/beregningsgrunnlag/FrisinnOpptjeningForBeregningTjeneste.java
@@ -4,11 +4,15 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import no.nav.abakus.iaygrunnlag.IayGrunnlagJsonMapper;
 import no.nav.folketrygdloven.beregningsgrunnlag.kalkulus.OpptjeningAktiviteter;
 import no.nav.folketrygdloven.beregningsgrunnlag.kalkulus.OpptjeningAktiviteter.OpptjeningPeriode;
 import no.nav.folketrygdloven.beregningsgrunnlag.kalkulus.OpptjeningForBeregningTjeneste;
@@ -17,6 +21,7 @@ import no.nav.folketrygdloven.beregningsgrunnlag.regelmodell.Periode;
 import no.nav.k9.kodeverk.opptjening.OpptjeningAktivitetType;
 import no.nav.k9.sak.behandling.BehandlingReferanse;
 import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
+import no.nav.k9.sak.domene.abakus.mapping.IAYTilDtoMapper;
 import no.nav.k9.sak.domene.iay.modell.InntektArbeidYtelseGrunnlag;
 import no.nav.k9.sak.domene.iay.modell.Opptjeningsnøkkel;
 import no.nav.k9.sak.domene.opptjening.OpptjeningAktivitetVurderingBeregning;
@@ -48,12 +53,26 @@ public class FrisinnOpptjeningForBeregningTjeneste implements OpptjeningForBereg
     @Override
     public OpptjeningAktiviteter hentEksaktOpptjeningForBeregning(BehandlingReferanse ref,
                                                                   InntektArbeidYtelseGrunnlag iayGrunnlag) {
-        Optional<OpptjeningAktiviteter> opptjeningAktiviteter = hentOpptjeningForBeregning(ref, iayGrunnlag, ref.getUtledetSkjæringstidspunkt(), LocalDate.of(2017, 3, 1));
+        LocalDate stp = ref.getUtledetSkjæringstidspunkt();
+        LocalDate fom = LocalDate.of(2017, 3, 1);
+        Optional<OpptjeningAktiviteter> opptjeningAktiviteter = hentOpptjeningForBeregning(ref, iayGrunnlag, stp, fom);
 
         if (opptjeningAktiviteter.isEmpty()) {
-            throw new IllegalStateException("Forventer opptjening!!!");
+            String iayDump = dumpForDebug(ref, iayGrunnlag);
+            throw new IllegalStateException(String.format("Forventer opptjening!!! ref=%s, stp=%s, fom=%s: iay=", ref, stp, fom, iayDump));
         }
         return opptjeningAktiviteter.get();
+    }
+
+    private String dumpForDebug(BehandlingReferanse ref, InntektArbeidYtelseGrunnlag iayGrunnlag) {
+        var dto = new IAYTilDtoMapper(ref.getAktørId(), UUID.randomUUID(), UUID.randomUUID()).mapTilDto(iayGrunnlag);
+        String iayDump;
+        try {
+            iayDump = IayGrunnlagJsonMapper.getMapper().writeValueAsString(dto);
+        } catch (JsonProcessingException e) {
+            iayDump = e.getMessage();
+        }
+        return iayDump;
     }
 
     private List<OpptjeningsperiodeForSaksbehandling> hentRelevanteOpptjeningsaktiviteterForBeregningFrisinn(BehandlingReferanse behandlingReferanse,
