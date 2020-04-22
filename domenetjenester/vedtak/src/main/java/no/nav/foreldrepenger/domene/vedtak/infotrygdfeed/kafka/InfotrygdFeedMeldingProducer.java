db3b6910ac93bef6e459d346c249343765fccf08
Marius Glittum <marius.glittum@nav.no>
2020-04-22 12:20:34 +0200

    Legger på manglende @Inject og legger til manglende property

--- domenetjenester/vedtak/src/main/java/no/nav/foreldrepenger/domene/vedtak/infotrygdfeed/kafka/InfotrygdFeedMeldingProducer.java
+++ domenetjenester/vedtak/src/main/java/no/nav/foreldrepenger/domene/vedtak/infotrygdfeed/kafka/InfotrygdFeedMeldingProducer.java
@@ -1,8 +1,9 @@
 package no.nav.foreldrepenger.domene.vedtak.infotrygdfeed.kafka;
 
-import no.nav.vedtak.konfig.KonfigVerdi;
-
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import no.nav.vedtak.konfig.KonfigVerdi;
 
 @ApplicationScoped
 public class InfotrygdFeedMeldingProducer extends GenerellMeldingProducer {
@@ -10,14 +11,13 @@ public class InfotrygdFeedMeldingProducer extends GenerellMeldingProducer {
         // for CDI proxy
     }
 
-    public InfotrygdFeedMeldingProducer(
-        @KonfigVerdi("kafka.infotrygdfeed.topic") String topic,
-        @KonfigVerdi("bootstrap.servers") String bootstrapServers,
-        @KonfigVerdi("schema.registry.url") String schemaRegistryUrl,
-        @KonfigVerdi("kafka.infotrygdfeed.client.id") String clientId,
-        @KonfigVerdi("systembruker.username") String username,
-        @KonfigVerdi("systembruker.password") String password
-    ) {
+    @Inject
+    public InfotrygdFeedMeldingProducer(@KonfigVerdi("kafka.infotrygdfeed.topic") String topic,
+                                        @KonfigVerdi("bootstrap.servers") String bootstrapServers,
+                                        @KonfigVerdi("schema.registry.url") String schemaRegistryUrl,
+                                        @KonfigVerdi("kafka.infotrygdfeed.client.id") String clientId,
+                                        @KonfigVerdi("systembruker.username") String username,
+                                        @KonfigVerdi("systembruker.password") String password) {
         super(topic, bootstrapServers, schemaRegistryUrl, clientId, username, password);
     }
 }
