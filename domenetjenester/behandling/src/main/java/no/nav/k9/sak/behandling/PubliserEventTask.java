fb11a981a2d224f95b60474b0aeffc3f39b39aee
Marius Glittum <marius.glittum@nav.no>
2020-04-22 16:02:38 +0200

    Skriver om til å benytte payload istedenfor property for json

--- domenetjenester/behandling/src/main/java/no/nav/k9/sak/behandling/PubliserEventTask.java
+++ domenetjenester/behandling/src/main/java/no/nav/k9/sak/behandling/PubliserEventTask.java
@@ -4,6 +4,5 @@ import no.nav.vedtak.felles.prosesstask.api.ProsessTaskHandler;
 
 public interface PubliserEventTask extends ProsessTaskHandler {
     String TASKTYPE = "oppgavebehandling.PubliserEvent";
-    String PROPERTY_EVENT = "event";
     String PROPERTY_KEY = "topicKey";
 }
