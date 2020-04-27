f74d7bc523bf815ac9e8d9493ad18cf1843df07d
Marius Glittum <marius.glittum@nav.no>
2020-04-27 07:46:25 +0200

    Håndterer overlapp mellom arbeidsgivere ved utledning av vilkårsperioder

--- ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/vilkår/SøktePerioder.java
+++ ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/vilkår/SøktePerioder.java
@@ -1,11 +1,12 @@
 package no.nav.k9.sak.ytelse.omsorgspenger.vilkår;
 
-import java.util.Collections;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import no.nav.fpsak.tidsserie.LocalDateSegment;
 import no.nav.fpsak.tidsserie.LocalDateTimeline;
+import no.nav.fpsak.tidsserie.StandardCombinators;
 import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
 import no.nav.k9.sak.inngangsvilkår.perioder.VilkårsPeriodiseringsFunksjon;
 import no.nav.k9.sak.ytelse.omsorgspenger.repo.OmsorgspengerGrunnlagRepository;
@@ -27,14 +28,26 @@ class SøktePerioder implements VilkårsPeriodiseringsFunksjon {
         if (søknadsperioder.isEmpty()) {
             return Set.of();
         } else {
-            final var perioder = søknadsperioder.map(OppgittFravær::getPerioder).orElse(Collections.emptySet()).stream().map(OppgittFraværPeriode::getPeriode).collect(Collectors.toSet());
-
-            final var timeline = new LocalDateTimeline<>(perioder.stream().map(a -> new LocalDateSegment<>(a.getFomDato(), a.getTomDato(), true)).collect(Collectors.toList())).compress();
-
-            return timeline.toSegments()
-                .stream()
-                .map(segment -> DatoIntervallEntitet.fraOgMedTilOgMed(segment.getFom(), segment.getTom()))
-                .collect(Collectors.toSet());
+            return utledPeriodeFraSøknadsPerioder(søknadsperioder.get());
         }
+    }
+
+    Set<DatoIntervallEntitet> utledPeriodeFraSøknadsPerioder(OppgittFravær søknadsperioder) {
+        var timeline = new LocalDateTimeline<Boolean>(List.of());
+        var perioder = søknadsperioder.getPerioder()
+            .stream()
+            .map(OppgittFraværPeriode::getPeriode)
+            .map(it -> new LocalDateSegment<>(it.getFomDato(), it.getTomDato(), true))
+            .collect(Collectors.toSet());
+
+        for (LocalDateSegment<Boolean> periode : perioder) {
+            timeline = timeline.combine(new LocalDateTimeline<>(List.of(periode)), StandardCombinators::coalesceRightHandSide, LocalDateTimeline.JoinStyle.CROSS_JOIN);
+        }
+
+        return timeline.compress()
+            .toSegments()
+            .stream()
+            .map(segment -> DatoIntervallEntitet.fraOgMedTilOgMed(segment.getFom(), segment.getTom()))
+            .collect(Collectors.toSet());
     }
 }
---
+++ ytelse-omsorgspenger/src/test/java/no/nav/k9/sak/ytelse/omsorgspenger/vilkår/SøktePerioderTest.java
@@ -0,0 +1,28 @@
+package no.nav.k9.sak.ytelse.omsorgspenger.vilkår;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.Duration;
+import java.time.LocalDate;
+
+import org.junit.Test;
+
+import no.nav.k9.kodeverk.uttak.UttakArbeidType;
+import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
+import no.nav.k9.sak.ytelse.omsorgspenger.repo.OppgittFravær;
+import no.nav.k9.sak.ytelse.omsorgspenger.repo.OppgittFraværPeriode;
+
+public class SøktePerioderTest {
+
+    @Test
+    public void skal_håndtere_overlapp_mellom_arbeidsgivere() {
+        var søktePerioder = new SøktePerioder(null);
+        var oppgittFravær = new OppgittFravær(new OppgittFraværPeriode(LocalDate.now().minusWeeks(6), LocalDate.now().minusDays(5), UttakArbeidType.ARBEIDSTAKER, Duration.ZERO),
+            new OppgittFraværPeriode(LocalDate.now().minusDays(5), LocalDate.now(), UttakArbeidType.ARBEIDSTAKER, Duration.ZERO));
+
+        var vilkårsPerioderFraSøktePerioder = søktePerioder.utledPeriodeFraSøknadsPerioder(oppgittFravær);
+
+        assertThat(vilkårsPerioderFraSøktePerioder).hasSize(1);
+        assertThat(vilkårsPerioderFraSøktePerioder.iterator().next()).isEqualTo(DatoIntervallEntitet.fraOgMedTilOgMed(LocalDate.now().minusWeeks(6), LocalDate.now()));
+    }
+}
