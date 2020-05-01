55abbaccba2f6fc1c3f7fccc56e4784ba3f71ced
Espen Velsvik <espenjv@users.noreply.github.com>
2020-05-01 11:30:17 +0200

    TSF-326 Avslag for avkortet grunnlag (#914)

--- behandlingsprosess/src/main/java/no/nav/k9/sak/domene/behandling/steg/beregningsgrunnlag/FastsettBeregningsgrunnlagSteg.java
+++ behandlingsprosess/src/main/java/no/nav/k9/sak/domene/behandling/steg/beregningsgrunnlag/FastsettBeregningsgrunnlagSteg.java
@@ -1,6 +1,7 @@
 package no.nav.k9.sak.domene.behandling.steg.beregningsgrunnlag;
 
 import static no.nav.k9.kodeverk.behandling.BehandlingStegType.FASTSETT_BEREGNINGSGRUNNLAG;
+import static no.nav.k9.sak.behandlingskontroll.transisjoner.FellesTransisjoner.FREMHOPP_TIL_FORESLÅ_BEHANDLINGSRESULTAT;
 
 import java.util.Collections;
 
@@ -10,6 +11,7 @@ import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 
 import no.nav.folketrygdloven.beregningsgrunnlag.kalkulus.KalkulusTjeneste;
+import no.nav.folketrygdloven.beregningsgrunnlag.output.KalkulusResultat;
 import no.nav.k9.kodeverk.behandling.BehandlingStegType;
 import no.nav.k9.sak.behandling.BehandlingReferanse;
 import no.nav.k9.sak.behandlingskontroll.BehandleStegResultat;
@@ -20,6 +22,7 @@ import no.nav.k9.sak.behandlingskontroll.BehandlingskontrollKontekst;
 import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
 import no.nav.k9.sak.behandlingslager.behandling.Behandling;
 import no.nav.k9.sak.behandlingslager.behandling.repository.BehandlingRepository;
+import no.nav.k9.sak.skjæringstidspunkt.SkjæringstidspunktTjeneste;
 
 @FagsakYtelseTypeRef("*")
 @BehandlingStegRef(kode = "FAST_BERGRUNN")
@@ -29,6 +32,9 @@ public class FastsettBeregningsgrunnlagSteg implements BeregningsgrunnlagSteg {
 
     private BehandlingRepository behandlingRepository;
     private Instance<KalkulusTjeneste> kalkulusTjeneste;
+    private BeregningsgrunnlagVilkårTjeneste vilkårTjeneste;
+    private BehandletPeriodeTjeneste behandletPeriodeTjeneste;
+    private SkjæringstidspunktTjeneste skjæringstidspunktTjeneste;
 
     protected FastsettBeregningsgrunnlagSteg() {
         // for CDI proxy
@@ -36,18 +42,32 @@ public class FastsettBeregningsgrunnlagSteg implements BeregningsgrunnlagSteg {
 
     @Inject
     public FastsettBeregningsgrunnlagSteg(BehandlingRepository behandlingRepository,
-                                          @Any Instance<KalkulusTjeneste> kalkulusTjeneste) {
+                                          @Any Instance<KalkulusTjeneste> kalkulusTjeneste,
+                                          BeregningsgrunnlagVilkårTjeneste vilkårTjeneste,
+                                          BehandletPeriodeTjeneste behandletPeriodeTjeneste,
+                                          SkjæringstidspunktTjeneste skjæringstidspunktTjeneste) {
 
         this.kalkulusTjeneste = kalkulusTjeneste;
         this.behandlingRepository = behandlingRepository;
+        this.vilkårTjeneste = vilkårTjeneste;
+        this.behandletPeriodeTjeneste = behandletPeriodeTjeneste;
+        this.skjæringstidspunktTjeneste = skjæringstidspunktTjeneste;
     }
 
     @Override
     public BehandleStegResultat utførSteg(BehandlingskontrollKontekst kontekst) {
         Behandling behandling = behandlingRepository.hentBehandling(kontekst.getBehandlingId());
-        FagsakYtelseTypeRef.Lookup.find(kalkulusTjeneste, behandling.getFagsakYtelseType())
+        KalkulusResultat kalkulusResultat = FagsakYtelseTypeRef.Lookup.find(kalkulusTjeneste, behandling.getFagsakYtelseType())
             .orElseThrow(() -> new IllegalArgumentException("Fant ikke kalkulustjeneste"))
             .fortsettBeregning(BehandlingReferanse.fra(behandling), FASTSETT_BEREGNINGSGRUNNLAG);
+        if (kalkulusResultat.getVilkårOppfylt() != null && !kalkulusResultat.getVilkårOppfylt()) {
+            var skjæringstidspunkter = skjæringstidspunktTjeneste.getSkjæringstidspunkter(behandling.getId());
+            var ref = BehandlingReferanse.fra(behandling, skjæringstidspunkter);
+            var vilkårsPeriode = behandletPeriodeTjeneste.utledPeriode(ref);
+            var orginalVilkårsPeriode = behandletPeriodeTjeneste.utledOrginalVilkårsPeriode(ref);
+            vilkårTjeneste.lagreAvslåttVilkårresultat(kontekst, vilkårsPeriode, orginalVilkårsPeriode, kalkulusResultat.getAvslagsårsak());
+            return BehandleStegResultat.fremoverført(FREMHOPP_TIL_FORESLÅ_BEHANDLINGSRESULTAT);
+        }
         return BehandleStegResultat.utførtMedAksjonspunktResultater(Collections.emptyList());
     }
 
