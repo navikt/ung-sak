98f6f5352928423e981abb80b0f3496c610bc127
Petter Ekern <43809623+pekern@users.noreply.github.com>
2020-05-18 07:56:11 +0200

    FIX: Setter endringsdato for frisinn (#1198)

--- ytelse-frisinn/src/main/java/no/nav/k9/sak/ytelse/frisinn/skjæringstidspunkt/FrisinnOpphørtidspunktTjeneste.java
+++ ytelse-frisinn/src/main/java/no/nav/k9/sak/ytelse/frisinn/skjæringstidspunkt/FrisinnOpphørtidspunktTjeneste.java
@@ -1,32 +1,31 @@
 package no.nav.k9.sak.ytelse.frisinn.skjæringstidspunkt;
 
 import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.Optional;
-import java.util.UUID;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
 import no.nav.k9.sak.behandling.BehandlingReferanse;
 import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
-import no.nav.k9.sak.domene.uttak.UttakTjeneste;
+import no.nav.k9.sak.domene.uttak.repo.UttakAktivitet;
+import no.nav.k9.sak.domene.uttak.repo.UttakRepository;
 import no.nav.k9.sak.skjæringstidspunkt.YtelseOpphørtidspunktTjeneste;
 
 @FagsakYtelseTypeRef("FRISINN")
 @ApplicationScoped
 public class FrisinnOpphørtidspunktTjeneste implements YtelseOpphørtidspunktTjeneste {
 
-    private UttakTjeneste uttakTjeneste;
-    private OpphørUttakTjeneste opphørUttakTjeneste;
+    private UttakRepository uttakRepository;
 
     protected FrisinnOpphørtidspunktTjeneste() {
         // for CDI proxy
     }
 
     @Inject
-    public FrisinnOpphørtidspunktTjeneste(UttakTjeneste uttakTjeneste) {
-        this.uttakTjeneste = uttakTjeneste;
-        this.opphørUttakTjeneste = new OpphørUttakTjeneste(uttakTjeneste);
+    public FrisinnOpphørtidspunktTjeneste(UttakRepository uttakRepository) {
+        this.uttakRepository = uttakRepository;
     }
 
     @Override
@@ -36,20 +35,28 @@ public class FrisinnOpphørtidspunktTjeneste implements YtelseOpphørtidspunktTj
 
     @Override
     public Boolean erOpphørEtterSkjæringstidspunkt(BehandlingReferanse ref) {
-        if (!ref.getBehandlingResultat().isBehandlingsresultatOpphørt()) {
+        if (!erOpphørtRevurdering(ref)) {
             return null; // ikke relevant //NOSONAR
         }
-
-        var uttakOpt = uttakTjeneste.hentUttaksplan(ref.getBehandlingUuid());
-        return uttakOpt.map(uttaksplan -> uttaksplan.harInnvilgetPerioder()).orElse(false);
+        Optional<LocalDate> førsteUttaksdato = hentFørsteUttaksdag(ref);
+        return førsteUttaksdato.isPresent();
     }
 
     @Override
     public Optional<LocalDate> getOpphørsdato(BehandlingReferanse ref) {
-        return opphørUttakTjeneste.getOpphørsdato(ref);
+        if (!erOpphørtRevurdering(ref)) {
+            return Optional.empty(); // ikke relevant
+        }
+        return hentFørsteUttaksdag(ref);
     }
-    
-    public boolean harAvslåttUttakPeriode(UUID behandlingUuid) {
-        return opphørUttakTjeneste.harAvslåttUttakPeriode(behandlingUuid);
+
+    private boolean erOpphørtRevurdering(BehandlingReferanse ref) {
+        return ref.erRevurdering() && ref.getOriginalBehandlingId().isPresent() && ref.getBehandlingResultat().isBehandlingsresultatOpphørt();
+    }
+
+    private Optional<LocalDate> hentFørsteUttaksdag(BehandlingReferanse ref) {
+        UttakAktivitet originaltFastsattUttak = uttakRepository.hentFastsattUttak(ref.getOriginalBehandlingId().get());
+        return originaltFastsattUttak.getPerioder().stream()
+            .map(p -> p.getPeriode().getFomDato()).min(Comparator.naturalOrder());
     }
 }
