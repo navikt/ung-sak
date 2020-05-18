834282696181d6b8b6e002913857723878eb191e
Ole Jørgen Rishoff <olejri@gmail.com>
2020-05-18 13:04:34 +0200

    Endrer nå fom på uttaksperiode (#1211)
    Co-authored-by: Ole Jørgen Rishoff <ole.jorgen.rishoff@nav.no>

---
+++ web/src/test/java/no/nav/k9/sak/web/app/tjenester/behandling/arbeidsforhold/OverstyringOppgittOpptjeningOppdatererTest.java
@@ -0,0 +1,85 @@
+package no.nav.k9.sak.web.app.tjenester.behandling.arbeidsforhold;
+
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import no.nav.k9.sak.kontrakt.arbeidsforhold.OppgittEgenNæringDto;
+import no.nav.k9.sak.kontrakt.arbeidsforhold.OppgittFrilansDto;
+import no.nav.k9.sak.kontrakt.arbeidsforhold.OppgittFrilansoppdragDto;
+import no.nav.k9.sak.kontrakt.arbeidsforhold.OppgittOpptjeningDto;
+import no.nav.k9.sak.kontrakt.arbeidsforhold.PeriodeDto;
+import no.nav.k9.sak.kontrakt.arbeidsforhold.SøknadsperiodeOgOppgittOpptjeningDto;
+import no.nav.k9.sak.typer.Beløp;
+
+public class OverstyringOppgittOpptjeningOppdatererTest {
+
+
+    @Test
+    public void skal_teste_at_riktig_dato_blir_satt_hvis_den_har_blitt_fremskynet_av_saksbehanlder_FL() {
+        OverstyringOppgittOpptjeningOppdaterer oppdaterer = new OverstyringOppgittOpptjeningOppdaterer();
+        LocalDate start = LocalDate.of(2020, 4, 1);
+        LocalDate startSN = LocalDate.of(2020, 4, 12);
+        LocalDate startFL = LocalDate.of(2020, 4, 10);
+        LocalDate slutt = LocalDate.of(2020, 4, 30);
+
+        PeriodeDto søkerPeriode = new PeriodeDto(start, slutt);
+        PeriodeDto snPeriode = new PeriodeDto(startSN, slutt);
+        PeriodeDto flPeriode = new PeriodeDto(startFL, slutt);
+
+        SøknadsperiodeOgOppgittOpptjeningDto søknadsperiodeOgOppgittOpptjeningDto = lagDto(true, true, snPeriode, flPeriode, søkerPeriode);
+
+        LocalDate dato = oppdaterer.finnFraOgMedDato(søknadsperiodeOgOppgittOpptjeningDto);
+
+        Assertions.assertThat(dato).isEqualTo(startFL);
+    }
+
+    @Test
+    public void skal_teste_at_riktig_dato_blir_satt_hvis_den_har_blitt_fremskynet_av_saksbehanlder_SN() {
+        OverstyringOppgittOpptjeningOppdaterer oppdaterer = new OverstyringOppgittOpptjeningOppdaterer();
+        LocalDate start = LocalDate.of(2020, 4, 1);
+        LocalDate startSN = LocalDate.of(2020, 4, 9);
+        LocalDate startFL = LocalDate.of(2020, 4, 10);
+        LocalDate slutt = LocalDate.of(2020, 4, 30);
+
+        PeriodeDto søkerPeriode = new PeriodeDto(start, slutt);
+        PeriodeDto snPeriode = new PeriodeDto(startSN, slutt);
+        PeriodeDto flPeriode = new PeriodeDto(startFL, slutt);
+
+        SøknadsperiodeOgOppgittOpptjeningDto søknadsperiodeOgOppgittOpptjeningDto = lagDto(true, true, snPeriode, flPeriode, søkerPeriode);
+
+        LocalDate dato = oppdaterer.finnFraOgMedDato(søknadsperiodeOgOppgittOpptjeningDto);
+
+        Assertions.assertThat(dato).isEqualTo(startSN);
+    }
+
+    private SøknadsperiodeOgOppgittOpptjeningDto lagDto(Boolean søkerFrilans, Boolean søkerSN, PeriodeDto periodeSN, PeriodeDto periodeFL, PeriodeDto periodeFraSøknad) {
+        SøknadsperiodeOgOppgittOpptjeningDto søknadsperiodeOgOppgittOpptjeningDto = new SøknadsperiodeOgOppgittOpptjeningDto();
+        søknadsperiodeOgOppgittOpptjeningDto.setPeriodeFraSøknad(periodeFraSøknad);
+        søknadsperiodeOgOppgittOpptjeningDto.setSøkerYtelseForFrilans(søkerFrilans);
+        søknadsperiodeOgOppgittOpptjeningDto.setSøkerYtelseForNæring(søkerSN);
+        OppgittOpptjeningDto oppgittOpptjeningDtoI = new OppgittOpptjeningDto();
+        OppgittOpptjeningDto oppgittOpptjeningDtoFør = new OppgittOpptjeningDto();
+
+        OppgittFrilansDto oppgittFrilansDto = new OppgittFrilansDto();
+        OppgittFrilansoppdragDto oppgittFrilansoppdragDto = new OppgittFrilansoppdragDto();
+        oppgittFrilansoppdragDto.setPeriode(periodeFL);
+        oppgittFrilansoppdragDto.setBruttoInntekt(new Beløp(BigDecimal.TEN));
+        oppgittFrilansDto.setOppgittFrilansoppdrag(List.of(oppgittFrilansoppdragDto));
+        oppgittOpptjeningDtoI.setOppgittFrilans(oppgittFrilansDto);
+
+        OppgittEgenNæringDto egenNæringDto = new OppgittEgenNæringDto();
+        egenNæringDto.setPeriode(periodeSN);
+        egenNæringDto.setBruttoInntekt(new Beløp(BigDecimal.TEN));
+        oppgittOpptjeningDtoI.setOppgittEgenNæring(List.of(egenNæringDto));
+
+        søknadsperiodeOgOppgittOpptjeningDto.setISøkerPerioden(oppgittOpptjeningDtoI);
+        søknadsperiodeOgOppgittOpptjeningDto.setFørSøkerPerioden(oppgittOpptjeningDtoFør);
+
+        return søknadsperiodeOgOppgittOpptjeningDto;
+    }
+}
