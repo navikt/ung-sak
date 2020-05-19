f68452085deeca89d881e872e17387de5b0a415c
Henning Solberg <henning.solberg@nav.no>
2020-05-19 15:56:58 +0200

    Deaktiverer bare ved avslått behandlingsresultat

--- ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/årskvantum/task/ÅrskvantumDeaktiveringTask.java
+++ ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/årskvantum/task/ÅrskvantumDeaktiveringTask.java
@@ -6,6 +6,7 @@ import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import no.nav.k9.kodeverk.behandling.BehandlingResultatType;
 import no.nav.k9.kodeverk.behandling.FagsakYtelseType;
 import no.nav.k9.sak.behandlingslager.behandling.Behandling;
 import no.nav.k9.sak.behandlingslager.behandling.repository.BehandlingRepositoryProvider;
@@ -40,7 +41,8 @@ public class ÅrskvantumDeaktiveringTask implements ProsessTaskHandler {
 
         Behandling behandling = repositoryProvider.getBehandlingRepository().hentBehandling(pd.getBehandlingId());
 
-        if (FagsakYtelseType.OMP.equals(behandling.getFagsakYtelseType()) || FagsakYtelseType.OMSORGSPENGER.equals(behandling.getFagsakYtelseType())) {
+        if (FagsakYtelseType.OMP.equals(behandling.getFagsakYtelseType()) || FagsakYtelseType.OMSORGSPENGER.equals(behandling.getFagsakYtelseType())
+            && BehandlingResultatType.AVSLÅTT.equals(behandling.getBehandlingResultatType())) {
             logger.info("Setter uttak til inaktivt. behandlingUUID: '{}'", behandling.getUuid());
 
             this.årskvantumRestKlient.deaktiverUttakForBehandling(behandling.getUuid());
