32cb874d74e015a4715e6d0f628981abc598bd6d
Bjornar Askevold <bjornar.askevold@nav.no>
2020-05-19 12:37:54 +0200

    Rettet mangler i fastsatt uttak

--- web/src/main/java/no/nav/k9/sak/web/app/tjenester/behandling/BehandlingDtoTjeneste.java
+++ web/src/main/java/no/nav/k9/sak/web/app/tjenester/behandling/BehandlingDtoTjeneste.java
@@ -330,6 +330,7 @@ public class BehandlingDtoTjeneste {
         switch (ytelseType) {
             case FRISINN:
                 dto.leggTil(getFraMap(UttakRestTjeneste.UTTAK_OPPGITT, "uttak-oppgitt", uuidQueryParams));
+                dto.leggTil(getFraMap(UttakRestTjeneste.UTTAK_FASTSATT, "uttak-fastsatt", uuidQueryParams));
                 dto.leggTil(getFraMap(InntektArbeidYtelseRestTjeneste.OPPGITT_OPPTJEING_PATH, "oppgitt-opptjening", uuidQueryParams));
                 break;
             case OMSORGSPENGER:
