14a714d8dbc20ab57a3909c73135de89646b2658
Henning Solberg <henning.solberg@nav.no>
2020-05-20 20:37:18 +0200

    Definer task for deaktivering av uttak (årskvantum)

---
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_067__deaktiver_aarskvantum_task.sql
@@ -0,0 +1,1 @@
+Insert into PROSESS_TASK_TYPE (KODE,NAVN,FEIL_MAKS_FORSOEK,FEIL_SEK_MELLOM_FORSOEK,FEILHANDTERING_ALGORITME,BESKRIVELSE,CRON_EXPRESSION) values ('iverksetteVedtak.deaktiverUttakForBehandling','Deaktiver uttak i årskvantum ved avslag',3,30,'DEFAULT','Deaktiverer uttak i årskvantum ved avslag', null);
