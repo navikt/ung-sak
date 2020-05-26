4ca7727fa2a7b124066b229e9c2d93245aa45923
s149212 <espen.svendsen@nav.no>
2020-05-26 12:12:15 +0200

    Legger tilbake variant-steg for revurdering, som dummy-steg
    Steget feilet fordi den søkte etter tilknyttet søknad til behandling,og pt. knyttet ikke søknader til revurderinger.
    
    Å fjerne steget fikset problemet for alle nye Frisinn-behandlinger, men ikke allerede feilede, som sto i variant-steget.
    
    Legger tilbake steget, men som rent dummy-steg. Filteret kreves ikke for revurdering.

--- ytelse-frisinn/src/main/java/no/nav/k9/sak/ytelse/frisinn/prosess/ProsessModell.java
+++ ytelse-frisinn/src/main/java/no/nav/k9/sak/ytelse/frisinn/prosess/ProsessModell.java
@@ -52,6 +52,7 @@ public class ProsessModell {
         modellBuilder.medSteg(
             BehandlingStegType.START_STEG,
             BehandlingStegType.VARSEL_REVURDERING,
+            BehandlingStegType.VARIANT_FILTER,
             BehandlingStegType.INIT_VILKÅR,
             BehandlingStegType.INNHENT_REGISTEROPP,
             BehandlingStegType.FASTSETT_SKJÆRINGSTIDSPUNKT_BEREGNING,
