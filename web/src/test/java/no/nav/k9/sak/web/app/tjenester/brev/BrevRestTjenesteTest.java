5aa865ea76d57568ed10386581b057703467dbd8
Qadeer Ahmad Khan <qadeer.ahmad.khan@nav.no>
2020-06-04 20:49:36 +0200

    legger til redusertUtbetalingÅrsaker dto slik at lagrede verdier kan hentes ut.

--- web/src/test/java/no/nav/k9/sak/web/app/tjenester/brev/BrevRestTjenesteTest.java
+++ web/src/test/java/no/nav/k9/sak/web/app/tjenester/brev/BrevRestTjenesteTest.java
@@ -1,11 +1,18 @@
 package no.nav.k9.sak.web.app.tjenester.brev;
 
 import static no.nav.k9.kodeverk.dokument.DokumentMalType.INNHENT_DOK;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+
+import javax.ws.rs.core.Response;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,11 +21,14 @@ import no.nav.k9.kodeverk.historikk.HistorikkAktør;
 import no.nav.k9.sak.behandlingslager.behandling.Behandling;
 import no.nav.k9.sak.behandlingslager.behandling.repository.BehandlingRepository;
 import no.nav.k9.sak.behandlingslager.behandling.vedtak.BehandlingVedtakRepository;
+import no.nav.k9.sak.behandlingslager.behandling.vedtak.VedtakVarsel;
 import no.nav.k9.sak.behandlingslager.behandling.vedtak.VedtakVarselRepository;
 import no.nav.k9.sak.behandlingslager.behandling.vilkår.VilkårResultatRepository;
 import no.nav.k9.sak.dokument.bestill.DokumentBehandlingTjeneste;
 import no.nav.k9.sak.dokument.bestill.DokumentBestillerApplikasjonTjeneste;
+import no.nav.k9.sak.kontrakt.behandling.BehandlingUuidDto;
 import no.nav.k9.sak.kontrakt.dokument.BestillBrevDto;
+import no.nav.k9.sak.kontrakt.vedtak.VedtakVarselDto;
 
 public class BrevRestTjenesteTest {
 
@@ -48,5 +58,23 @@ public class BrevRestTjenesteTest {
 
         // Assert
         verify(dokumentBestillerApplikasjonTjenesteMock).bestillDokument(eq(bestillBrevDto), eq(HistorikkAktør.SAKSBEHANDLER));
+    }
+
+    @Test
+    public void skal_hente_redusertUtbetalingÅrsaker_hentVedtakVarsel() {
+        // Arrange
+        UUID behandlingUuid = UUID.randomUUID();
+        Optional<VedtakVarsel> mockVedtakVarsel = Optional.of(mock(VedtakVarsel.class));
+        Set<String> årsaker = Set.of("ÅRSAK_1", "ÅRSAK_2");
+        when(mockVedtakVarsel.get().getRedusertUtbetalingÅrsaker()).thenReturn(årsaker);
+        when(vedtakVarselRepository.hentHvisEksisterer(behandlingUuid)).thenReturn(mockVedtakVarsel);
+
+        // Act
+        Response response = brevRestTjeneste.hentVedtakVarsel(new BehandlingUuidDto(behandlingUuid));
+
+        // Assert
+        Set<String> redusertUtbetalingÅrsaker = ((VedtakVarselDto) response.getEntity()).getRedusertUtbetalingÅrsaker();
+        assertThat(redusertUtbetalingÅrsaker).isEqualTo(årsaker);
+
     }
 }
