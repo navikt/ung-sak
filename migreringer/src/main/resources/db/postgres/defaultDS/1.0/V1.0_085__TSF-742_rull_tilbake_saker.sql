bcf88a61d7cb95c52a542f7deea032ebf7c4a60c
Espen Johansen Velsvik <espen.velsvik@nav.no>
2020-06-10 11:07:51 +0200

    FIX Kommentar på gruppeSekvens og fikse insert

--- migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_085__TSF-742_rull_tilbake_saker.sql
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_085__TSF-742_rull_tilbake_saker.sql
@@ -1,10 +1,10 @@
 insert into prosess_task (id, task_type, task_gruppe, neste_kjoering_etter, task_parametere)
 select nextval('seq_prosess_task'), 'beregning.tilbakeTilStart',
        nextval('seq_prosess_task_gruppe'), null,
-       (select 'fagsakId=' || b.fagsak_id || '
-                behandlingId=' ||b.id from behandling b
+       'fagsakId=' || b.fagsak_id || '
+                behandlingId=' || b.id from behandling b
             inner join aksjonspunkt a on b.id = a.behandling_id
             where a.aksjonspunkt_def = '5038'
             and a.aksjonspunkt_status = 'OPPR'
-            and a.opprettet_tid < '2020-06-05 07:30:00.000');
+            and a.opprettet_tid < '2020-06-05 07:30:00.000';
 commit;
