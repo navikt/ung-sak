4b6643273d9383452fdb8ec134c3637fb6035491
Marius Glittum <marius.glittum@nav.no>
2020-06-16 14:38:38 +0200

    Legger på postcondition etter all vilkårsprøving for å hindre at det fattes ugyldige vedtak

--- behandlingsprosess/src/main/java/no/nav/k9/sak/domene/behandling/steg/foreslåresultat/ForeslåBehandlingsresultatStegFelles.java
+++ behandlingsprosess/src/main/java/no/nav/k9/sak/domene/behandling/steg/foreslåresultat/ForeslåBehandlingsresultatStegFelles.java
@@ -6,6 +6,7 @@ import javax.enterprise.inject.Instance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import no.nav.k9.kodeverk.vilkår.Utfall;
 import no.nav.k9.sak.behandling.BehandlingReferanse;
 import no.nav.k9.sak.behandlingskontroll.BehandleStegResultat;
 import no.nav.k9.sak.behandlingskontroll.BehandlingskontrollKontekst;
@@ -13,6 +14,8 @@ import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
 import no.nav.k9.sak.behandlingslager.behandling.Behandling;
 import no.nav.k9.sak.behandlingslager.behandling.repository.BehandlingRepository;
 import no.nav.k9.sak.behandlingslager.behandling.repository.BehandlingRepositoryProvider;
+import no.nav.k9.sak.behandlingslager.behandling.vilkår.Vilkår;
+import no.nav.k9.sak.behandlingslager.behandling.vilkår.VilkårResultatRepository;
 import no.nav.k9.sak.skjæringstidspunkt.SkjæringstidspunktTjeneste;
 
 public abstract class ForeslåBehandlingsresultatStegFelles implements ForeslåBehandlingsresultatSteg {
@@ -23,6 +26,7 @@ public abstract class ForeslåBehandlingsresultatStegFelles implements ForeslåB
     private Instance<ForeslåBehandlingsresultatTjeneste> foreslåBehandlingsresultatTjeneste;
 
     private SkjæringstidspunktTjeneste skjæringstidspunktTjeneste;
+    private VilkårResultatRepository vilkårResultatRepository;
 
     protected ForeslåBehandlingsresultatStegFelles() {
         // for CDI proxy
@@ -33,23 +37,39 @@ public abstract class ForeslåBehandlingsresultatStegFelles implements ForeslåB
                                                 SkjæringstidspunktTjeneste skjæringstidspunktTjeneste) {
         this.skjæringstidspunktTjeneste = skjæringstidspunktTjeneste;
         this.behandlingRepository = repositoryProvider.getBehandlingRepository();
+        this.vilkårResultatRepository = repositoryProvider.getVilkårResultatRepository();
         this.foreslåBehandlingsresultatTjeneste = foreslåBehandlingsresultatTjeneste;
     }
 
     @Override
     public BehandleStegResultat utførSteg(BehandlingskontrollKontekst kontekst) {
+
+        validerAtAlleVilkårErVurdert(kontekst.getBehandlingId());
+
         Behandling behandling = behandlingRepository.hentBehandling(kontekst.getBehandlingId());
         var skjæringstidspunkt = skjæringstidspunktTjeneste.getSkjæringstidspunkter(kontekst.getBehandlingId());
         var ref = BehandlingReferanse.fra(behandling, skjæringstidspunkt);
         logger.info("Foreslår behandlingsresultat for behandling {}", (ref != null) ? ref.getBehandlingUuid() : "MANGLER REF");
-        
+
         var tjeneste = FagsakYtelseTypeRef.Lookup.find(foreslåBehandlingsresultatTjeneste, ref.getFagsakYtelseType()).orElseThrow();
         tjeneste.foreslåVedtakVarsel(ref, kontekst);
-        
+
         // TODO (Safir/OSS): Lagre Behandlingsresultat gjennom eget repository
         behandlingRepository.lagre(behandling, kontekst.getSkriveLås());
-        
+
         // Dette steget genererer ingen aksjonspunkter
         return BehandleStegResultat.utførtUtenAksjonspunkter();
+    }
+
+    private void validerAtAlleVilkårErVurdert(Long behandlingId) {
+        var vilkårene = vilkårResultatRepository.hent(behandlingId);
+
+        vilkårene.getVilkårene().forEach(this::validerVilkår);
+    }
+
+    private void validerVilkår(Vilkår vilkår) {
+        if (vilkår.getPerioder().stream().anyMatch(at -> Utfall.IKKE_VURDERT.equals(at.getGjeldendeUtfall()))) {
+            throw new IllegalStateException("Vilkåret " + vilkår.getVilkårType() + " har en eller flere perioder som ikke er vurdert.");
+        }
     }
 }
