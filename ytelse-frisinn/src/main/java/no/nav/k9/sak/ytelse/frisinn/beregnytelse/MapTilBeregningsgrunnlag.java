d9a777beb525235aa3b807d73be3f8291dae2c69
Espen Johansen Velsvik <espen.velsvik@nav.no>
2020-06-19 15:20:49 +0200

    TSF-887 Mapper perioder fra tidligere behandlinger i tilkjent ytelse ved ny søknadsperiode

--- ytelse-frisinn/src/main/java/no/nav/k9/sak/ytelse/frisinn/beregnytelse/MapTilBeregningsgrunnlag.java
+++ ytelse-frisinn/src/main/java/no/nav/k9/sak/ytelse/frisinn/beregnytelse/MapTilBeregningsgrunnlag.java
@@ -1,12 +1,15 @@
 package no.nav.k9.sak.ytelse.frisinn.beregnytelse;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import no.nav.folketrygdloven.beregningsgrunnlag.modell.Beregningsgrunnlag;
 import no.nav.folketrygdloven.beregningsgrunnlag.modell.BeregningsgrunnlagPeriode;
+import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
 
 class MapTilBeregningsgrunnlag {
 
@@ -14,11 +17,21 @@ class MapTilBeregningsgrunnlag {
         // Skjul
     }
 
-    static List<Beregningsgrunnlag> mapBeregningsgrunnlag(Beregningsgrunnlag beregningsgrunnlag) {
-        Set<BeregningsgrunnlagPeriode.Builder> perioder = beregningsgrunnlag.getBeregningsgrunnlagPerioder().stream()
-            .filter(p -> p.getDagsats() != null && p.getDagsats() > 0)
-            .map(BeregningsgrunnlagPeriode::builder)
-            .collect(Collectors.toSet());
+    static List<Beregningsgrunnlag> mapBeregningsgrunnlag(Beregningsgrunnlag beregningsgrunnlag,
+                                                          Optional<Beregningsgrunnlag> beregningsgrunnlagOriginalBehandling,
+                                                          DatoIntervallEntitet sisteSøknadsperiode,
+                                                          boolean erNySøknadsperiode, Boolean skalBenytteTidligereResultat) {
+        Set<BeregningsgrunnlagPeriode.Builder> perioder = new HashSet<>();
+        if (erNySøknadsperiode && skalBenytteTidligereResultat) {
+            perioder.addAll(finnPerioderForNySøknad(beregningsgrunnlag, sisteSøknadsperiode));
+            perioder.addAll(finnOriginalBehandlingPerioder(beregningsgrunnlagOriginalBehandling, sisteSøknadsperiode));
+        } else {
+            perioder.addAll(beregningsgrunnlag.getBeregningsgrunnlagPerioder().stream()
+                .filter(p -> p.getDagsats() != null && p.getDagsats() > 0)
+                .map(BeregningsgrunnlagPeriode::builder)
+                .collect(Collectors.toSet()));
+        }
+
 
         if (perioder.isEmpty()) {
             return Collections.emptyList();
@@ -31,5 +44,24 @@ class MapTilBeregningsgrunnlag {
 
         return List.of(bgBuilder.build());
     }
+
+    private static Set<BeregningsgrunnlagPeriode.Builder> finnOriginalBehandlingPerioder(Optional<Beregningsgrunnlag> beregningsgrunnlagOriginalBehandling, DatoIntervallEntitet sisteSøknadsperiode) {
+        return beregningsgrunnlagOriginalBehandling.stream()
+            .flatMap(orginal -> orginal.getBeregningsgrunnlagPerioder().stream()
+                .filter(p -> p.getDagsats() != null && p.getDagsats() > 0)
+                .filter(p -> !p.getPeriode().getTomDato().isAfter(sisteSøknadsperiode.getFomDato().withDayOfMonth(1).minusDays(1)))
+                .map(BeregningsgrunnlagPeriode::builder))
+            .collect(Collectors.toSet());
+    }
+
+    private static Set<BeregningsgrunnlagPeriode.Builder> finnPerioderForNySøknad(Beregningsgrunnlag beregningsgrunnlag, DatoIntervallEntitet sisteSøknadsperiode) {
+        return beregningsgrunnlag.getBeregningsgrunnlagPerioder().stream()
+            .filter(p -> p.getDagsats() != null && p.getDagsats() > 0)
+            .filter(p -> p.getPeriode().overlapper(sisteSøknadsperiode))
+            .map(BeregningsgrunnlagPeriode::builder)
+            .collect(Collectors.toSet());
+    }
+
+
 
 }
---
+++ ytelse-frisinn/src/test/java/no/nav/k9/sak/ytelse/frisinn/beregnytelse/MapTilBeregningsgrunnlagTest.java
@@ -0,0 +1,141 @@
+package no.nav.k9.sak.ytelse.frisinn.beregnytelse;
+
+import static no.nav.k9.sak.domene.typer.tid.AbstractLocalDateInterval.TIDENES_ENDE;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Test;
+
+import no.nav.folketrygdloven.beregningsgrunnlag.modell.Beregningsgrunnlag;
+import no.nav.folketrygdloven.beregningsgrunnlag.modell.BeregningsgrunnlagPeriode;
+import no.nav.folketrygdloven.beregningsgrunnlag.modell.BeregningsgrunnlagPrStatusOgAndel;
+import no.nav.k9.kodeverk.arbeidsforhold.AktivitetStatus;
+import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
+import no.nav.k9.sak.typer.Beløp;
+
+public class MapTilBeregningsgrunnlagTest {
+
+    public static final LocalDate STP = LocalDate.of(2020, 3, 1);
+
+    @Test
+    public void skal_mappe_førstegangsbehandling() {
+
+        DatoIntervallEntitet søknadsperiode = DatoIntervallEntitet.fraOgMedTilOgMed(LocalDate.of(2020, 4, 1), LocalDate.of(2020, 4, 30));
+        Beregningsgrunnlag bg = lagBeregningsgrunnlag(søknadsperiode, BigDecimal.valueOf(260));
+
+        List<Beregningsgrunnlag> mappetGrunnlag = MapTilBeregningsgrunnlag.mapBeregningsgrunnlag(bg, Optional.empty(), søknadsperiode, false, true);
+
+        assertThat(mappetGrunnlag.size()).isEqualTo(1);
+        assertThat(mappetGrunnlag.get(0).getBeregningsgrunnlagPerioder().size()).isEqualTo(1);
+        assertThat(mappetGrunnlag.get(0).getBeregningsgrunnlagPerioder().get(0).getDagsats()).isEqualTo(1);
+    }
+
+    @Test
+    public void skal_mappe_revurdering() {
+        DatoIntervallEntitet søknadsperiode = DatoIntervallEntitet.fraOgMedTilOgMed(LocalDate.of(2020, 4, 1), LocalDate.of(2020, 4, 30));
+        Beregningsgrunnlag bg = lagBeregningsgrunnlag(søknadsperiode, BigDecimal.valueOf(260));
+        Beregningsgrunnlag bgRevurdering = lagBeregningsgrunnlag(søknadsperiode, BigDecimal.valueOf(260 * 2));
+
+        List<Beregningsgrunnlag> mappetGrunnlag = MapTilBeregningsgrunnlag.mapBeregningsgrunnlag(bgRevurdering, Optional.of(bg), søknadsperiode, false, true);
+
+        assertThat(mappetGrunnlag.size()).isEqualTo(1);
+        assertThat(mappetGrunnlag.get(0).getBeregningsgrunnlagPerioder().size()).isEqualTo(1);
+        assertThat(mappetGrunnlag.get(0).getBeregningsgrunnlagPerioder().get(0).getDagsats()).isEqualTo(2);
+    }
+
+    @Test
+    public void skal_mappe_tilkommet_mai_søknad() {
+        DatoIntervallEntitet søknadsperiodeApril = DatoIntervallEntitet.fraOgMedTilOgMed(LocalDate.of(2020, 4, 1), LocalDate.of(2020, 4, 30));
+        DatoIntervallEntitet søknadsperiodeMai = DatoIntervallEntitet.fraOgMedTilOgMed(LocalDate.of(2020, 5, 1), LocalDate.of(2020, 5, 31));
+
+        Beregningsgrunnlag bg = lagBeregningsgrunnlag(søknadsperiodeApril, BigDecimal.valueOf(260));
+
+        Beregningsgrunnlag bgRevurderingSøknad = lagBeregningsgrunnlagMed2Søknadsperioer(søknadsperiodeApril, BigDecimal.valueOf(260 * 2), søknadsperiodeMai, BigDecimal.valueOf(260 * 2));
+
+        List<Beregningsgrunnlag> mappetGrunnlag = MapTilBeregningsgrunnlag.mapBeregningsgrunnlag(bgRevurderingSøknad, Optional.of(bg), søknadsperiodeMai, true, true);
+
+        assertThat(mappetGrunnlag.size()).isEqualTo(1);
+        assertThat(mappetGrunnlag.get(0).getBeregningsgrunnlagPerioder().size()).isEqualTo(2);
+        assertThat(mappetGrunnlag.get(0).getBeregningsgrunnlagPerioder().get(0).getDagsats()).isEqualTo(1);
+        assertThat(mappetGrunnlag.get(0).getBeregningsgrunnlagPerioder().get(1).getDagsats()).isEqualTo(2);
+    }
+
+
+    @Test
+    public void skal_mappe_tilkommet_juni_søknad_ved_overhopp_i_mai() {
+        DatoIntervallEntitet søknadsperiodeApril = DatoIntervallEntitet.fraOgMedTilOgMed(LocalDate.of(2020, 4, 1), LocalDate.of(2020, 4, 30));
+        DatoIntervallEntitet søknadsperiodeJuni = DatoIntervallEntitet.fraOgMedTilOgMed(LocalDate.of(2020, 6, 1), LocalDate.of(2020, 6, 30));
+
+        Beregningsgrunnlag bg = lagBeregningsgrunnlag(søknadsperiodeApril, BigDecimal.valueOf(260));
+
+        Beregningsgrunnlag bgRevurderingSøknad = lagBeregningsgrunnlagMed2Søknadsperioer(søknadsperiodeApril, BigDecimal.valueOf(260 * 2), søknadsperiodeJuni, BigDecimal.valueOf(260 * 2));
+
+        List<Beregningsgrunnlag> mappetGrunnlag = MapTilBeregningsgrunnlag.mapBeregningsgrunnlag(bgRevurderingSøknad, Optional.of(bg), søknadsperiodeJuni, true, true);
+
+        assertThat(mappetGrunnlag.size()).isEqualTo(1);
+        assertThat(mappetGrunnlag.get(0).getBeregningsgrunnlagPerioder().size()).isEqualTo(2);
+        assertThat(mappetGrunnlag.get(0).getBeregningsgrunnlagPerioder().get(0).getDagsats()).isEqualTo(1);
+        assertThat(mappetGrunnlag.get(0).getBeregningsgrunnlagPerioder().get(1).getDagsats()).isEqualTo(2);
+    }
+
+
+    @Test
+    public void skal_mappe_tilkommet_mai_søknad_starter_5_mai() {
+        DatoIntervallEntitet søknadsperiodeApril = DatoIntervallEntitet.fraOgMedTilOgMed(LocalDate.of(2020, 4, 1), LocalDate.of(2020, 4, 30));
+        DatoIntervallEntitet søknadsperiodeMai = DatoIntervallEntitet.fraOgMedTilOgMed(LocalDate.of(2020, 5, 5), LocalDate.of(2020, 5, 31));
+
+        Beregningsgrunnlag bg = lagBeregningsgrunnlag(søknadsperiodeApril, BigDecimal.valueOf(260));
+
+        Beregningsgrunnlag bgRevurderingSøknad = lagBeregningsgrunnlagMed2Søknadsperioer(søknadsperiodeApril, BigDecimal.valueOf(260 * 2), søknadsperiodeMai, BigDecimal.valueOf(260 * 2));
+
+        List<Beregningsgrunnlag> mappetGrunnlag = MapTilBeregningsgrunnlag.mapBeregningsgrunnlag(bgRevurderingSøknad, Optional.of(bg), søknadsperiodeMai, true, true);
+
+        assertThat(mappetGrunnlag.size()).isEqualTo(1);
+        assertThat(mappetGrunnlag.get(0).getBeregningsgrunnlagPerioder().size()).isEqualTo(2);
+        assertThat(mappetGrunnlag.get(0).getBeregningsgrunnlagPerioder().get(0).getDagsats()).isEqualTo(1);
+        assertThat(mappetGrunnlag.get(0).getBeregningsgrunnlagPerioder().get(1).getDagsats()).isEqualTo(2);
+    }
+
+    private Beregningsgrunnlag lagBeregningsgrunnlag(DatoIntervallEntitet søknadsperiode, BigDecimal bruttoPrÅr) {
+        Beregningsgrunnlag bg = Beregningsgrunnlag.builder()
+            .medSkjæringstidspunkt(STP)
+            .medGrunnbeløp(Beløp.ZERO)
+            .build();
+        lagPeriodeMedAndel(bg, BigDecimal.ZERO, STP, søknadsperiode.getFomDato().minusDays(1));
+        lagPeriodeMedAndel(bg, bruttoPrÅr, søknadsperiode.getFomDato(), søknadsperiode.getTomDato());
+        lagPeriodeMedAndel(bg, BigDecimal.ZERO, søknadsperiode.getTomDato(), TIDENES_ENDE);
+        return bg;
+    }
+
+    private Beregningsgrunnlag lagBeregningsgrunnlagMed2Søknadsperioer(DatoIntervallEntitet søknadsperiode, BigDecimal bruttoPrÅr, DatoIntervallEntitet søknadsperiode2, BigDecimal brutto2) {
+        Beregningsgrunnlag bg = Beregningsgrunnlag.builder()
+            .medSkjæringstidspunkt(STP)
+            .medGrunnbeløp(Beløp.ZERO)
+            .build();
+        lagPeriodeMedAndel(bg, BigDecimal.ZERO, STP, søknadsperiode.getFomDato().minusDays(1));
+        lagPeriodeMedAndel(bg, bruttoPrÅr, søknadsperiode.getFomDato(), søknadsperiode.getTomDato());
+        lagPeriodeMedAndel(bg, brutto2, søknadsperiode2.getFomDato(), søknadsperiode2.getTomDato());
+        lagPeriodeMedAndel(bg, BigDecimal.ONE, søknadsperiode2.getTomDato(), TIDENES_ENDE);
+
+        return bg;
+    }
+
+    private BeregningsgrunnlagPeriode lagPeriodeMedAndel(Beregningsgrunnlag bg, BigDecimal bruttoPrÅr, LocalDate fomDato, LocalDate tomDato) {
+        BeregningsgrunnlagPrStatusOgAndel.Builder andelBuilder = BeregningsgrunnlagPrStatusOgAndel.builder().medAktivitetStatus(AktivitetStatus.FRILANSER)
+            .medRedusertPrÅr(bruttoPrÅr)
+            .medRedusertBrukersAndelPrÅr(bruttoPrÅr)
+            .medRedusertRefusjonPrÅr(BigDecimal.ZERO);
+        BeregningsgrunnlagPeriode søknadBgPeriode = BeregningsgrunnlagPeriode.builder()
+            .medRedusertPrÅr(bruttoPrÅr)
+            .medBruttoPrÅr(bruttoPrÅr)
+            .leggTilBeregningsgrunnlagPrStatusOgAndel(andelBuilder)
+            .medBeregningsgrunnlagPeriode(fomDato, tomDato)
+            .build(bg);
+
+        return søknadBgPeriode;
+    }
+}
