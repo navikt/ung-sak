238b4ead00b798eb9177870336b0b0ff08626911
Espen Velsvik <espenjv@users.noreply.github.com>
2020-06-23 11:13:16 +0200

    TSF-887 Mapper perioder fra tidligere behandlinger i tilkjent ytelse ved ny søknadsperiode (#1730)

--- ytelse-frisinn/src/main/java/no/nav/k9/sak/ytelse/frisinn/beregnytelse/FrisinnBeregneYtelseSteg.java
+++ ytelse-frisinn/src/main/java/no/nav/k9/sak/ytelse/frisinn/beregnytelse/FrisinnBeregneYtelseSteg.java
@@ -1,6 +1,7 @@
 package no.nav.k9.sak.ytelse.frisinn.beregnytelse;
 
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -24,12 +25,16 @@ import no.nav.k9.sak.behandlingslager.behandling.beregning.BeregningsresultatRep
 import no.nav.k9.sak.behandlingslager.behandling.repository.BehandlingRepository;
 import no.nav.k9.sak.behandlingslager.behandling.repository.BehandlingRepositoryProvider;
 import no.nav.k9.sak.domene.behandling.steg.beregnytelse.BeregneYtelseSteg;
+import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
+import no.nav.k9.sak.domene.uttak.repo.Søknadsperioder;
 import no.nav.k9.sak.domene.uttak.repo.UttakAktivitet;
+import no.nav.k9.sak.domene.uttak.repo.UttakGrunnlag;
 import no.nav.k9.sak.domene.uttak.repo.UttakRepository;
 import no.nav.k9.sak.ytelse.beregning.BeregningsresultatVerifiserer;
 import no.nav.k9.sak.ytelse.beregning.FastsettBeregningsresultatTjeneste;
 import no.nav.k9.sak.ytelse.beregning.FinnEndringsdatoBeregningsresultatTjeneste;
 import no.nav.k9.sak.ytelse.beregning.regelmodell.UttakResultat;
+import no.nav.vedtak.konfig.KonfigVerdi;
 
 @FagsakYtelseTypeRef("FRISINN")
 @BehandlingStegRef(kode = "BERYT")
@@ -38,6 +43,7 @@ import no.nav.k9.sak.ytelse.beregning.regelmodell.UttakResultat;
 public class FrisinnBeregneYtelseSteg implements BeregneYtelseSteg {
 
     private static final LocalDate STP_FRISINN = LocalDate.of(2020, 3, 1);
+    private Boolean skalBenytteTidligereResultat;
     private BehandlingRepository behandlingRepository;
     private BeregningTjeneste kalkulusTjeneste;
     private BeregningsresultatRepository beregningsresultatRepository;
@@ -53,27 +59,35 @@ public class FrisinnBeregneYtelseSteg implements BeregneYtelseSteg {
                                     BeregningTjeneste kalkulusTjeneste,
                                     FastsettBeregningsresultatTjeneste fastsettBeregningsresultatTjeneste,
                                     UttakRepository uttakRepository,
-                                    @Any Instance<FinnEndringsdatoBeregningsresultatTjeneste> finnEndringsdatoBeregningsresultatTjeneste) {
+                                    @Any Instance<FinnEndringsdatoBeregningsresultatTjeneste> finnEndringsdatoBeregningsresultatTjeneste,
+                                    @KonfigVerdi(value = "SKAL_BENYTTE_RESULTAT_FRA_TIDLIGERE_BEHANDLING", defaultVerdi = "false") Boolean skalBenytteTidligereResultat) {
         this.behandlingRepository = repositoryProvider.getBehandlingRepository();
         this.kalkulusTjeneste = kalkulusTjeneste;
         this.beregningsresultatRepository = repositoryProvider.getBeregningsresultatRepository();
         this.fastsettBeregningsresultatTjeneste = fastsettBeregningsresultatTjeneste;
         this.uttakRepository = uttakRepository;
+        this.skalBenytteTidligereResultat = skalBenytteTidligereResultat;
     }
 
     @Override
     public BehandleStegResultat utførSteg(BehandlingskontrollKontekst kontekst) {
         Long behandlingId = kontekst.getBehandlingId();
         Behandling behandling = behandlingRepository.hentBehandling(behandlingId);
+        var originalBehandling = behandling.getOriginalBehandling();
 
         var beregningsgrunnlag = kalkulusTjeneste.hentEksaktFastsatt(BehandlingReferanse.fra(behandling), STP_FRISINN);
+        var beregningsgrunnlagOriginalBehandling = originalBehandling.flatMap(b -> kalkulusTjeneste.hentEksaktFastsatt(BehandlingReferanse.fra(b), STP_FRISINN));
 
         if (beregningsgrunnlag.isPresent()) {
             UttakAktivitet fastsattUttak = uttakRepository.hentFastsattUttak(behandlingId);
             UttakResultat uttakResultat = MapUttakFrisinnTilRegel.map(fastsattUttak, behandling.getFagsakYtelseType());
 
+            DatoIntervallEntitet sisteSøknadsperiode = uttakRepository.hentGrunnlag(behandlingId).map(UttakGrunnlag::getOppgittSøknadsperioder).map(Søknadsperioder::getMaksPeriode)
+                .orElseThrow(() -> new IllegalStateException("Forventer uttaksgrunnlag"));
+            Boolean erNySøknadsperiode = originalBehandling.map(b -> erNySøknadperiode(behandling, b)).orElse(false);
+
             // Kalle regeltjeneste
-            List<Beregningsgrunnlag> beregningsgrunnlagListe = MapTilBeregningsgrunnlag.mapBeregningsgrunnlag(beregningsgrunnlag.get());
+            List<Beregningsgrunnlag> beregningsgrunnlagListe = MapTilBeregningsgrunnlag.mapBeregningsgrunnlag(beregningsgrunnlag.get(), beregningsgrunnlagOriginalBehandling, sisteSøknadsperiode, erNySøknadsperiode, skalBenytteTidligereResultat);
             var beregningsresultat = fastsettBeregningsresultatTjeneste.fastsettBeregningsresultat(beregningsgrunnlagListe, uttakResultat);
 
             // Verifiser beregningsresultat
@@ -95,4 +109,12 @@ public class FrisinnBeregneYtelseSteg implements BeregneYtelseSteg {
         Behandling behandling = behandlingRepository.hentBehandling(kontekst.getBehandlingId());
         beregningsresultatRepository.deaktiverBeregningsresultat(behandling.getId(), kontekst.getSkriveLås());
     }
+
+    private boolean erNySøknadperiode(Behandling revurdering, Behandling origBehandling) {
+        var nyttUttak = uttakRepository.hentFastsattUttak(revurdering.getId());
+        var origUttak = uttakRepository.hentFastsattUttak(origBehandling.getId());
+
+        return nyttUttak.getPerioder().size() > origUttak.getPerioder().size();
+    }
+
 }
