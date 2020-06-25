8ee63180bbf4b9338d17b746b407df8321b9e877
Marius Glittum <marius.glittum@nav.no>
2020-06-25 15:50:56 +0200

    Kjører alle med 9999

---
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_106__marker-saker-med-overlappende-ytelse.sql
@@ -0,0 +1,5 @@
+UPDATE aksjonspunkt
+set frist_tid = current_date - interval '1 day'
+WHERE aksjonspunkt_def = '9999'
+  AND aksjonspunkt_status = 'OPPR';
+UPDATE prosess_task set neste_kjoering_etter = neste_kjoering_etter - interval '1 day' WHERE task_type = 'batch.automatiskGjenopptaglese' AND status = 'KLAR';
