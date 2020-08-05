c3988ca6e0e559aabb83247fd7d1262a04599b59
Frode Carlsen <frode.odde.carlsen@nav.no>
2020-08-05 12:57:35 +0200

    fix: reworked treg migrering

--- migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_119__Justere-behandlingstype-fjerne-kobling.sql
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_119__Justere-behandlingstype-fjerne-kobling.sql
@@ -1,18 +1,3 @@
 ALTER TABLE behandling
-    add column original_behandling_id bigint references behandling;
+    add column IF NOT EXISTS original_behandling_id bigint references behandling;
 
-create index idx_behandling_7
-    on behandling (original_behandling_id);
-
-update behandling b
-set original_behandling_id = (select DISTINCT original_behandling_id
-                              from behandling_arsak ba
-                              where ba.behandling_id = b.id);
-
-update behandling
-set original_behandling_id = null,
-    behandling_type        = 'BT-002'
-where uuid IN ('10a86dfd-59bc-4fc2-acf6-e6f2ceac8299', 'cc82d018-9b31-422d-9602-8cfd01ca502d',
-               '9c8b3c8e-22a6-49ef-a428-9958d5ed49b7', 'c13a2999-5b1c-4a83-af2b-71b65bbbd916',
-               '664f7751-31a5-44b0-81d7-da53d845a97f', '7e8479ca-7ab1-4463-84c4-03157cbcc2cb')
-  and behandling_type = 'BT-004';
---
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_121__fiks-original-behandling_id.sql
@@ -0,0 +1,16 @@
+
+update behandling b
+set original_behandling_id = x.original_behandling_id
+from (select distinct original_behandling_id, behandling_id from behandling_arsak) x
+where x.behandling_id = b.id;
+
+update behandling
+set original_behandling_id = null,
+    behandling_type        = 'BT-002'
+where uuid IN ('10a86dfd-59bc-4fc2-acf6-e6f2ceac8299', 'cc82d018-9b31-422d-9602-8cfd01ca502d',
+               '9c8b3c8e-22a6-49ef-a428-9958d5ed49b7', 'c13a2999-5b1c-4a83-af2b-71b65bbbd916',
+               '664f7751-31a5-44b0-81d7-da53d845a97f', '7e8479ca-7ab1-4463-84c4-03157cbcc2cb')
+  and behandling_type = 'BT-004';
+
+create index IF NOT EXISTS idx_behandling_7
+    on behandling (original_behandling_id);
