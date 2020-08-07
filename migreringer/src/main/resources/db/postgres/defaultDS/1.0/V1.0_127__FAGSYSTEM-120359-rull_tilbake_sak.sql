501a248c9a28ddc6c8955344cdfb7f7e3363f20d
s149212 <espen.svendsen@nav.no>
2020-08-07 14:06:57 +0200

    FAGSYSTEM-120359 Ruller tilbake for å gjenopprette beregningsgr.

---
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_127__FAGSYSTEM-120359-rull_tilbake_sak.sql
@@ -0,0 +1,13 @@
+-- Rull tilbake behandling for å gjenopprette beregningsgrunnlag
+insert into prosess_task (id, task_type, task_gruppe, neste_kjoering_etter, task_parametere)
+select nextval('seq_prosess_task'), 'beregning.tilbakeTilStart',
+       nextval('seq_prosess_task_gruppe'), null,
+       'fagsakId=' || f.id || '
+                behandlingId=' || b.id from fagsak f
+inner join behandling b on f.id = b.fagsak_id
+where f.saksnummer in ('6BQF2')
+  and b.behandling_status = 'UTRED'
+  and (select count(*) from behandling_steg_tilstand bst
+       where bst.behandling_id = b.id
+         and bst.behandling_steg = 'KOFAKBER'
+         and bst.aktiv = true) > 0;
