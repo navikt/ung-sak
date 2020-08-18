5ec6fda0bfed94c44af2929a4a4f6be827121847
alexraknesnav <53822920+alexraknesnav@users.noreply.github.com>
2020-08-18 14:46:28 +0200

    Lagt til historikkinnslag for aksjonspunkt (#2054)
    * Lagt til historikkinnslag for aksjonspunkt
    * review: byttet builder med adapter.
    * review: lagt til begrunnelse
    * review: Endret type for historikkinnslag

--- web/src/main/java/no/nav/k9/sak/web/app/tjenester/behandling/årskvantum/AvklarÅrskvantumKvote.java
+++ web/src/main/java/no/nav/k9/sak/web/app/tjenester/behandling/årskvantum/AvklarÅrskvantumKvote.java
@@ -1,19 +1,23 @@
 package no.nav.k9.sak.web.app.tjenester.behandling.årskvantum;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-
+import no.nav.k9.kodeverk.historikk.HistorikkinnslagType;
 import no.nav.k9.sak.behandling.aksjonspunkt.AksjonspunktOppdaterParameter;
 import no.nav.k9.sak.behandling.aksjonspunkt.AksjonspunktOppdaterer;
 import no.nav.k9.sak.behandling.aksjonspunkt.DtoTilServiceAdapter;
 import no.nav.k9.sak.behandling.aksjonspunkt.OppdateringResultat;
+import no.nav.k9.sak.historikk.HistorikkInnslagTekstBuilder;
+import no.nav.k9.sak.historikk.HistorikkTjenesteAdapter;
 import no.nav.k9.sak.kontrakt.uttak.AvklarÅrskvantumDto;
 import no.nav.k9.sak.ytelse.omsorgspenger.årskvantum.tjenester.ÅrskvantumTjeneste;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
 @ApplicationScoped
 @DtoTilServiceAdapter(dto = AvklarÅrskvantumDto.class, adapter = AksjonspunktOppdaterer.class)
 public class AvklarÅrskvantumKvote implements AksjonspunktOppdaterer<AvklarÅrskvantumDto> {
 
+    HistorikkTjenesteAdapter historikkTjenesteAdapter;
 
     ÅrskvantumTjeneste årskvantumTjeneste;
 
@@ -22,23 +26,32 @@ public class AvklarÅrskvantumKvote implements AksjonspunktOppdaterer<AvklarÅrs
     }
 
     @Inject
-    AvklarÅrskvantumKvote(ÅrskvantumTjeneste årskvantumTjeneste) {
+    AvklarÅrskvantumKvote(HistorikkTjenesteAdapter historikkTjenesteAdapter,
+                          ÅrskvantumTjeneste årskvantumTjeneste) {
+        this.historikkTjenesteAdapter = historikkTjenesteAdapter;
         this.årskvantumTjeneste = årskvantumTjeneste;
     }
 
     @Override
     public OppdateringResultat oppdater(AvklarÅrskvantumDto dto, AksjonspunktOppdaterParameter param) {
         var fortsettBehandling = dto.getfortsettBehandling();
-
-        // TODO: historikkinnslag
+        Long behandlingId = param.getBehandlingId();
 
         if (fortsettBehandling) {
             //Bekreft uttaksplan og fortsett behandling
-            årskvantumTjeneste.bekreftUttaksplan(param.getBehandlingId());
-            return OppdateringResultat.utenOveropp();
+            opprettHistorikkInnslag(dto, behandlingId, HistorikkinnslagType.FASTSATT_UTTAK);
+            årskvantumTjeneste.bekreftUttaksplan(behandlingId);
         } else {
             // kjør steget på nytt, aka hent nye rammevedtak fra infotrygd
-            return OppdateringResultat.utenOveropp();
+            opprettHistorikkInnslag(dto, behandlingId, HistorikkinnslagType.FAKTA_ENDRET);
         }
+
+        return OppdateringResultat.utenOveropp();
+    }
+
+    private void opprettHistorikkInnslag(AvklarÅrskvantumDto dto, Long behandlingId, HistorikkinnslagType historikkinnslagType) {
+        HistorikkInnslagTekstBuilder builder = historikkTjenesteAdapter.tekstBuilder();
+        builder.medBegrunnelse(dto.getBegrunnelse());
+        historikkTjenesteAdapter.opprettHistorikkInnslag(behandlingId, historikkinnslagType);
     }
 }
