e38a09ff6a38778e0feeebea853f07fd907627c0
Stian Grenborgen <stian.grenborgen@nav.no>
2020-08-25 09:49:20 +0200

    Ny godkjennelse av manuelle revurderinger på OMP.

---
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_143__godkjenn_manuelle_revurderinger2.sql
@@ -0,0 +1,16 @@
+INSERT INTO prosess_task (id, task_type, task_gruppe, neste_kjoering_etter, task_parametere)
+SELECT nextval('seq_prosess_task'),
+  'behandlingskontroll.fortsettBehandling',
+  nextval('seq_prosess_task_gruppe'),
+  current_timestamp at time zone 'UTC' + interval '15 minutes',
+  'fagsakId=' || f.id || '
+    behandlingId=' || b.id || '
+    aksjonspunktStatusTilUtfort='||a.aksjonspunkt_def  
+FROM AKSJONSPUNKT a INNER JOIN BEHANDLING b ON (
+    b.id = a.behandling_id
+  ) INNER JOIN Fagsak f ON (
+    f.id = b.fagsak_id
+  )
+  WHERE a.aksjonspunkt_def in ('5028', '5056')
+    AND a.aksjonspunkt_status = 'OPPR'
+    AND f.ytelse_type = 'OMP';
