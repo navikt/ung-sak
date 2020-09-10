7d847d3d16c5a8768b8c42ea06861883d5589ecd
Marius Bækken Glittum <marius.glittum@nav.no>
2020-09-10 13:51:51 +0200

    Rydder opp i fraværsperioder når nyere meldinger som berører samme ar… (#2231)
    * Rydder opp i fraværsperioder når nyere meldinger som berører samme arbeidsforhold
    
    * Navngiving

---
+++ ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/inntektsmelding/AktivitetMedIdentifikatorArbeidsgiverArbeidsforhold.java
@@ -0,0 +1,46 @@
+package no.nav.k9.sak.ytelse.omsorgspenger.inntektsmelding;
+
+import java.util.Objects;
+
+import no.nav.k9.kodeverk.uttak.UttakArbeidType;
+
+class AktivitetMedIdentifikatorArbeidsgiverArbeidsforhold {
+
+    private UttakArbeidType aktivitetType;
+    private ArbeidsgiverArbeidsforhold arbeidsgiverArbeidsforhold;
+
+    public AktivitetMedIdentifikatorArbeidsgiverArbeidsforhold(UttakArbeidType aktivitetType, ArbeidsgiverArbeidsforhold arbeidsgiverArbeidsforhold) {
+        this.aktivitetType = Objects.requireNonNull(aktivitetType);
+        this.arbeidsgiverArbeidsforhold = Objects.requireNonNull(arbeidsgiverArbeidsforhold);
+    }
+
+    public UttakArbeidType getAktivitetType() {
+        return aktivitetType;
+    }
+
+    public ArbeidsgiverArbeidsforhold getArbeidsgiverArbeidsforhold() {
+        return arbeidsgiverArbeidsforhold;
+    }
+
+    public boolean gjelderSamme(AktivitetMedIdentifikatorArbeidsgiverArbeidsforhold aktivitet) {
+        if (!aktivitetType.equals(aktivitet.getAktivitetType())) {
+            return false;
+        }
+
+        return arbeidsgiverArbeidsforhold.identifisererSamme(aktivitet.getArbeidsgiverArbeidsforhold());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        var that = (AktivitetMedIdentifikatorArbeidsgiverArbeidsforhold) o;
+        return aktivitetType == that.aktivitetType &&
+            Objects.equals(arbeidsgiverArbeidsforhold, that.arbeidsgiverArbeidsforhold);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(aktivitetType, arbeidsgiverArbeidsforhold);
+    }
+}
--- ytelse-omsorgspenger/src/test/java/no/nav/k9/sak/ytelse/omsorgspenger/inntektsmelding/InntektsmeldingFraværTest.java
+++ ytelse-omsorgspenger/src/test/java/no/nav/k9/sak/ytelse/omsorgspenger/inntektsmelding/InntektsmeldingFraværTest.java
@@ -13,10 +13,12 @@ import org.junit.Test;
 import no.nav.k9.sak.domene.arbeidsforhold.impl.SakInntektsmeldinger;
 import no.nav.k9.sak.domene.iay.modell.InntektsmeldingBuilder;
 import no.nav.k9.sak.domene.iay.modell.PeriodeAndel;
+import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
 import no.nav.k9.sak.typer.Arbeidsgiver;
 import no.nav.k9.sak.typer.EksternArbeidsforholdRef;
 import no.nav.k9.sak.typer.InternArbeidsforholdRef;
 import no.nav.k9.sak.typer.Saksnummer;
+import no.nav.k9.sak.ytelse.omsorgspenger.repo.OppgittFraværPeriode;
 
 public class InntektsmeldingFraværTest {
 
@@ -78,5 +80,142 @@ public class InntektsmeldingFraværTest {
         assertThat(oppgittFraværPeriode).hasSize(3);
         assertThat(oppgittFraværPeriode.stream().filter(it -> it.getArbeidsgiver().getOrgnr().equals("000000000"))).hasSize(2);
         assertThat(oppgittFraværPeriode.stream().filter(it -> it.getArbeidsgiver().getOrgnr().equals("000000001"))).hasSize(1);
+    }
+
+    @Test
+    public void skal_rydde_i_berørte_tidslinjer() {
+        var sakInntektsmeldinger = new SakInntektsmeldinger(new Saksnummer("123123123"));
+        var arbeidsforholdId = InternArbeidsforholdRef.nyRef();
+        var inntektsmelding1 = InntektsmeldingBuilder.builder()
+            .medArbeidsgiver(Arbeidsgiver.virksomhet("000000000"))
+            .medArbeidsforholdId(arbeidsforholdId)
+            .medArbeidsforholdId(EksternArbeidsforholdRef.ref("asdf"))
+            .medJournalpostId("1")
+            .medInnsendingstidspunkt(LocalDateTime.now().minusDays(10))
+            .medBeløp(BigDecimal.TEN)
+            .medKanalreferanse("AR123")
+            .medOppgittFravær(List.of(new PeriodeAndel(LocalDate.now().minusDays(30), LocalDate.now().minusDays(25))))
+            .medRefusjon(BigDecimal.TEN)
+            .build();
+        var inntektsmelding2 = InntektsmeldingBuilder.builder()
+            .medArbeidsgiver(Arbeidsgiver.virksomhet("000000000"))
+            .medArbeidsforholdId(InternArbeidsforholdRef.nullRef())
+            .medArbeidsforholdId(EksternArbeidsforholdRef.nullRef())
+            .medInnsendingstidspunkt(LocalDateTime.now().minusDays(9))
+            .medJournalpostId("2")
+            .medBeløp(BigDecimal.ONE)
+            .medKanalreferanse("AR124")
+            .medOppgittFravær(List.of(new PeriodeAndel(LocalDate.now().minusDays(28), LocalDate.now().minusDays(25))))
+            .medRefusjon(BigDecimal.ONE)
+            .build();
+
+        sakInntektsmeldinger.leggTil(1L, UUID.randomUUID(), LocalDateTime.now(), inntektsmelding1);
+        sakInntektsmeldinger.leggTil(2L, UUID.randomUUID(), LocalDateTime.now(), inntektsmelding2);
+
+        var oppgittFraværPeriode = new InntektsmeldingFravær().trekkUtAlleFraværOgValiderOverlapp(sakInntektsmeldinger.getAlleInntektsmeldinger());
+
+        assertThat(oppgittFraværPeriode).hasSize(2);
+        assertThat(oppgittFraværPeriode.stream().filter(it -> it.getArbeidsgiver().getOrgnr().equals("000000000") && it.getArbeidsforholdRef().equals(arbeidsforholdId))).hasSize(1);
+        assertThat(oppgittFraværPeriode.stream()
+            .filter(it -> it.getArbeidsgiver().getOrgnr().equals("000000000") && it.getArbeidsforholdRef().equals(arbeidsforholdId))
+            .map(OppgittFraværPeriode::getPeriode))
+            .contains(DatoIntervallEntitet.fraOgMedTilOgMed(LocalDate.now().minusDays(30), LocalDate.now().minusDays(29)));
+        assertThat(oppgittFraværPeriode.stream()
+            .filter(it -> it.getArbeidsgiver().getOrgnr().equals("000000000") && it.getArbeidsforholdRef().equals(InternArbeidsforholdRef.nullRef())))
+            .hasSize(1);
+        assertThat(oppgittFraværPeriode.stream()
+            .filter(it -> it.getArbeidsgiver().getOrgnr().equals("000000000") && it.getArbeidsforholdRef().equals(InternArbeidsforholdRef.nullRef()))
+            .map(OppgittFraværPeriode::getPeriode))
+            .contains(DatoIntervallEntitet.fraOgMedTilOgMed(LocalDate.now().minusDays(28), LocalDate.now().minusDays(25)));
+    }
+
+    @Test
+    public void skal_rydde_i_berørte_tidslinjer_reverse() {
+        var sakInntektsmeldinger = new SakInntektsmeldinger(new Saksnummer("123123123"));
+        var arbeidsforholdId = InternArbeidsforholdRef.nyRef();
+        var inntektsmelding1 = InntektsmeldingBuilder.builder()
+            .medArbeidsgiver(Arbeidsgiver.virksomhet("000000000"))
+            .medArbeidsforholdId(arbeidsforholdId)
+            .medArbeidsforholdId(EksternArbeidsforholdRef.ref("asdf"))
+            .medJournalpostId("1")
+            .medInnsendingstidspunkt(LocalDateTime.now().minusDays(9))
+            .medBeløp(BigDecimal.TEN)
+            .medKanalreferanse("AR124")
+            .medOppgittFravær(List.of(new PeriodeAndel(LocalDate.now().minusDays(30), LocalDate.now().minusDays(25))))
+            .medRefusjon(BigDecimal.TEN)
+            .build();
+        var inntektsmelding2 = InntektsmeldingBuilder.builder()
+            .medArbeidsgiver(Arbeidsgiver.virksomhet("000000000"))
+            .medArbeidsforholdId(InternArbeidsforholdRef.nullRef())
+            .medArbeidsforholdId(EksternArbeidsforholdRef.nullRef())
+            .medInnsendingstidspunkt(LocalDateTime.now().minusDays(10))
+            .medJournalpostId("2")
+            .medBeløp(BigDecimal.ONE)
+            .medKanalreferanse("AR123")
+            .medOppgittFravær(List.of(new PeriodeAndel(LocalDate.now().minusDays(28), LocalDate.now().minusDays(25))))
+            .medRefusjon(BigDecimal.ONE)
+            .build();
+
+        sakInntektsmeldinger.leggTil(1L, UUID.randomUUID(), LocalDateTime.now(), inntektsmelding2);
+        sakInntektsmeldinger.leggTil(2L, UUID.randomUUID(), LocalDateTime.now(), inntektsmelding1);
+
+        var oppgittFraværPeriode = new InntektsmeldingFravær().trekkUtAlleFraværOgValiderOverlapp(sakInntektsmeldinger.getAlleInntektsmeldinger());
+
+        assertThat(oppgittFraværPeriode).hasSize(1);
+        assertThat(oppgittFraværPeriode.stream().filter(it -> it.getArbeidsgiver().getOrgnr().equals("000000000") && it.getArbeidsforholdRef().equals(arbeidsforholdId))).hasSize(1);
+        assertThat(oppgittFraværPeriode.stream()
+            .filter(it -> it.getArbeidsgiver().getOrgnr().equals("000000000") && it.getArbeidsforholdRef().equals(arbeidsforholdId))
+            .map(OppgittFraværPeriode::getPeriode))
+            .contains(DatoIntervallEntitet.fraOgMedTilOgMed(LocalDate.now().minusDays(30), LocalDate.now().minusDays(25)));
+        assertThat(oppgittFraværPeriode.stream()
+            .filter(it -> it.getArbeidsgiver().getOrgnr().equals("000000000") && it.getArbeidsforholdRef().equals(InternArbeidsforholdRef.nullRef())))
+            .hasSize(0);
+    }
+
+    @Test
+    public void skal_rydde_i_berørte_tidslinjer_reverse_2() {
+        var sakInntektsmeldinger = new SakInntektsmeldinger(new Saksnummer("123123123"));
+        var arbeidsforholdId = InternArbeidsforholdRef.nyRef();
+        var inntektsmelding1 = InntektsmeldingBuilder.builder()
+            .medArbeidsgiver(Arbeidsgiver.virksomhet("000000000"))
+            .medArbeidsforholdId(arbeidsforholdId)
+            .medArbeidsforholdId(EksternArbeidsforholdRef.ref("asdf"))
+            .medJournalpostId("1")
+            .medInnsendingstidspunkt(LocalDateTime.now().minusDays(9))
+            .medBeløp(BigDecimal.TEN)
+            .medKanalreferanse("AR124")
+            .medOppgittFravær(List.of(new PeriodeAndel(LocalDate.now().minusDays(30), LocalDate.now().minusDays(25))))
+            .medRefusjon(BigDecimal.TEN)
+            .build();
+        var inntektsmelding2 = InntektsmeldingBuilder.builder()
+            .medArbeidsgiver(Arbeidsgiver.virksomhet("000000000"))
+            .medArbeidsforholdId(InternArbeidsforholdRef.nullRef())
+            .medArbeidsforholdId(EksternArbeidsforholdRef.nullRef())
+            .medInnsendingstidspunkt(LocalDateTime.now().minusDays(10))
+            .medJournalpostId("2")
+            .medBeløp(BigDecimal.ONE)
+            .medKanalreferanse("AR123")
+            .medOppgittFravær(List.of(new PeriodeAndel(LocalDate.now().minusDays(28), LocalDate.now().minusDays(22))))
+            .medRefusjon(BigDecimal.ONE)
+            .build();
+
+        sakInntektsmeldinger.leggTil(1L, UUID.randomUUID(), LocalDateTime.now(), inntektsmelding2);
+        sakInntektsmeldinger.leggTil(2L, UUID.randomUUID(), LocalDateTime.now(), inntektsmelding1);
+
+        var oppgittFraværPeriode = new InntektsmeldingFravær().trekkUtAlleFraværOgValiderOverlapp(sakInntektsmeldinger.getAlleInntektsmeldinger());
+
+        assertThat(oppgittFraværPeriode).hasSize(2);
+        assertThat(oppgittFraværPeriode.stream().filter(it -> it.getArbeidsgiver().getOrgnr().equals("000000000") && it.getArbeidsforholdRef().equals(arbeidsforholdId))).hasSize(1);
+        assertThat(oppgittFraværPeriode.stream()
+            .filter(it -> it.getArbeidsgiver().getOrgnr().equals("000000000") && it.getArbeidsforholdRef().equals(arbeidsforholdId))
+            .map(OppgittFraværPeriode::getPeriode))
+            .contains(DatoIntervallEntitet.fraOgMedTilOgMed(LocalDate.now().minusDays(30), LocalDate.now().minusDays(25)));
+        assertThat(oppgittFraværPeriode.stream()
+            .filter(it -> it.getArbeidsgiver().getOrgnr().equals("000000000") && it.getArbeidsforholdRef().equals(InternArbeidsforholdRef.nullRef())))
+            .hasSize(1);
+        assertThat(oppgittFraværPeriode.stream()
+            .filter(it -> it.getArbeidsgiver().getOrgnr().equals("000000000") && it.getArbeidsforholdRef().equals(InternArbeidsforholdRef.nullRef()))
+            .map(OppgittFraværPeriode::getPeriode))
+            .contains(DatoIntervallEntitet.fraOgMedTilOgMed(LocalDate.now().minusDays(24), LocalDate.now().minusDays(22)));
     }
 }
