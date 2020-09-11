570b307ef3d784a2c8c4d902e92767ab36095900
Marius Bækken Glittum <marius.glittum@nav.no>
2020-09-11 12:25:05 +0200

    Kjører tilbake til start og setter til full revurdering pga sorry-state (#2245)

---
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_156__feilende-tilbake-til-start-revurdering.sql
@@ -0,0 +1,19 @@
+INSERT INTO prosess_task (id, task_type, task_gruppe, neste_kjoering_etter, task_parametere)
+SELECT nextval('seq_prosess_task'),
+       'behandlingskontroll.tilbakeTilStart',
+       nextval('seq_prosess_task_gruppe'),
+       current_timestamp at time zone 'UTC' + interval '15 minutes',
+       'fagsakId=' || f.id || '
+    behandlingId=' || b.id || '
+    manueltOpprettet=true'
+FROM prosess_task p
+         INNER JOIN BEHANDLING b ON (
+            b.id = CAST(SUBSTRING(p.task_parametere, 'behandlingId=([^[:space:]]*)') AS BIGINT)
+        AND b.id IS NOT NULL
+    )
+         INNER JOIN Fagsak f ON (
+    f.id = b.fagsak_id
+    )
+WHERE p.status = 'FEILET'
+  AND p.task_type = 'behandlingskontroll.fortsettBehandling'
+  AND f.ytelse_type = 'OMP';
