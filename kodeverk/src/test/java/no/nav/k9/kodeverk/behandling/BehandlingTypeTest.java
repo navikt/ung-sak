6f73c5409ecf6bb465c0893dea48d4bbf74793e0
Frode Carlsen <frode.odde.carlsen@nav.no>
2020-09-17 09:34:21 +0200

    fix: støtter delegt parsing av enum for BehandlingType. Gjør at vi kan lese både {"kode":"BT-004","kodeverk":"BEHANDLING_TYPE"} og "BT-004"
    med samme fraKode metode

---
+++ kodeverk/src/test/java/no/nav/k9/kodeverk/behandling/BehandlingTypeTest.java
@@ -0,0 +1,25 @@
+package no.nav.k9.kodeverk.behandling;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class BehandlingTypeTest {
+
+    private ObjectMapper om = new ObjectMapper();
+
+    @Test
+    public void sjekk_rendering() throws Exception {
+        String json1 = om.writeValueAsString(BehandlingType.REVURDERING);
+        System.out.println(json1);
+
+        var t = om.reader().forType(BehandlingType.class).readValue(json1);
+        assertThat(t).isNotNull();
+
+        // forsøker med ren key input også
+        var t2 = om.reader().forType(BehandlingType.class).readValue("\"" + BehandlingType.REVURDERING.getKode() + "\"");
+        assertThat(t2).isNotNull().isSameAs(t);
+    }
+}
