d0e642a81dcd47e93f132a217057c56c1f60661b
Frode Carlsen <frode.odde.carlsen@nav.no>
2020-09-26 14:36:57 +0200

    fix: optimaliserer henting av beregningsgrunnlag for totrinn - ikke hent hvis ikke trengs

--- web/src/main/java/no/nav/k9/sak/web/app/tjenester/behandling/vedtak/TotrinnsBeregningDtoTjeneste.java
+++ web/src/main/java/no/nav/k9/sak/web/app/tjenester/behandling/vedtak/TotrinnsBeregningDtoTjeneste.java
@@ -1,13 +1,16 @@
 package no.nav.k9.sak.web.app.tjenester.behandling.vedtak;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.EnumSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
-import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 
 import no.nav.folketrygdloven.beregningsgrunnlag.kalkulus.BeregningTjeneste;
@@ -22,8 +25,12 @@ import no.nav.k9.sak.kontrakt.vedtak.TotrinnsBeregningDto;
 import no.nav.k9.sak.produksjonsstyring.totrinn.BeregningsgrunnlagToTrinn;
 import no.nav.k9.sak.produksjonsstyring.totrinn.Totrinnsvurdering;
 
-@ApplicationScoped
+@Dependent
 public class TotrinnsBeregningDtoTjeneste {
+    private static final AksjonspunktDefinisjon FAKTA_ATFL_SN = AksjonspunktDefinisjon.VURDER_FAKTA_FOR_ATFL_SN;
+    private static final AksjonspunktDefinisjon VARIG_ENDRET_SN = AksjonspunktDefinisjon.VURDER_VARIG_ENDRET_ELLER_NYOPPSTARTET_NÆRING_SELVSTENDIG_NÆRINGSDRIVENDE;
+    private static final Set<AksjonspunktDefinisjon> HENT_GRUNNLAG_APDEFS = EnumSet.of(VARIG_ENDRET_SN, FAKTA_ATFL_SN);
+
     private BeregningTjeneste tjeneste;
 
     protected TotrinnsBeregningDtoTjeneste() {
@@ -44,19 +51,27 @@ public class TotrinnsBeregningDtoTjeneste {
         var ref = BehandlingReferanse.fra(behandling);
         var dtoer = new ArrayList<TotrinnsBeregningDto>();
 
-        var totrinnTilBeregningsgrunnlagMap = hentBeregningsgrunnlag(ref, beregningsgrunnlagTotrinn);
+        Map<BeregningsgrunnlagToTrinn, Beregningsgrunnlag> totrinnTilBeregningsgrunnlagMap = Collections.emptyMap();
+        var apDef = aksjonspunkt.getAksjonspunktDefinisjon();
+        if (HENT_GRUNNLAG_APDEFS.contains(apDef)) {
+            totrinnTilBeregningsgrunnlagMap = hentBeregningsgrunnlag(ref, beregningsgrunnlagTotrinn);
+        }
 
         for (var bgTotrinn : beregningsgrunnlagTotrinn) {
             Beregningsgrunnlag bg = totrinnTilBeregningsgrunnlagMap.get(bgTotrinn);
             TotrinnsBeregningDto dto = new TotrinnsBeregningDto();
-            if (aksjonspunkt.getAksjonspunktDefinisjon().equals(AksjonspunktDefinisjon.VURDER_VARIG_ENDRET_ELLER_NYOPPSTARTET_NÆRING_SELVSTENDIG_NÆRINGSDRIVENDE)) {
+
+            if (VARIG_ENDRET_SN.equals(apDef)) {
                 dto.setFastsattVarigEndringNaering(erVarigEndringFastsattForSelvstendingNæringsdrivendeGittGrunnlag(bgTotrinn, bg));
             }
-            if (AksjonspunktDefinisjon.VURDER_FAKTA_FOR_ATFL_SN.equals(aksjonspunkt.getAksjonspunktDefinisjon())) {
-                List<FaktaOmBeregningTilfelle> tilfeller = bg.getFaktaOmBeregningTilfeller();
+
+            if (FAKTA_ATFL_SN.equals(apDef)) {
+                List<FaktaOmBeregningTilfelle> tilfeller = Objects.requireNonNull(bg, "Mangler beregningsgrunnlag for " + bgTotrinn).getFaktaOmBeregningTilfeller();
                 dto.setFaktaOmBeregningTilfeller(tilfeller);
                 dto.setSkjæringstidspunkt(bg.getSkjæringstidspunkt());
             }
+
+            // TODO : er det verdt å returnere tom dto for andre tilfeller her?
             dtoer.add(dto);
         }
         return dtoer;
--- web/src/main/java/no/nav/k9/sak/web/app/tjenester/behandling/vedtak/TotrinnsaksjonspunktDtoTjeneste.java
+++ web/src/main/java/no/nav/k9/sak/web/app/tjenester/behandling/vedtak/TotrinnsaksjonspunktDtoTjeneste.java
@@ -5,22 +5,25 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 
 import no.nav.k9.sak.behandlingslager.behandling.Behandling;
+import no.nav.k9.sak.kontrakt.vedtak.TotrinnsArbeidsforholdDto;
+import no.nav.k9.sak.kontrakt.vedtak.TotrinnsBeregningDto;
 import no.nav.k9.sak.kontrakt.vedtak.TotrinnskontrollAksjonspunkterDto;
+import no.nav.k9.sak.kontrakt.vedtak.TotrinnskontrollAktivitetDto;
 import no.nav.k9.sak.kontrakt.vedtak.TotrinnskontrollVurderÅrsak;
+import no.nav.k9.sak.produksjonsstyring.totrinn.BeregningsgrunnlagToTrinn;
 import no.nav.k9.sak.produksjonsstyring.totrinn.Totrinnresultatgrunnlag;
 import no.nav.k9.sak.produksjonsstyring.totrinn.Totrinnsvurdering;
 import no.nav.k9.sak.produksjonsstyring.totrinn.VurderÅrsakTotrinnsvurdering;
 
-@ApplicationScoped
+@Dependent
 public class TotrinnsaksjonspunktDtoTjeneste {
     private TotrinnsBeregningDtoTjeneste totrinnsBeregningDtoTjeneste;
     private TotrinnskontrollAktivitetDtoTjeneste totrinnskontrollAktivitetDtoTjeneste;
     private TotrinnArbeidsforholdDtoTjeneste totrinnArbeidsforholdDtoTjeneste;
-
 
     protected TotrinnsaksjonspunktDtoTjeneste() {
         // for CDI proxy
@@ -36,17 +39,25 @@ public class TotrinnsaksjonspunktDtoTjeneste {
     }
 
     public TotrinnskontrollAksjonspunkterDto lagTotrinnskontrollAksjonspunktDto(Totrinnsvurdering aksjonspunkt,
-                                                                                 Behandling behandling,
-                                                                                 Optional<Totrinnresultatgrunnlag> totrinnresultatgrunnlag) {
+                                                                                Behandling behandling,
+                                                                                Optional<Totrinnresultatgrunnlag> totrinnresultatgrunnlag) {
+
+        List<BeregningsgrunnlagToTrinn> totrinnBeregningsgrunnlag = totrinnresultatgrunnlag.map(Totrinnresultatgrunnlag::getBeregningsgrunnlagList).orElse(List.of());
+
+        List<TotrinnsBeregningDto> beregningDtoer = totrinnsBeregningDtoTjeneste.hentBeregningDto(aksjonspunkt, behandling, totrinnBeregningsgrunnlag);
+
+        List<TotrinnsArbeidsforholdDto> totrinnArbeidsforhold = totrinnArbeidsforholdDtoTjeneste.hentArbeidsforhold(behandling, aksjonspunkt,
+            totrinnresultatgrunnlag.flatMap(Totrinnresultatgrunnlag::getGrunnlagUuid));
+
+        List<TotrinnskontrollAktivitetDto> opptjeningAktiviteter = totrinnskontrollAktivitetDtoTjeneste.hentAktiviterEndretForOpptjening(aksjonspunkt, behandling,
+            totrinnresultatgrunnlag.flatMap(Totrinnresultatgrunnlag::getGrunnlagUuid));
+
         return new TotrinnskontrollAksjonspunkterDto.Builder()
             .medAksjonspunktKode(aksjonspunkt.getAksjonspunktDefinisjon())
-            .medOpptjeningAktiviteter(totrinnskontrollAktivitetDtoTjeneste.hentAktiviterEndretForOpptjening(aksjonspunkt, behandling,
-                totrinnresultatgrunnlag.flatMap(Totrinnresultatgrunnlag::getGrunnlagUuid)))
-            .medBeregningDtoer(totrinnsBeregningDtoTjeneste.hentBeregningDto(aksjonspunkt, behandling,
-                totrinnresultatgrunnlag.map(Totrinnresultatgrunnlag::getBeregningsgrunnlagList).orElse(List.of())))
+            .medOpptjeningAktiviteter(opptjeningAktiviteter)
+            .medBeregningDtoer(beregningDtoer)
             .medBesluttersBegrunnelse(aksjonspunkt.getBegrunnelse())
-            .medArbeidsforhold(totrinnArbeidsforholdDtoTjeneste.hentArbeidsforhold(behandling, aksjonspunkt,
-                totrinnresultatgrunnlag.flatMap(Totrinnresultatgrunnlag::getGrunnlagUuid)))
+            .medArbeidsforhold(totrinnArbeidsforhold)
             .medTotrinnskontrollGodkjent(aksjonspunkt.isGodkjent())
             .medVurderPaNyttArsaker(hentVurderPåNyttÅrsaker(aksjonspunkt))
             .build();
