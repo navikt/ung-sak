db8b69ea69de4cd44716732f9c54c76830bf6d4e
Espen Svendsen <62875567+espennav@users.noreply.github.com>
2020-10-02 15:32:39 +0200

    Setter behandling låst som  2-trinn i Frisinn til utført (#2376)
    Signed-off-by: s149212 <espen.svendsen@nav.no>

---
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_166__godkjenn_ap_fatte_vedtak.sql
@@ -0,0 +1,20 @@
+-- Frisinn har kommet i ugyldig tilstand, skal aldri havne i 2-trinnskontroll (5016)
+-- Setter 5016 som utført og fortsetter behandling til iverksettelse
+update aksjonspunkt
+set aksjonspunkt_status='UTFO'
+where aksjonspunkt_status='OPPR'
+  and aksjonspunkt_def = '5016' AND behandling_id = 1015329;
+
+
+insert into prosess_task (id, task_type, task_gruppe, neste_kjoering_etter, task_parametere)
+select nextval('seq_prosess_task'), 'behandlingskontroll.fortsettBehandling',
+       nextval('seq_prosess_task_gruppe'), null,
+       'fagsakId=' || f.id || '
+                behandlingId=' || b.id from fagsak f
+inner join behandling b on f.id = b.fagsak_id
+where f.saksnummer = '5Z99M'
+  and b.behandling_status = 'FVED'
+  and exists (select 1 from behandling_steg_tilstand bst
+              where bst.behandling_id = b.id
+                and bst.behandling_steg = 'FVEDSTEG'
+                and bst.aktiv = true);
