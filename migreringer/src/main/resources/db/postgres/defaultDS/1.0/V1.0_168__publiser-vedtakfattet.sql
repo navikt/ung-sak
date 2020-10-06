547b9bcebe2cfd4d245e3aaea5473909bd562261
Bjornar Askevold <bjornar.askevold@nav.no>
2020-10-06 12:21:58 +0200

    Starter task som publiserer allerede fattede vedtak

--- migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_168__publiser-vedtakfattet.sql
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_168__publiser-vedtakfattet.sql
@@ -21,9 +21,16 @@ INSERT INTO PROSESS_TASK_TYPE (KODE,
                                FEIL_SEK_MELLOM_FORSOEK,
                                FEILHANDTERING_ALGORITME,
                                BESKRIVELSE)
+
+
 VALUES ('vedtak.etterfyllHistoriske',
         'Etterfyller historiske vedtakhendelser',
         3,
         60,
         'DEFAULT',
         'Publiserer allerede fattede vedtak på kø.');
+
+
+-- Starter task for etterfylling av vedtakhendelser --
+insert into prosess_task (id, task_type, prioritet, status, task_gruppe, task_sekvens, partition_key)
+values (nextval('SEQ_PROSESS_TASK'), 'vedtak.etterfyllHistoriske', 50, 'KLAR', nextval('SEQ_PROSESS_TASK_GRUPPE'), 1, '10');
