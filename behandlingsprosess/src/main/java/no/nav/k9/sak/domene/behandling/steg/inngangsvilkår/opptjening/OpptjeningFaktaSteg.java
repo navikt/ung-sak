8ee0dc7a40e78a696df7cc536a62166d1fb3c269
Marius Bækken Glittum <marius.glittum@nav.no>
2020-10-13 11:34:30 +0200

    Stopper ved avslag i simulering av opptjening (#2424)

--- behandlingsprosess/src/main/java/no/nav/k9/sak/domene/behandling/steg/inngangsvilkår/opptjening/OpptjeningFaktaSteg.java
+++ behandlingsprosess/src/main/java/no/nav/k9/sak/domene/behandling/steg/inngangsvilkår/opptjening/OpptjeningFaktaSteg.java
@@ -1,14 +1,32 @@
 package no.nav.k9.sak.domene.behandling.steg.inngangsvilkår.opptjening;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+import no.nav.k9.kodeverk.vilkår.Utfall;
+import no.nav.k9.kodeverk.vilkår.VilkårType;
+import no.nav.k9.sak.behandling.BehandlingReferanse;
+import no.nav.k9.sak.behandling.aksjonspunkt.AksjonspunktUtlederInput;
+import no.nav.k9.sak.behandlingskontroll.AksjonspunktResultat;
+import no.nav.k9.sak.behandlingskontroll.BehandleStegResultat;
+import no.nav.k9.sak.behandlingskontroll.BehandlingSteg;
 import no.nav.k9.sak.behandlingskontroll.BehandlingStegRef;
 import no.nav.k9.sak.behandlingskontroll.BehandlingTypeRef;
+import no.nav.k9.sak.behandlingskontroll.BehandlingskontrollKontekst;
 import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
+import no.nav.k9.sak.behandlingslager.behandling.Behandling;
 import no.nav.k9.sak.behandlingslager.behandling.repository.BehandlingRepositoryProvider;
+import no.nav.k9.sak.behandlingslager.behandling.vilkår.Vilkår;
+import no.nav.k9.sak.behandlingslager.behandling.vilkår.Vilkårene;
+import no.nav.k9.sak.behandlingslager.behandling.vilkår.periode.VilkårPeriode;
 import no.nav.k9.sak.domene.opptjening.aksjonspunkt.AksjonspunktutlederForVurderBekreftetOpptjening;
 import no.nav.k9.sak.domene.opptjening.aksjonspunkt.AksjonspunktutlederForVurderOppgittOpptjening;
+import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
 import no.nav.k9.sak.inngangsvilkår.opptjening.OpptjeningsVilkårTjeneste;
 import no.nav.k9.sak.skjæringstidspunkt.SkjæringstidspunktTjeneste;
 
@@ -19,7 +37,13 @@ import no.nav.k9.sak.skjæringstidspunkt.SkjæringstidspunktTjeneste;
 @BehandlingTypeRef
 @FagsakYtelseTypeRef
 @ApplicationScoped
-public class OpptjeningFaktaSteg extends OpptjeningFaktaStegFelles {
+public class OpptjeningFaktaSteg implements BehandlingSteg {
+
+    private BehandlingRepositoryProvider repositoryProvider;
+    private AksjonspunktutlederForVurderOppgittOpptjening aksjonspunktutlederOppgitt;
+    private AksjonspunktutlederForVurderBekreftetOpptjening aksjonspunktutlederBekreftet;
+    private OpptjeningsVilkårTjeneste opptjeningsVilkårTjeneste;
+    private SkjæringstidspunktTjeneste skjæringstidspunktTjeneste;
 
     @Inject
     public OpptjeningFaktaSteg(BehandlingRepositoryProvider repositoryProvider,
@@ -27,6 +51,55 @@ public class OpptjeningFaktaSteg extends OpptjeningFaktaStegFelles {
                                  AksjonspunktutlederForVurderOppgittOpptjening aksjonspunktutlederOppgitt,
                                  @FagsakYtelseTypeRef OpptjeningsVilkårTjeneste opptjeningsVilkårTjeneste,
                                  SkjæringstidspunktTjeneste skjæringstidspunktTjeneste) {
-        super(repositoryProvider, aksjonspunktutlederBekreftet, aksjonspunktutlederOppgitt, opptjeningsVilkårTjeneste, skjæringstidspunktTjeneste);
+        this.repositoryProvider = repositoryProvider;
+        this.aksjonspunktutlederBekreftet = aksjonspunktutlederBekreftet;
+        this.aksjonspunktutlederOppgitt = aksjonspunktutlederOppgitt;
+        this.opptjeningsVilkårTjeneste = opptjeningsVilkårTjeneste;
+        this.skjæringstidspunktTjeneste = skjæringstidspunktTjeneste;
+    }
+
+    @Override
+    public BehandleStegResultat utførSteg(BehandlingskontrollKontekst kontekst) {
+        Long behandlingId = kontekst.getBehandlingId();
+        Behandling behandling = repositoryProvider.getBehandlingRepository().hentBehandling(behandlingId);
+        BehandlingReferanse ref = BehandlingReferanse.fra(behandling, skjæringstidspunktTjeneste.getSkjæringstidspunkter(behandlingId));
+
+        final var perioderTilVurdering = perioderTilVurdering(ref.getBehandlingId());
+        var utfall = Utfall.OPPFYLT;
+        var resultat = opptjeningsVilkårTjeneste.vurderOpptjeningsVilkår(ref, perioderTilVurdering);
+        for (var entry : resultat.entrySet()) {
+            var vilkårUtfall = entry.getValue().getUtfallType();
+            if (!Utfall.OPPFYLT.equals(vilkårUtfall)) {
+                utfall = vilkårUtfall;
+                break;
+            }
+        }
+        if (Utfall.OPPFYLT.equals(utfall)) {
+            return BehandleStegResultat.utførtUtenAksjonspunkter();
+        }
+
+        List<AksjonspunktResultat> resultatOppgitt = aksjonspunktutlederOppgitt.utledAksjonspunkterFor(new AksjonspunktUtlederInput(ref));
+        if (!resultatOppgitt.isEmpty()) {
+            return BehandleStegResultat.utførtMedAksjonspunktResultater(resultatOppgitt);
+        }
+
+        List<AksjonspunktResultat> resultatRegister = aksjonspunktutlederBekreftet.utledAksjonspunkterFor(new AksjonspunktUtlederInput(ref));
+        return BehandleStegResultat.utførtMedAksjonspunktResultater(resultatRegister);
+    }
+
+    private List<DatoIntervallEntitet> perioderTilVurdering(Long behandlingId) {
+        Optional<Vilkårene> resultatOpt = repositoryProvider.getVilkårResultatRepository().hentHvisEksisterer(behandlingId);
+        if (resultatOpt.isPresent()) {
+            Vilkårene vilkårene = resultatOpt.get();
+            return vilkårene.getVilkårene()
+                .stream()
+                .filter(vilkår -> VilkårType.OPPTJENINGSVILKÅRET.equals(vilkår.getVilkårType()))
+                .map(Vilkår::getPerioder)
+                .flatMap(Collection::stream)
+                .filter(vp -> Utfall.IKKE_VURDERT.equals(vp.getGjeldendeUtfall()))
+                .map(VilkårPeriode::getPeriode)
+                .collect(Collectors.toList());
+        }
+        return List.of();
     }
 }
