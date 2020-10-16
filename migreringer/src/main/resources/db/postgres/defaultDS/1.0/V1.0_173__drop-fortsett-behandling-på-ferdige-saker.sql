5cb609c2981fb2bbd5793672ea7b3e8454fe4387
Frode Carlsen <frode.odde.carlsen@nav.no>
2020-10-16 08:42:56 +0200

    fix: tweak spørring

--- migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_173__drop-fortsett-behandling-på-ferdige-saker.sql
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_173__drop-fortsett-behandling-på-ferdige-saker.sql
@@ -2,7 +2,7 @@
 delete from fagsak_prosess_task where
 prosess_task_id in (
     select
-        p.prosess_task_id
+        p.id
     from prosess_task p
     where 
         siste_kjoering_feil_tekst like '%Utvikler-feil - saken er ferdig behandlet, kan ikke oppdateres.%' 
@@ -10,12 +10,13 @@ prosess_task_id in (
         and task_type='behandlingskontroll.fortsettBehandling'
 );
 
+-- opphever veto
 update prosess_task set status='KLAR'
   where 
-     p.status='VETO'
+     status='VETO'
      and blokkert_av in (
          select
-            p.prosess_task_id
+            p.id
          from prosess_task p
          where 
              siste_kjoering_feil_tekst like '%Utvikler-feil - saken er ferdig behandlet, kan ikke oppdateres.%' 
@@ -26,7 +27,7 @@ update prosess_task set status='KLAR'
 update prosess_task set status='KJOERT'
   where 
      siste_kjoering_feil_tekst like '%Utvikler-feil - saken er ferdig behandlet, kan ikke oppdateres.%' 
-     and p.status='FEILET'
+     and status='FEILET'
      and task_type='behandlingskontroll.fortsettBehandling';
      
      
