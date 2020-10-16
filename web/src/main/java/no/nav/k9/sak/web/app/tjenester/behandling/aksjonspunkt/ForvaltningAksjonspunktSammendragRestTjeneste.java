950a93e7d26cbfa0f1bf063810ff9d44e0b158c3
Espen Svendsen <62875567+espennav@users.noreply.github.com>
2020-10-16 07:16:52 +0200

    Utvide spørremuligheter på sammendrag for aksjonspunkter (#2432)
    * Utvide spørremuligheter på sammendrag for aksjonspunkter
    
    Signed-off-by: s149212 <espen.svendsen@nav.no>
    
    * Reverterer GET spørring av sammendrag, legger til ny GET på periode
    
    Signed-off-by: s149212 <espen.svendsen@nav.no>
    
    * Korrigerer string-param til dto
    
    Signed-off-by: s149212 <espen.svendsen@nav.no>

--- web/src/main/java/no/nav/k9/sak/web/app/tjenester/behandling/aksjonspunkt/ForvaltningAksjonspunktSammendragRestTjeneste.java
+++ web/src/main/java/no/nav/k9/sak/web/app/tjenester/behandling/aksjonspunkt/ForvaltningAksjonspunktSammendragRestTjeneste.java
@@ -11,6 +11,7 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -23,6 +24,7 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Variant;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -37,6 +39,7 @@ import no.nav.k9.sak.behandlingslager.fagsak.Fagsak;
 import no.nav.k9.sak.kontrakt.aksjonspunkt.BehandlingAksjonspunktDto;
 import no.nav.k9.sak.kontrakt.behandling.SaksnummerDto;
 import no.nav.k9.sak.web.server.abac.AbacAttributtEmptySupplier;
+import no.nav.k9.søknad.felles.Periode;
 import no.nav.vedtak.sikkerhet.abac.BeskyttetRessurs;
 import no.nav.vedtak.sikkerhet.abac.TilpassetAbacAttributt;
 
@@ -63,8 +66,8 @@ public class ForvaltningAksjonspunktSammendragRestTjeneste {
     @GET
     @Produces({ MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN })
     @Operation(description = "Hent aksjonspunter for saker", tags = "aksjonspunkt", responses = {
-            @ApiResponse(responseCode = "200", description = "Returnerer behandlinger med aksjonspunkt på JSON format", content = @Content(array = @ArraySchema(uniqueItems = true, arraySchema = @Schema(implementation = List.class), schema = @Schema(implementation = BehandlingAksjonspunktDto.class)), mediaType = MediaType.APPLICATION_JSON)),
-            @ApiResponse(responseCode = "200", description = "Returnerer behandlinger med aksjonspunkt på CSV format", content = @Content(mediaType = MediaType.TEXT_PLAIN))
+        @ApiResponse(responseCode = "200", description = "Returnerer behandlinger med aksjonspunkt på JSON format", content = @Content(array = @ArraySchema(uniqueItems = true, arraySchema = @Schema(implementation = List.class), schema = @Schema(implementation = BehandlingAksjonspunktDto.class)), mediaType = MediaType.APPLICATION_JSON)),
+        @ApiResponse(responseCode = "200", description = "Returnerer behandlinger med aksjonspunkt på CSV format", content = @Content(mediaType = MediaType.TEXT_PLAIN))
     })
     @BeskyttetRessurs(action = READ, resource = FAGSAK)
     @SuppressWarnings("findsecbugs:JAXRS_ENDPOINT")
@@ -98,6 +101,75 @@ public class ForvaltningAksjonspunktSammendragRestTjeneste {
         }
     }
 
+    @GET
+    @Path("/sammendrag_periode")
+    @Produces(MediaType.TEXT_PLAIN)
+    @Operation(description = "Hent aksjonspunter for saker", tags = "aksjonspunkt", responses = {
+        @ApiResponse(responseCode = "200", description = "Returnerer behandlinger med aksjonspunkt opprettet innenfor angitt periode på CSV format", content = @Content(mediaType = MediaType.TEXT_PLAIN))
+    })
+    @BeskyttetRessurs(action = READ, resource = FAGSAK)
+    @SuppressWarnings("findsecbugs:JAXRS_ENDPOINT")
+    public Response getAksjonspunkterSammendrag(@QueryParam("opprettetPeriode") @Parameter(description = "Tidsrom for opprettelse. Format YYYY-MM-DD/YYYY-MM-DD.") @Valid @TilpassetAbacAttributt(supplierClass = AbacAttributtEmptySupplier.class) @NotNull Periode opprettetPeriode,
+                                      @QueryParam("medUtforte") @Parameter(description = "Valgfritt å inkludere utførte aksjonspunkt. Default er false.") @Valid @TilpassetAbacAttributt(supplierClass = AbacAttributtEmptySupplier.class) boolean medUtforte,
+                                      @Context Request request) { // NOSONAR
+
+        var aksjonspunktStatuser = medUtforte
+            ? new AksjonspunktStatus[]{AksjonspunktStatus.OPPRETTET, AksjonspunktStatus.UTFØRT}
+            : new AksjonspunktStatus[]{AksjonspunktStatus.OPPRETTET};
+
+        var map = aksjonspunktRepository.hentAksjonspunkter(opprettetPeriode.fraOgMed, opprettetPeriode.tilOgMed, aksjonspunktStatuser);
+
+        CacheControl cc = new CacheControl();
+        cc.setNoCache(true);
+        cc.setNoStore(true);
+        cc.setMaxAge(0);
+        cc.setPrivate(true);
+
+        String csv = mapToCsv(map);
+        return Response.ok(csv, TEXT).cacheControl(cc).build();
+    }
+
+    private String mapToCsv(Map<Behandling, List<Aksjonspunkt>> map) {
+        var sb = new StringBuilder(2048);
+
+        // quick and dirt til csv. Kan godt forbedres. Ingen av feltene trenger escaping
+
+        // headere, pass på rekkefølge her!
+        sb.append("ytelseType,saksnummer,fagsakStatus,behandlingUuid,behandlingType,behandlingStatus,"
+            + "aksjonspunktDef,aksjonspunktType,vilkårType,aksjonspunktStatus,venteårsak,fristTid,"
+            + "kanLøses,totrinnsbehandling,opprettetTid,endretTid\n");
+        for (var entry : map.entrySet()) {
+            var b = entry.getKey();
+            var f = b.getFagsak();
+            String yt = f.getYtelseType().getKode();
+            String sn = f.getSaksnummer().getVerdi();
+            String fs = f.getStatus().getKode();
+            String uuid = b.getUuid().toString();
+            String bt = b.getType().getKode();
+            String bs = b.getStatus().getKode();
+
+            for (var a : b.getAksjonspunkter()) {
+                var ad = a.getAksjonspunktDefinisjon().getKode();
+                var at = a.getAksjonspunktDefinisjon().getAksjonspunktType().getKode();
+                var vt = a.getAksjonspunktDefinisjon().getVilkårType() == null || a.getAksjonspunktDefinisjon().getVilkårType() == VilkårType.UDEFINERT ? "" : a.getAksjonspunktDefinisjon().getVilkårType(); // NOSONAR
+                var as = a.getStatus() == null ? "" : a.getStatus().getKode();
+                var vå = a.getVenteårsak() == null || a.getVenteårsak() == Venteårsak.UDEFINERT ? "" : a.getVenteårsak().getKode(); // NOSONAR
+                var ft = a.getFristTid();
+                var kl = AksjonspunktDtoMapper.kanLøses(a.getAksjonspunktDefinisjon(), b) == null ? "" : AksjonspunktDtoMapper.kanLøses(a.getAksjonspunktDefinisjon(), b);
+                var tt = a.isToTrinnsBehandling() || a.getAksjonspunktDefinisjon().getDefaultTotrinnBehandling();
+                var ot = a.getOpprettetTidspunkt();
+                var et = a.getEndretTidspunkt();
+
+                Object[] args = new Object[] { yt, sn, fs, uuid, bt, bs, ad, at, vt, as, vå, ft, kl, tt, ot, et };
+                String fmt = "%s,".repeat(args.length);
+                var s = String.format(fmt.substring(0, fmt.length() - 1), args);
+                sb.append(s).append('\n');
+            }
+        }
+
+        return sb.toString();
+    }
+
     private String mapToCsv(List<BehandlingAksjonspunktDto> dtos) {
         var sb = new StringBuilder(2048);
 
@@ -127,7 +199,7 @@ public class ForvaltningAksjonspunktSammendragRestTjeneste {
                 var kl = a.getKanLoses() == null ? "" : a.getKanLoses();
                 var tt = a.getToTrinnsBehandling() == null ? "" : a.getToTrinnsBehandling();
                 var ttg = a.getToTrinnsBehandlingGodkjent() == null ? "" : a.getToTrinnsBehandlingGodkjent();
-                
+
                 Object[] args = new Object[] { yt, sn, fs, uuid, bt, bs, ad, at, vt, as, vå, ft, kl, tt, ttg };
                 String fmt = "%s,".repeat(args.length);
                 var s = String.format(fmt.substring(0, fmt.length() - 1), args);
