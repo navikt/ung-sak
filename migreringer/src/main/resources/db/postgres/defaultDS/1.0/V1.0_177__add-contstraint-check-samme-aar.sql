d0df61d736bf889f51968dcbcd2834c36f071849
Frode Carlsen <frode.odde.carlsen@nav.no>
2020-10-18 15:39:17 +0200

    Update V1.0_177__add-contstraint-check-samme-aar.sql

--- migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_177__add-contstraint-check-samme-aar.sql
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_177__add-contstraint-check-samme-aar.sql
@@ -1,3 +1,4 @@
 -- upper gir dato etter tom (alltid exclusive i db)
+alter table br_andel drop constraint if exists chk_br_andel_samme_aar;
 alter table br_andel add constraint chk_br_andel_samme_aar check (periode is null OR date_part('year'::text, lower(periode)) = date_part('year'::text, upper(periode) - interval '1 day'));
 
