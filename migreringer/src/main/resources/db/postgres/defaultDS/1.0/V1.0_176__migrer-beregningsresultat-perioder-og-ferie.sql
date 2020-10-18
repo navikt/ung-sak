e7d6f75a39991318d3ae22120c8488cc74d61a47
Frode Carlsen <frode.odde.carlsen@nav.no>
2020-10-18 14:40:19 +0200

    Update V1.0_176__migrer-beregningsresultat-perioder-og-ferie.sql

--- migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_176__migrer-beregningsresultat-perioder-og-ferie.sql
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_176__migrer-beregningsresultat-perioder-og-ferie.sql
@@ -1,16 +1,1 @@
 alter table br_andel drop constraint if exists chk_br_andel_samme_aar  ;
--- upper gir dato etter tom (alltid exclusive i db)
-alter table br_andel add constraint chk_br_andel_samme_aar check (periode is null OR date_part('year'::text, lower(periode)) = date_part('year'::text, upper(periode) - interval '1 day'));
-
-update br_andel a 
- set beregningsresultat_id=b.BEREGNINGSRESULTAT_FP_ID,
-     periode = daterange(b.br_periode_fom, b.br_periode_tom, '[]')
-from br_periode b 
-  where a.br_periode_id=b.id
-;
-
-update br_andel a
-   set feriepenger_beloep = b.aarsbeloep
-from BR_FERIEPENGER_PR_AAR b
-   where a.id=b.beregningsresultat_andel_id
-;
