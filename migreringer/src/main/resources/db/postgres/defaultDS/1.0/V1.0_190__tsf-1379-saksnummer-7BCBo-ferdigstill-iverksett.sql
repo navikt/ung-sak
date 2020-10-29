e50bdaf09b55f13df75bf488ff69bfb5cc4a2ebc
Frode Carlsen <frode.odde.carlsen@nav.no>
2020-10-29 13:20:45 +0100

    fix: tsf-1379 fortsett behandling for saksnummer 7BCBo - ferdigstill iverksett

---
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_190__tsf-1379-saksnummer-7BCBo-ferdigstill-iverksett.sql
@@ -0,0 +1,9 @@
+INSERT INTO prosess_task (id, task_type, task_gruppe, neste_kjoering_etter, task_parametere)
+SELECT nextval('seq_prosess_task'),
+  'behandlingskontroll.fortsettBehandling',
+  nextval('seq_prosess_task_gruppe'),
+  null,
+  'fagsakId=' || b.fagsak_id || '
+    behandlingId=' || b.id
+from behandling b where 
+ b.id = 1222947 and b.fagsak_id=1047857 and b.behandling_status='IVED';
