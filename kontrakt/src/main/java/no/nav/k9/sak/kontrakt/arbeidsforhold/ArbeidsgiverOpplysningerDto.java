70df789c671fb06ba5ca584eb5263a1a4eda459c
Marius Glittum <marius.glittum@nav.no>
2020-11-06 13:01:58 +0100

    Legger på annotasjoner på dto

--- kontrakt/src/main/java/no/nav/k9/sak/kontrakt/arbeidsforhold/ArbeidsgiverOpplysningerDto.java
+++ kontrakt/src/main/java/no/nav/k9/sak/kontrakt/arbeidsforhold/ArbeidsgiverOpplysningerDto.java
@@ -2,11 +2,23 @@ package no.nav.k9.sak.kontrakt.arbeidsforhold;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+@JsonAutoDetect(getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE, fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class ArbeidsgiverOpplysningerDto {
 
+    @JsonProperty(value = "referanse")
     private final String referanse;
+    @JsonProperty(value = "identifikator")
     private final String identifikator;
+    @JsonProperty(value = "navn")
     private final String navn;
+    @JsonProperty(value = "fødselsdato")
     private LocalDate fødselsdato;
 
     public ArbeidsgiverOpplysningerDto(String referanse, String identifikator, String navn, LocalDate fødselsdato) {
