58ddb0e5256bca7cb7c7f6b2eef3f878474ff1cd
Marius Glittum <marius.glittum@nav.no>
2020-11-18 17:06:27 +0100

    Ruller tilbake alle saker med 5080 etter endret utledningskritterie

---
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_198__ruller-tilbake-5080.sql
@@ -0,0 +1,16 @@
+INSERT INTO prosess_task (id, task_type, task_gruppe, neste_kjoering_etter, task_parametere)
+SELECT nextval('seq_prosess_task'),
+       'behandlingskontroll.tilbakeTilStart',
+       nextval('seq_prosess_task_gruppe'),
+       null,
+       'fagsakId=' || f.id || '
+  behandlingId=' || b.id || '
+  startSteg=START'
+FROM AKSJONSPUNKT a INNER JOIN BEHANDLING b ON (
+        b.id = a.behandling_id
+    ) INNER JOIN Fagsak f ON (
+        f.id = b.fagsak_id
+    )
+WHERE a.aksjonspunkt_def IN ('5080')
+  AND a.aksjonspunkt_status = 'OPPR'
+  AND f.ytelse_type = 'OMP';
