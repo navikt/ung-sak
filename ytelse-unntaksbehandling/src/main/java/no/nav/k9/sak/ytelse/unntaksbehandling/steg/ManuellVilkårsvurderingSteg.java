c7c4ec903ae6dabe3ab70095bd1245e726140a88
s149212 <espen.svendsen@nav.no>
2020-11-19 09:29:27 +0100

    Nullstiller beregningsresultat dersom saksbehandler endrer fra Innvilget -> Avslått
    Signed-off-by: s149212 <espen.svendsen@nav.no>

--- ytelse-unntaksbehandling/src/main/java/no/nav/k9/sak/ytelse/unntaksbehandling/steg/ManuellVilkårsvurderingSteg.java
+++ ytelse-unntaksbehandling/src/main/java/no/nav/k9/sak/ytelse/unntaksbehandling/steg/ManuellVilkårsvurderingSteg.java
@@ -13,6 +13,8 @@ import no.nav.k9.sak.behandlingskontroll.BehandlingStegRef;
 import no.nav.k9.sak.behandlingskontroll.BehandlingTypeRef;
 import no.nav.k9.sak.behandlingskontroll.BehandlingskontrollKontekst;
 import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
+import no.nav.k9.sak.behandlingslager.behandling.Behandling;
+import no.nav.k9.sak.behandlingslager.behandling.beregning.BeregningsresultatRepository;
 import no.nav.k9.sak.behandlingslager.behandling.repository.BehandlingRepository;
 
 @FagsakYtelseTypeRef
@@ -22,23 +24,47 @@ import no.nav.k9.sak.behandlingslager.behandling.repository.BehandlingRepository
 public class ManuellVilkårsvurderingSteg implements BehandlingSteg {
 
     private BehandlingRepository behandlingRepository;
+    private BeregningsresultatRepository beregningsresultatRepository;
 
     public ManuellVilkårsvurderingSteg() {
         // CDO
     }
 
     @Inject
-    public ManuellVilkårsvurderingSteg(BehandlingRepository behandlingRepository) {
+    public ManuellVilkårsvurderingSteg(BehandlingRepository behandlingRepository, BeregningsresultatRepository beregningsresultatRepository) {
         this.behandlingRepository = behandlingRepository;
+        this.beregningsresultatRepository = beregningsresultatRepository;
     }
 
     @Override
     public BehandleStegResultat utførSteg(BehandlingskontrollKontekst kontekst) {
         var behandling = behandlingRepository.hentBehandling(kontekst.getBehandlingId());
-        if (BehandlingResultatType.INNVILGET.equals(behandling.getBehandlingResultatType())) {
-            return BehandleStegResultat.utførtMedAksjonspunkter(List.of(AksjonspunktDefinisjon.MANUELL_TILKJENT_YTELSE));
+
+        if (BehandlingResultatType.AVSLÅTT.equals(behandling.getBehandlingResultatType())) {
+            nullstillTilkjentYtelse(behandling, kontekst);
+            return BehandleStegResultat.utførtUtenAksjonspunkter();
         }
-        return BehandleStegResultat.utførtUtenAksjonspunkter();
+        return BehandleStegResultat.utførtMedAksjonspunkter(List.of(AksjonspunktDefinisjon.MANUELL_TILKJENT_YTELSE));
+    }
+
+    private void nullstillTilkjentYtelse(Behandling behandling, BehandlingskontrollKontekst kontekst) {
+        var origBehandlingId = behandling.getOriginalBehandlingId();
+
+        if (!origBehandlingId.isPresent()) {
+            beregningsresultatRepository.deaktiverBeregningsresultat(behandling.getId(), kontekst.getSkriveLås());
+            return;
+        }
+
+        // Reverter til beregningsresultat fra forrige behandling
+        beregningsresultatRepository.hentBeregningsresultatAggregat(origBehandlingId.get())
+            .ifPresent(origAggregat -> {
+                if (origAggregat.getBgBeregningsresultat() != null) {
+                    beregningsresultatRepository.lagre(behandling, origAggregat.getBgBeregningsresultat());
+                }
+                if (origAggregat.getOverstyrtBeregningsresultat() != null) {
+                    beregningsresultatRepository.lagre(behandling, origAggregat.getOverstyrtBeregningsresultat());
+                }
+            });
     }
 
 }
