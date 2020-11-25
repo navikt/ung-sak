181f5e0001aac46fc99d96a9d97b2416b800823f
Helga Nyrud <45488635+helganyrud@users.noreply.github.com>
2020-11-25 12:15:04 +0100

    Ruller tilbake alle saker på 9003 etter endringer (#2689)

---
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_205__ruller-tilbake-9003.sql
@@ -0,0 +1,17 @@
+INSERT INTO prosess_task (id, task_type, task_gruppe, neste_kjoering_etter, task_parametere)
+SELECT nextval('seq_prosess_task'),
+       'behandlingskontroll.tilbakeTilStart',
+       nextval('seq_prosess_task_gruppe'),
+       current_timestamp at time zone 'UTC' + floor(random() * 2 * 3600) * '1 second'::interval,
+       'fagsakId=' || f.id || '
+  behandlingId=' || b.id || '
+  startSteg=VURDER_UTTAK'
+FROM AKSJONSPUNKT a INNER JOIN BEHANDLING b ON (
+        b.id = a.behandling_id
+    ) INNER JOIN Fagsak f ON (
+        f.id = b.fagsak_id
+    )
+WHERE a.aksjonspunkt_def IN ('9003')
+  AND a.aksjonspunkt_status = 'OPPR'
+  AND b.behandling_status IN ('OPPRE', 'UTRED')
+  AND f.ytelse_type = 'OMP';
