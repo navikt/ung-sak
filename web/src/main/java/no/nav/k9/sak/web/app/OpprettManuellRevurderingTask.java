e44cf0a22c5505ba874cce437b17f5adf48689d0
Stian Grenborgen <stian.grenborgen@nav.no>
2020-12-15 12:20:56 +0100

    Fjerner unødvendig import.

--- web/src/main/java/no/nav/k9/sak/web/app/OpprettManuellRevurderingTask.java
+++ web/src/main/java/no/nav/k9/sak/web/app/OpprettManuellRevurderingTask.java
@@ -21,7 +21,6 @@ import no.nav.k9.sak.behandlingslager.behandling.repository.BehandlingRepository
 import no.nav.k9.sak.behandlingslager.fagsak.Fagsak;
 import no.nav.k9.sak.typer.Saksnummer;
 import no.nav.k9.sak.web.app.tjenester.behandling.aksjonspunkt.BehandlingsoppretterApplikasjonTjeneste;
-import no.nav.vedtak.exception.VLException;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTask;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskData;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskHandler;
