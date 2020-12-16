ffdd4e0cf21b9a3d698c2f8d8de97bb4147a3c40
Anders Flaten <anders.flaten@nav.no>
2020-12-16 12:13:07 +0100

    TSF-1298: Optimert litt
    Signed-off-by: Anders Flaten <anders.flaten@nav.no>

--- domenetjenester/person/src/main/java/no/nav/k9/sak/domene/person/pdl/AktørTjeneste.java
+++ domenetjenester/person/src/main/java/no/nav/k9/sak/domene/person/pdl/AktørTjeneste.java
@@ -1,6 +1,7 @@
 package no.nav.k9.sak.domene.person.pdl;
 
 import static java.util.Optional.ofNullable;
+import static org.jboss.weld.util.collections.ImmutableList.of;
 
 import java.util.List;
 import java.util.Objects;
@@ -29,7 +30,7 @@ import no.nav.vedtak.util.LRUCache;
 @ApplicationScoped
 public class AktørTjeneste {
 
-    private static final Logger LOG = LoggerFactory.getLogger(AktørTjeneste.class);
+    private static final Logger log = LoggerFactory.getLogger(AktørTjeneste.class);
 
     private static final int DEFAULT_CACHE_SIZE = 1000;
     private static final long DEFAULT_CACHE_TIMEOUT = TimeUnit.MILLISECONDS.convert(8, TimeUnit.HOURS);
@@ -66,7 +67,7 @@ public class AktørTjeneste {
         try {
             Optional<AktørId> aktørIdFraTps = aktørConsumer.hentAktørIdForPersonIdent(personIdent.getIdent()).map(AktørId::new);
             aktørIdFraTps.ifPresent(aktørId -> {
-                    hentAktørIdFraPDL(personIdent.getIdent(), aktørId.getId());
+                    sjekkAktørIdFraPDL(personIdent.getIdent(), aktørId.getId());
                     // Kan ikke legge til i cache aktørId -> ident ettersom ident kan være ikke-current
                     cacheIdentTilAktørId.put(personIdent, aktørId);
                 }
@@ -86,7 +87,7 @@ public class AktørTjeneste {
 
         Optional<PersonIdent> personIdent = aktørConsumer.hentPersonIdentForAktørId(aktørId.getId()).map(PersonIdent::new);
         personIdent.ifPresent(i -> {
-                hentPersonIdentFraPDL(aktørId.getId(), i.getIdent());
+                sjekkPersonIdentFraPDL(aktørId.getId(), i.getIdent());
                 cacheAktørIdTilIdent.put(aktørId, i);
                 cacheIdentTilAktørId.put(i, aktørId); // OK her, men ikke over ettersom dette er gjeldende mapping
             }
@@ -95,52 +96,57 @@ public class AktørTjeneste {
         return personIdent;
     }
 
-    private void hentAktørIdFraPDL(String fnr, String aktørFraConsumer) {
+    private void sjekkAktørIdFraPDL(String personIdent, String aktørFraConsumer) {
         try {
-            var request = new HentIdenterQueryRequest();
-            request.setIdent(fnr);
-            request.setGrupper(List.of(IdentGruppe.AKTORID));
-            request.setHistorikk(Boolean.FALSE);
-            var projection = new IdentlisteResponseProjection()
-                .identer(new IdentInformasjonResponseProjection().ident());
-            //TODO Verifiser tema
-            var identliste = pdlKlient.hentIdenter(request, projection, Tema.FOR);
-            int antall = identliste.getIdenter().size();
-            var aktørId = identliste.getIdenter().stream().findFirst().map(IdentInformasjon::getIdent).orElse(null);
+            var aktørIder = identerFor(personIdent, IdentGruppe.AKTORID);
+            int antall = aktørIder.size();
+            var aktørId = aktørIder.stream().findFirst().map(IdentInformasjon::getIdent).orElse(null);
 
             if (antall == 1 && Objects.equals(aktørFraConsumer, aktørId)) {
-                LOG.info("PDL fnr->aktørid: like aktørid");
+                log.info("PDL personIdent->aktørid: like aktørid");
             } else if (antall != 1 && Objects.equals(aktørFraConsumer, aktørId)) {
-                LOG.info("PDL fnr->aktørid: ulikt antall aktørid {}", antall);
+                log.info("PDL personIdent->aktørid: ulikt antall aktørid {}", antall);
             } else {
-                LOG.info("PDL fnr->aktørid: ulike aktørid TPS {} og PDL {} antall {}", aktørFraConsumer, aktørId, antall);
+                log.info("PDL personIdent->aktørid: ulike aktørid TPS {} og PDL {} antall {}", aktørFraConsumer, aktørId, antall);
             }
         } catch (Exception e) {
-            LOG.info("PDL fnr->aktørid error", e);
+            log.info("PDL personIdent->aktørid error", e);
         }
     }
 
-    private void hentPersonIdentFraPDL(String aktørId, String identFraConsumer) {
+    private void sjekkPersonIdentFraPDL(String aktørId, String personIdentFraTps) {
         try {
-            var request = new HentIdenterQueryRequest();
-            request.setIdent(aktørId);
-            request.setGrupper(List.of(IdentGruppe.FOLKEREGISTERIDENT));
-            request.setHistorikk(Boolean.FALSE);
-            var projection = new IdentlisteResponseProjection()
-                .identer(new IdentInformasjonResponseProjection().ident());
-            var identliste = pdlKlient.hentIdenter(request, projection, Tema.FOR);
-            int antall = identliste.getIdenter().size();
-            var fnr = identliste.getIdenter().stream().findFirst().map(IdentInformasjon::getIdent).orElse(null);
-            if (antall == 1 && Objects.equals(identFraConsumer, fnr)) {
-                LOG.info("PDL aktørid->fnr: like identer");
-            } else if (antall != 1 && Objects.equals(identFraConsumer, fnr)) {
-                LOG.info("PDL aktørid->fnr: ulikt antall identer {}", antall);
+            var personIdenter = identerFor(aktørId, IdentGruppe.FOLKEREGISTERIDENT);
+            int antall = personIdenter.size();
+            var personIdent = personIdenter.stream().findFirst().map(IdentInformasjon::getIdent).orElse(null);
+
+            if (antall == 1 && Objects.equals(personIdentFraTps, personIdent)) {
+                log.info("PDL aktørid->personIdent: like identer");
+            } else if (antall != 1 && Objects.equals(personIdentFraTps, personIdent)) {
+                log.info("PDL aktørid->personIdent: ulikt antall identer {}", antall);
             } else {
-                LOG.info("PDL aktørid->fnr: ulike identer TPS {} og PDL {} antall {}", identFraConsumer, fnr, antall);
+                log.info("PDL aktørid->personIdent: ulike identer TPS {} og PDL {} antall {}", personIdentFraTps, personIdent, antall);
             }
         } catch (Exception e) {
-            LOG.info("PDL aktørid->fnr error", e);
+            log.info("PDL aktørid->fnr error", e);
         }
     }
+
+    private List<IdentInformasjon> identerFor(String ident, IdentGruppe identGruppe) {
+        HentIdenterQueryRequest request = new HentIdenterQueryRequest();
+        request.setIdent(ident);
+        request.setGrupper(of(identGruppe));
+        request.setHistorikk(Boolean.FALSE);
+
+        IdentlisteResponseProjection projeksjon = new IdentlisteResponseProjection()
+            .identer(
+                new IdentInformasjonResponseProjection()
+                    .ident()
+                    .gruppe()
+            );
+
+        return pdlKlient.hentIdenter(request, projeksjon, Tema.FOR).getIdenter();
+    }
+
 
 }
