499303217399913c8c93502f501881ed00c5dc96
s149212 <espen.svendsen@nav.no>
2021-01-07 11:08:50 +0100

    Legger til bruker_part for ny visningslogikk frontend
    Personinfo hentes nå fra PDL
    
    Signed-off-by: s149212 <espen.svendsen@nav.no>

--- domenetjenester/person/src/main/java/no/nav/k9/sak/domene/person/pdl/PersonBasisTjeneste.java
+++ domenetjenester/person/src/main/java/no/nav/k9/sak/domene/person/pdl/PersonBasisTjeneste.java
@@ -10,14 +10,29 @@ import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import no.nav.k9.kodeverk.person.Diskresjonskode;
+import no.nav.k9.kodeverk.person.NavBrukerKjønn;
+import no.nav.k9.kodeverk.person.PersonstatusType;
 import no.nav.k9.sak.behandlingslager.aktør.PersoninfoArbeidsgiver;
+import no.nav.k9.sak.behandlingslager.aktør.PersoninfoBasis;
 import no.nav.k9.sak.typer.AktørId;
 import no.nav.k9.sak.typer.PersonIdent;
+import no.nav.pdl.Adressebeskyttelse;
+import no.nav.pdl.AdressebeskyttelseGradering;
+import no.nav.pdl.AdressebeskyttelseResponseProjection;
+import no.nav.pdl.Doedsfall;
+import no.nav.pdl.DoedsfallResponseProjection;
 import no.nav.pdl.Foedsel;
 import no.nav.pdl.FoedselResponseProjection;
+import no.nav.pdl.Folkeregisterpersonstatus;
+import no.nav.pdl.FolkeregisterpersonstatusResponseProjection;
 import no.nav.pdl.HentPersonQueryRequest;
+import no.nav.pdl.Kjoenn;
+import no.nav.pdl.KjoennResponseProjection;
+import no.nav.pdl.KjoennType;
 import no.nav.pdl.Navn;
 import no.nav.pdl.NavnResponseProjection;
+import no.nav.pdl.Person;
 import no.nav.pdl.PersonResponseProjection;
 import no.nav.vedtak.felles.integrasjon.pdl.PdlKlient;
 import no.nav.vedtak.felles.integrasjon.pdl.Tema;
@@ -44,6 +59,60 @@ public class PersonBasisTjeneste {
         if (navn.getForkortetNavn() != null)
             return navn.getForkortetNavn();
         return navn.getEtternavn() + " " + navn.getFornavn() + (navn.getMellomnavn() == null ? "" : " " + navn.getMellomnavn());
+    }
+
+    public PersoninfoBasis hentBasisPersoninfo(AktørId aktørId, PersonIdent personIdent) {
+        var query = new HentPersonQueryRequest();
+        query.setIdent(aktørId.getId());
+        var projection = new PersonResponseProjection()
+            .navn(new NavnResponseProjection().forkortetNavn().fornavn().mellomnavn().etternavn())
+            .foedsel(new FoedselResponseProjection().foedselsdato())
+            .doedsfall(new DoedsfallResponseProjection().doedsdato())
+            .folkeregisterpersonstatus(new FolkeregisterpersonstatusResponseProjection().status())
+            .kjoenn(new KjoennResponseProjection().kjoenn())
+            .adressebeskyttelse(new AdressebeskyttelseResponseProjection().gradering());
+
+        var person = pdlKlient.hentPerson(query, projection, Tema.FOR);
+
+        var fødselsdato = person.getFoedsel().stream()
+            .map(Foedsel::getFoedselsdato)
+            .filter(Objects::nonNull)
+            .findFirst().map(d -> LocalDate.parse(d, DateTimeFormatter.ISO_LOCAL_DATE)).orElseGet(() -> isProd ? null : LocalDate.now().minusDays(1));
+        var dødsdato = person.getDoedsfall().stream()
+            .map(Doedsfall::getDoedsdato)
+            .filter(Objects::nonNull)
+            .findFirst().map(d -> LocalDate.parse(d, DateTimeFormatter.ISO_LOCAL_DATE)).orElse(null);
+        var pdlStatus = person.getFolkeregisterpersonstatus().stream()
+            .map(Folkeregisterpersonstatus::getStatus)
+            .findFirst().map(PersonstatusType::fraFregPersonstatus).orElse(PersonstatusType.UDEFINERT);
+        return new PersoninfoBasis.Builder().medAktørId(aktørId).medPersonIdent(personIdent)
+            .medNavn(person.getNavn().stream().map(PersonBasisTjeneste::mapNavn).filter(Objects::nonNull).findFirst().orElseGet(() -> isProd ? null : "Navnløs i Folkeregister"))
+            .medFødselsdato(fødselsdato)
+            .medDødsdato(dødsdato)
+            .medDiskresjonsKode(getDiskresjonskode(person))
+            .medNavBrukerKjønn(mapKjønn(person))
+            .medPersonstatusType(pdlStatus)
+            .build();
+    }
+
+    private String getDiskresjonskode(Person person) {
+        var kode = person.getAdressebeskyttelse().stream()
+            .map(Adressebeskyttelse::getGradering)
+            .filter(g -> !AdressebeskyttelseGradering.UGRADERT.equals(g))
+            .findFirst().orElse(null);
+        if (AdressebeskyttelseGradering.STRENGT_FORTROLIG.equals(kode) || AdressebeskyttelseGradering.STRENGT_FORTROLIG_UTLAND.equals(kode))
+            return Diskresjonskode.KODE6.getKode();
+        return AdressebeskyttelseGradering.FORTROLIG.equals(kode) ? Diskresjonskode.KODE7.getKode() : null;
+    }
+
+    private static NavBrukerKjønn mapKjønn(Person person) {
+        var kode = person.getKjoenn().stream()
+            .map(Kjoenn::getKjoenn)
+            .filter(Objects::nonNull)
+            .findFirst().orElse(KjoennType.UKJENT);
+        if (KjoennType.MANN.equals(kode))
+            return NavBrukerKjønn.MANN;
+        return KjoennType.KVINNE.equals(kode) ? NavBrukerKjønn.KVINNE : NavBrukerKjønn.UDEFINERT;
     }
 
     public void hentOgSjekkPersoninfoArbeidsgiverFraPDL(AktørId aktørId, PersonIdent personIdent, PersoninfoArbeidsgiver fraTPS) {
