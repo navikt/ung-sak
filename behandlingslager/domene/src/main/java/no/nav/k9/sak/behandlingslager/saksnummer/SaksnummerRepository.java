8c9f0dcdc22059e44dc989b2674511a21c47cbbe
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2021-01-20 13:12:04 +0100

    Unngår kollisjon i saksnumre mellom prod og testmiljøet

--- behandlingslager/domene/src/main/java/no/nav/k9/sak/behandlingslager/saksnummer/SaksnummerRepository.java
+++ behandlingslager/domene/src/main/java/no/nav/k9/sak/behandlingslager/saksnummer/SaksnummerRepository.java
@@ -8,6 +8,8 @@ import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
+import no.nav.vedtak.util.env.Environment;
+
 @Dependent
 public class SaksnummerRepository {
 
@@ -25,7 +27,13 @@ public class SaksnummerRepository {
 
     public String genererNyttSaksnummer() {
         final Query query = entityManager.createNativeQuery("SELECT nextval('SEQ_SAKSNUMMER')");
-        final BigInteger tall = (BigInteger) query.getSingleResult();
+        BigInteger tall = (BigInteger) query.getSingleResult();
+
+        if (Environment.current().isDev()) {
+            //legg merke til at SEQ_SAKSNUMMER starter på samme verdi og øker med 50 i alle miljøer
+            //for å unngå å generere samme saksnummer i q og p legger vi til 1
+            tall = tall.add(BigInteger.ONE);
+        }
 
         return Long.toString(tall.longValue(), 36).toUpperCase().replace("O", "o").replace("I", "i");
     }
