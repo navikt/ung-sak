309d3f073d88a80089950f73b6a44e29a6f15065
s149212 <espen.svendsen@nav.no>
2021-01-27 15:06:14 +0100

    Justerer mapping for uttakgrunnlag igjen
    Signed-off-by: s149212 <espen.svendsen@nav.no>

--- ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/mottak/MapSøknadUttak.java
+++ ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/mottak/MapSøknadUttak.java
@@ -41,26 +41,22 @@ class MapSøknadUttak {
             .map(uttakPeriode -> new UttakAktivitetPeriode(uttakPeriode.getAktivitetType(), uttakPeriode.getPeriode().getFomDato(), uttakPeriode.getPeriode().getTomDato()))
             .collect(Collectors.toList());
 
-        // TODO: Lag filteringslogikk slik at vi kun ser på oppgitt opptjeningsperiode som motsvarer selve søknadsperioden
-        // For enkelthets skyld brukes kun første fraværsperiode
-        var fraværPeriode = søknad.getFraværsperioder().stream().findFirst().orElseThrow();
-
         // Nye uttaksaktivitetsperioder - SN
-        for (SelvstendigNæringsdrivende sn : snAktiviteter) {
-            for (var periode : sn.perioder.keySet()) {
+        var fraværsperioder = søknad.getFraværsperioder();
+        for (FraværPeriode fraværPeriode : fraværsperioder) {
+            for (SelvstendigNæringsdrivende sn : snAktiviteter) {
                 InternArbeidsforholdRef arbeidsforholdRef = null; // får ikke fra søknad, setter default null her, tolker om til InternArbeidsforholdRef.nullRef() ved fastsette uttak.
+                BigDecimal skalJobbeProsent = null; // får ikke fra søknad, må settes til null dersom jobberNormaltPerUke er satt til null
                 var arbeidsgiver = sn.organisasjonsnummer != null
                     ? Arbeidsgiver.virksomhet(sn.organisasjonsnummer.verdi)
                     : (søker.norskIdentitetsnummer != null
                     ? Arbeidsgiver.fra(new AktørId(søker.norskIdentitetsnummer.verdi))
                     : null);
-                // TODO: Blir det riktig å legge på arbeidsforhold slik for SELVSTENDIG_NÆRINGSDRIVENDE? Eller bare for ARBEIDSTAKER?
-                UttakAktivitetPeriode uttakAktivitetPeriode = new UttakAktivitetPeriode(periode.getFraOgMed(), periode.getTilOgMed(), UttakArbeidType.SELVSTENDIG_NÆRINGSDRIVENDE, arbeidsgiver, arbeidsforholdRef,
-                    fraværPeriode.getDuration(), BigDecimal.valueOf(100L));
+                UttakAktivitetPeriode uttakAktivitetPeriode = new UttakAktivitetPeriode(fraværPeriode.getPeriode().getFraOgMed(), fraværPeriode.getPeriode().getTilOgMed(),
+                    UttakArbeidType.SELVSTENDIG_NÆRINGSDRIVENDE, arbeidsgiver, arbeidsforholdRef, fraværPeriode.getDuration(), skalJobbeProsent);
                 uttakPerioder.add(uttakAktivitetPeriode);
             }
         }
-
         var oppgittUttak = new UttakAktivitet(uttakPerioder);
         var fastsattUttak = oppgittUttak;
         return new UttakGrunnlag(behandlingId, oppgittUttak, fastsattUttak, søknadsperioder);
