ebee018faf00dfbe9c1b6520b3d132afd8e6370d
Frode Carlsen <frode.odde.carlsen@nav.no>
2021-02-04 10:17:54 +0100

    oppgradering for nye baseimages (#3088)
    * oppgradering for nye baseimages
    
    kjører under apprunner bruker
    
    * Update and rename run-java.sh to app-init.sh
    
    * Update Dockerfile
    
    * Rename app-init.sh to init-app.sh

--- build/init-app.sh
+++ build/init-app.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env sh
 set -eu
 
-export JAVA_OPTS="${JAVA_OPTS:-} -XX:MaxRAMPercentage=75.0 -Djava.security.egd=file:/dev/./urandom -Duser.timezone=Europe/Oslo"
-export STARTUP_CLASS=${STARTUP_CLASS:-"no.nav.k9.sak.web.server.jetty.JettyServer"}
-
 if test -f /var/run/secrets/nais.io/serviceuser/username
 then
     export SYSTEMBRUKER_USERNAME="$(cat /var/run/secrets/nais.io/serviceuser/username)"
@@ -15,7 +12,3 @@ then
     export LDAP_USERNAME="$(cat /var/run/secrets/nais.io/ldap/username)"
     export LDAP_PASSWORD="$(cat /var/run/secrets/nais.io/ldap/password)"
 fi
-
-exec java -cp "app.jar:lib/*"${EXTRA_CLASS_PATH:-""} ${DEFAULT_JAVA_OPTS:-} ${JAVA_OPTS} \
-    -Dwebapp=${WEBAPP:-"./webapp"} \
-    -Dapplication.name=${APP_NAME} ${STARTUP_CLASS?} $@
