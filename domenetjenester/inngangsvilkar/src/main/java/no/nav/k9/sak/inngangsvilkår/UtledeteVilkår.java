efe84267f1a65ab31aa6f61b8170d7c3bf944ce5
Marius Bækken Glittum <marius.glittum@nav.no>
2021-02-05 11:29:26 +0100

    Skrur på validering mot fagsaksinterval i prod (#3094)
    Legger på validering at vi har samme antall vilkår i resultatet som vi utleder

--- domenetjenester/inngangsvilkar/src/main/java/no/nav/k9/sak/inngangsvilkår/UtledeteVilkår.java
+++ domenetjenester/inngangsvilkar/src/main/java/no/nav/k9/sak/inngangsvilkår/UtledeteVilkår.java
@@ -1,30 +1,31 @@
 package no.nav.k9.sak.inngangsvilkår;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 import no.nav.k9.kodeverk.vilkår.VilkårType;
 
 public class UtledeteVilkår {
 
-     // Avklart betinget vilkår. Kan initielt settes til null, men må avklares før steg Kontroller fakta er ferdig.
-     private VilkårType avklartBetingetVilkårType;
+    // Avklart betinget vilkår. Kan initielt settes til null, men må avklares før steg Kontroller fakta er ferdig.
+    private VilkårType avklartBetingetVilkårType;
 
-     // I tillegg til betinget vilkår vil det følge tilhørende vilkår
-     private List<VilkårType> tilhørendeVilkår;
+    // I tillegg til betinget vilkår vil det følge tilhørende vilkår
+    private List<VilkårType> tilhørendeVilkår;
 
-     public UtledeteVilkår(VilkårType betingetVilkårType, List<VilkårType> tilhørendeVilkårTyper) {
+    public UtledeteVilkår(VilkårType betingetVilkårType, List<VilkårType> tilhørendeVilkårTyper) {
         avklartBetingetVilkårType = betingetVilkårType;
         tilhørendeVilkår = tilhørendeVilkårTyper;
     }
 
-    public List<VilkårType> getAlleAvklarte() {
-         List<VilkårType> avklarteVilkår = new ArrayList<>();
-         // Betinget vilkår kan være uavklart inntil aksjonspunkt er løst.
-         if (avklartBetingetVilkårType != null) {
-             avklarteVilkår.add(avklartBetingetVilkårType);
-         }
-         avklarteVilkår.addAll(tilhørendeVilkår);
-         return avklarteVilkår;
-     }
- }
+    public Set<VilkårType> getAlleAvklarte() {
+        Set<VilkårType> avklarteVilkår = new TreeSet<>();
+        // Betinget vilkår kan være uavklart inntil aksjonspunkt er løst.
+        if (avklartBetingetVilkårType != null) {
+            avklarteVilkår.add(avklartBetingetVilkårType);
+        }
+        avklarteVilkår.addAll(tilhørendeVilkår);
+        return avklarteVilkår;
+    }
+}
