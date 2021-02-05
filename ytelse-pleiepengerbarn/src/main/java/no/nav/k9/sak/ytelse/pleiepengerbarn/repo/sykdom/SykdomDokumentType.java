47815ecdb24695e0553695e4f0a2e48025c7b165
Stian Grenborgen <stian.grenborgen@nav.no>
2021-02-05 09:35:56 +0100

    Setter eksplisitt apikode fremfor å bruke enumnavn.

--- ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/repo/sykdom/SykdomDokumentType.java
+++ ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/repo/sykdom/SykdomDokumentType.java
@@ -2,33 +2,52 @@ package no.nav.k9.sak.ytelse.pleiepengerbarn.repo.sykdom;
 
 import javax.persistence.AttributeConverter;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum SykdomDokumentType {
-    LEGEERKLÆRING_SYKEHUS("L"),
-    MEDISINSKE_OPPLYSNINGER("M"),
-    ANNET("A"),
-    UKLASSIFISERT("U");
+    LEGEERKLÆRING_SYKEHUS("LEGEERKLÆRING_SYKEHUS", "L"),
+    MEDISINSKE_OPPLYSNINGER("MEDISINSKE_OPPLYSNINGER", "M"),
+    ANNET("ANNET", "A"),
+    UKLASSIFISERT("UKLASSIFISERT", "U");
     
-    private final String kode;
     
-    SykdomDokumentType(String kode) {
-        this.kode = kode;
+    @JsonValue
+    private final String apikode;
+    
+    @JsonIgnore
+    private final String databasekode;
+    
+    
+    SykdomDokumentType(String apikode, String databasekode) {
+        this.apikode = apikode;
+        this.databasekode = databasekode;
     }
     
-    public String getKode() {
-        return kode;
+    
+    @JsonCreator(mode = Mode.DELEGATING)
+    public static SykdomDokumentType fraApikode(String s) {
+        for (var type : values()) {
+            if (type.apikode.equals(s)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("Ukjent type: " + s);
     }
     
     
     public static class SykdomDokumentTypeConverter implements AttributeConverter<SykdomDokumentType, String> {
         @Override
         public String convertToDatabaseColumn(SykdomDokumentType type) {
-            return type.kode;
+            return type.databasekode;
         }
         
         @Override
-        public SykdomDokumentType convertToEntityAttribute(String kode) {
+        public SykdomDokumentType convertToEntityAttribute(String databasekode) {
             for (SykdomDokumentType type : values()) {
-                if (type.kode.equals(kode)) {
+                if (type.databasekode.equals(databasekode)) {
                     return type;
                 }
             }
