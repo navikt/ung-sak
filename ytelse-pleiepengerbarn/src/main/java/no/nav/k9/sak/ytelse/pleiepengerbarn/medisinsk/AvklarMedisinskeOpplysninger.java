241555f865c498d72ca3928c0e5c58af98cf3c8c
Stian Grenborgen <stian.grenborgen@nav.no>
2021-02-15 09:30:25 +0100

    Legger til enkelt historikkinnslag for sykdomssteget.

--- ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/medisinsk/AvklarMedisinskeOpplysninger.java
+++ ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/medisinsk/AvklarMedisinskeOpplysninger.java
@@ -1,24 +1,44 @@
 package no.nav.k9.sak.ytelse.pleiepengerbarn.medisinsk;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
+import no.nav.k9.kodeverk.behandling.aksjonspunkt.SkjermlenkeType;
+import no.nav.k9.kodeverk.historikk.HistorikkinnslagType;
 import no.nav.k9.sak.behandling.aksjonspunkt.AksjonspunktOppdaterParameter;
 import no.nav.k9.sak.behandling.aksjonspunkt.AksjonspunktOppdaterer;
 import no.nav.k9.sak.behandling.aksjonspunkt.DtoTilServiceAdapter;
 import no.nav.k9.sak.behandling.aksjonspunkt.OppdateringResultat;
+import no.nav.k9.sak.historikk.HistorikkTjenesteAdapter;
 import no.nav.k9.sak.kontrakt.medisinsk.aksjonspunkt.AvklarMedisinskeOpplysningerDto;
 
 @ApplicationScoped
 @DtoTilServiceAdapter(dto = AvklarMedisinskeOpplysningerDto.class, adapter = AksjonspunktOppdaterer.class)
 public class AvklarMedisinskeOpplysninger implements AksjonspunktOppdaterer<AvklarMedisinskeOpplysningerDto> {
 
+    private HistorikkTjenesteAdapter historikkTjenesteAdapter;
+    
     AvklarMedisinskeOpplysninger() {
         // for CDI proxy
+    }
+    
+    @Inject
+    public AvklarMedisinskeOpplysninger(HistorikkTjenesteAdapter historikkTjenesteAdapter) {
+        this.historikkTjenesteAdapter = historikkTjenesteAdapter;
     }
 
     @Override
     public OppdateringResultat oppdater(AvklarMedisinskeOpplysningerDto dto, AksjonspunktOppdaterParameter param) {
+        lagHistorikkinnslag(param);
+
         return OppdateringResultat.utenOveropp();
+    }
+
+    private void lagHistorikkinnslag(AksjonspunktOppdaterParameter param) {
+        historikkTjenesteAdapter.tekstBuilder()
+            .medSkjermlenke(SkjermlenkeType.FAKTA_OM_MEDLEMSKAP)
+            .medBegrunnelse("Sykdom manuelt behandlet.");
+        historikkTjenesteAdapter.opprettHistorikkInnslag(param.getBehandlingId(), HistorikkinnslagType.FAKTA_ENDRET);
     }
 
 }
