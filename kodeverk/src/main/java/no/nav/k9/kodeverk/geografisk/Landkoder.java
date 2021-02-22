30f67c31f82b8eb049d6d47bc5d3d8437d94c30f
s149212 <espen.svendsen@nav.no>
2021-02-22 15:36:42 +0100

    Endrer til hardkodede stringverdier (LandkoderKodeverdiConverter feiler)
    Signed-off-by: s149212 <espen.svendsen@nav.no>

--- kodeverk/src/main/java/no/nav/k9/kodeverk/geografisk/Landkoder.java
+++ kodeverk/src/main/java/no/nav/k9/kodeverk/geografisk/Landkoder.java
@@ -35,11 +35,6 @@ public class Landkoder implements Kodeverdi {
     public static final Landkoder CAN = fraKode("CAN");
     public static final Landkoder ESP = fraKode("ESP");
 
-    /** NAV-spesfikke konstanter */
-    public static final Landkoder STATSLØS = fraKode("XXX");
-    public static final Landkoder UKJENT = fraKode("XUK");
-    public static final Landkoder KOSOVO = fraKode("XXK");
-
     /** Kodeverkklient spesifikk konstant. Bruker oppgir ikke land */
     public static final Landkoder UOPPGITT_UKJENT = fraKode("???");
 
@@ -127,7 +122,10 @@ public class Landkoder implements Kodeverdi {
 
         // ISO user defined codes (NAV spesifikke)
         // @see https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3#User-assigned_code_elements
-        List.of(STATSLØS.kode, UKJENT.kode, KOSOVO.kode)
+        List.of(
+            "XXX", // STATSLØS
+            "XUK", // UKJENT
+            "XXK") // KOSOVO
             .forEach(c -> map.put(c, new Landkoder(c)));
 
         // ISO transitional codes.
