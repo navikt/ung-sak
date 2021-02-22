70878745d337d523584b9381fdfde22192678a6e
Marius Bækken Glittum <marius.glittum@nav.no>
2021-02-22 22:50:17 +0100

    Mapper inn flere perioder typerinn til uttaket (#3261)

--- ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/uttak/InitierPerioderSteg.java
+++ ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/uttak/InitierPerioderSteg.java
@@ -1,6 +1,9 @@
 package no.nav.k9.sak.ytelse.pleiepengerbarn.uttak;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -17,11 +20,18 @@ import no.nav.k9.sak.behandlingslager.behandling.repository.BehandlingRepository
 import no.nav.k9.sak.mottak.repo.MottattDokument;
 import no.nav.k9.sak.mottak.repo.MottatteDokumentRepository;
 import no.nav.k9.sak.perioder.KravDokument;
+import no.nav.k9.sak.perioder.SøktPeriode;
 import no.nav.k9.sak.perioder.VurderSøknadsfristTjeneste;
+import no.nav.k9.sak.typer.JournalpostId;
 import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.søknadsperiode.Søknadsperiode;
+import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.søknadsperiode.SøknadsperiodeGrunnlag;
 import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.søknadsperiode.SøknadsperiodeRepository;
 import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.søknadsperiode.Søknadsperioder;
 import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.søknadsperiode.SøknadsperioderHolder;
+import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.uttak.PerioderFraSøknad;
+import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.uttak.UttakPerioderGrunnlagRepository;
+import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.uttak.UttakPerioderHolder;
+import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.uttak.UttaksPerioderGrunnlag;
 
 @ApplicationScoped
 @BehandlingStegRef(kode = "INIT_PERIODER")
@@ -32,6 +42,7 @@ public class InitierPerioderSteg implements BehandlingSteg {
     private BehandlingRepository behandlingRepository;
     private MottatteDokumentRepository mottatteDokumentRepository;
     private SøknadsperiodeRepository søknadsperiodeRepository;
+    private UttakPerioderGrunnlagRepository uttakPerioderGrunnlagRepository;
     private VurderSøknadsfristTjeneste<Søknadsperiode> søknadsfristTjeneste;
 
     InitierPerioderSteg() {
@@ -42,10 +53,12 @@ public class InitierPerioderSteg implements BehandlingSteg {
     public InitierPerioderSteg(BehandlingRepository behandlingRepository,
                                MottatteDokumentRepository mottatteDokumentRepository,
                                SøknadsperiodeRepository søknadsperiodeRepository,
+                               UttakPerioderGrunnlagRepository uttakPerioderGrunnlagRepository,
                                @FagsakYtelseTypeRef("PSB") VurderSøknadsfristTjeneste<Søknadsperiode> søknadsfristTjeneste) {
         this.behandlingRepository = behandlingRepository;
         this.mottatteDokumentRepository = mottatteDokumentRepository;
         this.søknadsperiodeRepository = søknadsperiodeRepository;
+        this.uttakPerioderGrunnlagRepository = uttakPerioderGrunnlagRepository;
         this.søknadsfristTjeneste = søknadsfristTjeneste;
     }
 
@@ -55,10 +68,12 @@ public class InitierPerioderSteg implements BehandlingSteg {
 
         var behandling = behandlingRepository.hentBehandling(behandlingId);
         var referanse = BehandlingReferanse.fra(behandling);
-        var søknadsperiodeGrunnlag = søknadsperiodeRepository.hentGrunnlag(behandlingId);
+        var søknadsperiodeGrunnlag = søknadsperiodeRepository.hentGrunnlag(behandlingId).orElseThrow();
+        var uttaksPerioderGrunnlag = uttakPerioderGrunnlagRepository.hentGrunnlag(behandlingId).orElseThrow();
 
         if (behandling.erManueltOpprettet()) {
-            søknadsperiodeRepository.lagreRelevanteSøknadsperioder(behandlingId, søknadsperiodeGrunnlag.orElseThrow().getOppgitteSøknadsperioder());
+            søknadsperiodeRepository.lagreRelevanteSøknadsperioder(behandlingId, søknadsperiodeGrunnlag.getOppgitteSøknadsperioder());
+            uttakPerioderGrunnlagRepository.lagreRelevantePerioder(behandlingId, uttaksPerioderGrunnlag.getOppgitteSøknadsperioder());
         } else {
             var kravDokumenterMedPerioder = søknadsfristTjeneste.hentPerioderTilVurdering(referanse);
             var mottatteDokumenter = mottatteDokumentRepository.hentMottatteDokumentMedFagsakId(behandling.getFagsakId())
@@ -67,21 +82,38 @@ public class InitierPerioderSteg implements BehandlingSteg {
                 .map(MottattDokument::getJournalpostId)
                 .collect(Collectors.toSet());
 
-            var entries = kravDokumenterMedPerioder.entrySet()
-                .stream()
-                .filter(it -> mottatteDokumenter.contains(it.getKey().getJournalpostId()))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-
-            var relevanteDokumenter = søknadsperiodeGrunnlag.orElseThrow().getOppgitteSøknadsperioder()
-                .getPerioder()
-                .stream()
-                .filter(it -> entries.keySet().stream().map(KravDokument::getJournalpostId).anyMatch(at -> at.getJournalpostId().equals(it.getJournalpostId())))
-                .collect(Collectors.toSet());
-
-            var søknadsperioderHolder = new SøknadsperioderHolder(relevanteDokumenter.stream().map(Søknadsperioder::new).collect(Collectors.toSet()));
+            var søknadsperioderHolder = mapSøknadsperioderRelevantForBehandlingen(kravDokumenterMedPerioder, mottatteDokumenter, søknadsperiodeGrunnlag);
             søknadsperiodeRepository.lagreRelevanteSøknadsperioder(behandlingId, søknadsperioderHolder);
 
+            var uttakPerioderHolder = mapUttaksPerioderRelevantForBehandlingen(uttaksPerioderGrunnlag, mottatteDokumenter);
+            uttakPerioderGrunnlagRepository.lagreRelevantePerioder(behandlingId, uttakPerioderHolder);
         }
         return BehandleStegResultat.utførtUtenAksjonspunkter();
+    }
+
+    private UttakPerioderHolder mapUttaksPerioderRelevantForBehandlingen(UttaksPerioderGrunnlag grunnlag, Set<JournalpostId> mottatteDokumenter) {
+        var perioderFraSøknader = grunnlag.getOppgitteSøknadsperioder()
+            .getUttakPerioder()
+            .stream()
+            .filter(it -> mottatteDokumenter.contains(it.getJournalpostId()))
+            .collect(Collectors.toSet());
+
+        return new UttakPerioderHolder(perioderFraSøknader);
+    }
+
+    private SøknadsperioderHolder mapSøknadsperioderRelevantForBehandlingen(Map<KravDokument, List<SøktPeriode<Søknadsperiode>>> kravDokumenterMedPerioder,
+                                                                            Set<JournalpostId> mottatteDokumenter, SøknadsperiodeGrunnlag grunnlag) {
+        var entries = kravDokumenterMedPerioder.entrySet()
+            .stream()
+            .filter(it -> mottatteDokumenter.contains(it.getKey().getJournalpostId()))
+            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        var relevanteDokumenter = grunnlag.getOppgitteSøknadsperioder()
+            .getPerioder()
+            .stream()
+            .filter(it -> entries.keySet().stream().map(KravDokument::getJournalpostId).anyMatch(at -> at.getJournalpostId().equals(it.getJournalpostId())))
+            .collect(Collectors.toSet());
+
+        return new SøknadsperioderHolder(relevanteDokumenter.stream().map(Søknadsperioder::new).collect(Collectors.toSet()));
     }
 }
--- ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/uttak/input/MapInputTilUttakTjeneste.java
+++ ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/uttak/input/MapInputTilUttakTjeneste.java
@@ -1,14 +1,15 @@
 package no.nav.k9.sak.ytelse.pleiepengerbarn.uttak.input;
 
-import java.math.BigDecimal;
 import java.time.Duration;
-import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 import javax.enterprise.context.Dependent;
@@ -17,21 +18,28 @@ import javax.inject.Inject;
 import no.nav.k9.kodeverk.medisinsk.Pleiegrad;
 import no.nav.k9.kodeverk.vilkår.VilkårType;
 import no.nav.k9.sak.behandling.BehandlingReferanse;
+import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
 import no.nav.k9.sak.behandlingslager.behandling.Behandling;
-import no.nav.k9.sak.behandlingslager.behandling.personopplysning.PersonopplysningEntitet;
 import no.nav.k9.sak.behandlingslager.behandling.personopplysning.PersonopplysningerAggregat;
 import no.nav.k9.sak.behandlingslager.behandling.repository.BehandlingRepository;
 import no.nav.k9.sak.behandlingslager.behandling.vilkår.VilkårResultatRepository;
 import no.nav.k9.sak.behandlingslager.behandling.vilkår.Vilkårene;
 import no.nav.k9.sak.domene.person.personopplysning.PersonopplysningTjeneste;
 import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
-import no.nav.k9.sak.domene.uttak.repo.UttakAktivitet;
-import no.nav.k9.sak.domene.uttak.repo.UttakAktivitetPeriode;
-import no.nav.k9.sak.domene.uttak.repo.UttakRepository;
 import no.nav.k9.sak.domene.uttak.repo.pleiebehov.PleiebehovResultat;
 import no.nav.k9.sak.domene.uttak.repo.pleiebehov.PleiebehovResultatRepository;
+import no.nav.k9.sak.perioder.KravDokument;
+import no.nav.k9.sak.perioder.VurderSøknadsfristTjeneste;
+import no.nav.k9.sak.perioder.VurdertSøktPeriode;
+import no.nav.k9.sak.typer.AktørId;
 import no.nav.k9.sak.typer.Arbeidsgiver;
 import no.nav.k9.sak.typer.InternArbeidsforholdRef;
+import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.søknadsperiode.Søknadsperiode;
+import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.uttak.ArbeidPeriode;
+import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.uttak.PerioderFraSøknad;
+import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.uttak.Tilsynsordning;
+import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.uttak.UttakPerioderGrunnlagRepository;
+import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.uttak.UttaksPerioderGrunnlag;
 import no.nav.pleiepengerbarn.uttak.kontrakter.Arbeid;
 import no.nav.pleiepengerbarn.uttak.kontrakter.Arbeidsforhold;
 import no.nav.pleiepengerbarn.uttak.kontrakter.ArbeidsforholdPeriodeInfo;
@@ -49,77 +57,157 @@ public class MapInputTilUttakTjeneste {
 
     private VilkårResultatRepository vilkårResultatRepository;
     private PleiebehovResultatRepository pleiebehovResultatRepository;
-    private UttakRepository uttakRepository;
+    private UttakPerioderGrunnlagRepository uttakPerioderGrunnlagRepository;
     private PersonopplysningTjeneste personopplysningTjeneste;
     private BehandlingRepository behandlingRepository;
-
+    private VurderSøknadsfristTjeneste<Søknadsperiode> søknadsfristTjeneste;
 
     @Inject
     public MapInputTilUttakTjeneste(VilkårResultatRepository vilkårResultatRepository,
                                     PleiebehovResultatRepository pleiebehovResultatRepository,
-                                    UttakRepository uttakRepository,
+                                    UttakPerioderGrunnlagRepository uttakPerioderGrunnlagRepository,
                                     PersonopplysningTjeneste personopplysningTjeneste,
-                                    BehandlingRepository behandlingRepository) {
+                                    BehandlingRepository behandlingRepository,
+                                    @FagsakYtelseTypeRef("PSB") VurderSøknadsfristTjeneste<Søknadsperiode> søknadsfristTjeneste) {
         this.vilkårResultatRepository = vilkårResultatRepository;
         this.pleiebehovResultatRepository = pleiebehovResultatRepository;
-        this.uttakRepository = uttakRepository;
+        this.uttakPerioderGrunnlagRepository = uttakPerioderGrunnlagRepository;
         this.personopplysningTjeneste = personopplysningTjeneste;
         this.behandlingRepository = behandlingRepository;
+        this.søknadsfristTjeneste = søknadsfristTjeneste;
     }
 
     public Uttaksgrunnlag hentUtOgMapRequest(BehandlingReferanse referanse) {
         var behandling = behandlingRepository.hentBehandling(referanse.getBehandlingId());
         var vilkårene = vilkårResultatRepository.hent(referanse.getBehandlingId());
-        var uttakGrunnlag = uttakRepository.hentGrunnlag(referanse.getBehandlingId()).orElseThrow();
+        var uttakGrunnlag = uttakPerioderGrunnlagRepository.hentGrunnlag(referanse.getBehandlingId()).orElseThrow();
         var personopplysningerAggregat = personopplysningTjeneste.hentPersonopplysninger(referanse, referanse.getFagsakPeriode().getFomDato());
-        var oppgittUttak = uttakGrunnlag.getOppgittUttak();
         var pleiebehov = pleiebehovResultatRepository.hent(referanse.getBehandlingId());
+        var vurderteSøknadsperioder = søknadsfristTjeneste.vurderSøknadsfrist(referanse);
 
-        return toRequestData(behandling, personopplysningerAggregat, vilkårene, oppgittUttak, pleiebehov);
+        return toRequestData(behandling, personopplysningerAggregat, vurderteSøknadsperioder, vilkårene, uttakGrunnlag, pleiebehov);
     }
 
-    private Uttaksgrunnlag toRequestData(Behandling behandling, PersonopplysningerAggregat personopplysningerAggregat, Vilkårene vilkårene, UttakAktivitet oppgittUttak, PleiebehovResultat pleiebehov) {
+    private Uttaksgrunnlag toRequestData(Behandling behandling,
+                                         PersonopplysningerAggregat personopplysningerAggregat,
+                                         Map<KravDokument, List<VurdertSøktPeriode<Søknadsperiode>>> vurderteSøknadsperioder,
+                                         Vilkårene vilkårene,
+                                         UttaksPerioderGrunnlag uttaksPerioderGrunnlag,
+                                         PleiebehovResultat pleiebehov) {
 
+        var perioderFraSøknader = uttaksPerioderGrunnlag.getRelevantSøknadsperioder().getUttakPerioder();
+        var kravDokumenter = vurderteSøknadsperioder.keySet()
+            .stream()
+            .filter(it -> perioderFraSøknader.stream().anyMatch(at -> at.getJournalpostId().equals(it.getJournalpostId())))
+            .collect(Collectors.toCollection(TreeSet::new));
         var søkerPersonopplysninger = personopplysningerAggregat.getSøker();
         var pleietrengendePersonopplysninger = personopplysningerAggregat.getPersonopplysning(behandling.getFagsak().getPleietrengendeAktørId());
-        final Barn barn = new Barn(pleietrengendePersonopplysninger.getAktørId().getId(), pleietrengendePersonopplysninger.getDødsdato());
 
-        final Søker søker = new Søker(søkerPersonopplysninger.getAktørId().getId(), søkerPersonopplysninger.getFødselsdato(), søkerPersonopplysninger.getDødsdato());
+        var barn = new Barn(pleietrengendePersonopplysninger.getAktørId().getId(), pleietrengendePersonopplysninger.getDødsdato());
+        var søker = new Søker(søkerPersonopplysninger.getAktørId().getId(), søkerPersonopplysninger.getFødselsdato(), søkerPersonopplysninger.getDødsdato());
 
         // TODO: Map:
         final List<String> andrePartersSaksnummer = List.of();
 
-        // TODO: Sett faktisk søkt uttak:
-        final List<SøktUttak> søktUttak = oppgittUttak.getPerioder()
-                .stream()
-                .map(uap -> new SøktUttak(toLukketPeriode(uap.getPeriode()), null))
-                .collect(Collectors.toList());
+        final List<SøktUttak> søktUttak = mapUttakPerioder(kravDokumenter, perioderFraSøknader);
 
         // TODO: Se kommentarer/TODOs under denne:
-        final List<Arbeid> arbeid = toArbeid(oppgittUttak);
+        final List<Arbeid> arbeid = mapArbeid(kravDokumenter, perioderFraSøknader);
 
         final Map<LukketPeriode, Pleiebehov> tilsynsbehov = toTilsynsbehov(pleiebehov);
 
-        // TODO: Map:
-        final List<LukketPeriode> lovbestemtFerie = List.of();
+        final List<LukketPeriode> lovbestemtFerie = mapFerie(kravDokumenter, perioderFraSøknader);
 
         final HashMap<String, List<Vilkårsperiode>> inngangsvilkår = toInngangsvilkår(vilkårene);
-
-        // TODO: Map:
-        final Map<LukketPeriode, Duration> tilsynsperioder = Map.of();
+        
+        final Map<LukketPeriode, Duration> tilsynsperioder = mapTilsynsperioder(kravDokumenter, perioderFraSøknader);
 
         return new Uttaksgrunnlag(
-                barn,
-                søker,
-                behandling.getFagsak().getSaksnummer().getVerdi(),
-                behandling.getUuid().toString(),
-                andrePartersSaksnummer,
-                søktUttak,
-                arbeid,
-                tilsynsbehov,
-                lovbestemtFerie,
-                inngangsvilkår,
-                tilsynsperioder);
+            barn,
+            søker,
+            behandling.getFagsak().getSaksnummer().getVerdi(),
+            behandling.getUuid().toString(),
+            andrePartersSaksnummer,
+            søktUttak,
+            arbeid,
+            tilsynsbehov,
+            lovbestemtFerie,
+            inngangsvilkår,
+            tilsynsperioder);
+    }
+
+    private Map<LukketPeriode, Duration> mapTilsynsperioder(TreeSet<KravDokument> kravDokumenter, Set<PerioderFraSøknad> perioderFraSøknader) {
+        Map<LukketPeriode, Duration> result = new HashMap<>();
+
+        kravDokumenter.stream()
+            .sorted()
+            .forEachOrdered(at -> {
+                var dokumenter = perioderFraSøknader.stream()
+                    .filter(it -> it.getJournalpostId().equals(at.getJournalpostId()))
+                    .collect(Collectors.toSet());
+                if (dokumenter.size() == 1) {
+                    // TODO: toTimeline and to map
+                    dokumenter.stream()
+                        .map(PerioderFraSøknad::getTilsynsordning)
+                        .flatMap(Collection::stream)
+                        .map(Tilsynsordning::getPerioder)
+                        .flatMap(Collection::stream)
+                        .forEach(it -> result.put(toLukketPeriode(it.getPeriode()), it.getVarighet()));
+                } else {
+                    throw new IllegalStateException("Fant " + dokumenter.size() + " for dokumentet : " + at);
+                }
+            });
+
+        return result;
+    }
+
+    private List<LukketPeriode> mapFerie(Set<KravDokument> kravDokumenter, Set<PerioderFraSøknad> perioderFraSøknader) {
+        var result = new ArrayList<LukketPeriode>();
+
+        kravDokumenter.stream()
+            .sorted()
+            .forEachOrdered(at -> {
+                var dokumenter = perioderFraSøknader.stream()
+                    .filter(it -> it.getJournalpostId().equals(at.getJournalpostId()))
+                    .collect(Collectors.toSet());
+                if (dokumenter.size() == 1) {
+                    // TODO: toTimeline and to map
+                    result.addAll(dokumenter.stream()
+                        .map(PerioderFraSøknad::getFerie)
+                        .flatMap(Collection::stream)
+                        .map(it -> toLukketPeriode(it.getPeriode()))
+                        .collect(Collectors.toList()));
+                } else {
+                    throw new IllegalStateException("Fant " + dokumenter.size() + " for dokumentet : " + at);
+                }
+            });
+
+        return result;
+    }
+
+    private List<SøktUttak> mapUttakPerioder(Set<KravDokument> kravDokumenter,
+                                             Set<PerioderFraSøknad> perioderFraSøknader) {
+
+        var result = new ArrayList<SøktUttak>();
+
+        kravDokumenter.stream()
+            .sorted()
+            .forEachOrdered(at -> {
+                var dokumenter = perioderFraSøknader.stream()
+                    .filter(it -> it.getJournalpostId().equals(at.getJournalpostId()))
+                    .collect(Collectors.toSet());
+                if (dokumenter.size() == 1) {
+                    result.addAll(dokumenter.stream()
+                        .map(PerioderFraSøknad::getUttakPerioder)
+                        .flatMap(Collection::stream)
+                        .map(it -> new SøktUttak(toLukketPeriode(it.getPeriode()), it.getTimerPleieAvBarnetPerDag()))
+                        .collect(Collectors.toList()));
+                } else {
+                    throw new IllegalStateException("Fant " + dokumenter.size() + " for dokumentet : " + at);
+                }
+            });
+
+        return result;
     }
 
     private Map<LukketPeriode, Pleiebehov> toTilsynsbehov(PleiebehovResultat pleiebehov) {
@@ -136,49 +224,66 @@ public class MapInputTilUttakTjeneste {
 
     private Pleiebehov mapToPleiebehov(Pleiegrad grad) {
         switch (grad) {
-        case INGEN: return Pleiebehov.PROSENT_0;
-        case KONTINUERLIG_TILSYN: return Pleiebehov.PROSENT_100;
-        case UTVIDET_KONTINUERLIG_TILSYN: return Pleiebehov.PROSENT_200;
-        case INNLEGGELSE:  return Pleiebehov.PROSENT_200;
-        default: throw new IllegalStateException("Ukjent Pleiegrad: " + grad);
+            case INGEN:
+                return Pleiebehov.PROSENT_0;
+            case KONTINUERLIG_TILSYN:
+                return Pleiebehov.PROSENT_100;
+            case UTVIDET_KONTINUERLIG_TILSYN:
+            case INNLEGGELSE:
+                return Pleiebehov.PROSENT_200;
+            default:
+                throw new IllegalStateException("Ukjent Pleiegrad: " + grad);
         }
     }
 
-    private List<Arbeid> toArbeid(UttakAktivitet oppgittUttak) {
-        // TODO: Skal vi ha arbeidsforhold på topp?
-        final Map<ArbeidsgiverArbeidsforhold, List<UttakAktivitetPeriode>> arbeidsforhold = new HashMap<>();
-        oppgittUttak.getPerioder().forEach(p -> {
-            final ArbeidsgiverArbeidsforhold key = new ArbeidsgiverArbeidsforhold(p.getArbeidsgiver(), p.getArbeidsforholdRef());
-            List<UttakAktivitetPeriode> perioder = arbeidsforhold.get(key);
-            if (perioder == null) {
-                perioder = new ArrayList<>();
-            }
-            perioder.add(p);
-            arbeidsforhold.put(key, perioder);
-        });
+    private List<Arbeid> mapArbeid(Set<KravDokument> kravDokumenter,
+                                   Set<PerioderFraSøknad> perioderFraSøknader) {
+        final Map<ArbeidsgiverArbeidsforhold, List<ArbeidPeriode>> arbeidsforhold = new HashMap<>();
 
-        return arbeidsforhold.entrySet().stream().map(e -> {
-                var uttakAktivitetPeriode = e.getValue().get(0);
-                final Map<LukketPeriode, ArbeidsforholdPeriodeInfo> perioder = new HashMap<>();
-                e.getValue().forEach(p -> {
-                    final Duration jobberNormaltPerUke = Optional.ofNullable(p.getJobberNormaltPerUke()).orElse(Duration.ZERO);
-                    final BigDecimal jobberAndel = Optional.ofNullable(p.getSkalJobbeProsent()).orElse(new BigDecimal(0L));
-                    final Duration jobber = Duration.ofMillis(new BigDecimal(jobberNormaltPerUke.toMillis()).multiply(jobberAndel).longValue());
+        kravDokumenter.stream()
+            .sorted()
+            .forEachOrdered(at -> {
+                var dokumenter = perioderFraSøknader.stream()
+                    .filter(it -> it.getJournalpostId().equals(at.getJournalpostId()))
+                    .collect(Collectors.toSet());
+                if (dokumenter.size() == 1) {
+                    dokumenter.stream()
+                        .map(PerioderFraSøknad::getArbeidPerioder)
+                        .flatMap(Collection::stream)
+                        .forEach(p -> {
+                            var key = new ArbeidsgiverArbeidsforhold(p.getArbeidsgiver(), p.getArbeidsforholdRef());
+                            var perioder = arbeidsforhold.getOrDefault(key, new ArrayList<>());
+                            perioder.add(p);
+                            arbeidsforhold.put(key, perioder);
+                        });
+                } else {
+                    throw new IllegalStateException("Fant " + dokumenter.size() + " for dokumentet : " + at);
+                }
+            });
+
+        return arbeidsforhold.values()
+            .stream()
+            .map(arbeidPeriodes -> {
+                var uttakAktivitetPeriode = arbeidPeriodes.get(0);
+                var perioder = new HashMap<LukketPeriode, ArbeidsforholdPeriodeInfo>();
+                arbeidPeriodes.forEach(p -> {
+                    var jobberNormalt = Optional.ofNullable(p.getJobberNormaltTimerPerDag()).orElse(Duration.ZERO);
+                    var jobberFaktisk = Optional.ofNullable(p.getFaktiskArbeidTimerPerDag()).orElse(Duration.ZERO);
                     perioder.put(new LukketPeriode(p.getPeriode().getFomDato(), p.getPeriode().getTomDato()),
-                            new ArbeidsforholdPeriodeInfo(jobberNormaltPerUke, jobber));
+                        new ArbeidsforholdPeriodeInfo(jobberNormalt, jobberFaktisk));
                 });
 
-                return new Arbeid(
-                        new Arbeidsforhold(
-                                uttakAktivitetPeriode.getAktivitetType().getKode(),
-                                uttakAktivitetPeriode.getArbeidsgiver().getArbeidsgiverOrgnr(),
-                                Optional.ofNullable(uttakAktivitetPeriode.getArbeidsgiver().getArbeidsgiverAktørId()).map(a -> a.getId()).orElse(null),
-                                Optional.ofNullable(uttakAktivitetPeriode.getArbeidsforholdRef()).map(a -> a.getReferanse()).orElse(null)
-                        ),
-                        perioder
-                );
+                return new Arbeid(mapArbeidsforhold(uttakAktivitetPeriode), perioder);
             })
             .collect(Collectors.toList());
+    }
+
+    private Arbeidsforhold mapArbeidsforhold(ArbeidPeriode uttakAktivitetPeriode) {
+        return new Arbeidsforhold(uttakAktivitetPeriode.getAktivitetType().getKode(),
+            uttakAktivitetPeriode.getArbeidsgiver().getArbeidsgiverOrgnr(),
+            Optional.ofNullable(uttakAktivitetPeriode.getArbeidsgiver().getArbeidsgiverAktørId()).map(AktørId::getId).orElse(null),
+            Optional.ofNullable(uttakAktivitetPeriode.getArbeidsforholdRef()).map(InternArbeidsforholdRef::getReferanse).orElse(null)
+        );
     }
 
     private HashMap<String, List<Vilkårsperiode>> toInngangsvilkår(Vilkårene vilkårene) {
@@ -188,9 +293,9 @@ public class MapInputTilUttakTjeneste {
                 return;
             }
             final List<Vilkårsperiode> vilkårsperioder = v.getPerioder()
-                    .stream()
-                    .map(vp -> new Vilkårsperiode(new LukketPeriode(vp.getFom(), vp.getTom()), Utfall.valueOf(vp.getUtfall().getKode())))
-                    .collect(Collectors.toList());
+                .stream()
+                .map(vp -> new Vilkårsperiode(new LukketPeriode(vp.getFom(), vp.getTom()), Utfall.valueOf(vp.getUtfall().getKode())))
+                .collect(Collectors.toList());
             inngangsvilkår.put(v.getVilkårType().getKode(), vilkårsperioder);
         });
         return inngangsvilkår;
