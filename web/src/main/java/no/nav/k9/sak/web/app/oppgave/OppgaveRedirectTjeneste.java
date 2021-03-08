ca96b772215740276118252a34f3dd96da9a44e8
Frode Carlsen <frode.odde.carlsen@nav.no>
2021-03-08 08:58:30 +0100

    log oppgaveredirect (#3404)

--- web/src/main/java/no/nav/k9/sak/web/app/oppgave/OppgaveRedirectTjeneste.java
+++ web/src/main/java/no/nav/k9/sak/web/app/oppgave/OppgaveRedirectTjeneste.java
@@ -10,6 +10,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -27,19 +30,19 @@ import no.nav.vedtak.sikkerhet.abac.TilpassetAbacAttributt;
 @ApplicationScoped
 @OpenAPIDefinition(tags = {@Tag(name = "redirect", description = "Oppgave redirect")})
 public class OppgaveRedirectTjeneste {
+    private static final Logger log = LoggerFactory.getLogger(OppgaveRedirectTjeneste.class);
 
     private OppgaveBehandlingKoblingRepository oppgaveBehandlingKoblingRepository;
     private FagsakRepository fagsakRepository;
-    private RedirectFactory redirectFactory; //For å kunne endre til alternativ implementasjon på Jetty
+    private RedirectFactory redirectFactory = new RedirectFactory();
 
     public OppgaveRedirectTjeneste() {
     }
 
     @Inject
-    public OppgaveRedirectTjeneste(OppgaveBehandlingKoblingRepository oppgaveBehandlingKoblingRepository, FagsakRepository fagsakRepository, RedirectFactory redirectFactory) {
+    public OppgaveRedirectTjeneste(OppgaveBehandlingKoblingRepository oppgaveBehandlingKoblingRepository, FagsakRepository fagsakRepository) {
         this.oppgaveBehandlingKoblingRepository = oppgaveBehandlingKoblingRepository;
         this.fagsakRepository = fagsakRepository;
-        this.redirectFactory = redirectFactory;
     }
 
     @GET
@@ -50,6 +53,7 @@ public class OppgaveRedirectTjeneste {
         OppgaveRedirectData data = OppgaveRedirectData.hent(oppgaveBehandlingKoblingRepository, fagsakRepository, oppgaveId, saksnummerDto);
         String url = redirectFactory.lagRedirect(data);
         Response.ResponseBuilder responser = Response.temporaryRedirect(URI.create(url));
+        log.warn("Mottok oppgaveredirect for : {}", url);
         responser.encoding("UTF-8");
         return responser.build();
     }
