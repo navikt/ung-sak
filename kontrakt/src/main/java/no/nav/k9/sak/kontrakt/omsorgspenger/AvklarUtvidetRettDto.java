695e3160676d118eda4e8a9920cebb6b31835cc6
Frode Carlsen <frode.odde.carlsen@nav.no>
2021-03-09 09:45:49 +0100

    legger til avslagsårsak for utvidet rett

--- kontrakt/src/main/java/no/nav/k9/sak/kontrakt/omsorgspenger/AvklarUtvidetRettDto.java
+++ kontrakt/src/main/java/no/nav/k9/sak/kontrakt/omsorgspenger/AvklarUtvidetRettDto.java
@@ -1,6 +1,7 @@
 package no.nav.k9.sak.kontrakt.omsorgspenger;
 
 import javax.validation.Valid;
+import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
@@ -11,6 +12,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 
 import no.nav.k9.kodeverk.behandling.aksjonspunkt.AksjonspunktKodeDefinisjon;
+import no.nav.k9.kodeverk.vilkår.Avslagsårsak;
 import no.nav.k9.sak.kontrakt.aksjonspunkt.BekreftetAksjonspunktDto;
 import no.nav.k9.sak.typer.Periode;
 
@@ -28,16 +30,28 @@ public class AvklarUtvidetRettDto extends BekreftetAksjonspunktDto {
     @Valid
     private Periode periode;
 
+    /** Angitt avslagsårsak (dersom erVilkarOk==false) */
+    @JsonProperty(value = "avslagsårsak")
+    @Valid
+    private Avslagsårsak avslagsårsak;
+
     public AvklarUtvidetRettDto() {
         //
     }
 
     public AvklarUtvidetRettDto(String begrunnelse,
                                 Boolean erVilkarOk,
-                                Periode periode) {
+                                Periode periode,
+                                Avslagsårsak avslagsårsak) {
         super(begrunnelse);
         this.erVilkarOk = erVilkarOk;
         this.periode = periode;
+        this.avslagsårsak = avslagsårsak;
+    }
+
+    @AssertTrue(message = "Kan ikke angi avslagsårsak dersom vilkår er ok")
+    private boolean isOk() {
+        return !erVilkarOk || (erVilkarOk && avslagsårsak == null);
     }
 
     public Boolean getErVilkarOk() {
@@ -46,5 +60,9 @@ public class AvklarUtvidetRettDto extends BekreftetAksjonspunktDto {
 
     public Periode getPeriode() {
         return periode;
+    }
+
+    public Avslagsårsak getAvslagsårsak() {
+        return avslagsårsak;
     }
 }
