bae0b878b6895809bba0d2f3424e75d9aadb61fa
Frode Carlsen <frode.odde.carlsen@nav.no>
2021-03-29 14:36:56 +0200

    Feature/flere dumpere aksjonspunkt prosesstask (#3605)
    * feature: dumper aksjonspunkter og alle åpne prosesstasks
    
    * feature: dump omsorgspenger oppgitt fravær + søknad entitet m/annen part

---
+++ web/src/main/java/no/nav/k9/sak/web/app/tjenester/forvaltning/dump/omsorgspenger/OppgittFraværDump.java
@@ -0,0 +1,72 @@
+package no.nav.k9.sak.web.app.tjenester.forvaltning.dump.omsorgspenger;
+
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.Tuple;
+
+import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
+import no.nav.k9.sak.behandlingslager.fagsak.Fagsak;
+import no.nav.k9.sak.web.app.tjenester.forvaltning.dump.DebugDumpFagsak;
+import no.nav.k9.sak.web.app.tjenester.forvaltning.dump.DebugDumpsters;
+import no.nav.k9.sak.web.app.tjenester.forvaltning.dump.DumpOutput;
+
+@ApplicationScoped
+@FagsakYtelseTypeRef("OMP")
+public class OppgittFraværDump implements DebugDumpFagsak {
+
+    private EntityManager entityManager;
+
+    OppgittFraværDump() {
+        // for proxys
+    }
+
+    @Inject
+    OppgittFraværDump(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public List<DumpOutput> dump(Fagsak fagsak) {
+        var sql = ""
+            + "select f.saksnummer"
+            + ", f.id as fagsak_id"
+            + ", gr.behandling_id"
+            + ", b.behandling_status"
+            + ", b.behandling_resultat_type"
+            + ", omp.fom"
+            + ", omp.tom"
+            + ", omp.fravaer_per_dag"
+            + ", omp.aktivitet_type"
+            + ", omp.arbeidsgiver_aktor_id"
+            + ", omp.arbeidsgiver_orgnr"
+            + ", omp.arbeidsforhold_intern_id"
+            + ", omp.journalpost_id"
+            + ", omp.fravaer_arsak"
+            + ", replace(cast(omp.opprettet_tid as varchar), ' ', 'T') opprettet_tid"
+            + " from "
+            + " gr_omp_aktivitet gr"
+            + " inner join behandling b on b.id=gr.behandling_id and gr.aktiv=true"
+            + " inner join fagsak f on f.id=b.fagsak_id"
+            + " inner join omp_oppgitt_fravaer_periode omp on omp.fravaer_id=gr.fravaer_id"
+            + " where f.saksnummer=:saksnummer"
+            + " order by 1, 2, 3, omp.opprettet_tid;";
+
+        var query = entityManager.createNativeQuery(sql, Tuple.class)
+            .setParameter("saksnummer", fagsak.getSaksnummer().getVerdi());
+        String path = "omsorgspenger_oppgitt_fravær.csv";
+
+        @SuppressWarnings("unchecked")
+        List<Tuple> results = query.getResultList();
+
+        if (results.isEmpty()) {
+            return List.of();
+        }
+
+        return DebugDumpsters.dumpResultSetToCsv(path, results)
+            .map(v -> List.of(v)).orElse(List.of());
+    }
+
+}
