67d50a50598af46bcdcbb4d9804b3f2940181e69
s149212 <espen.svendsen@nav.no>
2021-03-30 08:31:01 +0200

    Leger til mapping for flere mulige aktiveter i søknad per fraværsperiode
    Signed-off-by: s149212 <espen.svendsen@nav.no>

--- ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/mottak/SøknadOppgittFraværMapper.java
+++ ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/mottak/SøknadOppgittFraværMapper.java
@@ -5,9 +5,11 @@ import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import no.nav.k9.kodeverk.uttak.FraværÅrsak;
 import no.nav.k9.kodeverk.uttak.UttakArbeidType;
@@ -17,8 +19,10 @@ import no.nav.k9.sak.typer.Arbeidsgiver;
 import no.nav.k9.sak.typer.InternArbeidsforholdRef;
 import no.nav.k9.sak.typer.JournalpostId;
 import no.nav.k9.sak.ytelse.omsorgspenger.repo.OppgittFraværPeriode;
+import no.nav.k9.søknad.felles.fravær.AktivitetFravær;
 import no.nav.k9.søknad.felles.fravær.FraværPeriode;
 import no.nav.k9.søknad.felles.opptjening.Arbeidstaker;
+import no.nav.k9.søknad.felles.opptjening.Frilanser;
 import no.nav.k9.søknad.felles.opptjening.Organisasjonsnummer;
 import no.nav.k9.søknad.felles.opptjening.SelvstendigNæringsdrivende;
 import no.nav.k9.søknad.felles.personopplysninger.Søker;
@@ -53,28 +57,55 @@ class SøknadOppgittFraværMapper {
             LocalDate tom = fp.getPeriode().getTilOgMed();
             Duration varighet = fp.getDuration();
             FraværÅrsak fraværÅrsak = FraværÅrsak.fraKode(fp.getÅrsak().getKode());
+            List<AktivitetFravær> aktivitetFravær = Objects.requireNonNull(fp.getAktivitetFravær());
 
-            for (SelvstendigNæringsdrivende sn : snAktiviteter) {
-                Arbeidsgiver arbeidsgiver = byggArbeidsgiver(sn.getOrganisasjonsnummer(), søker.getPersonIdent());
-                InternArbeidsforholdRef arbeidsforholdRef = null; // får ikke fra søknad, setter default null her
-                OppgittFraværPeriode oppgittFraværPeriode = new OppgittFraværPeriode(journalpostId, fom, tom, UttakArbeidType.SELVSTENDIG_NÆRINGSDRIVENDE, arbeidsgiver, arbeidsforholdRef, varighet, fraværÅrsak);
-                oppgittFraværPerioder.add(oppgittFraværPeriode);
+            if (aktivitetFravær.contains(AktivitetFravær.SELVSTENDIG_VIRKSOMHET)) {
+                oppgittFraværPerioder.addAll(mapSn(snAktiviteter, fom, tom, varighet, fraværÅrsak));
             }
-            for (Arbeidstaker arbeidstaker : atAktiviteter) {
-                Arbeidsgiver arbeidsgiver = byggArbeidsgiver(arbeidstaker.getOrganisasjonsnummer(), søker.getPersonIdent());
-                InternArbeidsforholdRef arbeidsforholdRef = null; // får ikke fra søknad, setter default null her
-                OppgittFraværPeriode oppgittFraværPeriode = new OppgittFraværPeriode(journalpostId, fom, tom, UttakArbeidType.ARBEIDSTAKER, arbeidsgiver, arbeidsforholdRef, varighet, fraværÅrsak);
-                oppgittFraværPerioder.add(oppgittFraværPeriode);
+            if (aktivitetFravær.contains(AktivitetFravær.ARBEIDSTAKER)) {
+                oppgittFraværPerioder.addAll(mapAt(atAktiviteter, fom, tom, varighet, fraværÅrsak));
             }
-            if (frilanser != null) {
-                Arbeidsgiver arbeidsgiver = null;
-                InternArbeidsforholdRef arbeidsforholdRef = null;
-                OppgittFraværPeriode oppgittFraværPeriode = new OppgittFraværPeriode(journalpostId, fom, tom, UttakArbeidType.FRILANSER, arbeidsgiver, arbeidsforholdRef, varighet, fraværÅrsak);
-                oppgittFraværPerioder.add(oppgittFraværPeriode);
+            if (aktivitetFravær.contains(AktivitetFravær.FRILANSER)) {
+                oppgittFraværPerioder.addAll(mapFl(frilanser, fom, tom, varighet, fraværÅrsak));
             }
         }
 
         return oppgittFraværPerioder;
+    }
+
+    private Set<OppgittFraværPeriode> mapSn(List<SelvstendigNæringsdrivende> snAktiviteter, LocalDate fom, LocalDate tom, Duration varighet, FraværÅrsak fraværÅrsak) {
+        return snAktiviteter.stream()
+            .map(sn -> {
+                Arbeidsgiver arbeidsgiver = byggArbeidsgiver(sn.getOrganisasjonsnummer(), søker.getPersonIdent());
+                InternArbeidsforholdRef arbeidsforholdRef = null; // får ikke fra søknad, setter default null her
+                return new OppgittFraværPeriode(journalpostId, fom, tom, UttakArbeidType.SELVSTENDIG_NÆRINGSDRIVENDE,
+                    arbeidsgiver, arbeidsforholdRef, varighet, fraværÅrsak);
+            })
+            .collect(Collectors.toSet());
+    }
+
+    private Set<OppgittFraværPeriode> mapAt(List<Arbeidstaker> atAktiviteter, LocalDate fom, LocalDate tom, Duration varighet, FraværÅrsak fraværÅrsak) {
+        return atAktiviteter.stream()
+            .map(arbeidstaker -> {
+                Arbeidsgiver arbeidsgiver = byggArbeidsgiver(arbeidstaker.getOrganisasjonsnummer(), søker.getPersonIdent());
+                InternArbeidsforholdRef arbeidsforholdRef = null; // får ikke fra søknad, setter default null her
+                return new OppgittFraværPeriode(journalpostId, fom, tom, UttakArbeidType.ARBEIDSTAKER,
+                    arbeidsgiver, arbeidsforholdRef, varighet, fraværÅrsak);
+
+            })
+            .collect(Collectors.toSet());
+    }
+
+    private Set<OppgittFraværPeriode> mapFl(Frilanser frilanser, LocalDate fom, LocalDate tom, Duration varighet, FraværÅrsak fraværÅrsak) {
+        if (frilanser != null) {
+            Arbeidsgiver arbeidsgiver = null;
+            InternArbeidsforholdRef arbeidsforholdRef = null;
+            OppgittFraværPeriode oppgittFraværPeriode = new OppgittFraværPeriode(journalpostId, fom, tom, UttakArbeidType.FRILANSER,
+                arbeidsgiver, arbeidsforholdRef, varighet, fraværÅrsak);
+
+            return Set.of(oppgittFraværPeriode);
+        }
+        return Set.of();
     }
 
     private Arbeidsgiver byggArbeidsgiver(Organisasjonsnummer organisasjonsnummer, PersonIdent personIdent) {
