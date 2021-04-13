751d17fa65fb0908fb3440ba6d28a3da5a07d9e4
s149212 <espen.svendsen@nav.no>
2021-04-13 16:49:33 +0200

    Fiks feil mapping for aksjonspunktoppdaterer kode 5040
    Signed-off-by: s149212 <espen.svendsen@nav.no>

--- web/src/main/java/no/nav/k9/sak/web/app/tjenester/behandling/vedtak/aksjonspunkt/VurderOverlappendeYtelseFørVedtakOppdaterer.java
+++ web/src/main/java/no/nav/k9/sak/web/app/tjenester/behandling/vedtak/aksjonspunkt/VurderOverlappendeYtelseFørVedtakOppdaterer.java
@@ -6,15 +6,14 @@ import no.nav.k9.sak.behandling.aksjonspunkt.AksjonspunktOppdaterParameter;
 import no.nav.k9.sak.behandling.aksjonspunkt.AksjonspunktOppdaterer;
 import no.nav.k9.sak.behandling.aksjonspunkt.DtoTilServiceAdapter;
 import no.nav.k9.sak.behandling.aksjonspunkt.OppdateringResultat;
-import no.nav.k9.sak.kontrakt.vedtak.VurdereAnnenYteleseFørVedtakDto;
 import no.nav.k9.sak.kontrakt.vedtak.VurdereOverlappendeYteleseFørVedtakDto;
 
 @ApplicationScoped
 @DtoTilServiceAdapter(dto = VurdereOverlappendeYteleseFørVedtakDto.class, adapter=AksjonspunktOppdaterer.class)
-class VurderOverlappendeYtelseFørVedtakOppdaterer implements AksjonspunktOppdaterer<VurdereAnnenYteleseFørVedtakDto> {
+class VurderOverlappendeYtelseFørVedtakOppdaterer implements AksjonspunktOppdaterer<VurdereOverlappendeYteleseFørVedtakDto> {
 
     @Override
-    public OppdateringResultat oppdater(VurdereAnnenYteleseFørVedtakDto dto, AksjonspunktOppdaterParameter param) {
+    public OppdateringResultat oppdater(VurdereOverlappendeYteleseFørVedtakDto dto, AksjonspunktOppdaterParameter param) {
         return OppdateringResultat.utenOveropp();
     }
 }
