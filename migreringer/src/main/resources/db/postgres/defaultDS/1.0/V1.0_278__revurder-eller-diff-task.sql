99a470fc9886d77540939fc6f6737c31f609fe25
Marius Bækken Glittum <marius.glittum@nav.no>
2021-04-20 11:20:35 +0200

    Implementerer opprett revurdering eller diff task (#3790)
    * Implementerer opprett revurdering eller diff task
    
    * Legger på exceptionshåndtering

---
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_278__revurder-eller-diff-task.sql
@@ -0,0 +1,1 @@
+Insert into PROSESS_TASK_TYPE (KODE,NAVN,FEIL_MAKS_FORSOEK,FEIL_SEK_MELLOM_FORSOEK,FEILHANDTERING_ALGORITME,BESKRIVELSE,CRON_EXPRESSION) values ('behandlingskontroll.opprettRevurderingEllerDiff','Revurderer eller differ åpen behandling',1,60,'DEFAULT','Oppretter revurdering eller kjører diff på behandlingsgrunnlaget',null);
