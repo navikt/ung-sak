76a0a4cf4ec18065d566e579cccf1e9acfde437c
Marius Bækken Glittum <marius.glittum@nav.no>
2021-04-23 09:16:45 +0200

    Feature/differ sykdomsgrunnlaget ved registerinnhenting (#3814)
    * Legger inn differ av sykdomsgrunnlaget for å sjekke om andre saker har gjort endringer på grunnlaget og dermed må saken tilbake
    * Utleder startpunkt basert på endring
    * Setter java-version til 16 slik at jenv får med seg oppdateringen
    * Legger på manglende annotasjon

---
+++ ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/registerdata/PSBDiffUtvidetBehandlingsgrunnlagTjeneste.java
@@ -0,0 +1,69 @@
+package no.nav.k9.sak.ytelse.pleiepengerbarn.registerdata;
+
+import java.util.List;
+import java.util.UUID;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import no.nav.k9.kodeverk.vilkår.VilkårType;
+import no.nav.k9.sak.behandling.BehandlingReferanse;
+import no.nav.k9.sak.behandlingskontroll.BehandlingTypeRef;
+import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
+import no.nav.k9.sak.behandlingslager.behandling.EndringsresultatDiff;
+import no.nav.k9.sak.behandlingslager.behandling.EndringsresultatSnapshot;
+import no.nav.k9.sak.behandlingslager.diff.DiffResult;
+import no.nav.k9.sak.domene.registerinnhenting.DiffUtvidetBehandlingsgrunnlagTjeneste;
+import no.nav.k9.sak.perioder.VilkårsPerioderTilVurderingTjeneste;
+import no.nav.k9.sak.typer.Periode;
+import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.sykdom.SykdomGrunnlag;
+import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.sykdom.SykdomGrunnlagRepository;
+import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.sykdom.SykdomUtils;
+import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.sykdom.SykdomVurderingService;
+
+@FagsakYtelseTypeRef("PSB")
+@ApplicationScoped
+public class PSBDiffUtvidetBehandlingsgrunnlagTjeneste implements DiffUtvidetBehandlingsgrunnlagTjeneste {
+
+    private SykdomGrunnlagRepository sykdomGrunnlagRepository;
+    private SykdomVurderingService sykdomVurderingService;
+    private VilkårsPerioderTilVurderingTjeneste perioderTilVurderingTjeneste;
+
+    PSBDiffUtvidetBehandlingsgrunnlagTjeneste() {
+        // CDI
+    }
+
+    @Inject
+    public PSBDiffUtvidetBehandlingsgrunnlagTjeneste(SykdomGrunnlagRepository sykdomGrunnlagRepository,
+                                                     SykdomVurderingService sykdomVurderingService,
+                                                     @FagsakYtelseTypeRef("PSB") @BehandlingTypeRef VilkårsPerioderTilVurderingTjeneste perioderTilVurderingTjeneste) {
+        this.sykdomGrunnlagRepository = sykdomGrunnlagRepository;
+        this.sykdomVurderingService = sykdomVurderingService;
+        this.perioderTilVurderingTjeneste = perioderTilVurderingTjeneste;
+    }
+
+    @Override
+    public void leggTilSnapshot(BehandlingReferanse ref, EndringsresultatSnapshot snapshot) {
+        snapshot.leggTil(sykdomGrunnlagRepository.finnAktivGrunnlagId(ref.getBehandlingUuid()));
+    }
+
+    @Override
+    public void leggTilDiffResultat(BehandlingReferanse ref, EndringsresultatDiff sporedeEndringerDiff) {
+        sporedeEndringerDiff.hentDelresultat(SykdomGrunnlag.class)
+            .ifPresent(idEndring -> sporedeEndringerDiff.leggTilSporetEndring(idEndring, () -> diffSykdom(ref, idEndring)));
+    }
+
+    private DiffResult diffSykdom(BehandlingReferanse ref, EndringsresultatDiff idEndring) {
+        var sykdomGrunnlag = sykdomGrunnlagRepository.hentGrunnlagForId((UUID) idEndring.getGrunnlagId1());
+        if (sykdomGrunnlag.isEmpty()) {
+            return SykdomDiffResult.ingenDiff();
+        }
+
+        var perioder = perioderTilVurderingTjeneste.utled(ref.getBehandlingId(), VilkårType.BEREGNINGSGRUNNLAGVILKÅR);
+        List<Periode> nyeVurderingsperioder = SykdomUtils.toPeriodeList(perioder);
+        var utledGrunnlag = sykdomGrunnlagRepository.utledGrunnlag(ref.getSaksnummer(), ref.getBehandlingUuid(), ref.getPleietrengendeAktørId(), nyeVurderingsperioder);
+        var sykdomGrunnlagSammenlikningsresultat = sykdomVurderingService.sammenlignGrunnlag(sykdomGrunnlag, utledGrunnlag);
+
+        return new SykdomDiffResult(sykdomGrunnlagSammenlikningsresultat);
+    }
+}
