4f76c0fd98ec492dbc4f541c5fd9584575d6110e
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2021-04-26 16:27:58 +0200

    Flere tester på fjerning av helg

--- ytelse-omsorgspenger/src/test/java/no/nav/k9/sak/ytelse/omsorgspenger/mottak/FraværPeriodeSammenslåerTest.java
+++ ytelse-omsorgspenger/src/test/java/no/nav/k9/sak/ytelse/omsorgspenger/mottak/FraværPeriodeSammenslåerTest.java
@@ -1,29 +1,27 @@
 package no.nav.k9.sak.ytelse.omsorgspenger.mottak;
 
-import no.nav.k9.søknad.felles.fravær.AktivitetFravær;
-import no.nav.k9.søknad.felles.fravær.FraværPeriode;
-import no.nav.k9.søknad.felles.fravær.FraværÅrsak;
-import no.nav.k9.søknad.felles.type.Periode;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import static no.nav.k9.sak.ytelse.omsorgspenger.mottak.FraværPeriodeSammenslåer.fjernHelgdager;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
-import static no.nav.k9.sak.ytelse.omsorgspenger.mottak.FraværPeriodeSammenslåer.fjernHelgdager;
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+import no.nav.k9.søknad.felles.fravær.AktivitetFravær;
+import no.nav.k9.søknad.felles.fravær.FraværPeriode;
+import no.nav.k9.søknad.felles.fravær.FraværÅrsak;
+import no.nav.k9.søknad.felles.type.Periode;
 
 class FraværPeriodeSammenslåerTest {
-    LocalDate måndag = LocalDate.of(2021, 04, 26);
+    LocalDate mandag = LocalDate.of(2021, 4, 26);
 
-    Periode mån = new Periode(måndag, måndag);
-    Periode tis = new Periode(måndag.plusDays(1), måndag.plusDays(1));
-    Periode ons = new Periode(måndag.plusDays(2), måndag.plusDays(2));
-    Periode lør = new Periode(måndag.plusDays(5), måndag.plusDays(5));
-
+    Periode mån = new Periode(mandag, mandag);
+    Periode tis = new Periode(mandag.plusDays(1), mandag.plusDays(1));
+    Periode ons = new Periode(mandag.plusDays(2), mandag.plusDays(2));
+    Periode lør = new Periode(mandag.plusDays(5), mandag.plusDays(5));
 
     @Test
     void skal_slå_sammen_perioder_som_ligger_inntil_hverandre_og_har_samme_innhold() {
@@ -34,7 +32,7 @@ class FraværPeriodeSammenslåerTest {
         List<FraværPeriode> resultat = FraværPeriodeSammenslåer.slåSammen(Arrays.asList(fp1, fp2, fp3));
         assertThat(resultat).hasSize(1);
         FraværPeriode sammenslåttPeriode = resultat.get(0);
-        assertThat(sammenslåttPeriode.getPeriode()).isEqualTo(new Periode(måndag, måndag.plusDays(2)));
+        assertThat(sammenslåttPeriode.getPeriode()).isEqualTo(new Periode(mandag, mandag.plusDays(2)));
         assertThat(sammenslåttPeriode.getDuration()).isEqualTo(Duration.ofHours(4));
         assertThat(sammenslåttPeriode.getÅrsak()).isEqualTo(FraværÅrsak.ORDINÆRT_FRAVÆR);
         assertThat(sammenslåttPeriode.getAktivitetFravær()).containsOnly(AktivitetFravær.FRILANSER, AktivitetFravær.SELVSTENDIG_VIRKSOMHET);
@@ -88,23 +86,21 @@ class FraværPeriodeSammenslåerTest {
 
     @Test
     void fjerner_helger_fra_perioder() {
-        LocalDate fom = måndag;
-        LocalDate tom = måndag.plusDays(8);
-        var resultat = fjernHelgdager(new Periode(fom, tom));
+        LocalDate fredag = mandag.plusDays(4);
+        LocalDate lørdag = mandag.plusDays(5);
+        LocalDate måndagUke2 = mandag.plusWeeks(1);
+        LocalDate tirsdagUke2 = mandag.plusWeeks(1).plusDays(1);
+        LocalDate fredagUke2 = mandag.plusWeeks(1).plusDays(4);
+        LocalDate måndagUke3 = mandag.plusWeeks(2);
+        LocalDate tirsdagUke3 = mandag.plusWeeks(2).plusDays(1);
 
-        Assertions.assertTrue(resultat.size() == 2);
-
-        Boolean inneholderHelgdag = false;
-        for(Periode periode : resultat) {
-            var dato = periode.getFraOgMed();
-            while(dato.isBefore(periode.getTilOgMed().plusDays(1L))) {
-                if(dato.getDayOfWeek().equals(DayOfWeek.SATURDAY) || fom.getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
-                    inneholderHelgdag = true;
-                }
-                dato = dato.plusDays(1L);
-            }
-        }
-
-        Assertions.assertFalse(inneholderHelgdag);
+        assertThat(fjernHelgdager(new Periode(lørdag, lørdag))).isEmpty();
+        assertThat(fjernHelgdager(new Periode(mandag, mandag))).containsOnly(new Periode(mandag, mandag));
+        assertThat(fjernHelgdager(new Periode(mandag, fredag))).containsOnly(new Periode(mandag, fredag));
+        assertThat(fjernHelgdager(new Periode(mandag, lørdag))).containsOnly(new Periode(mandag, fredag));
+        assertThat(fjernHelgdager(new Periode(lørdag, måndagUke2))).containsOnly(new Periode(måndagUke2, måndagUke2));
+        assertThat(fjernHelgdager(new Periode(fredag, måndagUke2))).containsOnly(new Periode(fredag, fredag), new Periode(måndagUke2, måndagUke2));
+        assertThat(fjernHelgdager(new Periode(mandag, tirsdagUke2))).containsOnly(new Periode(mandag, fredag), new Periode(måndagUke2, tirsdagUke2));
+        assertThat(fjernHelgdager(new Periode(mandag, tirsdagUke3))).containsOnly(new Periode(mandag, fredag), new Periode(måndagUke2, fredagUke2), new Periode(måndagUke3, tirsdagUke3));
     }
 }
