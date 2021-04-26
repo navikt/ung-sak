af5fa7093aea3216e1be5e2954c630d6eeb407dc
Frode Carlsen <frode.odde.carlsen@nav.no>
2021-04-26 14:50:43 +0200

    fix: flere sjekker der det forventes kun 1 (eller ingen) personidenter som svar, men aldri mer enn 1

--- domenetjenester/person/src/main/java/no/nav/k9/sak/domene/person/pdl/AktørTjeneste.java
+++ domenetjenester/person/src/main/java/no/nav/k9/sak/domene/person/pdl/AktørTjeneste.java
@@ -1,6 +1,5 @@
 package no.nav.k9.sak.domene.person.pdl;
 
-import static java.util.Collections.emptyList;
 import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toSet;
@@ -22,8 +21,7 @@ import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import no.nav.k9.sak.typer.AktørId;
-import no.nav.k9.sak.typer.PersonIdent;
+import no.nav.k9.felles.exception.VLException;
 import no.nav.k9.felles.integrasjon.pdl.HentIdenterBolkQueryRequest;
 import no.nav.k9.felles.integrasjon.pdl.HentIdenterBolkResultResponseProjection;
 import no.nav.k9.felles.integrasjon.pdl.HentIdenterQueryRequest;
@@ -31,16 +29,17 @@ import no.nav.k9.felles.integrasjon.pdl.IdentGruppe;
 import no.nav.k9.felles.integrasjon.pdl.IdentInformasjon;
 import no.nav.k9.felles.integrasjon.pdl.IdentInformasjonResponseProjection;
 import no.nav.k9.felles.integrasjon.pdl.IdentlisteResponseProjection;
-import no.nav.k9.felles.exception.VLException;
 import no.nav.k9.felles.integrasjon.pdl.PdlKlient;
 import no.nav.k9.felles.util.LRUCache;
 import no.nav.k9.felles.util.Tuple;
+import no.nav.k9.sak.typer.AktørId;
+import no.nav.k9.sak.typer.PersonIdent;
 
 @ApplicationScoped
 public class AktørTjeneste {
     private static final Logger log = LoggerFactory.getLogger(AktørTjeneste.class);
     private static final int DEFAULT_CACHE_SIZE = 1000;
-    private static final long DEFAULT_CACHE_TIMEOUT = TimeUnit.MILLISECONDS.convert(8, TimeUnit.HOURS);
+    private static final long DEFAULT_CACHE_TIMEOUT = TimeUnit.MILLISECONDS.convert(1, TimeUnit.HOURS);
 
     private LRUCache<AktørId, PersonIdent> cacheAktørIdTilIdent;
     private LRUCache<PersonIdent, AktørId> cacheIdentTilAktørId;
@@ -70,10 +69,9 @@ public class AktørTjeneste {
 
         Optional<AktørId> aktørId = hentAktørIdFraPDL(personIdent.getIdent());
         aktørId.ifPresent(aid -> {
-                // Kan ikke legge til i cache aktørId -> ident ettersom ident kan være ikke-current
-                cacheIdentTilAktørId.put(personIdent, aid);
-            }
-        );
+            // Kan ikke legge til i cache aktørId -> ident ettersom ident kan være ikke-current
+            cacheIdentTilAktørId.put(personIdent, aid);
+        });
         return aktørId;
     }
 
@@ -88,30 +86,24 @@ public class AktørTjeneste {
             i -> {
                 cacheAktørIdTilIdent.put(aktørId, i);
                 cacheIdentTilAktørId.put(i, aktørId); // OK her, men ikke over ettersom dette er gjeldende mapping
-            }
-            , () -> log.info("Uventet resultat fra PDL: Fant ikke person med oppgitt aktørId.")
-        );
+            }, () -> log.info("Uventet resultat fra PDL: Fant ikke person med oppgitt aktørId."));
         return personIdent;
     }
 
     public Set<AktørId> hentAktørIdForPersonIdentSet(Set<PersonIdent> personIdentSet) {
-        var personIdentIkkeICache =
-            personIdentSet.stream()
-                .filter(pid -> ofNullable(cacheIdentTilAktørId.get(pid)).isEmpty())
-                .collect(toList());
+        var personIdentIkkeICache = personIdentSet.stream()
+            .filter(pid -> ofNullable(cacheIdentTilAktørId.get(pid)).isEmpty())
+            .collect(toList());
 
         return concat(
             personIdentSet.stream()
                 .map(pid -> ofNullable(cacheIdentTilAktørId.get(pid)))
                 .flatMap(Optional::stream),
-            personIdentIkkeICache.isEmpty() ?
-                empty()
-                :
-                hentBolkMedAktørId(personIdentIkkeICache)
+            personIdentIkkeICache.isEmpty() ? empty()
+                : hentBolkMedAktørId(personIdentIkkeICache)
                     .peek(aktørInfo -> cacheIdentTilAktørId.put(aktørInfo.getElement1(), aktørInfo.getElement2()))
-                    .map(Tuple::getElement2)
-        )
-            .collect(toSet());
+                    .map(Tuple::getElement2))
+                        .collect(toSet());
     }
 
     private Stream<Tuple<PersonIdent, AktørId>> hentBolkMedAktørId(List<PersonIdent> personIdents) {
@@ -123,46 +115,51 @@ public class AktørTjeneste {
             .ident()
             .identer(new IdentInformasjonResponseProjection()
                 .ident()
-                .gruppe()
-            )
+                .gruppe())
             .code();
 
         Predicate<IdentInformasjon> erØnsketIdentgruppe = identInformasjon -> identInformasjon.getGruppe().equals(IdentGruppe.AKTORID);
 
-        //noinspection OptionalGetWithoutIsPresent
+        // noinspection OptionalGetWithoutIsPresent
         return pdlKlient.hentIdenterBolkResults(query, projection).stream()
             .filter(r -> r.getIdenter().stream().anyMatch(erØnsketIdentgruppe))
             .map(r -> new Tuple<>(new PersonIdent(r.getIdent()), new AktørId(r.getIdenter().stream().filter(erØnsketIdentgruppe).findAny().get().getIdent())));
     }
 
     private Optional<AktørId> hentAktørIdFraPDL(String personIdent) {
-        return identerFor(personIdent, AKTORID).stream().findFirst().map(IdentInformasjon::getIdent).map(AktørId::new);
+        return identerFor(personIdent, AKTORID).map(IdentInformasjon::getIdent).map(AktørId::new);
     }
 
     private Optional<PersonIdent> hentPersonIdentFraPDL(String aktørId) {
-        var personIdenter = identerFor(aktørId, IdentGruppe.FOLKEREGISTERIDENT);
-        return personIdenter.stream().findFirst().map(IdentInformasjon::getIdent).map(PersonIdent::new);
+        return identerFor(aktørId, IdentGruppe.FOLKEREGISTERIDENT).map(IdentInformasjon::getIdent).map(PersonIdent::new);
 
     }
 
-    private List<IdentInformasjon> identerFor(String ident, IdentGruppe identGruppe) {
+    private Optional<IdentInformasjon> identerFor(String ident, IdentGruppe identGruppe) {
         HentIdenterQueryRequest request = new HentIdenterQueryRequest();
         request.setIdent(ident);
-        request.setGrupper(of(identGruppe));
+        request.setGrupper(List.of(identGruppe));
         request.setHistorikk(Boolean.FALSE);
 
         IdentlisteResponseProjection projeksjon = new IdentlisteResponseProjection()
             .identer(
                 new IdentInformasjonResponseProjection()
-                    .ident()
-            );
+                    .ident());
 
         try {
-            return pdlKlient.hentIdenter(request, projeksjon).getIdenter();
+            // med en gruppe og uten historikk forventer vi kun en gyldig ident for angitt
+            var identer = pdlKlient.hentIdenter(request, projeksjon).getIdenter();
+            if (identer.size() > 1) {
+                throw new IllegalStateException("Fant fler enn 1 ident for angitt gruppe (regner ikke med historikk): " + identGruppe);
+            } else if (identer.isEmpty()) {
+                return Optional.empty();
+            }
+
+            return Optional.of(identer.get(0));
         } catch (VLException e) {
             if (PdlKlient.PDL_KLIENT_NOT_FOUND_KODE.equals(e.getKode())) {
                 log.info("Ident av type {} ikke funnet for ident {}", identGruppe, ident);
-                return emptyList();
+                return Optional.empty();
             }
             throw e;
         }
