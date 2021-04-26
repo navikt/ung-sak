f63ea1bf328c27e97c42440e2a47e5793733b0cb
albrektsson <emil.albrektsson@nav.no>
2021-04-26 14:30:27 +0200

    Tar bort helgdager fra perioder som slås sammen i FraværsPerioderSammenslåer

--- ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/mottak/FraværPeriodeSammenslåer.java
+++ ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/mottak/FraværPeriodeSammenslåer.java
@@ -1,27 +1,44 @@
 package no.nav.k9.sak.ytelse.omsorgspenger.mottak;
 
+import no.nav.k9.søknad.felles.fravær.FraværPeriode;
+import no.nav.k9.søknad.felles.type.Periode;
+
+import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import no.nav.k9.søknad.felles.fravær.FraværPeriode;
-import no.nav.k9.søknad.felles.type.Periode;
+import static java.time.DayOfWeek.SATURDAY;
+import static java.time.DayOfWeek.SUNDAY;
 
 public class FraværPeriodeSammenslåer {
     public static List<FraværPeriode> slåSammen(List<FraværPeriode> søknadsperioder) {
-        var sorterteSøknadsperioder = søknadsperioder.stream().sorted().collect(Collectors.toList());
+
+        var fraværsPerioderUtenHelgdager = new ArrayList<FraværPeriode>();
+        for (FraværPeriode fp : søknadsperioder) {
+            var perioderMinusHelgdager = fjernHelgdager(fp.getPeriode());
+            for (Periode periode : perioderMinusHelgdager) {
+                fraværsPerioderUtenHelgdager.add(new FraværPeriode(periode, fp.getDuration(), fp.getÅrsak(), fp.getAktivitetFravær()));
+            }
+        }
+
+        var sorterteSøknadsperioderUtenHelgdager = fraværsPerioderUtenHelgdager.stream().sorted().collect(Collectors.toList());
+
         var sammenslåttePerioder = new LinkedList<FraværPeriode>();
 
-        for (FraværPeriode søknadPeriode : sorterteSøknadsperioder) {
+        for (FraværPeriode søknadPeriode : sorterteSøknadsperioderUtenHelgdager) {
             if (sammenslåttePerioder.isEmpty()) {
                 sammenslåttePerioder.add(søknadPeriode);
                 continue;
             }
             var sammenslåttPeriode = sammenslåttePerioder.getLast();
 
-            boolean periodenLiggerInntilForrigePeriode = ChronoUnit.DAYS.between(sammenslåttPeriode.getPeriode().getTilOgMed(), søknadPeriode.getPeriode().getFraOgMed()) == 1;
+            boolean periodenLiggerInntilForrigePeriode =
+                ChronoUnit.DAYS.between(sammenslåttPeriode.getPeriode().getTilOgMed(), søknadPeriode.getPeriode().getFraOgMed()) == 1;
+
             if (periodenLiggerInntilForrigePeriode &&
                 Optional.ofNullable(søknadPeriode.getDuration()).equals(Optional.ofNullable(sammenslåttPeriode.getDuration())) &&
                 søknadPeriode.getÅrsak().equals(sammenslåttPeriode.getÅrsak()) &&
@@ -42,5 +59,24 @@ public class FraværPeriodeSammenslåer {
         }
 
         return sammenslåttePerioder;
+    }
+
+    static List<Periode> fjernHelgdager(Periode periode) {
+        var resultat = new ArrayList<Periode>();
+        var fom = periode.getFraOgMed();
+        LocalDate startPåNyPeriode = null;
+        while (fom.isBefore(periode.getTilOgMed().plusDays(1))) {
+            if (startPåNyPeriode == null && !(fom.getDayOfWeek().equals(SATURDAY) || fom.getDayOfWeek().equals(SUNDAY))) {
+                startPåNyPeriode = fom;
+            }
+            if (startPåNyPeriode != null && fom.getDayOfWeek().equals(SATURDAY)) {
+                resultat.add(new Periode(startPåNyPeriode, fom.minusDays(1L)));
+                startPåNyPeriode = null;
+            } else if (startPåNyPeriode != null && fom.isEqual(periode.getTilOgMed())) {
+                resultat.add(new Periode(startPåNyPeriode, periode.getTilOgMed()));
+            }
+            fom = fom.plusDays(1L);
+        }
+        return resultat;
     }
 }
--- ytelse-omsorgspenger/src/test/java/no/nav/k9/sak/ytelse/omsorgspenger/mottak/FraværPeriodeSammenslåerTest.java
+++ ytelse-omsorgspenger/src/test/java/no/nav/k9/sak/ytelse/omsorgspenger/mottak/FraværPeriodeSammenslåerTest.java
@@ -1,36 +1,40 @@
 package no.nav.k9.sak.ytelse.omsorgspenger.mottak;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import no.nav.k9.søknad.felles.fravær.AktivitetFravær;
+import no.nav.k9.søknad.felles.fravær.FraværPeriode;
+import no.nav.k9.søknad.felles.fravær.FraværÅrsak;
+import no.nav.k9.søknad.felles.type.Periode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.jupiter.api.Test;
-
-import no.nav.k9.søknad.felles.fravær.AktivitetFravær;
-import no.nav.k9.søknad.felles.fravær.FraværPeriode;
-import no.nav.k9.søknad.felles.fravær.FraværÅrsak;
-import no.nav.k9.søknad.felles.type.Periode;
+import static no.nav.k9.sak.ytelse.omsorgspenger.mottak.FraværPeriodeSammenslåer.fjernHelgdager;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class FraværPeriodeSammenslåerTest {
-    LocalDate nå = LocalDate.now();
+    LocalDate måndag = LocalDate.of(2021, 04, 26);
 
-    Periode iDag = new Periode(nå, nå);
-    Periode iMorgen = new Periode(nå.plusDays(1), nå.plusDays(1));
-    Periode iOverimorgen = new Periode(nå.plusDays(2), nå.plusDays(2));
+    Periode mån = new Periode(måndag, måndag);
+    Periode tis = new Periode(måndag.plusDays(1), måndag.plusDays(1));
+    Periode ons = new Periode(måndag.plusDays(2), måndag.plusDays(2));
+    Periode lør = new Periode(måndag.plusDays(5), måndag.plusDays(5));
+
 
     @Test
     void skal_slå_sammen_perioder_som_ligger_inntil_hverandre_og_har_samme_innhold() {
-        FraværPeriode fp1 = new FraværPeriode(iDag, Duration.ofHours(4), FraværÅrsak.ORDINÆRT_FRAVÆR, List.of(AktivitetFravær.FRILANSER, AktivitetFravær.SELVSTENDIG_VIRKSOMHET));
-        FraværPeriode fp2 = new FraværPeriode(iMorgen, Duration.ofHours(4), FraværÅrsak.ORDINÆRT_FRAVÆR, List.of(AktivitetFravær.FRILANSER, AktivitetFravær.SELVSTENDIG_VIRKSOMHET));
-        FraværPeriode fp3 = new FraværPeriode(iOverimorgen, Duration.ofHours(4), FraværÅrsak.ORDINÆRT_FRAVÆR, List.of(AktivitetFravær.FRILANSER, AktivitetFravær.SELVSTENDIG_VIRKSOMHET));
+        FraværPeriode fp1 = new FraværPeriode(mån, Duration.ofHours(4), FraværÅrsak.ORDINÆRT_FRAVÆR, List.of(AktivitetFravær.FRILANSER, AktivitetFravær.SELVSTENDIG_VIRKSOMHET));
+        FraværPeriode fp2 = new FraværPeriode(tis, Duration.ofHours(4), FraværÅrsak.ORDINÆRT_FRAVÆR, List.of(AktivitetFravær.FRILANSER, AktivitetFravær.SELVSTENDIG_VIRKSOMHET));
+        FraværPeriode fp3 = new FraværPeriode(ons, Duration.ofHours(4), FraværÅrsak.ORDINÆRT_FRAVÆR, List.of(AktivitetFravær.FRILANSER, AktivitetFravær.SELVSTENDIG_VIRKSOMHET));
 
         List<FraværPeriode> resultat = FraværPeriodeSammenslåer.slåSammen(Arrays.asList(fp1, fp2, fp3));
         assertThat(resultat).hasSize(1);
         FraværPeriode sammenslåttPeriode = resultat.get(0);
-        assertThat(sammenslåttPeriode.getPeriode()).isEqualTo(new Periode(nå, nå.plusDays(2)));
+        assertThat(sammenslåttPeriode.getPeriode()).isEqualTo(new Periode(måndag, måndag.plusDays(2)));
         assertThat(sammenslåttPeriode.getDuration()).isEqualTo(Duration.ofHours(4));
         assertThat(sammenslåttPeriode.getÅrsak()).isEqualTo(FraværÅrsak.ORDINÆRT_FRAVÆR);
         assertThat(sammenslåttPeriode.getAktivitetFravær()).containsOnly(AktivitetFravær.FRILANSER, AktivitetFravær.SELVSTENDIG_VIRKSOMHET);
@@ -38,8 +42,8 @@ class FraværPeriodeSammenslåerTest {
 
     @Test
     void skal_ikke_slå_sammen_perioder_som_ikke_ligger_inntil_hverandre() {
-        FraværPeriode fp1 = new FraværPeriode(iDag, Duration.ofHours(4), FraværÅrsak.ORDINÆRT_FRAVÆR, List.of(AktivitetFravær.FRILANSER, AktivitetFravær.SELVSTENDIG_VIRKSOMHET));
-        FraværPeriode fp2 = new FraværPeriode(iOverimorgen, Duration.ofHours(4), FraværÅrsak.ORDINÆRT_FRAVÆR, List.of(AktivitetFravær.FRILANSER, AktivitetFravær.SELVSTENDIG_VIRKSOMHET));
+        FraværPeriode fp1 = new FraværPeriode(mån, Duration.ofHours(4), FraværÅrsak.ORDINÆRT_FRAVÆR, List.of(AktivitetFravær.FRILANSER, AktivitetFravær.SELVSTENDIG_VIRKSOMHET));
+        FraværPeriode fp2 = new FraværPeriode(ons, Duration.ofHours(4), FraværÅrsak.ORDINÆRT_FRAVÆR, List.of(AktivitetFravær.FRILANSER, AktivitetFravær.SELVSTENDIG_VIRKSOMHET));
 
         List<FraværPeriode> resultat = FraværPeriodeSammenslåer.slåSammen(Arrays.asList(fp1, fp2));
         assertThat(resultat).containsOnly(fp1, fp2);
@@ -47,8 +51,8 @@ class FraværPeriodeSammenslåerTest {
 
     @Test
     void skal_ikke_slå_sammen_perioder_som_har_ulik_duration() {
-        FraværPeriode fp1 = new FraværPeriode(iDag, Duration.ofHours(4), FraværÅrsak.ORDINÆRT_FRAVÆR, List.of(AktivitetFravær.FRILANSER, AktivitetFravær.SELVSTENDIG_VIRKSOMHET));
-        FraværPeriode fp2 = new FraværPeriode(iMorgen, Duration.ofHours(1), FraværÅrsak.ORDINÆRT_FRAVÆR, List.of(AktivitetFravær.FRILANSER, AktivitetFravær.SELVSTENDIG_VIRKSOMHET));
+        FraværPeriode fp1 = new FraværPeriode(mån, Duration.ofHours(4), FraværÅrsak.ORDINÆRT_FRAVÆR, List.of(AktivitetFravær.FRILANSER, AktivitetFravær.SELVSTENDIG_VIRKSOMHET));
+        FraværPeriode fp2 = new FraværPeriode(tis, Duration.ofHours(1), FraværÅrsak.ORDINÆRT_FRAVÆR, List.of(AktivitetFravær.FRILANSER, AktivitetFravær.SELVSTENDIG_VIRKSOMHET));
 
         List<FraværPeriode> resultat = FraværPeriodeSammenslåer.slåSammen(Arrays.asList(fp1, fp2));
         assertThat(resultat).containsOnly(fp1, fp2);
@@ -56,8 +60,8 @@ class FraværPeriodeSammenslåerTest {
 
     @Test
     void skal_ikke_slå_sammen_perioder_som_har_ulik_årsak() {
-        FraværPeriode fp1 = new FraværPeriode(iDag, Duration.ofHours(4), FraværÅrsak.SMITTEVERNHENSYN, List.of(AktivitetFravær.FRILANSER, AktivitetFravær.SELVSTENDIG_VIRKSOMHET));
-        FraværPeriode fp2 = new FraværPeriode(iMorgen, Duration.ofHours(4), FraværÅrsak.ORDINÆRT_FRAVÆR, List.of(AktivitetFravær.FRILANSER, AktivitetFravær.SELVSTENDIG_VIRKSOMHET));
+        FraværPeriode fp1 = new FraværPeriode(mån, Duration.ofHours(4), FraværÅrsak.SMITTEVERNHENSYN, List.of(AktivitetFravær.FRILANSER, AktivitetFravær.SELVSTENDIG_VIRKSOMHET));
+        FraværPeriode fp2 = new FraværPeriode(tis, Duration.ofHours(4), FraværÅrsak.ORDINÆRT_FRAVÆR, List.of(AktivitetFravær.FRILANSER, AktivitetFravær.SELVSTENDIG_VIRKSOMHET));
 
         List<FraværPeriode> resultat = FraværPeriodeSammenslåer.slåSammen(Arrays.asList(fp1, fp2));
         assertThat(resultat).containsOnly(fp1, fp2);
@@ -65,10 +69,42 @@ class FraværPeriodeSammenslåerTest {
 
     @Test
     void skal_ikke_slå_sammen_perioder_som_har_ulik_aktivitet() {
-        FraværPeriode fp1 = new FraværPeriode(iDag, Duration.ofHours(4), FraværÅrsak.SMITTEVERNHENSYN, List.of(AktivitetFravær.FRILANSER));
-        FraværPeriode fp2 = new FraværPeriode(iMorgen, Duration.ofHours(4), FraværÅrsak.ORDINÆRT_FRAVÆR, List.of(AktivitetFravær.FRILANSER, AktivitetFravær.SELVSTENDIG_VIRKSOMHET));
+        FraværPeriode fp1 = new FraværPeriode(mån, Duration.ofHours(4), FraværÅrsak.SMITTEVERNHENSYN, List.of(AktivitetFravær.FRILANSER));
+        FraværPeriode fp2 = new FraværPeriode(tis, Duration.ofHours(4), FraværÅrsak.ORDINÆRT_FRAVÆR, List.of(AktivitetFravær.FRILANSER, AktivitetFravær.SELVSTENDIG_VIRKSOMHET));
 
         List<FraværPeriode> resultat = FraværPeriodeSammenslåer.slåSammen(Arrays.asList(fp1, fp2));
         assertThat(resultat).containsOnly(fp1, fp2);
+    }
+
+    @Test
+    void skal_ikke_slå_sammen_helgdager_med_arbeidsdager() {
+        FraværPeriode fp1 = new FraværPeriode(mån, Duration.ofHours(4), FraværÅrsak.SMITTEVERNHENSYN, List.of(AktivitetFravær.FRILANSER));
+        FraværPeriode fp2 = new FraværPeriode(tis, Duration.ofHours(1), FraværÅrsak.ORDINÆRT_FRAVÆR, List.of(AktivitetFravær.FRILANSER, AktivitetFravær.SELVSTENDIG_VIRKSOMHET));
+        FraværPeriode fp3 = new FraværPeriode(lør, Duration.ofHours(4), FraværÅrsak.ORDINÆRT_FRAVÆR, List.of(AktivitetFravær.FRILANSER, AktivitetFravær.SELVSTENDIG_VIRKSOMHET));
+
+        List<FraværPeriode> resultat = FraværPeriodeSammenslåer.slåSammen(Arrays.asList(fp1, fp2, fp3));
+        assertThat(resultat).containsOnly(fp1, fp2);
+    }
+
+    @Test
+    void fjerner_helger_fra_perioder() {
+        LocalDate fom = måndag;
+        LocalDate tom = måndag.plusDays(8);
+        var resultat = fjernHelgdager(new Periode(fom, tom));
+
+        Assertions.assertTrue(resultat.size() == 2);
+
+        Boolean inneholderHelgdag = false;
+        for(Periode periode : resultat) {
+            var dato = periode.getFraOgMed();
+            while(dato.isBefore(periode.getTilOgMed().plusDays(1L))) {
+                if(dato.getDayOfWeek().equals(DayOfWeek.SATURDAY) || fom.getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
+                    inneholderHelgdag = true;
+                }
+                dato = dato.plusDays(1L);
+            }
+        }
+
+        Assertions.assertFalse(inneholderHelgdag);
     }
 }
