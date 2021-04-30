6bfbe249273c110de6fd4122bb0bb3f086b93e6c
Christoffer <45653861+christofferlohne@users.noreply.github.com>
2021-04-30 08:24:44 +0200

    oppdatert fil format versjon 5.1.3 og BarnRelasjon (#3867)
    * oppdatert fil format versjon 5.1.3 og BarnRelasjon
    
    * Lagt til OmsorgRelasjonTypeConverter og kopi av BarnRelasjon i kontrakter
    
    * BarnRelasjon kaster exception ved ukjent

---
+++ kontrakt/src/main/java/no/nav/k9/sak/kontrakt/omsorg/BarnRelasjon.java
@@ -0,0 +1,33 @@
+package no.nav.k9.sak.kontrakt.omsorg;
+
+public enum BarnRelasjon {
+    MOR("Mor"),
+    MEDMOR("Medmor"),
+    FAR("Far"),
+    FOSTERFORELDER("Fosterforelder"),
+    ANNET("Annet");
+
+    private final String rolle;
+
+    BarnRelasjon(String rolle) {
+        this.rolle = rolle;
+    }
+
+    public String getRolle() {
+        return this.rolle;
+    }
+
+    public static BarnRelasjon of(String rolle) {
+        if (rolle == null) {
+            return null;
+        }
+        for (BarnRelasjon relasjon : BarnRelasjon.values()) {
+            if (relasjon.getRolle().equals(rolle)) {
+                return relasjon;
+            }
+        }
+        throw new IllegalArgumentException("Ukjent rolle: " + rolle);
+
+    }
+
+}
