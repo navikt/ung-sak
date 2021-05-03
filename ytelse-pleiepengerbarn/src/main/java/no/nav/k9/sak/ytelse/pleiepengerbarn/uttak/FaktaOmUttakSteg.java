bd3a64f08b0b0765b8abcced790d583c7688841d
Frode Lindås <frodeli@gmail.com>
2021-05-03 09:43:48 +0200

    Implementasjon av kontroller fakta uttak med utleding av aksjonspunkt for nattevåk og beredskap.

--- ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/uttak/FaktaOmUttakSteg.java
+++ ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/uttak/FaktaOmUttakSteg.java
@@ -1,6 +1,7 @@
 package no.nav.k9.sak.ytelse.pleiepengerbarn.uttak;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
 import no.nav.k9.kodeverk.behandling.aksjonspunkt.AksjonspunktDefinisjon;
 import no.nav.k9.sak.behandlingskontroll.BehandleStegResultat;
@@ -9,6 +10,9 @@ import no.nav.k9.sak.behandlingskontroll.BehandlingStegRef;
 import no.nav.k9.sak.behandlingskontroll.BehandlingTypeRef;
 import no.nav.k9.sak.behandlingskontroll.BehandlingskontrollKontekst;
 import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
+import no.nav.k9.sak.kontrakt.sykdom.Resultat;
+import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.untaketablerttilsyn.UnntakEtablertTilsynGrunnlag;
+import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.untaketablerttilsyn.UnntakEtablertTilsynGrunnlagRepository;
 
 import java.util.ArrayList;
 
@@ -18,41 +22,52 @@ import java.util.ArrayList;
 @FagsakYtelseTypeRef("PSB")
 public class FaktaOmUttakSteg implements BehandlingSteg {
 
+    private UnntakEtablertTilsynGrunnlagRepository unntakEtablertTilsynGrunnlagRepository;
+
     protected FaktaOmUttakSteg() {
         // for proxy
+    }
+
+    @Inject
+    public FaktaOmUttakSteg(UnntakEtablertTilsynGrunnlagRepository unntakEtablertTilsynGrunnlagRepository) {
+        this.unntakEtablertTilsynGrunnlagRepository = unntakEtablertTilsynGrunnlagRepository;
     }
 
     @SuppressWarnings("unused")
     @Override
     public BehandleStegResultat utførSteg(BehandlingskontrollKontekst kontekst) {
         Long behandlingId = kontekst.getBehandlingId();
+        var unntakEtablerTilsynGrunnlag = unntakEtablertTilsynGrunnlagRepository.hent(behandlingId);
 
         var aksjonspunkter = new ArrayList<AksjonspunktDefinisjon>();
-        if (søktOmNattevåk()) {
-            aksjonspunkter.add(AksjonspunktDefinisjon.VURDER_NATTEVÅK);
-        }
-        if (søktOmBeredskap()) {
-            aksjonspunkter.add(AksjonspunktDefinisjon.VURDER_BEREDSKAP);
-        }
-        if (aksjonspunkter.isEmpty()) {
-            return BehandleStegResultat.utførtUtenAksjonspunkter();
+        if (unntakEtablerTilsynGrunnlag != null) {
+            if (søktOmNattevåk(unntakEtablerTilsynGrunnlag)) {
+                aksjonspunkter.add(AksjonspunktDefinisjon.VURDER_NATTEVÅK);
+            }
+            if (søktOmBeredskap(unntakEtablerTilsynGrunnlag)) {
+                aksjonspunkter.add(AksjonspunktDefinisjon.VURDER_BEREDSKAP);
+            }
+            if (aksjonspunkter.isEmpty()) {
+                return BehandleStegResultat.utførtUtenAksjonspunkter();
+            }
         }
         return BehandleStegResultat.utførtMedAksjonspunkter(aksjonspunkter);
-
-
     }
 
-    private boolean søktOmNattevåk() {
-        //TODO: implementert dette
+    private boolean søktOmNattevåk(UnntakEtablertTilsynGrunnlag unntakEtablerTilsynGrunnlag) {
+        var nattevåk = unntakEtablerTilsynGrunnlag.getUnntakEtablertTilsynForPleietrengende().getNattevåk();
+        if (nattevåk != null) {
+            return nattevåk.getPerioder().stream().anyMatch(periode -> periode.getResultat().equals(Resultat.IKKE_VURDERT));
+        }
         return false;
     }
 
-
-    private boolean søktOmBeredskap() {
-        //TODO: implementert dette
+    private boolean søktOmBeredskap(UnntakEtablertTilsynGrunnlag unntakEtablerTilsynGrunnlag) {
+        var beredskap = unntakEtablerTilsynGrunnlag.getUnntakEtablertTilsynForPleietrengende().getBeredskap();
+        if (beredskap != null) {
+            return beredskap.getPerioder().stream().anyMatch(periode -> periode.getResultat().equals(Resultat.IKKE_VURDERT));
+        }
         return false;
     }
-
-
 
 }
