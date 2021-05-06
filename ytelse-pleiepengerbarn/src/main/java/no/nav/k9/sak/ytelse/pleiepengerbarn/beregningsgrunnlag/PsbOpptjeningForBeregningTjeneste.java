6a370a550aca707b4f27a5e0510a02e4ec7fc038
kenkohn <kenneth.koh@nav.no>
2021-05-06 13:42:16 +0200

    Fjerner sjekk på om bruker har huket av for FL i søknaden mtp opptjeningsaktiveter per ytelse.
    Co-authored-by: Kjell Gustavsen <kjell.gustavsen@nav.no>

--- ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/beregningsgrunnlag/PsbOpptjeningForBeregningTjeneste.java
+++ ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/beregningsgrunnlag/PsbOpptjeningForBeregningTjeneste.java
@@ -75,7 +75,7 @@ public class PsbOpptjeningForBeregningTjeneste implements OpptjeningForBeregning
         return aktiviteter.stream()
             .filter(oa -> oa.getPeriode().getFomDato().isBefore(stp))
             .filter(oa -> !oa.getPeriode().getTomDato().isBefore(opptjening.getFom()))
-            .filter(oa -> opptjeningsaktiviteter.erRelevantAktivitet(oa.getOpptjeningAktivitetType(), oppgittOpptjening))
+            .filter(oa -> opptjeningsaktiviteter.erInkludert(oa.getOpptjeningAktivitetType()))
             .collect(Collectors.toList());
     }
 
