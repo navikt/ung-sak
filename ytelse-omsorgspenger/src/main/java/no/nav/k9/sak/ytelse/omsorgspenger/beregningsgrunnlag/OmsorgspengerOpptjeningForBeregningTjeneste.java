846a6ce9328ecbf3d563cf9e3cc9c1dcc340cb22
kenkohn <46488207+kenkohn@users.noreply.github.com>
2021-05-06 14:59:46 +0200

    Revert "Fjerner sjekk på om bruker har huket av for FL i søknaden mtp (#4002)" (#4007)
    This reverts commit e954a43f

--- ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/beregningsgrunnlag/OmsorgspengerOpptjeningForBeregningTjeneste.java
+++ ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/beregningsgrunnlag/OmsorgspengerOpptjeningForBeregningTjeneste.java
@@ -81,7 +81,7 @@ public class OmsorgspengerOpptjeningForBeregningTjeneste implements OpptjeningFo
         return aktiviteter.stream()
             .filter(oa -> oa.getPeriode().getFomDato().isBefore(stp))
             .filter(oa -> !oa.getPeriode().getTomDato().isBefore(opptjening.getFom()))
-            .filter(oa -> opptjeningsaktiviteter.erInkludert(oa.getOpptjeningAktivitetType()))
+            .filter(oa -> opptjeningsaktiviteter.erRelevantAktivitet(oa.getOpptjeningAktivitetType(), oppgittOpptjening))
             .collect(Collectors.toList());
     }
 
