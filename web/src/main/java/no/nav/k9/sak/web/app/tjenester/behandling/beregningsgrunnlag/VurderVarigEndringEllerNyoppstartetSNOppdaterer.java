32a8f7177678398d4c32cc000fa5eecf4335010f
Petter Ekern <petter.ekern@nav.no>
2021-05-10 09:51:09 +0200

    FIX: Legger til en guard i oppdaterer som ikke oppdaterer grunnlag utenfor perioder som skal vurderes

--- web/src/main/java/no/nav/k9/sak/web/app/tjenester/behandling/beregningsgrunnlag/VurderVarigEndringEllerNyoppstartetSNOppdaterer.java
+++ web/src/main/java/no/nav/k9/sak/web/app/tjenester/behandling/beregningsgrunnlag/VurderVarigEndringEllerNyoppstartetSNOppdaterer.java
@@ -3,6 +3,7 @@ package no.nav.k9.sak.web.app.tjenester.behandling.beregningsgrunnlag;
 
 import java.time.LocalDate;
 import java.util.Map;
+import java.util.NavigableSet;
 import java.util.stream.Collectors;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -14,6 +15,8 @@ import no.nav.k9.sak.behandling.aksjonspunkt.AksjonspunktOppdaterParameter;
 import no.nav.k9.sak.behandling.aksjonspunkt.AksjonspunktOppdaterer;
 import no.nav.k9.sak.behandling.aksjonspunkt.DtoTilServiceAdapter;
 import no.nav.k9.sak.behandling.aksjonspunkt.OppdateringResultat;
+import no.nav.k9.sak.domene.behandling.steg.beregningsgrunnlag.BeregningsgrunnlagVilkårTjeneste;
+import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
 import no.nav.k9.sak.kontrakt.beregningsgrunnlag.aksjonspunkt.VurderVarigEndringEllerNyoppstartetSNDtoer;
 
 @ApplicationScoped
@@ -21,20 +24,29 @@ import no.nav.k9.sak.kontrakt.beregningsgrunnlag.aksjonspunkt.VurderVarigEndring
 public class VurderVarigEndringEllerNyoppstartetSNOppdaterer implements AksjonspunktOppdaterer<VurderVarigEndringEllerNyoppstartetSNDtoer> {
 
     private BeregningTjeneste kalkulusTjeneste;
+    private BeregningsgrunnlagVilkårTjeneste vilkårTjeneste;
 
     VurderVarigEndringEllerNyoppstartetSNOppdaterer() {
         // CDI
     }
 
     @Inject
-    public VurderVarigEndringEllerNyoppstartetSNOppdaterer(BeregningTjeneste kalkulusTjeneste) {
+    public VurderVarigEndringEllerNyoppstartetSNOppdaterer(BeregningTjeneste kalkulusTjeneste,
+                                                           BeregningsgrunnlagVilkårTjeneste vilkårTjeneste) {
         this.kalkulusTjeneste = kalkulusTjeneste;
+        this.vilkårTjeneste = vilkårTjeneste;
     }
 
     @Override
     public OppdateringResultat oppdater(VurderVarigEndringEllerNyoppstartetSNDtoer dtoer, AksjonspunktOppdaterParameter param) {
         OppdateringResultat.Builder resultatBuilder = OppdateringResultat.utenTransisjon();
-
+        NavigableSet<DatoIntervallEntitet> perioderSomSkalKunneVurderes = vilkårTjeneste.utledPerioderTilVurdering(param.getRef(), false);
+        dtoer.getGrunnlag().forEach(dto -> {
+            if (!perioderSomSkalKunneVurderes.contains(DatoIntervallEntitet.fra(dto.getPeriode()))) {
+                throw new IllegalStateException("Prøver å endre periode " + dto.getPeriode() + " men denne er ikke i" +
+                    " listen over vilkårsperioder som er til vurdering " + perioderSomSkalKunneVurderes);
+            }
+        });
         Map<LocalDate, HåndterBeregningDto> stpTilDtoMap = dtoer.getGrunnlag().stream()
             .collect(Collectors.toMap(dto -> dto.getPeriode().getFom(), MapDtoTilRequest::map));
         kalkulusTjeneste.oppdaterBeregningListe(stpTilDtoMap, param.getRef());
