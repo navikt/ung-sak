3bff3cc163e80471fc2b36846e95bf57dfa23023
Marius Bækken Glittum <marius.glittum@nav.no>
2021-05-12 11:49:05 +0200

    Gjør det mulig å løse kompletthet for beregning (#4071)

---
+++ kontrakt/src/main/java/no/nav/k9/sak/kontrakt/kompletthet/aksjonspunkt/KompletthetsPeriode.java
@@ -0,0 +1,42 @@
+package no.nav.k9.sak.kontrakt.kompletthet.aksjonspunkt;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import no.nav.k9.sak.typer.Periode;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+@JsonAutoDetect(getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE, fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class KompletthetsPeriode {
+
+    @Valid
+    @NotNull
+    @JsonProperty("periode")
+    private Periode periode;
+
+    @NotNull
+    @JsonProperty("fortsett")
+    private Boolean kanFortsette;
+
+    @JsonCreator
+    public KompletthetsPeriode(@Valid @NotNull @JsonProperty(value = "periode", required = true) Periode periode,
+                               @NotNull @JsonProperty(value = "fortsett", required = true) Boolean kanFortsette) {
+        this.periode = periode;
+        this.kanFortsette = kanFortsette;
+    }
+
+    public Periode getPeriode() {
+        return periode;
+    }
+
+    public Boolean getKanFortsette() {
+        return kanFortsette;
+    }
+}
