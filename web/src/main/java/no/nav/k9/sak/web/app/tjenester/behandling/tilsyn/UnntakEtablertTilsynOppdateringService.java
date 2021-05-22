7e626a76b73bedb5813d902d224e93a8efe403af
Frode Lindås <frodeli@gmail.com>
2021-05-22 16:42:00 +0200

    Endret oppdatering av aksjonspunkt etter omskriving av nattevåk/beredskap grunnlag.

--- web/src/main/java/no/nav/k9/sak/web/app/tjenester/behandling/tilsyn/UnntakEtablertTilsynOppdateringService.java
+++ web/src/main/java/no/nav/k9/sak/web/app/tjenester/behandling/tilsyn/UnntakEtablertTilsynOppdateringService.java
@@ -2,10 +2,10 @@ package no.nav.k9.sak.web.app.tjenester.behandling.tilsyn;
 
 import no.nav.k9.sak.behandling.aksjonspunkt.OppdateringResultat;
 import no.nav.k9.sak.kontrakt.tilsyn.aksjonspunkt.Vurdering;
+import no.nav.k9.sak.kontrakt.tilsyn.aksjonspunkt.VurderingBeredskapDto;
+import no.nav.k9.sak.kontrakt.tilsyn.aksjonspunkt.VurderingNattevåkDto;
 import no.nav.k9.sak.typer.AktørId;
-import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.unntaketablerttilsyn.BeredskapOgNattevåkOversetter;
-import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.unntaketablerttilsyn.UnntakEtablertTilsynGrunnlagRepository;
-import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.unntaketablerttilsyn.Unntaksperiode;
+import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.unntaketablerttilsyn.*;
 
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
@@ -27,12 +27,12 @@ public class UnntakEtablertTilsynOppdateringService {
     }
 
     public OppdateringResultat oppdater(Vurdering dto, Long behandlingId, AktørId søkersAktørId) {
-        var unntakEtablertTilsynGrunnlag = unntakEtablertTilsynGrunnlagRepository.hent(behandlingId);
-
+        var eksisterendeGrunnlag = unntakEtablertTilsynGrunnlagRepository.hent(behandlingId);
+        var vurderingstype = finnVurderingstype(dto);
         var perioder = dto.getPerioder().stream().map(periode -> new Unntaksperiode(periode.getFom(), periode.getTom(), dto.getVurderingstekst())).toList();
 
-        var unntakEtablertTilsyn = BeredskapOgNattevåkOversetter.tilUnntakEtablertTilsynForPleietrengende(
-            unntakEtablertTilsynGrunnlag.getUnntakEtablertTilsynForPleietrengende().getBeredskap(),
+        var nyttUnntakEtablertTilsyn = BeredskapOgNattevåkOversetter.tilUnntakEtablertTilsynForPleietrengende(
+            finnUnntakEtablertTilsyn(vurderingstype, eksisterendeGrunnlag.getUnntakEtablertTilsynForPleietrengende()),
             LocalDate.now(),
             søkersAktørId,
             behandlingId,
@@ -40,10 +40,47 @@ public class UnntakEtablertTilsynOppdateringService {
             perioder,
             List.of());
 
-        unntakEtablertTilsynGrunnlag.getUnntakEtablertTilsynForPleietrengende().medBeredskap(unntakEtablertTilsyn);
-        unntakEtablertTilsynGrunnlagRepository.lagre(unntakEtablertTilsynGrunnlag);
+        eksisterendeGrunnlag.getUnntakEtablertTilsynForPleietrengende().medBeredskap(nyttUnntakEtablertTilsyn);
+
+        var nyttUnntakEtablertTilsynForPleietrengende = switch(vurderingstype) {
+            case BEREDSKAP ->
+                new UnntakEtablertTilsynForPleietrengende(
+                    eksisterendeGrunnlag.getUnntakEtablertTilsynForPleietrengende().getPleietrengendeAktørId(),
+                    nyttUnntakEtablertTilsyn,
+                    eksisterendeGrunnlag.getUnntakEtablertTilsynForPleietrengende().getNattevåk()
+                );
+            case NATTEVÅK ->
+                new UnntakEtablertTilsynForPleietrengende(
+                    eksisterendeGrunnlag.getUnntakEtablertTilsynForPleietrengende().getPleietrengendeAktørId(),
+                    eksisterendeGrunnlag.getUnntakEtablertTilsynForPleietrengende().getBeredskap(),
+                    nyttUnntakEtablertTilsyn
+                );
+        };
+
+        unntakEtablertTilsynGrunnlagRepository.lagre(behandlingId, nyttUnntakEtablertTilsynForPleietrengende);
 
         return OppdateringResultat.utenOveropp();
     }
 
+    private Vurderingstype finnVurderingstype(Vurdering vurdering) {
+        if (vurdering instanceof VurderingBeredskapDto) {
+            return Vurderingstype.BEREDSKAP;
+        } else if (vurdering instanceof VurderingNattevåkDto) {
+            return Vurderingstype.NATTEVÅK;
+        }
+        throw new IllegalArgumentException("Ugyldig subklasse av Vurdering");
+    }
+
+    private UnntakEtablertTilsyn finnUnntakEtablertTilsyn(Vurderingstype vurderingstype, UnntakEtablertTilsynForPleietrengende unntakEtablertTilsynForPleietrengende) {
+        return switch (vurderingstype) {
+            case BEREDSKAP -> unntakEtablertTilsynForPleietrengende.getBeredskap();
+            case NATTEVÅK -> unntakEtablertTilsynForPleietrengende.getNattevåk();
+        };
+    }
+
+}
+
+enum Vurderingstype {
+    BEREDSKAP,
+    NATTEVÅK
 }
