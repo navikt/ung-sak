e3341a669a228535a419b6ef993d99bebf38548a
Marius Glittum <marius.glittum@nav.no>
2021-05-25 12:55:57 +0200

    Endrer kode på inaktiv til AktivitetStatus.MIDLERTIDIG_INAKTIV

--- kodeverk/src/main/java/no/nav/k9/kodeverk/uttak/UttakArbeidType.java
+++ kodeverk/src/main/java/no/nav/k9/kodeverk/uttak/UttakArbeidType.java
@@ -25,7 +25,7 @@ public enum UttakArbeidType implements Kodeverdi {
     SELVSTENDIG_NÆRINGSDRIVENDE(AktivitetStatus.SELVSTENDIG_NÆRINGSDRIVENDE, "Selvstendig næringsdrivende"),
     FRILANSER(AktivitetStatus.FRILANSER, "Frilans"),
 
-    INAKTIV("INAKTIV", "Inaktiv"),
+    INAKTIV(AktivitetStatus.MIDLERTIDIG_INAKTIV, "Inaktiv"),
     ANNET("ANNET", "Annet"),
     ;
 
