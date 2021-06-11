66429cbab4a3e343a4c16629a7da2f6edaa1277d
Stian Grenborgen <stian.grenborgen@nav.no>
2021-06-11 20:44:14 +0200

    Beholder gammel informasjon som er uendret (kilde osv).

--- ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/repo/unntaketablerttilsyn/BeredskapOgNattevåkOppdaterer.java
+++ ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/repo/unntaketablerttilsyn/BeredskapOgNattevåkOppdaterer.java
@@ -41,19 +41,24 @@ public class BeredskapOgNattevåkOppdaterer {
     }
 
     private static List<UnntakEtablertTilsynPeriode> finnUnntakEtablertTilsynPerioder(UnntakEtablertTilsyn eksisterendeUnntakEtablertTilsyn, List<Unntaksperiode> nyeUnntak, List<Periode> unntakSomSkalSlettes, AktørId aktørId, Long kildeBehandlingId) {
-        var eksisterendeSegmenter = new ArrayList<LocalDateSegment<Unntak>>();
+        var eksisterendeSegmenter = new ArrayList<LocalDateSegment<UnntakEtablertTilsynPeriode>>();
         if (eksisterendeUnntakEtablertTilsyn != null) {
             eksisterendeUnntakEtablertTilsyn.getPerioder().forEach(periode ->
-                eksisterendeSegmenter.add(new LocalDateSegment<>(periode.getPeriode().toLocalDateInterval(), new Unntak(periode.getBegrunnelse(), periode.getResultat())))
+                eksisterendeSegmenter.add(new LocalDateSegment<>(periode.getPeriode().toLocalDateInterval(), periode))
             );
         }
 
         var segementerSomSkalSlettes = unntakSomSkalSlettes.stream().map(periode ->
-            new LocalDateSegment<>(new LocalDateInterval(periode.getFom(), periode.getTom()), new Unntak(null, null))
+            new LocalDateSegment<>(new LocalDateInterval(periode.getFom(), periode.getTom()), null)
         ).toList();
 
         var segmenterSomSkalLeggesTil = nyeUnntak.stream().map(periode ->
-            new LocalDateSegment<>(new LocalDateInterval(periode.fom(), periode.tom()), new Unntak(periode.begrunnelse(), periode.resultat()))
+            new LocalDateSegment<>(new LocalDateInterval(periode.fom(), periode.tom()), new UnntakEtablertTilsynPeriode()
+                    .medBegrunnelse(periode.begrunnelse())
+                    .medResultat(periode.resultat())
+                    .medAktørId(aktørId)
+                    .medKildeBehandlingId(kildeBehandlingId)
+                    )
         ).toList();
 
         var perioderTidslinje =
@@ -62,12 +67,11 @@ public class BeredskapOgNattevåkOppdaterer {
                 .crossJoin(new LocalDateTimeline<>(segmenterSomSkalLeggesTil), BeredskapOgNattevåkOppdaterer::siste);
 
         return perioderTidslinje.toSegments().stream().map(segment ->
-            new UnntakEtablertTilsynPeriode()
+            new UnntakEtablertTilsynPeriode(segment.getValue())
                 .medPeriode(DatoIntervallEntitet.fraOgMedTilOgMed(segment.getFom(), segment.getTom()))
+                /*
                 .medBegrunnelse(segment.getValue().resultat().equals(Resultat.IKKE_VURDERT) ? "" : segment.getValue().begrunnelse())
-                .medAktørId(aktørId)
-                .medKildeBehandlingId(kildeBehandlingId)
-                .medResultat(segment.getValue().resultat())
+                */
         ).toList();
     }
 
