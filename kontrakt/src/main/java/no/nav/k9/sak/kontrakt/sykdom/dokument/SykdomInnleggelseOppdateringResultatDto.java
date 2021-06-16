da39099815fe81bccfbfbb50d35eb0a143a3c373
Stian Grenborgen <stian.grenborgen@nav.no>
2021-06-16 14:58:30 +0200

    Legger til at dryRun ikke lagrer. (#4481)

---
+++ kontrakt/src/main/java/no/nav/k9/sak/kontrakt/sykdom/dokument/SykdomInnleggelseOppdateringResultatDto.java
@@ -0,0 +1,32 @@
+package no.nav.k9.sak.kontrakt.sykdom.dokument;
+
+import javax.validation.Valid;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+@JsonAutoDetect(getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE, fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class SykdomInnleggelseOppdateringResultatDto {
+
+    
+    @JsonProperty(value = "førerTilRevurdering")
+    @Valid
+    private boolean førerTilRevurdering = false;
+
+
+    public SykdomInnleggelseOppdateringResultatDto() {
+
+    }
+
+    public SykdomInnleggelseOppdateringResultatDto(boolean førerTilRevurdering) {
+        this.førerTilRevurdering = førerTilRevurdering;
+    }
+
+    public boolean isFørerTilRevurdering() {
+        return førerTilRevurdering;
+    }
+}
