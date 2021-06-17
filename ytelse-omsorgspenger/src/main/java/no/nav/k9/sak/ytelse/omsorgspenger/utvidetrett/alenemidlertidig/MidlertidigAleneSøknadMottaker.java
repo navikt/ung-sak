790c6eaf99ce7271df8b32d65809830e67b0c5b7
Marius Glittum <marius.glittum@nav.no>
2021-06-17 12:39:23 +0200

    Revert "Revert "fix: periodehåndtering der søknadsfrist kan være inntil 3 år før søknadstidspunktet (#4483)""
    This reverts commit 3a72ff30

--- ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/utvidetrett/alenemidlertidig/MidlertidigAleneSøknadMottaker.java
+++ ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/utvidetrett/alenemidlertidig/MidlertidigAleneSøknadMottaker.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.Objects;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Any;
 import javax.inject.Inject;
 
 import no.nav.k9.kodeverk.behandling.FagsakYtelseType;
@@ -23,6 +24,7 @@ public class MidlertidigAleneSøknadMottaker implements SøknadMottakTjeneste<In
 
     private FagsakTjeneste fagsakTjeneste;
     private SaksnummerRepository saksnummerRepository;
+    private MidlertidigAleneVilkårsVurderingTjeneste vilkårsVurderingTjeneste;
 
     MidlertidigAleneSøknadMottaker() {
         // proxy
@@ -30,7 +32,9 @@ public class MidlertidigAleneSøknadMottaker implements SøknadMottakTjeneste<In
 
     @Inject
     public MidlertidigAleneSøknadMottaker(SaksnummerRepository saksnummerRepository,
+                                          @Any MidlertidigAleneVilkårsVurderingTjeneste vilkårsVurderingTjeneste,
                                           FagsakTjeneste fagsakTjeneste) {
+        this.vilkårsVurderingTjeneste = vilkårsVurderingTjeneste;
         this.fagsakTjeneste = fagsakTjeneste;
         this.saksnummerRepository = saksnummerRepository;
     }
@@ -46,8 +50,9 @@ public class MidlertidigAleneSøknadMottaker implements SøknadMottakTjeneste<In
         }
 
         var saksnummer = new Saksnummer(saksnummerRepository.genererNyttSaksnummer());
+        var fagsakPeriode = vilkårsVurderingTjeneste.utledMaksPeriode(DatoIntervallEntitet.fra(startDato, sluttDato));
 
-        return opprettSakFor(saksnummer, søkerAktørId, pleietrengendeAktørId, relatertPersonAktørId, ytelseType, startDato, datoIntervall.getTomDato());
+        return opprettSakFor(saksnummer, søkerAktørId, pleietrengendeAktørId, relatertPersonAktørId, ytelseType, fagsakPeriode.getFomDato(), fagsakPeriode.getTomDato());
     }
 
     private Fagsak opprettSakFor(Saksnummer saksnummer, AktørId brukerIdent, AktørId pleietrengendeAktørId, AktørId relatertPersonAktørId, FagsakYtelseType ytelseType, LocalDate fom, LocalDate tom) {
--- ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/utvidetrett/alenemidlertidig/MidlertidigAleneVilkårsVurderingTjeneste.java
+++ ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/utvidetrett/alenemidlertidig/MidlertidigAleneVilkårsVurderingTjeneste.java
@@ -1,66 +1,93 @@
 package no.nav.k9.sak.ytelse.omsorgspenger.utvidetrett.alenemidlertidig;
 
+import java.util.Collections;
 import java.util.Map;
 import java.util.NavigableSet;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 
+import no.nav.k9.felles.konfigurasjon.konfig.Tid;
 import no.nav.k9.kodeverk.vilkår.VilkårType;
 import no.nav.k9.sak.behandlingskontroll.BehandlingTypeRef;
 import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
-import no.nav.k9.sak.behandlingslager.behandling.Behandling;
-import no.nav.k9.sak.behandlingslager.behandling.repository.BehandlingRepository;
 import no.nav.k9.sak.behandlingslager.behandling.søknad.SøknadRepository;
+import no.nav.k9.sak.behandlingslager.behandling.vilkår.VilkårResultatRepository;
 import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
 import no.nav.k9.sak.perioder.VilkårsPerioderTilVurderingTjeneste;
+import no.nav.k9.sak.ytelse.omsorgspenger.utvidetrett.UtvidetRettSøknadPerioder;
 
 @FagsakYtelseTypeRef("OMP_MA")
 @BehandlingTypeRef
 @RequestScoped
 public class MidlertidigAleneVilkårsVurderingTjeneste implements VilkårsPerioderTilVurderingTjeneste {
 
-    private BehandlingRepository behandlingRepository;
-    private SøknadRepository søknadRepository;
+    private UtvidetRettSøknadPerioder søktePerioder;
+    private VilkårResultatRepository vilkårResultatRepository;
 
     MidlertidigAleneVilkårsVurderingTjeneste() {
         // for proxy
     }
 
     @Inject
-    public MidlertidigAleneVilkårsVurderingTjeneste(BehandlingRepository behandlingRepository,
+    public MidlertidigAleneVilkårsVurderingTjeneste(VilkårResultatRepository vilkårResultatRepository,
                                                     SøknadRepository søknadRepository) {
-        this.behandlingRepository = behandlingRepository;
-        this.søknadRepository = søknadRepository;
+        this.vilkårResultatRepository = vilkårResultatRepository;
+        this.søktePerioder = new UtvidetRettSøknadPerioder(søknadRepository);
+    }
+
+    @Override
+    public NavigableSet<DatoIntervallEntitet> perioderSomSkalTilbakestilles(Long behandlingId) {
+        return new TreeSet<>();
+    }
+
+    @Override
+    public NavigableSet<DatoIntervallEntitet> utledFullstendigePerioder(Long behandlingId) {
+        var søknadsperioder = søktePerioder.utledPeriode(behandlingId);
+        var maksPeriode = utledMaksPeriode(søknadsperioder);
+        return Collections.unmodifiableNavigableSet(new TreeSet<>(Set.of(maksPeriode)));
     }
 
     @Override
     public NavigableSet<DatoIntervallEntitet> utled(Long behandlingId, VilkårType vilkårType) {
-        var behandling = behandlingRepository.hentBehandling(behandlingId);
-        var periode = utledPeriode(behandling);
-        return new TreeSet<>(Set.of(periode));
+        var optVilkårene = vilkårResultatRepository.hentHvisEksisterer(behandlingId);
+        if (optVilkårene.isPresent()) {
+            var vilkårTidslinje = optVilkårene.get().getVilkårTimeline(vilkårType);
+            var utlededePerioder = vilkårTidslinje.getLocalDateIntervals().stream().map(p -> DatoIntervallEntitet.fra(p)).collect(Collectors.toCollection(TreeSet::new));
+            return Collections.unmodifiableNavigableSet(utlededePerioder);
+        } else {
+            // default til søkte perioder hvis vilkår ikke angitt.
+            return søktePerioder.utledPeriode(behandlingId);
+        }
     }
 
     @Override
     public Map<VilkårType, NavigableSet<DatoIntervallEntitet>> utledRådataTilUtledningAvVilkårsperioder(Long behandlingId) {
-        var behandling = behandlingRepository.hentBehandling(behandlingId);
-        var søknadsperiode = utledPeriode(behandling);
-        var omsorgperiode = DatoIntervallEntitet.fraOgMed(søknadsperiode.getFomDato()); // lar denne perioden stå åpen for omsorg
         return Map.of(
-            VilkårType.UTVIDETRETT, new TreeSet<>(Set.of(søknadsperiode)),
-            VilkårType.OMSORGEN_FOR, new TreeSet<>(Set.of(omsorgperiode)));
+            VilkårType.UTVIDETRETT, utled(behandlingId, VilkårType.UTVIDETRETT),
+            VilkårType.OMSORGEN_FOR, utled(behandlingId, VilkårType.OMSORGEN_FOR));
     }
 
-    private DatoIntervallEntitet utledPeriode(Behandling behandling) {
-        var søknad = søknadRepository.hentSøknad(behandling);
-        var søknadsperiode = søknad.getSøknadsperiode();
-        return DatoIntervallEntitet.fraOgMedTilOgMed(søknadsperiode.getFomDato(), søknadsperiode.getTomDato());
+    DatoIntervallEntitet utledMaksPeriode(NavigableSet<DatoIntervallEntitet> søktePerioder) {
+        // 1. jan minst 3 år før søknad sendt inn (spesielle særtilfeller tillater at et går an å sette tilbake it itid
+        var førsteSøktePeriode = søktePerioder.first();
+        var fristFørSøknadsdato = førsteSøktePeriode.getFomDato().minusYears(3).withMonth(1).withDayOfMonth(1);
+
+        var mindato = fristFørSøknadsdato;
+        var maksdato = Tid.TIDENES_ENDE;
+        return DatoIntervallEntitet.fraOgMedTilOgMed(mindato, maksdato);
     }
 
     @Override
     public int maksMellomliggendePeriodeAvstand() {
         return Integer.MAX_VALUE;
     }
+
+    public DatoIntervallEntitet utledMaksPeriode(DatoIntervallEntitet periode) {
+        return utledMaksPeriode(new TreeSet<>(Set.of(periode)));
+    }
+
 }
