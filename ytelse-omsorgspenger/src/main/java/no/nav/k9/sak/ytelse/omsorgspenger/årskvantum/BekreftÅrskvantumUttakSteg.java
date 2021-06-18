028d4b9be4e8814645ec936bd422ead60cda931b
Helga Nyrud <helga.nyrud@nav.no>
2021-06-18 12:24:40 +0200

    TSF-399 Legger til nytt steg etter uttak for å ikke kjøre hele uttakssteget på nytt ved tilbakehopp for å løse 9004

---
+++ ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/årskvantum/BekreftÅrskvantumUttakSteg.java
@@ -0,0 +1,40 @@
+package no.nav.k9.sak.ytelse.omsorgspenger.årskvantum;
+
+import no.nav.k9.sak.behandlingskontroll.*;
+import no.nav.k9.sak.behandlingslager.behandling.repository.BehandlingRepository;
+import no.nav.k9.sak.ytelse.omsorgspenger.årskvantum.tjenester.ÅrskvantumTjeneste;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+@ApplicationScoped
+@BehandlingStegRef(kode = "BEKREFT_UTTAK")
+@BehandlingTypeRef
+@FagsakYtelseTypeRef("OMP")
+public class BekreftÅrskvantumUttakSteg implements BehandlingSteg {
+
+    private static final Logger log = LoggerFactory.getLogger(BekreftÅrskvantumUttakSteg.class);
+
+    private BehandlingRepository behandlingRepository;
+    private ÅrskvantumTjeneste årskvantumTjeneste;
+
+    BekreftÅrskvantumUttakSteg() {
+        // for proxy
+    }
+
+    @Inject
+    public BekreftÅrskvantumUttakSteg(BehandlingRepository behandlingRepository,
+                                     ÅrskvantumTjeneste årskvantumTjeneste) {
+        this.behandlingRepository = behandlingRepository;
+        this.årskvantumTjeneste = årskvantumTjeneste;
+    }
+
+    @Override
+    public BehandleStegResultat utførSteg(BehandlingskontrollKontekst kontekst) {
+        // Dette er et dummy steg for å unngå å kjøre hele uttakssteget på nytt ved tilbakehopp
+        return BehandleStegResultat.utførtUtenAksjonspunkter();
+    }
+
+}
