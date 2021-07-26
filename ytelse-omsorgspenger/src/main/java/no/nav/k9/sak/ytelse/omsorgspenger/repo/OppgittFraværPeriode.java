b4e1f7e150d43c2bc9764aadaaad237f427d34d0
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2021-07-26 14:25:17 +0200

    Bytter fra ordinal til kode for OppgittFraværPeriode.aktivitetType

--- ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/repo/OppgittFraværPeriode.java
+++ ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/repo/OppgittFraværPeriode.java
@@ -63,13 +63,9 @@ public class OppgittFraværPeriode extends BaseEntitet implements IndexKey, Søk
     @JsonIgnore //må ha denne for å unngå sirkulær traversering fra Jackson - ved serialisering av regelsporing
     private OppgittFravær oppgittFravær;
 
-    @ChangeTracked
-    @Column(name = "aktivitet_type", nullable = false, updatable = false)
-    private UttakArbeidType aktivitetType;
-
     @Convert(converter = UttakArbeidTypeKodeConverter.class)
     @ChangeTracked
-    @Column(name = "aktivitet", nullable = true, updatable = false)
+    @Column(name = "aktivitet", nullable = false, updatable = false)
     private UttakArbeidType aktivitet;
 
     @Convert(converter = FraværÅrsakKodeConverter.class)
@@ -102,8 +98,7 @@ public class OppgittFraværPeriode extends BaseEntitet implements IndexKey, Søk
     // Kun for test
     public OppgittFraværPeriode(LocalDate fom, LocalDate tom, UttakArbeidType aktivitetType, Duration fraværPerDag) {
         this.periode = DatoIntervallEntitet.fraOgMedTilOgMed(fom, tom);
-        this.aktivitetType = Objects.requireNonNull(aktivitetType, "aktivitetType");
-        this.aktivitet = aktivitetType;
+        this.aktivitet = Objects.requireNonNull(aktivitetType, "aktivitetType");
         this.fraværPerDag = fraværPerDag;
     }
 
@@ -112,8 +107,7 @@ public class OppgittFraværPeriode extends BaseEntitet implements IndexKey, Søk
         this.arbeidsgiver = arbeidsgiver;
         this.arbeidsforholdRef = arbeidsforholdRef;
         this.fraværPerDag = fraværPerDag;
-        this.aktivitetType = Objects.requireNonNull(aktivitetType, "aktivitetType");
-        this.aktivitet = aktivitetType;
+        this.aktivitet = Objects.requireNonNull(aktivitetType, "aktivitetType");
         this.periode = DatoIntervallEntitet.fraOgMedTilOgMed(fom, tom);
         this.fraværÅrsak = fraværÅrsak;
     }
@@ -127,8 +121,7 @@ public class OppgittFraværPeriode extends BaseEntitet implements IndexKey, Søk
         this.arbeidsgiver = periode.arbeidsgiver;
         this.arbeidsforholdRef = periode.arbeidsforholdRef;
         this.fraværPerDag = periode.fraværPerDag;
-        this.aktivitetType = Objects.requireNonNull(periode.aktivitetType, "aktivitetType");
-        this.aktivitet = aktivitetType;
+        this.aktivitet = Objects.requireNonNull(periode.aktivitet, "aktivitetType");
         this.periode = periode.periode;
         this.fraværÅrsak = periode.fraværÅrsak;
     }
@@ -136,7 +129,7 @@ public class OppgittFraværPeriode extends BaseEntitet implements IndexKey, Søk
 
     @Override
     public String getIndexKey() {
-        return IndexKeyComposer.createKey(journalpostId, periode, getAktivitetType(), arbeidsgiver, arbeidsforholdRef);
+        return IndexKeyComposer.createKey(journalpostId, periode, aktivitet, arbeidsgiver, arbeidsforholdRef);
     }
 
     public JournalpostId getJournalpostId() {
@@ -168,7 +161,7 @@ public class OppgittFraværPeriode extends BaseEntitet implements IndexKey, Søk
     }
 
     public UttakArbeidType getAktivitetType() {
-        return aktivitet != null ? aktivitet : aktivitetType;
+        return aktivitet;
     }
 
     public Duration getFraværPerDag() {
@@ -183,7 +176,7 @@ public class OppgittFraværPeriode extends BaseEntitet implements IndexKey, Søk
     @Override
     public <V> V getPayload() {
         // et lite objekt med periode dataene (ikke periode, ikke journalpostid) til sammenligning
-        return (V) Arrays.asList(fraværPerDag, getAktivitetType(), fraværÅrsak, arbeidsgiver, arbeidsforholdRef);
+        return (V) Arrays.asList(fraværPerDag, aktivitet, fraværÅrsak, arbeidsgiver, arbeidsforholdRef);
     }
 
     @Override
@@ -196,7 +189,7 @@ public class OppgittFraværPeriode extends BaseEntitet implements IndexKey, Søk
         return Objects.equals(periode, that.periode)
             && Objects.equals(journalpostId, that.journalpostId)
             && Objects.equals(fraværPerDag, that.fraværPerDag)
-            && Objects.equals(getAktivitetType(), that.getAktivitetType())
+            && Objects.equals(aktivitet, that.aktivitet)
             && Objects.equals(fraværÅrsak, that.fraværÅrsak)
             && Objects.equals(arbeidsgiver, that.arbeidsgiver)
             && Objects.equals(arbeidsforholdRef, that.arbeidsforholdRef);
@@ -204,7 +197,7 @@ public class OppgittFraværPeriode extends BaseEntitet implements IndexKey, Søk
 
     @Override
     public int hashCode() {
-        return Objects.hash(periode, journalpostId, fraværPerDag, getAktivitetType(), fraværÅrsak, arbeidsgiver, arbeidsforholdRef);
+        return Objects.hash(periode, journalpostId, fraværPerDag, aktivitet, fraværÅrsak, arbeidsgiver, arbeidsforholdRef);
     }
 
     @Override
@@ -213,7 +206,7 @@ public class OppgittFraværPeriode extends BaseEntitet implements IndexKey, Søk
             "id=" + id +
             ", periode=" + periode +
             ", journalpostId=" + journalpostId +
-            ", aktivitetType=" + getAktivitetType() +
+            ", aktivitetType=" + aktivitet +
             ", fraværÅrsak=" + fraværÅrsak +
             (arbeidsgiver != null ? ", arbeidsgiver=" + arbeidsgiver : "") +
             (arbeidsforholdRef != null ? ", arbeidsforholdRef=" + arbeidsforholdRef : "") +
