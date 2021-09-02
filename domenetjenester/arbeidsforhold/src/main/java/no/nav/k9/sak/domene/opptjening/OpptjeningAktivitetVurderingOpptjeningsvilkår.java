2eb13c567ef60ccb0b2d18d04efb2825064b64ba
s149212 <espen.svendsen@nav.no>
2021-09-02 14:42:06 +0200

    Mer småpirk
    Signed-off-by: s149212 <espen.svendsen@nav.no>

--- domenetjenester/arbeidsforhold/src/main/java/no/nav/k9/sak/domene/opptjening/OpptjeningAktivitetVurderingOpptjeningsvilkår.java
+++ domenetjenester/arbeidsforhold/src/main/java/no/nav/k9/sak/domene/opptjening/OpptjeningAktivitetVurderingOpptjeningsvilkår.java
@@ -1,12 +1,12 @@
 package no.nav.k9.sak.domene.opptjening;
 
-import no.nav.k9.kodeverk.opptjening.OpptjeningAktivitetType;
 import no.nav.k9.sak.domene.opptjening.aksjonspunkt.VurderStatusInput;
 
 public class OpptjeningAktivitetVurderingOpptjeningsvilkår implements OpptjeningAktivitetVurdering {
 
-    public VurderingsStatus vurderStatus(OpptjeningAktivitetType type) {
-        return switch (type) {
+    @Override
+    public VurderingsStatus vurderStatus(VurderStatusInput input) {
+        return switch (input.getType()) {
             // Automatisk godkjente opptjeningsaktiviteter mht opptjeningsvilkåret
             // - Arbeidsforhold registrert eller overstyrt gjennom AP 5080
             // - Egen næring oppgitt eller registrert
@@ -15,11 +15,5 @@ public class OpptjeningAktivitetVurderingOpptjeningsvilkår implements Opptjenin
             // Alle andre opptjeningsaktiviteter underkjennes mht automatisk vilkårsvurdering
             default -> VurderingsStatus.FERDIG_VURDERT_UNDERKJENT;
         };
-    }
-
-    @Override
-    public VurderingsStatus vurderStatus(VurderStatusInput input) {
-        return vurderStatus(input.getType()
-        );
     }
 }
