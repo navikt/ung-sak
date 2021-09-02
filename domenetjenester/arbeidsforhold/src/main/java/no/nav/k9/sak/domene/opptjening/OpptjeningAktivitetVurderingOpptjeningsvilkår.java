3910c08715d446c7ab35598704fe360b833a5dcb
s149212 <espen.svendsen@nav.no>
2021-09-02 14:35:36 +0200

    Småpirk
    Signed-off-by: s149212 <espen.svendsen@nav.no>

--- domenetjenester/arbeidsforhold/src/main/java/no/nav/k9/sak/domene/opptjening/OpptjeningAktivitetVurderingOpptjeningsvilkår.java
+++ domenetjenester/arbeidsforhold/src/main/java/no/nav/k9/sak/domene/opptjening/OpptjeningAktivitetVurderingOpptjeningsvilkår.java
@@ -5,29 +5,18 @@ import no.nav.k9.sak.domene.opptjening.aksjonspunkt.VurderStatusInput;
 
 public class OpptjeningAktivitetVurderingOpptjeningsvilkår implements OpptjeningAktivitetVurdering {
 
-    public OpptjeningAktivitetVurderingOpptjeningsvilkår() {
-    }
-
     public VurderingsStatus vurderStatus(OpptjeningAktivitetType type) {
-
-        if (OpptjeningAktivitetType.ARBEID.equals(type)) {
-            // Registrert eller overstyrt arbeidsforhold (AP 5080) godkjennes. Oppgitt arbeidsforhold (innenlands) skal ikke forekomme, da det ikke lagres på IAY
-            return VurderingsStatus.FERDIG_VURDERT_GODKJENT;
-        } else if (OpptjeningAktivitetType.NÆRING.equals(type)) {
-            // Både oppgitt og registrert selvstendig næringsdrivende godkjennes som opptjeningsaktivitet
-            return VurderingsStatus.FERDIG_VURDERT_GODKJENT;
-        } else if (OpptjeningAktivitetType.FRILANS.equals(type)) {
-            // Både oppgitt og registrert frilans godkjennes som opptjeningsaktivitet
-            return VurderingsStatus.FERDIG_VURDERT_GODKJENT;
-        } else if (OpptjeningAktivitetType.ANNEN_OPPTJENING.contains(type)) {
-            // Oppgitt annen aktivitet underkjennes som opptjeningsaktivitet (mht automatisk vilkårsvurdering)
-            return VurderingsStatus.FERDIG_VURDERT_UNDERKJENT;
-        } else if (OpptjeningAktivitetType.UTENLANDSK_ARBEIDSFORHOLD.equals(type)) {
-            // Oppgitt utenlandsk arbeidsforhold underkjennes som opptjeningsaktivitet (mht automatisk vilkårsvurdering)
-            return VurderingsStatus.FERDIG_VURDERT_UNDERKJENT;
+        VurderingsStatus vurderingsStatus;
+        switch (type) {
+            // Automatisk godkjente opptjeningsaktiviteter mht opptjeningsvilkåret
+            // - Arbeidsforhold registrert eller overstyrt gjennom AP 5080
+            // - Egen næring oppgitt eller registrert
+            // - Frilans oppgitt eller registrert
+            case ARBEID, NÆRING, FRILANS -> vurderingsStatus = VurderingsStatus.FERDIG_VURDERT_GODKJENT;
+            // Alle andre opptjeningsaktiviteter underkjennes mht automatisk vilkårsvurdering
+            default -> vurderingsStatus = VurderingsStatus.FERDIG_VURDERT_UNDERKJENT;
         }
-        // Alle andre opptjeningsaktiviteter underkjennes (mht automatisk vilkårsvurdering)
-        return VurderingsStatus.FERDIG_VURDERT_UNDERKJENT;
+        return vurderingsStatus;
     }
 
     @Override
