c3fe149911a79ae19fbc131fb44be69bfec94abe
s149212 <espen.svendsen@nav.no>
2021-09-03 07:48:38 +0200

    Endrer automatisk vurdering arbeid/SN/FL fra FERDIG_VURDERT_GODKJENT -> TIL_VURDERING
    Signed-off-by: s149212 <espen.svendsen@nav.no>

--- domenetjenester/arbeidsforhold/src/main/java/no/nav/k9/sak/domene/opptjening/OpptjeningAktivitetVurderingOpptjeningsvilkår.java
+++ domenetjenester/arbeidsforhold/src/main/java/no/nav/k9/sak/domene/opptjening/OpptjeningAktivitetVurderingOpptjeningsvilkår.java
@@ -11,7 +11,7 @@ public class OpptjeningAktivitetVurderingOpptjeningsvilkår implements Opptjenin
             // - Arbeidsforhold registrert eller overstyrt gjennom AP 5080
             // - Egen næring oppgitt eller registrert
             // - Frilans oppgitt eller registrert
-            case ARBEID, NÆRING, FRILANS -> VurderingsStatus.FERDIG_VURDERT_GODKJENT;
+            case ARBEID, NÆRING, FRILANS -> VurderingsStatus.TIL_VURDERING;
             // Alle andre opptjeningsaktiviteter underkjennes mht automatisk vilkårsvurdering
             default -> VurderingsStatus.FERDIG_VURDERT_UNDERKJENT;
         };
