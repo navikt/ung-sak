919b47610de2192ec3cc24f65214b7331e7bb2b9
christofferlohne <christoffer.lohne@nav.no>
2021-09-13 13:15:52 +0200

    oppdaterer til format versjon 5.4.18 og oppdaterer validering med endringsperioder

--- ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/mottak/PSBSøknadDokumentValidator.java
+++ ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/mottak/PSBSøknadDokumentValidator.java
@@ -1,7 +1,9 @@
 package no.nav.k9.sak.ytelse.pleiepengerbarn.mottak;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -11,6 +13,8 @@ import no.nav.k9.sak.behandlingslager.behandling.motattdokument.MottattDokument;
 import no.nav.k9.sak.mottak.dokumentmottak.DokumentGruppeRef;
 import no.nav.k9.sak.mottak.dokumentmottak.DokumentValidator;
 import no.nav.k9.sak.mottak.dokumentmottak.SøknadParser;
+import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.søknadsperiode.SøknadsperiodeTjeneste;
+import no.nav.k9.søknad.felles.type.Periode;
 import no.nav.k9.søknad.ytelse.psb.v1.PleiepengerSyktBarnSøknadValidator;
 
 @ApplicationScoped
@@ -18,14 +22,16 @@ import no.nav.k9.søknad.ytelse.psb.v1.PleiepengerSyktBarnSøknadValidator;
 public class PSBSøknadDokumentValidator implements DokumentValidator {
 
     private SøknadParser søknadParser;
+    private SøknadsperiodeTjeneste søknadsperiodeTjeneste;
 
     PSBSøknadDokumentValidator() {
         // CDI
     }
 
     @Inject
-    public PSBSøknadDokumentValidator(SøknadParser søknadParser) {
+    public PSBSøknadDokumentValidator(SøknadParser søknadParser, SøknadsperiodeTjeneste søknadsperiodeTjeneste) {
         this.søknadParser = søknadParser;
+        this.søknadsperiodeTjeneste = søknadsperiodeTjeneste;
     }
 
     @Override
@@ -41,8 +47,17 @@ public class PSBSøknadDokumentValidator implements DokumentValidator {
         if (!Objects.equals(Brevkode.PLEIEPENGER_BARN_SOKNAD, mottattDokument.getType())) {
             throw new IllegalArgumentException("Forventet brevkode: " + Brevkode.PLEIEPENGER_BARN_SOKNAD + ", fikk: " + mottattDokument.getType());
         }
+        var kravperioder = søknadsperiodeTjeneste.hentKravperioder(mottattDokument.getFagsakId(), mottattDokument.getBehandlingId());
         var søknad = søknadParser.parseSøknad(mottattDokument);
 
-        new PleiepengerSyktBarnSøknadValidator().forsikreValidert(søknad);
+        (new PleiepengerSyktBarnSøknadValidator()).forsikreValidert(søknad, tilPerioder(kravperioder));
+    }
+
+    private List<Periode> tilPerioder(List<SøknadsperiodeTjeneste.Kravperiode> kravperioder) {
+        return kravperioder.stream().map(this::tilPeriode).collect(Collectors.toList());
+    }
+
+    private Periode tilPeriode(SøknadsperiodeTjeneste.Kravperiode kravperiode) {
+        return new Periode(kravperiode.getPeriode().getFomDato(), kravperiode.getPeriode().getTomDato());
     }
 }
