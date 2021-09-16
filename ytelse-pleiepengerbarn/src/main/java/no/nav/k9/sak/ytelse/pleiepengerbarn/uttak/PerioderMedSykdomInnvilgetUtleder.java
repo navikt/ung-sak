39a1f9b43dc888110aa44662ef82ae7aec494bad
Marius Bækken Glittum <marius.glittum@nav.no>
2021-09-16 08:01:12 +0200

    Skal kun få aksjonspunkt ved nattevåk / beredskap hvis det er innvilget perioder med sykdom (#5056)

---
+++ ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/uttak/PerioderMedSykdomInnvilgetUtleder.java
@@ -0,0 +1,77 @@
+package no.nav.k9.sak.ytelse.pleiepengerbarn.uttak;
+
+import java.util.NavigableSet;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+
+import no.nav.fpsak.tidsserie.LocalDateSegment;
+import no.nav.fpsak.tidsserie.LocalDateTimeline;
+import no.nav.fpsak.tidsserie.StandardCombinators;
+import no.nav.k9.kodeverk.vilkår.Utfall;
+import no.nav.k9.kodeverk.vilkår.VilkårType;
+import no.nav.k9.sak.behandling.BehandlingReferanse;
+import no.nav.k9.sak.behandlingskontroll.BehandlingTypeRef;
+import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
+import no.nav.k9.sak.behandlingslager.behandling.vilkår.VilkårResultatRepository;
+import no.nav.k9.sak.behandlingslager.behandling.vilkår.Vilkårene;
+import no.nav.k9.sak.behandlingslager.behandling.vilkår.periode.VilkårPeriode;
+import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
+import no.nav.k9.sak.perioder.VilkårsPerioderTilVurderingTjeneste;
+
+@Dependent
+public class PerioderMedSykdomInnvilgetUtleder {
+
+    private VilkårResultatRepository vilkårResultatRepository;
+    private VilkårsPerioderTilVurderingTjeneste perioderTilVurderingTjeneste;
+
+    @Inject
+    public PerioderMedSykdomInnvilgetUtleder(@FagsakYtelseTypeRef("PSB") @BehandlingTypeRef VilkårsPerioderTilVurderingTjeneste perioderTilVurderingTjeneste,
+                                             VilkårResultatRepository vilkårResultatRepository) {
+        this.vilkårResultatRepository = vilkårResultatRepository;
+        this.perioderTilVurderingTjeneste = perioderTilVurderingTjeneste;
+    }
+
+    public Set<VilkårPeriode> utledInnvilgedePerioderTilVurdering(BehandlingReferanse referanse) {
+        var behandlingId = referanse.getBehandlingId();
+        final var perioderVurdertISykdom = utledPerioderVurdert(behandlingId);
+
+        var vilkårene = vilkårResultatRepository.hent(behandlingId);
+
+        return finnInnvilgedePerioder(vilkårene, perioderVurdertISykdom);
+    }
+
+    private NavigableSet<DatoIntervallEntitet> utledPerioderVurdert(Long behandlingId) {
+        var perioderUnder18 = perioderTilVurderingTjeneste.utled(behandlingId, VilkårType.MEDISINSKEVILKÅR_UNDER_18_ÅR);
+        var perioder18OgOver = perioderTilVurderingTjeneste.utled(behandlingId, VilkårType.MEDISINSKEVILKÅR_18_ÅR);
+
+        var perioderUnder = new LocalDateTimeline<>(perioderUnder18.stream().map(it -> new LocalDateSegment<>(it.getFomDato(), it.getTomDato(), true)).collect(Collectors.toList()));
+        var perioderOver = new LocalDateTimeline<>(perioder18OgOver.stream().map(it -> new LocalDateSegment<>(it.getFomDato(), it.getTomDato(), true)).collect(Collectors.toList()));
+
+        return perioderUnder.combine(perioderOver, StandardCombinators::alwaysTrueForMatch, LocalDateTimeline.JoinStyle.CROSS_JOIN)
+            .compress()
+            .toSegments()
+            .stream()
+            .map(it -> DatoIntervallEntitet.fraOgMedTilOgMed(it.getFom(), it.getTom()))
+            .collect(Collectors.toCollection(TreeSet::new));
+    }
+
+    private Set<VilkårPeriode> finnInnvilgedePerioder(Vilkårene vilkårene,
+                                                      NavigableSet<DatoIntervallEntitet> perioderTilVurdering) {
+        var s1 = vilkårene.getVilkår(VilkårType.MEDISINSKEVILKÅR_UNDER_18_ÅR).orElseThrow()
+            .getPerioder()
+            .stream();
+        var s2 = vilkårene.getVilkår(VilkårType.MEDISINSKEVILKÅR_18_ÅR).orElseThrow()
+            .getPerioder()
+            .stream();
+        return Stream.concat(s1, s2)
+            .filter(it -> perioderTilVurdering.stream().anyMatch(at -> at.overlapper(it.getPeriode())))
+            .filter(it -> Utfall.OPPFYLT.equals(it.getUtfall()))
+            .collect(Collectors.toSet());
+    }
+
+}
