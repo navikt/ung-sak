cbb493734a9a357c618c8ccfcbdd30e012e14cd6
Marius Glittum <marius.glittum@nav.no>
2021-10-06 10:12:54 +0200

    Håndterer at opptjening av avslått

--- ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/uttak/input/arbeid/ArbeidBrukerBurdeSøktOmUtleder.java
+++ ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/uttak/input/arbeid/ArbeidBrukerBurdeSøktOmUtleder.java
@@ -198,6 +198,10 @@ public class ArbeidBrukerBurdeSøktOmUtleder {
             var skjæringstidspunkt = segment.getFom();
             var opptjening = opptjeningResultat.finnOpptjening(skjæringstidspunkt);
 
+            if (opptjening.isEmpty()) {
+                return;
+            }
+
             var erFrilans = opptjening.orElseThrow()
                 .getOpptjeningAktivitet()
                 .stream()
