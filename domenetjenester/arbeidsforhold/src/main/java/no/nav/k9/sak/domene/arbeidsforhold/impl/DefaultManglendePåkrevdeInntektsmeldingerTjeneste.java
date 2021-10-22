7b95168de062e2e9543ce1f6434b7a27cd10436e
Marius Bækken Glittum <marius.glittum@nav.no>
2021-10-22 13:18:32 +0200

    Fjerner ubrukt kode (#5285)

--- domenetjenester/arbeidsforhold/src/main/java/no/nav/k9/sak/domene/arbeidsforhold/impl/DefaultManglendePåkrevdeInntektsmeldingerTjeneste.java
+++ domenetjenester/arbeidsforhold/src/main/java/no/nav/k9/sak/domene/arbeidsforhold/impl/DefaultManglendePåkrevdeInntektsmeldingerTjeneste.java
@@ -14,10 +14,8 @@ import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import no.nav.k9.sak.behandling.BehandlingReferanse;
+import no.nav.k9.felles.util.Tuple;
 import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
-import no.nav.k9.sak.behandlingslager.behandling.søknad.SøknadEntitet;
-import no.nav.k9.sak.behandlingslager.behandling.søknad.SøknadRepository;
 import no.nav.k9.sak.domene.iay.modell.InntektArbeidYtelseGrunnlag;
 import no.nav.k9.sak.domene.iay.modell.Inntektsmelding;
 import no.nav.k9.sak.domene.iay.modell.InntektsmeldingAggregat;
@@ -25,7 +23,6 @@ import no.nav.k9.sak.domene.iay.modell.YrkesaktivitetFilter;
 import no.nav.k9.sak.typer.AktørId;
 import no.nav.k9.sak.typer.Arbeidsgiver;
 import no.nav.k9.sak.typer.InternArbeidsforholdRef;
-import no.nav.k9.felles.util.Tuple;
 
 @ApplicationScoped
 @FagsakYtelseTypeRef
@@ -33,36 +30,9 @@ public class DefaultManglendePåkrevdeInntektsmeldingerTjeneste implements Ytels
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DefaultManglendePåkrevdeInntektsmeldingerTjeneste.class);
 
-    private InntektsmeldingRegisterTjeneste inntektsmeldingTjeneste;
-    private SøknadRepository søknadRepository;
-
-    DefaultManglendePåkrevdeInntektsmeldingerTjeneste() {
-        // CDI
-    }
-
     @Inject
-    public DefaultManglendePåkrevdeInntektsmeldingerTjeneste(InntektsmeldingRegisterTjeneste inntektsmeldingArkivTjeneste,
-                                                             SøknadRepository søknadRepository) {
-        this.inntektsmeldingTjeneste = inntektsmeldingArkivTjeneste;
-        this.søknadRepository = søknadRepository;
-    }
-
-    @Override
-    public Map<Arbeidsgiver, Set<ArbeidsforholdMedÅrsak>> leggTilArbeidsforholdHvorPåkrevdeInntektsmeldingMangler(InntektsmeldingVurderingInput input) {
-        var result = new HashMap<Arbeidsgiver, Set<ArbeidsforholdMedÅrsak>>();
-        var behandlingReferanse = input.getReferanse();
-        boolean erEndringssøknad = erEndringssøknad(behandlingReferanse);
-        boolean erIkkeEndringssøknad = !erEndringssøknad;
-
-        final Map<Arbeidsgiver, Set<InternArbeidsforholdRef>> manglendeInntektsmeldinger = inntektsmeldingTjeneste
-            .utledManglendeInntektsmeldingerFraGrunnlagForVurdering(behandlingReferanse, erEndringssøknad, behandlingReferanse.getUtledetSkjæringstidspunkt());
-        if (erIkkeEndringssøknad) {
-            for (Map.Entry<Arbeidsgiver, Set<InternArbeidsforholdRef>> entry : manglendeInntektsmeldinger.entrySet()) {
-                LeggTilResultat.leggTil(result, AksjonspunktÅrsak.MANGLENDE_INNTEKTSMELDING, entry.getKey(), entry.getValue());
-                LOGGER.info("Mangler inntektsmelding: arbeidsforholdRef={}", entry.getValue());
-            }
-        }
-        return result;
+    public DefaultManglendePåkrevdeInntektsmeldingerTjeneste() {
+        // CDI
     }
 
     @Override
@@ -121,12 +91,6 @@ public class DefaultManglendePåkrevdeInntektsmeldingerTjeneste implements Ytels
             return Stream.of(inntektsmelding.getArbeidsforholdRef()).collect(Collectors.toSet());
         }
         return Stream.of(InternArbeidsforholdRef.nullRef()).collect(Collectors.toSet());
-    }
-
-    private boolean erEndringssøknad(BehandlingReferanse referanse) {
-        return søknadRepository.hentSøknadHvisEksisterer(referanse.getBehandlingId())
-            .map(SøknadEntitet::erEndringssøknad)
-            .orElse(false);
     }
 
 }
