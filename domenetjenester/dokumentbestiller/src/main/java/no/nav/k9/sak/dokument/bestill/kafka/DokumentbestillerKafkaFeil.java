f5041a351f657bfc7408dbbcf83162190fbe248f
Marius Glittum <marius.glittum@nav.no>
2021-10-28 10:00:55 +0200

    Catch-rethrow istedenfor log og svelg exception

--- domenetjenester/dokumentbestiller/src/main/java/no/nav/k9/sak/dokument/bestill/kafka/DokumentbestillerKafkaFeil.java
+++ domenetjenester/dokumentbestiller/src/main/java/no/nav/k9/sak/dokument/bestill/kafka/DokumentbestillerKafkaFeil.java
@@ -1,16 +1,29 @@
 package no.nav.k9.sak.dokument.bestill.kafka;
 
+import org.apache.kafka.common.KafkaException;
+import org.apache.kafka.common.errors.RetriableException;
+
 import no.nav.k9.felles.feil.Feil;
 import no.nav.k9.felles.feil.FeilFactory;
 import no.nav.k9.felles.feil.LogLevel;
 import no.nav.k9.felles.feil.deklarasjon.DeklarerteFeil;
-import no.nav.k9.felles.feil.deklarasjon.TekniskFeil;
+import no.nav.k9.felles.feil.deklarasjon.IntegrasjonFeil;
+import no.nav.k9.felles.feil.deklarasjon.ManglerTilgangFeil;
 
 public interface DokumentbestillerKafkaFeil extends DeklarerteFeil {
 
     DokumentbestillerKafkaFeil FACTORY = FeilFactory.create(DokumentbestillerKafkaFeil.class);
 
-    @TekniskFeil(feilkode = "FP-533280", feilmelding = "Klarte ikke utlede ytelsetype: %s. Kan ikke bestille dokument", logLevel = LogLevel.ERROR)
-    Feil fantIkkeYtelseType(String ytelseType);
+    @ManglerTilgangFeil(feilkode = "K9-DOK-821005", feilmelding = "Feil i pålogging mot Kafka, topic:%s", logLevel = LogLevel.ERROR)
+    Feil feilIPålogging(String topic, Exception e);
+
+    @IntegrasjonFeil(feilkode = "K9-DOK-925469", feilmelding = "Uventet feil ved sending til Kafka, topic:%s", logLevel = LogLevel.WARN)
+    Feil uventetFeil(String topic, Exception e);
+
+    @IntegrasjonFeil(feilkode = "K9-DOK-127608", feilmelding = "Fikk transient feil mot Kafka, kan prøve igjen, topic:%s", logLevel = LogLevel.WARN)
+    Feil retriableExceptionMotKaka(String topic, RetriableException e);
+
+    @IntegrasjonFeil(feilkode = "K9-DOK-811208", feilmelding = "Fikk feil mot Kafka, topic:%s", logLevel = LogLevel.WARN)
+    Feil annenExceptionMotKafka(String topic, KafkaException e);
 
 }
