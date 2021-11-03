618a81942a0f10bad5a4cdbedfc57f5634c96808
Espen Svendsen <62875567+espennav@users.noreply.github.com>
2021-11-03 08:20:40 +0100

    Legger til søk på arbeidsforhold fra IM som skal brukes av Punsj (#5364)

--- web/src/main/java/no/nav/k9/sak/web/app/tjenester/behandling/arbeidsforhold/InntektArbeidYtelseRestTjeneste.java
+++ web/src/main/java/no/nav/k9/sak/web/app/tjenester/behandling/arbeidsforhold/InntektArbeidYtelseRestTjeneste.java
@@ -12,7 +12,9 @@ import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -27,16 +29,21 @@ import no.nav.k9.felles.sikkerhet.abac.BeskyttetRessurs;
 import no.nav.k9.felles.sikkerhet.abac.TilpassetAbacAttributt;
 import no.nav.k9.kodeverk.behandling.aksjonspunkt.AksjonspunktDefinisjon;
 import no.nav.k9.sak.behandling.BehandlingReferanse;
+import no.nav.k9.sak.behandling.FagsakTjeneste;
 import no.nav.k9.sak.behandling.Skjæringstidspunkt;
 import no.nav.k9.sak.behandlingslager.behandling.repository.BehandlingRepository;
 import no.nav.k9.sak.domene.arbeidsforhold.InntektArbeidYtelseTjeneste;
 import no.nav.k9.sak.domene.arbeidsforhold.impl.ArbeidsforholdAdministrasjonTjeneste;
 import no.nav.k9.sak.domene.arbeidsforhold.impl.ArbeidsforholdAdministrasjonTjeneste.UtledArbeidsforholdParametere;
 import no.nav.k9.sak.domene.iay.modell.InntektArbeidYtelseGrunnlag;
+import no.nav.k9.sak.domene.person.pdl.PersoninfoAdapter;
 import no.nav.k9.sak.kontrakt.arbeidsforhold.InntektArbeidYtelseArbeidsforholdV2Dto;
 import no.nav.k9.sak.kontrakt.behandling.BehandlingUuidDto;
+import no.nav.k9.sak.kontrakt.fagsak.MatchFagsak;
 import no.nav.k9.sak.kontrakt.frisinn.SøknadsperiodeOgOppgittOpptjeningV2Dto;
 import no.nav.k9.sak.skjæringstidspunkt.SkjæringstidspunktTjeneste;
+import no.nav.k9.sak.web.app.tjenester.fagsak.FagsakApplikasjonTjeneste;
+import no.nav.k9.sak.web.app.tjenester.fagsak.FagsakRestTjeneste;
 import no.nav.k9.sak.web.server.abac.AbacAttributtSupplier;
 
 @Produces(MediaType.APPLICATION_JSON)
@@ -46,6 +53,7 @@ import no.nav.k9.sak.web.server.abac.AbacAttributtSupplier;
 public class InntektArbeidYtelseRestTjeneste {
 
     public static final String INNTEKT_ARBEID_YTELSE_ARBEIDSFORHOLD_PATH = "/behandling/iay/arbeidsforhold-v2";
+    public static final String INNTEKT_ARBEID_YTELSE_INNTEKTSMELDINGER_PATH = "/behandling/iay/im-arbeidsforhold-v2";
 
     /** TODO: kun FRISINN som bruker denne? */
     public static final String OPPGITT_OPPTJENING_PATH_V2 = "/behandling/oppgitt-opptjening-v2";
@@ -59,6 +67,8 @@ public class InntektArbeidYtelseRestTjeneste {
     private MapOppgittOpptjening mapOppgittOpptjening;
 
     private InntektArbeidYtelseTjeneste iayTjeneste;
+    private FagsakTjeneste fagsakTjeneste;
+    private PersoninfoAdapter personinfoAdapter;
 
     public InntektArbeidYtelseRestTjeneste() {
         // for CDI proxy
@@ -69,12 +79,14 @@ public class InntektArbeidYtelseRestTjeneste {
                                            InntektArbeidYtelseTjeneste iayTjeneste,
                                            SkjæringstidspunktTjeneste skjæringstidspunktTjeneste,
                                            MapOppgittOpptjening mapOppgittOpptjening,
-                                           ArbeidsforholdAdministrasjonTjeneste arbeidsforholdAdministrasjonTjeneste) {
+                                           ArbeidsforholdAdministrasjonTjeneste arbeidsforholdAdministrasjonTjeneste, FagsakTjeneste fagsakTjeneste, FagsakApplikasjonTjeneste fagsakApplikasjonTjeneste, PersoninfoAdapter personinfoAdapter) {
         this.iayTjeneste = iayTjeneste;
         this.skjæringstidspunktTjeneste = skjæringstidspunktTjeneste;
         this.behandlingRepository = behandlingRepository;
         this.mapOppgittOpptjening = mapOppgittOpptjening;
         this.arbeidsforholdAdministrasjonTjeneste = arbeidsforholdAdministrasjonTjeneste;
+        this.fagsakTjeneste = fagsakTjeneste;
+        this.personinfoAdapter = personinfoAdapter;
     }
 
     @GET
@@ -100,6 +112,26 @@ public class InntektArbeidYtelseRestTjeneste {
 
         return arbeidsforholdAdministrasjonTjeneste.hentArbeidsforhold(ref, iayg, param);
     }
+
+    @POST
+    @Path(INNTEKT_ARBEID_YTELSE_INNTEKTSMELDINGER_PATH)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(description = "Hent arbeidsforhold fra inntektsmeldinger", summary = ("Returnerer arbeidsforhold oppgitt på inntektsmeldinger"), tags = "inntekt-arbeid-ytelse", responses = {
+        @ApiResponse(responseCode = "200", description = "Returnerer InntektArbeidYtelseDto, null hvis ikke eksisterer (GUI støtter ikke NOT_FOUND p.t.)", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = InntektArbeidYtelseArbeidsforholdV2Dto.class)))
+    })
+    @BeskyttetRessurs(action = READ, resource = FAGSAK)
+    @SuppressWarnings("findsecbugs:JAXRS_ENDPOINT")
+    public Set<InntektArbeidYtelseArbeidsforholdV2Dto> hentArbeidsforholdIdFraInntektsmeldinger(@Parameter(description = "Match-kritierer for å lete opp fagsaken med inntektsmeldinger") @Valid @TilpassetAbacAttributt(supplierClass = FagsakRestTjeneste.MatchFagsakAttributter.class) MatchFagsak dto) {
+        var aktørId = personinfoAdapter.hentAktørIdForPersonIdent(dto.getBruker()).orElseThrow(() -> new IllegalArgumentException("Finner ikke aktørId for bruker"));
+        var matchendeFagsak = fagsakTjeneste.finnesEnFagsakSomOverlapper(dto.getYtelseType(), aktørId, null, null, dto.getPeriode().getFom(), dto.getPeriode().getTom());
+        if (matchendeFagsak.isEmpty()) {
+            return Set.of();
+        }
+
+        return arbeidsforholdAdministrasjonTjeneste.hentArbeidsforholdFraInntektsmeldinger(matchendeFagsak.get().getSaksnummer());
+    }
+
 
     /** TODO: Brukes foreløpig bare av FRISINN. Fjernes, eller generaliseres? */
     @GET
