3d12906aabf6f6bfe3bc81b0eb9406ce1ca9c966
Espen Velsvik <espenjv@users.noreply.github.com>
2021-11-26 13:26:27 +0100

    Bedre logging historikk (#5536)

--- domenetjenester/behandling/src/main/java/no/nav/k9/sak/historikk/HistorikkInnsalgFeil.java
+++ domenetjenester/behandling/src/main/java/no/nav/k9/sak/historikk/HistorikkInnsalgFeil.java
@@ -3,6 +3,7 @@ package no.nav.k9.sak.historikk;
 import static no.nav.k9.felles.feil.LogLevel.ERROR;
 
 import java.util.List;
+import java.util.Set;
 
 import no.nav.k9.felles.feil.Feil;
 import no.nav.k9.felles.feil.FeilFactory;
@@ -15,8 +16,8 @@ interface HistorikkInnsalgFeil extends DeklarerteFeil {
     @TekniskFeil(feilkode = "FP-876694", feilmelding = "For type %s, mangler felter %s for historikkinnslag.", logLevel = ERROR)
     Feil manglerFeltForHistorikkInnslag(String type, List<String> manglendeFelt);
 
-    @TekniskFeil(feilkode = "FP-876693", feilmelding = "For type %s, forventer minst et felt av type %s", logLevel = ERROR)
-    Feil manglerMinstEtFeltForHistorikkinnslag(String type, List<String> manglendeFelt);
+    @TekniskFeil(feilkode = "FP-876693", feilmelding = "For type %s, forventer minst et felt av type %s. Eksisterende felttyper er %s", logLevel = ERROR)
+    Feil manglerMinstEtFeltForHistorikkinnslag(String type, List<String> manglendeFelt, Set<String> eksisterendeKoder);
 
     @TekniskFeil(feilkode = "FP-876692", feilmelding = "Ukjent historikkinnslagstype: %s", logLevel = ERROR)
     Feil ukjentHistorikkinnslagType(String kode);
