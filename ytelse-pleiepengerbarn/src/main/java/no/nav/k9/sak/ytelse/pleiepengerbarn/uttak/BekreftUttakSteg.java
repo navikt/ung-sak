bf9cb1b561c41132615a20d819651bc35b77121c
Marius Bækken Glittum <marius.glittum@nav.no>
2021-12-02 10:06:24 +0100

    Feature/bekreftet uttak (#5586)
    * Legger inn kall til uttak for "bekreftet-uttak"
    * Toggler kall til uttak

--- ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/uttak/BekreftUttakSteg.java
+++ ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/uttak/BekreftUttakSteg.java
@@ -1,10 +1,15 @@
 package no.nav.k9.sak.ytelse.pleiepengerbarn.uttak;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.NavigableSet;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+import no.nav.k9.felles.konfigurasjon.konfig.KonfigVerdi;
 import no.nav.k9.kodeverk.vilkår.Utfall;
 import no.nav.k9.kodeverk.vilkår.VilkårType;
 import no.nav.k9.sak.behandlingskontroll.BehandleStegResultat;
@@ -13,10 +18,16 @@ import no.nav.k9.sak.behandlingskontroll.BehandlingStegRef;
 import no.nav.k9.sak.behandlingskontroll.BehandlingTypeRef;
 import no.nav.k9.sak.behandlingskontroll.BehandlingskontrollKontekst;
 import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
+import no.nav.k9.sak.behandlingslager.behandling.repository.BehandlingRepository;
 import no.nav.k9.sak.behandlingslager.behandling.vilkår.Vilkår;
 import no.nav.k9.sak.behandlingslager.behandling.vilkår.VilkårResultatRepository;
+import no.nav.k9.sak.behandlingslager.behandling.vilkår.periode.VilkårPeriode;
 import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
 import no.nav.k9.sak.perioder.VilkårsPerioderTilVurderingTjeneste;
+import no.nav.k9.sak.ytelse.pleiepengerbarn.uttak.tjeneste.UttakTjeneste;
+import no.nav.pleiepengerbarn.uttak.kontrakter.EndrePerioderGrunnlag;
+import no.nav.pleiepengerbarn.uttak.kontrakter.LukketPeriode;
+import no.nav.pleiepengerbarn.uttak.kontrakter.Årsak;
 
 @ApplicationScoped
 @BehandlingStegRef(kode = "BEKREFT_UTTAK")
@@ -24,17 +35,27 @@ import no.nav.k9.sak.perioder.VilkårsPerioderTilVurderingTjeneste;
 @FagsakYtelseTypeRef("PSB")
 public class BekreftUttakSteg implements BehandlingSteg {
 
+    private BehandlingRepository behandlingRepository;
     private VilkårResultatRepository vilkårResultatRepository;
+    private UttakTjeneste uttakTjeneste;
     private VilkårsPerioderTilVurderingTjeneste perioderTilVurderingTjeneste;
+    private Boolean enableBekreftUttak;
 
     BekreftUttakSteg() {
         // CDI
     }
 
     @Inject
-    private BekreftUttakSteg(VilkårResultatRepository vilkårResultatRepository, @FagsakYtelseTypeRef("PSB") @BehandlingTypeRef VilkårsPerioderTilVurderingTjeneste perioderTilVurderingTjeneste) {
+    private BekreftUttakSteg(BehandlingRepository behandlingRepository,
+                             VilkårResultatRepository vilkårResultatRepository,
+                             UttakTjeneste uttakTjeneste,
+                             @FagsakYtelseTypeRef("PSB") @BehandlingTypeRef VilkårsPerioderTilVurderingTjeneste perioderTilVurderingTjeneste,
+                             @KonfigVerdi(value = "psb.enable.bekreft.uttak", defaultVerdi = "false") Boolean enableBekreftUttak) {
+        this.behandlingRepository = behandlingRepository;
         this.vilkårResultatRepository = vilkårResultatRepository;
+        this.uttakTjeneste = uttakTjeneste;
         this.perioderTilVurderingTjeneste = perioderTilVurderingTjeneste;
+        this.enableBekreftUttak = enableBekreftUttak;
     }
 
     @Override
@@ -45,14 +66,31 @@ public class BekreftUttakSteg implements BehandlingSteg {
 
         var beregningsgrunnlagsvilkåret = vilkårene.getVilkår(VilkårType.BEREGNINGSGRUNNLAGVILKÅR).orElseThrow();
 
-        if (harNoenPerioderTilVurderingBlittAvslåttIBeregning(perioderTilVurdering, beregningsgrunnlagsvilkåret)) {
-            // TODO: Kall på uttaket for å "ajurholde"
+        var perioderSomHarBlittAvslått = harNoenPerioderTilVurderingBlittAvslåttIBeregning(perioderTilVurdering, beregningsgrunnlagsvilkåret);
+
+        if (!perioderSomHarBlittAvslått.isEmpty() && enableBekreftUttak) {
+            var behandling = behandlingRepository.hentBehandling(kontekst.getBehandlingId());
+
+            var request = new EndrePerioderGrunnlag(behandling.getFagsak().getSaksnummer().getVerdi(), behandling.getUuid().toString(), opprettMap(perioderSomHarBlittAvslått));
+            uttakTjeneste.endreUttaksplan(request);
         }
 
         return BehandleStegResultat.utførtUtenAksjonspunkter();
     }
 
-    private boolean harNoenPerioderTilVurderingBlittAvslåttIBeregning(NavigableSet<DatoIntervallEntitet> perioderTilVurdering, Vilkår beregningsgrunnlagsvilkåret) {
-        return beregningsgrunnlagsvilkåret.getPerioder().stream().anyMatch(it -> Utfall.IKKE_OPPFYLT.equals(it.getUtfall()) && perioderTilVurdering.stream().anyMatch(at -> at.overlapper(it.getPeriode())));
+    private Map<LukketPeriode, Årsak> opprettMap(NavigableSet<DatoIntervallEntitet> perioderSomHarBlittAvslått) {
+        var map = new HashMap<LukketPeriode, Årsak>();
+        for (DatoIntervallEntitet periode : perioderSomHarBlittAvslått) {
+            map.put(new LukketPeriode(periode.getFomDato(), periode.getTomDato()), Årsak.FOR_LAV_INNTEKT);
+        }
+        return map;
+    }
+
+    private NavigableSet<DatoIntervallEntitet> harNoenPerioderTilVurderingBlittAvslåttIBeregning(NavigableSet<DatoIntervallEntitet> perioderTilVurdering, Vilkår beregningsgrunnlagsvilkåret) {
+        return beregningsgrunnlagsvilkåret.getPerioder()
+            .stream()
+            .filter(it -> Utfall.IKKE_OPPFYLT.equals(it.getUtfall()) && perioderTilVurdering.stream().anyMatch(at -> at.overlapper(it.getPeriode())))
+            .map(VilkårPeriode::getPeriode)
+            .collect(Collectors.toCollection(TreeSet::new));
     }
 }
--- ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/uttak/tjeneste/UttakTjeneste.java
+++ ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/uttak/tjeneste/UttakTjeneste.java
@@ -2,6 +2,7 @@ package no.nav.k9.sak.ytelse.pleiepengerbarn.uttak.tjeneste;
 
 import java.util.UUID;
 
+import no.nav.pleiepengerbarn.uttak.kontrakter.EndrePerioderGrunnlag;
 import no.nav.pleiepengerbarn.uttak.kontrakter.Simulering;
 import no.nav.pleiepengerbarn.uttak.kontrakter.Uttaksgrunnlag;
 import no.nav.pleiepengerbarn.uttak.kontrakter.Uttaksplan;
@@ -11,8 +12,10 @@ public interface UttakTjeneste {
     Uttaksplan hentUttaksplan(UUID behandlingId, boolean slåSammenLikePerioder);
 
     Uttaksplan opprettUttaksplan(Uttaksgrunnlag request);
-    
+
     Simulering simulerUttaksplan(Uttaksgrunnlag request);
+
+    Uttaksplan endreUttaksplan(EndrePerioderGrunnlag request);
 
     void slettUttaksplan(UUID behandlingId);
 
