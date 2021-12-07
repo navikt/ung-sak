a123556dffd8d97b2fb76a8a575e6677cd041ac2
Marius Glittum <marius.glittum@nav.no>
2021-12-07 12:07:49 +0100

    Rydder bort unødvendig logging

--- ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/repo/uttak/SamtidigUttakTjeneste.java
+++ ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/repo/uttak/SamtidigUttakTjeneste.java
@@ -1,6 +1,5 @@
 package no.nav.k9.sak.ytelse.pleiepengerbarn.repo.uttak;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,7 +26,6 @@ import no.nav.k9.sak.behandlingslager.behandling.repository.BehandlingRepository
 import no.nav.k9.sak.behandlingslager.fagsak.Fagsak;
 import no.nav.k9.sak.behandlingslager.fagsak.FagsakRepository;
 import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
-import no.nav.k9.sak.domene.typer.tid.JsonObjectMapper;
 import no.nav.k9.sak.ytelse.pleiepengerbarn.inngangsvilkår.søknadsfrist.PleietrengendeKravprioritet;
 import no.nav.k9.sak.ytelse.pleiepengerbarn.uttak.BekreftetUttakTjeneste;
 import no.nav.k9.sak.ytelse.pleiepengerbarn.uttak.input.MapInputTilUttakTjeneste;
@@ -194,11 +192,7 @@ public class SamtidigUttakTjeneste {
             Simuleringsgrunnlag simuleringsgrunnlag = byggRequest(ref, uttaksgrunnlag);
             var simulering = uttakTjeneste.simulerUttaksplanV2(simuleringsgrunnlag);
             if (enableAvslagBeregning && (Environment.current().isDev() || Environment.current().isLocal())) {
-                try {
-                    log.info("Simulering harForrigeUttaksplan={} endret={} \n respons: {}", (simulering.getForrigeUttaksplan() != null), simulering.getUttakplanEndret(), JsonObjectMapper.getJson(simulering));
-                } catch (IOException e) {
-                    log.info("Feil ved deserialisering. {}", e.getMessage());
-                }
+                log.info("Simulering harForrigeUttaksplan={} endret={}", (simulering.getForrigeUttaksplan() != null), simulering.getUttakplanEndret());
             }
             return simulering;
         } else {
@@ -213,11 +207,7 @@ public class SamtidigUttakTjeneste {
             Simuleringsgrunnlag simuleringsgrunnlag = byggRequest(ref, uttaksGrunnlag);
             var simulering = uttakTjeneste.simulerUttaksplanV2(simuleringsgrunnlag);
             if (enableAvslagBeregning && (Environment.current().isDev() || Environment.current().isLocal())) {
-                try {
-                    log.info("Simulering harForrigeUttaksplan={} endret={} \n respons: {}", (simulering.getForrigeUttaksplan() != null), simulering.getUttakplanEndret(), JsonObjectMapper.getJson(simulering));
-                } catch (IOException e) {
-                    log.info("Feil ved deserialisering. {}", e.getMessage());
-                }
+                log.info("Simulering harForrigeUttaksplan={} endret={}", (simulering.getForrigeUttaksplan() != null), simulering.getUttakplanEndret());
             }
             return simulering;
         } else {
