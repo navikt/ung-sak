2303dc8205918d7cc6efe8954d81f7fe1f5a973d
Ole Jørgen Rishoff <olejri@gmail.com>
2021-12-14 14:37:02 +0100

    Endret til post

--- web/src/main/java/no/nav/k9/sak/web/app/tjenester/punsj/PunsjRestTjeneste.java
+++ web/src/main/java/no/nav/k9/sak/web/app/tjenester/punsj/PunsjRestTjeneste.java
@@ -9,15 +9,15 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import javax.ws.rs.GET;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -26,7 +26,6 @@ import no.nav.k9.felles.sikkerhet.abac.TilpassetAbacAttributt;
 import no.nav.k9.sak.kontrakt.FeilDto;
 import no.nav.k9.sak.kontrakt.FeilType;
 import no.nav.k9.sak.kontrakt.dokument.JournalpostIderDto;
-import no.nav.k9.sak.kontrakt.person.AktørIdDto;
 import no.nav.k9.sak.punsj.PunsjRestKlient;
 import no.nav.k9.sak.web.server.abac.AbacAttributtSupplier;
 
@@ -47,25 +46,23 @@ public class PunsjRestTjeneste {
         this.klient = klient;
     }
 
-    @GET
+    @POST
     @Operation(description = "Henter uferdig journalposter fra punsj for en gitt aktørId", tags = "aktoer", responses = {
             @ApiResponse(responseCode = "200", description = "Returnerer en liste med uferdig journalpostIder som ligger i punsj på gitt aktørId.", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = JournalpostIderDto.class)))
     })
     @BeskyttetRessurs(action = READ, resource = FAGSAK)
     @Path("/journalpost/uferdig")
+    @Consumes(MediaType.APPLICATION_JSON)
     @SuppressWarnings("findsecbugs:JAXRS_ENDPOINT")
-    public Response getUferdigJournalpostIderPrAktoer(
-        @NotNull @QueryParam("aktoerId") @Valid @TilpassetAbacAttributt(supplierClass = AbacAttributtSupplier.class) AktørIdDto aktørIdDto,
-        @QueryParam("aktoerIdBarn") @Valid @TilpassetAbacAttributt(supplierClass = AbacAttributtSupplier.class) AktørIdDto aktørIdDtoBarn
-    ) {
-        if (aktørIdDto != null && aktørIdDto.getAktorId() != null) {
-            Optional<JournalpostIderDto> uferdigJournalpostIderPåAktør = klient.getUferdigJournalpostIderPåAktør(aktørIdDto.getAktorId(), aktørIdDtoBarn != null ? aktørIdDtoBarn.getAktorId() : null);
+    public Response getUferdigJournalpostIderPrAktoer(@Parameter(description = "Inneholder kode til brevmal og data som skal flettes inn i brevet") @Valid @TilpassetAbacAttributt(supplierClass = AbacAttributtSupplier.class) UferdigJournalpostSøkDto dto) {
+        if (dto.getAktorIdDto() != null && dto.getAktorIdDto().getAktorId() != null) {
+            Optional<JournalpostIderDto> uferdigJournalpostIderPåAktør = klient.getUferdigJournalpostIderPåAktør(dto.getAktorIdDto().getAktorId(), dto.getAktorIdDtoBarn() != null ? dto.getAktorIdDtoBarn().getAktorId() : null);
             if (uferdigJournalpostIderPåAktør.isPresent()) {
                 return Response.ok(uferdigJournalpostIderPåAktør.get()).build();
             }
             return Response.ok().build();
         } else {
-            FeilDto feilDto = new FeilDto(FeilType.GENERELL_FEIL, "Query parameteret 'aktoerId' mangler i forespørselen.");
+            FeilDto feilDto = new FeilDto(FeilType.GENERELL_FEIL, "'aktoerId' mangler i bodyen.");
             return Response.ok(feilDto).status(400).build();
         }
     }
