ee8bf0b510a228404911ca8c5bf2af1cca97fb23
Marius Glittum <marius.glittum@nav.no>
2021-12-17 15:51:19 +0100

    Glemte visst noen klasser

---
+++ domenetjenester/behandling/src/main/java/no/nav/k9/sak/perioder/DefaultForlengelseTjeneste.java
@@ -0,0 +1,29 @@
+package no.nav.k9.sak.perioder;
+
+
+import java.util.NavigableSet;
+import java.util.TreeSet;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import no.nav.k9.kodeverk.vilkår.VilkårType;
+import no.nav.k9.sak.behandling.BehandlingReferanse;
+import no.nav.k9.sak.behandlingskontroll.BehandlingTypeRef;
+import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
+import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
+
+/**
+ * Ingen forlengelse
+ *
+ * Aktuelt for førstegangsbehandling etc.
+ */
+@FagsakYtelseTypeRef
+@BehandlingTypeRef
+@ApplicationScoped
+public class DefaultForlengelseTjeneste implements ForlengelseTjeneste {
+
+    @Override
+    public NavigableSet<DatoIntervallEntitet> utledPerioderSomSkalBehandlesSomForlengelse(BehandlingReferanse referanse, NavigableSet<DatoIntervallEntitet> perioderTilVurdering, VilkårType vilkårType) {
+        return new TreeSet<>();
+    }
+}
