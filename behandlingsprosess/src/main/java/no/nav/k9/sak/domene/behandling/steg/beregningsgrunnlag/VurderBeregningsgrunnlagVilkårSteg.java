7485472ff4ec2cf2acbe8f79cc826c67769d75bf
Marius Bækken Glittum <marius.glittum@nav.no>
2021-12-22 14:13:38 +0100

    Flytter rydding av vilkår til vurder vilkår steget (#5817)

--- behandlingsprosess/src/main/java/no/nav/k9/sak/domene/behandling/steg/beregningsgrunnlag/VurderBeregningsgrunnlagVilkårSteg.java
+++ behandlingsprosess/src/main/java/no/nav/k9/sak/domene/behandling/steg/beregningsgrunnlag/VurderBeregningsgrunnlagVilkårSteg.java
@@ -58,6 +58,14 @@ public class VurderBeregningsgrunnlagVilkårSteg implements BeregningsgrunnlagSt
         return BehandleStegResultat.utførtMedAksjonspunktResultater(callback.aksjonspunktResultater);
     }
 
+    @Override
+    public void vedHoppOverBakover(BehandlingskontrollKontekst kontekst, BehandlingStegModell modell, BehandlingStegType tilSteg, BehandlingStegType fraSteg) {
+        Behandling behandling = behandlingRepository.hentBehandling(kontekst.getBehandlingId());
+        var ref = BehandlingReferanse.fra(behandling);
+        beregningsgrunnlagVilkårTjeneste.utledPerioderTilVurdering(ref, false)
+            .forEach(periode -> beregningsgrunnlagVilkårTjeneste.ryddVedtaksresultatOgVilkår(kontekst, periode));
+    }
+
     class HåndterResultat implements FortsettBeregningResultatCallback {
 
         private List<AksjonspunktResultat> aksjonspunktResultater = new ArrayList<>();
