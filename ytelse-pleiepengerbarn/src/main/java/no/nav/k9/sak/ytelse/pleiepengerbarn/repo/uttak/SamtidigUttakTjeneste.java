237384ea18c45a3f6812db7ec867031ab82c2842
Marius Glittum <marius.glittum@nav.no>
2022-01-03 21:15:02 +0100

    Fjerner ubrukt klasse

--- ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/repo/uttak/SamtidigUttakTjeneste.java
+++ ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/repo/uttak/SamtidigUttakTjeneste.java
@@ -20,7 +20,6 @@ import no.nav.k9.sak.behandlingslager.behandling.repository.BehandlingRepository
 import no.nav.k9.sak.behandlingslager.fagsak.Fagsak;
 import no.nav.k9.sak.behandlingslager.fagsak.FagsakRepository;
 import no.nav.k9.sak.ytelse.pleiepengerbarn.inngangsvilkår.søknadsfrist.PleietrengendeKravprioritet;
-import no.nav.k9.sak.ytelse.pleiepengerbarn.uttak.BekreftetUttakTjeneste;
 import no.nav.k9.sak.ytelse.pleiepengerbarn.uttak.input.MapInputTilUttakTjeneste;
 import no.nav.k9.sak.ytelse.pleiepengerbarn.uttak.tjeneste.UttakTjeneste;
 import no.nav.pleiepengerbarn.uttak.kontrakter.Simulering;
@@ -36,7 +35,6 @@ public class SamtidigUttakTjeneste {
     private BehandlingRepository behandlingRepository;
     private BehandlingModellRepository behandlingModellRepository;
     private PleietrengendeKravprioritet pleietrengendeKravprioritet;
-    private BekreftetUttakTjeneste bekreftetUttakTjeneste;
 
 
     @Inject
@@ -45,15 +43,13 @@ public class SamtidigUttakTjeneste {
                                  FagsakRepository fagsakRepository,
                                  BehandlingRepository behandlingRepository,
                                  BehandlingModellRepository behandlingModellRepository,
-                                 PleietrengendeKravprioritet pleietrengendeKravprioritet,
-                                 BekreftetUttakTjeneste bekreftetUttakTjeneste) {
+                                 PleietrengendeKravprioritet pleietrengendeKravprioritet) {
         this.mapInputTilUttakTjeneste = mapInputTilUttakTjeneste;
         this.uttakTjeneste = uttakTjeneste;
         this.fagsakRepository = fagsakRepository;
         this.behandlingRepository = behandlingRepository;
         this.behandlingModellRepository = behandlingModellRepository;
         this.pleietrengendeKravprioritet = pleietrengendeKravprioritet;
-        this.bekreftetUttakTjeneste = bekreftetUttakTjeneste;
     }
 
 
