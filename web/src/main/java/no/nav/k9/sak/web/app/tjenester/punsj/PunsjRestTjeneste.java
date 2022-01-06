cead621ba42d456c75fc83bf7de0ac2d42ff9ba7
Ole Jørgen Rishoff <olejri@gmail.com>
2022-01-06 17:54:08 +0100

    FIX: Endret til å slå opp via BehandlingUuidDto

--- web/src/main/java/no/nav/k9/sak/web/app/tjenester/punsj/PunsjRestTjeneste.java
+++ web/src/main/java/no/nav/k9/sak/web/app/tjenester/punsj/PunsjRestTjeneste.java
@@ -18,6 +18,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -25,7 +26,7 @@ import no.nav.k9.felles.sikkerhet.abac.BeskyttetRessurs;
 import no.nav.k9.felles.sikkerhet.abac.TilpassetAbacAttributt;
 import no.nav.k9.sak.behandling.prosessering.BehandlingsprosessApplikasjonTjeneste;
 import no.nav.k9.sak.behandlingslager.behandling.Behandling;
-import no.nav.k9.sak.kontrakt.behandling.BehandlingIdDto;
+import no.nav.k9.sak.kontrakt.behandling.BehandlingUuidDto;
 import no.nav.k9.sak.kontrakt.dokument.JournalpostIderDto;
 import no.nav.k9.sak.punsj.PunsjRestKlient;
 import no.nav.k9.sak.typer.AktørId;
@@ -51,21 +52,22 @@ public class PunsjRestTjeneste {
     }
 
     @GET
-    @Operation(description = "Henter uferdig journalposter fra punsj for en gitt aktørId", tags = "aktoer", responses = {
-        @ApiResponse(responseCode = "200", description = "Returnerer en liste med uferdig journalpostIder som ligger i punsj på gitt aktørId.", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = JournalpostIderDto.class)))
+    @Path("/journalpost/uferdig")
+    @Operation(description = "Henter uferdig journalposter fra punsj for en gitt behandlingUuid", tags = "journalposter", responses = {
+        @ApiResponse(responseCode = "200", description = "Returnerer en liste med uferdig journalpostIder som ligger i punsj på gitt behandlingUuid.",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON,
+                schema = @Schema(implementation = JournalpostIderDto.class)))
     })
     @BeskyttetRessurs(action = READ, resource = FAGSAK)
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("/journalpost/uferdig")
     @SuppressWarnings("findsecbugs:JAXRS_ENDPOINT")
-    public Response getUferdigJournalpostIderPrAktoer(@NotNull @QueryParam("behandlingId") @Valid @TilpassetAbacAttributt(supplierClass = AbacAttributtSupplier.class) BehandlingIdDto behandlingIdDto) {
-        Behandling behandling = behandlingsprosessTjeneste.hentBehandling(behandlingIdDto.getBehandlingUuid());
+    public Response getUferdigJournalpostIderPrAktoer(@NotNull @QueryParam(BehandlingUuidDto.NAME) @Parameter(description = BehandlingUuidDto.DESC) @Valid @TilpassetAbacAttributt(supplierClass = AbacAttributtSupplier.class) BehandlingUuidDto behandlingUuid) {
+        Behandling behandling = behandlingsprosessTjeneste.hentBehandling(behandlingUuid.getBehandlingUuid());
         AktørId søker = behandling.getAktørId();
         AktørId barnet = behandling.getFagsak().getPleietrengendeAktørId();
         Optional<JournalpostIderDto> uferdigJournalpostIderPåAktør = klient.getUferdigJournalpostIderPåAktør(søker.getAktørId(), barnet.getAktørId());
-            if (uferdigJournalpostIderPåAktør.isPresent()) {
-                return Response.ok(uferdigJournalpostIderPåAktør.get()).build();
-            }
-            return Response.ok().build();
+        if (uferdigJournalpostIderPåAktør.isPresent()) {
+            return Response.ok(uferdigJournalpostIderPåAktør.get()).build();
+        }
+        return Response.ok().build();
     }
 }
