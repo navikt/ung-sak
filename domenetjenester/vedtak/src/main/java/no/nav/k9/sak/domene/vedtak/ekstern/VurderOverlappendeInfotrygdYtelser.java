b0d82ec08742b0c1f4d9e74f240f7dcf7ea6db97
s149212 <espen.svendsen@nav.no>
2022-01-11 08:53:10 +0100

    Legger på info om ytelse anvist for overlappsjekk

--- domenetjenester/vedtak/src/main/java/no/nav/k9/sak/domene/vedtak/ekstern/VurderOverlappendeInfotrygdYtelser.java
+++ domenetjenester/vedtak/src/main/java/no/nav/k9/sak/domene/vedtak/ekstern/VurderOverlappendeInfotrygdYtelser.java
@@ -62,7 +62,7 @@ public class VurderOverlappendeInfotrygdYtelser {
         for (Map.Entry<Ytelse, NavigableSet<LocalDateInterval>> entry : overlappendeEksterneYtelser.entrySet()) {
             var ytelse = entry.getKey();
             var perioder = entry.getValue();
-            var beskrivelse = "Ytelse=" + ytelse.getYtelseType() + ", saksnummer=" + ytelse.getSaksnummer() + ", perioder=" + perioder;
+            var beskrivelse = "Ytelse=" + ytelse.getYtelseType() + ", saksnummer=" + ytelse.getSaksnummer() +  ", antall ytelser anvist=" + ytelse.getYtelseAnvist().size() + ", perioder=" + perioder;
 
             var oppgaveId = switch (ytelse.getYtelseType()) {
                 case SYKEPENGER -> oppgaveTjeneste.opprettVkyOppgaveOverlappendeYtelse(ref, beskrivelse, SP_OPPG_TEMA, BEH_TYPE_SAMHANDLING, SP_ANSV_ENHET_ID);
