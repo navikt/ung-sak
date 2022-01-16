a5be62d78933c12080645e651b385cba8bde33c0
Frode Carlsen <frode.odde.carlsen@nav.no>
2022-01-16 11:05:50 +0000

    Oppdatert JASPI Opprett btree_gist extension for unit test og lokal test dber Fiks merge feil exclude javax.xml.bind:bind-api

--- domenetjenester/vedtak/src/main/java/no/nav/k9/sak/hendelse/vedtak/VurderOmVedtakPåvirkerAndreSakerTask.java
+++ domenetjenester/vedtak/src/main/java/no/nav/k9/sak/hendelse/vedtak/VurderOmVedtakPåvirkerAndreSakerTask.java
@@ -3,6 +3,7 @@ package no.nav.k9.sak.hendelse.vedtak;
 import java.util.NavigableSet;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import jakarta.enterprise.context.ApplicationScoped;
@@ -28,64 +29,66 @@ import no.nav.k9.sak.domene.typer.tid.JsonObjectMapper;
 @FagsakProsesstaskRekkefølge(gruppeSekvens = false)
 public class VurderOmVedtakPåvirkerAndreSakerTask implements ProsessTaskHandler {
 
-    public static final String TASKNAME = "iverksetteVedtak.vurderRevurderingAndreSøknader";
-    private static final MdcExtendedLogContext LOG_CONTEXT = MdcExtendedLogContext.getContext("prosess");
-    private static final Logger log = LoggerFactory.getLogger(VurderOmVedtakPåvirkerAndreSakerTask.class);
+        public static final String TASKNAME = "iverksetteVedtak.vurderRevurderingAndreSøknader";
+        private static final MdcExtendedLogContext LOG_CONTEXT = MdcExtendedLogContext.getContext("prosess");
+        private static final Logger log = LoggerFactory.getLogger(VurderOmVedtakPåvirkerAndreSakerTask.class);
 
-    private BehandlingRepository behandlingRepository;
-    private FagsakRepository fagsakRepository;
-    private FagsakProsessTaskRepository fagsakProsessTaskRepository;
-    private Boolean persisterPerioderMedDiff;
+        private BehandlingRepository behandlingRepository;
+        private FagsakRepository fagsakRepository;
+        private FagsakProsessTaskRepository fagsakProsessTaskRepository;
+        private Boolean persisterPerioderMedDiff;
 
-    VurderOmVedtakPåvirkerAndreSakerTask() {
-    }
-
-    @Inject
-    public VurderOmVedtakPåvirkerAndreSakerTask(BehandlingRepository behandlingRepository,
-            FagsakRepository fagsakRepository,
-            FagsakProsessTaskRepository fagsakProsessTaskRepository,
-            @KonfigVerdi(value = "VEDTAK_PERSISTER_ENDRET_PERIODE", defaultVerdi = "false") Boolean persisterPerioderMedDiff) {
-        this.behandlingRepository = behandlingRepository;
-        this.fagsakRepository = fagsakRepository;
-        this.fagsakProsessTaskRepository = fagsakProsessTaskRepository;
-        this.persisterPerioderMedDiff = persisterPerioderMedDiff;
-    }
-
-    @Override
-    public void doTask(ProsessTaskData prosessTaskData) {
-        var vedtakHendelse = JsonObjectMapper.fromJson(prosessTaskData.getPayloadAsString(), Ytelse.class);
-        var fagsakYtelseType = FagsakYtelseType.fromString(vedtakHendelse.getType().getKode());
-        LOG_CONTEXT.add("ytelseType", fagsakYtelseType);
-
-        var vurderOmVedtakPåvirkerSakerTjeneste = VurderOmVedtakPåvirkerSakerTjeneste.finnTjeneste(fagsakYtelseType);
-        var kandidaterTilRevurdering = vurderOmVedtakPåvirkerSakerTjeneste
-                .utledSakerMedPerioderSomErKanVærePåvirket(vedtakHendelse);
-
-        log.info(
-                "Etter '{}' vedtak på saksnummer='{}', skal følgende saker '{}' som skal revurderes som følge av vedtak.",
-                fagsakYtelseType, vedtakHendelse.getSaksnummer(), kandidaterTilRevurdering);
-
-        for (SakMedPeriode kandidatsaksnummer : kandidaterTilRevurdering) {
-            ProsessTaskData tilRevurderingTaskData = new ProsessTaskData(
-                    OpprettRevurderingEllerOpprettDiffTask.TASKNAME);
-            tilRevurderingTaskData.setProperty(OpprettRevurderingEllerOpprettDiffTask.BEHANDLING_ÅRSAK,
-                    BehandlingÅrsakType.RE_ENDRING_FRA_ANNEN_OMSORGSPERSON.getKode());
-            if (persisterPerioderMedDiff) {
-                tilRevurderingTaskData.setProperty(OpprettRevurderingEllerOpprettDiffTask.PERIODER,
-                        utledPerioder(kandidatsaksnummer.getPerioder()));
-            }
-            var fagsak = fagsakRepository.hentSakGittSaksnummer(kandidatsaksnummer.getSaksnummer(), false)
-                    .orElseThrow();
-            var tilRevurdering = behandlingRepository.hentSisteYtelsesBehandlingForFagsakId(fagsak.getId())
-                    .orElseThrow();
-            tilRevurderingTaskData.setBehandling(tilRevurdering.getFagsakId(), tilRevurdering.getId(),
-                    tilRevurdering.getAktørId().getId());
-
-            fagsakProsessTaskRepository.lagreNyGruppe(tilRevurderingTaskData);
+        VurderOmVedtakPåvirkerAndreSakerTask() {
         }
-    }
 
-    private String utledPerioder(NavigableSet<DatoIntervallEntitet> perioder) {
-        return perioder.stream().map(it -> it.getFomDato() + "/" + it.getTomDato()).collect(Collectors.joining("|"));
-    }
+        @Inject
+        public VurderOmVedtakPåvirkerAndreSakerTask(BehandlingRepository behandlingRepository,
+                        FagsakRepository fagsakRepository,
+                        FagsakProsessTaskRepository fagsakProsessTaskRepository,
+                        @KonfigVerdi(value = "VEDTAK_PERSISTER_ENDRET_PERIODE", defaultVerdi = "false") Boolean persisterPerioderMedDiff) {
+                this.behandlingRepository = behandlingRepository;
+                this.fagsakRepository = fagsakRepository;
+                this.fagsakProsessTaskRepository = fagsakProsessTaskRepository;
+                this.persisterPerioderMedDiff = persisterPerioderMedDiff;
+        }
+
+        @Override
+        public void doTask(ProsessTaskData prosessTaskData) {
+                var vedtakHendelse = JsonObjectMapper.fromJson(prosessTaskData.getPayloadAsString(), Ytelse.class);
+                var fagsakYtelseType = FagsakYtelseType.fromString(vedtakHendelse.getType().getKode());
+                LOG_CONTEXT.add("ytelseType", fagsakYtelseType);
+
+                var vurderOmVedtakPåvirkerSakerTjeneste = VurderOmVedtakPåvirkerSakerTjeneste
+                                .finnTjeneste(fagsakYtelseType);
+                var kandidaterTilRevurdering = vurderOmVedtakPåvirkerSakerTjeneste
+                                .utledSakerMedPerioderSomErKanVærePåvirket(vedtakHendelse);
+
+                log.info(
+                                "Etter '{}' vedtak på saksnummer='{}', skal følgende saker '{}' som skal revurderes som følge av vedtak.",
+                                fagsakYtelseType, vedtakHendelse.getSaksnummer(), kandidaterTilRevurdering);
+
+                for (SakMedPeriode kandidatsaksnummer : kandidaterTilRevurdering) {
+                        ProsessTaskData tilRevurderingTaskData = new ProsessTaskData(
+                                        OpprettRevurderingEllerOpprettDiffTask.TASKNAME);
+                        tilRevurderingTaskData.setProperty(OpprettRevurderingEllerOpprettDiffTask.BEHANDLING_ÅRSAK,
+                                        BehandlingÅrsakType.RE_ENDRING_FRA_ANNEN_OMSORGSPERSON.getKode());
+                        if (persisterPerioderMedDiff) {
+                                tilRevurderingTaskData.setProperty(OpprettRevurderingEllerOpprettDiffTask.PERIODER,
+                                                utledPerioder(kandidatsaksnummer.getPerioder()));
+                        }
+                        var fagsak = fagsakRepository.hentSakGittSaksnummer(kandidatsaksnummer.getSaksnummer(), false)
+                                        .orElseThrow();
+                        var tilRevurdering = behandlingRepository.hentSisteYtelsesBehandlingForFagsakId(fagsak.getId())
+                                        .orElseThrow();
+                        tilRevurderingTaskData.setBehandling(tilRevurdering.getFagsakId(), tilRevurdering.getId(),
+                                        tilRevurdering.getAktørId().getId());
+
+                        fagsakProsessTaskRepository.lagreNyGruppe(tilRevurderingTaskData);
+                }
+        }
+
+        private String utledPerioder(NavigableSet<DatoIntervallEntitet> perioder) {
+                return perioder.stream().map(it -> it.getFomDato() + "/" + it.getTomDato())
+                                .collect(Collectors.joining("|"));
+        }
 }
