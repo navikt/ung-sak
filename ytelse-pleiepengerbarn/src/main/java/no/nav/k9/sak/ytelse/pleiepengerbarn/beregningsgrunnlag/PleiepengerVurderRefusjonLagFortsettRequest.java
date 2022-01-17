608c0cd5513078e86deed5a0ee7a9b2067085cab
Espen Johansen Velsvik <espen.velsvik@nav.no>
2022-01-17 19:01:55 +0100

    Sender med full input ved oppdatering av utbetalingsgrader til kalkulus

--- ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/beregningsgrunnlag/PleiepengerVurderRefusjonLagFortsettRequest.java
+++ ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/beregningsgrunnlag/PleiepengerVurderRefusjonLagFortsettRequest.java
@@ -14,17 +14,26 @@ import javax.enterprise.inject.Any;
 import javax.inject.Inject;
 
 import no.nav.folketrygdloven.beregningsgrunnlag.BgRef;
+import no.nav.folketrygdloven.beregningsgrunnlag.kalkulus.KalkulatorInputTjeneste;
 import no.nav.folketrygdloven.beregningsgrunnlag.kalkulus.LagFortsettRequest;
 import no.nav.folketrygdloven.kalkulus.beregning.v1.YtelsespesifiktGrunnlagDto;
+import no.nav.folketrygdloven.kalkulus.felles.v1.KalkulatorInputDto;
 import no.nav.folketrygdloven.kalkulus.kodeverk.StegType;
 import no.nav.folketrygdloven.kalkulus.kodeverk.YtelseTyperKalkulusStøtterKontrakt;
 import no.nav.folketrygdloven.kalkulus.request.v1.FortsettBeregningListeRequest;
 import no.nav.k9.kodeverk.behandling.BehandlingStegType;
+import no.nav.k9.kodeverk.vilkår.VilkårType;
+import no.nav.k9.kodeverk.vilkår.VilkårUtfallMerknad;
 import no.nav.k9.sak.behandling.BehandlingReferanse;
 import no.nav.k9.sak.behandlingskontroll.BehandlingStegRef;
 import no.nav.k9.sak.behandlingskontroll.BehandlingTypeRef;
 import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
+import no.nav.k9.sak.behandlingslager.behandling.vilkår.Vilkår;
+import no.nav.k9.sak.behandlingslager.behandling.vilkår.VilkårResultatRepository;
+import no.nav.k9.sak.domene.arbeidsforhold.InntektArbeidYtelseTjeneste;
 import no.nav.k9.sak.domene.behandling.steg.beregningsgrunnlag.BeregningsgrunnlagVilkårTjeneste;
+import no.nav.k9.sak.domene.iay.modell.InntektArbeidYtelseGrunnlag;
+import no.nav.k9.sak.domene.iay.modell.Inntektsmelding;
 import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
 
 @FagsakYtelseTypeRef("PSB")
@@ -35,52 +44,75 @@ import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
 class PleiepengerVurderRefusjonLagFortsettRequest implements LagFortsettRequest {
 
     private PleiepengerGrunnlagMapper ytelsesspesifiktGrunnlagMapper;
-    private BeregningsgrunnlagVilkårTjeneste beregningsgrunnlagVilkårTjeneste;
+    private VilkårResultatRepository vilkårResultatRepository;
+    private KalkulatorInputTjeneste kalkulatorInputTjeneste;
+    private InntektArbeidYtelseTjeneste iayTjeneste;
 
     public PleiepengerVurderRefusjonLagFortsettRequest() {
     }
 
     @Inject
     public PleiepengerVurderRefusjonLagFortsettRequest(@Any PleiepengerGrunnlagMapper ytelsesspesifiktGrunnlagMapper,
-                                                       BeregningsgrunnlagVilkårTjeneste beregningsgrunnlagVilkårTjeneste) {
+                                                       VilkårResultatRepository vilkårResultatRepository,
+                                                       @FagsakYtelseTypeRef("*") KalkulatorInputTjeneste kalkulatorInputTjeneste,
+                                                       InntektArbeidYtelseTjeneste iayTjeneste) {
         this.ytelsesspesifiktGrunnlagMapper = ytelsesspesifiktGrunnlagMapper;
-        this.beregningsgrunnlagVilkårTjeneste = beregningsgrunnlagVilkårTjeneste;
+        this.vilkårResultatRepository = vilkårResultatRepository;
+        this.kalkulatorInputTjeneste = kalkulatorInputTjeneste;
+        this.iayTjeneste = iayTjeneste;
     }
 
     @Override
-    public FortsettBeregningListeRequest lagRequest(BehandlingReferanse referanse, Collection<BgRef> bgReferanser, BehandlingStegType stegType) {
+    public FortsettBeregningListeRequest lagRequest(BehandlingReferanse referanse,
+                                                    Collection<BgRef> bgReferanser,
+                                                    BehandlingStegType stegType) {
         var bgRefs = BgRef.getRefs(bgReferanser);
-        var stpMap = bgReferanser.stream().collect(Collectors.toMap(BgRef::getRef, BgRef::getStp));
         var ytelseType = YtelseTyperKalkulusStøtterKontrakt.fraKode(referanse.getFagsakYtelseType().getKode());
-        var ytelsesspesifiktGrunnlagPrReferanse = getYtelsesspesifiktGrunnlagPrReferanse(referanse, stpMap);
-        return FortsettBeregningListeRequest.medOppdaterteUtbetalingsgrader(
+        return new FortsettBeregningListeRequest(
             referanse.getSaksnummer().getVerdi(),
             bgRefs,
-            ytelsesspesifiktGrunnlagPrReferanse,
+            lagInputMap(bgReferanser, referanse),
             ytelseType,
             new StegType(stegType.getKode()));
     }
 
-    private Map<UUID, YtelsespesifiktGrunnlagDto> getYtelsesspesifiktGrunnlagPrReferanse(BehandlingReferanse behandlingReferanse,
-                                                                                         Map<UUID, LocalDate> referanseSkjæringstidspunktMap) {
-        var utledPerioderTilVurdering = beregningsgrunnlagVilkårTjeneste.utledPerioderTilVurdering(behandlingReferanse, false);
+    private Map<UUID, KalkulatorInputDto> lagInputMap(Collection<BgRef> bgReferanser, BehandlingReferanse referanse) {
+        var iayGrunnlag = iayTjeneste.hentGrunnlag(referanse.getBehandlingId());
+        var sakInntektsmeldinger = iayTjeneste.hentUnikeInntektsmeldingerForSak(referanse.getSaksnummer());
 
+        Map<UUID, LocalDate> stpMap = bgReferanser.stream().collect(Collectors.toMap(BgRef::getRef, BgRef::getStp));
+        return getReferanseTilInputMap(referanse, iayGrunnlag, sakInntektsmeldinger, stpMap);
+    }
+
+    private Map<UUID, KalkulatorInputDto> getReferanseTilInputMap(BehandlingReferanse behandlingReferanse,
+                                                                  InntektArbeidYtelseGrunnlag iayGrunnlag,
+                                                                  Collection<Inntektsmelding> sakInntektsmeldinger,
+                                                                  Map<UUID, LocalDate> referanseSkjæringstidspunktMap) {
+        Vilkår vilkår = vilkårResultatRepository.hent(behandlingReferanse.getBehandlingId()).getVilkår(VilkårType.BEREGNINGSGRUNNLAGVILKÅR).orElseThrow();
+
+        var opptjeningsvilkår = vilkårResultatRepository.hent(behandlingReferanse.getBehandlingId()).getVilkår(VilkårType.OPPTJENINGSVILKÅRET);
         return referanseSkjæringstidspunktMap.entrySet()
             .stream()
             .sorted(Map.Entry.comparingByValue())
             .map(entry -> {
                 UUID bgReferanse = entry.getKey();
-                var vilkårPeriode = finnPeriodeForSkjæringstidspunkt(utledPerioderTilVurdering, entry);
-                var ytelsesGrunnlag = ytelsesspesifiktGrunnlagMapper.lagYtelsespesifiktGrunnlag(behandlingReferanse, vilkårPeriode);
-                return new AbstractMap.SimpleEntry<>(bgReferanse, ytelsesGrunnlag);
+                var vilkårPeriode = vilkår.finnPeriodeForSkjæringstidspunkt(entry.getValue());
+                VilkårUtfallMerknad vilkårsMerknad = null;
+                if (opptjeningsvilkår.isPresent()) {
+                    vilkårsMerknad = opptjeningsvilkår.get().finnPeriodeForSkjæringstidspunkt(vilkårPeriode.getSkjæringstidspunkt()).getMerknad();
+                }
+                var ytelsesGrunnlag = ytelsesspesifiktGrunnlagMapper.lagYtelsespesifiktGrunnlag(behandlingReferanse, vilkårPeriode.getPeriode());
+                KalkulatorInputDto kalkulatorInputDto = kalkulatorInputTjeneste.byggDto(
+                    behandlingReferanse,
+                    bgReferanse,
+                    iayGrunnlag,
+                    sakInntektsmeldinger,
+                    ytelsesGrunnlag,
+                    vilkårPeriode.getPeriode(),
+                    vilkårsMerknad);
+                return new AbstractMap.SimpleEntry<>(bgReferanse,
+                    kalkulatorInputDto);
             }).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
     }
-
-    private DatoIntervallEntitet finnPeriodeForSkjæringstidspunkt(NavigableSet<DatoIntervallEntitet> utledPerioderTilVurdering, Map.Entry<UUID, LocalDate> entry) {
-        return utledPerioderTilVurdering.stream()
-            .filter(p -> p.getFomDato().equals(entry.getValue()))
-            .findFirst().orElseThrow(() -> new IllegalStateException("Fant ikke periode for skjæringstidspunkt"));
-    }
-
 
 }
