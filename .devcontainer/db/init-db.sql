911cfbcaa3accf4385016495f08bd7ca920f79df
Frode Carlsen <frode.odde.carlsen@nav.no>
2022-01-18 16:59:02 +0100

    Update init-db.sql

--- .devcontainer/db/init-db.sql
+++ .devcontainer/db/init-db.sql
@@ -1,8 +1,9 @@
-CREATE DATABASE k9sak_unit;
-CREATE USER k9sak_unit WITH PASSWORD 'k9sak_unit';
 GRANT ALL PRIVILEGES ON DATABASE k9sak_unit TO k9sak_unit;
-create extension if not exists btree_gist with schema k9sak_unit;
+create extension if not exists btree_gist;
+
 CREATE DATABASE k9sak;
 CREATE USER k9sak WITH PASSWORD 'k9sak';
 GRANT ALL PRIVILEGES ON DATABASE k9sak TO k9sak;
-create extension if not exists btree_gist with schema k9sak;
+
+\connect k9sak k9sak;
+create extension if not exists btree_gist;
