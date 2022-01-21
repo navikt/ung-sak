17375c46a987671d5d8ddfa291bb5315d9b90fa6
Marius Glittum <marius.glittum@nav.no>
2022-01-21 19:34:23 +0100

    Legger på dokumentasjon for årsakene

--- kontrakt/src/main/java/no/nav/k9/sak/kontrakt/krav/ÅrsakTilVurdering.java
+++ kontrakt/src/main/java/no/nav/k9/sak/kontrakt/krav/ÅrsakTilVurdering.java
@@ -17,15 +17,25 @@ import no.nav.k9.kodeverk.behandling.BehandlingÅrsakType;
 public enum ÅrsakTilVurdering {
 
     MANUELT_REVURDERER_PERIODE("MANUELT_REVURDERER_PERIODE"),
+    // Ligger kant-i-kant med ny periode eller endring og blir dermed tatt med til vurdering
     REVURDERER_BERØRT_PERIODE("REVURDERER_BERØRT_PERIODE"),
+    // Tilkommet opplysninger på ny søknad fra bruker / punsj
     ENDRING_FRA_BRUKER("ENDRING_FRA_BRUKER"),
+    // Kravet for perioden har blitt trukket
     TRUKKET_KRAV("TRUKKET_KRAV"),
+    // Vurderes på nytt pga inntektsmelding som er sendt inn for perioden
     REVURDERER_NY_INNTEKTSMELDING("REVURDERER_NY_INNTEKTSMELDING"),
+    // Perioden ses på pga endringer i felles opplysninger (Nattevåk/beredskap/etablert tilsyn/sykdom
     REVURDERER_ENDRING_FRA_ANNEN_PART("REVURDERER_ENDRING_FRA_ANNEN_PART"),
+    // Endringer på felles opplysninger på sykdomsopplysningene
     REVURDERER_SYKDOM_ENDRING_FRA_ANNEN_OMSORGSPERSON("REVURDERER_SYKDOM_ENDRING_FRA_ANNEN_OMSORGSPERSON"),
+    // Endringer på felles opplysninger om etablert tilsyn
     REVURDERER_ETABLERT_TILSYN_ENDRING_FRA_ANNEN_OMSORGSPERSON("REVURDERER_ETABLERT_TILSYN_ENDRING_FRA_ANNEN_OMSORGSPERSON"),
+    // Endringer på felles opplysninger om nattevåk/beredskap
     REVURDERER_NATTEVÅKBEREDSKAP_ENDRING_FRA_ANNEN_OMSORGSPERSON("REVURDERER_NATTEVÅKBEREDSKAP_ENDRING_FRA_ANNEN_OMSORGSPERSON"),
+    // Vurderes på nytt pga G_REGULERING
     G_REGULERING("G_REGULERING"),
+    // Vurderes for første gang
     FØRSTEGANGSVURDERING("FØRSTEGANGSVURDERING");
 
     private static final Map<String, ÅrsakTilVurdering> KODER = new LinkedHashMap<>();
