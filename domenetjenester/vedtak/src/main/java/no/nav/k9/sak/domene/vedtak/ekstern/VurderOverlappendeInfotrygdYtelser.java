2c92b9684d59380ca3001c3709b7099421b13524
Espen Svendsen <62875567+espennav@users.noreply.github.com>
2022-02-07 18:40:01 +0100

    Legger til utbetalingsgrad på VKY til SP+FP (#6264)
    * Legger til utbetalingsgrad på VKY til SP+FP
    
    * Forbedring av tekst
    
    * Forbedring av tekst
    
    * Forbedring av tekst
    
    * Avgrenser overlappsjekk til å se på vilkårsperioder til vurdering (BEREGNINGSGRUNNLAGVILKÅR)

--- domenetjenester/vedtak/src/main/java/no/nav/k9/sak/domene/vedtak/ekstern/VurderOverlappendeInfotrygdYtelser.java
+++ domenetjenester/vedtak/src/main/java/no/nav/k9/sak/domene/vedtak/ekstern/VurderOverlappendeInfotrygdYtelser.java
@@ -1,20 +1,28 @@
 package no.nav.k9.sak.domene.vedtak.ekstern;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 import java.util.NavigableSet;
+import java.util.Set;
 import java.util.stream.Collectors;
-
-import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import no.nav.fpsak.tidsserie.LocalDateInterval;
+import no.nav.fpsak.tidsserie.LocalDateSegment;
+import no.nav.fpsak.tidsserie.LocalDateTimeline;
+import no.nav.fpsak.tidsserie.StandardCombinators;
 import no.nav.k9.kodeverk.Fagsystem;
 import no.nav.k9.sak.behandling.BehandlingReferanse;
 import no.nav.k9.sak.behandlingslager.behandling.Behandling;
+import no.nav.k9.sak.behandlingslager.behandling.beregning.BehandlingBeregningsresultatEntitet;
+import no.nav.k9.sak.behandlingslager.behandling.beregning.BeregningsresultatEntitet;
+import no.nav.k9.sak.behandlingslager.behandling.beregning.BeregningsresultatPeriode;
+import no.nav.k9.sak.behandlingslager.behandling.beregning.BeregningsresultatRepository;
 import no.nav.k9.sak.domene.iay.modell.Ytelse;
 import no.nav.k9.sak.produksjonsstyring.oppgavebehandling.OppgaveTjeneste;
 
@@ -34,6 +42,7 @@ public class VurderOverlappendeInfotrygdYtelser {
 
     private OverlappendeYtelserTjeneste overlappendeYtelserTjeneste;
     private OppgaveTjeneste oppgaveTjeneste;
+    private BeregningsresultatRepository beregningsresultatRepository;
 
     VurderOverlappendeInfotrygdYtelser() {
         // for CDI
@@ -41,9 +50,11 @@ public class VurderOverlappendeInfotrygdYtelser {
 
     @Inject
     public VurderOverlappendeInfotrygdYtelser(OverlappendeYtelserTjeneste overlappendeYtelserTjeneste,
-                                              OppgaveTjeneste oppgaveTjeneste) {
+                                              OppgaveTjeneste oppgaveTjeneste,
+                                              BeregningsresultatRepository beregningsresultatRepository) {
         this.overlappendeYtelserTjeneste = overlappendeYtelserTjeneste;
         this.oppgaveTjeneste = oppgaveTjeneste;
+        this.beregningsresultatRepository = beregningsresultatRepository;
     }
 
     /**
@@ -58,19 +69,43 @@ public class VurderOverlappendeInfotrygdYtelser {
             .stream()
             .filter(entry -> List.of(Fagsystem.INFOTRYGD, Fagsystem.VLSP).contains(entry.getKey().getKilde()))
             .collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue()));
+        var tilkjentYtelseTimeline = hentTilkjentYtelsePerioder(ref);
 
         for (Map.Entry<Ytelse, NavigableSet<LocalDateInterval>> entry : overlappendeEksterneYtelser.entrySet()) {
-            var ytelse = entry.getKey();
-            var perioder = entry.getValue();
-            var beskrivelse = "K9-ytelse '" + behandling.getFagsakYtelseType().getNavn() + "' er innvilget for saksnummer " + ref.getSaksnummer() +  " for periodene " + perioder;
+            var ytelseperioderTimeline = byggYtelsesperioderTidslinje(entry.getValue());
+            var overlappendeTilkjentYtelse = tilkjentYtelseTimeline.intersection(ytelseperioderTimeline);
 
-            var oppgaveId = switch (ytelse.getYtelseType()) {
+            var overlappendePerioderBeskrivelse = overlappendeTilkjentYtelse.toSegments().stream()
+                .map(segment -> "" + segment.getValue().getPeriode() + " og utbetalingsgrad: " + segment.getValue().getLavestUtbetalingsgrad().orElse(BigDecimal.ZERO))
+                .collect(Collectors.joining(", "));
+            var beskrivelse = "K9-ytelse '" + behandling.getFagsakYtelseType().getNavn() + "' er innvilget for saksnummer " + ref.getSaksnummer() +  " med overlappende perioder: " + overlappendePerioderBeskrivelse;
+
+            var oppgaveId = switch (entry.getKey().getYtelseType()) {
                 case SYKEPENGER -> oppgaveTjeneste.opprettVkyOppgaveOverlappendeYtelse(ref, beskrivelse, SP_OPPG_TEMA, BEH_TYPE_SAMHANDLING, SP_ANSV_ENHET_ID);
                 case FORELDREPENGER -> oppgaveTjeneste.opprettVkyOppgaveOverlappendeYtelse(ref, beskrivelse, FP_OPPG_TEMA, BEH_TYPE_SAMHANDLING, FP_ANSV_ENHET_ID);
-                default -> throw new IllegalArgumentException("Utviklerfeil: Ingen VKY-oppgave for InfoTrygd skal sendes for " + ytelse.getYtelseType());
+                default -> throw new IllegalArgumentException("Utviklerfeil: Ingen VKY-oppgave for InfoTrygd skal sendes for " + entry.getKey().getYtelseType());
             };
             log.info("Opprettet VKY-oppgave med oppgaveId={} for overlappende InfoTrygd-ytelse: {}", oppgaveId, beskrivelse);
         }
+    }
+
+    private LocalDateTimeline<Boolean> byggYtelsesperioderTidslinje(NavigableSet<LocalDateInterval> ytelsesPerioder) {
+        var periodeSegmenter = ytelsesPerioder.stream()
+            .map(p -> new LocalDateSegment<>(p.getFomDato(), p.getTomDato(), Boolean.TRUE))
+            .toList();
+        return new LocalDateTimeline<>(periodeSegmenter, StandardCombinators::alwaysTrueForMatch);
+    }
+
+    private LocalDateTimeline<BeregningsresultatPeriode> hentTilkjentYtelsePerioder(BehandlingReferanse ref) {
+        var segmenter = beregningsresultatRepository.hentBeregningsresultatAggregat(ref.getBehandlingId())
+            .map(BehandlingBeregningsresultatEntitet::getBgBeregningsresultat)
+            .map(BeregningsresultatEntitet::getBeregningsresultatPerioder)
+            .filter(perioder -> !perioder.isEmpty())
+            .map(perioder -> perioder.stream()
+                .map(brPeriode -> new LocalDateSegment<>(brPeriode.getPeriode().getFomDato(), brPeriode.getPeriode().getTomDato(), brPeriode))
+                .collect(Collectors.toSet()))
+            .orElse(Set.of());
+        return new LocalDateTimeline<>(segmenter);
     }
 
 }
