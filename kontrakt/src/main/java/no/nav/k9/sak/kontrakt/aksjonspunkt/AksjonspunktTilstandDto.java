b54b24b0ea1ca418da3d715eba63319d3bdca39b
Qadeer Ahmad Khan <qadeer.ahmad.khan@nav.no>
2022-02-11 14:43:00 +0100

    tar bort unødvendig konstruktør

--- kontrakt/src/main/java/no/nav/k9/sak/kontrakt/aksjonspunkt/AksjonspunktTilstandDto.java
+++ kontrakt/src/main/java/no/nav/k9/sak/kontrakt/aksjonspunkt/AksjonspunktTilstandDto.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -19,24 +18,19 @@ import no.nav.k9.kodeverk.behandling.aksjonspunkt.Venteårsak;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 @JsonAutoDetect(getterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE, fieldVisibility = Visibility.ANY)
-public record AksjonspunktTilstandDto(@NotNull String aksjonspunktKode, @NotNull AksjonspunktStatus status, Venteårsak venteårsak, LocalDateTime fristTid) {
-    @JsonCreator
-    public AksjonspunktTilstandDto(
-        @JsonProperty(value = "aksjonspunktKode", required = true)
-        @NotNull
-            String aksjonspunktKode,
-        @JsonProperty(value = "status", required = true)
-        @NotNull
-            AksjonspunktStatus status,
-        @JsonProperty(value = "venteårsak")
-            Venteårsak venteårsak,
-        @JsonProperty(value = "fristTid")
-        LocalDateTime fristTid) {
-        this.aksjonspunktKode = aksjonspunktKode;
-        this.status = status;
-        this.venteårsak = venteårsak;
-        this.fristTid = fristTid;
-    }
+public record AksjonspunktTilstandDto(
+    @JsonProperty(value = "aksjonspunktKode", required = true)
+    @NotNull String aksjonspunktKode,
+
+    @JsonProperty(value = "status", required = true)
+    @NotNull AksjonspunktStatus status,
+
+    @JsonProperty(value = "venteårsak")
+    Venteårsak venteårsak,
+
+    @JsonProperty(value = "fristTid")
+    LocalDateTime fristTid
+) {
 
     public AksjonspunktTilstandDto(AksjonspunktTilstandDto kopierFra) {
         this(kopierFra.aksjonspunktKode, kopierFra.status, kopierFra.venteårsak, kopierFra.fristTid);
