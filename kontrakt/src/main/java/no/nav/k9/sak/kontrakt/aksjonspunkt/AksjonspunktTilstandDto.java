7e49e8f0e24713d19c807da9ec1d0f13fc888d73
Qadeer Khan <qadeer.ahmad.khan@nav.no>
2022-02-14 09:25:10 +0100

    Behandlingevent venteaarsak (#6336)
    * sender med venteårsak og frist. Bruker nytt objekt for aksjonspunkttilstand istedenfor å key-value aksjonspunkt koder med status liste

---
+++ kontrakt/src/main/java/no/nav/k9/sak/kontrakt/aksjonspunkt/AksjonspunktTilstandDto.java
@@ -0,0 +1,48 @@
+package no.nav.k9.sak.kontrakt.aksjonspunkt;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.NotNull;
+import no.nav.k9.kodeverk.behandling.aksjonspunkt.AksjonspunktStatus;
+import no.nav.k9.kodeverk.behandling.aksjonspunkt.Venteårsak;
+
+/**
+ * Informasjon om aksjonspunktstilstanden i behandlingen.
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+@JsonAutoDetect(getterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE, fieldVisibility = Visibility.ANY)
+public record AksjonspunktTilstandDto(
+    @JsonProperty(value = "aksjonspunktKode", required = true)
+    @NotNull String aksjonspunktKode,
+
+    @JsonProperty(value = "status", required = true)
+    @NotNull AksjonspunktStatus status,
+
+    @JsonProperty(value = "venteårsak")
+    Venteårsak venteårsak,
+
+    @JsonProperty(value = "fristTid")
+    LocalDateTime fristTid
+) {
+
+    public AksjonspunktTilstandDto(AksjonspunktTilstandDto kopierFra) {
+        this(kopierFra.aksjonspunktKode, kopierFra.status, kopierFra.venteårsak, kopierFra.fristTid);
+    }
+
+    @Override
+    public String toString() {
+        return "AksjonspunktTilstandDto{" +
+            "aksjonspunktKode='" + aksjonspunktKode + '\'' +
+            ", status='" + status + '\'' +
+            ", venteårsak='" + venteårsak + '\'' +
+            ", fristTid=" + fristTid +
+            '}';
+    }
+}
