9c241a6bec69c2a5e6937bc428f382b4ecb4e9f0
Bjornar Askevold <bjornar.askevold@nav.no>
2022-02-22 16:11:51 +0100

    - Lagrer ansvarlig saksbehandler ved løsing av aksjonspunkt. - Ny producer mot aiven for å lage produksjonsstyringhendelser for k9-los.

---
+++ kontrakt/src/main/java/no/nav/k9/sak/kontrakt/produksjonsstyring/los/ProduksjonsstyringDokumentHendelse.java
@@ -0,0 +1,25 @@
+package no.nav.k9.sak.kontrakt.produksjonsstyring.los;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import no.nav.k9.sak.kontrakt.krav.KravDokumentType;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+@JsonAutoDetect(getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE, fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class ProduksjonsstyringDokumentHendelse extends ProduksjonsstyringHendelse {
+    public final List<KravDokumentType> kravdokumenter;
+
+    @JsonCreator
+    public ProduksjonsstyringDokumentHendelse(UUID eksternId, LocalDateTime hendelseTid, List<KravDokumentType> kravdokumenter) {
+        super(eksternId, hendelseTid, K9SakHendelseType.KRAVDOKUMENT);
+        this.kravdokumenter = kravdokumenter;
+    }
+}
