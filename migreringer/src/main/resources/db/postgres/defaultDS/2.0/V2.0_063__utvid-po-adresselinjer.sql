91b11f66a681c1624e14d7da647a1696443a85ad
s149212 <espen.svendsen@nav.no>
2022-03-14 11:31:45 +0100

    Utvider PO-adresselinjer til 1000 tegn

---
+++ migreringer/src/main/resources/db/postgres/defaultDS/2.0/V2.0_063__utvid-po-adresselinjer.sql
@@ -0,0 +1,7 @@
+-- Utvider adresser - PDLs adresselinjer kan være opp til 4000 tegn teknisk :(
+ALTER TABLE po_adresse
+    ALTER COLUMN adresselinje1 TYPE varchar(1000),
+    ALTER COLUMN adresselinje2 TYPE varchar(1000),
+    ALTER COLUMN adresselinje3 TYPE varchar(1000),
+    ALTER COLUMN adresselinje3 TYPE varchar(1000)
+    ;
