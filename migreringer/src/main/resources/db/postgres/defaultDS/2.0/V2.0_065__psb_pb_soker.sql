6aa55f9e84476d3887f31b9b771001cee068bdac
Stian Grenborgen <stian.grenborgen@nav.no>
2022-03-22 14:52:47 +0100

    Oppretter kun index hvis den ikke er der fra før. Må håndteres manuelt i q, men virker trolig i p.

--- migreringer/src/main/resources/db/postgres/defaultDS/2.0/V2.0_065__psb_pb_soker.sql
+++ migreringer/src/main/resources/db/postgres/defaultDS/2.0/V2.0_065__psb_pb_soker.sql
@@ -3,5 +3,5 @@ create table if not exists PSB_PB_SAK
     ID BIGINT NOT NULL
 );
 
-create UNIQUE index PK_PSB_PB_SAK on PSB_PB_SAK(ID);
+create UNIQUE index if not exists PK_PSB_PB_SAK on PSB_PB_SAK(ID);
 alter table PSB_PB_SAK add constraint FK_PSB_PB_SAK_1 foreign key (ID) references FAGSAK(ID);
