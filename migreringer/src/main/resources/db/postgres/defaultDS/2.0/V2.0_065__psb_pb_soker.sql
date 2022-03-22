250373497a1890e822349ec588c3045ed67ad9c6
Stian Grenborgen <stian.grenborgen@nav.no>
2022-03-22 15:18:54 +0100

    Håndterer at constraint finnes fra før.

--- migreringer/src/main/resources/db/postgres/defaultDS/2.0/V2.0_065__psb_pb_soker.sql
+++ migreringer/src/main/resources/db/postgres/defaultDS/2.0/V2.0_065__psb_pb_soker.sql
@@ -4,4 +4,6 @@ create table if not exists PSB_PB_SAK
 );
 
 create UNIQUE index if not exists PK_PSB_PB_SAK on PSB_PB_SAK(ID);
+
+alter table PSB_PB_SAK drop CONSTRAINT IF EXISTS FK_PSB_PB_SAK_1;
 alter table PSB_PB_SAK add constraint FK_PSB_PB_SAK_1 foreign key (ID) references FAGSAK(ID);
