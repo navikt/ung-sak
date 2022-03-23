5c957bb9dd0604dca3737103f644f2dd90ad278d
Marius Glittum <marius.glittum@nav.no>
2022-03-23 13:37:50 +0100

    Endrer tekst på vurderings årsak

--- kontrakt/src/main/java/no/nav/k9/sak/kontrakt/krav/ÅrsakTilVurdering.java
+++ kontrakt/src/main/java/no/nav/k9/sak/kontrakt/krav/ÅrsakTilVurdering.java
@@ -22,7 +22,7 @@ public enum ÅrsakTilVurdering implements Kodeverdi {
     // Ligger kant-i-kant med ny periode eller endring og blir dermed tatt med til vurdering
     REVURDERER_BERØRT_PERIODE("REVURDERER_BERØRT_PERIODE", "Tilstøtende periode"),
     // Tilkommet opplysninger på ny søknad fra bruker / punsj
-    ENDRING_FRA_BRUKER("ENDRING_FRA_BRUKER", "Endring fra digital søknad/Punsj"),
+    ENDRING_FRA_BRUKER("ENDRING_FRA_BRUKER", "Endring fra søknad/Punsj"),
     // Kravet for perioden har blitt trukket
     TRUKKET_KRAV("TRUKKET_KRAV", "Endret/fjernet søknadsperiode"),
     // Vurderes på nytt pga inntektsmelding som er sendt inn for perioden
