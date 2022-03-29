5d1afe751c4e24006332b4c09180df7d17d21375
Marius Glittum <marius.glittum@nav.no>
2022-03-29 06:29:51 +0200

    Legger på toggle og disabler i prod

--- domenetjenester/behandling-prosessering/src/main/java/no/nav/k9/sak/fagsak/prosessering/avsluttning/FinnFagsakerForAvsluttningBatchTask.java
+++ domenetjenester/behandling-prosessering/src/main/java/no/nav/k9/sak/fagsak/prosessering/avsluttning/FinnFagsakerForAvsluttningBatchTask.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import no.nav.k9.felles.konfigurasjon.konfig.KonfigVerdi;
 import no.nav.k9.prosesstask.api.ProsessTask;
 import no.nav.k9.prosesstask.api.ProsessTaskData;
 import no.nav.k9.prosesstask.api.ProsessTaskHandler;
@@ -14,26 +15,32 @@ import no.nav.k9.prosesstask.api.ProsessTaskRepository;
 @ProsessTask(FinnFagsakerForAvsluttningBatchTask.TASKTYPE)
 public class FinnFagsakerForAvsluttningBatchTask implements ProsessTaskHandler {
 
-    private static final Logger log = LoggerFactory.getLogger(FinnFagsakerForAvsluttningBatchTask.class);
-
     public static final String TASKTYPE = "batch.finnFagsakerForAvsluttning";
-
+    private static final Logger log = LoggerFactory.getLogger(FinnFagsakerForAvsluttningBatchTask.class);
     private FagsakAvsluttningTjeneste tjeneste;
     private ProsessTaskRepository prosessTaskRepository;
+    private boolean enableFagsakAvslutting;
 
     FinnFagsakerForAvsluttningBatchTask() {
         // CDI
     }
 
     @Inject
-    public FinnFagsakerForAvsluttningBatchTask(FagsakAvsluttningTjeneste tjeneste, ProsessTaskRepository prosessTaskRepository) {
+    public FinnFagsakerForAvsluttningBatchTask(FagsakAvsluttningTjeneste tjeneste, ProsessTaskRepository prosessTaskRepository,
+                                               @KonfigVerdi(value = "enable.fagsak.avslutting", defaultVerdi = "true") boolean enableFagsakAvslutting) {
         this.tjeneste = tjeneste;
         this.prosessTaskRepository = prosessTaskRepository;
+        this.enableFagsakAvslutting = enableFagsakAvslutting;
     }
 
     @Override
     public void doTask(ProsessTaskData prosessTaskData) {
         log.info("Finner saker som kan avsluttes");
+
+        if (!enableFagsakAvslutting) {
+            log.info("Fagsak avslutting er togglet av");
+            return;
+        }
         var fagsakerSomKanAvsluttes = tjeneste.finnFagsakerSomSkalAvsluttes();
 
         log.info("Fant {} saker som kan avsluttes", fagsakerSomKanAvsluttes.size());
