9b9f3af10bb8439af0fbe7e58cfc80d6995e22c1
Marius Bækken Glittum <marius.glittum@nav.no>
2022-04-20 08:21:41 +0200

    Korrigerer utledning av diff ved nye IMer siden forrige vedtak (#6888)
    * Korrigerer utledning av diff ved nye IMer siden forrige vedtak

--- domenetjenester/registerinnhenting/src/main/java/no/nav/k9/sak/domene/registerinnhenting/impl/startpunkt/StartpunktUtlederInntektsmeldinger.java
+++ domenetjenester/registerinnhenting/src/main/java/no/nav/k9/sak/domene/registerinnhenting/impl/startpunkt/StartpunktUtlederInntektsmeldinger.java
@@ -11,7 +11,7 @@ import no.nav.k9.sak.domene.iay.modell.InntektArbeidYtelseGrunnlag;
 @Dependent
 class StartpunktUtlederInntektsmeldinger {
 
-    private InntektArbeidYtelseTjeneste inntektArbeidYtelseTjeneste;
+    private final InntektArbeidYtelseTjeneste inntektArbeidYtelseTjeneste;
 
     @Inject
     StartpunktUtlederInntektsmeldinger(InntektArbeidYtelseTjeneste inntektArbeidYtelseTjeneste) {
@@ -20,12 +20,12 @@ class StartpunktUtlederInntektsmeldinger {
 
     StartpunktType utledStartpunkt(BehandlingReferanse ref, InntektArbeidYtelseGrunnlag grunnlag1) {
 
-        if (inntektsmeldingErSøknad(ref) && grunnlag1 != null) {
+        if (grunnlag1 != null) {
             var nyeInntektsmeldinger = inntektArbeidYtelseTjeneste.hentInntektsmeldingerSidenRef(ref.getSaksnummer(), ref.getBehandlingId(), grunnlag1.getEksternReferanse());
 
-            // TODO: Validere at det faktisk blir endring i periodene
             if (!nyeInntektsmeldinger.isEmpty()) {
-                return StartpunktType.INIT_PERIODER;
+                // TODO: Validere at det faktisk blir endring i periodene / Endring i IM i bruk
+                return inntektsmeldingErSøknad(ref) ? StartpunktType.INIT_PERIODER : StartpunktType.BEREGNING;
             }
         }
 
