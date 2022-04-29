82164b7de9154bf1c5c1bea5a78b5323afe78284
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2022-04-29 16:53:09 +0200

    OMP fix Setter henlagt-type på gammel behandling som skal henlegges

--- migreringer/src/main/resources/db/postgres/defaultDS/2.0/V2.0_073__peker-til-riktig-behandling-gammel-omp-sak.sql
+++
@@ -1,6 +0,0 @@
--- TSF-2566 Saksnummer 8DSJM (OMP)
-
---siste behandling peker til en behandling som ikke er ordentlig avsluttet - i praksis henlagt
---må peke til tidligere behandling for å kunne fullføre saksbehandling
-update behandling set original_behandling_id = 1132872, sist_oppdatert_tidspunkt=current_timestamp at time zone 'UTC'  where id = 1544012 and uuid = '3f4f2dc0-3685-4a4c-b021-260c59d4a707';
-
