120401e42d931de6722cdfb90d4effb2aa1e4de8
Espen Johansen Velsvik <espen.velsvik@nav.no>
2022-04-29 13:34:07 +0200

    Støtter bekreft og overstyr i samme kall

--- kontrakt/src/main/java/no/nav/k9/sak/kontrakt/aksjonspunkt/BekreftetOgOverstyrteAksjonspunkterDto.java
+++ kontrakt/src/main/java/no/nav/k9/sak/kontrakt/aksjonspunkt/BekreftetOgOverstyrteAksjonspunkterDto.java
@@ -2,16 +2,11 @@ package no.nav.k9.sak.kontrakt.aksjonspunkt;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
-
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
@@ -20,13 +15,20 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import no.nav.k9.abac.AbacAttributt;
+import no.nav.k9.kodeverk.arbeidsforhold.ArbeidType;
 import no.nav.k9.sak.kontrakt.behandling.BehandlingIdDto;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 @JsonAutoDetect(getterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE, fieldVisibility = Visibility.ANY)
-public class OverstyrteAksjonspunkterDto {
+public class BekreftetOgOverstyrteAksjonspunkterDto {
 
     @JsonProperty(value = "behandlingId", required = true)
     @NotNull
@@ -42,15 +44,23 @@ public class OverstyrteAksjonspunkterDto {
 
     @JsonProperty(value = "overstyrteAksjonspunktDtoer")
     @Valid
-    @Size(min = 1, max = 10)
+    @Size(max = 10)
     private Collection<OverstyringAksjonspunktDto> overstyrteAksjonspunktDtoer = Collections.emptyList();
 
-    public static OverstyrteAksjonspunkterDto lagDto(Long behandlingId, Long behandlingVersjon,
-                                                     Collection<OverstyringAksjonspunktDto> overstyrteAksjonspunktDtoer) {
-        OverstyrteAksjonspunkterDto dto = new OverstyrteAksjonspunkterDto();
+    @JsonProperty(value = "bekreftedeAksjonspunktDtoer")
+    @Size(max = 10)
+    @NotNull
+    @Valid
+    private Collection<BekreftetAksjonspunktDto> bekreftedeAksjonspunktDtoer = Collections.emptyList();
+
+    public static BekreftetOgOverstyrteAksjonspunkterDto lagDto(Long behandlingId, Long behandlingVersjon,
+                                                                Collection<OverstyringAksjonspunktDto> overstyrteAksjonspunktDtoer,
+                                                                Collection<BekreftetAksjonspunktDto> bekreftedeAksjonspunktDtoer) {
+        BekreftetOgOverstyrteAksjonspunkterDto dto = new BekreftetOgOverstyrteAksjonspunkterDto();
         dto.behandlingId = new BehandlingIdDto(behandlingId);
         dto.behandlingVersjon = behandlingVersjon;
         dto.overstyrteAksjonspunktDtoer = overstyrteAksjonspunktDtoer;
+        dto.bekreftedeAksjonspunktDtoer = bekreftedeAksjonspunktDtoer;
         return dto;
     }
 
@@ -72,9 +82,15 @@ public class OverstyrteAksjonspunkterDto {
         return Collections.unmodifiableCollection(overstyrteAksjonspunktDtoer);
     }
 
+    public Collection<BekreftetAksjonspunktDto> getBekreftedeAksjonspunktDtoer() {
+        return Collections.unmodifiableCollection(bekreftedeAksjonspunktDtoer);
+    }
+
     @AbacAttributt("aksjonspunktKode")
-    public Set<String> getOverstyrteAksjonspunktKoder() {
-        return overstyrteAksjonspunktDtoer.stream().map(OverstyringAksjonspunktDto::getKode).collect(Collectors.toSet());
+    public Set<String> getAksjonspunktKoder() {
+        var koder = overstyrteAksjonspunktDtoer.stream().map(OverstyringAksjonspunktDto::getKode).collect(Collectors.toCollection(HashSet::new));
+        koder.addAll(bekreftedeAksjonspunktDtoer.stream().map(BekreftetAksjonspunktDto::getKode).collect(Collectors.toSet()));
+        return koder;
     }
 
     public void setBehandlingId(BehandlingIdDto behandlingId) {
@@ -87,6 +103,15 @@ public class OverstyrteAksjonspunkterDto {
 
     public void setOverstyrteAksjonspunktDtoer(Collection<OverstyringAksjonspunktDto> overstyrteAksjonspunktDtoer) {
         this.overstyrteAksjonspunktDtoer = List.copyOf(overstyrteAksjonspunktDtoer);
+    }
+
+    public void setBekreftedeAksjonspunktDtoer(Collection<BekreftetAksjonspunktDto> bekreftedeAksjonspunktDtoer) {
+        this.bekreftedeAksjonspunktDtoer = bekreftedeAksjonspunktDtoer;
+    }
+
+    @AssertTrue(message = "Må ha minst ett aksjonspunkt")
+    public boolean minstEttAksjonspunkt() {
+        return (bekreftedeAksjonspunktDtoer != null && bekreftedeAksjonspunktDtoer.size() > 0) || (overstyrteAksjonspunktDtoer != null && overstyrteAksjonspunktDtoer.size() > 0);
     }
 
 }
