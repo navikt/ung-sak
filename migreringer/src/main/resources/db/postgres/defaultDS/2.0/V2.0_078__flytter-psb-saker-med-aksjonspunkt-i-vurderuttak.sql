86b5de1bb95b69c164070b217db6442ea49c8f7a
Marius Glittum <marius.glittum@nav.no>
2022-05-13 13:28:01 +0200

    oppdaterer endret av

--- migreringer/src/main/resources/db/postgres/defaultDS/2.0/V2.0_078__flytter-psb-saker-med-aksjonspunkt-i-vurderuttak.sql
+++ migreringer/src/main/resources/db/postgres/defaultDS/2.0/V2.0_078__flytter-psb-saker-med-aksjonspunkt-i-vurderuttak.sql
@@ -4,7 +4,7 @@
 update aksjonspunkt
 set behandling_steg_funnet = 'PRECONDITION_BERGRUNN',
     endret_tid             = current_timestamp at time zone 'UTC',
-    endret_av              = 'migrering V2.0_077'
+    endret_av              = 'migrering V2.0_078'
 where id in (select a.id
              from aksjonspunkt a
                       join behandling b on b.id = a.behandling_id
