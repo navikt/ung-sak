1752af132eb99a688380317053d92dd598ecb627
Marius Glittum <marius.glittum@nav.no>
2022-08-09 13:20:39 +0200

    Retter feil i query

--- migreringer/src/main/resources/db/postgres/defaultDS/2.0/V2.0_088__korrigere-søkers-søkte-uttak.sql
+++ migreringer/src/main/resources/db/postgres/defaultDS/2.0/V2.0_088__korrigere-søkers-søkte-uttak.sql
@@ -1,5 +1,5 @@
-UPDATE up_uttaksperiode u SET u.timer_per_dag = 27000000000000
-where u.holder_id IN (select distinct sp.id
+UPDATE up_uttaksperiode SET timer_per_dag = 27000000000000
+where holder_id IN (select distinct sp.id
                       from fagsak f
                           inner join behandling b on b.fagsak_id = f.id
                           inner join GR_UTTAKSPERIODER gr on gr.behandling_id = b.id
@@ -8,8 +8,8 @@ where u.holder_id IN (select distinct sp.id
                        where f.saksnummer IN ('BSQFK', 'BSQBE', 'BSQW8', 'BTC5i', 'BWH9Q', 'BRTK8', 'BRSZE', 'BRJZS', 'BX6Go', 'BRK5C', 'BRXGi', 'BW7FK', 'BSFMo', 'BRLNi', 'BRQWE', 'BW9D0', 'BRXQ8', 'BTWDA', 'BRZJi', 'BVFQC', 'BSMGi', 'BToB0', 'BU8PQ', 'BSQJQ', 'BToKQ', 'BVKXU', 'BRZT8', 'BW5XE', 'BWJXK', 'BRLUG', 'BSUG0', 'BWKTi', 'BRKAW', 'BWR3W', 'BW674', 'BWRRi', 'BWN90', 'BV9ZE', 'BRWX2', 'BWX8Q', 'BWXA4', 'BVFXA', 'BSPL0', 'BRLRo', 'BRQ7E', 'BRQH4', 'BSQA0', 'BX2HM', 'BVZWQ', 'BVUE4', 'BRQS8', 'BX646', 'BX65K', 'BX66Y', 'BX68C', 'BX6Ci', 'BRK3Y', 'BRL6U', 'BRRiM', 'BTWTY', 'BRXHW', 'BRJX0', 'BRNYU', 'BRZi4', 'BSCi6', 'BSG7i', 'BSGTQ') and f.ytelse_type='PPN');
 
 
-UPDATE up_uttaksperiode u SET u.timer_per_dag = 27000000000000
-where u.holder_id IN (select distinct sp.id
+UPDATE up_uttaksperiode SET timer_per_dag = 27000000000000
+where holder_id IN (select distinct sp.id
                       from fagsak f
                                inner join behandling b on b.fagsak_id = f.id
                                inner join GR_UTTAKSPERIODER gr on gr.behandling_id = b.id
