3ceea0835d5ca55bbf0574c78bc045245381aa6b
Stian Grenborgen <stian.grenborgen@nav.no>
2022-09-02 11:07:49 +0200

    Retter manglende setting av verdi etter oppdatering.

--- ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/utils/Hjelpetidslinjer.java
+++ ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/utils/Hjelpetidslinjer.java
@@ -52,7 +52,7 @@ public final class Hjelpetidslinjer {
             førsteFom = førsteFom.minusDays(2);
         }
         if (førsteFom.getDayOfWeek() != DayOfWeek.SATURDAY) {
-            førsteFom.plusDays(DayOfWeek.SUNDAY.getValue() - førsteFom.getDayOfWeek().getValue() + 1);
+            førsteFom = førsteFom.plusDays(DayOfWeek.SUNDAY.getValue() - førsteFom.getDayOfWeek().getValue() + 1);
         }
         return førsteFom;
     }
