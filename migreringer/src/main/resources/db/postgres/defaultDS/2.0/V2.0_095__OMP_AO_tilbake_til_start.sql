48de61d30305bbf6a720cb780c3d4e703236169f
Helga Nyrud <helga.nyrud@nav.no>
2022-09-22 14:26:10 +0200

    Setter aleneomsorg-sakene tilbake til start da det har blitt lagt til nytt vilkår

---
+++ migreringer/src/main/resources/db/postgres/defaultDS/2.0/V2.0_095__OMP_AO_tilbake_til_start.sql
@@ -0,0 +1,5 @@
+insert into prosess_task (id, task_type, task_gruppe, neste_kjoering_etter, task_parametere)
+select nextval('seq_prosess_task'), 'behandlingskontroll.tilbakeTilStart',
+       nextval('seq_prosess_task_gruppe'), null,
+       'fagsakId=' || b.fagsak_id || 'behandlingId=' || b.id from fagsak f inner join behandling b on f.id = b.fagsak_id
+where f.ytelse_type = 'OMP_AO' and b.behandling_status in ('UTRED', 'IVED', 'FVED');
