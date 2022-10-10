2b4438c7e40a2a4c13deb9cfc58603b485e53f2f
Marius Glittum <marius.glittum@nav.no>
2022-10-10 12:28:37 +0200

    Justerer utledning av vilkårsperiodisering fra MA

--- ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/utvidetrett/prosess/UtvidetRettInngangsvilkårUtleder.java
+++ ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/utvidetrett/prosess/UtvidetRettInngangsvilkårUtleder.java
@@ -8,7 +8,6 @@ import static no.nav.k9.kodeverk.behandling.FagsakYtelseType.OMSORGSPENGER_MA;
 import java.util.List;
 
 import jakarta.enterprise.context.ApplicationScoped;
-
 import no.nav.k9.kodeverk.vilkår.VilkårType;
 import no.nav.k9.sak.behandlingskontroll.BehandlingTypeRef;
 import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
@@ -21,7 +20,7 @@ import no.nav.k9.sak.inngangsvilkår.VilkårUtleder;
 @FagsakYtelseTypeRef(OMSORGSPENGER_AO)
 @BehandlingTypeRef
 @ApplicationScoped
-class UtvidetRettInngangsvilkårUtleder implements VilkårUtleder {
+public class UtvidetRettInngangsvilkårUtleder implements VilkårUtleder {
 
     private static final List<VilkårType> YTELSE_VILKÅR = asList(
         VilkårType.OMSORGEN_FOR,
