09a5c43a7d8f54aea2b80980d1f4cca03835204a
Marius Bækken Glittum <marius.glittum@nav.no>
2022-10-11 07:35:38 +0200

    Justerer utledning av vilkårsperiodisering fra MA (#7874)
    * Justerer utledning av vilkårsperiodisering fra MA
    * Oppdatert periodeutledning for KS og AO
    
    Co-authored-by: Tore Langedal Endestad <tore.langedal.endestad@nav.no>

--- ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/utvidetrett/alenemidlertidig/MidlertidigAleneVilkårsVurderingTjeneste.java
+++ ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/utvidetrett/alenemidlertidig/MidlertidigAleneVilkårsVurderingTjeneste.java
@@ -3,12 +3,13 @@ package no.nav.k9.sak.ytelse.omsorgspenger.utvidetrett.alenemidlertidig;
 import static no.nav.k9.kodeverk.behandling.FagsakYtelseType.OMSORGSPENGER_MA;
 
 import java.util.Collections;
+import java.util.EnumMap;
 import java.util.Map;
 import java.util.NavigableSet;
 import java.util.Set;
 import java.util.TreeSet;
 
-import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.context.Dependent;
 import jakarta.inject.Inject;
 import no.nav.k9.felles.konfigurasjon.konfig.Tid;
 import no.nav.k9.kodeverk.vilkår.VilkårType;
@@ -18,24 +19,25 @@ import no.nav.k9.sak.behandlingslager.behandling.søknad.SøknadRepository;
 import no.nav.k9.sak.behandlingslager.behandling.vilkår.VilkårResultatRepository;
 import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
 import no.nav.k9.sak.domene.typer.tid.TidslinjeUtil;
+import no.nav.k9.sak.inngangsvilkår.UtledeteVilkår;
+import no.nav.k9.sak.inngangsvilkår.VilkårUtleder;
 import no.nav.k9.sak.perioder.VilkårsPerioderTilVurderingTjeneste;
 import no.nav.k9.sak.ytelse.omsorgspenger.utvidetrett.UtvidetRettSøknadPerioder;
 
 @FagsakYtelseTypeRef(OMSORGSPENGER_MA)
 @BehandlingTypeRef
-@RequestScoped
+@Dependent
 public class MidlertidigAleneVilkårsVurderingTjeneste implements VilkårsPerioderTilVurderingTjeneste {
 
     private UtvidetRettSøknadPerioder søktePerioder;
     private VilkårResultatRepository vilkårResultatRepository;
-
-    MidlertidigAleneVilkårsVurderingTjeneste() {
-        // for proxy
-    }
+    private VilkårUtleder vilkårUtleder;
 
     @Inject
-    public MidlertidigAleneVilkårsVurderingTjeneste(VilkårResultatRepository vilkårResultatRepository,
+    public MidlertidigAleneVilkårsVurderingTjeneste(@FagsakYtelseTypeRef(OMSORGSPENGER_MA) VilkårUtleder vilkårUtleder,
+                                                    VilkårResultatRepository vilkårResultatRepository,
                                                     SøknadRepository søknadRepository) {
+        this.vilkårUtleder = vilkårUtleder;
         this.vilkårResultatRepository = vilkårResultatRepository;
         this.søktePerioder = new UtvidetRettSøknadPerioder(søknadRepository);
     }
@@ -69,18 +71,20 @@ public class MidlertidigAleneVilkårsVurderingTjeneste implements VilkårsPeriod
 
     @Override
     public Map<VilkårType, NavigableSet<DatoIntervallEntitet>> utledRådataTilUtledningAvVilkårsperioder(Long behandlingId) {
-        return Map.of(
-            VilkårType.UTVIDETRETT, utled(behandlingId, VilkårType.UTVIDETRETT),
-            VilkårType.OMSORGEN_FOR, utled(behandlingId, VilkårType.OMSORGEN_FOR));
+        final var vilkårPeriodeSet = new EnumMap<VilkårType, NavigableSet<DatoIntervallEntitet>>(VilkårType.class);
+        UtledeteVilkår utledeteVilkår = vilkårUtleder.utledVilkår(null);
+        utledeteVilkår.getAlleAvklarte()
+            .forEach(vilkår -> vilkårPeriodeSet.put(vilkår, utledFullstendigePerioder(behandlingId)));
+
+        return vilkårPeriodeSet;
     }
 
     DatoIntervallEntitet utledMaksPeriode(NavigableSet<DatoIntervallEntitet> søktePerioder) {
         // 1. jan minst 3 år før søknad sendt inn (spesielle særtilfeller tillater at et går an å sette tilbake it itid
         var førsteSøktePeriode = søktePerioder.first();
-        var fristFørSøknadsdato = førsteSøktePeriode.getFomDato().minusYears(3).withMonth(1).withDayOfMonth(1);
 
-        var mindato = fristFørSøknadsdato;
-        var maksdato = Tid.TIDENES_ENDE;
+        var mindato = førsteSøktePeriode.getFomDato().minusYears(3).withMonth(1).withDayOfMonth(1);
+        var maksdato = Tid.TIDENES_ENDE; // TODO: Sjekk mot personopplysninger?
         return DatoIntervallEntitet.fraOgMedTilOgMed(mindato, maksdato);
     }
 
