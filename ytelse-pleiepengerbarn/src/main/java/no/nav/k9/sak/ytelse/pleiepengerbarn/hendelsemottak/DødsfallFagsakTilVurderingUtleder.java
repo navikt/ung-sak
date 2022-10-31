d54348f0d712ea6b0649a1f0e4eaecb33b557de1
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2022-10-31 10:13:56 +0100

    Fix: Dødsfallehendelse trigger også fagsaker som har periode bare etter dødsfallet

--- ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/hendelsemottak/DødsfallFagsakTilVurderingUtleder.java
+++ ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/hendelsemottak/DødsfallFagsakTilVurderingUtleder.java
@@ -1,5 +1,6 @@
 package no.nav.k9.sak.ytelse.pleiepengerbarn.hendelsemottak;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,33 +19,33 @@ import no.nav.k9.sak.typer.AktørId;
 
 @ApplicationScoped
 @HendelseTypeRef("PDL_DØDSFALL")
-public class PSBDødsfallFagsakTilVurderingUtleder implements FagsakerTilVurderingUtleder {
+public class DødsfallFagsakTilVurderingUtleder implements FagsakerTilVurderingUtleder {
 
     public static final Set<FagsakYtelseType> RELEVANTE_YTELSER = Set.of(FagsakYtelseType.PLEIEPENGER_SYKT_BARN, FagsakYtelseType.PLEIEPENGER_NÆRSTÅENDE);
     private FagsakRepository fagsakRepository;
 
-    public PSBDødsfallFagsakTilVurderingUtleder() {
+    public DødsfallFagsakTilVurderingUtleder() {
         // For CDI
     }
 
     @Inject
-    public PSBDødsfallFagsakTilVurderingUtleder(FagsakRepository fagsakRepository) {
+    public DødsfallFagsakTilVurderingUtleder(FagsakRepository fagsakRepository) {
         this.fagsakRepository = fagsakRepository;
     }
 
     @Override
     public Map<Fagsak, BehandlingÅrsakType> finnFagsakerTilVurdering(AktørId aktørId, Hendelse hendelse) {
         List<AktørId> dødsfallAktører = hendelse.getHendelseInfo().getAktørIder();
-        var periode = hendelse.getHendelsePeriode();
+        LocalDate dødsdato = hendelse.getHendelsePeriode().getFom();
 
         var fagsaker = new HashMap<Fagsak, BehandlingÅrsakType>();
 
         for (FagsakYtelseType fagsakYtelseType : RELEVANTE_YTELSER) {
             for (AktørId aktør : dødsfallAktører) {
-                for (Fagsak fagsak : fagsakRepository.finnFagsakRelatertTil(fagsakYtelseType, aktør, null, null, periode.getFom(), periode.getTom())) {
+                for (Fagsak fagsak : fagsakRepository.finnFagsakRelatertTil(fagsakYtelseType, aktør, null, null, dødsdato, null)) {
                     fagsaker.put(fagsak, BehandlingÅrsakType.RE_HENDELSE_DØD_FORELDER);
                 }
-                for (Fagsak fagsak : fagsakRepository.finnFagsakRelatertTil(fagsakYtelseType, null, aktør, null, periode.getFom(), periode.getTom())) {
+                for (Fagsak fagsak : fagsakRepository.finnFagsakRelatertTil(fagsakYtelseType, null, aktør, null, dødsdato, null)) {
                     fagsaker.put(fagsak, BehandlingÅrsakType.RE_HENDELSE_DØD_BARN);
                 }
             }
