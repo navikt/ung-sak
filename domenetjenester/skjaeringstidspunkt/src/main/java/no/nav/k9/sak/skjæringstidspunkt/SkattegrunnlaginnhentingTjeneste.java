d313178c3c581a726589ce6a78a1bd5d28b83c37
Espen Velsvik <espenjv@users.noreply.github.com>
2022-12-08 12:22:36 +0100

    Dersom skatteoppgjøret for året før første uttaksdato er klart innhentes kun 3 år fra SIGRUN. Ellers innhentes 4. (#8278)

--- domenetjenester/skjaeringstidspunkt/src/main/java/no/nav/k9/sak/skjæringstidspunkt/SkattegrunnlaginnhentingTjeneste.java
+++ domenetjenester/skjaeringstidspunkt/src/main/java/no/nav/k9/sak/skjæringstidspunkt/SkattegrunnlaginnhentingTjeneste.java
@@ -7,27 +7,43 @@ import no.nav.k9.sak.typer.Periode;
 
 public class SkattegrunnlaginnhentingTjeneste {
 
-    public static final int ANTALL_FERDIGLIGNEDE_ÅR = 4;
+    public static final int MAKS_ANTALL_FERDIGLIGNEDE_ÅR = 4;
+    public static final int MIN_ANTALL_FERDIGLIGNEDE_ÅR = 3;
+
     public static final MonthDay FØRSTE_MULIGE_SKATTEOPPGJØRSDATO = MonthDay.of(5, 1);
 
     public static Periode utledSkattegrunnlagOpplysningsperiode(LocalDate førsteSkjæringstidspunkt, LocalDate fagsakperiodeTom) {
         int sisteÅr = finnSisteÅr(fagsakperiodeTom);
         int førsteÅr = finnFørsteÅr(førsteSkjæringstidspunkt);
+        if (førsteÅr <= 2015) {
+            throw new IllegalStateException("Første år må være etter 2015");
+        }
         return new Periode(LocalDate.of(førsteÅr, 1, 1), LocalDate.of(sisteÅr, 12, 31));
     }
 
     private static int finnFørsteÅr(LocalDate førsteSkjæringstidspunkt) {
-        return førsteSkjæringstidspunkt.getYear() - ANTALL_FERDIGLIGNEDE_ÅR;
+        var stpÅr = førsteSkjæringstidspunkt.getYear();
+        int sisteTilgjengeligeÅr = finnSisteTilgjengeligeÅr();
+        if (sisteTilgjengeligeÅr == stpÅr - 1) {
+            return stpÅr - MIN_ANTALL_FERDIGLIGNEDE_ÅR;
+        } else {
+            return stpÅr - MAKS_ANTALL_FERDIGLIGNEDE_ÅR;
+        }
     }
 
     private static int finnSisteÅr(LocalDate fagsakperiodeTom) {
         var åretFørFagsakTom = fagsakperiodeTom.getYear() - 1;
-        var fjoråret = LocalDate.now().getYear() - 1;
-        var sisteTilgjengeligeÅr = kanFjoråretVæreTilgjengelig() ? fjoråret : fjoråret - 1;
+        int sisteTilgjengeligeÅr = finnSisteTilgjengeligeÅr();
         return Math.min(åretFørFagsakTom, sisteTilgjengeligeÅr);
     }
 
-    private static boolean kanFjoråretVæreTilgjengelig() {
+    private static int finnSisteTilgjengeligeÅr() {
+        var fjoråret = LocalDate.now().getYear() - 1;
+        var sisteTilgjengeligeÅr = erSkatteoppgjørForÅretFørTilgjengelig() ? fjoråret : fjoråret - 1;
+        return sisteTilgjengeligeÅr;
+    }
+
+    private static boolean erSkatteoppgjørForÅretFørTilgjengelig() {
         return MonthDay.now().isAfter(FØRSTE_MULIGE_SKATTEOPPGJØRSDATO);
     }
 
