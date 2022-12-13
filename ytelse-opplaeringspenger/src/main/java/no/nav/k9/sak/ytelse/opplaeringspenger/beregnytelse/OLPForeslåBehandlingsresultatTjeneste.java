68bb50ac4b3bcd9b370b2cfa34cc89ebbc746ca1
oyvemb <oyvind.andre.hagen.emblemsvag@nav.no>
2022-12-13 10:14:14 +0100

    Trenger ikke override skalBehandlingenSettesTilAvslått her

--- ytelse-opplaeringspenger/src/main/java/no/nav/k9/sak/ytelse/opplaeringspenger/beregnytelse/OLPForeslåBehandlingsresultatTjeneste.java
+++ ytelse-opplaeringspenger/src/main/java/no/nav/k9/sak/ytelse/opplaeringspenger/beregnytelse/OLPForeslåBehandlingsresultatTjeneste.java
@@ -3,7 +3,6 @@ package no.nav.k9.sak.ytelse.opplaeringspenger.beregnytelse;
 import static no.nav.k9.kodeverk.behandling.FagsakYtelseType.OPPLÆRINGSPENGER;
 
 import java.util.List;
-import java.util.Set;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Any;
@@ -13,35 +12,32 @@ import no.nav.fpsak.tidsserie.LocalDateSegment;
 import no.nav.fpsak.tidsserie.LocalDateTimeline;
 import no.nav.fpsak.tidsserie.StandardCombinators;
 import no.nav.k9.kodeverk.vilkår.VilkårType;
-import no.nav.k9.sak.behandling.BehandlingReferanse;
 import no.nav.k9.sak.behandling.revurdering.ytelse.RevurderingBehandlingsresultatutleder;
 import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
 import no.nav.k9.sak.behandlingslager.behandling.Behandling;
 import no.nav.k9.sak.behandlingslager.behandling.repository.BehandlingRepository;
 import no.nav.k9.sak.behandlingslager.behandling.repository.BehandlingRepositoryProvider;
 import no.nav.k9.sak.behandlingslager.behandling.vedtak.VedtakVarselRepository;
-import no.nav.k9.sak.behandlingslager.behandling.vilkår.Vilkår;
-import no.nav.k9.sak.behandlingslager.behandling.vilkår.Vilkårene;
 import no.nav.k9.sak.domene.behandling.steg.foreslåresultat.ForeslåBehandlingsresultatTjeneste;
 import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
 import no.nav.k9.sak.perioder.VilkårsPerioderTilVurderingTjeneste;
 
 @FagsakYtelseTypeRef(OPPLÆRINGSPENGER)
 @ApplicationScoped
-public class OLPUttakForeslåBehandlingsresultatTjeneste extends ForeslåBehandlingsresultatTjeneste {
+public class OLPForeslåBehandlingsresultatTjeneste extends ForeslåBehandlingsresultatTjeneste {
 
     private BehandlingRepository behandlingRepository;
     private Instance<VilkårsPerioderTilVurderingTjeneste> vilkårsPerioderTilVurderingTjenester;
 
-    OLPUttakForeslåBehandlingsresultatTjeneste() {
+    OLPForeslåBehandlingsresultatTjeneste() {
         // for proxy
     }
 
     @Inject
-    public OLPUttakForeslåBehandlingsresultatTjeneste(BehandlingRepositoryProvider repositoryProvider,
-                                                      VedtakVarselRepository vedtakVarselRepository,
-                                                      @Any Instance<VilkårsPerioderTilVurderingTjeneste> vilkårsPerioderTilVurderingTjenester,
-                                                      @FagsakYtelseTypeRef RevurderingBehandlingsresultatutleder revurderingBehandlingsresultatutleder) {
+    public OLPForeslåBehandlingsresultatTjeneste(BehandlingRepositoryProvider repositoryProvider,
+                                                 VedtakVarselRepository vedtakVarselRepository,
+                                                 @Any Instance<VilkårsPerioderTilVurderingTjeneste> vilkårsPerioderTilVurderingTjenester,
+                                                 @FagsakYtelseTypeRef RevurderingBehandlingsresultatutleder revurderingBehandlingsresultatutleder) {
         super(repositoryProvider, vedtakVarselRepository, revurderingBehandlingsresultatutleder);
         this.behandlingRepository = repositoryProvider.getBehandlingRepository();
         this.vilkårsPerioderTilVurderingTjenester = vilkårsPerioderTilVurderingTjenester;
@@ -65,40 +61,6 @@ public class OLPUttakForeslåBehandlingsresultatTjeneste extends ForeslåBehandl
             return behandling.getFagsak().getPeriode();
         }
         return DatoIntervallEntitet.fraOgMedTilOgMed(timeline.getMinLocalDate(), timeline.getMaxLocalDate());
-    }
-
-    @Override
-    protected boolean skalBehandlingenSettesTilAvslått(BehandlingReferanse ref, Vilkårene vilkårene) {
-        if (skalAvslåsBasertPåAndreForhold(ref)) {
-            return true;
-        }
-        Behandling behandling = behandlingRepository.hentBehandling(ref.getBehandlingId());
-        var harIngenPerioderForDefinerendeVilkår = harIngenPerioderForDefinerendeVilkår(behandling, vilkårene);
-        if (harIngenPerioderForDefinerendeVilkår) {
-            return true;
-        }
-
-        final var maksPeriode = getMaksPeriode(ref.getBehandlingId());
-        final var vilkårTidslinjer = vilkårene.getVilkårTidslinjer(maksPeriode);
-
-        return vilkårTidslinjer.entrySet().stream()
-            .anyMatch(e -> harAvslåtteVilkårsPerioder(e.getValue())
-                && harIngenOppfylteVilkårsPerioder(e.getValue())
-            );
-    }
-
-    private boolean harIngenPerioderForDefinerendeVilkår(Behandling behandling, Vilkårene vilkårene) {
-        return definerendeVilkårTyper(behandling)
-            .stream()
-            .anyMatch(it -> harIngenPerioder(it, vilkårene));
-    }
-
-    private boolean harIngenPerioder(VilkårType vilkårType, Vilkårene vilkårene) {
-        return vilkårene.getVilkår(vilkårType).map(Vilkår::getPerioder).orElse(List.of()).isEmpty();
-    }
-
-    private Set<VilkårType> definerendeVilkårTyper(Behandling behandling) {
-        return finnVilkårsperioderTilVurderingTjeneste(behandling).definerendeVilkår();
     }
 
     private VilkårsPerioderTilVurderingTjeneste finnVilkårsperioderTilVurderingTjeneste(Behandling behandling) {
