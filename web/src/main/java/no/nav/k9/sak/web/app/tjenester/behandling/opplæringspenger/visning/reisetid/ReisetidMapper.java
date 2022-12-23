e664101899c588c81ebac8ddc6d13429dad7592c
oyvemb <oyvind.andre.hagen.emblemsvag@nav.no>
2022-12-23 14:16:55 +0100

    fikser mapping

--- web/src/main/java/no/nav/k9/sak/web/app/tjenester/behandling/opplæringspenger/visning/reisetid/ReisetidMapper.java
+++ web/src/main/java/no/nav/k9/sak/web/app/tjenester/behandling/opplæringspenger/visning/reisetid/ReisetidMapper.java
@@ -1,14 +1,14 @@
 package no.nav.k9.sak.web.app.tjenester.behandling.opplæringspenger.visning.reisetid;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import no.nav.fpsak.tidsserie.LocalDateInterval;
 import no.nav.fpsak.tidsserie.LocalDateSegment;
 import no.nav.fpsak.tidsserie.LocalDateTimeline;
-import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
-import no.nav.k9.sak.domene.typer.tid.TidslinjeUtil;
 import no.nav.k9.sak.typer.Periode;
 import no.nav.k9.sak.ytelse.opplaeringspenger.repo.VurdertOpplæringGrunnlag;
 import no.nav.k9.sak.ytelse.opplaeringspenger.repo.VurdertReisetid;
@@ -19,7 +19,7 @@ class ReisetidMapper {
 
     ReisetidDto mapTilDto(VurdertOpplæringGrunnlag grunnlag, Set<PerioderFraSøknad> perioderFraSøknad) {
         List<ReisetidPeriodeDto> perioder = mapPerioder(perioderFraSøknad);
-        List<ReisetidVurderingDto> vurderinger = mapVurderinger(grunnlag, perioder);
+        List<ReisetidVurderingDto> vurderinger = mapVurderinger(grunnlag, perioderFraSøknad);
         return new ReisetidDto(perioder, vurderinger);
     }
 
@@ -38,88 +38,88 @@ class ReisetidMapper {
         return perioder;
     }
 
-    private List<ReisetidVurderingDto> mapVurderinger(VurdertOpplæringGrunnlag grunnlag, List<ReisetidPeriodeDto> perioder) {
-        List<ReisetidVurderingDto> vurderinger = new ArrayList<>();
+    private List<ReisetidVurderingDto> mapVurderinger(VurdertOpplæringGrunnlag grunnlag, Set<PerioderFraSøknad> perioderFraSøknad) {
+        List<ReisetidPeriodeVurderingDto> periodevurderinger = new ArrayList<>();
 
+        List<LocalDateSegment<Boolean>> segmenterVurdert = new ArrayList<>();
         if (grunnlag != null && grunnlag.getVurdertReisetid() != null) {
             for (VurdertReisetid vurdertReisetid : grunnlag.getVurdertReisetid().getReisetid()) {
-                Periode opplæringPeriode = finnOpplæringsperiodeFraReisetid(vurdertReisetid.getPeriode(), perioder);
+                periodevurderinger.add(new ReisetidPeriodeVurderingDto(vurdertReisetid.getPeriode().tilPeriode(),
+                    vurdertReisetid.getGodkjent() ? Resultat.GODKJENT : Resultat.IKKE_GODKJENT,
+                    vurdertReisetid.getBegrunnelse()));
 
-                List<ReisetidPeriodeVurderingDto> reisetidTil = new ArrayList<>();
-                List<ReisetidPeriodeVurderingDto> reisetidHjem = new ArrayList<>();
-
-                if (vurdertReisetid.getPeriode().getFomDato().isBefore(opplæringPeriode.getFom())) {
-                    reisetidTil.add(new ReisetidPeriodeVurderingDto(
-                        vurdertReisetid.getPeriode().tilPeriode(),
-                        vurdertReisetid.getGodkjent() ? Resultat.GODKJENT : Resultat.IKKE_GODKJENT));
-                } else {
-                    reisetidHjem.add(new ReisetidPeriodeVurderingDto(
-                        vurdertReisetid.getPeriode().tilPeriode(),
-                        vurdertReisetid.getGodkjent() ? Resultat.GODKJENT : Resultat.IKKE_GODKJENT));
-                }
-
-                LocalDateTimeline<Boolean> oppgittTidslinjeTil = TidslinjeUtil.tilTidslinjeKomprimert(List.of(perioder.stream()
-                    .filter(reisetidPeriode -> reisetidPeriode.getOpplæringPeriode().equals(opplæringPeriode)).findFirst().orElseThrow().getReisetidTil()));
-                if (harVarighetEnDag(oppgittTidslinjeTil)) {
-                    reisetidTil.add(new ReisetidPeriodeVurderingDto(new Periode(oppgittTidslinjeTil.getMinLocalDate(), oppgittTidslinjeTil.getMaxLocalDate()), Resultat.GODKJENT_AUTOMATISK));
-                } else {
-                    LocalDateTimeline<Boolean> vurdertTidslinjeTil = TidslinjeUtil.tilTidslinjeKomprimert(reisetidTil.stream()
-                        .map(ReisetidPeriodeVurderingDto::getPeriode).toList());
-                    List<Periode> ikkevurdertePerioderTil = TidslinjeUtil.tilPerioder(oppgittTidslinjeTil.disjoint(vurdertTidslinjeTil));
-                    reisetidTil.addAll(ikkevurdertePerioderTil.stream()
-                        .map(ikkeVurdertPeriode -> new ReisetidPeriodeVurderingDto(ikkeVurdertPeriode, Resultat.MÅ_VURDERES)).toList());
-                }
-
-                LocalDateTimeline<Boolean> oppgittTidslinjeHjem = TidslinjeUtil.tilTidslinjeKomprimert(List.of(perioder.stream()
-                    .filter(reisetidPeriode -> reisetidPeriode.getOpplæringPeriode().equals(opplæringPeriode)).findFirst().orElseThrow().getReisetidHjem()));
-                if (harVarighetEnDag(oppgittTidslinjeHjem)) {
-                    reisetidHjem.add(new ReisetidPeriodeVurderingDto(new Periode(oppgittTidslinjeHjem.getMinLocalDate(), oppgittTidslinjeHjem.getMaxLocalDate()), Resultat.GODKJENT_AUTOMATISK));
-                } else {
-                    LocalDateTimeline<Boolean> vurdertTidslinjeHjem = TidslinjeUtil.tilTidslinjeKomprimert(reisetidHjem.stream()
-                        .map(ReisetidPeriodeVurderingDto::getPeriode).toList());
-                    List<Periode> ikkevurdertePerioderHjem = TidslinjeUtil.tilPerioder(oppgittTidslinjeHjem.disjoint(vurdertTidslinjeHjem));
-                    reisetidHjem.addAll(ikkevurdertePerioderHjem.stream()
-                        .map(ikkeVurdertPeriode -> new ReisetidPeriodeVurderingDto(ikkeVurdertPeriode, Resultat.MÅ_VURDERES)).toList());
-                }
-
-                vurderinger.add(new ReisetidVurderingDto(opplæringPeriode, reisetidTil, reisetidHjem, vurdertReisetid.getBegrunnelse()));
+                segmenterVurdert.add(new LocalDateSegment<>(
+                    vurdertReisetid.getPeriode().getFomDato(),
+                    vurdertReisetid.getPeriode().getTomDato(),
+                    true));
             }
         }
 
-        for (ReisetidPeriodeDto periode : perioder) {
-            if (vurderinger.stream().noneMatch(vurdering -> vurdering.getOpplæringPeriode().equals(periode.getOpplæringPeriode()))) {
-                vurderinger.add(new ReisetidVurderingDto(periode.getOpplæringPeriode(),
-                    List.of(new ReisetidPeriodeVurderingDto(
-                        periode.getReisetidTil(),
-                        harVarighetEnDag(periode.getReisetidTil()) ? Resultat.GODKJENT_AUTOMATISK : Resultat.MÅ_VURDERES)),
-                    List.of(new ReisetidPeriodeVurderingDto(
-                        periode.getReisetidHjem(),
-                        harVarighetEnDag(periode.getReisetidHjem()) ? Resultat.GODKJENT_AUTOMATISK : Resultat.MÅ_VURDERES)),
-                    null));
+        List<LocalDateSegment<Resultat>> segmenterOppgitt = new ArrayList<>();
+        for (PerioderFraSøknad fraSøknad : perioderFraSøknad) {
+            for (KursPeriode kursPeriode : fraSøknad.getKurs()) {
+                segmenterOppgitt.add(new LocalDateSegment<>(
+                    kursPeriode.getReiseperiodeTil().getFomDato(),
+                    kursPeriode.getReiseperiodeTil().getTomDato(),
+                    harVarighetEnDag(kursPeriode.getReiseperiodeTil().tilPeriode()) ? Resultat.GODKJENT_AUTOMATISK : Resultat.MÅ_VURDERES));
+                segmenterOppgitt.add(new LocalDateSegment<>(
+                    kursPeriode.getReiseperiodeHjem().getFomDato(),
+                    kursPeriode.getReiseperiodeHjem().getTomDato(),
+                    harVarighetEnDag(kursPeriode.getReiseperiodeHjem().tilPeriode()) ? Resultat.GODKJENT_AUTOMATISK : Resultat.MÅ_VURDERES));
             }
+        }
+
+        LocalDateTimeline<Boolean> vurdertTidslinje = new LocalDateTimeline<>(segmenterVurdert);
+        LocalDateTimeline<Resultat> oppgittTidslinje = new LocalDateTimeline<>(segmenterOppgitt);
+
+        oppgittTidslinje.disjoint(vurdertTidslinje).stream().forEach(segment ->
+            periodevurderinger.add(new ReisetidPeriodeVurderingDto(segment.getFom(), segment.getTom(), segment.getValue(), null)));
+
+        LocalDateTimeline<Periode> tidslinjeMedKursperiode = lagTidslinjeForReisetidMedTilhørendeKursperiode(perioderFraSøknad);
+
+        Map<Periode, List<ReisetidPeriodeVurderingDto>> map = new HashMap<>();
+
+        for (ReisetidPeriodeVurderingDto periodevurdering : periodevurderinger) {
+            Periode kursperiode = tidslinjeMedKursperiode.getSegment(new LocalDateInterval(periodevurdering.getPeriode().getFom(), periodevurdering.getPeriode().getFom())).getValue();
+            List<ReisetidPeriodeVurderingDto> vurderingerForKursperiode = map.containsKey(kursperiode) ? new ArrayList<>(map.get(kursperiode)) : new ArrayList<>();
+            vurderingerForKursperiode.add(periodevurdering);
+            map.put(kursperiode, vurderingerForKursperiode);
+        }
+
+        List<ReisetidVurderingDto> vurderinger = new ArrayList<>();
+
+        for (Map.Entry<Periode, List<ReisetidPeriodeVurderingDto>> entry : map.entrySet()) {
+            Periode kursperiode = entry.getKey();
+            List<ReisetidPeriodeVurderingDto> reisetidTil = new ArrayList<>();
+            List<ReisetidPeriodeVurderingDto> reisetidHjem = new ArrayList<>();
+            for (ReisetidPeriodeVurderingDto periodevurdering : entry.getValue()) {
+                if (periodevurdering.getPeriode().getFom().isBefore(kursperiode.getFom())) {
+                    reisetidTil.add(periodevurdering);
+                } else {
+                    reisetidHjem.add(periodevurdering);
+                }
+            }
+            vurderinger.add(new ReisetidVurderingDto(kursperiode, reisetidTil, reisetidHjem, null));
         }
 
         return vurderinger;
     }
 
-    private Periode finnOpplæringsperiodeFraReisetid(DatoIntervallEntitet reisetid, List<ReisetidPeriodeDto> perioder) {
+    private LocalDateTimeline<Periode> lagTidslinjeForReisetidMedTilhørendeKursperiode(Set<PerioderFraSøknad> perioderFraSøknad) {
         List<LocalDateSegment<Periode>> segmenter = new ArrayList<>();
-        for (ReisetidPeriodeDto periodeDto : perioder) {
-            segmenter.add(new LocalDateSegment<>(
-                periodeDto.getReisetidTil().getFom(),
-                periodeDto.getReisetidTil().getTom(),
-                periodeDto.getOpplæringPeriode()));
-            segmenter.add(new LocalDateSegment<>(
-                periodeDto.getReisetidHjem().getFom(),
-                periodeDto.getReisetidHjem().getTom(),
-                periodeDto.getOpplæringPeriode()));
+        for (PerioderFraSøknad fraSøknad : perioderFraSøknad) {
+            for (KursPeriode kursPeriode : fraSøknad.getKurs()) {
+                segmenter.add(new LocalDateSegment<>(
+                    kursPeriode.getReiseperiodeTil().getFomDato(),
+                    kursPeriode.getReiseperiodeTil().getTomDato(),
+                    kursPeriode.getPeriode().tilPeriode()));
+                segmenter.add(new LocalDateSegment<>(
+                    kursPeriode.getReiseperiodeHjem().getFomDato(),
+                    kursPeriode.getReiseperiodeHjem().getTomDato(),
+                    kursPeriode.getPeriode().tilPeriode()));
+            }
         }
-        LocalDateTimeline<Periode> tidslinje = new LocalDateTimeline<>(segmenter);
-        return tidslinje.getSegment(new LocalDateInterval(reisetid.getFomDato(), reisetid.getTomDato())).getValue();
-    }
-
-    private boolean harVarighetEnDag(LocalDateTimeline<Boolean> tidslinje) {
-        return tidslinje.getMinLocalDate().equals(tidslinje.getMaxLocalDate());
+        return new LocalDateTimeline<>(segmenter);
     }
 
     private boolean harVarighetEnDag(Periode periode) {
--- web/src/main/java/no/nav/k9/sak/web/app/tjenester/behandling/opplæringspenger/visning/reisetid/ReisetidPeriodeVurderingDto.java
+++ web/src/main/java/no/nav/k9/sak/web/app/tjenester/behandling/opplæringspenger/visning/reisetid/ReisetidPeriodeVurderingDto.java
@@ -1,11 +1,15 @@
 package no.nav.k9.sak.web.app.tjenester.behandling.opplæringspenger.visning.reisetid;
 
+import java.time.LocalDate;
+
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import no.nav.k9.sak.typer.Periode;
 
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
@@ -21,9 +25,20 @@ public class ReisetidPeriodeVurderingDto {
     @NotNull
     private Resultat resultat;
 
-    public ReisetidPeriodeVurderingDto(Periode periode, Resultat resultat) {
+    @JsonProperty(value = "begrunnelse", required = true)
+    @Size(max = 4000)
+    @Pattern(regexp = "^[\\p{Graph}\\p{Space}\\p{Sc}\\p{L}\\p{M}\\p{N}§]+$", message = "[${validatedValue}] matcher ikke tillatt pattern [{regexp}]")
+    @Valid
+    private String begrunnelse;
+
+    public ReisetidPeriodeVurderingDto(Periode periode, Resultat resultat, String begrunnelse) {
         this.periode = periode;
         this.resultat = resultat;
+        this.begrunnelse = begrunnelse;
+    }
+
+    public ReisetidPeriodeVurderingDto(LocalDate fom, LocalDate tom, Resultat resultat, String begrunnelse) {
+        this(new Periode(fom, tom), resultat, begrunnelse);
     }
 
     public Periode getPeriode() {
