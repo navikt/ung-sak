0d97b8feb498e2850b1d65897a80bb8ea486beaa
oyvemb <107673448+oyvemb@users.noreply.github.com>
2023-01-26 10:14:37 +0100

    default verdi (#8500)
    * default verdi
    
    * if not exists

--- migreringer/src/main/resources/db/postgres/defaultDS/2.0/V2.0_118__olp_vurdert_av.sql
+++ migreringer/src/main/resources/db/postgres/defaultDS/2.0/V2.0_118__olp_vurdert_av.sql
@@ -1,12 +1,19 @@
-alter table olp_vurdert_institusjon add column vurdert_av VARCHAR(20) not null;
-alter table olp_vurdert_institusjon add column vurdert_tid TIMESTAMP(3) not null;
+alter table olp_vurdert_institusjon add column if not exists vurdert_av VARCHAR(20) default 'VL' not null;
+alter table olp_vurdert_institusjon add column if not exists vurdert_tid TIMESTAMP(3) default CURRENT_TIMESTAMP not null;
+alter table olp_vurdert_institusjon alter column vurdert_av drop default;
+alter table olp_vurdert_institusjon alter column vurdert_tid drop default;
 
-alter table olp_vurdert_opplaering add column vurdert_av VARCHAR(20) not null;
-alter table olp_vurdert_opplaering add column vurdert_tid TIMESTAMP(3) not null;
+alter table olp_vurdert_opplaering add column if not exists vurdert_av VARCHAR(20) default 'VL' not null;
+alter table olp_vurdert_opplaering add column if not exists vurdert_tid TIMESTAMP(3) default CURRENT_TIMESTAMP not null;
+alter table olp_vurdert_opplaering alter column vurdert_av drop default;
+alter table olp_vurdert_opplaering alter column vurdert_tid drop default;
 
-alter table olp_vurdert_opplaering_periode add column vurdert_av VARCHAR(20) not null;
-alter table olp_vurdert_opplaering_periode add column vurdert_tid TIMESTAMP(3) not null;
+alter table olp_vurdert_opplaering_periode add column if not exists vurdert_av VARCHAR(20) default 'VL' not null;
+alter table olp_vurdert_opplaering_periode add column if not exists vurdert_tid TIMESTAMP(3) default CURRENT_TIMESTAMP not null;
+alter table olp_vurdert_opplaering_periode alter column vurdert_av drop default;
+alter table olp_vurdert_opplaering_periode alter column vurdert_tid drop default;
 
-alter table olp_vurdert_reisetid add column vurdert_av VARCHAR(20) not null;
-alter table olp_vurdert_reisetid add column vurdert_tid TIMESTAMP(3) not null;
-
+alter table olp_vurdert_reisetid add column if not exists vurdert_av VARCHAR(20) default 'VL' not null;
+alter table olp_vurdert_reisetid add column if not exists vurdert_tid TIMESTAMP(3) default CURRENT_TIMESTAMP not null;
+alter table olp_vurdert_reisetid alter column vurdert_av drop default;
+alter table olp_vurdert_reisetid alter column vurdert_tid drop default;
