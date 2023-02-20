7e78554a93a36571567028773e110cd37f887651
Stian Grenborgen <stian.grenborgen@nav.no>
2023-02-20 16:59:29 +0100

    Oppdaterer kode før kjøring i prod.

--- migreringer/src/main/resources/db/postgres/defaultDS/2.0/V2.0_059__prosesstask_init_omsorg_brukerdialoginnsyn.sql
+++
@@ -1,4 +0,0 @@
-Insert into PROSESS_TASK_TYPE
-(KODE,NAVN,FEIL_MAKS_FORSOEK,FEIL_SEK_MELLOM_FORSOEK,FEILHANDTERING_ALGORITME,BESKRIVELSE,CRON_EXPRESSION)
-values
-('brukerdialoginnsyn.publiserOmsorg','Publiserer Omsorg-hendelse til brukerdialoginnsyn',1,60,'DEFAULT','Publiserer Omsorg-hendelse til brukerdialoginnsyn',null) ON CONFLICT DO NOTHING;
