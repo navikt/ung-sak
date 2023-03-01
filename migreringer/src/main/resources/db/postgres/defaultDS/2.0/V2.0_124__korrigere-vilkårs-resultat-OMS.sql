8193e7e2ac251a5d023c319e8e073590585a06b1
Helga Nyrud <helga.nyrud@nav.no>
2023-03-01 15:30:19 +0100

    Setter omsorgen for til oppfylt for de sakene som gikk gjennom i starten av 2023

--- migreringer/src/main/resources/db/postgres/defaultDS/2.0/V2.0_124__korrigere-vilkårs-resultat-OMS.sql
+++ migreringer/src/main/resources/db/postgres/defaultDS/2.0/V2.0_124__korrigere-vilkårs-resultat-OMS.sql
@@ -4,18 +4,10 @@ WHERE utfall = 'IKKE_VURDERT'
   and vilkar_id IN (SELECT vv.id
                     from vr_vilkar vv
                              INNER JOIN rs_vilkars_resultat rv on vv.vilkar_resultat_id = rv.vilkarene_id
+                             INNER JOIN vr_vilkar_periode vp on vv.id = vp.vilkar_id
                              INNER JOIN behandling b on rv.behandling_id = b.id
                              INNER JOIN fagsak f on b.fagsak_id = f.id
-                    WHERE f.saksnummer in ('CTXZ6',
-                                           'CTZBS',
-                                           'CUDWS',
-                                           'CUGSY',
-                                           'CV0XY',
-                                           'CVT12',
-                                           'CVT3U',
-                                           'CW6SW',
-                                           'CW7M2',
-                                           'CWABA',
-                                           'CWARY')
+                    WHERE f.ytelse_type = 'OMP'
                       and rv.aktiv = true
-                      and vv.vilkar_type = 'FP_VK_1');
+                      and b.behandling_status = 'AVSLU'
+                      and vv.vilkar_type = 'K9_VK_1');
