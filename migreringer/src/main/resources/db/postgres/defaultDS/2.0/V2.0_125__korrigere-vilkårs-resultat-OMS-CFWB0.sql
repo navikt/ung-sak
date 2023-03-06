5c76ebc8b7b2ad0df2b0877bc5792f752b9dc0df
Espen Johansen Velsvik <espen.velsvik@nav.no>
2023-03-06 11:24:02 +0100

    Korrigerer patch for vilkårsperiode

--- migreringer/src/main/resources/db/postgres/defaultDS/2.0/V2.0_125__korrigere-vilkårs-resultat-OMS-CFWB0.sql
+++ migreringer/src/main/resources/db/postgres/defaultDS/2.0/V2.0_125__korrigere-vilkårs-resultat-OMS-CFWB0.sql
@@ -1,18 +1,11 @@
-UPDATE vr_vilkar_periode periodeTilOppdatering
-SET utfall           = sub.utfall,
-    regel_evaluering = sub.regel_evaluering FROM (SELECT vp.utfall, vp.regel_evaluering, vp.fom
-                                                  from vr_vilkar vv
-                                                           INNER JOIN rs_vilkars_resultat rv on vv.vilkar_resultat_id = rv.vilkarene_id
-                                                           INNER JOIN vr_vilkar_periode vp on vv.id = vp.vilkar_id
-                                                  WHERE rv.aktiv = true
-                                                    and rv.behandling_id = 1611637
-                                                    and vv.vilkar_type = 'FP_VK_23') as sub
-WHERE periodeTilOppdatering.fom = sub.fom
-  and periodeTilOppdatering.utfall = 'IKKE_VURDERT'
-  and periodeTilOppdatering.id IN (SELECT vp.id
-    from vr_vilkar vv
-    INNER JOIN rs_vilkars_resultat rv on vv.vilkar_resultat_id = rv.vilkarene_id
-    INNER JOIN vr_vilkar_periode vp on vv.id = vp.vilkar_id
-    WHERE rv.aktiv = true
-  and rv.behandling_id = 1611655
-  and vv.vilkar_type = 'FP_VK_23');
+UPDATE vr_vilkar_periode
+SET utfall = 'OPPFYLT',
+    regel_evaluering = 'Utfall kopiert fra behandling 1611637 (TSF-3211)'
+WHERE id IN (SELECT vp.id
+             from vr_vilkar vv
+                      INNER JOIN rs_vilkars_resultat rv on vv.vilkar_resultat_id = rv.vilkarene_id
+                      INNER JOIN vr_vilkar_periode vp on vv.id = vp.vilkar_id
+             WHERE rv.aktiv = true
+               and vp.utfall = 'IKKE_VURDERT'
+               and rv.behandling_id = 1611655
+               and vv.vilkar_type = 'FP_VK_23');
