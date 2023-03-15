d764b96af391832b0d541cd8dc8b6c409c2ef5ce
oyvemb <oyvind.andre.hagen.emblemsvag@nav.no>
2023-03-15 08:14:16 +0100

    prepared statement

--- domenetjenester/inngangsvilkar/src/main/java/no/nav/k9/sak/inngangsvilkår/medlemskap/MigrerMedlemskapVurdertAvTask.java
+++ domenetjenester/inngangsvilkar/src/main/java/no/nav/k9/sak/inngangsvilkår/medlemskap/MigrerMedlemskapVurdertAvTask.java
@@ -31,7 +31,7 @@ public class MigrerMedlemskapVurdertAvTask implements ProsessTaskHandler {
     @Override
     public void doTask(ProsessTaskData pd) {
         final Long behandlingId = Long.parseLong(pd.getBehandlingId());
-        final Query q = entityManager.createNativeQuery("with gr as (select * from gr_medlemskap where behandling_id = " + behandlingId + ") " +
+        final Query q = entityManager.createNativeQuery("with gr as (select * from gr_medlemskap where behandling_id = :behandlingId) " +
             "update medlemskap_vurdering_lopende it " +
             "set vurdert_av = " +
                 "(select mvl.opprettet_av from medlemskap_vurdering_lopende mvl " +
@@ -44,6 +44,7 @@ public class MigrerMedlemskapVurdertAvTask implements ProsessTaskHandler {
                 "and mvl.oppholdsrett_vurdering is not distinct from it.oppholdsrett_vurdering and mvl.lovlig_opphold_vurdering is not distinct from it.lovlig_opphold_vurdering and mvl.bosatt_vurdering is not distinct from it.bosatt_vurdering and mvl.er_eos_borger is not distinct from it.er_eos_borger and mvl.vurderingsdato = it.vurderingsdato and mvl.begrunnelse is not distinct from it.begrunnelse and mvl.manuell_vurd = it.manuell_vurd " +
                 "order by opprettet_tid limit 1) " +
             "where vurdert_periode_id in (select vurdering_lopende_id from gr) and vurdert_av is null and vurdert_tid is null");
+        q.setParameter("behandlingId", behandlingId);
         final int antall = q.executeUpdate();
         logger.info(TASKTYPE + " oppdatert " + antall + " rader");
     }
