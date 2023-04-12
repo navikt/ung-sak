9e6ae0683b032fd5d058538b7e5accd6a2d24a78
Qadeer Ahmad Khan <qadeer.ahmad.khan@nav.no>
2023-04-12 15:40:03 +0200

    Dropper implementasjon av utledRevurderingPerioder slik at det blir som før. Perioder fra dokumenter fra andre parter er (trolig) ikke relevant på omsorgspenger...

--- ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/vilkår/OMPVilkårsPerioderTilVurderingTjeneste.java
+++ ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/vilkår/OMPVilkårsPerioderTilVurderingTjeneste.java
@@ -19,7 +19,6 @@ import jakarta.inject.Inject;
 import no.nav.k9.aarskvantum.kontrakter.Aktivitet;
 import no.nav.k9.aarskvantum.kontrakter.Periodetype;
 import no.nav.k9.aarskvantum.kontrakter.Uttaksperiode;
-import no.nav.k9.kodeverk.behandling.BehandlingÅrsakType;
 import no.nav.k9.kodeverk.vilkår.VilkårType;
 import no.nav.k9.sak.behandling.BehandlingReferanse;
 import no.nav.k9.sak.behandlingskontroll.BehandlingTypeRef;
@@ -34,12 +33,10 @@ import no.nav.k9.sak.behandlingslager.behandling.vilkår.periode.VilkårPeriode;
 import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
 import no.nav.k9.sak.inngangsvilkår.UtledeteVilkår;
 import no.nav.k9.sak.inngangsvilkår.VilkårUtleder;
-import no.nav.k9.sak.perioder.PeriodeMedÅrsak;
 import no.nav.k9.sak.perioder.VilkårsPerioderTilVurderingTjeneste;
 import no.nav.k9.sak.perioder.VilkårsPeriodiseringsFunksjon;
 import no.nav.k9.sak.trigger.ProsessTriggere;
 import no.nav.k9.sak.trigger.ProsessTriggereRepository;
-import no.nav.k9.sak.utsatt.UtsattBehandlingAvPeriode;
 import no.nav.k9.sak.ytelse.omsorgspenger.inntektsmelding.KravDokumentFravær;
 import no.nav.k9.sak.ytelse.omsorgspenger.repo.OmsorgspengerGrunnlagRepository;
 import no.nav.k9.sak.ytelse.omsorgspenger.repo.OppgittFraværPeriode;
@@ -210,21 +207,4 @@ public class OMPVilkårsPerioderTilVurderingTjeneste implements VilkårsPerioder
         return erKantIKantVurderer;
     }
 
-    @Override
-    public NavigableSet<PeriodeMedÅrsak> utledRevurderingPerioder(BehandlingReferanse referanse) {
-        if (!referanse.erRevurdering())
-            return new TreeSet<>();
-
-        var behandling = behandlingRepository.hentBehandling(referanse.getBehandlingId());
-        var periodeMedÅrsaks = new TreeSet<PeriodeMedÅrsak>();
-
-        periodeMedÅrsaks.addAll(revurderingPerioderTjeneste.utledPerioderFraProsessTriggereMedÅrsak(referanse));
-
-        periodeMedÅrsaks.addAll(revurderingPerioderTjeneste.utledPerioderFraInntektsmeldinger(referanse, utledFullstendigePerioder(behandling.getId()))
-            .stream()
-            .map(it -> new PeriodeMedÅrsak(it, BehandlingÅrsakType.RE_ENDRET_INNTEKTSMELDING))
-            .collect(Collectors.toSet()));
-
-        return periodeMedÅrsaks;
-    }
 }
