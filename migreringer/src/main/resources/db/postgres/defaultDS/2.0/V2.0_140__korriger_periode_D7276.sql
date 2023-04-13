8098acce0023c8ca69f04880eb5034d691170721
Helga Nyrud <helga.nyrud@nav.no>
2023-04-13 15:40:50 +0200

    OMP_KS Setter fagsak til riktig periode og resultat til avslått så det ikke skal se misvisende ut i visning

---
+++ migreringer/src/main/resources/db/postgres/defaultDS/2.0/V2.0_140__korriger_periode_D7276.sql
@@ -0,0 +1,4 @@
+--se TSF-3238
+update fagsak set periode='[2023-02-16, 2023-02-16]' where saksnummer ='D7276';
+
+update behandling set behandling_resultat_type = 'AVSLÅTT' where id = '1660384' amd uuid = 'f15227ab-559f-48f0-933b-8c2b7f53e672';
