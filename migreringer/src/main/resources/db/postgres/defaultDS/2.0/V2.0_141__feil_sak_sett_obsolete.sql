3b29767296cdc94ec08e3bc0444f6df99b70be39
oyvemb <107673448+oyvemb@users.noreply.github.com>
2023-04-24 15:20:53 +0200

    sett saker til obsolete (#8957)

---
+++ migreringer/src/main/resources/db/postgres/defaultDS/2.0/V2.0_141__feil_sak_sett_obsolete.sql
@@ -0,0 +1,3 @@
+--se TSF-3350
+update fagsak set ytelse_type='OBSOLETE'
+              where saksnummer in ('D38GU','DHG1i','DHFJG','DHFGo','DHFDW','DHEDS','DHE9M','DHDSY','DHDKM','DHCVM','DHBVi','DHBRC','DHBKE','DHBHM','DHBEU','DHBDG','DHB9A','DHB0Y');
