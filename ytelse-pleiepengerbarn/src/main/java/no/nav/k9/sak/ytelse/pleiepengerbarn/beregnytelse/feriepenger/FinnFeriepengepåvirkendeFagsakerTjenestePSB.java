95f4412ec42133884de58eee683942ed5e888149
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2023-05-03 10:54:27 +0200

    Hensynta saker OLP/PSB i infotrygd ved utregning av feriepenger

--- ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/beregnytelse/feriepenger/FinnFeriepengepåvirkendeFagsakerTjenestePSB.java
+++ ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/beregnytelse/feriepenger/FinnFeriepengepåvirkendeFagsakerTjenestePSB.java
@@ -3,6 +3,7 @@ package no.nav.k9.sak.ytelse.pleiepengerbarn.beregnytelse.feriepenger;
 import static no.nav.k9.kodeverk.behandling.FagsakYtelseType.OPPLÆRINGSPENGER;
 import static no.nav.k9.kodeverk.behandling.FagsakYtelseType.PLEIEPENGER_SYKT_BARN;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -10,11 +11,22 @@ import java.util.stream.Stream;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import no.nav.abakus.iaygrunnlag.kodeverk.Inntektskategori;
+import no.nav.fpsak.tidsserie.LocalDateSegment;
+import no.nav.fpsak.tidsserie.LocalDateTimeline;
+import no.nav.fpsak.tidsserie.StandardCombinators;
+import no.nav.k9.felles.konfigurasjon.konfig.KonfigVerdi;
+import no.nav.k9.kodeverk.Fagsystem;
 import no.nav.k9.kodeverk.behandling.FagsakYtelseType;
 import no.nav.k9.sak.behandling.BehandlingReferanse;
 import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
 import no.nav.k9.sak.behandlingslager.fagsak.Fagsak;
 import no.nav.k9.sak.behandlingslager.fagsak.FagsakRepository;
+import no.nav.k9.sak.domene.arbeidsforhold.InntektArbeidYtelseTjeneste;
+import no.nav.k9.sak.domene.iay.modell.InntektArbeidYtelseGrunnlag;
+import no.nav.k9.sak.domene.iay.modell.Ytelse;
+import no.nav.k9.sak.domene.iay.modell.YtelseAnvistAndel;
+import no.nav.k9.sak.ytelse.beregning.regler.feriepenger.SaksnummerOgSisteBehandling;
 
 @FagsakYtelseTypeRef(PLEIEPENGER_SYKT_BARN)
 @FagsakYtelseTypeRef(OPPLÆRINGSPENGER)
@@ -22,23 +34,69 @@ import no.nav.k9.sak.behandlingslager.fagsak.FagsakRepository;
 public class FinnFeriepengepåvirkendeFagsakerTjenestePSB implements FinnFeriepengepåvirkendeFagsakerTjeneste {
 
     private FagsakRepository fagsakRepository;
+    private HentFeriepengeAndelerTjeneste hentFeriepengeAndelerTjeneste;
+    private InntektArbeidYtelseTjeneste iayTjeneste;
+    private boolean korrigerMotInfotrygd;
 
     FinnFeriepengepåvirkendeFagsakerTjenestePSB() {
         //for CDI proxy
     }
 
     @Inject
-    public FinnFeriepengepåvirkendeFagsakerTjenestePSB(FagsakRepository fagsakRepository) {
+    public FinnFeriepengepåvirkendeFagsakerTjenestePSB(FagsakRepository fagsakRepository,
+                                                       HentFeriepengeAndelerTjeneste hentFeriepengeAndelerTjeneste,
+                                                       InntektArbeidYtelseTjeneste iayTjeneste,
+                                                       @KonfigVerdi(value = "FERIEPENGER_INFOTRYGD_KORRIGER", defaultVerdi = "false") boolean korrigerMotInfotrygd) {
         this.fagsakRepository = fagsakRepository;
+        this.hentFeriepengeAndelerTjeneste = hentFeriepengeAndelerTjeneste;
+        this.iayTjeneste = iayTjeneste;
+        this.korrigerMotInfotrygd = korrigerMotInfotrygd;
     }
 
     @Override
-    public Set<Fagsak> finnSakerSomPåvirkerFeriepengerFor(BehandlingReferanse behandlingReferanse) {
-        List<Fagsak> psbFagsakerPleietrengende = fagsakRepository.finnFagsakRelatertTil(FagsakYtelseType.PSB, behandlingReferanse.getAktørId(), behandlingReferanse.getPleietrengendeAktørId(), null, null, null);
-        List<Fagsak> oppFagsakerPleietrengende = fagsakRepository.finnFagsakRelatertTil(FagsakYtelseType.OPPLÆRINGSPENGER, behandlingReferanse.getAktørId(), behandlingReferanse.getPleietrengendeAktørId(), null, null, null);
+    public LocalDateTimeline<Set<SaksnummerOgSisteBehandling>> finnPåvirkedeSaker(BehandlingReferanse referanse) {
+        LocalDateTimeline<Set<SaksnummerOgSisteBehandling>> påvirkendeLokaleSaker = finnPåvirkendeLokaleSaker(referanse);
+        if (korrigerMotInfotrygd) {
+            LocalDateTimeline<Set<SaksnummerOgSisteBehandling>> påvirkendeInfotrygdsaker = finnPåvirkendeInfotrygdsaker(referanse);
+            return påvirkendeLokaleSaker.crossJoin(påvirkendeInfotrygdsaker, StandardCombinators::union);
+        } else {
+            return påvirkendeLokaleSaker;
+        }
+    }
+
+    private LocalDateTimeline<Set<SaksnummerOgSisteBehandling>> finnPåvirkendeLokaleSaker(BehandlingReferanse referanse) {
+        Set<Fagsak> påvirkendeFagsaker = finnLokaleSakerSomPåvirkerFeriepengerFor(referanse);
+        return hentFeriepengeAndelerTjeneste.finnAndelerSomKanGiFeriepenger(påvirkendeFagsaker);
+    }
+
+    private LocalDateTimeline<Set<SaksnummerOgSisteBehandling>> finnPåvirkendeInfotrygdsaker(BehandlingReferanse referanse) {
+        InntektArbeidYtelseGrunnlag iayGrunnlag = iayTjeneste.hentGrunnlag(referanse.getId());
+        List<Ytelse> infotrygdYtelse = iayGrunnlag.getAktørYtelseFraRegister(referanse.getAktørId())
+            .stream()
+            .flatMap(ay -> ay.getAlleYtelser().stream())
+            .filter(ay -> ay.getKilde() == Fagsystem.INFOTRYGD)
+            .filter(ay -> ay.getYtelseType() == OPPLÆRINGSPENGER || ay.getYtelseType() == PLEIEPENGER_SYKT_BARN)
+            .filter(ay -> ay.getYtelseAnvist().stream().flatMap(ya -> ya.getYtelseAnvistAndeler().stream()).anyMatch(this::kanHaFereipenger))
+            .toList();
+
+        long dummyInfotrygdBehandlingId = 0;
+        List<LocalDateSegment<Set<SaksnummerOgSisteBehandling>>> segmenter = infotrygdYtelse.stream().map(y -> new LocalDateSegment<>(y.getPeriode().toLocalDateInterval(), Set.of(new SaksnummerOgSisteBehandling(y.getSaksnummer(), dummyInfotrygdBehandlingId)))).toList();
+        return new LocalDateTimeline<>(segmenter, StandardCombinators::union);
+    }
+
+    private boolean kanHaFereipenger(YtelseAnvistAndel andel) {
+        boolean inntektskategoriMedFereiepenger = andel.getInntektskategori() == Inntektskategori.ARBEIDSTAKER || andel.getInntektskategori() == Inntektskategori.SJØMANN;
+        boolean harTilkjentYtelse = andel.getDagsats() != null && andel.getDagsats().getVerdi().compareTo(BigDecimal.ZERO) > 0;
+        return inntektskategoriMedFereiepenger && harTilkjentYtelse;
+
+    }
+
+    private Set<Fagsak> finnLokaleSakerSomPåvirkerFeriepengerFor(BehandlingReferanse referanse) {
+        List<Fagsak> psbFagsakerPleietrengende = fagsakRepository.finnFagsakRelatertTil(FagsakYtelseType.PSB, referanse.getAktørId(), referanse.getPleietrengendeAktørId(), null, null, null);
+        List<Fagsak> oppFagsakerPleietrengende = fagsakRepository.finnFagsakRelatertTil(FagsakYtelseType.OPPLÆRINGSPENGER, referanse.getAktørId(), referanse.getPleietrengendeAktørId(), null, null, null);
 
         return Stream.concat(psbFagsakerPleietrengende.stream(), oppFagsakerPleietrengende.stream())
-            .filter(s -> !s.getSaksnummer().equals(behandlingReferanse.getSaksnummer()))
+            .filter(s -> !s.getSaksnummer().equals(referanse.getSaksnummer()))
             .collect(Collectors.toSet());
     }
 }
