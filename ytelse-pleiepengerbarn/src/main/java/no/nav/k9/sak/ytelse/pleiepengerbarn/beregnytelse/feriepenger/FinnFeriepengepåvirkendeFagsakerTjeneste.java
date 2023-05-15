22fe12338bd610cc2b4245dc3d551d978700b7b3
Stian Grenborgen <stian.grenborgen@nav.no>
2023-05-15 11:31:30 +0200

    Revert "Revert "Hensynta saker OLP/PSB i infotrygd ved utregning av feriepenger (#9009)""
    This reverts commit 5d1337945c270c31ed46eb60e934d97c8eb9aef4.

--- ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/beregnytelse/feriepenger/FinnFeriepengepåvirkendeFagsakerTjeneste.java
+++ ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/beregnytelse/feriepenger/FinnFeriepengepåvirkendeFagsakerTjeneste.java
@@ -3,15 +3,16 @@ package no.nav.k9.sak.ytelse.pleiepengerbarn.beregnytelse.feriepenger;
 import java.util.Set;
 
 import jakarta.enterprise.inject.Instance;
+import no.nav.fpsak.tidsserie.LocalDateTimeline;
 import no.nav.k9.kodeverk.behandling.FagsakYtelseType;
 import no.nav.k9.sak.behandling.BehandlingReferanse;
 import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
-import no.nav.k9.sak.behandlingslager.fagsak.Fagsak;
+import no.nav.k9.sak.ytelse.beregning.regler.feriepenger.SaksnummerOgSisteBehandling;
 
 public interface FinnFeriepengepåvirkendeFagsakerTjeneste {
     static FinnFeriepengepåvirkendeFagsakerTjeneste finnTjeneste(Instance<FinnFeriepengepåvirkendeFagsakerTjeneste> feriepengepåvirkendeFagsakerTjenester, FagsakYtelseType fagsakYtelseType) {
         return FagsakYtelseTypeRef.Lookup.find(feriepengepåvirkendeFagsakerTjenester, fagsakYtelseType).orElseThrow();
     }
 
-    Set<Fagsak> finnSakerSomPåvirkerFeriepengerFor(BehandlingReferanse behandlingReferanse);
+    LocalDateTimeline<Set<SaksnummerOgSisteBehandling>> finnPåvirkedeSaker(BehandlingReferanse behandling);
 }
