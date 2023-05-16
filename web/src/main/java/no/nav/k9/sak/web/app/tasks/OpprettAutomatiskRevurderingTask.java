7ba5e8e29c2ffd93f45cef6e42e0bcd8e14139ba
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2023-05-16 14:14:11 +0200

    Task for å opprette automatisk revurdering

---
+++ web/src/main/java/no/nav/k9/sak/web/app/tasks/OpprettAutomatiskRevurderingTask.java
@@ -0,0 +1,35 @@
+package no.nav.k9.sak.web.app.tasks;
+
+import java.util.Objects;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import no.nav.k9.kodeverk.behandling.BehandlingÅrsakType;
+import no.nav.k9.prosesstask.api.ProsessTask;
+import no.nav.k9.prosesstask.api.ProsessTaskData;
+import no.nav.k9.prosesstask.api.ProsessTaskHandler;
+import no.nav.k9.sak.typer.Saksnummer;
+
+@ApplicationScoped
+@ProsessTask(OpprettAutomatiskRevurderingTask.TASKTYPE)
+public class OpprettAutomatiskRevurderingTask implements ProsessTaskHandler {
+    public static final String TASKTYPE = "forvaltning.opprettAutomatiskRevurdering";
+
+    private OpprettRevurderingService opprettRevurderingService;
+
+    protected OpprettAutomatiskRevurderingTask() {
+        // CDI proxy
+    }
+
+    @Inject
+    public OpprettAutomatiskRevurderingTask(OpprettRevurderingService opprettRevurderingService) {
+        this.opprettRevurderingService = opprettRevurderingService;
+    }
+
+    @Override
+    public void doTask(ProsessTaskData pd) {
+        var saksnummer = Objects.requireNonNull(pd.getSaksnummer(), "saksnummer");
+        BehandlingÅrsakType revurderingÅrsak = BehandlingÅrsakType.fraKode(Objects.requireNonNull(pd.getPropertyValue("revurderingÅrsak"), "revurderingÅrsak"));
+        opprettRevurderingService.opprettAutomatiskRevurdering(new Saksnummer(saksnummer), revurderingÅrsak);
+    }
+}
