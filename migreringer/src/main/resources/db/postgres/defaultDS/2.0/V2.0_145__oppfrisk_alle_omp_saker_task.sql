e59dddc47f2e76c7090ae787d49290bbe0c3e266
oyvemb <oyvind.andre.hagen.emblemsvag@nav.no>
2023-06-06 13:16:59 +0200

    Lar være å inserte task med en gang for å kunne prodsette uten risiko (task må opprettes manuelt med swagger)

--- migreringer/src/main/resources/db/postgres/defaultDS/2.0/V2.0_145__oppfrisk_alle_omp_saker_task.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO prosess_task (id, task_type, task_gruppe, neste_kjoering_etter, task_parametere)
-VALUES (nextval('seq_prosess_task'), 'omp.oppfriskalle', nextval('seq_prosess_task_gruppe'), current_date + time '23:30', null);
