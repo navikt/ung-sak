f3159d0b7af7772cdd8dcbee2dc94684326a37dc
oyvemb <oyvind.andre.hagen.emblemsvag@nav.no>
2023-06-22 10:19:14 +0200

    Håndtere dødsfall i helgen rett før eller etter periode med psb

--- ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/uttak/død/VilkårForlengingTjeneste.java
+++ ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/uttak/død/VilkårForlengingTjeneste.java
@@ -1,12 +1,17 @@
 package no.nav.k9.sak.ytelse.pleiepengerbarn.uttak.død;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Optional;
 import java.util.Set;
 import java.util.TreeSet;
 
 import no.nav.k9.kodeverk.vilkår.VilkårType;
 import no.nav.k9.sak.behandlingslager.behandling.personopplysning.PersonopplysningEntitet;
+import no.nav.k9.sak.behandlingslager.behandling.vilkår.Vilkår;
 import no.nav.k9.sak.behandlingslager.behandling.vilkår.VilkårResultatBuilder;
 import no.nav.k9.sak.behandlingslager.behandling.vilkår.Vilkårene;
+import no.nav.k9.sak.behandlingslager.behandling.vilkår.periode.VilkårPeriode;
 import no.nav.k9.sak.domene.behandling.steg.inngangsvilkår.alder.VurderAldersVilkårTjeneste;
 import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
 
@@ -25,15 +30,36 @@ class VilkårForlengingTjeneste {
         resultatBuilder.leggTil(vilkårBuilder);
     }
 
-    public void forlengeVilkårMedPeriode(Set<VilkårType> vilkår, VilkårResultatBuilder resultatBuilder, Vilkårene vilkårene, DatoIntervallEntitet periode) {
+    public void forlengeVilkårMedPeriode(Set<VilkårType> vilkår, VilkårResultatBuilder resultatBuilder, Vilkårene vilkårene, DatoIntervallEntitet periode, LocalDate dødsdato, boolean dødsdatoIHelgFiks) {
         for (VilkårType vilkårType : vilkår) {
             var vilkårBuilder = resultatBuilder.hentBuilderFor(vilkårType);
-            var dødsdato = periode.getFomDato().minusDays(1); //periode begynner 1 dag etter dødsdato
-            var eksisterendeResultat = vilkårene.getVilkår(vilkårType).orElseThrow().finnPeriodeSomInneholderDato(dødsdato).orElseThrow();
+            var eksisterendeResultat = finnVurderingPåDødsdato(dødsdato, vilkårene.getVilkår(vilkårType).orElseThrow(), dødsdatoIHelgFiks).orElseThrow();
 
             vilkårBuilder.leggTil(vilkårBuilder.hentBuilderFor(periode).forlengelseAv(eksisterendeResultat));
             resultatBuilder.leggTil(vilkårBuilder);
         }
+    }
+
+    public Optional<VilkårPeriode> finnVurderingPåDødsdato(LocalDate dødsdato, Vilkår vilkår, boolean dødsdatoIHelgFiks) {
+        Optional<VilkårPeriode> vilkårPeriodeForDødsdato = vilkår.finnPeriodeSomInneholderDato(dødsdato);
+
+        if (vilkårPeriodeForDødsdato.isEmpty() && dødsdatoIHelgFiks) {
+            final DayOfWeek ukedag = dødsdato.getDayOfWeek();
+            if (ukedag == DayOfWeek.SATURDAY) {
+                vilkårPeriodeForDødsdato = vilkår.finnPeriodeSomInneholderDato(dødsdato.plusDays(2));
+                if (vilkårPeriodeForDødsdato.isEmpty()) {
+                    vilkårPeriodeForDødsdato = vilkår.finnPeriodeSomInneholderDato(dødsdato.minusDays(1));
+                }
+            }
+            if (ukedag == DayOfWeek.SUNDAY) {
+                vilkårPeriodeForDødsdato = vilkår.finnPeriodeSomInneholderDato(dødsdato.plusDays(1));
+                if (vilkårPeriodeForDødsdato.isEmpty()) {
+                    vilkårPeriodeForDødsdato = vilkår.finnPeriodeSomInneholderDato(dødsdato.minusDays(2));
+                }
+            }
+        }
+
+        return vilkårPeriodeForDødsdato;
     }
 }
 
