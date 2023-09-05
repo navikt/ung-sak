1001f12cfe8e04035ad9c54725d5ddd63e758ea5
Bjørn Erik Lømo <bjorn.erik.lomo@nav.no>
2023-09-05 14:54:01 +0200

    Codereview

--- kontrakt/src/main/java/no/nav/k9/sak/kontrakt/behandling/BehandlingProsessHendelse.java
+++ kontrakt/src/main/java/no/nav/k9/sak/kontrakt/behandling/BehandlingProsessHendelse.java
@@ -146,7 +146,6 @@ public class BehandlingProsessHendelse {
 
     @JsonSerialize(using = ToStringSerializer.class)
     @JsonDeserialize(using = LocalDateTimeDeserializer.class)
-    @NotNull
     @Valid
     @JsonProperty(value = "eldsteDatoMedEndringFraSøker", required = true)
     private LocalDateTime eldsteDatoMedEndringFraSøker;
