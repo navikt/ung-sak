1de35fb5b5c49822868af41f526577d177271c32
Espen Johansen Velsvik <espen.velsvik@nav.no>
2023-09-12 13:43:46 +0200

    Legger på logging for startpunktperioder

--- behandlingsprosess/src/main/java/no/nav/k9/sak/domene/behandling/steg/beregningsgrunnlag/KalkulusStartpunktUtleder.java
+++ behandlingsprosess/src/main/java/no/nav/k9/sak/domene/behandling/steg/beregningsgrunnlag/KalkulusStartpunktUtleder.java
@@ -7,6 +7,9 @@ import java.util.NavigableSet;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import no.nav.k9.kodeverk.behandling.BehandlingStegType;
@@ -22,6 +25,7 @@ public class KalkulusStartpunktUtleder {
     private VilkårPeriodeFilterProvider vilkårPeriodeFilterProvider;
     private BeregningsgrunnlagVilkårTjeneste vilkårTjeneste;
 
+    private static final Logger log = LoggerFactory.getLogger(KalkulusStartpunktUtleder.class);
 
     @Inject
     public KalkulusStartpunktUtleder(FinnPerioderMedStartIKontrollerFakta finnPerioderMedStartIKontrollerFakta,
@@ -35,7 +39,8 @@ public class KalkulusStartpunktUtleder {
     public KalkulusStartpunktUtleder() {
     }
 
-    /** Utleder Map fra BehandlingStegType til en samling PeriodeTilVurdering som sier hvilke perioder som har det gitte steget som startpunkt.
+    /**
+     * Utleder Map fra BehandlingStegType til en samling PeriodeTilVurdering som sier hvilke perioder som har det gitte steget som startpunkt.
      * <p>
      * Periodene pr behandlingsstegtype vil ha kopiert beregningsgrunnlag fra steget før og kjører beregning for alle steg etter (se BehandlingModell)
      *
@@ -48,14 +53,28 @@ public class KalkulusStartpunktUtleder {
         periodeFilter.ignorerAvslåtteUnntattForLavtBeregningsgrunnlag();
         var utenAvslagFørBeregning = vilkårTjeneste.utledDetaljertPerioderTilVurdering(ref, periodeFilter);
 
+        if (!utenAvslagFørBeregning.isEmpty()) {
+            log.info("Perioder som skal vurderes i beregning: " + utenAvslagFørBeregning);
+        }
+
         if (ref.getBehandlingType().equals(BehandlingType.REVURDERING)) {
+
             var forlengelseperioder = utenAvslagFørBeregning.stream().filter(PeriodeTilVurdering::erForlengelse).collect(Collectors.toCollection(TreeSet::new));
+            if (!forlengelseperioder.isEmpty()) {
+                log.info("Perioder med start i vurder refusjon: " + forlengelseperioder);
+            }
             settStartpunkt(forlengelseperioder, periodeStartStegMap, BehandlingStegType.VURDER_REF_BERGRUNN);
             var startIKontrollerFaktaBeregning = finnPerioderMedStartIKontrollerFakta.finnPerioder(ref, utenAvslagFørBeregning, forlengelseperioder);
+            if (!startIKontrollerFaktaBeregning.isEmpty()) {
+                log.info("Perioder med start i kontroller fakta beregning: " + startIKontrollerFaktaBeregning);
+            }
             settStartpunkt(startIKontrollerFaktaBeregning, periodeStartStegMap, BehandlingStegType.KONTROLLER_FAKTA_BEREGNING);
         }
 
         var perioderFraStart = finnPerioderFraStart(periodeStartStegMap, utenAvslagFørBeregning);
+        if (!perioderFraStart.isEmpty()) {
+            log.info("Perioder med start i fastsett skjæringstidspunkt: " + perioderFraStart);
+        }
         settStartpunkt(perioderFraStart, periodeStartStegMap, BehandlingStegType.FASTSETT_SKJÆRINGSTIDSPUNKT_BEREGNING);
         return periodeStartStegMap;
 
