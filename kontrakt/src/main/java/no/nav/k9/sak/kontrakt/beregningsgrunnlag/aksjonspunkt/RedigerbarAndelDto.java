cd8007abffc838b2e8b01a2147978e9d2c669729
Espen Johansen Velsvik <espen.velsvik@nav.no>
2023-09-21 12:03:27 +0200

    Tillater at andelsnr er lik null når det er ny andel

--- kontrakt/src/main/java/no/nav/k9/sak/kontrakt/beregningsgrunnlag/aksjonspunkt/RedigerbarAndelDto.java
+++ kontrakt/src/main/java/no/nav/k9/sak/kontrakt/beregningsgrunnlag/aksjonspunkt/RedigerbarAndelDto.java
@@ -1,21 +1,22 @@
 package no.nav.k9.sak.kontrakt.beregningsgrunnlag.aksjonspunkt;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 @JsonAutoDetect(getterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE, fieldVisibility = Visibility.ANY)
 public class RedigerbarAndelDto {
 
-    @JsonProperty(value = "andelsnr", required = true)
-    @NotNull
+    @JsonProperty(value = "andelsnr")
     @Min(0)
     @Max(Long.MAX_VALUE)
     private Long andelsnr;
@@ -46,6 +47,14 @@ public class RedigerbarAndelDto {
 
     public Boolean getNyAndel() {
         return nyAndel;
+    }
+
+    @AssertTrue(message = "Andelsnr må vere ulik null når nyAndel er false")
+    public boolean isAndelsnrNotNullWhenNotNew() {
+        if (nyAndel == null || !nyAndel) {
+            return andelsnr != null;
+        }
+        return true;
     }
 
 
