bf9d35e08546295d94a9d4c2cd9f3c67693fe3ea
Helga Nyrud <45488635+helganyrud@users.noreply.github.com>
2023-10-11 09:48:56 +0200

    Legger til utenlandsopphold for PILS også, likt som for PSB (#9571)

--- ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerlivetsslutt/mottak/SøknadOversetter.java
+++ ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerlivetsslutt/mottak/SøknadOversetter.java
@@ -12,6 +12,9 @@ import jakarta.inject.Inject;
 import no.nav.fpsak.tidsserie.LocalDateSegment;
 import no.nav.fpsak.tidsserie.LocalDateTimeline;
 import no.nav.fpsak.tidsserie.StandardCombinators;
+import no.nav.k9.felles.konfigurasjon.konfig.KonfigVerdi;
+import no.nav.k9.kodeverk.geografisk.Landkoder;
+import no.nav.k9.kodeverk.uttak.UtenlandsoppholdÅrsak;
 import no.nav.k9.sak.behandlingslager.behandling.Behandling;
 import no.nav.k9.sak.domene.person.tps.TpsTjeneste;
 import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
@@ -21,8 +24,10 @@ import no.nav.k9.sak.ytelse.pleiepengerbarn.mottak.SøknadPersisterer;
 import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.uttak.ArbeidPeriode;
 import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.uttak.FeriePeriode;
 import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.uttak.PerioderFraSøknad;
+import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.uttak.UtenlandsoppholdPeriode;
 import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.uttak.UttakPeriode;
 import no.nav.k9.søknad.Søknad;
+import no.nav.k9.søknad.felles.personopplysninger.Utenlandsopphold;
 import no.nav.k9.søknad.felles.type.Periode;
 import no.nav.k9.søknad.ytelse.pls.v1.PleipengerLivetsSluttfase;
 import no.nav.k9.søknad.ytelse.psb.v1.LovbestemtFerie;
@@ -33,11 +38,14 @@ class SøknadOversetter {
 
     private TpsTjeneste tpsTjeneste;
     private SøknadPersisterer søknadPersisterer;
+    private boolean enableUtenlandsoppholdPILS;
+
 
     @Inject
-    SøknadOversetter(TpsTjeneste tpsTjeneste, SøknadPersisterer søknadPersisterer) {
+    SøknadOversetter(TpsTjeneste tpsTjeneste, SøknadPersisterer søknadPersisterer, @KonfigVerdi(value = "ENABLE_UTENLANDSOPPHOLD_PILS", defaultVerdi = "false") boolean enableUtenlandsoppholdPILS) {
         this.tpsTjeneste = tpsTjeneste;
         this.søknadPersisterer = søknadPersisterer;
+        this.enableUtenlandsoppholdPILS = enableUtenlandsoppholdPILS;
     }
 
     void persister(Søknad søknad, JournalpostId journalpostId, Behandling behandling) {
@@ -54,7 +62,7 @@ class SøknadOversetter {
             uttakPerioder,
             arbeidPerioder,
             List.of(),
-            List.of(),
+            (enableUtenlandsoppholdPILS) ? mapUtenlandsopphold(ytelse.getUtenlandsopphold()) : List.of(),
             mapFerie(søknadsperioder, ytelse.getLovbestemtFerie()),
             List.of(),
             List.of());
@@ -67,6 +75,35 @@ class SøknadOversetter {
         søknadPersisterer.lagreSøknadsperioder(søknadsperioder, ytelse.getTrekkKravPerioder(), journalpostId, behandlingId);
         søknadPersisterer.lagreUttak(perioderFraSøknad, behandlingId);
         søknadPersisterer.oppdaterFagsakperiode(maksSøknadsperiode, fagsakId);
+    }
+
+    private List<UtenlandsoppholdPeriode> mapUtenlandsopphold(Utenlandsopphold utenlandsopphold) {
+        final List<UtenlandsoppholdPeriode> utenlandsoppholdPerioder = utenlandsopphold.getPerioder()
+            .entrySet()
+            .stream()
+            .map(entry ->
+                new UtenlandsoppholdPeriode(
+                    entry.getKey().getFraOgMed(),
+                    entry.getKey().getTilOgMed(),
+                    true,
+                    Landkoder.fraKode(entry.getValue().getLand().getLandkode()),
+                    entry.getValue().getÅrsak() == null ? UtenlandsoppholdÅrsak.INGEN : UtenlandsoppholdÅrsak.fraKode(entry.getValue().getÅrsak().name())))
+            .collect(Collectors.toList());
+
+        if (utenlandsopphold.getPerioderSomSkalSlettes() != null) {
+            utenlandsoppholdPerioder.addAll(utenlandsopphold.getPerioderSomSkalSlettes()
+                .entrySet()
+                .stream()
+                .map(entry ->
+                    new UtenlandsoppholdPeriode(
+                        entry.getKey().getFraOgMed(),
+                        entry.getKey().getTilOgMed(),
+                        false,
+                        Landkoder.fraKode(entry.getValue().getLand().getLandkode()),
+                        entry.getValue().getÅrsak() == null ? UtenlandsoppholdÅrsak.INGEN : UtenlandsoppholdÅrsak.fraKode(entry.getValue().getÅrsak().name())))
+                .collect(Collectors.toList()));
+        }
+        return utenlandsoppholdPerioder;
     }
 
     private Collection<FeriePeriode> mapFerie(List<Periode> søknadsperioder, LovbestemtFerie input) {
