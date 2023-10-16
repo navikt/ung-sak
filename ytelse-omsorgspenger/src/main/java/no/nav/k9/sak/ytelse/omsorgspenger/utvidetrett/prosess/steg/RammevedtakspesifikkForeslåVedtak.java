4ca0e6326b128ff8bb4e0169d80cd8651d86ab5e
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2023-10-16 12:54:55 +0200

    Sjekker PERIODISERT_RAMMEVEDTAK i foreslå vedtak

--- ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/utvidetrett/prosess/steg/RammevedtakspesifikkForeslåVedtak.java
+++ ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/utvidetrett/prosess/steg/RammevedtakspesifikkForeslåVedtak.java
@@ -9,6 +9,7 @@ import org.jboss.weld.exceptions.IllegalStateException;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import no.nav.fpsak.tidsserie.LocalDateTimeline;
+import no.nav.k9.felles.konfigurasjon.konfig.KonfigVerdi;
 import no.nav.k9.kodeverk.vilkår.Utfall;
 import no.nav.k9.sak.behandling.BehandlingReferanse;
 import no.nav.k9.sak.behandlingskontroll.BehandleStegResultat;
@@ -26,25 +27,30 @@ import no.nav.k9.sak.ytelse.omsorgspenger.utvidetrett.prosess.PeriodisertUtvidet
 public class RammevedtakspesifikkForeslåVedtak implements YtelsespesifikkForeslåVedtak {
 
     private BehandlingRepository behandlingRepository;
-
     private PeriodisertUtvidetRettIverksettTjeneste periodisertUtvidetRettIverksettTjeneste;
+    private boolean brukPeriodisertRammevedtak;
 
     public RammevedtakspesifikkForeslåVedtak() {
         //for CDI proxy
     }
 
     @Inject
-    public RammevedtakspesifikkForeslåVedtak(BehandlingRepository behandlingRepository, PeriodisertUtvidetRettIverksettTjeneste periodisertUtvidetRettIverksettTjeneste) {
+    public RammevedtakspesifikkForeslåVedtak(BehandlingRepository behandlingRepository,
+                                             PeriodisertUtvidetRettIverksettTjeneste periodisertUtvidetRettIverksettTjeneste,
+                                             @KonfigVerdi(value = "PERIODISERT_RAMMEVEDTAK", defaultVerdi = "false") boolean brukPeriodisertRammevedtak) {
         this.behandlingRepository = behandlingRepository;
         this.periodisertUtvidetRettIverksettTjeneste = periodisertUtvidetRettIverksettTjeneste;
+        this.brukPeriodisertRammevedtak = brukPeriodisertRammevedtak;
     }
 
     @Override
     public BehandleStegResultat run(BehandlingReferanse ref) {
-        Behandling behandling = behandlingRepository.hentBehandling(ref.getBehandlingId());
-        LocalDateTimeline<Utfall> endringer = periodisertUtvidetRettIverksettTjeneste.utfallSomErEndret(behandling);
-        if (endringer.size() > 1) {
-            throw new IllegalStateException("Det er ikke støttet i omsorgsdager å motta mer enn 1 periode med endring for hver behandling, stopper derfor her så det kan fikses. Har endringer for perioder: " + endringer);
+        if (brukPeriodisertRammevedtak) {
+            Behandling behandling = behandlingRepository.hentBehandling(ref.getBehandlingId());
+            LocalDateTimeline<Utfall> endringer = periodisertUtvidetRettIverksettTjeneste.utfallSomErEndret(behandling);
+            if (endringer.size() > 1) {
+                throw new IllegalStateException("Det er ikke støttet i omsorgsdager å motta mer enn 1 periode med endring for hver behandling, stopper derfor her så det kan fikses. Har endringer for perioder: " + endringer);
+            }
         }
         return null; //null gir fallthrough til vanlig foreslå-vedtak-implementasjon
     }
