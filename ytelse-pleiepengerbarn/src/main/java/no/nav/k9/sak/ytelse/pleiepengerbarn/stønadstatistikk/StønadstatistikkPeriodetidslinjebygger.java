ef44a29daa67fcd00a7e931cac0ea232154953ca
Stian Grenborgen <stian.grenborgen@nav.no>
2023-10-23 13:08:50 +0200

    Stønadstatistikk: Legger til felter for gradering mot tilkommet inntekt. (#9580)
    * Stønadstatistikk: Legger til felter for gradering mot tilkommet inntekt.

--- ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/stønadstatistikk/StønadstatistikkPeriodetidslinjebygger.java
+++ ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/stønadstatistikk/StønadstatistikkPeriodetidslinjebygger.java
@@ -1,5 +1,6 @@
 package no.nav.k9.sak.ytelse.pleiepengerbarn.stønadstatistikk;
 
+import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
@@ -8,11 +9,10 @@ import jakarta.enterprise.context.Dependent;
 import jakarta.inject.Inject;
 import no.nav.folketrygdloven.beregningsgrunnlag.kalkulus.BeregningsgrunnlagTjeneste;
 import no.nav.folketrygdloven.beregningsgrunnlag.modell.Beregningsgrunnlag;
+import no.nav.folketrygdloven.beregningsgrunnlag.modell.BeregningsgrunnlagPeriode;
 import no.nav.fpsak.tidsserie.LocalDateSegment;
 import no.nav.fpsak.tidsserie.LocalDateTimeline;
 import no.nav.fpsak.tidsserie.LocalDateTimeline.JoinStyle;
-import no.nav.fpsak.tidsserie.StandardCombinators;
-import no.nav.k9.felles.konfigurasjon.konfig.Tid;
 import no.nav.k9.sak.behandling.BehandlingReferanse;
 import no.nav.k9.sak.behandlingslager.behandling.Behandling;
 import no.nav.k9.sak.behandlingslager.behandling.beregning.BeregningsresultatAndel;
@@ -49,7 +49,7 @@ class StønadstatistikkPeriodetidslinjebygger {
         final LocalDateTimeline<UttaksperiodeInfo> uttaksperiodeTidslinje = toUttaksperiodeTidslinje(uttakRestKlient.hentUttaksplan(behandling.getUuid(), true));
         final LocalDateTimeline<UttaksperiodeInfo> uttaksperiodeUtenHelgerTidslinje = Hjelpetidslinjer.fjernHelger(uttaksperiodeTidslinje);
 
-        final LocalDateTimeline<Beregningsgrunnlag> beregningsgrunnlagTidslinje = toBeregningsgrunnlagTidslinje(beregningsgrunnlagTjeneste.hentEksaktFastsattForAllePerioder(BehandlingReferanse.fra(behandling)));
+        final LocalDateTimeline<BeregningsgrunnlagPeriode> beregningsgrunnlagTidslinje = toBeregningsgrunnlagPeriodeTidslinje(beregningsgrunnlagTjeneste.hentEksaktFastsattForAllePerioder(BehandlingReferanse.fra(behandling)));
         final LocalDateTimeline<List<BeregningsresultatAndel>> beregningsresultatTidslinje = toBeregningsresultatTidslinje(beregningsresultatRepository.hentEndeligBeregningsresultat(behandling.getId()));
 
         final LocalDateTimeline<InformasjonTilStønadstatistikkHendelse> mellomregning = uttaksperiodeUtenHelgerTidslinje.combine(beregningsgrunnlagTidslinje, (datoInterval, datoSegment, datoSegment2) -> new LocalDateSegment<>(datoInterval, new InformasjonTilStønadstatistikkHendelse(datoSegment.getValue(), valueOrNull(datoSegment2))), JoinStyle.LEFT_JOIN);
@@ -66,17 +66,34 @@ class StønadstatistikkPeriodetidslinjebygger {
         return new LocalDateTimeline<>(uttaksplan.getPerioder().entrySet().stream().map(e -> new LocalDateSegment<>(e.getKey().getFom(), e.getKey().getTom(), e.getValue())).toList());
     }
 
-    private LocalDateTimeline<Beregningsgrunnlag> toBeregningsgrunnlagTidslinje(List<Beregningsgrunnlag> beregningsgrunnlagListe) {
+    private LocalDateTimeline<BeregningsgrunnlagPeriode> toBeregningsgrunnlagPeriodeTidslinje(List<Beregningsgrunnlag> beregningsgrunnlagListe) {
         if (beregningsgrunnlagListe.isEmpty()) {
             return LocalDateTimeline.empty();
         }
+        
+        assertSammeVerdierForSammeStp(beregningsgrunnlagListe);
 
         var segmenter = beregningsgrunnlagListe.stream()
             .sorted(Comparator.comparing(Beregningsgrunnlag::getSkjæringstidspunkt))
-            .map(b -> new LocalDateSegment<>(b.getSkjæringstidspunkt(), Tid.TIDENES_ENDE, b))
+            .map(Beregningsgrunnlag::getBeregningsgrunnlagPerioder)
+            .flatMap(Collection::stream)
+            .map(bp -> new LocalDateSegment<>(bp.getPeriode().getFomDato(), bp.getPeriode().getTomDato(), bp))
             .toList();
 
-        return new LocalDateTimeline<>(segmenter, StandardCombinators::coalesceRightHandSide);
+        return new LocalDateTimeline<>(segmenter);
+    }
+    
+    private void assertSammeVerdierForSammeStp(List<Beregningsgrunnlag> beregningsgrunnlagListe) {
+        for (Beregningsgrunnlag bg : beregningsgrunnlagListe) {
+            final long antallUlikeBruttoPerÅr = bg.getBeregningsgrunnlagPerioder()
+                    .stream()
+                    .map(BeregningsgrunnlagPeriode::getBruttoPrÅr)
+                    .distinct()
+                    .count();
+            if (antallUlikeBruttoPerÅr > 1) {
+                throw new IllegalStateException("Det skal ikke være mulig med ulik brutto-per-år på samme STP.");
+            }
+        }
     }
 
     private LocalDateTimeline<List<BeregningsresultatAndel>> toBeregningsresultatTidslinje(Optional<BeregningsresultatEntitet> beregningsresultatEntitet) {
@@ -89,18 +106,18 @@ class StønadstatistikkPeriodetidslinjebygger {
 
     static class InformasjonTilStønadstatistikkHendelse {
         private UttaksperiodeInfo uttaksperiodeInfo;
-        private Beregningsgrunnlag beregningsgrunnlag;
+        private BeregningsgrunnlagPeriode beregningsgrunnlagPeriode;
         private List<BeregningsresultatAndel> beregningsresultatAndeler;
 
         public InformasjonTilStønadstatistikkHendelse(UttaksperiodeInfo uttaksperiodeInfo,
-                                                      Beregningsgrunnlag beregningsgrunnlag) {
+                                                      BeregningsgrunnlagPeriode beregningsgrunnlagPeriode) {
             this.uttaksperiodeInfo = uttaksperiodeInfo;
-            this.beregningsgrunnlag = beregningsgrunnlag;
+            this.beregningsgrunnlagPeriode = beregningsgrunnlagPeriode;
         }
 
         public InformasjonTilStønadstatistikkHendelse(InformasjonTilStønadstatistikkHendelse info, List<BeregningsresultatAndel> beregningsresultatAndeler) {
             this.uttaksperiodeInfo = info.uttaksperiodeInfo;
-            this.beregningsgrunnlag = info.beregningsgrunnlag;
+            this.beregningsgrunnlagPeriode = info.beregningsgrunnlagPeriode;
             this.beregningsresultatAndeler = beregningsresultatAndeler;
         }
 
@@ -108,8 +125,8 @@ class StønadstatistikkPeriodetidslinjebygger {
             return uttaksperiodeInfo;
         }
 
-        public Beregningsgrunnlag getBeregningsgrunnlag() {
-            return beregningsgrunnlag;
+        public BeregningsgrunnlagPeriode getBeregningsgrunnlagPeriode() {
+            return beregningsgrunnlagPeriode;
         }
 
         public List<BeregningsresultatAndel> getBeregningsresultatAndeler() {
