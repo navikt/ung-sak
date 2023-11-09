b2bca1747d1788496a2d9b8aa1b92487f6e1bd92
Stian Grenborgen <stian.grenborgen@nav.no>
2023-11-09 12:20:24 +0100

    Flytter migreringsskript over til ny PR.

--- migreringer/src/main/resources/db/postgres/defaultDS/2.0/V2.0_161__republiser_til_los2.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO prosess_task (id, task_type, task_gruppe, neste_kjoering_etter, task_parametere)
-SELECT nextval('seq_prosess_task'), 'oppgavebehandling.RepubliserEvent',
-       nextval('seq_prosess_task_gruppe'), current_timestamp at time zone 'UTC' + floor(600 + random() * 2 * 3600 * 24) * '1 second'::interval,
-       'behandlingUuid=' || b.uuid
-FROM Behandling b INNER JOIN Fagsak f ON (
-    f.id = b.fagsak_id
-  )
-WHERE f.ytelse_type <> 'OBSOLETE'
-;
