fb8c637231231edbd5808a70a354a7499018eb00
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2023-11-14 13:41:21 +0100

    Fjern noen forvaltningstjenester for FRISINN

--- web/src/main/java/no/nav/k9/sak/web/app/tjenester/forvaltning/ForvaltningMidlertidigDriftRestTjeneste.java
+++ web/src/main/java/no/nav/k9/sak/web/app/tjenester/forvaltning/ForvaltningMidlertidigDriftRestTjeneste.java
@@ -3,10 +3,7 @@ package no.nav.k9.sak.web.app.tjenester.forvaltning;
 import static no.nav.k9.abac.BeskyttetRessursKoder.DRIFT;
 import static no.nav.k9.abac.BeskyttetRessursKoder.FAGSAK;
 import static no.nav.k9.felles.sikkerhet.abac.BeskyttetRessursActionAttributt.CREATE;
-import static no.nav.k9.kodeverk.behandling.FagsakYtelseType.FRISINN;
 import static no.nav.k9.kodeverk.behandling.FagsakYtelseType.PLEIEPENGER_SYKT_BARN;
-import static no.nav.k9.kodeverk.behandling.aksjonspunkt.AksjonspunktDefinisjon.KONTROLL_AV_MANUELT_OPPRETTET_REVURDERINGSBEHANDLING;
-import static no.nav.k9.kodeverk.behandling.aksjonspunkt.AksjonspunktDefinisjon.OVERSTYRING_FRISINN_OPPGITT_OPPTJENING;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -14,22 +11,15 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
-import java.math.BigDecimal;
 import java.time.Duration;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.function.Function;
 import java.util.stream.Stream;
 
 import org.slf4j.Logger;
@@ -37,7 +27,6 @@ import org.slf4j.LoggerFactory;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -77,17 +66,12 @@ import no.nav.k9.kodeverk.dokument.DokumentStatus;
 import no.nav.k9.prosesstask.api.ProsessTaskData;
 import no.nav.k9.prosesstask.api.ProsessTaskTjeneste;
 import no.nav.k9.sak.behandling.FagsakTjeneste;
-import no.nav.k9.sak.behandlingskontroll.BehandlingskontrollKontekst;
-import no.nav.k9.sak.behandlingskontroll.BehandlingskontrollTjeneste;
-import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
 import no.nav.k9.sak.behandlingslager.behandling.motattdokument.MottatteDokumentRepository;
-import no.nav.k9.sak.behandlingslager.behandling.personopplysning.PersonopplysningRepository;
 import no.nav.k9.sak.behandlingslager.behandling.repository.BehandlingRepository;
 import no.nav.k9.sak.behandlingslager.fagsak.Fagsak;
 import no.nav.k9.sak.domene.person.tps.TpsTjeneste;
 import no.nav.k9.sak.domene.typer.tid.Hjelpetidslinjer;
 import no.nav.k9.sak.hendelse.stønadstatistikk.StønadstatistikkService;
-import no.nav.k9.sak.kontrakt.FeilDto;
 import no.nav.k9.sak.kontrakt.KortTekst;
 import no.nav.k9.sak.kontrakt.behandling.BehandlingIdDto;
 import no.nav.k9.sak.kontrakt.behandling.SaksnummerDto;
@@ -105,18 +89,9 @@ import no.nav.k9.sak.web.app.tjenester.forvaltning.dump.logg.DiagnostikkFagsakLo
 import no.nav.k9.sak.web.app.tjenester.forvaltning.rapportering.DriftLesetilgangVurderer;
 import no.nav.k9.sak.web.server.abac.AbacAttributtEmptySupplier;
 import no.nav.k9.sak.web.server.abac.AbacAttributtSupplier;
-import no.nav.k9.sak.ytelse.frisinn.mottak.FrisinnSøknadInnsending;
-import no.nav.k9.sak.ytelse.frisinn.mottak.FrisinnSøknadMottaker;
 import no.nav.k9.søknad.JsonUtils;
 import no.nav.k9.søknad.Søknad;
-import no.nav.k9.søknad.felles.type.NorskIdentitetsnummer;
 import no.nav.k9.søknad.felles.type.Periode;
-import no.nav.k9.søknad.felles.type.Språk;
-import no.nav.k9.søknad.felles.type.SøknadId;
-import no.nav.k9.søknad.frisinn.FrisinnSøknad;
-import no.nav.k9.søknad.frisinn.Inntekter;
-import no.nav.k9.søknad.frisinn.PeriodeInntekt;
-import no.nav.k9.søknad.frisinn.SelvstendigNæringsdrivende;
 import no.nav.k9.søknad.ytelse.pls.v1.PleipengerLivetsSluttfase;
 import no.nav.k9.søknad.ytelse.psb.v1.PleiepengerSyktBarn;
 
@@ -130,9 +105,7 @@ public class ForvaltningMidlertidigDriftRestTjeneste {
 
     private static final Logger logger = LoggerFactory.getLogger(ForvaltningMidlertidigDriftRestTjeneste.class);
 
-    private FrisinnSøknadMottaker frisinnSøknadMottaker;
     private TpsTjeneste tpsTjeneste;
-    private BehandlingskontrollTjeneste behandlingskontrollTjeneste;
 
     private ProsessTaskTjeneste prosessTaskRepository;
     private FagsakTjeneste fagsakTjeneste;
@@ -145,20 +118,15 @@ public class ForvaltningMidlertidigDriftRestTjeneste {
     private StønadstatistikkService stønadstatistikkService;
 
     private DriftLesetilgangVurderer lesetilgangVurderer;
-
-    private PersonopplysningRepository personopplysningRepository;
     private OpprettRevurderingService opprettRevurderingService;
 
-    private ProsessTaskTjeneste prosessTaskTjeneste;
 
     public ForvaltningMidlertidigDriftRestTjeneste() {
         // For Rest-CDI
     }
 
     @Inject
-    public ForvaltningMidlertidigDriftRestTjeneste(@FagsakYtelseTypeRef(FRISINN) FrisinnSøknadMottaker frisinnSøknadMottaker,
-                                                   TpsTjeneste tpsTjeneste,
-                                                   BehandlingskontrollTjeneste behandlingskontrollTjeneste,
+    public ForvaltningMidlertidigDriftRestTjeneste(TpsTjeneste tpsTjeneste,
                                                    FagsakTjeneste fagsakTjeneste,
                                                    ProsessTaskTjeneste prosessTaskRepository,
                                                    MottatteDokumentRepository mottatteDokumentRepository,
@@ -167,13 +135,9 @@ public class ForvaltningMidlertidigDriftRestTjeneste {
                                                    EntityManager entityManager,
                                                    StønadstatistikkService stønadstatistikkService,
                                                    DriftLesetilgangVurderer lesetilgangVurderer,
-                                                   PersonopplysningRepository personopplysningRepository,
-                                                   OpprettRevurderingService opprettRevurderingService,
-                                                   ProsessTaskTjeneste prosessTaskTjeneste) {
+                                                   OpprettRevurderingService opprettRevurderingService) {
 
-        this.frisinnSøknadMottaker = frisinnSøknadMottaker;
         this.tpsTjeneste = tpsTjeneste;
-        this.behandlingskontrollTjeneste = behandlingskontrollTjeneste;
         this.fagsakTjeneste = fagsakTjeneste;
         this.prosessTaskRepository = prosessTaskRepository;
         this.mottatteDokumentRepository = mottatteDokumentRepository;
@@ -182,60 +146,7 @@ public class ForvaltningMidlertidigDriftRestTjeneste {
         this.entityManager = entityManager;
         this.stønadstatistikkService = stønadstatistikkService;
         this.lesetilgangVurderer = lesetilgangVurderer;
-        this.personopplysningRepository = personopplysningRepository;
         this.opprettRevurderingService = opprettRevurderingService;
-        this.prosessTaskTjeneste = prosessTaskTjeneste;
-    }
-
-    /**
-     * @deprecated Bør fjernes når FRISINN nedlegges.
-     */
-    @Deprecated(forRemoval = true)
-    @POST
-    @Path("/frisinn/opprett-manuell-frisinn/TO_BE_REMOVED")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Operation(description = "Opprett behandling hvor saksbehandler kan legge inn inntektsopplysninger", summary = ("Returnerer saksnummer som er tilknyttet den nye fagsaken som har blitt opprettet."), tags = "frisinn", responses = {
-        @ApiResponse(responseCode = "200", description = "Returnerer saksnummer", content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = SaksnummerDto.class)))
-    })
-    @Produces(MediaType.APPLICATION_JSON)
-    @BeskyttetRessurs(action = BeskyttetRessursActionAttributt.CREATE, resource = FAGSAK)
-    public Response opprettManuellFrisinnSøknad(@Valid @TilpassetAbacAttributt(supplierClass = AbacAttributtSupplier.class) ManuellSøknadDto manuellSøknadDto) {
-        PersonIdent fnr = new PersonIdent(manuellSøknadDto.getFnr());
-        AktørId aktørId = tpsTjeneste.hentAktørForFnr(fnr).orElse(null);
-        if (aktørId == null) {
-            return Response.serverError().entity(new FeilDto("Oppgitt personummer er ukjent")).build();
-        }
-
-        // Samme utledning for fagsakperiode som i k9-fordel
-        LocalDate fom = LocalDate.of(2020, 3, 1);
-        LocalDate tom = manuellSøknadDto.getPeriode().getTilOgMed();
-
-        Fagsak fagsak = frisinnSøknadMottaker.finnEllerOpprettFagsak(FRISINN, aktørId, null, null, fom, tom);
-
-        loggForvaltningTjeneste(fagsak, "/frisinn/opprett-manuell-frisinn/", "kjører manuell frisinn søknad");
-
-        FrisinnSøknad søknad = FrisinnSøknad.builder()
-            .språk(Språk.NORSK_BOKMÅL)
-            .søknadId(SøknadId.of("ManueltOpprettet" + fagsak.getSaksnummer().getVerdi())) // lagres ingensteds
-            .inntekter(lagDummyInntekt(manuellSøknadDto))
-            .søknadsperiode(manuellSøknadDto.getPeriode())
-            .mottattDato(ZonedDateTime.now(ZoneId.of("Europe/Paris")))
-            .søker(new no.nav.k9.søknad.felles.personopplysninger.Søker(NorskIdentitetsnummer.of(fnr.getIdent())))
-            .build();
-        var valideringsfeil = validerSøknad(fagsak, søknad);
-        if (valideringsfeil.isPresent()) {
-            return Response.serverError().entity(new FeilDto(valideringsfeil.get().getMessage())).build();
-        }
-
-        FrisinnSøknadInnsending frisinnSøknadInnsending = new FrisinnSøknadInnsending();
-        frisinnSøknadInnsending.setSøknad(søknad);
-        var behandling = frisinnSøknadMottaker.mottaSøknad(fagsak.getSaksnummer(), null, frisinnSøknadInnsending);
-
-        BehandlingskontrollKontekst kontekst = behandlingskontrollTjeneste.initBehandlingskontroll(behandling);
-        // ønsker at saksbehandler må ta stilling til disse
-        behandlingskontrollTjeneste.lagreAksjonspunkterFunnet(kontekst, List.of(OVERSTYRING_FRISINN_OPPGITT_OPPTJENING, KONTROLL_AV_MANUELT_OPPRETTET_REVURDERINGSBEHANDLING));
-
-        return Response.ok(new SaksnummerDto(fagsak.getSaksnummer())).build();
     }
 
     @POST
@@ -696,131 +607,6 @@ public class ForvaltningMidlertidigDriftRestTjeneste {
         return Response.ok(resultatString).build();
     }
 
-    @GET
-    @Path("/frisinn/uttrekk-antall")
-    @Produces(MediaType.TEXT_PLAIN)
-    @Operation(description = "Adhoc-uttrekk for Frisinn", summary = ("Adhoc-uttrekk for Frisinn."), tags = "forvaltning")
-    @BeskyttetRessurs(action = BeskyttetRessursActionAttributt.READ, resource = DRIFT)
-    public Response antallFrisinnsøknader() {
-
-        final Query q = entityManager.createNativeQuery("SELECT '2020' aar, (\n"
-            + "  SELECT COUNT(*)\n"
-            + "  FROM Behandling b INNER JOIN Fagsak f on (\n"
-            + "    f.id = b.fagsak_id\n"
-            + "  )\n"
-            + "  WHERE f.ytelse_type = 'FRISINN'\n"
-            + "    AND b.opprettet_tid < '2021-02-01'\n"
-            + "    AND b.behandling_status IN ('AVSLU', 'FVED', 'IVED')\n"
-            + "    AND b.ansvarlig_saksbehandler is not null\n"
-            + "    AND NOT EXISTS (\n"
-            + "      SELECT * FROM BEHANDLING_ARSAK aarsak WHERE aarsak.behandling_id = b.id AND manuelt_opprettet = true\n"
-            + "    )\n"
-            + ") AS manuell, (\n"
-            + "  SELECT COUNT(*)\n"
-            + "  FROM Behandling b INNER JOIN Fagsak f on (\n"
-            + "    f.id = b.fagsak_id\n"
-            + "  )\n"
-            + "  WHERE f.ytelse_type = 'FRISINN'\n"
-            + "    AND b.opprettet_tid < '2021-02-01'\n"
-            + "    AND b.behandling_status IN ('AVSLU', 'FVED', 'IVED')\n"
-            + "    AND b.ansvarlig_saksbehandler is null\n"
-            + "    AND NOT EXISTS (\n"
-            + "      SELECT * FROM BEHANDLING_ARSAK aarsak WHERE aarsak.behandling_id = b.id AND manuelt_opprettet = true\n"
-            + "    )\n"
-            + ") AS automatisk\n"
-            + "\n"
-            + "UNION\n"
-            + "\n"
-            + "SELECT '2021' aar, (\n"
-            + "  SELECT COUNT(*)\n"
-            + "  FROM Behandling b INNER JOIN Fagsak f on (\n"
-            + "    f.id = b.fagsak_id\n"
-            + "  )\n"
-            + "  WHERE f.ytelse_type = 'FRISINN'\n"
-            + "    AND b.opprettet_tid >= '2021-02-01'\n"
-            + "    AND b.behandling_status IN ('AVSLU', 'FVED', 'IVED')\n"
-            + "    AND b.ansvarlig_saksbehandler is not null\n"
-            + "    AND NOT EXISTS (\n"
-            + "      SELECT * FROM BEHANDLING_ARSAK aarsak WHERE aarsak.behandling_id = b.id AND manuelt_opprettet = true\n"
-            + "    )\n"
-            + ") AS manuell, (\n"
-            + "  SELECT COUNT(*)\n"
-            + "  FROM Behandling b INNER JOIN Fagsak f on (\n"
-            + "    f.id = b.fagsak_id\n"
-            + "  )\n"
-            + "  WHERE f.ytelse_type = 'FRISINN'\n"
-            + "    AND b.opprettet_tid >= '2021-02-01'\n"
-            + "    AND b.behandling_status IN ('AVSLU', 'FVED', 'IVED')\n"
-            + "    AND b.ansvarlig_saksbehandler is null\n"
-            + "    AND NOT EXISTS (\n"
-            + "      SELECT * FROM BEHANDLING_ARSAK aarsak WHERE aarsak.behandling_id = b.id AND manuelt_opprettet = true\n"
-            + "    )\n"
-            + ") AS automatisk\n"
-            + "\n"
-            + "UNION\n"
-            + "\n"
-            + "SELECT '2020-ubehandlet' aar, (\n"
-            + "  SELECT COUNT(*)\n"
-            + "  FROM Behandling b INNER JOIN Fagsak f on (\n"
-            + "    f.id = b.fagsak_id\n"
-            + "  )\n"
-            + "  WHERE f.ytelse_type = 'FRISINN'\n"
-            + "    AND b.opprettet_tid < '2021-02-01'\n"
-            + "    AND b.behandling_status NOT IN ('AVSLU', 'FVED', 'IVED')\n"
-            + "    AND b.ansvarlig_saksbehandler is not null\n"
-            + "    AND NOT EXISTS (\n"
-            + "      SELECT * FROM BEHANDLING_ARSAK aarsak WHERE aarsak.behandling_id = b.id AND manuelt_opprettet = true\n"
-            + "    )\n"
-            + ") AS manuell, (\n"
-            + "  SELECT COUNT(*)\n"
-            + "  FROM Behandling b INNER JOIN Fagsak f on (\n"
-            + "    f.id = b.fagsak_id\n"
-            + "  )\n"
-            + "  WHERE f.ytelse_type = 'FRISINN'\n"
-            + "    AND b.opprettet_tid < '2021-02-01'\n"
-            + "    AND b.behandling_status NOT IN ('AVSLU', 'FVED', 'IVED')\n"
-            + "    AND b.ansvarlig_saksbehandler is null\n"
-            + "    AND NOT EXISTS (\n"
-            + "      SELECT * FROM BEHANDLING_ARSAK aarsak WHERE aarsak.behandling_id = b.id AND manuelt_opprettet = true\n"
-            + "    )\n"
-            + ") AS automatisk\n"
-            + "\n"
-            + "UNION\n"
-            + "\n"
-            + "SELECT '2021-ubehandlet' aar, (\n"
-            + "  SELECT COUNT(*)\n"
-            + "  FROM Behandling b INNER JOIN Fagsak f on (\n"
-            + "    f.id = b.fagsak_id\n"
-            + "  )\n"
-            + "  WHERE f.ytelse_type = 'FRISINN'\n"
-            + "    AND b.opprettet_tid >= '2021-02-01'\n"
-            + "    AND b.behandling_status NOT IN ('AVSLU', 'FVED', 'IVED')\n"
-            + "    AND b.ansvarlig_saksbehandler is not null\n"
-            + "    AND NOT EXISTS (\n"
-            + "      SELECT * FROM BEHANDLING_ARSAK aarsak WHERE aarsak.behandling_id = b.id AND manuelt_opprettet = true\n"
-            + "    )\n"
-            + ") AS manuell, (\n"
-            + "  SELECT COUNT(*)\n"
-            + "  FROM Behandling b INNER JOIN Fagsak f on (\n"
-            + "    f.id = b.fagsak_id\n"
-            + "  )\n"
-            + "  WHERE f.ytelse_type = 'FRISINN'\n"
-            + "    AND b.opprettet_tid >= '2021-02-01'\n"
-            + "    AND b.behandling_status NOT IN ('AVSLU', 'FVED', 'IVED')\n"
-            + "    AND b.ansvarlig_saksbehandler is null\n"
-            + "    AND NOT EXISTS (\n"
-            + "      SELECT * FROM BEHANDLING_ARSAK aarsak WHERE aarsak.behandling_id = b.id AND manuelt_opprettet = true\n"
-            + "    )\n"
-            + ") AS automatisk");
-
-        @SuppressWarnings("unchecked") final List<Object[]> result = q.getResultList();
-        final String resultatString = result.stream()
-            .map(a -> a[0].toString() + ";" + a[1].toString() + ";" + a[2].toString())
-            .reduce((a, b) -> a + "\n" + b)
-            .orElse("");
-        return Response.ok(resultatString).build();
-    }
-
     @POST
     @Path("/marker-ugyldig")
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
@@ -894,37 +680,6 @@ public class ForvaltningMidlertidigDriftRestTjeneste {
          */
         entityManager.persist(new DiagnostikkFagsakLogg(fagsak.getId(), tjeneste, begrunnelse));
         entityManager.flush();
-    }
-
-    public static class AbacDataSupplier implements Function<Object, AbacDataAttributter> {
-        @Override
-        public AbacDataAttributter apply(Object obj) {
-            return AbacDataAttributter.opprett();
-        }
-    }
-
-    @NotNull
-    private Optional<Exception> validerSøknad(Fagsak fagsak, FrisinnSøknad søknad) {
-        try {
-            frisinnSøknadMottaker.validerSøknad(fagsak, søknad);
-        } catch (Exception e) {
-            return Optional.of(e);
-        }
-        return Optional.empty();
-    }
-
-    private Inntekter lagDummyInntekt(ManuellSøknadDto manuellSøknadDto) {
-        Map<Periode, PeriodeInntekt> periodePeriodeInntektMap = new HashMap<>();
-        periodePeriodeInntektMap.put(manuellSøknadDto.getPeriode(), new PeriodeInntekt(BigDecimal.ZERO));
-        Map<Periode, PeriodeInntekt> periodeFør = new HashMap<>();
-        periodeFør.put(new Periode(LocalDate.of(2019, 1, 1), LocalDate.of(2019, 12, 31)), new PeriodeInntekt(BigDecimal.ONE));
-
-        SelvstendigNæringsdrivende selvstendigNæringsdrivende = SelvstendigNæringsdrivende.builder()
-            .inntekterSøknadsperiode(periodePeriodeInntektMap)
-            .inntekterFør(periodeFør)
-            .build();
-
-        return new Inntekter(null, selvstendigNæringsdrivende, null);
     }
 
     public static class OpprettManuellRevurdering implements AbacDto {
