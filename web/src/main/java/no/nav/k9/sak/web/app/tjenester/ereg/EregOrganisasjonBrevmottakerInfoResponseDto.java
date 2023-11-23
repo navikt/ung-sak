933f9ef2c82b543dce379f78aa2bd4057d84415b
Jostein Stuhaug <jostein.stuhaug@nav.no>
2023-11-23 17:01:50 +0100

    EregOrganisasjonBrevmottakerInfoResponseDto navn skulle vere NotNull.

--- web/src/main/java/no/nav/k9/sak/web/app/tjenester/ereg/EregOrganisasjonBrevmottakerInfoResponseDto.java
+++ web/src/main/java/no/nav/k9/sak/web/app/tjenester/ereg/EregOrganisasjonBrevmottakerInfoResponseDto.java
@@ -4,22 +4,22 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.NotNull;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 @JsonAutoDetect(getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE, fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class EregOrganisasjonBrevmottakerInfoResponseDto {
 
-    public EregOrganisasjonBrevmottakerInfoResponseDto(@Nullable String navn) {
+    public EregOrganisasjonBrevmottakerInfoResponseDto(@NotNull String navn) {
         this.navn = navn;
     }
 
     @JsonProperty("navn")
-    @Nullable
+    @NotNull
     private String navn;
 
-    @Nullable
+    @NotNull
     public String getNavn() {
         return navn;
     }
