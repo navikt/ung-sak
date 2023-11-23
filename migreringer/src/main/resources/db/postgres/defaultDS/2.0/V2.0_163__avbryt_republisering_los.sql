c09ed1a49a0fbd915453cb580d91d994c44a8459
Bjornar Askevold <bjornar.askevold@nav.no>
2023-11-23 14:46:58 +0100

    Legger på flagg i task_payload for å kunne restarte taskprosess hvis nødvendig

--- migreringer/src/main/resources/db/postgres/defaultDS/2.0/V2.0_163__avbryt_republisering_los.sql
+++ migreringer/src/main/resources/db/postgres/defaultDS/2.0/V2.0_163__avbryt_republisering_los.sql
@@ -1,3 +1,3 @@
-UPDATE prosess_task SET status = 'KJOERT'
+UPDATE prosess_task SET status = 'KJOERT', task_payload='STOPPET_MANUELT'
     WHERE task_type = 'oppgavebehandling.RepubliserEvent'
     AND status = 'KLAR';
