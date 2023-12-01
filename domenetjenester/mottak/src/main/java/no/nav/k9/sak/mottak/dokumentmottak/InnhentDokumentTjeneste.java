c86bd3035e78d1077ae1020c49535e0e0082f572
oyvemb <oyvind.andre.hagen.emblemsvag@nav.no>
2023-12-01 11:32:20 +0100

    ønsker ikke å stoppe dokumentmottak dersom vi bare har feilende tasks, fordi mottak kan føre til at feilen blir løst

--- domenetjenester/mottak/src/main/java/no/nav/k9/sak/mottak/dokumentmottak/InnhentDokumentTjeneste.java
+++ domenetjenester/mottak/src/main/java/no/nav/k9/sak/mottak/dokumentmottak/InnhentDokumentTjeneste.java
@@ -1,9 +1,12 @@
 package no.nav.k9.sak.mottak.dokumentmottak;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.EnumSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
@@ -13,6 +16,7 @@ import jakarta.enterprise.context.Dependent;
 import jakarta.enterprise.inject.Any;
 import jakarta.enterprise.inject.Instance;
 import jakarta.inject.Inject;
+import no.nav.k9.felles.konfigurasjon.konfig.Tid;
 import no.nav.k9.kodeverk.behandling.BehandlingStatus;
 import no.nav.k9.kodeverk.behandling.BehandlingStegType;
 import no.nav.k9.kodeverk.behandling.BehandlingÅrsakType;
@@ -21,6 +25,7 @@ import no.nav.k9.kodeverk.behandling.aksjonspunkt.AksjonspunktKodeDefinisjon;
 import no.nav.k9.kodeverk.dokument.Brevkode;
 import no.nav.k9.prosesstask.api.ProsessTaskData;
 import no.nav.k9.prosesstask.api.ProsessTaskGruppe;
+import no.nav.k9.prosesstask.api.ProsessTaskStatus;
 import no.nav.k9.prosesstask.api.ProsessTaskTjeneste;
 import no.nav.k9.sak.behandling.prosessering.BehandlingProsesseringTjeneste;
 import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
@@ -201,8 +206,11 @@ public class InnhentDokumentTjeneste {
     }
 
     private void sjekkBehandlingHarIkkeÅpneTasks(Behandling behandling) {
+        final Set<ProsessTaskStatus> aktuelleStatuser = EnumSet.of(ProsessTaskStatus.KLAR, ProsessTaskStatus.VENTER_SVAR, ProsessTaskStatus.VETO);
+        final LocalDateTime fom = Tid.TIDENES_BEGYNNELSE.atStartOfDay();
+        final LocalDateTime tom = Tid.TIDENES_ENDE.plusDays(1).atStartOfDay();
         //merk at denne bare finner tasks med gruppesekvensnummer != null (hindrer at den finner seg selv eller andre av typen innhentsaksopplysninger.håndterMottattDokument)
-        List<ProsessTaskData> åpneTasks = fagsakProsessTaskRepository.finnAlleÅpneTasksForAngittSøk(behandling.getFagsakId(), behandling.getId(), null);
+        final List<ProsessTaskData> åpneTasks = fagsakProsessTaskRepository.finnAlleForAngittSøk(behandling.getFagsakId(), behandling.getId().toString(), null, aktuelleStatuser, true, fom, tom);
         if (!åpneTasks.isEmpty()) {
             //behandlingen har åpne tasks og mottak av dokument kan føre til parallelle prosesser som går i beina på hverandre
             log.info("Fant følgende åpne tasks: [" + åpneTasks.stream().map(Object::toString).collect(Collectors.joining(", ")) + "]");
