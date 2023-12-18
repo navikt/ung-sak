5ccc62b031ef6b713425e44770a73abb9d5023d8
Qadeer Ahmad Khan <qadeer.ahmad.khan@nav.no>
2023-12-18 15:17:04 +0100

    tillater &nsbp;

--- kontrakt/src/main/java/no/nav/k9/sak/kontrakt/dokument/TekstValideringRegex.java
+++ kontrakt/src/main/java/no/nav/k9/sak/kontrakt/dokument/TekstValideringRegex.java
@@ -2,5 +2,5 @@ package no.nav.k9.sak.kontrakt.dokument;
 
 public final class TekstValideringRegex {
 
-    public static final String FRITEKST = "^[\\p{Pd}\\p{Graph}\\p{Space}\\p{Sc}\\p{L}\\p{M}\\p{N}§]*$";
+    public static final String FRITEKST = "^[\\p{Zs}\\p{Pd}\\p{Graph}\\s\\p{Sc}\\p{L}\\p{M}\\p{N}§]*$";
 }
