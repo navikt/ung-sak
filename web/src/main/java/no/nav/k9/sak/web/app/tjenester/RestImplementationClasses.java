316a5446a72aad1b6cc0d4d21b8dd76f540d897e
oyvemb <oyvind.andre.hagen.emblemsvag@nav.no>
2023-12-21 11:25:16 +0100

    rest implementation

--- web/src/main/java/no/nav/k9/sak/web/app/tjenester/RestImplementationClasses.java
+++ web/src/main/java/no/nav/k9/sak/web/app/tjenester/RestImplementationClasses.java
@@ -69,6 +69,7 @@ import no.nav.k9.sak.web.app.tjenester.register.RedirectToRegisterRestTjeneste;
 import no.nav.k9.sak.web.app.tjenester.saksbehandler.InitielleLinksRestTjeneste;
 import no.nav.k9.sak.web.app.tjenester.saksbehandler.NavAnsattRestTjeneste;
 import no.nav.k9.sak.web.app.tjenester.saksbehandler.SaksbehandlerRestTjeneste;
+import no.nav.k9.sak.web.app.tjenester.saksnummer.SaksnummerAktorRestTjeneste;
 import no.nav.k9.sak.web.app.tjenester.saksnummer.SaksnummerRestTjeneste;
 import no.nav.k9.sak.web.server.abac.PipRestTjeneste;
 
@@ -132,6 +133,7 @@ public class RestImplementationClasses {
         classes.add(ReisetidRestTjeneste.class);
         classes.add(OpplæringDokumentRestTjeneste.class);
         classes.add(SaksnummerRestTjeneste.class);
+        classes.add(SaksnummerAktorRestTjeneste.class);
 
         // Forvaltningstjenester - fjernes løpende
         classes.add(ProsessTaskRestTjeneste.class);
