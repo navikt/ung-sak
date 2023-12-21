89424fb963b407ab929af17e55a1d33fbed0bbdb
oyvemb <oyvind.andre.hagen.emblemsvag@nav.no>
2023-12-21 11:11:22 +0100

    Revert "Tar ut tjenestene for kobling av saksnummer og aktør"
    This reverts commit 06a07a0450935e508a8651d8e6fe097305276b4e.

--- web/src/main/java/no/nav/k9/sak/web/app/tjenester/saksnummer/SaksnummerRestTjeneste.java
+++ web/src/main/java/no/nav/k9/sak/web/app/tjenester/saksnummer/SaksnummerRestTjeneste.java
@@ -9,14 +9,29 @@ import io.swagger.v3.oas.annotations.Operation;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import no.nav.k9.felles.konfigurasjon.konfig.KonfigVerdi;
 import no.nav.k9.felles.sikkerhet.abac.BeskyttetRessurs;
 import no.nav.k9.felles.sikkerhet.abac.BeskyttetRessursActionAttributt;
+import no.nav.k9.felles.sikkerhet.abac.TilpassetAbacAttributt;
+import no.nav.k9.sak.behandlingslager.saksnummer.SaksnummerAktørKoblingRepository;
+import no.nav.k9.sak.behandlingslager.saksnummer.SaksnummerAktørKoblingEntitet;
 import no.nav.k9.sak.behandlingslager.saksnummer.SaksnummerRepository;
 import no.nav.k9.sak.kontrakt.behandling.SaksnummerDto;
+import no.nav.k9.sak.kontrakt.person.AktørIdDto;
+import no.nav.k9.sak.kontrakt.saksnummer.SaksnummerAktørKoblingDto;
+import no.nav.k9.sak.typer.JournalpostId;
+import no.nav.k9.sak.typer.Saksnummer;
+import no.nav.k9.sak.web.server.abac.AbacAttributtSupplier;
 
 @Path(SaksnummerRestTjeneste.BASE_PATH)
 @ApplicationScoped
@@ -27,6 +42,7 @@ public class SaksnummerRestTjeneste {
     private static final Logger log = LoggerFactory.getLogger(SaksnummerRestTjeneste.class);
 
     private SaksnummerRepository saksnummerRepository;
+    private SaksnummerAktørKoblingRepository saksnummerAktørKoblingRepository;
     private boolean enableReservertSaksnummer;
 
     public SaksnummerRestTjeneste() {// For Rest-CDI
@@ -34,8 +50,10 @@ public class SaksnummerRestTjeneste {
 
     @Inject
     public SaksnummerRestTjeneste(SaksnummerRepository saksnummerRepository,
+                                  SaksnummerAktørKoblingRepository saksnummerAktørKoblingRepository,
                                   @KonfigVerdi(value = "ENABLE_RESERVERT_SAKSNUMMER", defaultVerdi = "false") boolean enableReservertSaksnummer) {
         this.saksnummerRepository = saksnummerRepository;
+        this.saksnummerAktørKoblingRepository = saksnummerAktørKoblingRepository;
         this.enableReservertSaksnummer = enableReservertSaksnummer;
     }
 
@@ -51,5 +69,56 @@ public class SaksnummerRestTjeneste {
         final SaksnummerDto saksnummer = new SaksnummerDto(saksnummerRepository.genererNyttSaksnummer());
         log.info("Reserverte saksnummer: " + saksnummer);
         return saksnummer;
+    }
+
+    @POST
+    @Path("/aktor")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Operation(description = "Kobler aktør til saksnummer.", summary = ("Kobler aktør til saksnummer"), tags = "saksnummer")
+    @BeskyttetRessurs(action = BeskyttetRessursActionAttributt.UPDATE, resource = FAGSAK)
+    public Response kobleAktørPåSaksnummer(@NotNull @QueryParam("saksnummer") @Valid @TilpassetAbacAttributt(supplierClass = AbacAttributtSupplier.class) Saksnummer saksnummer,
+                                           @NotNull @QueryParam("aktørId") @Valid @TilpassetAbacAttributt(supplierClass = AbacAttributtSupplier.class) AktørIdDto aktørId,
+                                           @NotNull @QueryParam("journalpostId") @Valid @TilpassetAbacAttributt(supplierClass = AbacAttributtSupplier.class) JournalpostId journalpostId) {
+        if (!enableReservertSaksnummer) {
+            throw new UnsupportedOperationException("Funksjonaliteten er avskrudd");
+        }
+        saksnummerAktørKoblingRepository.lagre(saksnummer.getVerdi(), aktørId.getAktorId(), journalpostId.getVerdi());
+        return Response.ok().build();
+    }
+
+    //TODO hva er egentlig behovet her? Gir det verdi sånn som det står nå? Bør vi kunne slå opp med bare journalpostId?
+    @GET
+    @Path("/aktor")
+    @Produces(JSON_UTF8)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Operation(description = "Henter koblinger mellom aktør og saksnummer.", summary = ("Henter koblinger mellom aktør og saksnummer."), tags = "saksnummer")
+    @BeskyttetRessurs(action = BeskyttetRessursActionAttributt.UPDATE, resource = FAGSAK)
+    public Response hentSaksnummerKobling(@NotNull @QueryParam("saksnummer") @Valid @TilpassetAbacAttributt(supplierClass = AbacAttributtSupplier.class) Saksnummer saksnummer,
+                                          @NotNull @QueryParam("aktørId") @Valid @TilpassetAbacAttributt(supplierClass = AbacAttributtSupplier.class) AktørIdDto aktørId,
+                                          @NotNull @QueryParam("journalpostId") @Valid @TilpassetAbacAttributt(supplierClass = AbacAttributtSupplier.class) JournalpostId journalpostId) {
+        if (!enableReservertSaksnummer) {
+            throw new UnsupportedOperationException("Funksjonaliteten er avskrudd");
+        }
+        SaksnummerAktørKoblingEntitet kobling = saksnummerAktørKoblingRepository.hent(saksnummer.getVerdi(), aktørId.getAktorId(), journalpostId.getVerdi());
+        if (kobling == null) {
+            return Response.ok().build();
+        }
+        SaksnummerAktørKoblingDto dto = new SaksnummerAktørKoblingDto(kobling.getSaksnummer(), kobling.getAktørId(), kobling.getJournalpostId());
+        return Response.ok(dto).build();
+    }
+
+    @POST
+    @Path("/aktor/slett")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Operation(description = "Sletter kobling mellom aktør og saksnummer.", summary = ("Sletter kobling mellom aktør og saksnummer."), tags = "saksnummer")
+    @BeskyttetRessurs(action = BeskyttetRessursActionAttributt.UPDATE, resource = FAGSAK)
+    public Response slettSaksnummerKobling(@NotNull @QueryParam("saksnummer") @Valid @TilpassetAbacAttributt(supplierClass = AbacAttributtSupplier.class) Saksnummer saksnummer,
+                                           @NotNull @QueryParam("aktørId") @Valid @TilpassetAbacAttributt(supplierClass = AbacAttributtSupplier.class) AktørIdDto aktørId,
+                                           @NotNull @QueryParam("journalpostId") @Valid @TilpassetAbacAttributt(supplierClass = AbacAttributtSupplier.class) JournalpostId journalpostId) {
+        if (!enableReservertSaksnummer) {
+            throw new UnsupportedOperationException("Funksjonaliteten er avskrudd");
+        }
+        saksnummerAktørKoblingRepository.slett(saksnummer.getVerdi(), aktørId.getAktorId(), journalpostId.getVerdi());
+        return Response.ok().build();
     }
 }
