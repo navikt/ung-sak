e5af852aad66540a845119a4fe4e230ee6255825
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2024-01-10 16:28:54 +0100

    Diagnostikktjeneste: splitter ut sporing/input i egne filer, mapper vilkårtype (#10029)
    * Splitter flere store csv-er i flere småfiler i diagnostikk-dump
    
    * Flytter begrunnelse til sporing, legger til vilkårtype-navn

--- web/src/main/java/no/nav/k9/sak/web/app/tjenester/forvaltning/dump/vilkår/VilkårDump.java
+++ web/src/main/java/no/nav/k9/sak/web/app/tjenester/forvaltning/dump/vilkår/VilkårDump.java
@@ -1,14 +1,17 @@
 package no.nav.k9.sak.web.app.tjenester.forvaltning.dump.vilkår;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Query;
 import jakarta.persistence.Tuple;
-
+import jakarta.persistence.TupleElement;
+import no.nav.k9.kodeverk.vilkår.VilkårType;
 import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
 import no.nav.k9.sak.web.app.tjenester.forvaltning.CsvOutput;
 import no.nav.k9.sak.web.app.tjenester.forvaltning.dump.DebugDumpFagsak;
@@ -35,7 +38,7 @@ public class VilkårDump implements DebugDumpFagsak {
             + "  f.saksnummer"
             + " ,rv.behandling_id"
             + " ,vv.vilkar_resultat_id"
-            + " ,vv.vilkar_type"
+            + " ,vv.vilkar_type as vilkar_type_kode"
             + " , replace(cast(vv.opprettet_tid as varchar), ' ', 'T') opprettet_tid"
             + " ,vp.vilkar_id"
             + " ,vp.fom"
@@ -47,9 +50,6 @@ public class VilkårDump implements DebugDumpFagsak {
             + " ,vp.avslag_kode"
             + " ,vp.merknad_parametere"
             + " , replace(cast(vp.opprettet_tid as varchar), ' ', 'T') vp_opprettet_tid"
-            + " ,vp.begrunnelse"
-            + " ,regexp_replace(regexp_replace(convert_from(lo_get(cast(regel_input  as oid)), 'UTF8'), '\\\\n', '\\n'), '\\\\', '') as regel_input"
-            + " ,regexp_replace(regexp_replace(convert_from(lo_get(cast(regel_evaluering as oid)), 'UTF8'), '\\\\n', '\\n'), '\\\\', '') as regel_evaluering"
             + " from behandling b"
             + "  inner join fagsak f on f.id=b.fagsak_id  "
             + "  inner join rs_vilkars_resultat rv on rv.behandling_id=b.id "
@@ -65,11 +65,25 @@ public class VilkårDump implements DebugDumpFagsak {
 
         @SuppressWarnings("unchecked")
         List<Tuple> results = query.getResultList();
-
-        Optional<String> output = CsvOutput.dumpResultSetToCsv(results);
-        if (output.isPresent()) {
-            dumpMottaker.newFile("vilkårresultat.csv");
-            dumpMottaker.write(output.get());
+        if (results.isEmpty()) {
+            return;
         }
+
+        var valueMapper = settOppMappingAvVilkårType(results);
+        String output = CsvOutput.dumpAsCsv(true, results, valueMapper);
+
+        dumpMottaker.newFile("vilkår/resultat.csv");
+        dumpMottaker.write(output);
+    }
+
+    private static LinkedHashMap<String, Function<Tuple, ?>> settOppMappingAvVilkårType(List<Tuple> results) {
+        var valueMapper = new LinkedHashMap<String, Function<Tuple, ?>>();
+        for (TupleElement<?> kolonne : results.getFirst().getElements()) {
+            valueMapper.put(Optional.ofNullable(kolonne.getAlias()).orElse("col-" + kolonne), t -> t.get(kolonne));
+            if (kolonne.getAlias().equals("vilkar_type_kode")) {
+                valueMapper.put("vilkar_type", t -> VilkårType.fraKode(t.get(kolonne)).name());
+            }
+        }
+        return valueMapper;
     }
 }
