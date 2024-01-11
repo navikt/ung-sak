6534a56b62551ad728b9cd0a4a37ecbf05f4947a
oyvemb <oyvind.andre.hagen.emblemsvag@nav.no>
2024-01-11 15:34:26 +0100

    revert unødvendig

--- behandlingsprosess/src/main/java/no/nav/k9/sak/domene/behandling/steg/medlemskap/VurderMedlemskapSteg.java
+++ behandlingsprosess/src/main/java/no/nav/k9/sak/domene/behandling/steg/medlemskap/VurderMedlemskapSteg.java
@@ -135,15 +135,15 @@ public class VurderMedlemskapSteg implements BehandlingSteg {
 
         mapPerioderTilVilkårsPerioderMedForlengelse(vilkårBuilder, utgangspunkt, vurderingerOgForlengelsesPerioder);
 
+        vilkårResultatBuilder.leggTil(vilkårBuilder);
+        final Vilkårene nyttResultat = vilkårResultatBuilder.build();
+
         //TODO revert
         if (behandlingId == 1744679L) { // 9WKJA
             for (LocalDate key : vurderingerOgForlengelsesPerioder.getVurderinger().keySet()) {
                 log.info("dato: {}, vurderingsperiode: {}, vurdering: {}", key, vurderingerOgForlengelsesPerioder.getVurderinger().get(key).getPeriode(), vurderingerOgForlengelsesPerioder.getVurderinger().get(key).getUtfallType());
             }
         }
-
-        vilkårResultatBuilder.leggTil(vilkårBuilder);
-        final Vilkårene nyttResultat = vilkårResultatBuilder.build();
 
         validerAtAltErVurdert(nyttResultat);
 
