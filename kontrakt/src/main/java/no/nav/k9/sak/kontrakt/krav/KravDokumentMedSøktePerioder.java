f1e44dd663943d3a7c7c6fa0fdbd10b3774db5bd
oyvemb <oyvind.andre.hagen.emblemsvag@nav.no>
2024-01-17 16:02:19 +0100

    json validering

--- kontrakt/src/main/java/no/nav/k9/sak/kontrakt/krav/KravDokumentMedSøktePerioder.java
+++ kontrakt/src/main/java/no/nav/k9/sak/kontrakt/krav/KravDokumentMedSøktePerioder.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
@@ -39,6 +40,9 @@ public class KravDokumentMedSøktePerioder {
     @JsonProperty("søktePerioder")
     private List<SøktPeriode> søktePerioder;
 
+    @Valid
+    @Size(max = 100)
+    @Pattern(regexp = "^\\p{L}+$")
     @JsonProperty("kildesystem")
     private String kildesystem;
 
