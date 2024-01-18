50054bd4b4c79a87ee8e4fafd83913160f1e0898
Espen Johansen Velsvik <espen.velsvik@nav.no>
2024-01-18 12:44:25 +0100

    Toggler på endring som filtrerer bort perioder som ikke har relevante endringer for revurdering i beregning

--- ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/vilkår/OMPUttakEndringsutleder.java
+++ ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/vilkår/OMPUttakEndringsutleder.java
@@ -4,34 +4,21 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import jakarta.enterprise.context.Dependent;
-import jakarta.inject.Inject;
 import no.nav.k9.aarskvantum.kontrakter.Aktivitet;
 import no.nav.k9.aarskvantum.kontrakter.FullUttaksplanForBehandlinger;
 import no.nav.k9.aarskvantum.kontrakter.Uttaksperiode;
-import no.nav.k9.felles.konfigurasjon.konfig.KonfigVerdi;
-import no.nav.k9.sak.typer.Saksnummer;
 
-@Dependent
 public class OMPUttakEndringsutleder {
 
-    private static final Set<String> ENABLE_SAKSNUMMER = Set.of("Bo8FM");
 
-    private final boolean enableRelevantEndringUtledning;
-
-    @Inject
-    public OMPUttakEndringsutleder(@KonfigVerdi(value = "OMP_RELEVANT_ENDRING_UTLEDNING", defaultVerdi = "false") boolean enableRelevantEndringUtledning) {
-        this.enableRelevantEndringUtledning = enableRelevantEndringUtledning;
+    private OMPUttakEndringsutleder() {
+        // SKjul konstruktør
     }
 
-    boolean harRelevantEndringFraForrige(Saksnummer saksnummer, Uttaksperiode periode, Optional<FullUttaksplanForBehandlinger> fullUttaksplanForrigeBehandling) {
-
-        if (!enableRelevantEndringUtledning && !ENABLE_SAKSNUMMER.contains(saksnummer.getVerdi())) {
-            return true;
-        }
+    static boolean harRelevantEndringFraForrige(Uttaksperiode periode, Optional<FullUttaksplanForBehandlinger> fullUttaksplanForrigeBehandling) {
 
         var overlappendePerioder = fullUttaksplanForrigeBehandling.map(p -> p
             .getAktiviteter()
