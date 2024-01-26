8ce8041cd3b14c94f38b5cd38b274f11827bd49f
Bjørn Erik Lømo <boerta@gmail.com>
2024-01-26 14:33:12 +0100

    Restoppslag for å berike klagesaker i k9-los (#10186)
    * Restoppslag for å berike klagesaker i k9-los
    
    * Navneendring
    
    * Annotasjon for beskyttet ressurs
    
    * Valideringsannotasjon
    
    * codereview restendepunkt-adresse
    
    * Codereview tilgjengeliggjøre dto for ekstern

--- web/src/main/java/no/nav/k9/sak/web/app/tjenester/los/LosRestTjeneste.java
+++ web/src/main/java/no/nav/k9/sak/web/app/tjenester/los/LosRestTjeneste.java
@@ -6,8 +6,6 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.enterprise.inject.Any;
-import jakarta.enterprise.inject.Instance;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
@@ -17,17 +15,21 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import no.nav.k9.felles.sikkerhet.abac.BeskyttetRessurs;
 import no.nav.k9.felles.sikkerhet.abac.TilpassetAbacAttributt;
+import no.nav.k9.kodeverk.behandling.aksjonspunkt.AksjonspunktKodeDefinisjon;
 import no.nav.k9.kodeverk.behandling.aksjonspunkt.SkjermlenkeType;
 import no.nav.k9.kodeverk.historikk.HistorikkinnslagType;
 import no.nav.k9.sak.behandling.hendelse.produksjonsstyring.BehandlingProsessHendelseMapper;
+import no.nav.k9.sak.behandlingslager.behandling.Behandling;
 import no.nav.k9.sak.behandlingslager.behandling.repository.BehandlingRepository;
+import no.nav.k9.sak.behandlingslager.fagsak.FagsakRepository;
 import no.nav.k9.sak.historikk.HistorikkTjenesteAdapter;
 import no.nav.k9.sak.kontrakt.behandling.BehandlingUuidDto;
 import no.nav.k9.sak.kontrakt.produksjonsstyring.los.BehandlingMedFagsakDto;
-import no.nav.k9.sak.perioder.VurderSøknadsfristTjeneste;
-import no.nav.k9.sak.web.app.tjenester.behandling.BehandlingDtoTjeneste;
+import no.nav.k9.sak.kontrakt.produksjonsstyring.los.LosOpplysningerSomManglerIKlageDto;
 import no.nav.k9.sak.web.server.abac.AbacAttributtSupplier;
 import no.nav.k9.sikkerhet.context.SubjectHandler;
+
+import java.util.Optional;
 
 import static no.nav.k9.abac.BeskyttetRessursKoder.FAGSAK;
 import static no.nav.k9.felles.sikkerhet.abac.BeskyttetRessursActionAttributt.READ;
@@ -42,6 +44,7 @@ public class LosRestTjeneste {
     public static final String BASE_PATH = "/los";
     public static final String MERKNAD = "/merknad";
     public static final String BEHANDLING = "/behandling";
+    public static final String BERIK_KLAGE = "/klage/berik";
     public static final String MERKNAD_PATH = BASE_PATH + MERKNAD;
     public static final String BEHANDLING_PATH = BASE_PATH + BEHANDLING;
 
@@ -49,6 +52,7 @@ public class LosRestTjeneste {
     private HistorikkTjenesteAdapter historikkTjenesteAdapter;
     private BehandlingRepository behandlingRepository;
     private BehandlingProsessHendelseMapper behandlingProsessHendelseMapper;
+    private FagsakRepository fagsakRepository;
 
 
     public LosRestTjeneste() {
@@ -60,11 +64,54 @@ public class LosRestTjeneste {
         LosSystemUserKlient losKlient,
         HistorikkTjenesteAdapter historikkTjenesteAdapter,
         BehandlingRepository behandlingRepository,
-        BehandlingProsessHendelseMapper behandlingProsessHendelseMapper) {
+        BehandlingProsessHendelseMapper behandlingProsessHendelseMapper, FagsakRepository fagsakRepository) {
         this.losKlient = losKlient;
         this.historikkTjenesteAdapter = historikkTjenesteAdapter;
         this.behandlingRepository = behandlingRepository;
         this.behandlingProsessHendelseMapper = behandlingProsessHendelseMapper;
+        this.fagsakRepository = fagsakRepository;
+    }
+
+    @GET
+    @Path(BERIK_KLAGE)
+    @Operation(
+        description = "",
+        summary = "",
+        tags = "los",
+        responses = {
+            @ApiResponse(
+                responseCode = "200",
+                description = "Returnerer opplysninger los trenger for å fylle ut klageoppgaver",
+                content = @Content(mediaType = MediaType.APPLICATION_JSON,
+                    schema = @Schema(implementation = LosOpplysningerSomManglerIKlageDto.class))
+            )
+        })
+    @BeskyttetRessurs(action = READ, resource = FAGSAK)
+    public Response hentLosdataForKlage(
+            @NotNull
+            @QueryParam(BehandlingUuidDto.NAME)
+            @Parameter(description = BehandlingUuidDto.DESC)
+            @Valid
+            @TilpassetAbacAttributt(supplierClass = AbacAttributtSupplier.class)
+            BehandlingUuidDto påklagdBehandlingUuid) {
+        Optional<Behandling> behandling = behandlingRepository.hentBehandlingHvisFinnes(påklagdBehandlingUuid.getBehandlingUuid());
+        if (behandling.isPresent()) {
+            LosOpplysningerSomManglerIKlageDto dto = new LosOpplysningerSomManglerIKlageDto();
+            dto.setPleietrengendeAktørId(behandling.get().getFagsak().getPleietrengendeAktørId());
+
+            dto.setUtenlandstilsnitt(behandling.get().getAksjonspunkter()
+                .stream()
+                .anyMatch(ap ->
+                    ap.getAksjonspunktDefinisjon().getKode().equals(AksjonspunktKodeDefinisjon.AUTOMATISK_MARKERING_AV_UTENLANDSSAK_KODE)
+                 || ap.getAksjonspunktDefinisjon().getKode().equals(AksjonspunktKodeDefinisjon.MANUELL_MARKERING_AV_UTLAND_SAKSTYPE_KODE)));
+
+
+
+            Response.ResponseBuilder responseBuilder = Response.ok().entity(dto);
+            return responseBuilder.build();
+        } else {
+            return Response.noContent().build();
+        }
     }
 
     @GET
