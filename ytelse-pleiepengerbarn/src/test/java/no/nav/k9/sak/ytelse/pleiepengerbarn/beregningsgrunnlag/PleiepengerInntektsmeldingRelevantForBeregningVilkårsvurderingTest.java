6eade4b115e9076a1df63a58dbf4147bf9f880b7
Espen Johansen Velsvik <espen.velsvik@nav.no>
2024-01-30 13:46:23 +0100

    TSFF-61 Tar hensyn til arbeidsforholdId i filtrering mot opptjeningaktiviteter

--- ytelse-pleiepengerbarn/src/test/java/no/nav/k9/sak/ytelse/pleiepengerbarn/beregningsgrunnlag/PleiepengerInntektsmeldingRelevantForBeregningVilkårsvurderingTest.java
+++ ytelse-pleiepengerbarn/src/test/java/no/nav/k9/sak/ytelse/pleiepengerbarn/beregningsgrunnlag/PleiepengerInntektsmeldingRelevantForBeregningVilkårsvurderingTest.java
@@ -52,4 +52,122 @@ class PleiepengerInntektsmeldingRelevantForBeregningVilkårsvurderingTest {
         assertThat(filtrert.size()).isEqualTo(1);
         assertThat(filtrert.get(0).getArbeidsgiver().getArbeidsgiverOrgnr()).isEqualTo(orgnr1);
     }
+
+
+    @Test
+    void skal_filtrere_bort_arbeidsforhold_dersom_det_ikke_finnes_i_opptjeningsaktiviteter() {
+
+        var orgnr1 = "123456789";
+        var ref1 = InternArbeidsforholdRef.nyRef();
+        var im1 = InntektsmeldingBuilder.builder()
+            .medJournalpostId("1")
+            .medStartDatoPermisjon(LocalDate.now())
+            .medKanalreferanse("kanalreferanser")
+            .medArbeidsgiver(Arbeidsgiver.virksomhet(orgnr1))
+            .medArbeidsforholdId(ref1)
+            .medBeløp(BigDecimal.TEN)
+            .build();
+
+        var im2 = InntektsmeldingBuilder.builder()
+            .medJournalpostId("2")
+            .medArbeidsgiver(Arbeidsgiver.virksomhet(orgnr1))
+            .medArbeidsforholdId(InternArbeidsforholdRef.nyRef())
+            .medStartDatoPermisjon(LocalDate.now())
+            .medKanalreferanse("kanalreferanser")
+            .medBeløp(BigDecimal.TEN)
+            .build();
+
+
+        var filtrert = PleiepengerInntektsmeldingRelevantForBeregningVilkårsvurdering.filtrerForAktiviteter(List.of(im1, im2),
+            Optional.of(OpptjeningAktiviteter.fraOrgnr(OpptjeningAktivitetType.ARBEID,
+                DatoIntervallEntitet.fraOgMedTilOgMed(LocalDate.now().minusDays(10), LocalDate.now().plusDays(10)),
+                orgnr1,
+                ref1
+            )));
+
+
+        assertThat(filtrert.size()).isEqualTo(1);
+        assertThat(filtrert.get(0).getArbeidsgiver().getArbeidsgiverOrgnr()).isEqualTo(orgnr1);
+        assertThat(filtrert.get(0).getArbeidsforholdRef().getReferanse()).isEqualTo(ref1.getReferanse());
+
+    }
+
+    @Test
+    void skal_filtrere_bort_arbeidsforhold_uten_id_dersom_det_ikke_finnes_i_opptjeningsaktiviteter() {
+
+        var orgnr1 = "123456789";
+        var ref1 = InternArbeidsforholdRef.nyRef();
+        var im1 = InntektsmeldingBuilder.builder()
+            .medJournalpostId("1")
+            .medStartDatoPermisjon(LocalDate.now())
+            .medKanalreferanse("kanalreferanser")
+            .medArbeidsgiver(Arbeidsgiver.virksomhet(orgnr1))
+            .medArbeidsforholdId(ref1)
+            .medBeløp(BigDecimal.TEN)
+            .build();
+
+        var im2 = InntektsmeldingBuilder.builder()
+            .medJournalpostId("2")
+            .medArbeidsgiver(Arbeidsgiver.virksomhet(orgnr1))
+            .medArbeidsforholdId(InternArbeidsforholdRef.nullRef())
+            .medStartDatoPermisjon(LocalDate.now())
+            .medKanalreferanse("kanalreferanser")
+            .medBeløp(BigDecimal.TEN)
+            .build();
+
+
+        var filtrert = PleiepengerInntektsmeldingRelevantForBeregningVilkårsvurdering.filtrerForAktiviteter(List.of(im1, im2),
+            Optional.of(OpptjeningAktiviteter.fraOrgnr(OpptjeningAktivitetType.ARBEID,
+                DatoIntervallEntitet.fraOgMedTilOgMed(LocalDate.now().minusDays(10), LocalDate.now().plusDays(10)),
+                orgnr1,
+                ref1
+            )));
+
+
+        assertThat(filtrert.size()).isEqualTo(1);
+        assertThat(filtrert.get(0).getArbeidsgiver().getArbeidsgiverOrgnr()).isEqualTo(orgnr1);
+        assertThat(filtrert.get(0).getArbeidsforholdRef().getReferanse()).isEqualTo(ref1.getReferanse());
+
+    }
+
+
+    @Test
+    void skal_filtrere_bort_arbeidsforhold_med_id_dersom_opptjeningsaktiviteter_ikke_har_id() {
+
+        var orgnr1 = "123456789";
+        var ref1 = InternArbeidsforholdRef.nyRef();
+        var im1 = InntektsmeldingBuilder.builder()
+            .medJournalpostId("1")
+            .medStartDatoPermisjon(LocalDate.now())
+            .medKanalreferanse("kanalreferanser")
+            .medArbeidsgiver(Arbeidsgiver.virksomhet(orgnr1))
+            .medArbeidsforholdId(ref1)
+            .medBeløp(BigDecimal.TEN)
+            .build();
+
+        var im2 = InntektsmeldingBuilder.builder()
+            .medJournalpostId("2")
+            .medArbeidsgiver(Arbeidsgiver.virksomhet(orgnr1))
+            .medArbeidsforholdId(InternArbeidsforholdRef.nullRef())
+            .medStartDatoPermisjon(LocalDate.now())
+            .medKanalreferanse("kanalreferanser")
+            .medBeløp(BigDecimal.TEN)
+            .build();
+
+
+        var filtrert = PleiepengerInntektsmeldingRelevantForBeregningVilkårsvurdering.filtrerForAktiviteter(List.of(im1, im2),
+            Optional.of(OpptjeningAktiviteter.fraOrgnr(OpptjeningAktivitetType.ARBEID,
+                DatoIntervallEntitet.fraOgMedTilOgMed(LocalDate.now().minusDays(10), LocalDate.now().plusDays(10)),
+                orgnr1,
+                InternArbeidsforholdRef.nullRef()
+            )));
+
+
+        assertThat(filtrert.size()).isEqualTo(1);
+        assertThat(filtrert.get(0).getArbeidsgiver().getArbeidsgiverOrgnr()).isEqualTo(orgnr1);
+        assertThat(filtrert.get(0).getArbeidsforholdRef().getReferanse()).isNull();
+
+    }
+
+
 }
