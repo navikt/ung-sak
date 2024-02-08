b879018d50ebbbbaf1c835fc5f5de820b13e551e
Espen Johansen Velsvik <espen.velsvik@nav.no>
2024-02-08 17:01:45 +0100

    Fjerner rekursive kall til vilkårsperiodeutleder

--- ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/vilkår/revurdering/PleietrengendeRevurderingPerioderTjeneste.java
+++ ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/vilkår/revurdering/PleietrengendeRevurderingPerioderTjeneste.java
@@ -3,6 +3,7 @@ package no.nav.k9.sak.ytelse.pleiepengerbarn.vilkår.revurdering;
 import java.util.List;
 import java.util.NavigableSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -10,9 +11,14 @@ import no.nav.fpsak.tidsserie.LocalDateSegment;
 import no.nav.fpsak.tidsserie.LocalDateTimeline;
 import no.nav.fpsak.tidsserie.StandardCombinators;
 import no.nav.k9.kodeverk.behandling.BehandlingÅrsakType;
+import no.nav.k9.kodeverk.vilkår.VilkårType;
 import no.nav.k9.sak.behandling.BehandlingReferanse;
 import no.nav.k9.sak.behandlingslager.behandling.repository.BehandlingRepository;
+import no.nav.k9.sak.behandlingslager.behandling.vilkår.VilkårResultatRepository;
+import no.nav.k9.sak.behandlingslager.behandling.vilkår.Vilkårene;
+import no.nav.k9.sak.behandlingslager.behandling.vilkår.periode.VilkårPeriode;
 import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
+import no.nav.k9.sak.domene.typer.tid.TidslinjeUtil;
 import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.etablerttilsyn.ErEndringPåEtablertTilsynTjeneste;
 import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.sykdom.medisinsk.MedisinskGrunnlag;
 import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.sykdom.medisinsk.MedisinskGrunnlagTjeneste;
@@ -22,9 +28,9 @@ import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.unntaketablerttilsyn.EndringUnn
 public class PleietrengendeRevurderingPerioderTjeneste {
 
     private BehandlingRepository behandlingRepository;
+    private VilkårResultatRepository vilkårResultatRepository;
     private MedisinskGrunnlagTjeneste medisinskGrunnlagTjeneste;
     private ErEndringPåEtablertTilsynTjeneste etablertTilsynTjeneste;
-
     private EndringUnntakEtablertTilsynTjeneste endringUnntakEtablertTilsynTjeneste;
 
     PleietrengendeRevurderingPerioderTjeneste() {
@@ -33,25 +39,28 @@ public class PleietrengendeRevurderingPerioderTjeneste {
 
     @Inject
     public PleietrengendeRevurderingPerioderTjeneste(BehandlingRepository behandlingRepository,
+                                                     VilkårResultatRepository vilkårResultatRepository,
                                                      MedisinskGrunnlagTjeneste medisinskGrunnlagTjeneste,
                                                      ErEndringPåEtablertTilsynTjeneste etablertTilsynTjeneste,
                                                      EndringUnntakEtablertTilsynTjeneste endringUnntakEtablertTilsynTjeneste) {
         this.behandlingRepository = behandlingRepository;
+        this.vilkårResultatRepository = vilkårResultatRepository;
         this.medisinskGrunnlagTjeneste = medisinskGrunnlagTjeneste;
         this.etablertTilsynTjeneste = etablertTilsynTjeneste;
         this.endringUnntakEtablertTilsynTjeneste = endringUnntakEtablertTilsynTjeneste;
     }
 
 
-    /** Finner tidslinje for endringer på pleietrengende med tilhørende behandlingsårsak
+    /**
+     * Finner tidslinje for endringer på pleietrengende med tilhørende behandlingsårsak
      *
-     * @param referanse Behandlingsreferanser
-     * @param vurderingsperioder Oppdaterte vurderingsperioder etter vurdering av sykdom
+     * @param referanse         Behandlingsreferanser
+     * @param definerendeVilkår Definerende vilkår for ytelsen
      * @return Tidslinje med årsaker
      */
     public LocalDateTimeline<Set<BehandlingÅrsakType>> utledBerørtePerioderPåPleietrengende(BehandlingReferanse referanse,
-                                                                                            NavigableSet<DatoIntervallEntitet> vurderingsperioder) {
-        LocalDateTimeline<Set<BehandlingÅrsakType>> utvidedePerioder = utledUtvidetPeriodeForSykdom(referanse, vurderingsperioder);
+                                                                                            Set<VilkårType> definerendeVilkår) {
+        LocalDateTimeline<Set<BehandlingÅrsakType>> utvidedePerioder = utledUtvidetPeriodeForSykdom(referanse, definerendeVilkår);
         utvidedePerioder = utvidedePerioder.union(utledPerioderEndringIEtablertTilsyn(referanse), StandardCombinators::union);
         utvidedePerioder = utvidedePerioder.union(utledPerioderMedEndringIUnntakFraEtablertTilsyn(referanse), StandardCombinators::union);
         return utvidedePerioder;
@@ -68,13 +77,29 @@ public class PleietrengendeRevurderingPerioderTjeneste {
     }
 
 
-    private LocalDateTimeline<Set<BehandlingÅrsakType>> utledUtvidetPeriodeForSykdom(BehandlingReferanse referanse, NavigableSet<DatoIntervallEntitet> vurderingsperioder) {
+    private LocalDateTimeline<Set<BehandlingÅrsakType>> utledUtvidetPeriodeForSykdom(BehandlingReferanse referanse, Set<VilkårType> definerendeVilkår) {
         var forrigeVedtatteBehandling = behandlingRepository.hentBehandling(referanse.getOriginalBehandlingId().orElseThrow()).getUuid();
         var vedtattSykdomGrunnlagBehandling = medisinskGrunnlagTjeneste.hentGrunnlagHvisEksisterer(forrigeVedtatteBehandling);
         var pleietrengende = referanse.getPleietrengendeAktørId();
+        var vurderingsperioder = utledVurderingsperiode(vilkårResultatRepository.hent(referanse.getBehandlingId()), definerendeVilkår);
         var utledetGrunnlag = medisinskGrunnlagTjeneste.utledGrunnlagMedManglendeOmsorgFjernet(referanse.getSaksnummer(), referanse.getBehandlingUuid(), referanse.getBehandlingId(), pleietrengende, vurderingsperioder);
         var diffPerioder = medisinskGrunnlagTjeneste.sammenlignGrunnlag(vedtattSykdomGrunnlagBehandling.map(MedisinskGrunnlag::getGrunnlagsdata), utledetGrunnlag).getDiffPerioder();
         return diffPerioder.map(s -> List.of(new LocalDateSegment<>(s.getLocalDateInterval(), Set.of(BehandlingÅrsakType.RE_SYKDOM_ENDRING_FRA_ANNEN_OMSORGSPERSON))));
+    }
+
+
+    private NavigableSet<DatoIntervallEntitet> utledVurderingsperiode(Vilkårene vilkårene, Set<VilkårType> definerendeVilkår) {
+        LocalDateTimeline<Boolean> tidslinje = LocalDateTimeline.empty();
+        for (VilkårType vilkårType : definerendeVilkår) {
+            var vilkår = vilkårene.getVilkår(vilkårType);
+            if (vilkår.isPresent()) {
+                var vilkårperioder = vilkår.get().getPerioder();
+                var perioder = vilkårperioder.stream().map(VilkårPeriode::getPeriode).toList();
+                tidslinje = tidslinje.combine(TidslinjeUtil.tilTidslinjeKomprimert(new TreeSet<>(perioder)), StandardCombinators::alwaysTrueForMatch, LocalDateTimeline.JoinStyle.CROSS_JOIN);
+            }
+        }
+
+        return TidslinjeUtil.tilDatoIntervallEntiteter(tidslinje);
     }
 
 }
