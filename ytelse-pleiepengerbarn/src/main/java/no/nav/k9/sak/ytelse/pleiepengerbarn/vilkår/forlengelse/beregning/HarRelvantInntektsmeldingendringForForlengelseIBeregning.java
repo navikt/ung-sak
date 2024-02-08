fb7e6f311cf7ecc354476415e3273a6b3c1c2608
Espen Johansen Velsvik <espen.velsvik@nav.no>
2024-02-08 16:20:35 +0100

    Sjekker også om det er diff i naturalytelser

--- ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/vilkår/forlengelse/beregning/HarRelvantInntektsmeldingendringForForlengelseIBeregning.java
+++ ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/vilkår/forlengelse/beregning/HarRelvantInntektsmeldingendringForForlengelseIBeregning.java
@@ -6,6 +6,7 @@ import java.util.Set;
 import jakarta.enterprise.context.ApplicationScoped;
 import no.nav.k9.sak.domene.behandling.steg.beregningsgrunnlag.HarEndretInntektsmeldingVurderer;
 import no.nav.k9.sak.domene.iay.modell.Inntektsmelding;
+import no.nav.k9.sak.domene.iay.modell.NaturalYtelse;
 import no.nav.k9.sak.typer.JournalpostId;
 
 @ApplicationScoped
@@ -13,9 +14,36 @@ public class HarRelvantInntektsmeldingendringForForlengelseIBeregning implements
 
     @Override
     public boolean erEndret(Collection<Inntektsmelding> relevanteInntektsmeldinger, Collection<Inntektsmelding> relevanteInntektsmeldingerForrigeVedtak) {
-        return !erLikeStore(relevanteInntektsmeldinger, relevanteInntektsmeldingerForrigeVedtak) || relevanteInntektsmeldinger.stream().anyMatch(im -> harEndretBeløpFraForrige(relevanteInntektsmeldingerForrigeVedtak, im));
+        return !erLikeStore(relevanteInntektsmeldinger, relevanteInntektsmeldingerForrigeVedtak) ||
+            relevanteInntektsmeldinger.stream().anyMatch(im -> harEndretBeløpFraForrige(relevanteInntektsmeldingerForrigeVedtak, im) ||
+                harEndretNaturalytelserFraForrige(relevanteInntektsmeldingerForrigeVedtak, im));
     }
 
+
+    private static boolean harEndretNaturalytelserFraForrige(Collection<Inntektsmelding> relevanteInntektsmeldingerForrigeVedtak, Inntektsmelding im) {
+        var matchendeIM = relevanteInntektsmeldingerForrigeVedtak.stream().filter(imForrige -> imForrige.getArbeidsgiver().equals(im.getArbeidsgiver()) && imForrige.getArbeidsforholdRef().equals(im.getArbeidsforholdRef()))
+            .toList();
+
+        if (matchendeIM.size() != 1) {
+            return true;
+        }
+
+        var matchFraForrige = matchendeIM.get(0);
+        var harLikeMangeNaturalytelser = erLikeStore(matchFraForrige.getNaturalYtelser(), im.getNaturalYtelser());
+        return !harLikeMangeNaturalytelser || im.getNaturalYtelser().stream().anyMatch(naturalYtelse -> harIngenMatchendeNaturalytelse(naturalYtelse, matchFraForrige));
+    }
+
+    private static boolean harIngenMatchendeNaturalytelse(NaturalYtelse naturalYtelse, Inntektsmelding inntektmelding) {
+        return inntektmelding.getNaturalYtelser().stream().noneMatch(
+            naturalYtelseForrige -> erLike(naturalYtelse, naturalYtelseForrige)
+        );
+    }
+
+    private static boolean erLike(NaturalYtelse naturalYtelse, NaturalYtelse naturalYtelseForrige) {
+        return naturalYtelseForrige.getType().equals(naturalYtelse.getType()) &&
+            naturalYtelseForrige.getPeriode().equals(naturalYtelse.getPeriode()) &&
+            naturalYtelseForrige.getBeloepPerMnd().compareTo(naturalYtelse.getBeloepPerMnd()) == 0;
+    }
 
     private static boolean harEndretBeløpFraForrige(Collection<Inntektsmelding> relevanteInntektsmeldingerForrigeVedtak, Inntektsmelding im) {
         var matchendeIM = relevanteInntektsmeldingerForrigeVedtak.stream().filter(imForrige -> imForrige.getArbeidsgiver().equals(im.getArbeidsgiver()) && imForrige.getArbeidsforholdRef().equals(im.getArbeidsforholdRef()))
