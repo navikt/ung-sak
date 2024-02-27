d4f959114d83288920bdb951f4c332c65d0165ca
oyvemb <107673448+oyvemb@users.noreply.github.com>
2024-02-27 10:37:32 +0100

    Reservert saksnummer v3 (#10368)
    * legger på behandlingsår
    
    * Returnerer eksisterende reservert saksnummer hvis det finnes
    
    * Tjeneste for å hente alle reserverte på søker

--- behandlingslager/domene/src/main/java/no/nav/k9/sak/behandlingslager/saksnummer/ReservertSaksnummerRepository.java
+++ behandlingslager/domene/src/main/java/no/nav/k9/sak/behandlingslager/saksnummer/ReservertSaksnummerRepository.java
@@ -1,5 +1,6 @@
 package no.nav.k9.sak.behandlingslager.saksnummer;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -9,6 +10,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 import no.nav.k9.felles.jpa.HibernateVerktøy;
 import no.nav.k9.kodeverk.behandling.FagsakYtelseType;
+import no.nav.k9.sak.typer.AktørId;
 import no.nav.k9.sak.typer.Saksnummer;
 
 @Dependent
@@ -28,11 +30,53 @@ public class ReservertSaksnummerRepository {
         return HibernateVerktøy.hentUniktResultat(query);
     }
 
-    public void lagre(Saksnummer saksnummer, FagsakYtelseType ytelseType, String brukerAktørid, String pleietrengendeAktørId) {
+    public List<ReservertSaksnummerEntitet> hent(AktørId brukerAktørId) {
+        final TypedQuery<ReservertSaksnummerEntitet> query = entityManager.createQuery("FROM ReservertSaksnummer where brukerAktørId=:brukerAktørId and slettet=false", ReservertSaksnummerEntitet.class);
+        query.setParameter("brukerAktørId", brukerAktørId);
+        return query.getResultList();
+    }
+
+    public Optional<ReservertSaksnummerEntitet> hent(FagsakYtelseType ytelseType, String brukerAktørId, String pleietrengendeAktørId, String behandlingsår) {
+        //TODO vurder refaktorering når vi får avklart hvordan null pleietrengende skal oppføre seg. Går an å lage ytelsesspesifikke tjenesteklasser.
+        if (pleietrengendeAktørId == null && behandlingsår == null) {
+            return Optional.empty();
+        }
+        if (pleietrengendeAktørId == null) {
+            return hentUtenPleietrengende(ytelseType, brukerAktørId, behandlingsår);
+        }
+        if (behandlingsår == null) {
+            return hentUtenBehandlingsår(ytelseType, brukerAktørId, pleietrengendeAktørId);
+        }
+
+        final TypedQuery<ReservertSaksnummerEntitet> query = entityManager.createQuery("FROM ReservertSaksnummer where ytelseType=:ytelseType and brukerAktørId=:brukerAktørId and pleietrengendeAktørId=:pleietrengendeAktørId and behandlingsår=:behandlingsår and slettet=false", ReservertSaksnummerEntitet.class);
+        query.setParameter("ytelseType", ytelseType);
+        query.setParameter("brukerAktørId", new AktørId(brukerAktørId));
+        query.setParameter("pleietrengendeAktørId", new AktørId(pleietrengendeAktørId));
+        query.setParameter("behandlingsår", behandlingsår);
+        return HibernateVerktøy.hentUniktResultat(query);
+    }
+
+    private Optional<ReservertSaksnummerEntitet> hentUtenPleietrengende(FagsakYtelseType ytelseType, String brukerAktørId, String behandlingsår) {
+        final TypedQuery<ReservertSaksnummerEntitet> query = entityManager.createQuery("FROM ReservertSaksnummer where ytelseType=:ytelseType and brukerAktørId=:brukerAktørId and pleietrengendeAktørId is null and behandlingsår=:behandlingsår and slettet=false", ReservertSaksnummerEntitet.class);
+        query.setParameter("ytelseType", ytelseType);
+        query.setParameter("brukerAktørId", new AktørId(brukerAktørId));
+        query.setParameter("behandlingsår", behandlingsår);
+        return HibernateVerktøy.hentUniktResultat(query);
+    }
+
+    private Optional<ReservertSaksnummerEntitet> hentUtenBehandlingsår(FagsakYtelseType ytelseType, String brukerAktørId, String pleietrengendeAktørId) {
+        final TypedQuery<ReservertSaksnummerEntitet> query = entityManager.createQuery("FROM ReservertSaksnummer where ytelseType=:ytelseType and brukerAktørId=:brukerAktørId and pleietrengendeAktørId=:pleietrengendeAktørId and behandlingsår is null and slettet=false", ReservertSaksnummerEntitet.class);
+        query.setParameter("ytelseType", ytelseType);
+        query.setParameter("brukerAktørId", new AktørId(brukerAktørId));
+        query.setParameter("pleietrengendeAktørId", new AktørId(pleietrengendeAktørId));
+        return HibernateVerktøy.hentUniktResultat(query);
+    }
+
+    public void lagre(Saksnummer saksnummer, FagsakYtelseType ytelseType, String brukerAktørId, String pleietrengendeAktørId, String behandlingsår) {
         if (hent(saksnummer).isPresent()) {
             throw new IllegalArgumentException("Saksnummer er allerede reservert");
         }
-        final var entitet = new ReservertSaksnummerEntitet(saksnummer, ytelseType, brukerAktørid, pleietrengendeAktørId);
+        final var entitet = new ReservertSaksnummerEntitet(saksnummer, ytelseType, brukerAktørId, pleietrengendeAktørId, behandlingsår);
         entityManager.persist(entitet);
         entityManager.flush();
     }
--- kontrakt/src/main/java/no/nav/k9/sak/kontrakt/mottak/HentReservertSaksnummerDto.java
+++ kontrakt/src/main/java/no/nav/k9/sak/kontrakt/mottak/HentReservertSaksnummerDto.java
@@ -39,15 +39,21 @@ public class HentReservertSaksnummerDto {
     @Digits(integer = 19, fraction = 0)
     private String pleietrengendeAktørId;
 
+    @JsonProperty(value = "behandlingsår")
+    @Digits(integer = 4, fraction = 0)
+    private String behandlingsår;
+
     @JsonCreator
     public HentReservertSaksnummerDto(@JsonProperty(value = "saksnummer", required = true) @NotNull @Size(max = 19) @Pattern(regexp = "^[a-zA-Z0-9]*$") String saksnummer,
                                       @JsonProperty(value = "ytelseType", required = true) @NotNull FagsakYtelseType ytelseType,
                                       @JsonProperty(value = "brukerAktørId", required = true) @NotNull String brukerAktørId,
-                                      @JsonProperty(value = "pleietrengendeAktørId") String pleietrengendeAktørId) {
+                                      @JsonProperty(value = "pleietrengendeAktørId") String pleietrengendeAktørId,
+                                      @JsonProperty(value = "behandlingsår") String behandlingsår) {
         this.saksnummer = Objects.requireNonNull(saksnummer, "saksnummer");
         this.ytelseType = ytelseType;
         this.brukerAktørId = Objects.requireNonNull(brukerAktørId, "brukerAktørId");
         this.pleietrengendeAktørId = pleietrengendeAktørId;
+        this.behandlingsår = behandlingsår;
     }
 
     public String getSaksnummer() {
@@ -64,5 +70,9 @@ public class HentReservertSaksnummerDto {
 
     public String getPleietrengendeAktørId() {
         return pleietrengendeAktørId;
+    }
+
+    public String getBehandlingsår() {
+        return behandlingsår;
     }
 }
