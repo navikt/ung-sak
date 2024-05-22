a74daa63a4b21ed29ec355942d4419b113f7961a
Morten Osvik <morten.osvik@bekk.no>
2024-05-22 11:03:53 +0200

    Legger til Consumes json

--- web/src/main/java/no/nav/k9/sak/web/app/tjenester/brukerdialog/BrukerdialogRestTjeneste.java
+++ web/src/main/java/no/nav/k9/sak/web/app/tjenester/brukerdialog/BrukerdialogRestTjeneste.java
@@ -5,6 +5,7 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -37,6 +38,7 @@ public class BrukerdialogRestTjeneste {
 
     @POST
     @Path("/omsorgsdager-kronisk-sykt-barn/har-gyldig-vedtak")
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Operation(description = "Returnerer nyeste gyldige vedtak for en gitt aktørId", summary = "Returnerer nyeste gyldige vedtak for en gitt aktørId", tags = "brukerdialog")
     @BeskyttetRessurs(action = BeskyttetRessursActionAttributt.READ, resource = APPLIKASJON)
