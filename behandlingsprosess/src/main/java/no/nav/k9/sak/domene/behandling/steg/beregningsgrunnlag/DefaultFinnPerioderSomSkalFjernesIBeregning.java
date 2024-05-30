50f72b520d0b28ed9b64eb2e6d1bbef9e689d625
Audun Eltvik <audun.eltvik@nav.no>
2024-05-30 12:16:20 +0200

    Bruk riktig metode i defaultimplementasjon

--- behandlingsprosess/src/main/java/no/nav/k9/sak/domene/behandling/steg/beregningsgrunnlag/DefaultFinnPerioderSomSkalFjernesIBeregning.java
+++ behandlingsprosess/src/main/java/no/nav/k9/sak/domene/behandling/steg/beregningsgrunnlag/DefaultFinnPerioderSomSkalFjernesIBeregning.java
@@ -1,16 +1,14 @@
 package no.nav.k9.sak.domene.behandling.steg.beregningsgrunnlag;
 
 import no.nav.k9.sak.behandling.BehandlingReferanse;
-import no.nav.k9.sak.behandlingslager.behandling.vilkår.VilkårBuilder;
 import no.nav.k9.sak.behandlingslager.behandling.vilkår.Vilkårene;
 import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
 
-import java.util.HashSet;
 import java.util.Set;
 
 public class DefaultFinnPerioderSomSkalFjernesIBeregning implements FinnPerioderSomSkalFjernesIBeregning {
     @Override
-    public Set<DatoIntervallEntitet> finnPerioderSomSkalFjernes(VilkårBuilder vilkårBuilder, Vilkårene vilkårene, BehandlingReferanse behandlingReferanse) {
-        return new HashSet<>();
+    public Set<DatoIntervallEntitet> finnPerioderSomSkalFjernes(Vilkårene vilkårene, BehandlingReferanse behandlingReferanse) {
+        return Set.of();
     }
 }
