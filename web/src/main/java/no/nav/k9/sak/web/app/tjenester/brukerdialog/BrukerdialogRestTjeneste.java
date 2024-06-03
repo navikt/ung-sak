4fc402a3f0b3f1965a1d207f92c2eefa86446e06
Ramin Esfandiari <ramin.esfandiari@nav.no>
2024-06-03 13:58:06 +0200

    Legger til støtte for validering og tilgangsstyring av tokenX gjennom k9-felles for BrukerDialogRestTjeneste. (#10719)
    * Legger til vtp konfig for tokenx
    
    * Enabler tokenx i dev-fss
    
    * Fjerner annotasjon BeskyttetRessurs.
    
    * Legger tilbake @BeskyttetRessurs annotasjon for BrukerdialogRestTjeneste.
    
    * Bruk versjon 2.8.0 av k9-felles for tokenX
    
    * Toggler BrukerdialogRestTjeneste av i prod.
    
    * Hent ut ident fra token og kaller pdl for aktørId
    
    * k9-sak.openapi.json updated by build pipeline
    
    ---------
    
    Co-authored-by: Morten Osvik <morten.osvik@bekk.no>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- web/src/main/java/no/nav/k9/sak/web/app/tjenester/brukerdialog/BrukerdialogRestTjeneste.java
+++ web/src/main/java/no/nav/k9/sak/web/app/tjenester/brukerdialog/BrukerdialogRestTjeneste.java
@@ -10,10 +10,11 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import no.nav.k9.felles.konfigurasjon.konfig.KonfigVerdi;
 import no.nav.k9.felles.sikkerhet.abac.BeskyttetRessurs;
 import no.nav.k9.felles.sikkerhet.abac.BeskyttetRessursActionAttributt;
 
-import static no.nav.k9.abac.BeskyttetRessursKoder.FAGSAK;
+import static no.nav.k9.felles.sikkerhet.abac.PepImpl.TOKENX_RESOURCE;
 
 @Path(BrukerdialogRestTjeneste.BASE_PATH)
 @ApplicationScoped
@@ -24,16 +25,19 @@ public class BrukerdialogRestTjeneste {
     private static final String JSON_UTF8 = "application/json; charset=UTF-8";
 
     private BrukerdialogTjeneste brukerdialogTjeneste;
+    private boolean brukerdialogRestTjenesteEnabled;
 
     public BrukerdialogRestTjeneste() {// For Rest-CDI
     }
 
     @Inject
     public BrukerdialogRestTjeneste(
-        BrukerdialogTjeneste brukerdialogTjeneste
+        BrukerdialogTjeneste brukerdialogTjeneste,
+        @KonfigVerdi(value = "BRUKERDIALOG_REST_TJENESTE_ENABLED", required = false, defaultVerdi = "false") boolean brukerdialogRestTjenesteEnabled
 
     ) {
         this.brukerdialogTjeneste = brukerdialogTjeneste;
+        this.brukerdialogRestTjenesteEnabled = brukerdialogRestTjenesteEnabled;
     }
 
     @POST
@@ -41,12 +45,15 @@ public class BrukerdialogRestTjeneste {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Operation(description = "Returnerer nyeste gyldige vedtak for en gitt aktørId", summary = "Returnerer nyeste gyldige vedtak for en gitt aktørId", tags = "brukerdialog")
-    @BeskyttetRessurs(action = BeskyttetRessursActionAttributt.READ, resource = FAGSAK)
+    @BeskyttetRessurs(action = BeskyttetRessursActionAttributt.READ, resource = TOKENX_RESOURCE)
     // Dette endepunktet brukes av k9-sak-innsyn-api. I k9-sak-innsyn-api hentes aktørId fra token.
     public HarGyldigOmsorgsdagerVedtakDto hentSisteGyldigeVedtakForAktorId(
         @Valid HentSisteGyldigeVedtakForAktørInputDto inputDto
     ) {
-        return brukerdialogTjeneste.harGyldigOmsorgsdagerVedtak(inputDto.aktørId(), inputDto.pleietrengendeAktørId());
+        if (!brukerdialogRestTjenesteEnabled) {
+            throw new IllegalStateException("BrukerdialogRestTjeneste er ikke aktivert");
+        }
+        return brukerdialogTjeneste.harGyldigOmsorgsdagerVedtak(inputDto.pleietrengendeAktørId());
     }
 }
 
