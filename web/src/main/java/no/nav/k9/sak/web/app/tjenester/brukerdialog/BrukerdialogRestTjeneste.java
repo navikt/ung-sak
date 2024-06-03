c3c16a20379042ecfb689aa884d5e84e1f2056dc
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2024-06-03 11:49:48 +0200

    Toggler BrukerdialogRestTjeneste av i prod.

--- web/src/main/java/no/nav/k9/sak/web/app/tjenester/brukerdialog/BrukerdialogRestTjeneste.java
+++ web/src/main/java/no/nav/k9/sak/web/app/tjenester/brukerdialog/BrukerdialogRestTjeneste.java
@@ -10,6 +10,7 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import no.nav.k9.felles.konfigurasjon.konfig.KonfigVerdi;
 import no.nav.k9.felles.sikkerhet.abac.BeskyttetRessurs;
 import no.nav.k9.felles.sikkerhet.abac.BeskyttetRessursActionAttributt;
 
@@ -24,16 +25,19 @@ public class BrukerdialogRestTjeneste {
     private static final String JSON_UTF8 = "application/json; charset=UTF-8";
 
     private BrukerdialogTjeneste brukerdialogTjeneste;
+    private boolean brukerdialogRestTjenesteEnabled;
 
     public BrukerdialogRestTjeneste() {// For Rest-CDI
     }
 
     @Inject
     public BrukerdialogRestTjeneste(
-        BrukerdialogTjeneste brukerdialogTjeneste
+        BrukerdialogTjeneste brukerdialogTjeneste,
+        @KonfigVerdi(value = "BRUKERDIALOG_REST_TJENESTE_ENABLED", required = false, defaultVerdi = "false") boolean brukerdialogRestTjenesteEnabled
 
     ) {
         this.brukerdialogTjeneste = brukerdialogTjeneste;
+        this.brukerdialogRestTjenesteEnabled = brukerdialogRestTjenesteEnabled;
     }
 
     @POST
@@ -46,6 +50,9 @@ public class BrukerdialogRestTjeneste {
     public HarGyldigOmsorgsdagerVedtakDto hentSisteGyldigeVedtakForAktorId(
         @Valid HentSisteGyldigeVedtakForAktørInputDto inputDto
     ) {
+        if (!brukerdialogRestTjenesteEnabled) {
+            throw new IllegalStateException("BrukerdialogRestTjeneste er ikke aktivert");
+        }
         return brukerdialogTjeneste.harGyldigOmsorgsdagerVedtak(inputDto.aktørId(), inputDto.pleietrengendeAktørId());
     }
 }
