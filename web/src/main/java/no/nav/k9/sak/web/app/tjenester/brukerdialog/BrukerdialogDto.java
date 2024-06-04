2a688a57bf5c53bfcf11b0452754882a79c27c9c
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2024-06-04 14:05:46 +0200

    JsomIgnore felt.

--- web/src/main/java/no/nav/k9/sak/web/app/tjenester/brukerdialog/BrukerdialogDto.java
+++ web/src/main/java/no/nav/k9/sak/web/app/tjenester/brukerdialog/BrukerdialogDto.java
@@ -1,11 +1,12 @@
 package no.nav.k9.sak.web.app.tjenester.brukerdialog;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import no.nav.k9.sak.web.app.tjenester.brukerdialog.policy.PolicyEvaluation;
 
 import java.util.Objects;
 
 public class BrukerdialogDto {
-    private final PolicyEvaluation evaluation;
+    @JsonIgnore private final PolicyEvaluation evaluation;
 
     public BrukerdialogDto(PolicyEvaluation policyEvaluation) {
         Objects.requireNonNull(policyEvaluation, "policyEvaluation");
