ba0f3b3dabca9984797a516bc3ab5f4d7e4375ff
Espen Velsvik <espenjv@users.noreply.github.com>
2024-06-10 09:06:33 +0200

    Fjerner ikkje vilkårsperioder der kun helg/kant-i-kant periode er avslått, bruker definerende vilkår til å finne aktuelle perioder for behandlingen til initiering av perioder (#10742)

--- ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/beregningsgrunnlag/OMPFinnPerioderSomSkalFjernesIBeregning.java
+++ ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/beregningsgrunnlag/OMPFinnPerioderSomSkalFjernesIBeregning.java
@@ -1,6 +1,8 @@
 package no.nav.k9.sak.ytelse.omsorgspenger.beregningsgrunnlag;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Any;
+import jakarta.enterprise.inject.Instance;
 import jakarta.inject.Inject;
 import no.nav.k9.aarskvantum.kontrakter.FullUttaksplan;
 import no.nav.k9.aarskvantum.kontrakter.Utfall;
@@ -8,11 +10,14 @@ import no.nav.k9.kodeverk.behandling.FagsakYtelseType;
 import no.nav.k9.kodeverk.vilkår.VilkårType;
 import no.nav.k9.sak.behandling.BehandlingReferanse;
 import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
+import no.nav.k9.sak.behandlingslager.behandling.vilkår.KantIKantVurderer;
 import no.nav.k9.sak.behandlingslager.behandling.vilkår.VilkårBuilder;
 import no.nav.k9.sak.behandlingslager.behandling.vilkår.Vilkårene;
 import no.nav.k9.sak.behandlingslager.behandling.vilkår.periode.VilkårPeriode;
 import no.nav.k9.sak.domene.behandling.steg.beregningsgrunnlag.FinnPerioderSomSkalFjernesIBeregning;
 import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
+import no.nav.k9.sak.perioder.VilkårsPerioderTilVurderingTjeneste;
+import no.nav.k9.sak.vilkår.FinnPerioderMedAvslåtteInngangsvilkårForBeregning;
 import no.nav.k9.sak.ytelse.omsorgspenger.årskvantum.tjenester.ÅrskvantumTjeneste;
 
 import java.util.Set;
@@ -27,13 +32,17 @@ public class OMPFinnPerioderSomSkalFjernesIBeregning implements FinnPerioderSomS
     );
 
     private ÅrskvantumTjeneste årskvantumTjeneste;
+    private Instance<VilkårsPerioderTilVurderingTjeneste> vilkårsPerioderTilVurderingTjeneste;
 
     public OMPFinnPerioderSomSkalFjernesIBeregning() {}
 
     @Inject
     public OMPFinnPerioderSomSkalFjernesIBeregning(
-        ÅrskvantumTjeneste årskvantumTjeneste
+        ÅrskvantumTjeneste årskvantumTjeneste,
+        @Any Instance<VilkårsPerioderTilVurderingTjeneste> vilkårsPerioderTilVurderingTjeneste
+
     ) {
+        this.vilkårsPerioderTilVurderingTjeneste = vilkårsPerioderTilVurderingTjeneste;
         this.årskvantumTjeneste = årskvantumTjeneste;
     }
 
@@ -41,7 +50,7 @@ public class OMPFinnPerioderSomSkalFjernesIBeregning implements FinnPerioderSomS
     public Set<DatoIntervallEntitet> finnPerioderSomSkalFjernes(Vilkårene vilkårene, BehandlingReferanse behandlingReferanse) {
         var uttaksplan = årskvantumTjeneste.hentFullUttaksplan(behandlingReferanse.getSaksnummer());
         var perioderMedAvslåtteUttaksvilkår = finnPerioderMedAvslåtteUttaksvilkår(uttaksplan);
-        var perioderMedAvslåtteBeregningsvilkår = finnPerioderMedAvslåtteBeregningsvilkår(vilkårene);
+        var perioderMedAvslåtteBeregningsvilkår = FinnPerioderMedAvslåtteInngangsvilkårForBeregning.finnPerioderMedAvslåtteInngangsvilkår(vilkårene, finnKantIKantVurderer(behandlingReferanse));
         // Fjern perioder som overlapper med perioder som har avslåtte inngangsvilkår eller er avslått i uttak
         return vilkårene
             .getVilkår(VilkårType.BEREGNINGSGRUNNLAGVILKÅR).orElseThrow(() -> new IllegalStateException("Hadde ikke beregningsGrunnlagvilkår"))
@@ -52,6 +61,10 @@ public class OMPFinnPerioderSomSkalFjernesIBeregning implements FinnPerioderSomS
                 perioderMedAvslåtteBeregningsvilkår.stream().anyMatch(periode::overlapper) || perioderMedAvslåtteUttaksvilkår.stream().anyMatch(periode::overlapper)
             )
             .collect(Collectors.toSet());
+    }
+
+    private KantIKantVurderer finnKantIKantVurderer(BehandlingReferanse behandlingReferanse) {
+        return VilkårsPerioderTilVurderingTjeneste.finnTjeneste(vilkårsPerioderTilVurderingTjeneste, behandlingReferanse.getFagsakYtelseType(), behandlingReferanse.getBehandlingType()).getKantIKantVurderer();
     }
 
     private static Set<DatoIntervallEntitet> finnPerioderMedAvslåtteUttaksvilkår(FullUttaksplan uttaksplan) {
