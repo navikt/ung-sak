7f9f6063200754e7eb5ce89dc978e1efac89faf2
Espen Velsvik <espenjv@users.noreply.github.com>
2024-06-27 10:35:32 +0200

    TSFF-614 Kun dagpenger før stp skal tas med i vurderingen (#10806)

--- ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/beregnytelse/OmsorgspengerBeregnFeriepenger.java
+++ ytelse-omsorgspenger/src/main/java/no/nav/k9/sak/ytelse/omsorgspenger/beregnytelse/OmsorgspengerBeregnFeriepenger.java
@@ -2,15 +2,22 @@ package no.nav.k9.sak.ytelse.omsorgspenger.beregnytelse;
 
 import static no.nav.k9.kodeverk.behandling.FagsakYtelseType.OMSORGSPENGER;
 
+import java.time.LocalDate;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import no.nav.fpsak.tidsserie.LocalDateTimeline;
+import no.nav.k9.kodeverk.vilkår.VilkårType;
 import no.nav.k9.sak.behandling.BehandlingReferanse;
 import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
 import no.nav.k9.sak.behandlingslager.behandling.beregning.BeregningsresultatEntitet;
+import no.nav.k9.sak.behandlingslager.behandling.vilkår.Vilkår;
+import no.nav.k9.sak.behandlingslager.behandling.vilkår.periode.VilkårPeriode;
+import no.nav.k9.sak.vilkår.VilkårTjeneste;
 import no.nav.k9.sak.ytelse.beregning.BeregnFeriepengerTjeneste;
 import no.nav.k9.sak.ytelse.beregning.FeriepengeBeregner;
 import no.nav.k9.sak.ytelse.beregning.adapter.MapBeregningsresultatFeriepengerFraVLTilRegel;
@@ -30,13 +37,15 @@ public class OmsorgspengerBeregnFeriepenger implements BeregnFeriepengerTjeneste
     private static final boolean FERIEOPPTJENING_HELG = true;
     private static final boolean UBEGRENSET_DAGER_VED_REFUSJON = true;
     private FeriepengeBeregner feriepengeBeregner;
+    private VilkårTjeneste vilkårTjeneste;
 
     public OmsorgspengerBeregnFeriepenger() {
     }
 
     @Inject
-    public OmsorgspengerBeregnFeriepenger(FeriepengeBeregner feriepengeBeregner) {
+    public OmsorgspengerBeregnFeriepenger(FeriepengeBeregner feriepengeBeregner, VilkårTjeneste vilkårTjeneste) {
         this.feriepengeBeregner = feriepengeBeregner;
+        this.vilkårTjeneste = vilkårTjeneste;
     }
 
     @Override
@@ -48,7 +57,8 @@ public class OmsorgspengerBeregnFeriepenger implements BeregnFeriepengerTjeneste
         BeregningsresultatFeriepengerRegelModell regelModell = MapBeregningsresultatFeriepengerFraVLTilRegel.mapFra(beregningsresultat,
             påvirkendeSaker, infotrygdFeriepengegrunnlag,
             ANTALL_DAGER_FERIPENGER, FERIEOPPTJENING_HELG, UBEGRENSET_DAGER_VED_REFUSJON,
-            Collections.emptyList()); // Bruker mottar aldri omsorgspenger for dagpenger
+            Collections.emptyList(),
+            finnSkjæringstidspunkter(ref)); // Bruker mottar aldri omsorgspenger for dagpenger
         feriepengeBeregner.beregnFeriepenger(beregningsresultat, regelModell);
     }
 
@@ -62,8 +72,19 @@ public class OmsorgspengerBeregnFeriepenger implements BeregnFeriepengerTjeneste
             påvirkendeSaker,
             infotrygdFeriepengegrunnlag,
             ANTALL_DAGER_FERIPENGER, FERIEOPPTJENING_HELG, UBEGRENSET_DAGER_VED_REFUSJON,
-            Collections.emptyList()); // Bruker mottar aldri omsorgspenger for dagpenger
+            Collections.emptyList(),
+            finnSkjæringstidspunkter(ref)); // Bruker mottar aldri omsorgspenger for dagpenger
         return feriepengeBeregner.beregnFeriepengerOppsummering(regelModell);
+    }
+
+    private Set<LocalDate> finnSkjæringstidspunkter(BehandlingReferanse behandling) {
+        var vilkårene = vilkårTjeneste.hentVilkårResultat(behandling.getBehandlingId());
+        var bgVilkåret = vilkårene.getVilkår(VilkårType.BEREGNINGSGRUNNLAGVILKÅR);
+        return bgVilkåret.map(Vilkår::getPerioder)
+            .stream()
+            .flatMap(Collection::stream)
+            .map(VilkårPeriode::getFom)
+            .collect(Collectors.toSet());
     }
 
 
