5bf1df34bf19f4229176422c2ea07820637d5697
Morten Osvik <morten.osvik@bekk.no>
2024-07-02 10:06:55 +0200

    Ny task for å kjøre revurderingsmetrikker manuelt

---
+++ domenetjenester/datavarehus/src/main/java/no/nav/k9/sak/metrikker/RevurderingMetrikkManuellTask.java
@@ -0,0 +1,63 @@
+package no.nav.k9.sak.metrikker;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import no.nav.k9.felles.integrasjon.sensu.SensuEvent;
+import no.nav.k9.felles.integrasjon.sensu.SensuKlient;
+import no.nav.k9.prosesstask.api.ProsessTask;
+import no.nav.k9.prosesstask.api.ProsessTaskData;
+import no.nav.k9.prosesstask.api.ProsessTaskHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+@ApplicationScoped
+@ProsessTask(value = RevurderingMetrikkManuellTask.TASKTYPE)
+public class RevurderingMetrikkManuellTask implements ProsessTaskHandler {
+
+    private static final int CHUNK_EVENT_SIZE = 1000;
+
+    private static final int LOG_THRESHOLD = 5000;
+
+    static final String TASKTYPE = "revurdering.metrikk.manuell.task";
+
+    private static final Logger log = LoggerFactory.getLogger(RevurderingMetrikkManuellTask.class);
+
+    private SensuKlient sensuKlient;
+
+    private RevurderingMetrikkRepository revurderingMetrikkRepository;
+
+    RevurderingMetrikkManuellTask() {
+        // for proxyd
+    }
+
+    @Inject
+    public RevurderingMetrikkManuellTask(SensuKlient sensuKlient, RevurderingMetrikkRepository revurderingMetrikkRepository) {
+        this.sensuKlient = sensuKlient;
+        this.revurderingMetrikkRepository = revurderingMetrikkRepository;
+    }
+
+    @Override
+    public void doTask(ProsessTaskData data) {
+        var revurderingUtenSoknadTom = data.getPropertyValue("revurderingUtenSoknadTom") != null ? LocalDate.parse(data.getPropertyValue("revurderingUtenSoknadTom")) : LocalDate.now();
+        var okrMetrikker = revurderingMetrikkRepository.hentAlle(revurderingUtenSoknadTom);
+        logMetrics(okrMetrikker);
+        if (okrMetrikker.size() > LOG_THRESHOLD) {
+            log.info("Generert {} okr-metrikker til sensu", okrMetrikker.size());
+        }
+
+
+    }
+
+    private void logMetrics(List<SensuEvent> events) {
+        var counter = new AtomicInteger();
+        var chunkSize = CHUNK_EVENT_SIZE;
+        Map<Integer, List<SensuEvent>> chunked = events.stream().collect(Collectors.groupingBy(it -> counter.getAndIncrement() / chunkSize));
+        chunked.entrySet().forEach(e -> sensuKlient.logMetrics(e.getValue()));
+    }
+}
