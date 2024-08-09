9128d943e680933d2f9e30d028589dbe0397c0d7
oyvemb <oyvind.andre.hagen.emblemsvag@nav.no>
2024-08-09 10:21:40 +0200

    Forbedring av løsning for korrigering av utenlandsopphold

--- ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/repo/uttak/UtenlandsoppholdTidslinjeTjeneste.java
+++ ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/repo/uttak/UtenlandsoppholdTidslinjeTjeneste.java
@@ -7,8 +7,12 @@ import no.nav.k9.sak.perioder.KravDokument;
 import no.nav.k9.sak.perioder.VurdertSøktPeriode;
 import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.søknadsperiode.Søknadsperiode;
 import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.uttak.delt.UtledetUtenlandsopphold;
+import no.nav.k9.søknad.felles.Kildesystem;
 
-import java.util.*;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class UtenlandsoppholdTidslinjeTjeneste {
@@ -16,38 +20,38 @@ public class UtenlandsoppholdTidslinjeTjeneste {
     public static LocalDateTimeline<UtledetUtenlandsopphold> byggTidslinje(
             Map<KravDokument, List<VurdertSøktPeriode<Søknadsperiode>>> kravDokumenter,
             Set<PerioderFraSøknad> perioderFraSøknader) {
-        var kravDokumenterSorted = kravDokumenter.keySet().stream().sorted(KravDokument::compareTo).collect(Collectors.toCollection(LinkedHashSet::new));
-        var resultatTimeline = new LocalDateTimeline<UtledetUtenlandsopphold>(List.of());
+        Set<KravDokument> kravDokumenterSorted = kravDokumenter.keySet().stream().sorted(KravDokument::compareTo).collect(Collectors.toCollection(LinkedHashSet::new));
+        LocalDateTimeline<UtledetUtenlandsopphold> resultatTimeline = new LocalDateTimeline<>(List.of());
+
         for (KravDokument kravDokument : kravDokumenterSorted) {
-            var dokumenter = perioderFraSøknader.stream()
+            Set<PerioderFraSøknad> perioderFraSøknaderForKravdokument = perioderFraSøknader.stream()
                 .filter(it -> it.getJournalpostId().equals(kravDokument.getJournalpostId()))
                 .collect(Collectors.toSet());
-            if (dokumenter.size() == 1) {
-                var perioderFraSøknad = dokumenter.iterator().next();
-                if (perioderFraSøknad.getUtenlandsopphold().isEmpty() && !perioderFraSøknad.getUttakPerioder().isEmpty()) {
-                    var søktPeriode = perioderFraSøknad.utledSøktPeriode();
+            if (perioderFraSøknaderForKravdokument.size() != 1) {
+                throw new IllegalStateException("Forventet ett sett perioder fra søknad per kravdokument. Fant " + perioderFraSøknaderForKravdokument.size() + " for " + kravDokument.getJournalpostId());
+            }
+            var perioderFraSøknad = perioderFraSøknaderForKravdokument.iterator().next();
+            if (perioderFraSøknad.getUtenlandsopphold().isEmpty() && kravDokument.getKildesystem() == Kildesystem.SØKNADSDIALOG) {
+                LocalDateTimeline<UtledetUtenlandsopphold> tidslinjeSomSkalSlettes =
+                    new LocalDateTimeline<>(perioderFraSøknad.getUttakPerioder().stream()
+                        .map(UttakPeriode::getPeriode)
+                        .map(periode -> new LocalDateSegment<>(periode.getFomDato(), periode.getTomDato(), (UtledetUtenlandsopphold) null))
+                        .toList());
+                resultatTimeline = resultatTimeline.disjoint(tidslinjeSomSkalSlettes);
+            } else {
+                for (UtenlandsoppholdPeriode utenlandsoppholdPeriode : perioderFraSøknad.getUtenlandsopphold()) {
                     LocalDateTimeline<UtledetUtenlandsopphold> timeline = new LocalDateTimeline<>(List.of(new LocalDateSegment<>(
-                        søktPeriode.getFomDato(),
-                        søktPeriode.getTomDato(),
-                        null
+                        utenlandsoppholdPeriode.getPeriode().getFomDato(),
+                        utenlandsoppholdPeriode.getPeriode().getTomDato(),
+                        new UtledetUtenlandsopphold(utenlandsoppholdPeriode.getLand(), utenlandsoppholdPeriode.getÅrsak())
                     )));
-                    resultatTimeline = resultatTimeline.disjoint(timeline);
-                } else {
-                    for (UtenlandsoppholdPeriode utenlandsoppholdPeriode : perioderFraSøknad.getUtenlandsopphold()) {
-                        LocalDateTimeline<UtledetUtenlandsopphold> timeline = new LocalDateTimeline<>(List.of(new LocalDateSegment<>(
-                            utenlandsoppholdPeriode.getPeriode().getFomDato(),
-                            utenlandsoppholdPeriode.getPeriode().getTomDato(),
-                            new UtledetUtenlandsopphold(utenlandsoppholdPeriode.getLand(), utenlandsoppholdPeriode.getÅrsak())
-                        )));
-                        if (utenlandsoppholdPeriode.isAktiv()) {
-                            resultatTimeline = resultatTimeline.combine(timeline, StandardCombinators::coalesceRightHandSide, LocalDateTimeline.JoinStyle.CROSS_JOIN);
-                        } else {
-                            resultatTimeline = resultatTimeline.disjoint(timeline);
-                        }
+                    if (utenlandsoppholdPeriode.isAktiv()) {
+                        resultatTimeline = resultatTimeline.combine(timeline, StandardCombinators::coalesceRightHandSide, LocalDateTimeline.JoinStyle.CROSS_JOIN);
+                    } else {
+                        //TODO Det ser ut til at denne koden er død fordi feltet "perioderSomSkalSlettes" under utenlandsopphold i søknaden ikke er i bruk. Bør finne en annen løsning for å slette perioder.
+                        resultatTimeline = resultatTimeline.disjoint(timeline);
                     }
                 }
-            } else {
-                throw new IllegalStateException("Fant " + dokumenter.size() + " for dokumentet : " + dokumenter);
             }
         }
         return resultatTimeline;
