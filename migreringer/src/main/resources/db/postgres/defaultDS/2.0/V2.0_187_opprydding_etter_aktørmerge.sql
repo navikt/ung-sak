56f9777e8177988f15602e6e1deb648bb5640a3c
Espen Johansen Velsvik <espen.velsvik@nav.no>
2024-08-28 09:17:56 +0200

    Rydder i persontabell for aktørmerge

---
+++ migreringer/src/main/resources/db/postgres/defaultDS/2.0/V2.0_187_opprydding_etter_aktørmerge.sql
@@ -0,0 +1,5 @@
+update pleietrengende_sykdom_dokument set pleietrengende_sykdom_id = 2820020 /* gammel */
+where pleietrengende_sykdom_id = 3817900; /* ny */
+
+delete from pleietrengende_sykdom where id = 3817900; /* ny pleietrengende_sykdom */
+delete from person where id = 10398490; /* ny person (gyldig aktørid) */
