9af348ec1a4dc3456caa7f707798eadc522e43da
Espen Johansen Velsvik <espen.velsvik@nav.no>
2024-09-05 10:35:24 +0200

    Legger inn mulighet for å manuelt revurdere kun nattevåk og beredskap uten å måtte opprette full manuell revurdering

--- ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/vilkår/forlengelse/PleiepengerForlengelseTjeneste.java
+++ ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/vilkår/forlengelse/PleiepengerForlengelseTjeneste.java
@@ -36,7 +36,6 @@ import no.nav.k9.sak.ytelse.pleiepengerbarn.repo.søknadsperiode.Søknadsperiode
 @ApplicationScoped
 public class PleiepengerForlengelseTjeneste implements ForlengelseTjeneste {
 
-    public static final long HASTESAK_SEPT_2024 = 1820868L;
     private SøknadsperiodeTjeneste søknadsperiodeTjeneste;
     private VilkårResultatRepository vilkårResultatRepository;
     private InntektArbeidYtelseTjeneste iayTjeneste;
@@ -64,7 +63,7 @@ public class PleiepengerForlengelseTjeneste implements ForlengelseTjeneste {
         var behandling = behandlingRepository.hentBehandling(referanse.getBehandlingId());
         var resultat = new TreeSet<DatoIntervallEntitet>();
 
-        if (behandling.erManueltOpprettet() && !behandling.getId().equals(HASTESAK_SEPT_2024)) {
+        if (behandling.erManueltOpprettet()) {
             return resultat;
         }
 
