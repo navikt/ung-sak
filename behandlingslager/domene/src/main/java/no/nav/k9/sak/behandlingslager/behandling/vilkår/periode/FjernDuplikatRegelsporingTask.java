230e5c3bd31475f9295a8746cbda86f3cd1f55d6
Espen Johansen Velsvik <espen.velsvik@nav.no>
2024-09-11 10:18:15 +0200

    FIX: Bruker kun en select for å hente ut rader for å unngå at låser går i beina på kvarandre

--- behandlingslager/domene/src/main/java/no/nav/k9/sak/behandlingslager/behandling/vilkår/periode/FjernDuplikatRegelsporingTask.java
+++ behandlingslager/domene/src/main/java/no/nav/k9/sak/behandlingslager/behandling/vilkår/periode/FjernDuplikatRegelsporingTask.java
@@ -1,5 +1,8 @@
 package no.nav.k9.sak.behandlingslager.behandling.vilkår.periode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,6 +21,8 @@ public class FjernDuplikatRegelsporingTask implements ProsessTaskHandler {
     public static final String TASKTYPE = "forvaltning.fjernduplikatregelsporing";
     public static final String ANTALL_PERIODER = "antallPerioder";
     public static final String SKAL_KJORE_REKURSIVT = "skalKjoreRekursivt";
+    public static final String OPPDATER_KUN_INPUT = "oppdaterKunInput";
+
 
     private static final Logger log = LoggerFactory.getLogger(FjernDuplikatRegelsporingTask.class);
 
@@ -38,30 +43,32 @@ public class FjernDuplikatRegelsporingTask implements ProsessTaskHandler {
     public void doTask(ProsessTaskData prosessTaskData) {
         var maksAntallPerioder = Integer.parseInt(prosessTaskData.getPropertyValue(ANTALL_PERIODER));
         var skalKjoreRekursivt = Boolean.parseBoolean(prosessTaskData.getPropertyValue(SKAL_KJORE_REKURSIVT));
+        var oppdaterKunInput = prosessTaskData.getPropertyValue(OPPDATER_KUN_INPUT) != null && Boolean.parseBoolean(prosessTaskData.getPropertyValue(OPPDATER_KUN_INPUT));
+
+        List<Long> ider = new ArrayList<>();
+        if (!oppdaterKunInput) {
+            ider = forsøkOppdaterEvalueringOgInput(maksAntallPerioder);
+
+        }
+
+        if (ider.isEmpty()) {
+            ider = forsøkOppdaterKunInput(maksAntallPerioder);
+            oppdaterKunInput = true;
+        }
+
+        if (skalKjoreRekursivt && (maksAntallPerioder == ider.size())) {
+            var nyTask = ProsessTaskData.forProsessTask(FjernDuplikatRegelsporingTask.class);
+            nyTask.setProperty(ANTALL_PERIODER, String.valueOf(maksAntallPerioder));
+            nyTask.setProperty(SKAL_KJORE_REKURSIVT, "true");
+            nyTask.setProperty(OPPDATER_KUN_INPUT, String.valueOf(oppdaterKunInput));
+
+            prosessTaskTjeneste.lagre(nyTask);
+        }
 
 
-        // Regel evaluering
-        var query = entityManager.createNativeQuery(
-            "select id from vr_vilkar_periode where regel_evaluering is not null and regel_evaluering::oid != regel_evaluering_oid limit :antall FOR UPDATE SKIP LOCKED"
-        ).setParameter("antall", maksAntallPerioder);
-        var regelEvalueringider = query.getResultList();
+    }
 
-        var unlinkQuery = entityManager.createNativeQuery(
-            "select lo_unlink(regel_evaluering::oid) " +
-                "from vr_vilkar_periode where id in (:ider)"
-        ).setParameter("ider", regelEvalueringider);
-        unlinkQuery.getResultList();
-
-        var updateQuery = entityManager.createNativeQuery(
-            "update vr_vilkar_periode " +
-                " set regel_evaluering=null " +
-                " where id in (:ider)"
-        ).setParameter("ider", regelEvalueringider);
-        updateQuery.executeUpdate();
-
-        log.info("Antall rader oppdatert for evaluering: "  + regelEvalueringider.size());
-
-        // Regel input
+    private List<Long> forsøkOppdaterKunInput(int maksAntallPerioder) {
         var regelInputQuery = entityManager.createNativeQuery(
             "select id from vr_vilkar_periode where regel_input is not null and regel_input::oid != regel_input_oid limit :antall FOR UPDATE SKIP LOCKED"
         ).setParameter("antall", maksAntallPerioder);
@@ -79,17 +86,34 @@ public class FjernDuplikatRegelsporingTask implements ProsessTaskHandler {
                 " where id in (:ider)"
         ).setParameter("ider", regelInputider);
         updateInputQuery.executeUpdate();
+        log.info("Antall rader oppdatert for input: " + regelInputider.size());
+        return regelInputider;
+    }
 
-        log.info("Antall rader oppdatert for input: "  + regelInputider.size());
+    private List<Long> forsøkOppdaterEvalueringOgInput(int maksAntallPerioder) {
+        var regelInputQuery = entityManager.createNativeQuery(
+            "select id from vr_vilkar_periode where regel_input is not null and regel_input::oid != regel_input_oid " +
+                "and regel_evaluering is not null and regel_evaluering::oid != regel_evaluering_oid limit :antall FOR UPDATE SKIP LOCKED"
+        ).setParameter("antall", maksAntallPerioder);
+        var ider = regelInputQuery.getResultList();
 
-        if (skalKjoreRekursivt && (maksAntallPerioder == regelEvalueringider.size() || maksAntallPerioder == regelInputider.size())) {
-            var nyTask = ProsessTaskData.forProsessTask(FjernDuplikatRegelsporingTask.class);
-            nyTask.setProperty(ANTALL_PERIODER, String.valueOf(maksAntallPerioder));
-            nyTask.setProperty(SKAL_KJORE_REKURSIVT, "true");
-            prosessTaskTjeneste.lagre(nyTask);
-        }
+        var unlinkQuery = entityManager.createNativeQuery(
+            "select lo_unlink(regel_input::oid), " +
+                " lo_unlink(regel_evaluering::oid) " +
+                "from vr_vilkar_periode where id in (:ider)"
+        ).setParameter("ider", ider);
+        unlinkQuery.getResultList();
 
+        var updateInputQuery = entityManager.createNativeQuery(
+            "update vr_vilkar_periode " +
+                " set regel_input=null, " +
+                " regeL_evaluering=null " +
+                " where id in (:ider)"
+        ).setParameter("ider", ider);
+        updateInputQuery.executeUpdate();
 
+        log.info("Antall rader oppdatert for input og evaluering: " + ider.size());
+        return ider;
     }
 
 }
