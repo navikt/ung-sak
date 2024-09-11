c8684c6bc54f599ed1fd0ecb46cba90bdf365335
oyvemb <107673448+oyvemb@users.noreply.github.com>
2024-09-11 14:52:46 +0200

    Reservert saksnummer aktør sikring (#10990)
    * Reservert saksnummer aktør sikring
    
    * k9-sak.openapi.json updated by build pipeline 
    
    
    skip-checks:true
    
    * fikser og tester
    
    * k9-sak.openapi.json updated by build pipeline 
    
    
    skip-checks:true
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ behandlingslager/domene/src/main/java/no/nav/k9/sak/behandlingslager/saksnummer/ReservertSaksnummerAktørEntitet.java
@@ -0,0 +1,63 @@
+package no.nav.k9.sak.behandlingslager.saksnummer;
+
+import java.util.Objects;
+
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.AttributeOverrides;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import no.nav.k9.sak.behandlingslager.BaseEntitet;
+import no.nav.k9.sak.typer.AktørId;
+
+@Entity(name = "ReservertSaksnummerAktør")
+@Table(name = "RESERVERT_SAKSNUMMER_AKTOR")
+public class ReservertSaksnummerAktørEntitet extends BaseEntitet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_RESERVERT_SAKSNUMMER_AKTOR")
+    @Column(name = "id")
+    private Long id;
+
+    @Embedded
+    @AttributeOverrides(@AttributeOverride(name = "aktørId", column = @Column(name = "aktoer_id", nullable = false, updatable = false)))
+    private AktørId aktørId;
+
+    ReservertSaksnummerAktørEntitet() {
+        // for hibernate
+    }
+
+    ReservertSaksnummerAktørEntitet(AktørId aktørId) {
+        this.aktørId = Objects.requireNonNull(aktørId);
+    }
+
+    public AktørId getAktørId() {
+        return aktørId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ReservertSaksnummerAktørEntitet entitet = (ReservertSaksnummerAktørEntitet) o;
+        return Objects.equals(aktørId, entitet.aktørId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(aktørId);
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "<" + "id=" + id + '>';
+    }
+}
--- kontrakt/src/main/java/no/nav/k9/sak/kontrakt/mottak/ReserverSaksnummerDto.java
+++ kontrakt/src/main/java/no/nav/k9/sak/kontrakt/mottak/ReserverSaksnummerDto.java
@@ -1,5 +1,6 @@
 package no.nav.k9.sak.kontrakt.mottak;
 
+import java.util.List;
 import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
@@ -11,6 +12,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import no.nav.k9.abac.AbacAttributt;
 import no.nav.k9.kodeverk.behandling.FagsakYtelseType;
 
@@ -40,17 +42,25 @@ public class ReserverSaksnummerDto {
     @Digits(integer = 4, fraction = 0)
     private String behandlingsår;
 
+    //Barn eller fosterbarn for OMP_MA og OMP_UT
+    @JsonProperty(value = "barnAktørIder")
+    @Size(max = 100)
+    @Valid
+    private List<String> barnAktørIder;
+
     @JsonCreator
     public ReserverSaksnummerDto(@JsonProperty(value = "ytelseType") FagsakYtelseType ytelseType,
                                  @JsonProperty(value = "brukerAktørId", required = true) @NotNull String brukerAktørId,
                                  @JsonProperty(value = "pleietrengendeAktørId") String pleietrengendeAktørId,
                                  @JsonProperty(value = "relatertPersonAktørId") String relatertPersonAktørId,
-                                 @JsonProperty(value = "behandlingsår") String behandlingsår) {
+                                 @JsonProperty(value = "behandlingsår") String behandlingsår,
+                                 @JsonProperty(value = "barn") List<String> barnAktørIder) {
         this.ytelseType = ytelseType != null ? ytelseType : FagsakYtelseType.UDEFINERT;
         this.brukerAktørId = Objects.requireNonNull(brukerAktørId, "aktørId");
         this.pleietrengendeAktørId = pleietrengendeAktørId;
         this.relatertPersonAktørId = relatertPersonAktørId;
         this.behandlingsår = behandlingsår;
+        this.barnAktørIder = barnAktørIder;
     }
 
     public FagsakYtelseType getYtelseType() {
@@ -72,5 +82,9 @@ public class ReserverSaksnummerDto {
 
     public String getBehandlingsår() {
         return behandlingsår;
+    }
+
+    public List<String> getBarnAktørIder() {
+        return barnAktørIder;
     }
 }
