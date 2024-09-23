4219a70bf8fa0088c3c1b36d9217d98b08625285
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2024-09-23 12:50:43 +0200

    Bytter til nais-opentelemetry og lanserer  (#10980)
    * La på manglende annotering på LosRestTjeneste
    
    * Bytter til nais-opentelemetry og lanserer i prod
    
    * k9-sak.openapi.json updated by build pipeline 
    
    
    skip-checks:true
    
    * Revert "La på manglende annotering på LosRestTjeneste"
    
    This reverts commit 743c6c3019150f8eac91675cca10739ddc56952b.
    
    * k9-sak.openapi.json updated by build pipeline 
    
    
    skip-checks:true
    
    * k9-sak.openapi.json updated by build pipeline 
    
    
    skip-checks:true
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- build/init-app.sh
+++ build/init-app.sh
@@ -12,10 +12,3 @@ then
     export LDAP_USERNAME="$(cat /var/run/secrets/nais.io/ldap/username)"
     export LDAP_PASSWORD="$(cat /var/run/secrets/nais.io/ldap/password)"
 fi
-
-#start opentelemetyr-agenten dersom konfigurert
-set +u
-if [ ! -z "${OTEL_EXPORTER_OTLP_ENDPOINT}" ]; then
-    echo "Configuring attatchment of opentelemetry-agent"
-    JAVA_OPTS="${JAVA_OPTS} -javaagent:/app/opentelemetry-javaagent.jar"
-fi
