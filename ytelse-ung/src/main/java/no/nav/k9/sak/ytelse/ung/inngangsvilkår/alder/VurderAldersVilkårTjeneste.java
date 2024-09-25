5eaca89de76e1e69890a41df93232bf33a2d13d0
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2024-09-25 13:57:46 +0200

    Vurderer også nedre grense på aldersvilkåret på ungdomsytelse

--- ytelse-ung/src/main/java/no/nav/k9/sak/ytelse/ung/inngangsvilkår/alder/VurderAldersVilkårTjeneste.java
+++ ytelse-ung/src/main/java/no/nav/k9/sak/ytelse/ung/inngangsvilkår/alder/VurderAldersVilkårTjeneste.java
@@ -1,51 +1,50 @@
 package no.nav.k9.sak.ytelse.ung.inngangsvilkår.alder;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.temporal.TemporalAdjusters;
 import java.util.NavigableSet;
 
+import no.nav.fpsak.tidsserie.LocalDateInterval;
+import no.nav.fpsak.tidsserie.LocalDateSegment;
+import no.nav.fpsak.tidsserie.LocalDateTimeline;
 import no.nav.k9.kodeverk.vilkår.Avslagsårsak;
 import no.nav.k9.kodeverk.vilkår.Utfall;
 import no.nav.k9.sak.behandlingslager.behandling.vilkår.VilkårBuilder;
+import no.nav.k9.sak.behandlingslager.behandling.vilkår.periode.VilkårPeriodeBuilder;
 import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
+import no.nav.k9.sak.domene.typer.tid.TidslinjeUtil;
 
 public class VurderAldersVilkårTjeneste {
 
     public void vurderPerioder(VilkårBuilder vilkårBuilder, NavigableSet<DatoIntervallEntitet> perioderTilVurdering, LocalDate fødselsdato) {
-        var maksdato = fødselsdato.plusYears(29).with(TemporalAdjusters.lastDayOfMonth());
-        var regelInput = "{ 'fødselsdato': '" + fødselsdato + "', ', 'maksdato': '" + maksdato + "' }";
+        var førsteDagMedGodkjentAlder = fødselsdato.plusYears(18).with(TemporalAdjusters.lastDayOfMonth()).plusDays(1);
+        var sisteDagMedGodkjentAlder = fødselsdato.plusYears(29).with(TemporalAdjusters.lastDayOfMonth());
+        var regelInput = """
+            { "fødselsdato": ":fødselsdato", "førsteDagMedGodkjentAlder": ":førsteDagMedGodkjentAlder", "sisteDagMedGodkjentAlder: ":sisteDagMedGodkjentAlder"}""".stripLeading()
+            .replaceFirst(":fødselsdato", fødselsdato.toString())
+            .replaceFirst(":førsteDagMedGodkjentAlder", førsteDagMedGodkjentAlder.toString())
+            .replaceFirst(":sisteDagMedGodkjentAlder", sisteDagMedGodkjentAlder.toString());
 
-        for (DatoIntervallEntitet periode : perioderTilVurdering) {
-            vurderPeriode(vilkårBuilder, maksdato, regelInput, periode);
+        LocalDateTimeline<Boolean> tidslinjeTilVurdering = TidslinjeUtil.tilTidslinjeKomprimert(perioderTilVurdering);
+
+        LocalDateTimeline<Boolean> tidslinjeSøkerForUng = tidslinjeTilVurdering.disjoint(new LocalDateInterval(førsteDagMedGodkjentAlder, LocalDate.MAX));
+        LocalDateTimeline<Boolean> tidslinjeSøkerForGammel = tidslinjeTilVurdering.disjoint(new LocalDateInterval(LocalDate.MIN, sisteDagMedGodkjentAlder));
+        LocalDateTimeline<Boolean> tidslinjeAldersvilkårOppfylt = tidslinjeTilVurdering.disjoint(tidslinjeSøkerForUng).disjoint(tidslinjeSøkerForGammel);
+
+        for (LocalDateSegment<Boolean> segment : tidslinjeSøkerForUng.toSegments()) {
+            VilkårPeriodeBuilder builder = vilkårBuilder.hentBuilderFor(DatoIntervallEntitet.fra(segment.getLocalDateInterval()));
+            builder.medUtfall(Utfall.IKKE_OPPFYLT).medAvslagsårsak(Avslagsårsak.SØKER_UNDER_MINSTE_ALDER).medRegelInput(regelInput);
+            vilkårBuilder.leggTil(builder);
         }
-    }
-
-    private void vurderPeriode(VilkårBuilder vilkårBuilder, LocalDate sisteDagMedGodkjentAlder, String regelInput, DatoIntervallEntitet periode) {
-        var førsteDagMedForHøyAlder = sisteDagMedGodkjentAlder.plusDays(1);
-        if (periode.inkluderer(førsteDagMedForHøyAlder) && !periode.getFomDato().equals(førsteDagMedForHøyAlder)) {
-            var builder = vilkårBuilder.hentBuilderFor(DatoIntervallEntitet.fraOgMedTilOgMed(periode.getFomDato(), sisteDagMedGodkjentAlder));
-            builder.medUtfall(Utfall.OPPFYLT)
-                .medRegelInput(regelInput);
-
+        for (LocalDateSegment<Boolean> oppfyltSegment : tidslinjeAldersvilkårOppfylt.toSegments()) {
+            VilkårPeriodeBuilder builder = vilkårBuilder.hentBuilderFor(DatoIntervallEntitet.fra(oppfyltSegment.getLocalDateInterval()));
+            builder.medUtfall(Utfall.OPPFYLT).medRegelInput(regelInput);
             vilkårBuilder.leggTil(builder);
-
-            builder = vilkårBuilder.hentBuilderFor(DatoIntervallEntitet.fraOgMedTilOgMed(førsteDagMedForHøyAlder, periode.getTomDato()));
-            builder.medUtfall(Utfall.IKKE_OPPFYLT)
-                .medAvslagsårsak(Avslagsårsak.SØKER_OVER_HØYESTE_ALDER)
-                .medRegelInput(regelInput);
-
-            vilkårBuilder.leggTil(builder);
-
-        } else {
-            var builder = vilkårBuilder.hentBuilderFor(periode);
-            if (periode.getFomDato().isAfter(sisteDagMedGodkjentAlder) || periode.getFomDato().isEqual(sisteDagMedGodkjentAlder)) {
-                builder.medUtfall(Utfall.IKKE_OPPFYLT)
-                    .medAvslagsårsak(Avslagsårsak.SØKER_OVER_HØYESTE_ALDER)
-                    .medRegelInput(regelInput);
-            } else {
-                builder.medUtfall(Utfall.OPPFYLT)
-                    .medRegelInput(regelInput);
-            }
+        }
+        for (LocalDateSegment<Boolean> segment : tidslinjeSøkerForGammel.toSegments()) {
+            VilkårPeriodeBuilder builder = vilkårBuilder.hentBuilderFor(DatoIntervallEntitet.fra(segment.getLocalDateInterval()));
+            builder.medUtfall(Utfall.IKKE_OPPFYLT).medAvslagsårsak(Avslagsårsak.SØKER_OVER_HØYESTE_ALDER).medRegelInput(regelInput);
             vilkårBuilder.leggTil(builder);
         }
     }
