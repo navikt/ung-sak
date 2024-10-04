3d2b46832f20f77105573f57d2e481b33312abc2
oyvemb <oyvind.andre.hagen.emblemsvag@nav.no>
2024-10-04 08:09:45 +0200

    Håndtere ikke vurderte perioder i aldersvilkår og livets sluttfase (TSFF-861)

--- behandlingsprosess/src/main/java/no/nav/k9/sak/domene/behandling/steg/inngangsvilkår/VurderAldersvilkåretSteg.java
+++ behandlingsprosess/src/main/java/no/nav/k9/sak/domene/behandling/steg/inngangsvilkår/VurderAldersvilkåretSteg.java
@@ -2,11 +2,20 @@ package no.nav.k9.sak.domene.behandling.steg.inngangsvilkår;
 
 import static no.nav.k9.kodeverk.behandling.BehandlingStegType.ALDERSVILKÅRET;
 
+import java.time.LocalDate;
+import java.util.NavigableSet;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Any;
 import jakarta.enterprise.inject.Instance;
 import jakarta.inject.Inject;
 import no.nav.k9.kodeverk.behandling.FagsakYtelseType;
+import no.nav.k9.kodeverk.vilkår.Utfall;
 import no.nav.k9.kodeverk.vilkår.VilkårType;
 import no.nav.k9.sak.behandlingskontroll.BehandleStegResultat;
 import no.nav.k9.sak.behandlingskontroll.BehandlingSteg;
@@ -18,8 +27,10 @@ import no.nav.k9.sak.behandlingslager.behandling.Behandling;
 import no.nav.k9.sak.behandlingslager.behandling.repository.BehandlingRepository;
 import no.nav.k9.sak.behandlingslager.behandling.vilkår.VilkårResultatRepository;
 import no.nav.k9.sak.behandlingslager.behandling.vilkår.Vilkårene;
+import no.nav.k9.sak.behandlingslager.behandling.vilkår.periode.VilkårPeriode;
 import no.nav.k9.sak.domene.behandling.steg.inngangsvilkår.alder.VurderAldersVilkårTjeneste;
 import no.nav.k9.sak.domene.person.personopplysning.BasisPersonopplysningTjeneste;
+import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
 import no.nav.k9.sak.perioder.VilkårsPerioderTilVurderingTjeneste;
 
 @ApplicationScoped
@@ -29,6 +40,8 @@ import no.nav.k9.sak.perioder.VilkårsPerioderTilVurderingTjeneste;
 @FagsakYtelseTypeRef(FagsakYtelseType.PLEIEPENGER_NÆRSTÅENDE)
 @FagsakYtelseTypeRef(FagsakYtelseType.OPPLÆRINGSPENGER)
 public class VurderAldersvilkåretSteg implements BehandlingSteg {
+
+    private static final Logger log = LoggerFactory.getLogger(VurderAldersvilkåretSteg.class);
 
     private final VurderAldersVilkårTjeneste vurderAldersVilkårTjeneste = new VurderAldersVilkårTjeneste();
     private Instance<VilkårsPerioderTilVurderingTjeneste> perioderTilVurderingTjenester;
@@ -53,20 +66,26 @@ public class VurderAldersvilkåretSteg implements BehandlingSteg {
 
     @Override
     public BehandleStegResultat utførSteg(BehandlingskontrollKontekst kontekst) {
-        var behandling = behandlingRepository.hentBehandling(kontekst.getBehandlingId());
-        var vilkårene = vilkårResultatRepository.hent(kontekst.getBehandlingId());
+        final var behandling = behandlingRepository.hentBehandling(kontekst.getBehandlingId());
+        final var vilkårene = vilkårResultatRepository.hent(kontekst.getBehandlingId());
 
-        var resultatBuilder = Vilkårene.builderFraEksisterende(vilkårene);
-        var vilkårBuilder = resultatBuilder.hentBuilderFor(VilkårType.ALDERSVILKÅR);
+        final var perioderTilVurderingTjeneste = getPerioderTilVurderingTjeneste(behandling);
+        final var perioderTilVurdering = perioderTilVurderingTjeneste.utled(behandling.getId(), VilkårType.ALDERSVILKÅR);
 
-        var perioderTilVurderingTjeneste = getPerioderTilVurderingTjeneste(behandling);
+        final var personopplysningerAggregat = personopplysningTjeneste.hentGjeldendePersoninformasjonPåTidspunkt(behandling.getId(), behandling.getAktørId(), behandling.getFagsak().getPeriode().getFomDato());
+        final LocalDate fødselsdato = personopplysningerAggregat.getSøker().getFødselsdato();
 
-        var perioderTilVurdering = perioderTilVurderingTjeneste.utled(behandling.getId(), VilkårType.ALDERSVILKÅR);
-        var personopplysningerAggregat = personopplysningTjeneste.hentGjeldendePersoninformasjonPåTidspunkt(behandling.getId(), behandling.getAktørId(), behandling.getFagsak().getPeriode().getFomDato());
-        var fødselsdato = personopplysningerAggregat.getSøker().getFødselsdato();
-        vurderAldersVilkårTjeneste.vurderPerioder(vilkårBuilder, perioderTilVurdering, fødselsdato);
-        resultatBuilder.leggTil(vilkårBuilder);
-        vilkårResultatRepository.lagre(kontekst.getBehandlingId(), resultatBuilder.build());
+        Vilkårene resultat = vurderVilkår(behandling, vilkårene, perioderTilVurdering, fødselsdato);
+
+        final var perioderUtenVurdering = finnPerioderUtenVurdering(resultat);
+        if (!perioderUtenVurdering.isEmpty()) {
+            log.warn("Hadde perioder uten vurdering etter vurdering av perioder til vurdering: {}", perioderUtenVurdering);
+            if (behandling.getId() == 1828755L) { //EAWVS //TODO fjern når vi ser om det funker for denne behandlingen
+                resultat = vurderVilkår(behandling, resultat, perioderUtenVurdering, fødselsdato);
+            }
+        }
+
+        vilkårResultatRepository.lagre(kontekst.getBehandlingId(), resultat);
 
         return BehandleStegResultat.utførtUtenAksjonspunkter();
     }
@@ -74,5 +93,23 @@ public class VurderAldersvilkåretSteg implements BehandlingSteg {
     private VilkårsPerioderTilVurderingTjeneste getPerioderTilVurderingTjeneste(Behandling behandling) {
         return BehandlingTypeRef.Lookup.find(VilkårsPerioderTilVurderingTjeneste.class, perioderTilVurderingTjenester, behandling.getFagsakYtelseType(), behandling.getType())
             .orElseThrow(() -> new UnsupportedOperationException("VilkårsPerioderTilVurderingTjeneste ikke implementert for ytelse [" + behandling.getFagsakYtelseType() + "], behandlingtype [" + behandling.getType() + "]"));
+    }
+
+    private static NavigableSet<DatoIntervallEntitet> finnPerioderUtenVurdering(Vilkårene resultat) {
+        return resultat.getVilkår(VilkårType.ALDERSVILKÅR).orElseThrow().getPerioder()
+            .stream()
+            .filter(vilkårPeriode -> vilkårPeriode.getGjeldendeUtfall().equals(Utfall.IKKE_VURDERT))
+            .map(VilkårPeriode::getPeriode)
+            .collect(Collectors.toCollection(TreeSet::new));
+    }
+
+    private Vilkårene vurderVilkår(Behandling behandling, Vilkårene eksisterende, NavigableSet<DatoIntervallEntitet> perioderTilVurdering, LocalDate fødselsdato) {
+        final var resultatBuilder = Vilkårene.builderFraEksisterende(eksisterende);
+        final var vilkårBuilder = resultatBuilder.hentBuilderFor(VilkårType.ALDERSVILKÅR);
+        vurderAldersVilkårTjeneste.vurderPerioder(vilkårBuilder, perioderTilVurdering, fødselsdato);
+        resultatBuilder.leggTil(vilkårBuilder);
+        final var resultat = resultatBuilder.build();
+        vilkårResultatRepository.lagre(behandling.getId(), resultat);
+        return resultat;
     }
 }
