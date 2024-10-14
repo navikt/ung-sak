e07a75a365d5dd056a94dfd903c4c72bc7807105
Espen Velsvik <espenjv@users.noreply.github.com>
2024-10-14 08:33:43 +0200

    Fjerne kode som ikkje lenger brukes og setter aksjonspunkt for infortrygdmigrering til deprekert (#11082)

--- ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/infotrygdovergang/InfotrygdMigreringTjeneste.java
+++ ytelse-pleiepengerbarn/src/main/java/no/nav/k9/sak/ytelse/pleiepengerbarn/infotrygdovergang/InfotrygdMigreringTjeneste.java
@@ -1,43 +1,27 @@
 package no.nav.k9.sak.ytelse.pleiepengerbarn.infotrygdovergang;
 
 import static no.nav.k9.kodeverk.behandling.FagsakYtelseType.PLEIEPENGER_SYKT_BARN;
-import static no.nav.k9.sak.domene.typer.tid.AbstractLocalDateInterval.TIDENES_ENDE;
 
-import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.temporal.TemporalAdjusters;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.NavigableSet;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import no.nav.fpsak.tidsserie.LocalDateSegment;
-import no.nav.fpsak.tidsserie.LocalDateTimeline;
-import no.nav.fpsak.tidsserie.StandardCombinators;
 import no.nav.k9.kodeverk.Fagsystem;
 import no.nav.k9.kodeverk.behandling.FagsakYtelseType;
-import no.nav.k9.kodeverk.behandling.aksjonspunkt.AksjonspunktDefinisjon;
 import no.nav.k9.kodeverk.vilkår.VilkårType;
-import no.nav.k9.sak.behandling.BehandlingReferanse;
-import no.nav.k9.sak.behandlingskontroll.AksjonspunktResultat;
 import no.nav.k9.sak.behandlingskontroll.BehandlingTypeRef;
 import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
-import no.nav.k9.sak.behandlingslager.behandling.Behandling;
-import no.nav.k9.sak.behandlingslager.behandling.repository.BehandlingRepository;
 import no.nav.k9.sak.behandlingslager.behandling.vilkår.VilkårResultatRepository;
 import no.nav.k9.sak.behandlingslager.behandling.vilkår.periode.VilkårPeriode;
-import no.nav.k9.sak.behandlingslager.fagsak.Fagsak;
 import no.nav.k9.sak.behandlingslager.fagsak.FagsakRepository;
 import no.nav.k9.sak.behandlingslager.fagsak.SakInfotrygdMigrering;
 import no.nav.k9.sak.domene.arbeidsforhold.InntektArbeidYtelseTjeneste;
@@ -45,24 +29,18 @@ import no.nav.k9.sak.domene.iay.modell.AktørYtelse;
 import no.nav.k9.sak.domene.iay.modell.InntektArbeidYtelseGrunnlag;
 import no.nav.k9.sak.domene.iay.modell.YtelseFilter;
 import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
-import no.nav.k9.sak.domene.typer.tid.Hjelpetidslinjer;
 import no.nav.k9.sak.perioder.VilkårsPerioderTilVurderingTjeneste;
 import no.nav.k9.sak.typer.AktørId;
-import no.nav.k9.sak.ytelse.pleiepengerbarn.infotrygdovergang.infotrygd.InfotrygdService;
 
 @ApplicationScoped
 public class InfotrygdMigreringTjeneste {
 
     private static final Logger log = LoggerFactory.getLogger(InfotrygdMigreringTjeneste.class);
 
-    public static final String GAMMEL_ORDNING_KODE = "PB";
-    public static final int ÅR_2022 = 2022;
     private InntektArbeidYtelseTjeneste inntektArbeidYtelseTjeneste;
     private VilkårsPerioderTilVurderingTjeneste perioderTilVurderingTjeneste;
     private VilkårResultatRepository vilkårResultatRepository;
     private FagsakRepository fagsakRepository;
-    private BehandlingRepository behandlingRepository;
-    private InfotrygdService infotrygdService;
 
     public InfotrygdMigreringTjeneste() {
     }
@@ -70,94 +48,11 @@ public class InfotrygdMigreringTjeneste {
     @Inject
     public InfotrygdMigreringTjeneste(InntektArbeidYtelseTjeneste inntektArbeidYtelseTjeneste,
                                       @BehandlingTypeRef @FagsakYtelseTypeRef(PLEIEPENGER_SYKT_BARN) VilkårsPerioderTilVurderingTjeneste vilkårsPerioderTilVurderingTjeneste,
-                                      VilkårResultatRepository vilkårResultatRepository, FagsakRepository fagsakRepository,
-                                      BehandlingRepository behandlingRepository, InfotrygdService infotrygdService) {
+                                      VilkårResultatRepository vilkårResultatRepository, FagsakRepository fagsakRepository) {
         this.inntektArbeidYtelseTjeneste = inntektArbeidYtelseTjeneste;
         this.perioderTilVurderingTjeneste = vilkårsPerioderTilVurderingTjeneste;
         this.vilkårResultatRepository = vilkårResultatRepository;
         this.fagsakRepository = fagsakRepository;
-        this.behandlingRepository = behandlingRepository;
-        this.infotrygdService = infotrygdService;
-    }
-
-    /** Utleder aksjonspunkter for migrering av PSB perioder fra infotrygd.
-     *
-     * Denne delen av koden er ikke lenger i bruk i k9-sak, men beholdes inntil videre for å enklere kunne migrere opplæringspenger.
-     *
-     * Dersom det skulle være større tekniske utfordringer ved at koden blir liggende kan den fjernes
-     * @param ref Behandlingref
-     * @return aksjonspunkter knyttet til migrering fra infotrygd
-     */
-    public List<AksjonspunktResultat> utledAksjonspunkter(BehandlingReferanse ref) {
-
-        NavigableSet<DatoIntervallEntitet> perioderTilVurdering = perioderTilVurderingTjeneste.utled(ref.getBehandlingId(), VilkårType.BEREGNINGSGRUNNLAGVILKÅR);
-
-        var psbInfotrygdFilter = finnPSBInfotryd(ref.getBehandlingId(), ref.getAktørId());
-        var perioderSomManglerSøknad = finnPerioderFraInfotrygdUtenSøknad(perioderTilVurdering, psbInfotrygdFilter, ref.getBehandlingId());
-
-        var aksjonspunkter = new ArrayList<AksjonspunktResultat>();
-        if (!perioderSomManglerSøknad.isEmpty()) {
-            log.info("Fant perioder i infotrygd som mangler søknad: " + perioderSomManglerSøknad);
-            aksjonspunkter.add(AksjonspunktResultat.opprettForAksjonspunkt(AksjonspunktDefinisjon.TRENGER_SØKNAD_FOR_INFOTRYGD_PERIODE));
-        }
-
-        var grunnlagsperioderPrAktør = infotrygdService.finnGrunnlagsperioderForAndreAktører(
-            ref.getPleietrengendeAktørId(),
-            ref.getAktørId(),
-            LocalDate.now().minusYears(1),
-            Set.of("PN", GAMMEL_ORDNING_KODE));
-
-        if (!grunnlagsperioderPrAktør.isEmpty()) {
-            log.info("Fant berørte perioder i infotrygd: " + grunnlagsperioderPrAktør.values());
-        }
-
-        var ikkeSøktMedOverlappMap = grunnlagsperioderPrAktør.entrySet()
-            .stream()
-            .collect(Collectors.toMap(Map.Entry::getKey,
-                e -> finnIkkeSøktOverlapp(perioderTilVurdering, getInfotrygdPerioder(e), e.getKey())));
-
-        var harAnnenAktørMedOverlappendeInfotrygdperiode = ikkeSøktMedOverlappMap.values().stream().anyMatch(v -> !v.isEmpty());
-
-        if (harAnnenAktørMedOverlappendeInfotrygdperiode) {
-            aksjonspunkter.add(AksjonspunktResultat.opprettForAksjonspunkt(AksjonspunktDefinisjon.TRENGER_SØKNAD_FOR_INFOTRYGD_PERIODE_ANNEN_PART));
-        }
-
-        return aksjonspunkter;
-    }
-
-    private List<DatoIntervallEntitet> getInfotrygdPerioder(Map.Entry<AktørId, List<IntervallMedBehandlingstema>> e) {
-        return e.getValue().stream().map(IntervallMedBehandlingstema::intervall).collect(Collectors.toList());
-    }
-
-    private List<DatoIntervallEntitet> finnIkkeSøktOverlapp(NavigableSet<DatoIntervallEntitet> perioderTilVurdering,
-                                                            List<DatoIntervallEntitet> infotrygdperioder, AktørId annenPartAktørId) {
-        var annenPartFagsakId = fagsakRepository.hentForBruker(annenPartAktørId).stream().filter(f -> f.getYtelseType().equals(FagsakYtelseType.PSB))
-            .findFirst()
-            .map(Fagsak::getId);
-
-        var annenPartSøktePerioderStream = annenPartFagsakId.flatMap(behandlingRepository::hentSisteBehandlingForFagsakId)
-            .map(Behandling::getId)
-            .stream()
-            .flatMap(id -> perioderTilVurderingTjeneste.utledFullstendigePerioder(id).stream());
-        var annenPartSøktTidslinje = lagTidslinje(annenPartSøktePerioderStream);
-        var annenPartInfotrygdTidslinje = lagTidslinje(infotrygdperioder.stream());
-        var tilVurderingTidslinje = lagTidslinje(perioderTilVurdering.stream());
-
-        var tidslinjeMangelendePerioder = annenPartInfotrygdTidslinje.intersection(tilVurderingTidslinje)
-            .disjoint(annenPartSøktTidslinje);
-        return Hjelpetidslinjer.fjernHelger(tidslinjeMangelendePerioder)
-            .toSegments()
-            .stream()
-            .map(s -> DatoIntervallEntitet.fraOgMedTilOgMed(s.getFom(), s.getTom()))
-            .collect(Collectors.toList());
-    }
-
-
-    private LocalDateTimeline<Boolean> lagTidslinje(Stream<DatoIntervallEntitet> periodeStream) {
-        var annenPartSøktePerioderSegments = periodeStream
-            .map(p -> new LocalDateSegment<>(p.getFomDato(), p.getTomDato(), true))
-            .collect(Collectors.toList());
-        return new LocalDateTimeline<>(annenPartSøktePerioderSegments, StandardCombinators::coalesceLeftHandSide);
     }
 
     /**
@@ -283,42 +178,6 @@ public class InfotrygdMigreringTjeneste {
         return new YtelseFilter(aktørYtelse).filter(y ->
             y.getYtelseType().equals(FagsakYtelseType.PSB) &&
                 y.getKilde().equals(Fagsystem.INFOTRYGD));
-    }
-
-    private List<DatoIntervallEntitet> finnPerioderFraInfotrygdUtenSøknad(NavigableSet<DatoIntervallEntitet> perioderTilVurdering, YtelseFilter psbInfotrygdFilter, Long behandlingId) {
-        var fullstendigePerioder = perioderTilVurderingTjeneste.utledFullstendigePerioder(behandlingId);
-        if (!harPerioderTilVurderingIEllerEtterÅr(perioderTilVurdering, ÅR_2022)) {
-            return Collections.emptyList();
-        }
-        var ytelseTidslinje = new LocalDateTimeline<>(psbInfotrygdFilter.getFiltrertYtelser().stream()
-            .flatMap(y -> y.getYtelseAnvist().stream())
-            .map(ya -> new LocalDateSegment<>(ya.getAnvistFOM(), ya.getAnvistTOM(), true))
-            .collect(Collectors.toList()), StandardCombinators::coalesceLeftHandSide);
-        var søknadTidslinje = new LocalDateTimeline<>(fullstendigePerioder.stream()
-            .map(this::utvidetOverHelgSegment)
-            .collect(Collectors.toList()), StandardCombinators::coalesceLeftHandSide);
-        var tidslinjeFra2022 = new LocalDateTimeline<>(List.of(new LocalDateSegment<>(
-            LocalDate.of(2022, 1, 1),
-            TIDENES_ENDE, true)));
-
-        var ytelseIkkeSøktForTidslinje = ytelseTidslinje.intersection(tidslinjeFra2022)
-            .disjoint(søknadTidslinje);
-
-        return ytelseIkkeSøktForTidslinje.toSegments()
-            .stream().map(s -> DatoIntervallEntitet.fraOgMedTilOgMed(s.getFom(), s.getTom()))
-            .toList();
-    }
-
-    private LocalDateSegment<Boolean> utvidetOverHelgSegment(DatoIntervallEntitet s) {
-        var tomUkedag = s.getTomDato().getDayOfWeek();
-        var fomUkedag = s.getFomDato().getDayOfWeek();
-        var tom = tomUkedag.equals(DayOfWeek.FRIDAY) || tomUkedag.equals(DayOfWeek.SATURDAY) ? s.getTomDato().with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY)) : s.getTomDato();
-        var fom = fomUkedag.equals(DayOfWeek.MONDAY) ? s.getFomDato().with(TemporalAdjusters.previousOrSame(DayOfWeek.SATURDAY)) : s.getFomDato();
-        return new LocalDateSegment<>(fom, tom, true);
-    }
-
-    private boolean harPerioderTilVurderingIEllerEtterÅr(NavigableSet<DatoIntervallEntitet> perioderTilVurdering, int år) {
-        return perioderTilVurdering.stream().anyMatch(p -> p.getTomDato().getYear() >= år);
     }
 
 }
