92b6fba8c2036e0b9fe6d8f1af1695b84a64cd74
Qadeer Khan <qadeer.ahmad.khan@nav.no>
2024-10-16 22:53:47 +0200

    Ung ungdomsprogram register integrasjon (#11086)
    * integrasjon mot ung deltagelse opplyser
    * setter maks dato på vilkår ved ingen tom dato i register
    validerer register periode før lagring slik at vi ikke får overlapp - mulig dette endrer seg senere. Slår sammen kontinuerlige perioder (tar ikke hensyn til helg per nå)
    * Kopierer ikke perioder ved oppretting av nytt grunnlag, men bruker input fordi det er alle periodene som finnes i registeret. Uten dette blir det dupliserte perioder.
    
    ---------
    
    Co-authored-by: Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
    Co-authored-by: Ramin Esfandiari <ramin.esfandiari@nav.no>

--- ytelse-ung/src/main/java/no/nav/k9/sak/ytelse/ung/mottak/DokumentMottakerSøknadUng.java
+++ ytelse-ung/src/main/java/no/nav/k9/sak/ytelse/ung/mottak/DokumentMottakerSøknadUng.java
@@ -1,5 +1,11 @@
 package no.nav.k9.sak.ytelse.ung.mottak;
 
+import static no.nav.k9.kodeverk.behandling.FagsakYtelseType.UNGDOMSYTELSE;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import no.nav.k9.felles.konfigurasjon.konfig.KonfigVerdi;
@@ -10,11 +16,9 @@ import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
 import no.nav.k9.sak.behandlingslager.behandling.Behandling;
 import no.nav.k9.sak.behandlingslager.behandling.motattdokument.MottattDokument;
 import no.nav.k9.sak.behandlingslager.behandling.motattdokument.MottatteDokumentRepository;
-import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
 import no.nav.k9.sak.mottak.dokumentmottak.DokumentGruppeRef;
 import no.nav.k9.sak.mottak.dokumentmottak.Dokumentmottaker;
 import no.nav.k9.sak.mottak.dokumentmottak.SøknadParser;
-import no.nav.k9.sak.ytelse.ung.periode.UngdomsprogramPeriode;
 import no.nav.k9.sak.ytelse.ung.periode.UngdomsprogramPeriodeRepository;
 import no.nav.k9.søknad.Søknad;
 import no.nav.k9.søknad.ytelse.ung.v1.Ungdomsytelse;
@@ -63,28 +67,9 @@ public class DokumentMottakerSøknadUng implements Dokumentmottaker {
             }
             mottatteDokumentRepository.lagre(dokument, DokumentStatus.BEHANDLER);
 
-
-            lagreSøknadsperioderSomUngdomsprogram(søknad, behandlingId);
-
         }
     }
 
-    /** Lagrer søknadsperioder fra søknad som gyldige perioder med ungdomsprogram
-     * @param søknad Søknad
-     * @param behandlingId BehandlingId
-     */
-    // TODO: Ungdomsprogramperioder skal innhentes fra riktig kilde når dette er klart, men for testing lagrer vi ned perioder fra søknaden enn så lenge
-    private void lagreSøknadsperioderSomUngdomsprogram(Søknad søknad, Long behandlingId) {
-        Ungdomsytelse ytelse = søknad.getYtelse();
-        var søknadsperiode = ytelse.getSøknadsperiode();
-
-        LocalDate fraOgMed = søknadsperiode.getFraOgMed();
-        LocalDate tilOgMed = søknadsperiode.getTilOgMed();
-        if (tilOgMed == null) {
-            tilOgMed = fraOgMed.plusDays(260);
-        }
-        ungdomsprogramPeriodeRepository.lagre(behandlingId, List.of(new UngdomsprogramPeriode(DatoIntervallEntitet.fraOgMedTilOgMed(fraOgMed, tilOgMed))));
-    }
 
     @Override
     public BehandlingÅrsakType getBehandlingÅrsakType(Brevkode brevkode) {
