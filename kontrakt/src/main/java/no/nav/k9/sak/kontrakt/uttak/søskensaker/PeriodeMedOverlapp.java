fd0c7cf64df0ca7658d39b5522efe92038f16512
Espen Johansen Velsvik <espen.velsvik@nav.no>
2024-10-16 12:49:57 +0200

    Legger til validering

--- kontrakt/src/main/java/no/nav/k9/sak/kontrakt/uttak/søskensaker/PeriodeMedOverlapp.java
+++ kontrakt/src/main/java/no/nav/k9/sak/kontrakt/uttak/søskensaker/PeriodeMedOverlapp.java
@@ -9,6 +9,9 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import no.nav.k9.sak.typer.Periode;
@@ -18,8 +21,20 @@ import no.nav.k9.sak.typer.Saksnummer;
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 @JsonAutoDetect(getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE, fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public record PeriodeMedOverlapp(
-    @Valid @JsonProperty(value = "periode") @NotNull Periode periode,
-    @Valid @JsonProperty(value = "fastsattUttaksgrad") @NotNull BigDecimal fastsattUttaksgrad,
-    @Valid @JsonProperty(value = "saksnummer") @NotNull @Size(min = 1, max = 10) Set<Saksnummer> saksnummer
+        @Valid
+        @JsonProperty(value = "periode")
+        @NotNull
+        Periode periode,
+        @Valid
+        @JsonProperty(value = "fastsattUttaksgrad")
+        @NotNull @DecimalMin("0")
+        @DecimalMax("100")
+        @Digits(integer = 3, fraction = 2)
+        BigDecimal fastsattUttaksgrad,
+        @Valid
+        @JsonProperty(value = "saksnummer")
+        @NotNull
+        @Size(min = 1, max = 10)
+        Set<Saksnummer> saksnummer
 ) {
 }
