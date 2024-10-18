b284d3f92c3967208e1a8723f008a7a4e65afc1d
Espen Johansen Velsvik <espen.velsvik@nav.no>
2024-10-18 14:14:26 +0200

    Reutleder aksjonspunkt for alle saker som står i AP 9291

---
+++ migreringer/src/main/resources/db/postgres/defaultDS/2.0/V2.0_198__reutled_ap_9291.sql
@@ -0,0 +1,14 @@
+-- Lar saker som står i aksjonspunkt 9291 kjøre dette steget på nytt for å reutlede aksjonspunkt
+insert into prosess_task (id, task_type, task_gruppe, neste_kjoering_etter, task_parametere)
+select nextval('seq_prosess_task'), 'behandlingskontroll.tilbakeTilStart',
+       nextval('seq_prosess_task_gruppe'), null,
+       'fagsakId=' || b.fagsak_id || '
+                behandlingId=' || b.id || '
+                startSteg=VURDER_STARTDATO_UTTAKSREGLER'
+from fagsak f
+
+                                                                                     inner join behandling b on f.id = b.fagsak_id
+                                                                                     inner join aksjonspunkt a on b.id = a.behandling_id
+where a.aksjonspunkt_def = '9291'
+  and a.aksjonspunkt_status = 'OPPR'
+  and a.opprettet_tid < '2024-10-18 16:02:00.000';
