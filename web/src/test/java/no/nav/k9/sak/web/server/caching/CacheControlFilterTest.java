459b25e16b7fa7587c52325136efb2f37e19b865
Jostein Stuhaug <jostein.stuhaug@nav.no>
2024-10-24 13:04:25 +0200

    Legger til @Cache-Control annotasjon og implementasjon av den for rest endepunkt.
    Bakgrunn:
    For å få generert bra openapi spesifikasjon ønsker vi å returnere DTO typer direkte frå rest endepunkt metoder, istadenfor eit generelt Response objekt.
    
    Når ein returnere DTO direkte har ein ikkje mulighet til å direkte sette headers på respons.
    
    Løysing:
    Legger her til støtte for at ein kan annotere rest endepunkt metoden med @Cache-Control annotasjon for å få satt Cache-Control header på respons automatisk.

---
+++ web/src/test/java/no/nav/k9/sak/web/server/caching/CacheControlFilterTest.java
@@ -0,0 +1,46 @@
+package no.nav.k9.sak.web.server.caching;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.core.MultivaluedHashMap;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class CacheControlFilterTest {
+
+    @Test
+    public void filter_setter_response_header() throws IOException {
+        final ContainerRequestContext req = mock(ContainerRequestContext.class);
+        when(req.getMethod()).thenReturn("GET");
+        final ContainerResponseContext res = mock(ContainerResponseContext.class);
+        final var responseHeaders = new MultivaluedHashMap<String, Object>();
+        when(res.getHeaders()).thenReturn(responseHeaders);
+        final CacheControlFilter ccf = new CacheControlFilter(
+            333,
+            false,
+            true,
+            true,
+            true,
+            false,
+            444,
+           555
+        );
+
+        ccf.filter(req, res);
+        final var cacheControlObjList = responseHeaders.get("Cache-Control");
+        assertThat(cacheControlObjList).hasSize(1);
+        final var cacheControlObj = cacheControlObjList.getFirst();
+        assertThat(cacheControlObj).isInstanceOf(ExtendedCacheControl.class);
+        final ExtendedCacheControl ecc = (ExtendedCacheControl) cacheControlObj;
+        assertThat(ecc.getMaxAge()).isEqualTo(333);
+        assertThat(ecc.isNoStore()).isFalse();
+        assertThat(ecc.isPrivate()).isTrue();
+        assertThat(ecc.getStaleWhileRevalidate()).isEqualTo(444);
+        assertThat(ecc.getStaleIfError()).isEqualTo(555);
+    }
+}
