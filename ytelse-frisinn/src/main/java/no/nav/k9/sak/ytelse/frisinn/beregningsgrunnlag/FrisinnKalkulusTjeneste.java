579152d42b9645fcc60da7eb53d2bd79b662b337
Espen Johansen Velsvik <espen.velsvik@nav.no>
2024-10-25 13:51:39 +0200

    Flytte ut i egen mapper

--- ytelse-frisinn/src/main/java/no/nav/k9/sak/ytelse/frisinn/beregningsgrunnlag/FrisinnKalkulusTjeneste.java
+++ ytelse-frisinn/src/main/java/no/nav/k9/sak/ytelse/frisinn/beregningsgrunnlag/FrisinnKalkulusTjeneste.java
@@ -77,6 +77,7 @@ public class FrisinnKalkulusTjeneste extends KalkulusTjeneste {
         var ytelseGrunnlagMapper = getYtelsesspesifikkMapper(FRISINN);
 
         for (var input : sortertInput) {
+            var iayGrunnlag = iayTjeneste.hentGrunnlag(ref.getBehandlingId());
             var ytelseGrunnlag = ytelseGrunnlagMapper.lagYtelsespesifiktGrunnlag(ref, input.getVilkårsperiode(), iayGrunnlag);
             var bgReferanse = input.getBgReferanse();
             FrisinnGrunnlag frisinnGrunnlag = (FrisinnGrunnlag) ytelseGrunnlag;
@@ -85,7 +86,6 @@ public class FrisinnKalkulusTjeneste extends KalkulusTjeneste {
                 uuidKalkulusResulat.put(bgReferanse, new KalkulusResultat(Collections.emptyList()).medAvslåttVilkår(Avslagsårsak.INGEN_STØNADSDAGER_I_SØKNADSPERIODEN));
             } else {
                 // tar en og en
-                var iayGrunnlag = iayTjeneste.hentGrunnlag(ref.getBehandlingId());
                 var startBeregningRequest = beregnRequestTjeneste.lagMedInput(stegType, ref, beregnInput, iayGrunnlag, Collections.emptyList());
                 var inputPerRef = startBeregningRequest.getBeregnForListe();
                 if (inputPerRef.size() != 1) {
