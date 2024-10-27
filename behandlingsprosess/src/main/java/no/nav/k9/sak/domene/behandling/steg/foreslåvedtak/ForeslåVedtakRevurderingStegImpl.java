6d3a96459e5728d833a69fb57ad0deacd66d76fc
Espen Johansen Velsvik <espen.velsvik@nav.no>
2024-10-27 16:24:04 +0100

    Revert "Validerer at behandling har minst en periode (#11141)"
    This reverts commit 754487882223b27a5b22d6feae9711f3ba7b6842.

--- behandlingsprosess/src/main/java/no/nav/k9/sak/domene/behandling/steg/foreslåvedtak/ForeslåVedtakRevurderingStegImpl.java
+++ behandlingsprosess/src/main/java/no/nav/k9/sak/domene/behandling/steg/foreslåvedtak/ForeslåVedtakRevurderingStegImpl.java
@@ -3,12 +3,9 @@ package no.nav.k9.sak.domene.behandling.steg.foreslåvedtak;
 import static no.nav.k9.kodeverk.behandling.BehandlingStegType.FORESLÅ_VEDTAK;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.NavigableSet;
-import java.util.Set;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
 
@@ -16,11 +13,9 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Any;
 import jakarta.enterprise.inject.Instance;
 import jakarta.inject.Inject;
-import no.nav.fpsak.tidsserie.LocalDateTimeline;
 import no.nav.k9.kodeverk.behandling.BehandlingStegType;
 import no.nav.k9.kodeverk.behandling.BehandlingType;
 import no.nav.k9.kodeverk.behandling.aksjonspunkt.AksjonspunktDefinisjon;
-import no.nav.k9.kodeverk.vilkår.Utfall;
 import no.nav.k9.kodeverk.vilkår.VilkårType;
 import no.nav.k9.sak.behandling.BehandlingReferanse;
 import no.nav.k9.sak.behandlingskontroll.AksjonspunktResultat;
@@ -37,9 +32,6 @@ import no.nav.k9.sak.behandlingslager.behandling.vilkår.Vilkår;
 import no.nav.k9.sak.behandlingslager.behandling.vilkår.VilkårResultatRepository;
 import no.nav.k9.sak.behandlingslager.behandling.vilkår.periode.VilkårPeriode;
 import no.nav.k9.sak.dokument.bestill.tjenester.FormidlingDokumentdataTjeneste;
-import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
-import no.nav.k9.sak.domene.typer.tid.TidslinjeUtil;
-import no.nav.k9.sak.perioder.VilkårsPerioderTilVurderingTjeneste;
 
 @BehandlingStegRef(value = FORESLÅ_VEDTAK)
 @BehandlingTypeRef(BehandlingType.REVURDERING) //Revurdering
@@ -52,7 +44,6 @@ public class ForeslåVedtakRevurderingStegImpl implements ForeslåVedtakSteg {
     private VilkårResultatRepository vilkårResultatRepository;
     private Instance<ErEndringIBeregningVurderer> endringIBeregningTjenester;
     private FormidlingDokumentdataTjeneste formidlingDokumentdataTjeneste;
-    private Instance<VilkårsPerioderTilVurderingTjeneste> vilkårsPerioderTilVurderingTjeneste;
 
     ForeslåVedtakRevurderingStegImpl() {
     }
@@ -61,14 +52,12 @@ public class ForeslåVedtakRevurderingStegImpl implements ForeslåVedtakSteg {
     ForeslåVedtakRevurderingStegImpl(ForeslåVedtakTjeneste foreslåVedtakTjeneste,
                                      BehandlingRepositoryProvider repositoryProvider,
                                      @Any Instance<ErEndringIBeregningVurderer> endringIBeregningTjenester,
-                                     FormidlingDokumentdataTjeneste formidlingDokumentdataTjeneste,
-                                     @Any Instance<VilkårsPerioderTilVurderingTjeneste> vilkårsPerioderTilVurderingTjeneste) {
+                                     FormidlingDokumentdataTjeneste formidlingDokumentdataTjeneste) {
         this.behandlingRepository = repositoryProvider.getBehandlingRepository();
         this.vilkårResultatRepository = repositoryProvider.getVilkårResultatRepository();
         this.foreslåVedtakTjeneste = foreslåVedtakTjeneste;
         this.endringIBeregningTjenester = endringIBeregningTjenester;
         this.formidlingDokumentdataTjeneste = formidlingDokumentdataTjeneste;
-        this.vilkårsPerioderTilVurderingTjeneste = vilkårsPerioderTilVurderingTjeneste;
     }
 
     @Override
@@ -78,8 +67,6 @@ public class ForeslåVedtakRevurderingStegImpl implements ForeslåVedtakSteg {
         var revurderingRef = BehandlingReferanse.fra(revurdering);
         var originalRef = BehandlingReferanse.fra(orginalBehandling);
         BehandleStegResultat behandleStegResultat = foreslåVedtakTjeneste.foreslåVedtak(revurdering, kontekst);
-
-        validerHarVilkårsperioder(revurdering);
 
         //Oppretter aksjonspunkt dersom revurdering har mindre beregningsgrunnlag enn orginal
         NavigableSet<LocalDate> skjæringstidspunkter = vilkårResultatRepository.hent(revurdering.getId())
@@ -103,59 +90,6 @@ public class ForeslåVedtakRevurderingStegImpl implements ForeslåVedtakSteg {
         }
 
         return behandleStegResultat;
-    }
-
-    private void validerHarVilkårsperioder(Behandling behandling) {
-        var perioderTilVurderingTjeneste = VilkårsPerioderTilVurderingTjeneste.finnTjeneste(vilkårsPerioderTilVurderingTjeneste, behandling.getFagsakYtelseType(), behandling.getType());
-
-        var definerendeVilkår = perioderTilVurderingTjeneste.definerendeVilkår();
-
-        List<VilkårPeriode> vilkårPerioder = new ArrayList<>();
-        for (var v : definerendeVilkår) {
-            vilkårPerioder.addAll(vilkårResultatRepository.hent(behandling.getId())
-                .getVilkår(v)
-                .map(Vilkår::getPerioder)
-                .orElse(Collections.emptyList()));
-        }
-        if (vilkårPerioder.isEmpty()) {
-            throw new IllegalStateException("Fant ingen vilkårsperiode for definerende vilkår");
-        }
-        var oppfyltePerioder = vilkårPerioder.stream().filter(p -> p.getUtfall().equals(Utfall.OPPFYLT)).map(VilkårPeriode::getPeriode).toList();
-        var rådataForVilkårsperioder = perioderTilVurderingTjeneste.utledRådataTilUtledningAvVilkårsperioder(behandling.getId());
-        rådataForVilkårsperioder.entrySet()
-            .forEach(e -> validerHarAllePerioder(behandling, e.getKey(), finnForventedePerioderForVurdering(e, definerendeVilkår, oppfyltePerioder)));
-
-    }
-
-    private static NavigableSet<DatoIntervallEntitet> finnForventedePerioderForVurdering(Map.Entry<VilkårType, NavigableSet<DatoIntervallEntitet>> e, Set<VilkårType> definerendeVilkår, List<DatoIntervallEntitet> oppfyltePerioder) {
-        if (erDefinerendeVilkår(e, definerendeVilkår)) {
-            return e.getValue();
-        }
-        var rådataTidslinje = TidslinjeUtil.tilTidslinjeKomprimertMedMuligOverlapp(e.getValue());
-        var oppfyltePerioderTidslinje = TidslinjeUtil.tilTidslinjeKomprimert(oppfyltePerioder);
-        var tidslinjeSomSkulleHaBlittVurdert = rådataTidslinje.intersection(oppfyltePerioderTidslinje);
-        return TidslinjeUtil.tilDatoIntervallEntiteter(tidslinjeSomSkulleHaBlittVurdert);
-    }
-
-    private void validerHarAllePerioder(Behandling behandling, VilkårType type, NavigableSet<DatoIntervallEntitet> forventetPerioder) {
-        var vilkårsperioderTidslinje = vilkårResultatRepository.hent(behandling.getId())
-            .getVilkår(type)
-            .stream()
-            .flatMap(v -> v.getPerioder().stream())
-                .map(p -> new LocalDateTimeline<>(p.getFom(), p.getTom(), true))
-                    .reduce(LocalDateTimeline::crossJoin)
-                        .orElse(LocalDateTimeline.empty());
-
-        var forventetPeriodeTidslinje = TidslinjeUtil.tilTidslinjeKomprimertMedMuligOverlapp(forventetPerioder);
-
-        if (!forventetPeriodeTidslinje.disjoint(vilkårsperioderTidslinje).isEmpty()) {
-            throw new IllegalStateException("Det finnes forventede perioder som ikke finnes i vilkårsresultat");
-
-        }
-    }
-
-    private static boolean erDefinerendeVilkår(Map.Entry<VilkårType, NavigableSet<DatoIntervallEntitet>> e, Set<VilkårType> definerendeVilkår) {
-        return definerendeVilkår.contains(e.getKey());
     }
 
     private Behandling getOriginalBehandling(Behandling behandling) {
--- behandlingsprosess/src/main/java/no/nav/k9/sak/domene/behandling/steg/foreslåvedtak/ForeslåVedtakStegImpl.java
+++ behandlingsprosess/src/main/java/no/nav/k9/sak/domene/behandling/steg/foreslåvedtak/ForeslåVedtakStegImpl.java
@@ -2,22 +2,13 @@ package no.nav.k9.sak.domene.behandling.steg.foreslåvedtak;
 
 import static no.nav.k9.kodeverk.behandling.BehandlingStegType.FORESLÅ_VEDTAK;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.NavigableSet;
 import java.util.Optional;
-import java.util.Set;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Any;
 import jakarta.enterprise.inject.Instance;
 import jakarta.inject.Inject;
-import no.nav.fpsak.tidsserie.LocalDateTimeline;
 import no.nav.k9.kodeverk.behandling.BehandlingStegType;
-import no.nav.k9.kodeverk.vilkår.Utfall;
-import no.nav.k9.kodeverk.vilkår.VilkårType;
 import no.nav.k9.sak.behandling.BehandlingReferanse;
 import no.nav.k9.sak.behandlingskontroll.BehandleStegResultat;
 import no.nav.k9.sak.behandlingskontroll.BehandlingStegModell;
@@ -27,13 +18,7 @@ import no.nav.k9.sak.behandlingskontroll.BehandlingskontrollKontekst;
 import no.nav.k9.sak.behandlingskontroll.FagsakYtelseTypeRef;
 import no.nav.k9.sak.behandlingslager.behandling.Behandling;
 import no.nav.k9.sak.behandlingslager.behandling.repository.BehandlingRepository;
-import no.nav.k9.sak.behandlingslager.behandling.vilkår.Vilkår;
-import no.nav.k9.sak.behandlingslager.behandling.vilkår.VilkårResultatRepository;
-import no.nav.k9.sak.behandlingslager.behandling.vilkår.periode.VilkårPeriode;
 import no.nav.k9.sak.dokument.bestill.tjenester.FormidlingDokumentdataTjeneste;
-import no.nav.k9.sak.domene.typer.tid.DatoIntervallEntitet;
-import no.nav.k9.sak.domene.typer.tid.TidslinjeUtil;
-import no.nav.k9.sak.perioder.VilkårsPerioderTilVurderingTjeneste;
 
 @BehandlingStegRef(value = FORESLÅ_VEDTAK)
 @BehandlingTypeRef
@@ -45,8 +30,6 @@ public class ForeslåVedtakStegImpl implements ForeslåVedtakSteg {
     private ForeslåVedtakTjeneste foreslåVedtakTjeneste;
     private FormidlingDokumentdataTjeneste formidlingDokumentdataTjeneste;
     private Instance<YtelsespesifikkForeslåVedtak> ytelsespesifikkForeslåVedtak;
-    private Instance<VilkårsPerioderTilVurderingTjeneste> vilkårsPerioderTilVurderingTjeneste;
-    private VilkårResultatRepository vilkårResultatRepository;
 
     ForeslåVedtakStegImpl() {
         // for CDI proxy
@@ -54,23 +37,18 @@ public class ForeslåVedtakStegImpl implements ForeslåVedtakSteg {
 
     @Inject
     ForeslåVedtakStegImpl(BehandlingRepository behandlingRepository,
-                          ForeslåVedtakTjeneste foreslåVedtakTjeneste,
-                          FormidlingDokumentdataTjeneste formidlingDokumentdataTjeneste,
-                          @Any Instance<YtelsespesifikkForeslåVedtak> ytelsespesifikkForeslåVedtak,
-                          @Any Instance<VilkårsPerioderTilVurderingTjeneste> vilkårsPerioderTilVurderingTjeneste, VilkårResultatRepository vilkårResultatRepository) {
+            ForeslåVedtakTjeneste foreslåVedtakTjeneste,
+            FormidlingDokumentdataTjeneste formidlingDokumentdataTjeneste,
+            @Any Instance<YtelsespesifikkForeslåVedtak> ytelsespesifikkForeslåVedtak) {
         this.behandlingRepository = behandlingRepository;
         this.foreslåVedtakTjeneste = foreslåVedtakTjeneste;
         this.formidlingDokumentdataTjeneste = formidlingDokumentdataTjeneste;
         this.ytelsespesifikkForeslåVedtak = ytelsespesifikkForeslåVedtak;
-        this.vilkårsPerioderTilVurderingTjeneste = vilkårsPerioderTilVurderingTjeneste;
-        this.vilkårResultatRepository = vilkårResultatRepository;
     }
 
     @Override
     public BehandleStegResultat utførSteg(BehandlingskontrollKontekst kontekst) {
         Behandling behandling = behandlingRepository.hentBehandling(kontekst.getBehandlingId());
-
-        validerHarVilkårsperioder(behandling);
 
         final Optional<BehandleStegResultat> ytelsespesifikkForeslåVedtakResultat = hentAlternativForeslåVedtak(behandling)
             .map(afv -> afv.run(BehandlingReferanse.fra(behandling)));
@@ -80,60 +58,6 @@ public class ForeslåVedtakStegImpl implements ForeslåVedtakSteg {
 
         return foreslåVedtakTjeneste.foreslåVedtak(behandling, kontekst);
     }
-
-    private void validerHarVilkårsperioder(Behandling behandling) {
-        var perioderTilVurderingTjeneste = VilkårsPerioderTilVurderingTjeneste.finnTjeneste(vilkårsPerioderTilVurderingTjeneste, behandling.getFagsakYtelseType(), behandling.getType());
-
-        var definerendeVilkår = perioderTilVurderingTjeneste.definerendeVilkår();
-
-        List<VilkårPeriode> vilkårPerioder = new ArrayList<>();
-        for (var v : definerendeVilkår) {
-            vilkårPerioder.addAll(vilkårResultatRepository.hent(behandling.getId())
-                .getVilkår(v)
-                .map(Vilkår::getPerioder)
-                .orElse(Collections.emptyList()));
-        }
-        if (vilkårPerioder.isEmpty()) {
-            throw new IllegalStateException("Fant ingen vilkårsperiode for definerende vilkår");
-        }
-        var oppfyltePerioder = vilkårPerioder.stream().filter(p -> p.getUtfall().equals(Utfall.OPPFYLT)).map(VilkårPeriode::getPeriode).toList();
-        var rådataForVilkårsperioder = perioderTilVurderingTjeneste.utledRådataTilUtledningAvVilkårsperioder(behandling.getId());
-        rådataForVilkårsperioder.entrySet()
-            .forEach(e -> validerHarAllePerioder(behandling, e.getKey(), finnForventedePerioderForVurdering(e, definerendeVilkår, oppfyltePerioder)));
-
-    }
-
-    private static NavigableSet<DatoIntervallEntitet> finnForventedePerioderForVurdering(Map.Entry<VilkårType, NavigableSet<DatoIntervallEntitet>> e, Set<VilkårType> definerendeVilkår, List<DatoIntervallEntitet> oppfyltePerioder) {
-        if (erDefinerendeVilkår(e, definerendeVilkår)) {
-            return e.getValue();
-        }
-        var rådataTidslinje = TidslinjeUtil.tilTidslinjeKomprimertMedMuligOverlapp(e.getValue());
-        var oppfyltePerioderTidslinje = TidslinjeUtil.tilTidslinjeKomprimert(oppfyltePerioder);
-        var tidslinjeSomSkulleHaBlittVurdert = rådataTidslinje.intersection(oppfyltePerioderTidslinje);
-        return TidslinjeUtil.tilDatoIntervallEntiteter(tidslinjeSomSkulleHaBlittVurdert);
-    }
-
-    private void validerHarAllePerioder(Behandling behandling, VilkårType type, NavigableSet<DatoIntervallEntitet> forventetPerioder) {
-        var vilkårsperioderTidslinje = vilkårResultatRepository.hent(behandling.getId())
-            .getVilkår(type)
-            .stream()
-            .flatMap(v -> v.getPerioder().stream())
-            .map(p -> new LocalDateTimeline<>(p.getFom(), p.getTom(), true))
-            .reduce(LocalDateTimeline::crossJoin)
-            .orElse(LocalDateTimeline.empty());
-
-        var forventetPeriodeTidslinje = TidslinjeUtil.tilTidslinjeKomprimertMedMuligOverlapp(forventetPerioder);
-
-        if (!forventetPeriodeTidslinje.disjoint(vilkårsperioderTidslinje).isEmpty()) {
-            throw new IllegalStateException("Det finnes forventede perioder som ikke finnes i vilkårsresultat");
-
-        }
-    }
-
-    private static boolean erDefinerendeVilkår(Map.Entry<VilkårType, NavigableSet<DatoIntervallEntitet>> e, Set<VilkårType> definerendeVilkår) {
-        return definerendeVilkår.contains(e.getKey());
-    }
-
 
     private Optional<? extends YtelsespesifikkForeslåVedtak> hentAlternativForeslåVedtak(Behandling behandling) {
         return FagsakYtelseTypeRef.Lookup.find(ytelsespesifikkForeslåVedtak, behandling.getFagsakYtelseType());
