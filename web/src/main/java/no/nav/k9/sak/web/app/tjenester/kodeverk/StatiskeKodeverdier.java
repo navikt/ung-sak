59de8a128b6d97b84aca3a71c3f35c80594f6946
Jostein Stuhaug <135307852+josstn@users.noreply.github.com>
2024-10-28 11:08:08 +0100

    Nytt endepunkt for å hente alle kodeverdier (#11145)
    * Fjern (abakus) VirksomhetType frå "kodeverdier som brukast på klient" liste.
    
    Bakgrunn:
    Den ser ikkje ut til å vere brukt i frontend, og returnerte ikkje objekt, berre kode string verdier, så kunne igrunn ikkje vere nyttig å ha i responsen.
    
    * Nytt endepunkt for retur av alle Kodeverdi objekt til frontend som objekt.
    
    Erstatter gammalt endepunkt med meir typesikker løysing som genererer betre openapi definisjon, og gjere det mulig å etterkvart rydde meir i gammal serialiseringskode på server.
    
    Deprekerer gammalt endepunkt som no er implementert basert på nytt endepunkt.
    
    * La til manglande BeskyttetRessurs annotasjon.
    
    * Fiks test for sjekk av påkrevde annotasjoner.
    
    Slik at den fungerer med generiske typer.
    
    * Legg til påkrevde valideringsannotasjoner på AlleKodeverdierSomObjektResponse klasse.
    
    Slik at test blir fornøgd.
    
    * k9-sak.openapi.json updated by build pipeline 
    
    
    skip-checks:true
    
    * Legger til @Cache-Control annotasjon og implementasjon av den for rest endepunkt.
    
    Bakgrunn:
    For å få generert bra openapi spesifikasjon ønsker vi å returnere DTO typer direkte frå rest endepunkt metoder, istadenfor eit generelt Response objekt.
    
    Når ein returnere DTO direkte har ein ikkje mulighet til å direkte sette headers på respons.
    
    Løysing:
    Legger her til støtte for at ein kan annotere rest endepunkt metoden med @Cache-Control annotasjon for å få satt Cache-Control header på respons automatisk.
    
    * Ta i bruk ny @Cache-Control header.
    
    * Rydd vekk gammal unødvendig kode. Fornya test av KodeverkRestTjeneste.
    
    Erstatta med ny kode for å returnere alle kodeverdier.
    
    * Fjern serialisering med overstyring til objekt for Kodeverdi.
    
    Trengs ikkje lenger.
    
    * Fjern getEkstraFelter() frå Kodeverdi interface.
    
    Trengs ikkje lenger.
    
    * Litt meir fornuftig valideringsannotasjon på AlleKodeverdierSomObjektResponse
    
    * k9-sak.openapi.json updated by build pipeline 
    
    
    skip-checks:true
    
    * Fjerner midlertidig test som forsikra at omskriving ikkje førte til endring i resultat frå gammalt endepunkt.
    
    Denne ville feila neste gang nokon endra ein Kodeverdi, og bør ikkje vere nødvendig å ha lenger.
    
    * Legger til test på at hentGruppertKodeliste endepunkt framleis leverer rett resultat basert på nytt endepunkt.
    
    * Fjerna ubrukt testressurs.
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ web/src/main/java/no/nav/k9/sak/web/app/tjenester/kodeverk/StatiskeKodeverdier.java
@@ -0,0 +1,142 @@
+package no.nav.k9.sak.web.app.tjenester.kodeverk;
+
+import jakarta.validation.constraints.NotNull;
+import no.nav.k9.kodeverk.Fagsystem;
+import no.nav.k9.kodeverk.api.Kodeverdi;
+import no.nav.k9.kodeverk.arbeidsforhold.*;
+import no.nav.k9.kodeverk.behandling.*;
+import no.nav.k9.kodeverk.behandling.aksjonspunkt.SkjermlenkeType;
+import no.nav.k9.kodeverk.behandling.aksjonspunkt.Venteårsak;
+import no.nav.k9.kodeverk.behandling.aksjonspunkt.VurderÅrsak;
+import no.nav.k9.kodeverk.beregningsgrunnlag.FaktaOmBeregningTilfelle;
+import no.nav.k9.kodeverk.dokument.DokumentTypeId;
+import no.nav.k9.kodeverk.geografisk.Landkoder;
+import no.nav.k9.kodeverk.geografisk.Region;
+import no.nav.k9.kodeverk.geografisk.Språkkode;
+import no.nav.k9.kodeverk.historikk.*;
+import no.nav.k9.kodeverk.medlem.MedlemskapDekningType;
+import no.nav.k9.kodeverk.medlem.MedlemskapManuellVurderingType;
+import no.nav.k9.kodeverk.medlem.MedlemskapType;
+import no.nav.k9.kodeverk.opptjening.OpptjeningAktivitetType;
+import no.nav.k9.kodeverk.person.PersonstatusType;
+import no.nav.k9.kodeverk.person.SivilstandType;
+import no.nav.k9.kodeverk.produksjonsstyring.OppgaveÅrsak;
+import no.nav.k9.kodeverk.uttak.UtenlandsoppholdÅrsak;
+import no.nav.k9.kodeverk.vedtak.VedtakResultatType;
+import no.nav.k9.kodeverk.vilkår.Avslagsårsak;
+import no.nav.k9.kodeverk.vilkår.VilkårType;
+import no.nav.k9.kodeverk.økonomi.tilbakekreving.TilbakekrevingVidereBehandling;
+import no.nav.k9.sak.kontrakt.krav.ÅrsakTilVurdering;
+
+import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public record StatiskeKodeverdier(
+    @NotNull Set<RelatertYtelseTilstand> relatertYtelseTilstander,
+    @NotNull Set<FagsakStatus> fagsakStatuser,
+    @NotNull Set<FagsakYtelseType> fagsakYtelseTyper,
+    @NotNull Set<BehandlingÅrsakType> behandlingÅrsakTyper,
+    @NotNull Set<HistorikkBegrunnelseType> historikkBegrunnelseTyper,
+    @NotNull Set<OppgaveÅrsak> oppgaveÅrsaker,
+    @NotNull Set<MedlemskapManuellVurderingType> medlemskapManuellVurderingTyper,
+    @NotNull Set<BehandlingResultatType> behandlingResultatTyper,
+    @NotNull Set<PersonstatusType> personstatusTyper,
+    @NotNull Set<Venteårsak> venteårsaker,
+    @NotNull Set<BehandlingType> behandlingTyper,
+    @NotNull Set<ArbeidType> arbeidTyper,
+    @NotNull Set<OpptjeningAktivitetType> opptjeningAktivitetTyper,
+    @NotNull Set<RevurderingVarslingÅrsak> revurderingVarslingÅrsaker,
+    @NotNull Set<Inntektskategori> inntektskategorier,
+    @NotNull Set<AktivitetStatus> aktivitetStatuser,
+    @NotNull Set<Arbeidskategori> arbeidskategorier,
+    @NotNull Set<Fagsystem> fagsystemer,
+    @NotNull Set<SivilstandType> sivilstandTyper,
+    @NotNull Set<FaktaOmBeregningTilfelle> faktaOmBeregningTilfeller,
+    @NotNull Set<SkjermlenkeType> skjermlenkeTyper,
+    @NotNull Set<ArbeidsforholdHandlingType> arbeidsforholdHandlingTyper,
+    @NotNull Set<HistorikkOpplysningType> historikkOpplysningTyper,
+    @NotNull Set<HistorikkEndretFeltType> historikkEndretFeltTyper,
+    @NotNull Set<HistorikkEndretFeltVerdiType>  historikkEndretFeltVerdiTyper,
+    @NotNull Set<HistorikkinnslagType> historikkinnslagTyper,
+    @NotNull Set<HistorikkAktør> historikkAktører,
+    @NotNull Set<HistorikkAvklartSoeknadsperiodeType> historikkAvklartSoeknadsperiodeTyper,
+    @NotNull Set<HistorikkResultatType> historikkResultatTyper,
+    @NotNull Set<BehandlingStatus> behandlingStatuser,
+    @NotNull Set<MedlemskapDekningType> medlemskapDekningTyper,
+    @NotNull Set<MedlemskapType> medlemskapTyper,
+    @NotNull Set<Avslagsårsak> avslagsårsaker,
+    @NotNull Set<KonsekvensForYtelsen> konsekvenserForYtelsen,
+    @NotNull Set<VilkårType> vilkårTyper,
+    @NotNull Set<PermisjonsbeskrivelseType> permisjonsbeskrivelseTyper,
+    @NotNull Set<VurderArbeidsforholdHistorikkinnslag> vurderArbeidsforholdHistorikkinnslag,
+    @NotNull Set<TilbakekrevingVidereBehandling> tilbakekrevingVidereBehandlinger,
+    @NotNull Set<VurderÅrsak> vurderingsÅrsaker,
+    @NotNull Set<Region> regioner,
+    @NotNull Set<Landkoder> landkoder,
+    @NotNull Set<Språkkode> språkkoder,
+    @NotNull Set<VedtakResultatType> vedtakResultatTyper,
+    @NotNull Set<DokumentTypeId> dokumentTypeIder,
+    @NotNull Set<UtenlandsoppholdÅrsak> utenlandsoppholdÅrsaker,
+    @NotNull Set<ÅrsakTilVurdering> årsakerTilVurdering
+) {
+
+    // Eigentleg ikkje så nødvendig, men signaliserer tydleg at dei fleste verdier er enums som implementerer Kodeverdi.
+    public static <K extends Enum<K> & Kodeverdi> Set<K> alleEnumVerdier(Class<K> k) {
+        return EnumSet.allOf(k);
+    }
+
+    public static final StatiskeKodeverdier alle;
+
+    static {
+        alle = new StatiskeKodeverdier(
+            alleEnumVerdier(RelatertYtelseTilstand.class),
+            alleEnumVerdier(FagsakStatus.class),
+            alleEnumVerdier(FagsakYtelseType.class),
+            alleEnumVerdier(BehandlingÅrsakType.class),
+            alleEnumVerdier(HistorikkBegrunnelseType.class),
+            alleEnumVerdier(OppgaveÅrsak.class),
+            alleEnumVerdier(MedlemskapManuellVurderingType.class).stream().filter(v -> v.visesPåKlient()).collect(Collectors.toSet()),
+            alleEnumVerdier(BehandlingResultatType.class),
+            alleEnumVerdier(PersonstatusType.class),
+            EnumSet.allOf(Venteårsak.class),
+            alleEnumVerdier(BehandlingType.class),
+            alleEnumVerdier(ArbeidType.class).stream().filter(v -> v.erAnnenOpptjening()).collect(Collectors.toSet()),
+            alleEnumVerdier(OpptjeningAktivitetType.class),
+            alleEnumVerdier(RevurderingVarslingÅrsak.class),
+            alleEnumVerdier(Inntektskategori.class),
+            alleEnumVerdier(AktivitetStatus.class),
+            alleEnumVerdier(Arbeidskategori.class),
+            alleEnumVerdier(Fagsystem.class),
+            alleEnumVerdier(SivilstandType.class),
+            alleEnumVerdier(FaktaOmBeregningTilfelle.class),
+            alleEnumVerdier(SkjermlenkeType.class),
+            alleEnumVerdier(ArbeidsforholdHandlingType.class),
+            alleEnumVerdier(HistorikkOpplysningType.class),
+            alleEnumVerdier(HistorikkEndretFeltType.class),
+            alleEnumVerdier(HistorikkEndretFeltVerdiType.class),
+            alleEnumVerdier(HistorikkinnslagType.class),
+            alleEnumVerdier(HistorikkAktør.class),
+            alleEnumVerdier(HistorikkAvklartSoeknadsperiodeType.class),
+            alleEnumVerdier(HistorikkResultatType.class),
+            alleEnumVerdier(BehandlingStatus.class),
+            alleEnumVerdier(MedlemskapDekningType.class),
+            alleEnumVerdier(MedlemskapType.class),
+            alleEnumVerdier(Avslagsårsak.class),
+            alleEnumVerdier(KonsekvensForYtelsen.class),
+            alleEnumVerdier(VilkårType.class),
+            alleEnumVerdier(PermisjonsbeskrivelseType.class),
+            alleEnumVerdier(VurderArbeidsforholdHistorikkinnslag.class),
+            alleEnumVerdier(TilbakekrevingVidereBehandling.class),
+            alleEnumVerdier(VurderÅrsak.class),
+            alleEnumVerdier(Region.class),
+            new HashSet<>(Landkoder.kodeMap().values()),
+            new HashSet<>(Språkkode.kodeMap().values()),
+            alleEnumVerdier(VedtakResultatType.class),
+            alleEnumVerdier(DokumentTypeId.class),
+            alleEnumVerdier(UtenlandsoppholdÅrsak.class),
+            alleEnumVerdier(ÅrsakTilVurdering.class)
+        );
+    }
+}
