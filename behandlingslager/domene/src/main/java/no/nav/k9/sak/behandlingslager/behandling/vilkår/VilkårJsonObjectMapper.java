8108fa78183aff195de7560452ff01334fffca65
Espen Velsvik <espenjv@users.noreply.github.com>
2024-11-05 08:55:24 +0100

    Fjerner deprekert serliserer for å håndtere rare datoer i fp-sak (#11174)

--- behandlingslager/domene/src/main/java/no/nav/k9/sak/behandlingslager/behandling/vilkår/VilkårJsonObjectMapper.java
+++ behandlingslager/domene/src/main/java/no/nav/k9/sak/behandlingslager/behandling/vilkår/VilkårJsonObjectMapper.java
@@ -5,23 +5,11 @@ import java.io.StringWriter;
 import java.io.Writer;
 import java.net.URL;
 import java.time.LocalDate;
-import java.time.Month;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
-import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+
 import no.nav.k9.sak.domene.typer.tid.JsonObjectMapper;
 
 /**
@@ -32,13 +20,6 @@ import no.nav.k9.sak.domene.typer.tid.JsonObjectMapper;
 public class VilkårJsonObjectMapper {
 
     private static final ObjectMapper OM = JsonObjectMapper.getMapper().copy();
-
-    static {
-        // Legacy support for gamle åpne behandlinger fra Fundamentet. Fjernes når disse er vedtatt og ferdig
-        SimpleModule module = new SimpleModule();
-        module.addDeserializer(LocalDate.class, new OldLocalDateDeserializer());
-        OM.registerModule(module);
-    }
 
     public void write(Object data, Writer writer) {
         try {
@@ -68,42 +49,6 @@ public class VilkårJsonObjectMapper {
         StringWriter sw = new StringWriter(512);
         write(data, sw);
         return sw.toString();
-    }
-
-    /**
-     * @deprecated tolker datao serialisert på merkelig måte i fundamentet (field dump). Kan fjernes når ikke flere åpne
-     *             behandlinger fra Fundamentet i systemet.
-     */
-    @Deprecated
-    static class OldLocalDateDeserializer extends StdDeserializer<LocalDate> {
-
-        public OldLocalDateDeserializer() {
-            this(null);
-        }
-
-        public OldLocalDateDeserializer(Class<?> vc) {
-            super(vc);
-        }
-
-        @Override
-        public LocalDate deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
-            try {
-                // korrekt håndtering av datoer
-                return LocalDateDeserializer.INSTANCE.deserialize(jsonParser, deserializationContext);
-            } catch (MismatchedInputException e) {
-
-                // Gammelt Legacy format(har ikke håndterert ikke java.time klasser ordentlig men dumper felter rett ut)
-                JsonNode json = jsonParser.getCodec().readTree(jsonParser);
-                JsonNode year = json.get("year");
-
-                if (year == null) {
-                    throw new IllegalArgumentException("Kan ikke deserialisere input hverken fra legacy format eller nytt forat", e);
-                }
-                JsonNode month = json.get("monthValue");
-                JsonNode dayOfMonth = json.get("dayOfMonth");
-                return LocalDate.of(year.asInt(), Month.of(month.intValue()), dayOfMonth.intValue());
-            }
-        }
     }
 
 }
