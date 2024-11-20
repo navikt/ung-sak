3b673f6f353e4975ed233fb7753ce805b41a1483
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2024-11-20 11:12:08 +0100

    Bruker token for henting av saksbehandlernavn og grupper istedenfor LDAP (#30)
    * WIP: Leser groups fra claim i token.
    
    * Fjern ldap
    
    * inline
    
    * Fikset testen
    
    * bruk mockbruker lokalt
    
    * Forenkler assertions i test.
    
    * Fjerner ldap kode, og grupper fra application.properties
    
    * Bumper k9-felles
    
    * Flytter allowAllUsers over claims
    
    * Kommenterer azure konfig.
    
    * legger på k9-drift
    
    ---------
    
    Co-authored-by: Tore Langedal Endestad <tore.langedal.endestad@nav.no>
    Co-authored-by: Qadeer Khan <ahm.qadeer@gmail.com>

--- web/src/test/java/no/nav/ung/sak/web/app/tjenester/saksbehandler/NavAnsattRestTjenesteTest.java
+++ web/src/test/java/no/nav/ung/sak/web/app/tjenester/saksbehandler/NavAnsattRestTjenesteTest.java
@@ -1,15 +1,90 @@
 package no.nav.ung.sak.web.app.tjenester.saksbehandler;
 
-public class NavAnsattRestTjenesteTest {
-    private static final String gruppenavnSaksbehandler = "Saksbehandler";
-    private static final String gruppenavnVeileder = "Veileder";
-    private static final String gruppenavnBeslutter = "Beslutter";
-    private static final String gruppenavnOverstyrer = "Overstyrer";
-    private static final String gruppenavnEgenAnsatt = "EgenAnsatt";
-    private static final String gruppenavnKode6 = "Kode6";
-    private static final String gruppenavnKode7 = "Kode7";
-    private static final Boolean skalViseDetaljerteFeilmeldinger = true;
-    private NavAnsattRestTjeneste saksbehandlerTjeneste;
+import no.nav.k9.felles.testutilities.sikkerhet.StaticSubjectHandler;
+import no.nav.k9.felles.testutilities.sikkerhet.SubjectHandlerUtils;
+import no.nav.k9.sikkerhet.context.domene.IdentType;
+import no.nav.k9.sikkerhet.context.domene.OidcCredential;
+import no.nav.ung.sak.kontrakt.abac.InnloggetAnsattDto;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-    // TODO: Skriv tester for NavAnsattRestTjeneste
+import javax.security.auth.Subject;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class NavAnsattRestTjenesteTest {
+
+    private static final String tokenHeader = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Inp4ZWcyV09OcFRrd041R21lWWN1VGR0QzZKMCJ9";
+
+    @InjectMocks
+    private NavAnsattRestTjeneste navAnsattRestTjeneste;
+
+    @BeforeEach
+    public void setUp() {
+        navAnsattRestTjeneste = new NavAnsattRestTjeneste(
+            "Saksbehandler",
+            "Veileder",
+            "Beslutter",
+            "Overstyrer",
+            "EgenAnsatt",
+            "Kode6",
+            "Kode7",
+            true
+        ) {
+            @Override
+            protected boolean brukMockBrukerLokalt() {
+                //gjør at vi kan teste-prod-kode isdf å treffe shortcut til mock-saksbehandler som brukes lokalt
+                return false;
+            }
+        };
+    }
+
+    @AfterEach
+    public void tearDown() {
+        SubjectHandlerUtils.reset();
+    }
+
+    @Test
+    public void testInnloggetBruker() {
+        String ident = "testIdent";
+        String navnet = "saksbehandler 1";
+        List<String> groupIds = Arrays.asList("Saksbehandler", "Kode7");
+        String token = createJWT(ident, navnet, groupIds);
+
+        Subject subject = new SubjectHandlerUtils.SubjectBuilder(ident, IdentType.InternBruker).getSubject();
+        subject.getPublicCredentials().add(new OidcCredential(token));
+        SubjectHandlerUtils.useSubjectHandler(StaticSubjectHandler.class);
+        SubjectHandlerUtils.setSubject(subject);
+
+        InnloggetAnsattDto result = navAnsattRestTjeneste.innloggetBruker();
+
+        assertEquals(ident, result.getBrukernavn());
+        assertEquals(navnet, result.getNavn());
+        assertTrue(result.getKanSaksbehandle());
+        assertFalse(result.getKanVeilede());
+        assertFalse(result.getKanBeslutte());
+        assertFalse(result.getKanOverstyre());
+        assertFalse(result.getKanBehandleKodeEgenAnsatt());
+        assertFalse(result.getKanBehandleKode6());
+        assertTrue(result.getKanBehandleKode7());
+        assertTrue(result.getSkalViseDetaljerteFeilmeldinger());
+    }
+
+    public static String createJWT(String ident, String name, List<String> groupIds) {
+        String jwtBodyContent = """
+            {"name" : ":name", "groups": [:groups],"NAVident": ":ident"}
+            """
+            .replaceAll(":ident", ident)
+            .replaceAll(":name", name)
+            .replaceAll(":groups", groupIds.stream().map(it -> '"' + it + '"').collect(Collectors.joining(",")));
+        return tokenHeader + "." + java.util.Base64.getUrlEncoder().encodeToString(jwtBodyContent.getBytes()).replaceAll("=", "") + "." + "DUMMYSIGNATURE";
+    }
 }
