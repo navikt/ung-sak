7cd5886ad7d8069755cb8dd8c19fb3014e96e943
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2024-12-04 11:27:38 +0100

    Lagrer ingen uttak ved ingen godkjente perioder.

--- ytelse-ung/src/main/java/no/nav/ung/sak/ytelse/ung/uttak/VurderAntallDagerSteg.java
+++ ytelse-ung/src/main/java/no/nav/ung/sak/ytelse/ung/uttak/VurderAntallDagerSteg.java
@@ -9,6 +9,8 @@ import no.nav.ung.sak.vilkår.VilkårTjeneste;
 import no.nav.ung.sak.ytelse.ung.beregning.UngdomsytelseGrunnlagRepository;
 import no.nav.ung.sak.ytelse.ung.periode.UngdomsprogramPeriodeTjeneste;
 
+import java.util.logging.Logger;
+
 import static no.nav.ung.kodeverk.behandling.BehandlingStegType.VURDER_ANTALL_DAGER;
 import static no.nav.ung.kodeverk.behandling.FagsakYtelseType.UNGDOMSYTELSE;
 import static no.nav.ung.sak.domene.typer.tid.AbstractLocalDateInterval.TIDENES_BEGYNNELSE;
@@ -18,6 +20,7 @@ import static no.nav.ung.sak.domene.typer.tid.AbstractLocalDateInterval.TIDENES_
 @BehandlingTypeRef
 @ApplicationScoped
 public class VurderAntallDagerSteg implements BehandlingSteg {
+    private static final Logger LOGGER = Logger.getLogger(VurderAntallDagerSteg.class.getName());
 
     private VilkårTjeneste vilkårTjeneste;
     private UngdomsytelseGrunnlagRepository ungdomsytelseGrunnlagRepository;
@@ -41,17 +44,17 @@ public class VurderAntallDagerSteg implements BehandlingSteg {
             .mapValue(it -> it.getSamletUtfall().equals(Utfall.OPPFYLT))
             .filterValue(Boolean.TRUE::equals);
 
-        var ungdomsprogramtidslinje = ungdomsprogramPeriodeTjeneste.finnPeriodeTidslinje(behandlingId);
-        if (!godkjentePerioder.isEmpty()) {
-            ungdomsprogramtidslinje = ungdomsprogramtidslinje
-                // Fjerner deler av programperiode som er etter søkte perioder.
-                .intersection(new LocalDateTimeline<>(TIDENES_BEGYNNELSE, godkjentePerioder.getMaxLocalDate(), true));
+        if (godkjentePerioder.isEmpty()) {
+            LOGGER.info("Ingen godkjente perioder funnet, ingen uttaksperioder vil bli lagret.");
+            return BehandleStegResultat.utførtUtenAksjonspunkter();
         }
+
+        var ungdomsprogramtidslinje = ungdomsprogramPeriodeTjeneste.finnPeriodeTidslinje(behandlingId)
+            // Fjerner deler av programperiode som er etter søkte perioder.
+            .intersection(new LocalDateTimeline<>(TIDENES_BEGYNNELSE, godkjentePerioder.getMaxLocalDate(), true));
 
         var ungdomsytelseUttakPerioder = VurderAntallDagerTjeneste.vurderAntallDagerOgLagUttaksperioder(godkjentePerioder, ungdomsprogramtidslinje);
         ungdomsytelseUttakPerioder.ifPresent(it -> ungdomsytelseGrunnlagRepository.lagre(behandlingId, it));
         return BehandleStegResultat.utførtUtenAksjonspunkter();
     }
-
-
 }
