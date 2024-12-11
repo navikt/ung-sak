8f349386bd2ddeef9ad85aa603ccecec465f014c
Espen Johansen Velsvik <espen.velsvik@nav.no>
2024-12-11 16:17:21 +0100

    Gir barnetillegg for alle barn som har levd i måneden før

--- ytelse-ung/src/main/java/no/nav/ung/sak/ytelse/ung/beregning/barnetillegg/LagBarnetilleggTidslinje.java
+++ ytelse-ung/src/main/java/no/nav/ung/sak/ytelse/ung/beregning/barnetillegg/LagBarnetilleggTidslinje.java
@@ -1,8 +1,9 @@
 package no.nav.ung.sak.ytelse.ung.beregning.barnetillegg;
 
-import static no.nav.ung.sak.domene.typer.tid.AbstractLocalDateInterval.TIDENES_ENDE;
+import static no.nav.fpsak.tidsserie.LocalDateInterval.TIDENES_ENDE;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.temporal.TemporalAdjusters;
 import java.util.List;
 
@@ -11,44 +12,75 @@ import jakarta.inject.Inject;
 import no.nav.fpsak.tidsserie.LocalDateSegment;
 import no.nav.fpsak.tidsserie.LocalDateTimeline;
 import no.nav.fpsak.tidsserie.StandardCombinators;
+import no.nav.ung.kodeverk.person.RelasjonsRolleType;
 import no.nav.ung.sak.behandling.BehandlingReferanse;
+import no.nav.ung.sak.behandlingslager.behandling.personopplysning.PersonRelasjonEntitet;
+import no.nav.ung.sak.behandlingslager.behandling.personopplysning.PersonopplysningRepository;
+import no.nav.ung.sak.typer.AktørId;
 
+/**
+ * Finner tidslinje for antall barn.
+ */
 @Dependent
 public class LagBarnetilleggTidslinje {
 
+
     public static final BigDecimal BARNETILLEGG_DAGSATS = BigDecimal.valueOf(36);
-    private final LagAntallBarnTidslinje lagAntallBarnTidslinje;
+
+    private final PersonopplysningRepository personopplysningRepository;
+    private final HentFødselOgDød hentFødselOgDød;
 
     @Inject
-    public LagBarnetilleggTidslinje(LagAntallBarnTidslinje lagAntallBarnTidslinje) {
-        this.lagAntallBarnTidslinje = lagAntallBarnTidslinje;
+    public LagBarnetilleggTidslinje(PersonopplysningRepository personopplysningRepository, HentFødselOgDød hentFødselOgDød) {
+        this.personopplysningRepository = personopplysningRepository;
+        this.hentFødselOgDød = hentFødselOgDød;
     }
 
     /**
-     * Utleder tidslinje for barnetillegg basert på antall barn i siste dag i måneden før
-     * Barn som fødes og dør i samme måned gir ikke barnetillegg basert på denne logikken.
+     * Utleder tidslinje for utbetaling av barnetillegg
      *
      * @param behandlingReferanse Behandlingreferanse
-     * @param perioder
-     * @return Tidslinje for barnetillegg
+     * @return Tidslinje for barnetillegg der denne overstiger 0
      */
     public BarnetilleggVurdering lagTidslinje(BehandlingReferanse behandlingReferanse, LocalDateTimeline<Boolean> perioder) {
-        var barnetilleggMellomregning = lagAntallBarnTidslinje.lagAntallBarnTidslinje(behandlingReferanse);
-        return new BarnetilleggVurdering(beregnBarnetillegg(barnetilleggMellomregning.antallBarnTidslinje()), barnetilleggMellomregning.barnFødselOgDødInfo()) ;
+        var personopplysningGrunnlagEntitet = personopplysningRepository.hentPersonopplysninger(behandlingReferanse.getBehandlingId());
+        var barnAvSøkerAktørId = personopplysningGrunnlagEntitet.getGjeldendeVersjon().getRelasjoner()
+            .stream().filter(r -> r.getRelasjonsrolle().equals(RelasjonsRolleType.BARN))
+            .map(PersonRelasjonEntitet::getTilAktørId)
+            .toList();
+
+        var relevantPersonInfoBarn = barnAvSøkerAktørId.stream()
+            .map(this::finnRelevantPersonInfo)
+            .toList();
+
+        return beregnBarnetillegg(perioder, relevantPersonInfoBarn);
+
     }
 
-    static LocalDateTimeline<Barnetillegg> beregnBarnetillegg(LocalDateTimeline<Integer> antallBarnTidslinje) {
-        List<LocalDateSegment<Integer>> antallBarnMånedsvisSegmenter = antallBarnTidslinje
-            .toSegments()
-            .stream()
-            .map(s -> new LocalDateSegment<>(s.getFom().plusMonths(1).withDayOfMonth(1), s.getFom().plusMonths(1).with(TemporalAdjusters.lastDayOfMonth()), s.getValue())) // Mapper alle segmenter til å starte første dag i neste måned
-            .toList();
-        var antallBarnForBarnetilleggTidslinje = new LocalDateTimeline<>(antallBarnMånedsvisSegmenter, StandardCombinators::max);
-        return antallBarnForBarnetilleggTidslinje.mapValue(antallBarn -> new Barnetillegg(finnUtregnetBarnetillegg(antallBarn), antallBarn));
+    static BarnetilleggVurdering beregnBarnetillegg(LocalDateTimeline<Boolean> perioder, List<FødselOgDødInfo> relevantPersonInfoBarn) {
+        var antallBarnGrunnlagTidslinje = relevantPersonInfoBarn.stream()
+            .map(info -> new LocalDateTimeline<>(info.fødselsdato().plusMonths(1).withDayOfMonth(1), getTilDato(info), 1))
+            .reduce((t1, t2) -> t1.crossJoin(t2, StandardCombinators::sum))
+            .orElse(LocalDateTimeline.empty());
+
+        var relevantBarnetilleggTidslinje = antallBarnGrunnlagTidslinje.intersection(perioder)
+            .mapValue(antallBarn -> new Barnetillegg(finnUtregnetBarnetillegg(antallBarn), antallBarn));
+
+        return new BarnetilleggVurdering(relevantBarnetilleggTidslinje, relevantPersonInfoBarn);
     }
 
     private static int finnUtregnetBarnetillegg(Integer antallBarn) {
         return BigDecimal.valueOf(antallBarn).multiply(BARNETILLEGG_DAGSATS).intValue(); // Både sats og antall barn er heltall, så vi trenger ingen avrunding. Caster direkte til long
     }
+
+
+    private static LocalDate getTilDato(FødselOgDødInfo info) {
+        return info.dødsdato() != null ? info.dødsdato().plusMonths(1).with(TemporalAdjusters.lastDayOfMonth()) : TIDENES_ENDE;
+    }
+
+    private FødselOgDødInfo finnRelevantPersonInfo(AktørId barnAktørId) {
+        return hentFødselOgDød.hentFødselOgDødInfo(barnAktørId);
+    }
+
 
 }
