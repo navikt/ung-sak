d41453d0032527f014e0338aad1468b2bc62f45c
Espen Johansen Velsvik <espen.velsvik@nav.no>
2024-12-18 07:52:21 +0100

    Fikser tester for vedtatt ytelse, fjerner test for refusjon

--- domenetjenester/vedtak/src/test/java/no/nav/ung/sak/domene/vedtak/observer/VedtattYtelseMapperTest.java
+++ domenetjenester/vedtak/src/test/java/no/nav/ung/sak/domene/vedtak/observer/VedtattYtelseMapperTest.java
@@ -33,17 +33,11 @@ class VedtattYtelseMapperTest {
             .medRegelSporing("regelsporing")
             .build();
 
-        Arbeidsgiver arbeidsgiver = Arbeidsgiver.virksomhet("123566324");
         BeregningsresultatPeriode periode = BeregningsresultatPeriode.builder()
             .medBeregningsresultatPeriodeFomOgTom(SKJÆRINGSTIDSPUNKT, SKJÆRINGSTIDSPUNKT.plusDays(10))
             .build(resultat);
-        InternArbeidsforholdRef arbeidsforholdRef = InternArbeidsforholdRef.nyRef();
-        String eksternReferanse = "jifesjsioejf";
         int dagsats = 500;
-        ArbeidsforholdReferanse arbeidsforholdReferanse = lagReferanser(arbeidsgiver, arbeidsforholdRef, eksternReferanse);
         BeregningsresultatAndel arbeidsforhold = BeregningsresultatAndel.builder()
-            .medArbeidsforholdRef(arbeidsforholdRef)
-            .medArbeidsgiver(arbeidsgiver)
             .medStillingsprosent(BigDecimal.valueOf(100))
             .medArbeidsforholdType(OpptjeningAktivitetType.ARBEID)
             .medDagsats(dagsats)
@@ -57,126 +51,11 @@ class VedtattYtelseMapperTest {
 
         assertThat(anvisninger.size()).isEqualTo(1);
         assertThat(anvisninger.get(0).getAndeler().size()).isEqualTo(1);
-        assertThat(anvisninger.get(0).getAndeler().get(0).getArbeidsgiverIdent().erOrganisasjon()).isTrue();
-        assertThat(anvisninger.get(0).getAndeler().get(0).getArbeidsgiverIdent().ident()).isEqualTo(arbeidsgiver.getIdentifikator());
-        assertThat(anvisninger.get(0).getAndeler().get(0).getArbeidsforholdId()).isEqualTo(eksternReferanse);
         assertThat(anvisninger.get(0).getAndeler().get(0).getInntektklasse()).isEqualTo(Inntektklasse.ARBEIDSTAKER);
         assertThat(anvisninger.get(0).getAndeler().get(0).getUtbetalingsgrad().getVerdi()).isEqualByComparingTo(BigDecimal.valueOf(100));
         assertThat(anvisninger.get(0).getAndeler().get(0).getDagsats().getVerdi()).isEqualByComparingTo(BigDecimal.valueOf(dagsats));
         assertThat(anvisninger.get(0).getAndeler().get(0).getRefusjonsgrad().getVerdi()).isEqualByComparingTo(BigDecimal.ZERO);
     }
-
-    @Test
-    void skal_mappe_to_arbeidsforhold_med_full_utbetaling_til_arbeidsgiver() {
-
-        BeregningsresultatEntitet resultat = BeregningsresultatEntitet.builder()
-            .medRegelInput("input")
-            .medRegelSporing("regelsporing")
-            .build();
-
-        BeregningsresultatPeriode periode = BeregningsresultatPeriode.builder()
-            .medBeregningsresultatPeriodeFomOgTom(SKJÆRINGSTIDSPUNKT, SKJÆRINGSTIDSPUNKT.plusDays(10))
-            .build(resultat);
-        InternArbeidsforholdRef arbeidsforholdRef1 = InternArbeidsforholdRef.nyRef();
-        String eksternReferanse1 = "jifesjsioejf";
-        int dagsats1 = 500;
-        Arbeidsgiver arbeidsgiver1 = Arbeidsgiver.virksomhet("123566324");
-        ArbeidsforholdReferanse arbeidsforholdReferanse1 = lagReferanser(arbeidsgiver1, arbeidsforholdRef1, eksternReferanse1);
-        fullRefusjon(arbeidsgiver1, periode, arbeidsforholdRef1, dagsats1);
-
-        InternArbeidsforholdRef arbeidsforholdRef2 = InternArbeidsforholdRef.nyRef();
-        String eksternReferanse2 = "husefiuse";
-        int dagsats2 = 200;
-        Arbeidsgiver arbeidsgiver2 = Arbeidsgiver.virksomhet("463465235");
-        ArbeidsforholdReferanse arbeidsforholdReferanse2 = lagReferanser(arbeidsgiver1, arbeidsforholdRef2, eksternReferanse2);
-        fullRefusjon(arbeidsgiver2, periode, arbeidsforholdRef2, dagsats2);
-
-        List<Anvisning> anvisninger = VedtattYtelseMapper.mapAnvisninger(resultat);
-
-        assertThat(anvisninger.size()).isEqualTo(1);
-        assertThat(anvisninger.get(0).getAndeler().size()).isEqualTo(2);
-        assertThat(anvisninger.get(0).getAndeler().get(0).getArbeidsgiverIdent().erOrganisasjon()).isTrue();
-        assertThat(anvisninger.get(0).getAndeler().get(0).getArbeidsgiverIdent().ident()).isEqualTo(arbeidsgiver1.getIdentifikator());
-        assertThat(anvisninger.get(0).getAndeler().get(0).getArbeidsforholdId()).isEqualTo(eksternReferanse1);
-        assertThat(anvisninger.get(0).getAndeler().get(0).getInntektklasse()).isEqualTo(Inntektklasse.ARBEIDSTAKER);
-        assertThat(anvisninger.get(0).getAndeler().get(0).getUtbetalingsgrad().getVerdi()).isEqualByComparingTo(BigDecimal.valueOf(100));
-        assertThat(anvisninger.get(0).getAndeler().get(0).getDagsats().getVerdi()).isEqualByComparingTo(BigDecimal.valueOf(dagsats1));
-        assertThat(anvisninger.get(0).getAndeler().get(0).getRefusjonsgrad().getVerdi()).isEqualByComparingTo(BigDecimal.valueOf(100));
-
-        assertThat(anvisninger.get(0).getAndeler().get(1).getArbeidsgiverIdent().erOrganisasjon()).isTrue();
-        assertThat(anvisninger.get(0).getAndeler().get(1).getArbeidsgiverIdent().ident()).isEqualTo(arbeidsgiver2.getIdentifikator());
-        assertThat(anvisninger.get(0).getAndeler().get(1).getArbeidsforholdId()).isEqualTo(eksternReferanse2);
-        assertThat(anvisninger.get(0).getAndeler().get(0).getInntektklasse()).isEqualTo(Inntektklasse.ARBEIDSTAKER);
-        assertThat(anvisninger.get(0).getAndeler().get(1).getUtbetalingsgrad().getVerdi()).isEqualByComparingTo(BigDecimal.valueOf(100));
-        assertThat(anvisninger.get(0).getAndeler().get(1).getDagsats().getVerdi()).isEqualByComparingTo(BigDecimal.valueOf(dagsats2));
-        assertThat(anvisninger.get(0).getAndeler().get(1).getRefusjonsgrad().getVerdi()).isEqualByComparingTo(BigDecimal.valueOf(100));
-    }
-
-    private ArbeidsforholdReferanse lagReferanser(Arbeidsgiver arbeidsgiver1, InternArbeidsforholdRef arbeidsforholdRef2, String eksternReferanse2) {
-        return new ArbeidsforholdReferanse(arbeidsgiver1, arbeidsforholdRef2, EksternArbeidsforholdRef.ref(eksternReferanse2));
-    }
-
-    @Test
-    void skal_mappe_ett_arbeidsforhold_med_full_utbetaling_til_arbeidsgiver() {
-
-        BeregningsresultatEntitet resultat = BeregningsresultatEntitet.builder()
-            .medRegelInput("input")
-            .medRegelSporing("regelsporing")
-            .build();
-
-        Arbeidsgiver arbeidsgiver = Arbeidsgiver.virksomhet("123566324");
-        BeregningsresultatPeriode periode = BeregningsresultatPeriode.builder()
-            .medBeregningsresultatPeriodeFomOgTom(SKJÆRINGSTIDSPUNKT, SKJÆRINGSTIDSPUNKT.plusDays(10))
-            .build(resultat);
-        InternArbeidsforholdRef arbeidsforholdRef = InternArbeidsforholdRef.nyRef();
-        String eksternReferanse = "jifesjsioejf";
-        int dagsats = 500;
-        ArbeidsforholdReferanse arbeidsforholdReferanse = lagReferanser(arbeidsgiver, arbeidsforholdRef, eksternReferanse);
-        fullRefusjon(arbeidsgiver, periode, arbeidsforholdRef, dagsats);
-
-        List<Anvisning> anvisninger = VedtattYtelseMapper.mapAnvisninger(resultat);
-
-        assertThat(anvisninger.size()).isEqualTo(1);
-        assertThat(anvisninger.get(0).getAndeler().size()).isEqualTo(1);
-        assertThat(anvisninger.get(0).getAndeler().get(0).getArbeidsgiverIdent().erOrganisasjon()).isTrue();
-        assertThat(anvisninger.get(0).getAndeler().get(0).getArbeidsgiverIdent().ident()).isEqualTo(arbeidsgiver.getIdentifikator());
-        assertThat(anvisninger.get(0).getAndeler().get(0).getArbeidsforholdId()).isEqualTo(eksternReferanse);
-        assertThat(anvisninger.get(0).getAndeler().get(0).getInntektklasse()).isEqualTo(Inntektklasse.ARBEIDSTAKER);
-        assertThat(anvisninger.get(0).getAndeler().get(0).getUtbetalingsgrad().getVerdi()).isEqualByComparingTo(BigDecimal.valueOf(100));
-        assertThat(anvisninger.get(0).getAndeler().get(0).getDagsats().getVerdi()).isEqualByComparingTo(BigDecimal.valueOf(dagsats));
-        assertThat(anvisninger.get(0).getAndeler().get(0).getRefusjonsgrad().getVerdi()).isEqualByComparingTo(BigDecimal.valueOf(100));
-    }
-
-
-    @Test
-    void skal_mappe_ett_arbeidsforhold_med_full_utbetaling_til_arbeidsgiver_aggregert() {
-
-        BeregningsresultatEntitet resultat = BeregningsresultatEntitet.builder()
-            .medRegelInput("input")
-            .medRegelSporing("regelsporing")
-            .build();
-
-        Arbeidsgiver arbeidsgiver = Arbeidsgiver.virksomhet("123566324");
-        BeregningsresultatPeriode periode = BeregningsresultatPeriode.builder()
-            .medBeregningsresultatPeriodeFomOgTom(SKJÆRINGSTIDSPUNKT, SKJÆRINGSTIDSPUNKT.plusDays(10))
-            .build(resultat);
-        InternArbeidsforholdRef arbeidsforholdRef = InternArbeidsforholdRef.nullRef();
-        int dagsats = 500;
-        fullRefusjon(arbeidsgiver, periode, arbeidsforholdRef, dagsats);
-
-        List<Anvisning> anvisninger = VedtattYtelseMapper.mapAnvisninger(resultat);
-
-        assertThat(anvisninger.size()).isEqualTo(1);
-        assertThat(anvisninger.get(0).getAndeler().size()).isEqualTo(1);
-        assertThat(anvisninger.get(0).getAndeler().get(0).getArbeidsgiverIdent().erOrganisasjon()).isTrue();
-        assertThat(anvisninger.get(0).getAndeler().get(0).getArbeidsgiverIdent().ident()).isEqualTo(arbeidsgiver.getIdentifikator());
-        assertThat(anvisninger.get(0).getAndeler().get(0).getArbeidsforholdId()).isEqualTo(null);
-        assertThat(anvisninger.get(0).getAndeler().get(0).getInntektklasse()).isEqualTo(Inntektklasse.ARBEIDSTAKER);
-        assertThat(anvisninger.get(0).getAndeler().get(0).getUtbetalingsgrad().getVerdi()).isEqualByComparingTo(BigDecimal.valueOf(100));
-        assertThat(anvisninger.get(0).getAndeler().get(0).getDagsats().getVerdi()).isEqualByComparingTo(BigDecimal.valueOf(dagsats));
-        assertThat(anvisninger.get(0).getAndeler().get(0).getRefusjonsgrad().getVerdi()).isEqualByComparingTo(BigDecimal.valueOf(100));
-    }
-
 
     @Test
     void skal_mappe_dagpenger() {
@@ -189,7 +68,6 @@ class VedtattYtelseMapperTest {
         BeregningsresultatPeriode periode = BeregningsresultatPeriode.builder()
             .medBeregningsresultatPeriodeFomOgTom(SKJÆRINGSTIDSPUNKT, SKJÆRINGSTIDSPUNKT.plusDays(10))
             .build(resultat);
-        InternArbeidsforholdRef arbeidsforholdRef = InternArbeidsforholdRef.nyRef();
         int dagsats = 500;
         BeregningsresultatAndel.builder()
             .medStillingsprosent(BigDecimal.valueOf(100))
@@ -259,35 +137,6 @@ class VedtattYtelseMapperTest {
         assertThat(brukers_andel.getInntektklasse()).isEqualTo(Inntektklasse.ARBEIDSTAKER_UTEN_FERIEPENGER);
         assertThat(brukers_andel.getDagsats().getVerdi()).isEqualByComparingTo(BigDecimal.ZERO);
         assertThat(brukers_andel.getRefusjonsgrad().getVerdi()).isEqualByComparingTo(BigDecimal.ZERO);
-    }
-
-
-
-
-    private void fullRefusjon(Arbeidsgiver arbeidsgiver, BeregningsresultatPeriode periode, InternArbeidsforholdRef arbeidsforholdRef, int dagsats) {
-        BeregningsresultatAndel.builder()
-            .medArbeidsforholdRef(arbeidsforholdRef)
-            .medArbeidsgiver(arbeidsgiver)
-            .medStillingsprosent(BigDecimal.valueOf(100))
-            .medArbeidsforholdType(OpptjeningAktivitetType.ARBEID)
-            .medDagsats(dagsats)
-            .medDagsatsFraBg(dagsats)
-            .medBrukerErMottaker(false)
-            .medUtbetalingsgrad(BigDecimal.valueOf(100))
-            .medInntektskategori(Inntektskategori.ARBEIDSTAKER)
-            .buildFor(periode);
-
-        BeregningsresultatAndel.builder()
-            .medArbeidsforholdRef(arbeidsforholdRef)
-            .medArbeidsgiver(arbeidsgiver)
-            .medStillingsprosent(BigDecimal.valueOf(100))
-            .medArbeidsforholdType(OpptjeningAktivitetType.ARBEID)
-            .medDagsats(0)
-            .medDagsatsFraBg(0)
-            .medBrukerErMottaker(true)
-            .medUtbetalingsgrad(BigDecimal.valueOf(100))
-            .medInntektskategori(Inntektskategori.ARBEIDSTAKER)
-            .buildFor(periode);
     }
 
 }
