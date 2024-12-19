339fe2e88e46e7d84bcf2df02004359c4b591f25
Espen Johansen Velsvik <espen.velsvik@nav.no>
2024-12-19 09:54:50 +0100

    Fjerner også constraint

--- migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_007__fjerne-pleietrengende-relatertpart-sakinfotryg-fra-fagsak.sql
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_007__fjerne-pleietrengende-relatertpart-sakinfotryg-fra-fagsak.sql
@@ -1,3 +1,4 @@
+alter table FAGSAK drop constraint unik_fagsak_2;
 alter table FAGSAK drop column pleietrengende_aktoer_id;
 alter table FAGSAK drop column relatert_person_aktoer_id;
 alter table FAGSAK drop column til_infotrygd;
