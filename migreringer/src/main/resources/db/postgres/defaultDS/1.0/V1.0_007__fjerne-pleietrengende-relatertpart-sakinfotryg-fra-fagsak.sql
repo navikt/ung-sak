ff81d74e5c647195b0c9d3d51b9966f9cb6bdb05
Espen Johansen Velsvik <espen.velsvik@nav.no>
2024-12-19 13:59:45 +0100

    Legger til constraint for bruker og fagsak

--- migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_007__fjerne-pleietrengende-relatertpart-sakinfotryg-fra-fagsak.sql
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_007__fjerne-pleietrengende-relatertpart-sakinfotryg-fra-fagsak.sql
@@ -1,4 +1,7 @@
-alter table FAGSAK drop constraint unik_fagsak_2;
-alter table FAGSAK drop column pleietrengende_aktoer_id;
-alter table FAGSAK drop column relatert_person_aktoer_id;
-alter table FAGSAK drop column til_infotrygd;
+alter table fagsak drop constraint unik_fagsak_1;
+alter table fagsak drop constraint unik_fagsak_2;
+alter table fagsak drop constraint unik_fagsak_3;
+alter table fagsak add constraint unik_en_fagsak_pr_bruker_og_periode exclude using gist (ytelse_type with =, bruker_aktoer_id with =, periode with &&) where ((periode is not null));
+alter table fagsak drop column pleietrengende_aktoer_id;
+alter table fagsak drop column relatert_person_aktoer_id;
+alter table fagsak drop column til_infotrygd;
