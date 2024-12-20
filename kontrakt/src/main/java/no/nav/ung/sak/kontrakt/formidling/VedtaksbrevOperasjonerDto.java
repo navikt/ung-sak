70f137eea230fbc3a235f757cf01a1b8d74d1335
Qadeer Khan <ahm.qadeer@gmail.com>
2024-12-20 13:40:21 +0100

    flagg for å angi om det finnes brev i det hele tatt (#89)
    * flagg for å angi om det finnes brev i det hele tatt
    
    * ung-sak.openapi.json updated by build pipeline 
    
    
    skip-checks:true
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- kontrakt/src/main/java/no/nav/ung/sak/kontrakt/formidling/VedtaksbrevOperasjonerDto.java
+++ kontrakt/src/main/java/no/nav/ung/sak/kontrakt/formidling/VedtaksbrevOperasjonerDto.java
@@ -1,10 +1,20 @@
 package no.nav.ung.sak.kontrakt.formidling;
 
+
+/**
+ *
+ * @param harBrev  true hvis det finnes brev for behandling
+ * @param enableRediger true hvis saksbehandler kan velge mellom manuell og automatisk brev
+ * @param redigert true hvis brev er redigert eller må redigeres.
+ * @param enableHindre true hvis brevet kan undertrykkes
+ * @param hindret true hvis brevet har blitt undertrykket
+ */
 public record VedtaksbrevOperasjonerDto(
-    boolean kanRedigere,
-    boolean harRedigert,
-    boolean kanHindre,
-    boolean harHindret
+    boolean harBrev,
+    boolean enableRediger,
+    boolean redigert,
+    boolean enableHindre,
+    boolean hindret
     //TODO legg ved original html for redigering
     // og html som er redigert
     // eller putt i eget endepunkt
