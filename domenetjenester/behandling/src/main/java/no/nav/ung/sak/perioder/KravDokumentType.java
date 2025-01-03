01a9f1ea5a42533697c8f600fcbf12f33675d38f
Espen Johansen Velsvik <espen.velsvik@nav.no>
2025-01-03 09:21:38 +0100

    Fjerner inntektsmelding som kravdokument og fjerner arbeidsgiverreferanser fra søkte perioder

--- domenetjenester/behandling/src/main/java/no/nav/ung/sak/perioder/KravDokumentType.java
+++ domenetjenester/behandling/src/main/java/no/nav/ung/sak/perioder/KravDokumentType.java
@@ -1,5 +1,5 @@
 package no.nav.ung.sak.perioder;
 
 public enum KravDokumentType {
-    INNTEKTSMELDING, SØKNAD, INNTEKTSMELDING_UTEN_REFUSJONSKRAV
+    SØKNAD
 }
--- domenetjenester/behandling/src/main/java/no/nav/ung/sak/perioder/SøktPeriode.java
+++ domenetjenester/behandling/src/main/java/no/nav/ung/sak/perioder/SøktPeriode.java
@@ -2,17 +2,12 @@ package no.nav.ung.sak.perioder;
 
 import java.util.Objects;
 
-import no.nav.ung.kodeverk.uttak.UttakArbeidType;
 import no.nav.ung.sak.domene.typer.tid.DatoIntervallEntitet;
-import no.nav.ung.sak.typer.Arbeidsgiver;
 import no.nav.ung.sak.typer.InternArbeidsforholdRef;
 
 public class SøktPeriode<T> {
 
     private DatoIntervallEntitet periode;
-    private UttakArbeidType type;
-    private Arbeidsgiver arbeidsgiver;
-    private InternArbeidsforholdRef arbeidsforholdRef;
     private T raw;
 
     /**
@@ -26,36 +21,12 @@ public class SøktPeriode<T> {
         this.raw = raw;
     }
 
-    public SøktPeriode(DatoIntervallEntitet periode, UttakArbeidType type, Arbeidsgiver arbeidsgiver, InternArbeidsforholdRef arbeidsforholdRef, T raw) {
-        this.periode = periode;
-        this.type = type;
-        this.arbeidsgiver = arbeidsgiver;
-        this.arbeidsforholdRef = arbeidsforholdRef;
-        this.raw = raw;
-    }
-
     public DatoIntervallEntitet getPeriode() {
         return periode;
     }
 
-    public UttakArbeidType getType() {
-        return type;
-    }
-
-    public Arbeidsgiver getArbeidsgiver() {
-        return arbeidsgiver;
-    }
-
-    public InternArbeidsforholdRef getArbeidsforholdRef() {
-        return arbeidsforholdRef;
-    }
-
     public T getRaw() {
         return raw;
-    }
-
-    public static <T> SøktPeriode<T> arbeid(DatoIntervallEntitet periode, Arbeidsgiver arbeidsgiver, InternArbeidsforholdRef arbeidsforholdRef, T raw) {
-        return new SøktPeriode<>(periode, UttakArbeidType.ARBEIDSTAKER, arbeidsgiver, arbeidsforholdRef, raw);
     }
 
     @SuppressWarnings("rawtypes")
@@ -66,24 +37,18 @@ public class SøktPeriode<T> {
         if (o == null || getClass() != o.getClass())
             return false;
         var that = (SøktPeriode) o;
-        return type == that.type &&
-            Objects.equals(arbeidsgiver, that.arbeidsgiver)
-            && Objects.equals(arbeidsforholdRef, that.arbeidsforholdRef)
-            && Objects.equals(raw, that.raw);
+        return Objects.equals(raw, that.raw);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(type, arbeidsgiver, arbeidsforholdRef, raw);
+        return Objects.hash(raw);
     }
 
     @Override
     public String toString() {
         return "SøktPeriode{" +
             "periode=" + periode +
-            ", type=" + type +
-            ", arbeidsgiver=" + arbeidsgiver +
-            ", arbeidsforholdRef=" + arbeidsforholdRef +
             '}';
     }
 }
