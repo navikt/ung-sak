97879069f722ab3b7c31e2fdc2e37975518e3aef
Qadeer Khan <qadeer.ahmad.khan@nav.no>
2025-01-06 13:47:00 +0100

    endrer port da det kolliderte med ft-inntektsmelding

--- web/src/test/java/no/nav/ung/sak/web/server/jetty/JettyDevKonfigurasjon.java
+++ web/src/test/java/no/nav/ung/sak/web/server/jetty/JettyDevKonfigurasjon.java
@@ -2,7 +2,7 @@ package no.nav.ung.sak.web.server.jetty;
 
 public class JettyDevKonfigurasjon extends JettyWebKonfigurasjon {
     private static final int SSL_SERVER_PORT = 8443;
-    private static final int DEV_SERVER_PORT = 8991;
+    private static final int DEV_SERVER_PORT = 8901;
 
     public JettyDevKonfigurasjon() {
         super(DEV_SERVER_PORT);
