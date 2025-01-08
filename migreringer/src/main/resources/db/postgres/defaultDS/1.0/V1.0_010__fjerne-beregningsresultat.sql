f7141bab3f3d3ad1ff6102dfa86d96dac15bf898
Espen Johansen Velsvik <espen.velsvik@nav.no>
2025-01-08 08:19:36 +0100

    Fjerner orm og tabeller

---
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_010__fjerne-beregningsresultat.sql
@@ -0,0 +1,8 @@
+drop sequence if exists SEQ_BR_ANDEL;
+drop sequence if exists SEQ_BR_PERIODE;
+drop sequence if exists SEQ_BR_BEREGNINGSRESULTAT;
+drop sequence if exists SEQ_BR_RESULTAT_BEHANDLING;
+drop table if exists BR_ANDEL;
+drop table if exists BR_PERIODE;
+drop table if exists BR_BEREGNINGSRESULTAT;
+drop table if exists BR_RESULTAT_BEHANDLING;
