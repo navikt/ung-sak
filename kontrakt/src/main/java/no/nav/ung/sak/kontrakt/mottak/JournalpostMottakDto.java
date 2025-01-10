3c84d8932a051f36bf782968e13b2e542fe41a02
Espen Johansen Velsvik <espen.velsvik@nav.no>
2025-01-10 11:59:36 +0100

    Flytter fordel inn i ung-sak

--- kontrakt/src/main/java/no/nav/ung/sak/kontrakt/mottak/JournalpostMottakDto.java
+++ kontrakt/src/main/java/no/nav/ung/sak/kontrakt/mottak/JournalpostMottakDto.java
@@ -118,6 +118,10 @@ public class JournalpostMottakDto {
         return base64EncodedPayload;
     }
 
+    public String getBase64EncodedPayload() {
+        return base64EncodedPayload;
+    }
+
     @Override
     public String toString() {
         return getClass().getSimpleName() + "<"
