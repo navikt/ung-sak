19379c3376dc1d3a587b84fc9282a31f64a90863
Espen Velsvik <espenjv@users.noreply.github.com>
2025-01-27 20:16:29 +0100

    Utleder status på perioder (#110)
    * Utleder status på perioder
    
    * Fikser injection av søknadsfristtjeneste
    
    * ung-sak.openapi.json updated by build pipeline 
    
    
    skip-checks:true
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- kontrakt/src/main/java/no/nav/ung/sak/kontrakt/krav/ÅrsakTilVurdering.java
+++ kontrakt/src/main/java/no/nav/ung/sak/kontrakt/krav/ÅrsakTilVurdering.java
@@ -26,52 +26,28 @@ public enum ÅrsakTilVurdering implements Kodeverdi {
     ENDRING_FRA_BRUKER("ENDRING_FRA_BRUKER", "Endring fra søknad/Punsj"),
     // Kravet for perioden har blitt trukket
     TRUKKET_KRAV("TRUKKET_KRAV", "Endret/fjernet søknadsperiode"),
-    // Vurderes på nytt pga inntektsmelding som er sendt inn for perioden
-    REVURDERER_NY_INNTEKTSMELDING("REVURDERER_NY_INNTEKTSMELDING", "Ny inntektsmelding"),
-    // Perioden ses på pga endringer i felles opplysninger (Nattevåk/beredskap/etablert tilsyn/sykdom
-    REVURDERER_ENDRING_FRA_ANNEN_PART("REVURDERER_ENDRING_FRA_ANNEN_PART", "Annen parts vedtak endrer uttak"),
-    REVURDERER_ENDRING_FERIEPENGER("REVURDERER_ENDRING_FERIEPENGER_ANNEN_SAK", "Feriepenger reberegnes som følge av vedtak i annen sak"),
-    UTSATT_BEHANDLING("UTSATT_BEHANDLING", "Utsatt behandling"),
-    GJENOPPTAR_UTSATT_BEHANDLING("GJENOPPTAR_UTSATT_BEHANDLING", "Gjenopptar utsatt behandling"),
-    // Endringer på felles opplysninger på sykdomsopplysningene
-    REVURDERER_SYKDOM_ENDRING_FRA_ANNEN_OMSORGSPERSON("REVURDERER_SYKDOM_ENDRING_FRA_ANNEN_OMSORGSPERSON", "Endring i vurdering av sykdom"),
-    // Endringer på felles opplysninger om etablert tilsyn
-    REVURDERER_ETABLERT_TILSYN_ENDRING_FRA_ANNEN_OMSORGSPERSON("REVURDERER_ETABLERT_TILSYN_ENDRING_FRA_ANNEN_OMSORGSPERSON", "Endring i felles opplysninger om etablert tilsyn"),
-    // Endringer på felles opplysninger om nattevåk/beredskap
-    REVURDERER_NATTEVÅKBEREDSKAP_ENDRING_FRA_ANNEN_OMSORGSPERSON("REVURDERER_NATTEVÅKBEREDSKAP_ENDRING_FRA_ANNEN_OMSORGSPERSON", "Endring i felles opplysninger om nattevåk/beredskap"),
     HENDELSE_DØD_BRUKER("HENDELSE_DØD_BRUKER", "Dødsfall bruker"),
+    HENDELSE_DØD_BARN("HENDELSE_DØD_BARN", "Dødsfall barn"),
+    OPPHØR_UNGDOMSPROGRAM("OPPHØR_UNGDOMSPROGRAM", "Opphør av ungdomsprogram"),
     // Vurderes på nytt pga G_REGULERING
     G_REGULERING("G_REGULERING", "G-regulering"),
     REVURDERER_BEREGNING("REVURDERER_BEREGNING", "Endring opplysninger som påvirker beregningsgrunnlaget."),
-    ENDRING_I_FORDELING("ENDRING_I_FORDELING", "Endring som påvirker fordeling og/eller gradering av ytelse."),
-
     // Vurderes for første gang
-    FØRSTEGANGSVURDERING("FØRSTEGANGSVURDERING", "Ny periode"),
-    // Vurdering etter klage på lignet inntekt
-    REVURDERER_LIGNET_INNTEKT("REVURDERER_LIGNET_INNTEKT", "Manuell revurdering der lignet inntekt innhentes på nytt");
-
+    FØRSTEGANGSVURDERING("FØRSTEGANGSVURDERING", "Ny periode");
     private static final Map<String, ÅrsakTilVurdering> KODER = new LinkedHashMap<>();
     private static final Map<BehandlingÅrsakType, ÅrsakTilVurdering> SAMMENHENG;
 
     static {
         EnumMap<BehandlingÅrsakType, ÅrsakTilVurdering> sammenheng = new EnumMap<>(BehandlingÅrsakType.class);
-        sammenheng.put(BehandlingÅrsakType.RE_SATS_REGULERING, G_REGULERING);
-        sammenheng.put(BehandlingÅrsakType.RE_ENDRING_FRA_ANNEN_OMSORGSPERSON, REVURDERER_ENDRING_FRA_ANNEN_PART);
-        sammenheng.put(BehandlingÅrsakType.RE_UTSATT_BEHANDLING, UTSATT_BEHANDLING);
-        sammenheng.put(BehandlingÅrsakType.RE_GJENOPPTAR_UTSATT_BEHANDLING, GJENOPPTAR_UTSATT_BEHANDLING);
-        sammenheng.put(BehandlingÅrsakType.RE_SYKDOM_ENDRING_FRA_ANNEN_OMSORGSPERSON, REVURDERER_SYKDOM_ENDRING_FRA_ANNEN_OMSORGSPERSON);
-        sammenheng.put(BehandlingÅrsakType.RE_ETABLERT_TILSYN_ENDRING_FRA_ANNEN_OMSORGSPERSON, REVURDERER_ETABLERT_TILSYN_ENDRING_FRA_ANNEN_OMSORGSPERSON);
-        sammenheng.put(BehandlingÅrsakType.RE_NATTEVÅKBEREDSKAP_ENDRING_FRA_ANNEN_OMSORGSPERSON, REVURDERER_NATTEVÅKBEREDSKAP_ENDRING_FRA_ANNEN_OMSORGSPERSON);
+        sammenheng.put(BehandlingÅrsakType.RE_SATS_REGULERING, G_REGULERING);;
         sammenheng.put(BehandlingÅrsakType.RE_HENDELSE_DØD_FORELDER, HENDELSE_DØD_BRUKER);
-        sammenheng.put(BehandlingÅrsakType.RE_ENDRET_INNTEKTSMELDING, REVURDERER_NY_INNTEKTSMELDING);
-        sammenheng.put(BehandlingÅrsakType.RE_FERIEPENGER_ENDRING_FRA_ANNEN_SAK, REVURDERER_ENDRING_FERIEPENGER);
+        sammenheng.put(BehandlingÅrsakType.RE_HENDELSE_DØD_BARN, HENDELSE_DØD_BARN);
         sammenheng.put(BehandlingÅrsakType.RE_OPPLYSNINGER_OM_MEDLEMSKAP, REVURDERER_BERØRT_PERIODE);
-        sammenheng.put(BehandlingÅrsakType.RE_KLAGE_NY_INNH_LIGNET_INNTEKT, REVURDERER_LIGNET_INNTEKT);
         sammenheng.put(BehandlingÅrsakType.RE_KLAGE_NATTEVÅKBEREDSKAP, MANUELT_REVURDERER_PERIODE);
         sammenheng.put(BehandlingÅrsakType.RE_OPPLYSNINGER_OM_BEREGNINGSGRUNNLAG, MANUELT_REVURDERER_PERIODE);
         sammenheng.put(BehandlingÅrsakType.RE_OPPLYSNINGER_OM_OPPTJENING, MANUELT_REVURDERER_PERIODE);
         sammenheng.put(BehandlingÅrsakType.RE_ENDRING_BEREGNINGSGRUNNLAG, REVURDERER_BEREGNING);
-        sammenheng.put(BehandlingÅrsakType.RE_ENDRET_FORDELING, ENDRING_I_FORDELING);
+        sammenheng.put(BehandlingÅrsakType.RE_HENDELSE_OPPHØR_UNGDOMSPROGRAM, OPPHØR_UNGDOMSPROGRAM);
         SAMMENHENG = Collections.unmodifiableMap(sammenheng);
     }
 
