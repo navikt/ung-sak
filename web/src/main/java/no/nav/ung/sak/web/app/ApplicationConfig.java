16956298e7ac812285e99eebe4159bb53801d62a
Jostein Stuhaug <jostein.stuhaug@nav.no>
2025-02-11 13:54:03 +0100

    Ta i bruk openapi-spec-utils for openapi konfigurasjon og tilpasning av objectmapper.
    Med dette får vi samme openapi spesifikasjon oppsett/generering frå ung-sak som frå k9-sak. Får også samme oppsett for (dynamisk) openapi kompatibel serialisering/deserialisering.

--- web/src/main/java/no/nav/ung/sak/web/app/ApplicationConfig.java
+++ web/src/main/java/no/nav/ung/sak/web/app/ApplicationConfig.java
@@ -1,74 +1,54 @@
 package no.nav.ung.sak.web.app;
 
 
-import java.util.LinkedHashSet;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.server.ServerProperties;
-
-import io.swagger.v3.core.converter.ModelConverters;
-import io.swagger.v3.jaxrs2.integration.JaxrsOpenApiContextBuilder;
-import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
 import io.swagger.v3.oas.integration.OpenApiConfigurationException;
-import io.swagger.v3.oas.integration.SwaggerConfiguration;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import jakarta.ws.rs.ApplicationPath;
+import no.nav.openapi.spec.utils.jackson.DynamicJacksonJsonProvider;
+import no.nav.openapi.spec.utils.openapi.OpenApiSetupHelper;
 import no.nav.ung.sak.web.app.exceptions.KnownExceptionMappers;
-import no.nav.ung.sak.web.app.jackson.CustomJacksonJsonProvider;
 import no.nav.ung.sak.web.app.jackson.ObjectMapperResolver;
 import no.nav.ung.sak.web.app.tjenester.RestImplementationClasses;
 import no.nav.ung.sak.web.server.caching.CacheControlFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.ServerProperties;
+
+import java.util.LinkedHashSet;
 
 @ApplicationPath(ApplicationConfig.API_URI)
 public class ApplicationConfig extends ResourceConfig {
 
     public static final String API_URI = "/api";
 
-    public static OpenAPI resolveOpenAPI() {
-        OpenAPI oas = new OpenAPI();
-        Info info = new Info()
+    public OpenAPI resolveOpenAPI() {
+        final var info = new Info()
             .title("Ung saksbehandling - Saksbehandling for ungdomsytelse")
-            .version("0.1")
+            .version("0.2")
             .description("REST grensesnitt for Vedtaksløsningen.");
 
-        oas.info(info)
-            .addServersItem(new Server()
-                .url("/ung/sak"));
-        // Alle properties som kan vere null skal ha nullable satt
-        ModelConverters.getInstance().addConverter(new NullablePropertyConverter());
-        SwaggerConfiguration oasConfig = new SwaggerConfiguration()
-            .openAPI(oas)
-            .prettyPrint(true)
-            .readerClass(CustomResponseTypeAdjustingReader.class.getCanonicalName())
-            .scannerClass("io.swagger.v3.jaxrs2.integration.JaxrsAnnotationScanner")
-            .resourcePackages(Stream.of("no.nav.ung.sak", "no.nav.k9")
-                .collect(Collectors.toSet()));
-
+        final var server =new Server().url("/ung/sak");
+        final var openapiSetupHelper = new OpenApiSetupHelper(this, info, server);
+        openapiSetupHelper.addResourcePackage("no.nav.ung.sak");
+        openapiSetupHelper.addResourcePackage("no.nav.k9");
         try {
-            return new JaxrsOpenApiContextBuilder<>()
-                .openApiConfiguration(oasConfig)
-                .buildContext(true)
-                .read();
+            return openapiSetupHelper.resolveOpenAPI();
         } catch (OpenApiConfigurationException e) {
             throw new RuntimeException(e.getMessage(), e);
         }
     }
 
     public ApplicationConfig() {
-        ApplicationConfig.resolveOpenAPI();
+        final var resolvedOpenAPI = resolveOpenAPI();
+        register(new no.nav.openapi.spec.utils.openapi.OpenApiResource(resolvedOpenAPI));
 
         property(ServerProperties.BV_SEND_ERROR_IN_RESPONSE, true);
-
-        register(OpenApiResource.class);
 
         registerClasses(new LinkedHashSet<>(new RestImplementationClasses().getImplementationClasses()));
 
         register(ObjectMapperResolver.class);
-        register(CustomJacksonJsonProvider.class);
+        register(DynamicJacksonJsonProvider.class);
 
         registerInstances(new LinkedHashSet<>(new KnownExceptionMappers().getExceptionMappers()));
         register(CacheControlFeature.class);
