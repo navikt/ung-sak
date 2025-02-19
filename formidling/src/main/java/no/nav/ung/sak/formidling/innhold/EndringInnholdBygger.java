bfe4c096c30e4657b5552c3e1f6b8c7e7d7845d9
Qadeer Khan <qadeer.ahmad.khan@nav.no>
2025-02-19 13:42:19 +0100

    Første versjon av reduksjon av inntekt tekst

--- formidling/src/main/java/no/nav/ung/sak/formidling/innhold/EndringInnholdBygger.java
+++ formidling/src/main/java/no/nav/ung/sak/formidling/innhold/EndringInnholdBygger.java
@@ -1,11 +1,14 @@
 package no.nav.ung.sak.formidling.innhold;
 
+import java.time.LocalDate;
+
 import jakarta.enterprise.context.Dependent;
 import no.nav.ung.kodeverk.dokument.DokumentMalType;
 import no.nav.ung.sak.behandlingslager.behandling.Behandling;
 import no.nav.ung.sak.formidling.template.TemplateType;
 import no.nav.ung.sak.formidling.template.dto.EndringDto;
 import no.nav.ung.sak.formidling.template.dto.endring.EndringRapportertInntektDto;
+import no.nav.ung.sak.formidling.template.dto.felles.PeriodeDto;
 
 @Dependent
 public class EndringInnholdBygger implements VedtaksbrevInnholdBygger  {
@@ -13,6 +16,16 @@ public class EndringInnholdBygger implements VedtaksbrevInnholdBygger  {
 
     @Override
     public TemplateInnholdResultat bygg(Behandling behandlingId) {
-        return new TemplateInnholdResultat(DokumentMalType.ENDRING_DOK, TemplateType.ENDRING_INNTEKT, new EndringDto(new EndringRapportertInntektDto(1000)));
+        return new TemplateInnholdResultat(DokumentMalType.ENDRING_DOK, TemplateType.ENDRING_INNTEKT, new EndringDto(
+            new EndringRapportertInntektDto(
+                new PeriodeDto(LocalDate.now(), LocalDate.now()),
+                10000,
+                2000,
+                48,
+                500,
+                686,
+                355
+            )
+        ));
     }
 }
--- formidling/src/main/java/no/nav/ung/sak/formidling/template/dto/endring/EndringRapportertInntektDto.java
+++ formidling/src/main/java/no/nav/ung/sak/formidling/template/dto/endring/EndringRapportertInntektDto.java
@@ -1,6 +1,15 @@
 package no.nav.ung.sak.formidling.template.dto.endring;
 
+import no.nav.ung.sak.formidling.template.dto.felles.PeriodeDto;
+
 public record EndringRapportertInntektDto(
-    long rapportertInntekt
+    PeriodeDto periode,
+    long rapportertInntekt,
+    long utbetalingBeløp,
+    int reduksjonssats,
+    long reduksjonBeløp,
+    long dagsats,
+    long redusertDagsats
+
 ) {
 }
