0a532dee463f5a439b41b12965a7b25cb8bab032
Espen Velsvik <espen.velsvik@nav.no>
2025-03-04 14:57:54 +0100

    TSFF-1158: Søknadsperioder er lik perioder i programmet

--- domenetjenester/perioder/src/main/java/no/nav/ung/sak/perioder/UngdomsytelseSøknadsperiodeTjeneste.java
+++ domenetjenester/perioder/src/main/java/no/nav/ung/sak/perioder/UngdomsytelseSøknadsperiodeTjeneste.java
@@ -1,25 +1,22 @@
 package no.nav.ung.sak.perioder;
 
-import static no.nav.ung.sak.domene.typer.tid.AbstractLocalDateInterval.TIDENES_ENDE;
-import static no.nav.ung.sak.domene.typer.tid.TidslinjeUtil.tilTidslinje;
-
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
-import java.util.*;
-import java.util.function.Function;
-
 import jakarta.enterprise.context.Dependent;
 import jakarta.inject.Inject;
-import no.nav.fpsak.tidsserie.LocalDateInterval;
 import no.nav.fpsak.tidsserie.LocalDateTimeline;
 import no.nav.ung.sak.behandlingslager.behandling.repository.BehandlingRepository;
 import no.nav.ung.sak.behandlingslager.behandling.startdato.UngdomsytelseStartdatoGrunnlag;
 import no.nav.ung.sak.behandlingslager.behandling.startdato.UngdomsytelseStartdatoRepository;
 import no.nav.ung.sak.behandlingslager.behandling.startdato.UngdomsytelseStartdatoer;
-import no.nav.ung.sak.behandlingslager.behandling.startdato.UngdomsytelseSøktStartdato;
-import no.nav.ung.sak.behandlingslager.fagsak.Fagsak;
 import no.nav.ung.sak.domene.typer.tid.DatoIntervallEntitet;
 import no.nav.ung.sak.ungdomsprogram.UngdomsprogramPeriodeTjeneste;
+
+import java.util.Collections;
+import java.util.NavigableSet;
+import java.util.TreeSet;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static no.nav.ung.sak.domene.typer.tid.TidslinjeUtil.tilTidslinje;
 
 @Dependent
 public class UngdomsytelseSøknadsperiodeTjeneste {
@@ -39,7 +36,7 @@ public class UngdomsytelseSøknadsperiodeTjeneste {
     /**
      * Finner søknadsperioder som har kommet inn i denne behandlingen
      *
-     * @param behandlingId      BehandlingId
+     * @param behandlingId BehandlingId
      * @return Relevante søknadsperioder for denne behandlingen
      */
     public NavigableSet<DatoIntervallEntitet> utledPeriode(Long behandlingId) {
@@ -68,40 +65,15 @@ public class UngdomsytelseSøknadsperiodeTjeneste {
             return Collections.emptyNavigableSet();
         } else {
             LocalDateTimeline<Boolean> ungdomsprogramtidslinje = ungdomsprogramPeriodeTjeneste.finnPeriodeTidslinje(behandlingId);
-            final var søkteDatoer = søknadsperioder.get().getStartdatoer();
-            var startdatoer = søkteDatoer.stream().map(UngdomsytelseSøktStartdato::getStartdato)
-                .sorted()
-                .toList();
-
             var behandling = behandlingRepository.hentBehandling(behandlingId);
-
             var fagsak = behandling.getFagsak();
-
-            final NavigableSet<DatoIntervallEntitet> resultatPerioder = new TreeSet<>();
-
-            final var ungdomsprogramperioder = ungdomsprogramtidslinje.getLocalDateIntervals();
-            for (var startDato: startdatoer) {
-                final var sluttdatoForPeriodeMedMinstAvstandTilStartdato = ungdomsprogramperioder.stream()
-                    .filter(p -> !p.getFomDato().isBefore(startDato)) // Støtter i første omgang kun endring av startdato fram i tid
-                    .min(Comparator.comparing(i1 -> startDato.until(i1.getFomDato().plusDays(1), ChronoUnit.DAYS)))
-                    .map(LocalDateInterval::getTomDato)
-                    .map(d -> begrensTomDato(fagsak, d));
-                if (sluttdatoForPeriodeMedMinstAvstandTilStartdato.isPresent() && harIkkeBruktPeriode(resultatPerioder, sluttdatoForPeriodeMedMinstAvstandTilStartdato)) {
-                    resultatPerioder.add(DatoIntervallEntitet.fraOgMedTilOgMed(startDato, sluttdatoForPeriodeMedMinstAvstandTilStartdato.get()));
-                }
-
-            }
-
-            return resultatPerioder;
+            final var fagsakTidslinje = new LocalDateTimeline<>(fagsak.getPeriode().getFomDato(), fagsak.getPeriode().getTomDato(), true);
+            return ungdomsprogramtidslinje.intersection(fagsakTidslinje)
+                .getLocalDateIntervals()
+                .stream()
+                .map(DatoIntervallEntitet::fra)
+                .collect(Collectors.toCollection(TreeSet::new));
         }
-    }
-
-    private static boolean harIkkeBruktPeriode(NavigableSet<DatoIntervallEntitet> resultatPerioder, Optional<LocalDate> sluttdato) {
-        return resultatPerioder.stream().noneMatch(p -> p.getTomDato().equals(sluttdato.get()));
-    }
-
-    private static LocalDate begrensTomDato(Fagsak fagsak, LocalDate sluttDato) {
-        return sluttDato.equals(TIDENES_ENDE) ? fagsak.getPeriode().getTomDato() : sluttDato;
     }
 
 
