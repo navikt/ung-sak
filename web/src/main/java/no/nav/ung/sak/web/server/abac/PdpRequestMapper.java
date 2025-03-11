58feb459480e7d65a83271ac001565d73d31d857
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2025-03-11 15:04:10 +0100

    Bump avhengighet til sif-abac-pdp-kontrakt

--- web/src/main/java/no/nav/ung/sak/web/server/abac/PdpRequestMapper.java
+++ web/src/main/java/no/nav/ung/sak/web/server/abac/PdpRequestMapper.java
@@ -1,10 +1,14 @@
 package no.nav.ung.sak.web.server.abac;
 
 import no.nav.k9.felles.sikkerhet.abac.PdpRequest;
-import no.nav.sif.abac.kontrakt.abac.*;
+import no.nav.sif.abac.kontrakt.abac.AbacBehandlingStatus;
+import no.nav.sif.abac.kontrakt.abac.AbacFagsakStatus;
+import no.nav.sif.abac.kontrakt.abac.AksjonspunktType;
+import no.nav.sif.abac.kontrakt.abac.BeskyttetRessursActionAttributt;
+import no.nav.sif.abac.kontrakt.abac.ResourceType;
 import no.nav.sif.abac.kontrakt.abac.dto.OperasjonDto;
 import no.nav.sif.abac.kontrakt.abac.dto.SaksinformasjonDto;
-import no.nav.sif.abac.kontrakt.abac.dto.SaksinformasjonTilgangskontrollInputDto;
+import no.nav.sif.abac.kontrakt.abac.dto.SaksinformasjonOgPersonerTilgangskontrollInputDto;
 import no.nav.sif.abac.kontrakt.person.AktørId;
 import no.nav.sif.abac.kontrakt.person.PersonIdent;
 
@@ -15,8 +19,8 @@ import java.util.Set;
 
 public class PdpRequestMapper {
 
-    public static SaksinformasjonTilgangskontrollInputDto map(PdpRequest pdpRequest){
-        return new SaksinformasjonTilgangskontrollInputDto(
+    public static SaksinformasjonOgPersonerTilgangskontrollInputDto map(PdpRequest pdpRequest){
+        return new SaksinformasjonOgPersonerTilgangskontrollInputDto(
             pdpRequest.getListOfString(AbacAttributter.RESOURCE_FELLES_PERSON_AKTOERID_RESOURCE).stream().map(AktørId::new).toList(),
             pdpRequest.getListOfString(AbacAttributter.RESOURCE_FELLES_PERSON_FNR).stream().map(PersonIdent::new).toList(),
             operasjon(pdpRequest),
