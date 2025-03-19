8c92073f826140f88d89a29afccfaca5c1454a89
Qadeer Khan <qadeer.ahmad.khan@nav.no>
2025-03-19 10:54:28 +0100

    konstruktør

--- domenetjenester/mottak/src/main/java/no/nav/ung/sak/mottak/dokumentmottak/oppgavebekreftelse/InntektBekreftelseHåndterer.java
+++ domenetjenester/mottak/src/main/java/no/nav/ung/sak/mottak/dokumentmottak/oppgavebekreftelse/InntektBekreftelseHåndterer.java
@@ -1,12 +1,17 @@
 package no.nav.ung.sak.mottak.dokumentmottak.oppgavebekreftelse;
 
 import jakarta.enterprise.context.Dependent;
+import jakarta.inject.Inject;
 import no.nav.k9.oppgave.bekreftelse.Bekreftelse;
 
 @Dependent
 @OppgaveTypeRef(Bekreftelse.Type.UNG_AVVIK_REGISTERINNTEKT)
 public class InntektBekreftelseHåndterer implements BekreftelseHåndterer {
 
+
+    @Inject
+    public InntektBekreftelseHåndterer() {
+    }
 
     @Override
     public void håndter(OppgaveBekreftelseInnhold bekreftelse) {
