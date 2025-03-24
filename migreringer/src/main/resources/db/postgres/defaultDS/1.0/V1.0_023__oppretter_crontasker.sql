c1ac4527d928fa25bdc3e0685c3f51f9210dd1d4
Qadeer Khan <qadeer.ahmad.khan@nav.no>
2025-03-24 11:51:18 +0100

    endringer etter QA. Usikre på tilbakekreving tasken

--- migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_023__oppretter_crontasker.sql
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_023__oppretter_crontasker.sql
@@ -1,18 +1,20 @@
-insert into prosess_task (id, task_type, prioritet, status, task_gruppe, task_sekvens, partition_key)
-select nextval('SEQ_PROSESS_TASK'), 'batch.automatiskGjenopptaglese', 1, 'KLAR', nextval('SEQ_PROSESS_TASK_GRUPPE'), 1,'05'
-where not exists (select 1 from prosess_task where task_type = 'batch.automatiskGjenopptaglese'  and status IN ('KLAR', 'FEILET'));
-
+-- nye for UNG
 insert into prosess_task (id, task_type, prioritet, status, task_gruppe, task_sekvens, partition_key)
 select nextval('SEQ_PROSESS_TASK'), 'batch.opprettRevurderingHøySats', 1, 'KLAR', nextval('SEQ_PROSESS_TASK_GRUPPE'), 1,'05'
 where not exists (select 1 from prosess_task where task_type = 'batch.opprettRevurderingHøySats'  and status IN ('KLAR', 'FEILET'));
 
 insert into prosess_task (id, task_type, prioritet, status, task_gruppe, task_sekvens, partition_key)
-select nextval('SEQ_PROSESS_TASK'), 'daglig.sensu.metrikk.task', 1, 'KLAR', nextval('SEQ_PROSESS_TASK_GRUPPE'), 1,'05'
-where not exists (select 1 from prosess_task where task_type = 'daglig.sensu.metrikk.task'  and status IN ('KLAR', 'FEILET'));
+select nextval('SEQ_PROSESS_TASK'), 'batch.opprettRevurderingForInntektskontrollBatch', 1, 'KLAR', nextval('SEQ_PROSESS_TASK_GRUPPE'), 1,'05'
+where not exists (select 1 from prosess_task where task_type = 'batch.opprettRevurderingForInntektskontrollBatch'  and status IN ('KLAR', 'FEILET'));
 
 insert into prosess_task (id, task_type, prioritet, status, task_gruppe, task_sekvens, partition_key)
-select nextval('SEQ_PROSESS_TASK'), 'batch.gjenopptaVenterPåTilbakekreving', 1, 'KLAR', nextval('SEQ_PROSESS_TASK_GRUPPE'), 1,'05'
-where not exists (select 1 from prosess_task where task_type = 'batch.gjenopptaVenterPåTilbakekreving'  and status IN ('KLAR', 'FEILET'));
+select nextval('SEQ_PROSESS_TASK'), 'batch.automatiskSettEtterlysningUtlopt', 1, 'KLAR', nextval('SEQ_PROSESS_TASK_GRUPPE'), 1,'05'
+where not exists (select 1 from prosess_task where task_type = 'batch.automatiskSettEtterlysningUtlopt'  and status IN ('KLAR', 'FEILET'));
+
+-- andre
+insert into prosess_task (id, task_type, prioritet, status, task_gruppe, task_sekvens, partition_key)
+select nextval('SEQ_PROSESS_TASK'), 'batch.automatiskGjenopptaglese', 1, 'KLAR', nextval('SEQ_PROSESS_TASK_GRUPPE'), 1,'05'
+where not exists (select 1 from prosess_task where task_type = 'batch.automatiskGjenopptaglese'  and status IN ('KLAR', 'FEILET'));
 
 insert into prosess_task (id, task_type, prioritet, status, task_gruppe, task_sekvens, partition_key)
 select nextval('SEQ_PROSESS_TASK'), 'batch.partitionCleanBucket', 1, 'KLAR', nextval('SEQ_PROSESS_TASK_GRUPPE'), 1,'05'
@@ -23,13 +25,10 @@ select nextval('SEQ_PROSESS_TASK'), 'batch.retryFeilendeTasks', 1, 'KLAR', nextv
 where not exists (select 1 from prosess_task where task_type = 'batch.retryFeilendeTasks'  and status IN ('KLAR', 'FEILET'));
 
 insert into prosess_task (id, task_type, prioritet, status, task_gruppe, task_sekvens, partition_key)
-select nextval('SEQ_PROSESS_TASK'), 'batch.opprettRevurderingForInntektskontrollBatch', 1, 'KLAR', nextval('SEQ_PROSESS_TASK_GRUPPE'), 1,'05'
-where not exists (select 1 from prosess_task where task_type = 'batch.opprettRevurderingForInntektskontrollBatch'  and status IN ('KLAR', 'FEILET'));
-
-insert into prosess_task (id, task_type, prioritet, status, task_gruppe, task_sekvens, partition_key)
-select nextval('SEQ_PROSESS_TASK'), 'batch.automatiskSettEtterlysningUtlopt', 1, 'KLAR', nextval('SEQ_PROSESS_TASK_GRUPPE'), 1,'05'
-where not exists (select 1 from prosess_task where task_type = 'batch.automatiskSettEtterlysningUtlopt'  and status IN ('KLAR', 'FEILET'));
-
-insert into prosess_task (id, task_type, prioritet, status, task_gruppe, task_sekvens, partition_key)
 select nextval('SEQ_PROSESS_TASK'), 'sensu.metrikk.task', 1, 'KLAR', nextval('SEQ_PROSESS_TASK_GRUPPE'), 1,'05'
 where not exists (select 1 from prosess_task where task_type = 'sensu.metrikk.task'  and status IN ('KLAR', 'FEILET'));
+
+insert into prosess_task (id, task_type, prioritet, status, task_gruppe, task_sekvens, partition_key)
+select nextval('SEQ_PROSESS_TASK'), 'batch.finnFagsakerForAvsluttning', 1, 'KLAR', nextval('SEQ_PROSESS_TASK_GRUPPE'), 1,'05'
+where not exists (select 1 from prosess_task where task_type = 'batch.finnFagsakerForAvsluttning'  and status IN ('KLAR', 'FEILET'));
+
