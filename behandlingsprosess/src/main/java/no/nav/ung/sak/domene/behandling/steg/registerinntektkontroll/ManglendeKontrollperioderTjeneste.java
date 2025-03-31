161114b2ce0d1f85594da6190e9c57b30d450f2a
Espen Velsvik <espen.velsvik@nav.no>
2025-03-31 08:47:40 +0200

    FIX: Kopierer segmenter over i mutable set før manipulasjon

--- behandlingsprosess/src/main/java/no/nav/ung/sak/domene/behandling/steg/registerinntektkontroll/ManglendeKontrollperioderTjeneste.java
+++ behandlingsprosess/src/main/java/no/nav/ung/sak/domene/behandling/steg/registerinntektkontroll/ManglendeKontrollperioderTjeneste.java
@@ -3,6 +3,7 @@ package no.nav.ung.sak.domene.behandling.steg.registerinntektkontroll;
 import jakarta.enterprise.context.Dependent;
 import jakarta.inject.Inject;
 import no.nav.fpsak.tidsserie.LocalDateTimeline;
+import no.nav.k9.felles.konfigurasjon.konfig.KonfigVerdi;
 import no.nav.ung.kodeverk.behandling.BehandlingÅrsakType;
 import no.nav.ung.sak.domene.typer.tid.DatoIntervallEntitet;
 import no.nav.ung.sak.perioder.ProsessTriggerPeriodeUtleder;
@@ -10,13 +11,13 @@ import no.nav.ung.sak.trigger.ProsessTriggereRepository;
 import no.nav.ung.sak.trigger.Trigger;
 import no.nav.ung.sak.ytelse.KontrollerteInntektperioderTjeneste;
 import no.nav.ung.sak.ytelseperioder.YtelseperiodeUtleder;
-import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.LocalDate;
 import java.time.temporal.TemporalAdjusters;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 import static no.nav.ung.sak.domene.typer.tid.AbstractLocalDateInterval.TIDENES_BEGYNNELSE;
@@ -26,7 +27,7 @@ public class ManglendeKontrollperioderTjeneste {
 
     private static final Logger LOG = LoggerFactory.getLogger(ManglendeKontrollperioderTjeneste.class);
 
-    public static final int RAPPORTERINGSFRIST_I_MÅNED = 7;
+    private final int rapporteringsfristIMåned;
     private KontrollerteInntektperioderTjeneste kontrollerteInntektperioderTjeneste;
     private YtelseperiodeUtleder ytelseperiodeUtleder;
     private ProsessTriggerPeriodeUtleder prosessTriggerPeriodeUtleder;
@@ -35,11 +36,14 @@ public class ManglendeKontrollperioderTjeneste {
     @Inject
     public ManglendeKontrollperioderTjeneste(KontrollerteInntektperioderTjeneste kontrollerteInntektperioderTjeneste,
                                              YtelseperiodeUtleder ytelseperiodeUtleder,
-                                             ProsessTriggerPeriodeUtleder prosessTriggerPeriodeUtleder, ProsessTriggereRepository prosessTriggereRepository) {
+                                             ProsessTriggerPeriodeUtleder prosessTriggerPeriodeUtleder,
+                                             ProsessTriggereRepository prosessTriggereRepository,
+                                             @KonfigVerdi(value = "RAPPORTERINGSFRIST_DAG_I_MAANED", defaultVerdi = "6") int rapporteringsfristIMåned) {
         this.kontrollerteInntektperioderTjeneste = kontrollerteInntektperioderTjeneste;
         this.ytelseperiodeUtleder = ytelseperiodeUtleder;
         this.prosessTriggerPeriodeUtleder = prosessTriggerPeriodeUtleder;
         this.prosessTriggereRepository = prosessTriggereRepository;
+        this.rapporteringsfristIMåned = rapporteringsfristIMåned;
     }
 
     /** Legger til triggere for kontroll der denne mangler
@@ -77,14 +81,14 @@ public class ManglendeKontrollperioderTjeneste {
         return kontrollerteInntektperioderTjeneste.hentTidslinje(behandlingId).mapValue(it -> true);
     }
 
-    private static LocalDateTimeline<Boolean> finnPerioderMedPassertRapporteringsfrist() {
+    private LocalDateTimeline<Boolean> finnPerioderMedPassertRapporteringsfrist() {
         return new LocalDateTimeline<>(TIDENES_BEGYNNELSE, getTomDatoForPassertRapporteringsfrist(), true);
     }
 
     private static LocalDateTimeline<Boolean> finnPerioderSomSkalKontrolleres(LocalDateTimeline<Boolean> ytelsesPerioder) {
         LocalDateTimeline<Boolean> perioderForKontroll = LocalDateTimeline.empty();
         if (ytelsesPerioder.toSegments().size() > 2) {
-            final var segmenterForKontroll = ytelsesPerioder.toSegments();
+            final var segmenterForKontroll = new TreeSet<>(ytelsesPerioder.toSegments());
             segmenterForKontroll.removeFirst();
             segmenterForKontroll.removeLast();
             perioderForKontroll = new LocalDateTimeline<>(segmenterForKontroll);
@@ -92,9 +96,9 @@ public class ManglendeKontrollperioderTjeneste {
         return perioderForKontroll;
     }
 
-    private static LocalDate getTomDatoForPassertRapporteringsfrist() {
+    private LocalDate getTomDatoForPassertRapporteringsfrist() {
         final var dagensDato = LocalDate.now();
-        return dagensDato.getDayOfMonth() < RAPPORTERINGSFRIST_I_MÅNED ? dagensDato.minusMonths(2).with(TemporalAdjusters.lastDayOfMonth()) : dagensDato.minusMonths(1).with(TemporalAdjusters.lastDayOfMonth());
+        return dagensDato.getDayOfMonth() <= rapporteringsfristIMåned ? dagensDato.minusMonths(2).with(TemporalAdjusters.lastDayOfMonth()) : dagensDato.minusMonths(1).with(TemporalAdjusters.lastDayOfMonth());
     }
 
 }
