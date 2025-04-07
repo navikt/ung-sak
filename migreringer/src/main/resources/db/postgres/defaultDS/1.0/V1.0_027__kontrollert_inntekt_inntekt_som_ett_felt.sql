94d33dc0967de55320e2cf76ea374ecac470d48b
Espen Velsvik <espen.velsvik@nav.no>
2025-04-07 15:39:55 +0200

    TSFF-1316: Fix typo

--- migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_027__kontrollert_inntekt_inntekt_som_ett_felt.sql
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_027__kontrollert_inntekt_inntekt_som_ett_felt.sql
@@ -1,6 +1,6 @@
 alter table if exists kontrollert_inntekt_periode
 add column inntekt numeric;
-comment on column kontrollert_inntekt_periode.inntekt is 'Konttrollert inntekt for perioden.';
+comment on column kontrollert_inntekt_periode.inntekt is 'Kontrollert inntekt for perioden.';
 
 update kontrollert_inntekt_periode set inntekt = COALESCE (ytelse, 0) + COALESCE (arbeidsinntekt, 0);
 
