d13f5c80dc8bd8b7d37e3b77eb3b2096b441dbdb
Ramin Esfandiari <ramin.esfandiari@nav.no>
2025-04-08 14:05:39 +0200

    Oppretter etterlysning for prosesstrigger.

--- behandlingsprosess/src/main/java/no/nav/ung/sak/domene/behandling/steg/varselrevurdering/VarselRevurderingStegImpl.java
+++ behandlingsprosess/src/main/java/no/nav/ung/sak/domene/behandling/steg/varselrevurdering/VarselRevurderingStegImpl.java
@@ -2,13 +2,12 @@ package no.nav.ung.sak.domene.behandling.steg.varselrevurdering;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import no.nav.fpsak.tidsserie.LocalDateInterval;
 import no.nav.fpsak.tidsserie.LocalDateTimeline;
+import no.nav.fpsak.tidsserie.StandardCombinators;
 import no.nav.k9.felles.konfigurasjon.konfig.KonfigVerdi;
 import no.nav.k9.prosesstask.api.ProsessTaskData;
 import no.nav.k9.prosesstask.api.ProsessTaskTjeneste;
 import no.nav.ung.kodeverk.behandling.BehandlingÅrsakType;
-import no.nav.ung.kodeverk.etterlysning.EtterlysningType;
 import no.nav.ung.sak.behandling.BehandlingReferanse;
 import no.nav.ung.sak.behandlingskontroll.*;
 import no.nav.ung.sak.behandlingslager.behandling.Behandling;
@@ -22,10 +21,16 @@ import no.nav.ung.sak.behandlingslager.perioder.UngdomsprogramPeriodeGrunnlag;
 import no.nav.ung.sak.behandlingslager.perioder.UngdomsprogramPeriodeRepository;
 import no.nav.ung.sak.domene.typer.tid.DatoIntervallEntitet;
 import no.nav.ung.sak.etterlysning.OpprettEtterlysningTask;
+import no.nav.ung.sak.trigger.ProsessTriggere;
+import no.nav.ung.sak.trigger.ProsessTriggereRepository;
+import no.nav.ung.sak.trigger.Trigger;
 import no.nav.ung.sak.ungdomsprogram.UngdomsprogramPeriodeTjeneste;
+import org.jetbrains.annotations.Nullable;
 
 import java.time.Duration;
+import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 import static no.nav.ung.kodeverk.behandling.BehandlingStegType.VARSEL_REVURDERING;
@@ -44,13 +49,14 @@ public class VarselRevurderingStegImpl implements VarselRevurderingSteg {
     private MottatteDokumentRepository mottatteDokumentRepository;
     private ProsessTaskTjeneste prosessTaskTjeneste;
     private EtterlysningRepository etterlysningRepository;
+    private ProsessTriggereRepository prosessTriggereRepository;
     private final Duration ventePeriode;
 
     @Inject
     public VarselRevurderingStegImpl(BehandlingRepository behandlingRepository,
                                      UngdomsprogramPeriodeTjeneste ungdomsprogramPeriodeTjeneste,
                                      UngdomsytelseStartdatoRepository ungdomsytelseStartdatoRepository, UngdomsprogramPeriodeRepository ungdomsprogramPeriodeRepository,
-                                     MottatteDokumentRepository mottatteDokumentRepository, ProsessTaskTjeneste prosessTaskTjeneste, EtterlysningRepository etterlysningRepository,
+                                     MottatteDokumentRepository mottatteDokumentRepository, ProsessTaskTjeneste prosessTaskTjeneste, EtterlysningRepository etterlysningRepository, ProsessTriggereRepository prosessTriggereRepository,
                                      @KonfigVerdi(value = "REVURDERING_ENDRET_PERIODE_VENTEFRIST", defaultVerdi = "P14D") String ventePeriode) {
         this.behandlingRepository = behandlingRepository;
         this.ungdomsprogramPeriodeTjeneste = ungdomsprogramPeriodeTjeneste;
@@ -59,29 +65,24 @@ public class VarselRevurderingStegImpl implements VarselRevurderingSteg {
         this.mottatteDokumentRepository = mottatteDokumentRepository;
         this.prosessTaskTjeneste = prosessTaskTjeneste;
         this.etterlysningRepository = etterlysningRepository;
+        this.prosessTriggereRepository = prosessTriggereRepository;
         this.ventePeriode = Duration.parse(ventePeriode);
     }
 
     @Override
     public BehandleStegResultat utførSteg(BehandlingskontrollKontekst kontekst) {
-
         Behandling behandling = behandlingRepository.hentBehandling(kontekst.getBehandlingId());
 
         if (behandling.getBehandlingÅrsaker().isEmpty()) {
             return BehandleStegResultat.utførtUtenAksjonspunkter();
         }
 
-        final var ungdomsprogramTidslinje = ungdomsprogramPeriodeTjeneste.finnPeriodeTidslinje(behandling.getId()).compress();
         LocalDateTimeline<Boolean> endretUngdomsprogramTidslinje = ungdomsprogramPeriodeTjeneste.finnEndretPeriodeTidslinje(BehandlingReferanse.fra(behandling));
+        opprettTaskForEtterlysning(kontekst, endretUngdomsprogramTidslinje);
+
         final var bekreftelser = finnBekreftelser(behandling);
         final var gyldigeDokumenter = mottatteDokumentRepository.hentGyldigeDokumenterMedFagsakId(behandling.getFagsakId());
-
-        if (behandling.harBehandlingÅrsak(BehandlingÅrsakType.RE_HENDELSE_ENDRET_STARTDATO_UNGDOMSPROGRAM)) {
-            opprettTaskForEtterlysning(EtterlysningType.UTTALELSE_ENDRET_STARTDATO, kontekst, endretUngdomsprogramTidslinje);
-        } else if (behandling.harBehandlingÅrsak(BehandlingÅrsakType.RE_HENDELSE_OPPHØR_UNGDOMSPROGRAM)) {
-            // TODO: Utled om RE_HENDELSE_OPPHØR_UNGDOMSPROGRAM burde trigge etterlysning første gang.
-            opprettTaskForEtterlysning(EtterlysningType.UTTALELSE_ENDRET_SLUTTDATO, kontekst, endretUngdomsprogramTidslinje);
-        }
+        final var ungdomsprogramTidslinje = ungdomsprogramPeriodeTjeneste.finnPeriodeTidslinje(behandling.getId()).compress();
 
         return VarselRevurderingAksjonspunktUtleder.utledAksjonspunkt(
                 behandling.getBehandlingÅrsakerTyper(),
@@ -102,41 +103,60 @@ public class VarselRevurderingStegImpl implements VarselRevurderingSteg {
             .flatMap(it -> it.getBekreftetPeriodeEndringer().stream()).toList();
     }
 
-    private void opprettTaskForEtterlysning(EtterlysningType etterlysningType, BehandlingskontrollKontekst kontekst, LocalDateTimeline<Boolean> endretUngdomsprogramTidslinje) {
+    private void opprettTaskForEtterlysning(BehandlingskontrollKontekst kontekst, LocalDateTimeline<Boolean> endretUngdomsprogramTidslinje) {
         UngdomsprogramPeriodeGrunnlag ungdomsprogramPeriodeGrunnlag = ungdomsprogramPeriodeRepository.hentGrunnlag(kontekst.getBehandlingId()).orElseThrow();
         UUID grunnlagsreferanse = ungdomsprogramPeriodeGrunnlag.getGrunnlagsreferanse();
         UUID eksternReferanse = UUID.randomUUID();
 
-        // TODO: Utled datoIntervallEntitet.
-        LocalDateInterval localDateInterval = endretUngdomsprogramTidslinje.toSegments().descendingIterator().next().getLocalDateInterval();
-        DatoIntervallEntitet datoIntervallEntitet = DatoIntervallEntitet.fra(localDateInterval.getFomDato(), localDateInterval.getTomDato());
+        List<Trigger> triggere = prosessTriggereRepository.hentGrunnlag(kontekst.getBehandlingId())
+            .stream()
+            .map(ProsessTriggere::getTriggere)
+            .flatMap(Collection::stream)
+            .toList();
 
-        Etterlysning etterlysning;
-        switch (etterlysningType) {
-            case UTTALELSE_ENDRET_STARTDATO -> {
-                etterlysning = Etterlysning.forEndretStartdatoUttalelse(
-                    kontekst.getBehandlingId(),
-                    grunnlagsreferanse,
-                    eksternReferanse,
-                    datoIntervallEntitet
-                );
-            }
-            case UTTALELSE_ENDRET_SLUTTDATO -> {
-                etterlysning = Etterlysning.forEndretSluttdatoUttalelse(
-                    kontekst.getBehandlingId(),
-                    grunnlagsreferanse,
-                    eksternReferanse,
-                    datoIntervallEntitet
-                );
-            }
-            default -> throw new IllegalArgumentException("Ikke støttet etterlysningstype: " + etterlysningType);
-        }
+        LocalDateTimeline<Trigger> triggereLocalDatetimeline = triggere
+            .stream()
+            .map(trigger -> new LocalDateTimeline(trigger.getPeriode().getFomDato(), trigger.getPeriode().getTomDato(), trigger))
+            .reduce((t1, t2) -> t1.crossJoin(t2, StandardCombinators::union))
+            .orElse(LocalDateTimeline.empty());
 
-        etterlysningRepository.lagre(etterlysning);
+        List<Etterlysning> etterlysninger = triggereLocalDatetimeline.intersection(endretUngdomsprogramTidslinje)
+            .stream()
+            .map(segment -> mapTilEtterlysning(kontekst, segment.getValue(), grunnlagsreferanse, eksternReferanse))
+            .toList();
 
-        var prosessTaskData = ProsessTaskData.forProsessTask(OpprettEtterlysningTask.class);
-        prosessTaskData.setProperty(OpprettEtterlysningTask.ETTERLYSNING_TYPE, etterlysning.getType().getKode());
-        prosessTaskData.setBehandling(kontekst.getFagsakId(), kontekst.getBehandlingId());
-        prosessTaskTjeneste.lagre(prosessTaskData);
+        etterlysninger.stream()
+            .filter(Objects::nonNull)
+            .forEach(etterlysning -> {
+                etterlysningRepository.lagre(etterlysning);
+
+                var prosessTaskData = ProsessTaskData.forProsessTask(OpprettEtterlysningTask.class);
+                prosessTaskData.setProperty(OpprettEtterlysningTask.ETTERLYSNING_TYPE, etterlysning.getType().getKode());
+                prosessTaskData.setBehandling(kontekst.getFagsakId(), kontekst.getBehandlingId());
+                prosessTaskTjeneste.lagre(prosessTaskData);
+            });
+    }
+
+    @Nullable
+    private static Etterlysning mapTilEtterlysning(BehandlingskontrollKontekst kontekst, Trigger trigger, UUID grunnlagsreferanse, UUID eksternReferanse) {
+        BehandlingÅrsakType behandlingÅrsakType = trigger.getÅrsak();
+        DatoIntervallEntitet periode = trigger.getPeriode();
+        final Long behandlingId = kontekst.getBehandlingId();
+
+        return switch (behandlingÅrsakType) {
+            case RE_HENDELSE_ENDRET_STARTDATO_UNGDOMSPROGRAM -> Etterlysning.forEndretStartdatoUttalelse(
+                behandlingId,
+                grunnlagsreferanse,
+                eksternReferanse,
+                periode
+            );
+            case RE_HENDELSE_OPPHØR_UNGDOMSPROGRAM -> Etterlysning.forEndretSluttdatoUttalelse(
+                behandlingId,
+                grunnlagsreferanse,
+                eksternReferanse,
+                periode
+            );
+            case null, default -> null;
+        };
     }
 }
