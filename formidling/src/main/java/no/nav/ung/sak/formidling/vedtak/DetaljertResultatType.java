b05797c7e964188a863742aaa99db325a90e63e6
Qadeer Khan <qadeer.ahmad.khan@nav.no>
2025-04-22 21:46:35 +0200

    detaljert resultat for startdato.

--- formidling/src/main/java/no/nav/ung/sak/formidling/vedtak/DetaljertResultatType.java
+++ formidling/src/main/java/no/nav/ung/sak/formidling/vedtak/DetaljertResultatType.java
@@ -5,6 +5,7 @@ public enum DetaljertResultatType {
     ENDRING_ØKT_SATS("Endring økt sats 25 prosent"),
     ENDRING_RAPPORTERT_INNTEKT("Reduksjon pga inntekt"),
     AVSLAG_RAPPORTERT_INNTEKT("Avslag pga rapportert inntekt"),
+    ENDRING_STARTDATO("Endring av startdato"),
     IKKE_VURDERT("Ikke vurdert"),
     INNVILGELSE_VILKÅR_NY_PERIODE("Innvilgelse av vilkår for ny periode uten utbetaling"),
     INNVILGELSE_UTBETALING_NY_PERIODE("Innvilgelse av ny periode med utbetaling"),
