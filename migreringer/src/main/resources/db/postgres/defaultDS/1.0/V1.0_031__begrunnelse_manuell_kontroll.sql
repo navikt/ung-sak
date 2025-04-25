81e2a85ed5e66e2a1f46b8f644513eb500373482
Espen Velsvik <espen.velsvik@nav.no>
2025-04-25 12:19:58 +0200

    FIX: Bruker riktig navn på db-kolonne

--- migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_031__begrunnelse_manuell_kontroll.sql
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_031__begrunnelse_manuell_kontroll.sql
@@ -1,4 +1,4 @@
 alter table if exists kontrollert_inntekt_periode
-add column if not exists begrunnelse_manuell_kontroll varchar(4000);
+add column if not exists manuelt_vurdert_begrunnelse varchar(4000);
 
-comment on column kontrollert_inntekt_periode.begrunnelse_manuell_kontroll is 'Begrunnelse for valg og inntekt ved manuell vurdering';
+comment on column kontrollert_inntekt_periode.manuelt_vurdert_begrunnelse is 'Begrunnelse for valg og inntekt ved manuell vurdering';
