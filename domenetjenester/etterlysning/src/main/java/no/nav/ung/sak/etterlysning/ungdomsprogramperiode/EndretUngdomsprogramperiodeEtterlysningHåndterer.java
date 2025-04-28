45f8eda844ca08954d172b33b2a6f506068feb53
Espen Velsvik <espenjv@users.noreply.github.com>
2025-04-28 08:22:20 +0200

    TSFF-1156: Mapper forrige periode (#325)

--- domenetjenester/etterlysning/src/main/java/no/nav/ung/sak/etterlysning/ungdomsprogramperiode/EndretUngdomsprogramperiodeEtterlysningHåndterer.java
+++ domenetjenester/etterlysning/src/main/java/no/nav/ung/sak/etterlysning/ungdomsprogramperiode/EndretUngdomsprogramperiodeEtterlysningHåndterer.java
@@ -8,6 +8,10 @@ import no.nav.ung.deltakelseopplyser.kontrakt.oppgave.periodeendring.Programperi
 import no.nav.ung.kodeverk.etterlysning.EtterlysningType;
 import no.nav.ung.sak.behandlingslager.behandling.repository.BehandlingRepository;
 import no.nav.ung.sak.behandlingslager.etterlysning.EtterlysningRepository;
+import no.nav.ung.sak.behandlingslager.perioder.UngdomsprogramPeriode;
+import no.nav.ung.sak.behandlingslager.perioder.UngdomsprogramPeriodeGrunnlag;
+import no.nav.ung.sak.behandlingslager.perioder.UngdomsprogramPeriodeRepository;
+import no.nav.ung.sak.behandlingslager.perioder.UngdomsprogramPerioder;
 import no.nav.ung.sak.domene.person.pdl.PersoninfoAdapter;
 import no.nav.ung.sak.etterlysning.EtterlysningHåndterer;
 import no.nav.ung.sak.etterlysning.UngOppgaveKlient;
@@ -16,6 +20,7 @@ import no.nav.ung.sak.typer.PersonIdent;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.Collection;
 
 @Dependent
 public class EndretUngdomsprogramperiodeEtterlysningHåndterer implements EtterlysningHåndterer {
@@ -25,17 +30,20 @@ public class EndretUngdomsprogramperiodeEtterlysningHåndterer implements Etterl
     private UngOppgaveKlient ungOppgaveKlient;
     private PersoninfoAdapter personinfoAdapter;
     private final Duration ventePeriode;
+    private final UngdomsprogramPeriodeRepository ungdomsprogramPeriodeRepository;
 
     @Inject
     public EndretUngdomsprogramperiodeEtterlysningHåndterer(EtterlysningRepository etterlysningRepository,
                                                             BehandlingRepository behandlingRepository,
                                                             UngOppgaveKlient ungOppgaveKlient, PersoninfoAdapter personinfoAdapter,
-                                                            @KonfigVerdi(value = "REVURDERING_ENDRET_PERIODE_VENTEFRIST", defaultVerdi = "P14D") String ventePeriode) {
+                                                            @KonfigVerdi(value = "REVURDERING_ENDRET_PERIODE_VENTEFRIST", defaultVerdi = "P14D") String ventePeriode,
+                                                            UngdomsprogramPeriodeRepository ungdomsprogramPeriodeRepository) {
         this.etterlysningRepository = etterlysningRepository;
         this.behandlingRepository = behandlingRepository;
         this.ungOppgaveKlient = ungOppgaveKlient;
         this.personinfoAdapter = personinfoAdapter;
         this.ventePeriode = Duration.parse(ventePeriode);
+        this.ungdomsprogramPeriodeRepository = ungdomsprogramPeriodeRepository;
     }
 
     public void håndterOpprettelse(long behandlingId, EtterlysningType etterlysningType) {
@@ -43,18 +51,22 @@ public class EndretUngdomsprogramperiodeEtterlysningHåndterer implements Etterl
         final var etterlysninger = etterlysningRepository.hentOpprettetEtterlysninger(behandlingId, etterlysningType);
         AktørId aktørId = behandling.getAktørId();
         PersonIdent deltakerIdent = personinfoAdapter.hentIdentForAktørId(aktørId).orElseThrow(() -> new IllegalStateException("Fant ikke ident for aktørId"));
-
+        final var originalePerioder = behandling.getOriginalBehandlingId().flatMap(ungdomsprogramPeriodeRepository::hentGrunnlag).stream().map(UngdomsprogramPeriodeGrunnlag::getUngdomsprogramPerioder)
+            .map(UngdomsprogramPerioder::getPerioder)
+            .flatMap(Collection::stream)
+            .map(UngdomsprogramPeriode::getPeriode)
+            .toList();
         etterlysninger.forEach(e -> e.vent(getFrist()));
         final var oppgaveDtoer = etterlysninger.stream().map(etterlysning -> new EndretProgamperiodeOppgaveDTO(
                 deltakerIdent.getIdent(),
                 etterlysning.getEksternReferanse(),
                 etterlysning.getFrist(),
                 new ProgramperiodeDTO(etterlysning.getPeriode().getFomDato(), etterlysning.getPeriode().getTomDato()),
-                null // TODO: Legg til forrige programperiode
+                originalePerioder.stream().filter(p -> p.overlapper(etterlysning.getPeriode())).findFirst().map(it -> new ProgramperiodeDTO(it.getFomDato(), it.getTomDato())).orElse(null)
             )
         ).toList();
 
-       oppgaveDtoer.forEach(ungOppgaveKlient::opprettEndretSluttdatoOppgave);
+        oppgaveDtoer.forEach(ungOppgaveKlient::opprettEndretSluttdatoOppgave);
 
         etterlysningRepository.lagre(etterlysninger);
     }
