3c2beca966bc291c933a507af97b5c714fdb8150
Qadeer Khan <qadeer.ahmad.khan@nav.no>
2025-05-05 13:27:06 +0200

    Brev sanitze html (#337)
    * Renser html før lagring.
    Rydder opp i avhengigheter
    
    * logger hvis tekst fjernes typisk ved manuell tilbakestilling
    
    * flytter kunHtml til dto da det er lettere for frontend.
    * Setter response content-type basert på html flagget i requesten
    
    * fjerner deprekerte felter
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Hallvard Andreas Stark <hallvard@scel.to>

--- kontrakt/src/main/java/no/nav/ung/sak/kontrakt/formidling/vedtaksbrev/VedtaksbrevValgDto.java
+++ kontrakt/src/main/java/no/nav/ung/sak/kontrakt/formidling/vedtaksbrev/VedtaksbrevValgDto.java
@@ -1,19 +1,15 @@
 package no.nav.ung.sak.kontrakt.formidling.vedtaksbrev;
 
 
-import jakarta.validation.Valid;
-
 /**
  * Response objekt - skal ikke sendes inn
  *
  * @param harBrev                   true hvis det finnes brev for behandling
- * @param automatiskBrevOperasjoner satt hvis det skal brukes automatisk brev.
- * @param fritekstbrev              true hvis det skal skrives fritekstbrev. Både fritekstbrev og automatiskbrev kan ikke være true/satt samtidig
  * @param enableHindre              true hvis valg for hindring av brev er relevant og skal vises
  * @param hindret                   true hvis brevet har blitt undertrykket - blir nullstilt ved tilbakehopp
  *                                  <p>
  *                                  hvis en behandling først hadde automatisk brev som ble redigert av saksbehandler, som så får fritekstbrev etter at behandling endret seg så vil
- *                                  automatiskBrevOperasjoner bli null, fritekstbrev bli true, men redigertHtml vil fortsatt innholde den gamle teksten.
+ *                                  redigertHtml fortsatt innholde den gamle teksten.
  *                                  </p>
  * @param kanOverstyreHindre        true hvis det er mulig å overstyre hindring brevet
  * @param enableRediger             true hvis valget for redigering er relevant og skal vises
@@ -24,10 +20,6 @@ import jakarta.validation.Valid;
  */
 public record VedtaksbrevValgDto(
     boolean harBrev,
-    @Valid @Deprecated
-    AutomatiskBrevOperasjoner automatiskBrevOperasjoner,
-    @Deprecated
-    boolean fritekstbrev,
     boolean enableHindre,
     boolean hindret,
     boolean kanOverstyreHindre,
@@ -37,13 +29,6 @@ public record VedtaksbrevValgDto(
     String forklaring,
     String redigertBrevHtml) {
 
-    /**
-     * @param enableRediger true hvis brevet kan redigeres
-     * @param redigert      true hvis brevet er redigert
-     */
-    @Deprecated
-    public record AutomatiskBrevOperasjoner(boolean enableRediger, boolean redigert) {
-    }
 }
 
 
