7060aacd3734b705cdda3a6cdee897a167501de2
Espen Velsvik <espen.velsvik@nav.no>
2025-05-08 12:33:43 +0200

    FIX: Legger tilbake aksjonspunktkode

--- kodeverk/src/main/java/no/nav/ung/kodeverk/behandling/aksjonspunkt/AksjonspunktKodeDefinisjon.java
+++ kodeverk/src/main/java/no/nav/ung/kodeverk/behandling/aksjonspunkt/AksjonspunktKodeDefinisjon.java
@@ -15,7 +15,7 @@ public class AksjonspunktKodeDefinisjon {
 
     public static final String AUTO_MANUELT_SATT_PÅ_VENT_KODE = "7001";
     public static final String AUTO_VENTER_PÅ_KOMPLETT_SØKNAD_KODE = "7003";
-    public static final String AUTO_SATT_PÅ_VENT_REVURDERING_KODE = "";
+    public static final String AUTO_SATT_PÅ_VENT_REVURDERING_KODE = "7005";
     public static final String AUTO_VENT_PÅ_INNTEKT_RAPPORTERINGSFRIST_KODE = "7014";
 
     public static final String AUTO_VENT_ETTERLYST_INNTEKTSMELDING_KODE = "7030";
