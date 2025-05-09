86dc826c384dea296fab0e8967829c15f97f105f
Qadeer Khan <qadeer.ahmad.khan@nav.no>
2025-05-09 14:13:55 +0200

    fjerner ubrukt kode

--- formidling/src/main/java/no/nav/ung/sak/formidling/template/dto/innvilgelse/TilkjentPeriodeDto.java
+++
@@ -1,9 +0,0 @@
-package no.nav.ung.sak.formidling.template.dto.innvilgelse;
-
-import no.nav.ung.sak.formidling.template.dto.felles.PeriodeDto;
-
-public record TilkjentPeriodeDto(
-    PeriodeDto periode,
-    TilkjentSatsDto tilkjentSats
-) {
-}
