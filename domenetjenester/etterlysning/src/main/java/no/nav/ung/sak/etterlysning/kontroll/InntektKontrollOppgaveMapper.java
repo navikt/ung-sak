d98a7be0164f1873b85c17061fe2bef8a1ac30d4
Ramin Esfandiari <ramin.esfandiari@nav.no>
2025-05-12 10:29:42 +0200

    Mapper FagsakYtelsetype til YtelseType for RegisterInntektYtelseDTO
    Signed-off-by: Ramin Esfandiari <ramin.esfandiari@nav.no>

--- domenetjenester/etterlysning/src/main/java/no/nav/ung/sak/etterlysning/kontroll/InntektKontrollOppgaveMapper.java
+++ domenetjenester/etterlysning/src/main/java/no/nav/ung/sak/etterlysning/kontroll/InntektKontrollOppgaveMapper.java
@@ -5,6 +5,7 @@ import no.nav.fpsak.tidsserie.LocalDateTimeline;
 import no.nav.ung.deltakelseopplyser.kontrakt.oppgave.registerinntekt.RegisterInntektArbeidOgFrilansDTO;
 import no.nav.ung.deltakelseopplyser.kontrakt.oppgave.registerinntekt.RegisterInntektDTO;
 import no.nav.ung.deltakelseopplyser.kontrakt.oppgave.registerinntekt.RegisterInntektYtelseDTO;
+import no.nav.ung.deltakelseopplyser.kontrakt.oppgave.registerinntekt.YtelseType;
 import no.nav.ung.kodeverk.arbeidsforhold.InntektspostType;
 import no.nav.ung.sak.domene.iay.modell.InntektArbeidYtelseGrunnlag;
 import no.nav.ung.sak.domene.iay.modell.Inntektspost;
@@ -30,10 +31,30 @@ public class InntektKontrollOppgaveMapper {
             .flatMap(inntekt -> inntekt.getAlleInntektsposter().stream()
                 .filter(ip -> ip.getInntektspostType().equals(InntektspostType.YTELSE))
                 .filter(ip -> ip.getPeriode().overlapper(periode))
-                .map(ip -> {
-                    var beløp = finnBeløpInnenforPeriode(periode, ip);
-                    return new RegisterInntektYtelseDTO(beløp.intValue(), ip.getInntektYtelseType().getYtelseType().getKode());
+                .map(inntektspost -> {
+                    var beløp = finnBeløpInnenforPeriode(periode, inntektspost);
+                    YtelseType ytelseType = maptilYtelseType(inntektspost);
+                    return new RegisterInntektYtelseDTO(beløp.intValue(), ytelseType);
                 })).toList();
+    }
+
+
+    private static YtelseType maptilYtelseType(Inntektspost ip) {
+        switch (ip.getInntektYtelseType().getYtelseType()) {
+            case SYKEPENGER -> {
+                return YtelseType.SYKEPENGER;
+            }
+            case OMSORGSPENGER, OMSORGSPENGER_AO, OMSORGSPENGER_KS, OMSORGSPENGER_MA -> {
+                return YtelseType.OMSORGSPENGER;
+            }
+            case PLEIEPENGER_SYKT_BARN -> {
+                return YtelseType.PLEIEPENGER_SYKT_BARN;
+            }
+            case PLEIEPENGER_NÆRSTÅENDE -> {
+                return YtelseType.PLEIEPENGER_LIVETS_SLUTTFASE;
+            }
+            default -> throw new IllegalStateException("Ikke støttet ytelsetype: " + ip.getInntektYtelseType().getYtelseType());
+        }
     }
 
     private static List<RegisterInntektArbeidOgFrilansDTO> finnArbeidOgFrilansInntekter(InntektArbeidYtelseGrunnlag grunnlag, DatoIntervallEntitet periode) {
