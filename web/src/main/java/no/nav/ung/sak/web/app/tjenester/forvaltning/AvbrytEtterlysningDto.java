94868279a4130947eff6097c3ee907c7e48bdc7e
Espen Velsvik <espen.velsvik@nav.no>
2025-05-21 15:39:01 +0200

    FIX: Legger på pattern annotasjon

--- web/src/main/java/no/nav/ung/sak/web/app/tjenester/forvaltning/AvbrytEtterlysningDto.java
+++ web/src/main/java/no/nav/ung/sak/web/app/tjenester/forvaltning/AvbrytEtterlysningDto.java
@@ -1,8 +1,10 @@
 package no.nav.ung.sak.web.app.tjenester.forvaltning;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record AvbrytEtterlysningDto(@Size(max = 50)
+                                    @Pattern(regexp = "^[\\p{Alnum}-]+$", message = "[${validatedValue}] matcher ikke tillatt pattern [{regexp}]")
                                     @NotNull String etterlysningId) {
 }
