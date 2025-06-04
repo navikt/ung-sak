f2a9eb62c281738fab7dd757e46a1007d124fe46
Jostein Stuhaug <jostein.stuhaug@nav.no>
2025-06-04 08:23:55 +0200

    BehandlingStatus JsonFormat => JsonValue

--- kodeverk/src/main/java/no/nav/ung/kodeverk/behandling/BehandlingStatus.java
+++ kodeverk/src/main/java/no/nav/ung/kodeverk/behandling/BehandlingStatus.java
@@ -1,28 +1,17 @@
 package no.nav.ung.kodeverk.behandling;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+import no.nav.ung.kodeverk.api.Kodeverdi;
+
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
-import com.fasterxml.jackson.annotation.JsonCreator.Mode;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonFormat.Shape;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import no.nav.ung.kodeverk.TempAvledeKode;
-import no.nav.ung.kodeverk.api.Kodeverdi;
-
 /**
  * NB: Pass på! Ikke legg koder vilkårlig her
  * Denne definerer etablerte behandlingstatuser ihht. modell angitt av FFA (Forretning og Fag).
  */
-@JsonFormat(shape = Shape.OBJECT)
-@JsonAutoDetect(getterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE, fieldVisibility = Visibility.ANY)
 public enum BehandlingStatus implements Kodeverdi {
 
     AVSLUTTET("AVSLU", "Avsluttet"),
@@ -39,7 +28,6 @@ public enum BehandlingStatus implements Kodeverdi {
 
     private static final Set<BehandlingStatus> FERDIGBEHANDLET_STATUS = Set.of(AVSLUTTET, IVERKSETTER_VEDTAK);
 
-    @JsonIgnore
     private String navn;
 
     private String kode;
@@ -63,15 +51,13 @@ public enum BehandlingStatus implements Kodeverdi {
         return this.getKode();
     }
 
-    @JsonCreator(mode = Mode.DELEGATING)
-    public static BehandlingStatus fraKode(Object node) {
-        if (node == null) {
+    public static BehandlingStatus fraKode(final String kode) {
+        if (kode == null) {
             return null;
         }
-        String kode = TempAvledeKode.getVerdi(BehandlingStatus.class, node, "kode");
         var ad = KODER.get(kode);
         if (ad == null) {
-            throw new IllegalArgumentException("Ukjent BehandlingStatus: for input " + node);
+            throw new IllegalArgumentException("Ukjent BehandlingStatus: for input " + kode);
         }
         return ad;
     }
@@ -93,13 +79,12 @@ public enum BehandlingStatus implements Kodeverdi {
         return FERDIGBEHANDLET_STATUS.contains(this);
     }
 
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @Override
     public String getKodeverk() {
         return KODEVERK;
     }
 
-    @JsonProperty
+    @JsonValue
     @Override
     public String getKode() {
         return kode;
