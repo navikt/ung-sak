ba56e3323ecd70794a533786fa665e5e7f32de41
Espen Velsvik <espen.velsvik@nav.no>
2025-06-05 08:12:26 +0200

    FIX: Flere tester

--- behandlingslager/domene/src/test/java/no/nav/ung/sak/behandlingslager/behandling/historikk/HistorikkinnslagLinjeBuilderTest.java
+++ behandlingslager/domene/src/test/java/no/nav/ung/sak/behandlingslager/behandling/historikk/HistorikkinnslagLinjeBuilderTest.java
@@ -13,4 +13,19 @@ class HistorikkinnslagLinjeBuilderTest {
         assertEquals("Dette er en \\_test\\_ med \\_underscore\\_", tekst);
     }
 
+    @Test
+    void skal_escape_dobbel_underscore() {
+        var tekst = HistorikkinnslagLinjeBuilder.plainTekstLinje("Dette er en __test__ med __dobbel__ __underscore__").tilTekst();
+
+        assertEquals("Dette er en \\_\\_test\\_\\_ med \\_\\_dobbel\\_\\_ \\_\\_underscore\\_\\_", tekst);
+    }
+
+    @Test
+    void skal_escape_trippel_underscore() {
+        var tekst = HistorikkinnslagLinjeBuilder.plainTekstLinje("Dette er en ___test___ med ___trippel___ ___underscore___").tilTekst();
+
+        assertEquals("Dette er en \\_\\_\\_test\\_\\_\\_ med \\_\\_\\_trippel\\_\\_\\_ \\_\\_\\_underscore\\_\\_\\_", tekst);
+    }
+
+
 }
