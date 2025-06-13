b9c8a45e7fa93086227af3f5df7ab71e966110c8
Espen Velsvik <espenjv@users.noreply.github.com>
2025-06-13 13:50:04 +0200

    FIX: Finner original periode basert på startdato fra søknad om vi ikke har originalbehandling (#477)

--- domenetjenester/etterlysning/src/main/java/no/nav/ung/sak/etterlysning/startdato/EndretStartdatoOppgaveOppretter.java
+++ domenetjenester/etterlysning/src/main/java/no/nav/ung/sak/etterlysning/startdato/EndretStartdatoOppgaveOppretter.java
@@ -4,44 +4,65 @@ import jakarta.enterprise.context.Dependent;
 import jakarta.inject.Inject;
 import no.nav.ung.deltakelseopplyser.kontrakt.oppgave.startdato.EndretStartdatoOppgaveDTO;
 import no.nav.ung.sak.behandlingslager.behandling.Behandling;
+import no.nav.ung.sak.behandlingslager.behandling.startdato.UngdomsytelseStartdatoGrunnlag;
+import no.nav.ung.sak.behandlingslager.behandling.startdato.UngdomsytelseStartdatoRepository;
+import no.nav.ung.sak.behandlingslager.behandling.startdato.UngdomsytelseStartdatoer;
+import no.nav.ung.sak.behandlingslager.behandling.startdato.UngdomsytelseSøktStartdato;
 import no.nav.ung.sak.behandlingslager.etterlysning.Etterlysning;
 import no.nav.ung.sak.behandlingslager.perioder.UngdomsprogramPeriodeGrunnlag;
 import no.nav.ung.sak.behandlingslager.perioder.UngdomsprogramPeriodeRepository;
 import no.nav.ung.sak.domene.typer.tid.DatoIntervallEntitet;
 import no.nav.ung.sak.etterlysning.UngOppgaveKlient;
 import no.nav.ung.sak.typer.PersonIdent;
+import org.jetbrains.annotations.NotNull;
 
 import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Dependent
 public class EndretStartdatoOppgaveOppretter {
 
     private UngOppgaveKlient ungOppgaveKlient;
     private final UngdomsprogramPeriodeRepository ungdomsprogramPeriodeRepository;
+    private final UngdomsytelseStartdatoRepository ungdomsytelseStartdatoRepository;
 
     @Inject
     public EndretStartdatoOppgaveOppretter(UngOppgaveKlient ungOppgaveKlient,
-                                           UngdomsprogramPeriodeRepository ungdomsprogramPeriodeRepository) {
+                                           UngdomsprogramPeriodeRepository ungdomsprogramPeriodeRepository,
+                                           UngdomsytelseStartdatoRepository ungdomsytelseStartdatoRepository) {
         this.ungOppgaveKlient = ungOppgaveKlient;
         this.ungdomsprogramPeriodeRepository = ungdomsprogramPeriodeRepository;
+        this.ungdomsytelseStartdatoRepository = ungdomsytelseStartdatoRepository;
     }
 
 
     public void opprettOppgave(Behandling behandling, List<Etterlysning> etterlysninger, PersonIdent deltakerIdent) {
-        var originalPeriode = behandling.getOriginalBehandlingId().flatMap(ungdomsprogramPeriodeRepository::hentGrunnlag).map(UngdomsprogramPeriodeGrunnlag::hentForEksaktEnPeriode);
+        var originalPeriode = finnOriginalPeriode(behandling);
         var oppgaveDtoer = etterlysninger.stream().map(etterlysning -> mapTilDto(etterlysning, deltakerIdent, originalPeriode)).toList();
         oppgaveDtoer.forEach(ungOppgaveKlient::opprettEndretStartdatoOppgave);
     }
 
-    private EndretStartdatoOppgaveDTO mapTilDto(Etterlysning etterlysning, PersonIdent deltakerIdent, Optional<DatoIntervallEntitet> originalPeriode) {
+    private DatoIntervallEntitet finnOriginalPeriode(Behandling behandling) {
+        var startdato = ungdomsytelseStartdatoRepository.hentGrunnlag(behandling.getId()).map(UngdomsytelseStartdatoGrunnlag::getOppgitteStartdatoer).map(UngdomsytelseStartdatoer::getStartdatoer)
+            .stream()
+            .flatMap(Set::stream)
+            .map(UngdomsytelseSøktStartdato::getStartdato)
+            .min(Comparator.naturalOrder())
+            .orElseThrow(() -> new IllegalStateException("Forventer å finne startdato for behandling med id: " + behandling.getId()));
+        return behandling.getOriginalBehandlingId().flatMap(ungdomsprogramPeriodeRepository::hentGrunnlag).map(UngdomsprogramPeriodeGrunnlag::hentForEksaktEnPeriode)
+            .orElse(DatoIntervallEntitet.fraOgMed(startdato));
+    }
+
+    private EndretStartdatoOppgaveDTO mapTilDto(Etterlysning etterlysning, PersonIdent deltakerIdent, DatoIntervallEntitet originalPeriode) {
         return new EndretStartdatoOppgaveDTO(
             deltakerIdent.getIdent(),
             etterlysning.getEksternReferanse(),
             etterlysning.getFrist(),
             hentStartdato(etterlysning),
-            originalPeriode.map(DatoIntervallEntitet::getFomDato).orElseThrow((() -> new IllegalStateException("Forventer å finne original startdato")))
+            originalPeriode.getFomDato()
         );
     }
 
