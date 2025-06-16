eae2bd7b300e3b59c09f4f7ab4d5245f0cc8d642
Ramin Esfandiari <ramin.esfandiari@nav.no>
2025-06-16 15:04:00 +0200

    Flipper kolonne verdi.
    Signed-off-by: Ramin Esfandiari <ramin.esfandiari@nav.no>

--- migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_043__uttalelse_entitet_har_uttalelse.sql
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_043__uttalelse_entitet_har_uttalelse.sql
@@ -1,2 +1,6 @@
 ALTER TABLE IF EXISTS uttalelse
     RENAME COLUMN har_godtatt_endringen TO har_uttalelse;
+
+UPDATE uttalelse
+SET har_uttalelse = NOT har_uttalelse
+WHERE har_uttalelse IS NOT NULL;
