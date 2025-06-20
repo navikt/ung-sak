0e68725fb3ed247b58b16235cc190cbf56c59c67
Espen Velsvik <espenjv@users.noreply.github.com>
2025-06-20 15:05:10 +0200

    TSFF-1722: Utleder status på utbetaling (#514)
    * TSFF-1722: Viser status for utbetaling basert på dagens dato og antatt utbetalingsdato
    
    * TSFF-1722: Viser status for utbetaling basert på dagens dato og antatt utbetalingsdato, kombinerer tidslinjer fra tidligere behandlinger
    
    * TSFF-1722: Legger på kommentarer
    
    * TSFF-1722: Fjerner ubrukt metode
    
    ---------
    
    Co-authored-by: Ramin Esfandiari <ramin.esfandiari@nav.no>

--- web/src/test/java/no/nav/ung/sak/web/app/ungdomsytelse/MånedsvisningDtoMapperTest.java
+++ web/src/test/java/no/nav/ung/sak/web/app/ungdomsytelse/MånedsvisningDtoMapperTest.java
@@ -13,9 +13,11 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.YearMonth;
 import java.time.temporal.TemporalAdjusters;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -35,20 +37,21 @@ class MånedsvisningDtoMapperTest {
         final var antallBarn = 1;
         final var satser = new UngdomsytelseSatser(dagsats, BigDecimal.valueOf(100_000), BigDecimal.valueOf(2), UngdomsytelseSatsType.HØY, antallBarn, dagsatsBarnetillegg);
 
-        final var månedstidslinje = new LocalDateTimeline<YearMonth>(fom, tom, YearMonth.of(2025, 1));
-        final var tilkjentYtelseTidslinje = new LocalDateTimeline<TilkjentYtelseVerdi>(fom, tom, new TilkjentYtelseVerdi(uredusert, reduksjon, redusert, dagsats, 100));
+        final var månedstidslinje = new LocalDateTimeline<>(fom, tom, YearMonth.of(2025, 1));
+        final var tilkjentYtelseTidslinje = new LocalDateTimeline<>(fom, tom, new TilkjentYtelseVerdi(uredusert, reduksjon, redusert, dagsats, 100));
         final var kontrollTidslinje = new LocalDateTimeline<BigDecimal>(List.of());
         final var satsperioder = new UngdomsytelseSatsPerioder(List.of(new UngdomsytelseSatsPeriode(new LocalDateInterval(fom, tom), satser)), "", "");
-        final var originalTilkjentYtelse = new LocalDateTimeline<TilkjentYtelseVerdi>(List.of());
+        var avsluttetTid = LocalDateTime.now();
+        var aktuellAvsluttetTid = new BehandlingAvsluttetTidspunkt(avsluttetTid);
 
         // Act
         final var månedsvisningDto = MånedsvisningDtoMapper.mapSatsOgUtbetalingPrMåned(
+            aktuellAvsluttetTid,
             månedstidslinje,
             tilkjentYtelseTidslinje,
             kontrollTidslinje,
             satsperioder,
-            originalTilkjentYtelse
-        );
+            Map.of(aktuellAvsluttetTid, tilkjentYtelseTidslinje));
 
         // Assert
         assertThat(månedsvisningDto.size()).isEqualTo(1);
@@ -60,7 +63,7 @@ class MånedsvisningDtoMapperTest {
         assertThat(månedDto.rapportertInntekt()).isNull();
         assertThat(månedDto.utbetaling()).isEqualByComparingTo(redusert);
         assertThat(månedDto.reduksjon()).isEqualByComparingTo(reduksjon);
-        assertThat(månedDto.status()).isEqualTo(UtbetalingStatus.TIL_UTBETALING);
+        assertThat(månedDto.status()).isEqualTo(UtbetalingStatus.UTBETALT);
         assertThat(månedDto.satsperioder().size()).isEqualTo(1);
         final var satsPeriode = månedDto.satsperioder().getFirst();
 
@@ -77,38 +80,38 @@ class MånedsvisningDtoMapperTest {
     @Test
     void skal_mappe_to_måneder_med_kontroll_uten_inntekt_i_revurdering() {
         // Assert
-        final var fom = LocalDate.of(2025, 1, 15);
-        final var tom = fom.plusWeeks(51).minusDays(1);
+        var fom = LocalDate.of(2025, 1, 15);
+        var tom = fom.plusWeeks(51).minusDays(1);
 
-        final var uredusert = BigDecimal.TEN;
-        final var reduksjon = BigDecimal.ZERO;
-        final var redusert = BigDecimal.TEN;
-        final var dagsats = BigDecimal.ONE;
-        final var dagsatsBarnetillegg = 37;
-        final var antallBarn = 1;
-        final var satser = new UngdomsytelseSatser(dagsats, BigDecimal.valueOf(100_000), BigDecimal.valueOf(2), UngdomsytelseSatsType.HØY, antallBarn, dagsatsBarnetillegg);
+        var uredusert = BigDecimal.TEN;
+        var reduksjon = BigDecimal.ZERO;
+        var redusert = BigDecimal.TEN;
+        var dagsats = BigDecimal.ONE;
+        var dagsatsBarnetillegg = 37;
+        var antallBarn = 1;
+        var satser = new UngdomsytelseSatser(dagsats, BigDecimal.valueOf(100_000), BigDecimal.valueOf(2), UngdomsytelseSatsType.HØY, antallBarn, dagsatsBarnetillegg);
 
         var fomMåned1 = fom;
         var tomMåned1 = fom.withDayOfMonth(fom.lengthOfMonth());
         var fomMåned2 = tomMåned1.plusDays(1);
         var tomMåned2 = fomMåned2.with(TemporalAdjusters.lastDayOfMonth());
-        final var månedstidslinje = new LocalDateTimeline<YearMonth>(List.of(new LocalDateSegment<>(fomMåned1, tomMåned1, YearMonth.of(2025, 1)), new LocalDateSegment<>(fomMåned2, tomMåned2, YearMonth.of(2025, 2))));
-        final var tilkjentYtelseTidslinje = new LocalDateTimeline<TilkjentYtelseVerdi>(List.of(
+        final var månedstidslinje = new LocalDateTimeline<>(List.of(new LocalDateSegment<>(fomMåned1, tomMåned1, YearMonth.of(2025, 1)), new LocalDateSegment<>(fomMåned2, tomMåned2, YearMonth.of(2025, 2))));
+        final var tilkjentYtelseTidslinje = new LocalDateTimeline<>(List.of(
             new LocalDateSegment<>(fomMåned1, tomMåned1, new TilkjentYtelseVerdi(uredusert, reduksjon, redusert, dagsats, 100)),
             new LocalDateSegment<>(fomMåned2, tomMåned2, new TilkjentYtelseVerdi(uredusert, reduksjon, redusert, dagsats, 100))));
-        final var kontrollTidslinje = new LocalDateTimeline<BigDecimal>(fomMåned2, tomMåned2, BigDecimal.ZERO);
+        final var kontrollTidslinje = new LocalDateTimeline<>(fomMåned2, tomMåned2, BigDecimal.ZERO);
         final var satsperioder = new UngdomsytelseSatsPerioder(List.of(new UngdomsytelseSatsPeriode(new LocalDateInterval(fom, tom), satser)), "", "");
-        final var originalTilkjentYtelse = new LocalDateTimeline<TilkjentYtelseVerdi>(List.of(
-            new LocalDateSegment<>(fomMåned1, tomMåned1, new TilkjentYtelseVerdi(uredusert, reduksjon, redusert, dagsats, 100))));
+        var avsluttetTid = LocalDateTime.now();
+        var aktuellAvsluttetTid = new BehandlingAvsluttetTidspunkt(avsluttetTid);
 
         // Act
         final var månedsvisningDto = MånedsvisningDtoMapper.mapSatsOgUtbetalingPrMåned(
+            aktuellAvsluttetTid,
             månedstidslinje,
             tilkjentYtelseTidslinje,
             kontrollTidslinje,
             satsperioder,
-            originalTilkjentYtelse
-        );
+            Map.of(aktuellAvsluttetTid, tilkjentYtelseTidslinje));
 
         // Assert
         assertThat(månedsvisningDto.size()).isEqualTo(2);
@@ -140,7 +143,7 @@ class MånedsvisningDtoMapperTest {
         assertThat(månedDto2.rapportertInntekt()).isEqualByComparingTo(BigDecimal.ZERO);
         assertThat(månedDto2.utbetaling()).isEqualByComparingTo(redusert);
         assertThat(månedDto2.reduksjon()).isEqualByComparingTo(reduksjon);
-        assertThat(månedDto2.status()).isEqualTo(UtbetalingStatus.TIL_UTBETALING);
+        assertThat(månedDto2.status()).isEqualTo(UtbetalingStatus.UTBETALT);
         assertThat(månedDto2.satsperioder().size()).isEqualTo(1);
         final var satsPeriodeMåned2 = månedDto2.satsperioder().getFirst();
 
@@ -160,16 +163,16 @@ class MånedsvisningDtoMapperTest {
         final var fom = LocalDate.of(2025, 1, 15);
         final var tom = fom.plusWeeks(51).minusDays(1);
 
-        final var uredusert = BigDecimal.TEN;
-        final var reduksjon = BigDecimal.ZERO;
-        final var redusert = BigDecimal.TEN;
-        final var dagsats = BigDecimal.ONE;
-        final var dagsatsBarnetillegg = 37;
-        final var antallBarn = 1;
-        final var satser = new UngdomsytelseSatser(dagsats, BigDecimal.valueOf(100_000), BigDecimal.valueOf(2), UngdomsytelseSatsType.HØY, antallBarn, dagsatsBarnetillegg);
-        final var dagsatsBarnetillegg2 = 74;
-        final var antallBarn2 = 2;
-        final var satser2 = new UngdomsytelseSatser(dagsats, BigDecimal.valueOf(100_000), BigDecimal.valueOf(2), UngdomsytelseSatsType.HØY, antallBarn2, dagsatsBarnetillegg2);
+        var uredusert = BigDecimal.TEN;
+        var reduksjon = BigDecimal.ZERO;
+        var redusert = BigDecimal.TEN;
+        var dagsats = BigDecimal.ONE;
+        var dagsatsBarnetillegg = 37;
+        var antallBarn = 1;
+        var satser = new UngdomsytelseSatser(dagsats, BigDecimal.valueOf(100_000), BigDecimal.valueOf(2), UngdomsytelseSatsType.HØY, antallBarn, dagsatsBarnetillegg);
+        var dagsatsBarnetillegg2 = 74;
+        var antallBarn2 = 2;
+        var satser2 = new UngdomsytelseSatser(dagsats, BigDecimal.valueOf(100_000), BigDecimal.valueOf(2), UngdomsytelseSatsType.HØY, antallBarn2, dagsatsBarnetillegg2);
 
         var fomMåned1 = fom;
         var tomMåned1 = fom.withDayOfMonth(fom.lengthOfMonth());
@@ -188,17 +191,18 @@ class MånedsvisningDtoMapperTest {
             new UngdomsytelseSatsPeriode(new LocalDateInterval(fom, fomMåned2.plusDays(10)), satser),
             new UngdomsytelseSatsPeriode(new LocalDateInterval(satsendringDato, tom), satser2)),
             "", "");
-        final var originalTilkjentYtelse = new LocalDateTimeline<TilkjentYtelseVerdi>(List.of(
-            new LocalDateSegment<>(fomMåned1, tomMåned1, new TilkjentYtelseVerdi(uredusert, reduksjon, redusert, dagsats, 100))));
+        var avsluttetTid = LocalDateTime.now();
+        var aktuellAvsluttetTid = new BehandlingAvsluttetTidspunkt(avsluttetTid);
+
 
         // Act
         final var månedsvisningDto = MånedsvisningDtoMapper.mapSatsOgUtbetalingPrMåned(
+            aktuellAvsluttetTid,
             månedstidslinje,
             tilkjentYtelseTidslinje,
             kontrollTidslinje,
             satsperioder,
-            originalTilkjentYtelse
-        );
+            Map.of(aktuellAvsluttetTid, tilkjentYtelseTidslinje));
 
         // Assert
         assertThat(månedsvisningDto.size()).isEqualTo(2);
@@ -230,7 +234,7 @@ class MånedsvisningDtoMapperTest {
         assertThat(månedDto2.rapportertInntekt()).isEqualByComparingTo(BigDecimal.ZERO);
         assertThat(månedDto2.utbetaling()).isEqualByComparingTo(redusert);
         assertThat(månedDto2.reduksjon()).isEqualByComparingTo(reduksjon);
-        assertThat(månedDto2.status()).isEqualTo(UtbetalingStatus.TIL_UTBETALING);
+        assertThat(månedDto2.status()).isEqualTo(UtbetalingStatus.UTBETALT);
         assertThat(månedDto2.satsperioder().size()).isEqualTo(2);
         final var satsPeriode1Måned2 = månedDto2.satsperioder().get(0);
 
