e33ce84ef7ffb1143e8512bae81bebbbbf08e023
Espen Velsvik <espen.velsvik@nav.no>
2025-06-24 14:47:42 +0200

    TSFF-1615: Fjerner default

--- migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_046__tilkjent_ytelse_avrunding_feil.sql
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_046__tilkjent_ytelse_avrunding_feil.sql
@@ -1,4 +1,4 @@
 alter table tilkjent_ytelse_periode
-    add column avvik_avrunding numeric not null default 0;
+    add column avvik_avrunding numeric not null;
 
 comment on column tilkjent_ytelse_periode.avvik_avrunding is 'Avvik grunnet avrunding til dagsats i hele kroner for perioden.';
