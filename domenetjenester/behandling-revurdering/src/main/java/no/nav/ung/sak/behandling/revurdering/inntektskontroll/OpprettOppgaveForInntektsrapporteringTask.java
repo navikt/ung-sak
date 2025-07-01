e9f3e93cabcc4cfbe15e1e587746371a9068f6a2
Espen Velsvik <espen.velsvik@nav.no>
2025-07-01 09:42:25 +0200

    Fjerner sjekk på saksnummer

--- domenetjenester/behandling-revurdering/src/main/java/no/nav/ung/sak/behandling/revurdering/inntektskontroll/OpprettOppgaveForInntektsrapporteringTask.java
+++ domenetjenester/behandling-revurdering/src/main/java/no/nav/ung/sak/behandling/revurdering/inntektskontroll/OpprettOppgaveForInntektsrapporteringTask.java
@@ -2,22 +2,16 @@ package no.nav.ung.sak.behandling.revurdering.inntektskontroll;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import no.nav.k9.felles.exception.IntegrasjonException;
-import no.nav.k9.felles.feil.Feil;
 import no.nav.k9.felles.konfigurasjon.konfig.KonfigVerdi;
 import no.nav.k9.prosesstask.api.ProsessTask;
 import no.nav.k9.prosesstask.api.ProsessTaskData;
-import no.nav.k9.prosesstask.api.ProsessTaskHandler;
 import no.nav.ung.deltakelseopplyser.kontrakt.oppgave.inntektsrapportering.InntektsrapporteringOppgaveDTO;
 import no.nav.ung.sak.behandlingslager.task.FagsakProsessTask;
 import no.nav.ung.sak.domene.person.pdl.PersoninfoAdapter;
 import no.nav.ung.sak.etterlysning.UngOppgaveKlient;
-import no.nav.ung.sak.etterlysning.UngOppgavetjenesteFeil;
-import no.nav.ung.sak.kontrakt.FeilDto;
 import no.nav.ung.sak.typer.AktørId;
 import no.nav.ung.sak.typer.Periode;
 import no.nav.ung.sak.typer.PersonIdent;
-import no.nav.ung.sak.typer.Saksnummer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -64,9 +58,7 @@ public class OpprettOppgaveForInntektsrapporteringTask extends FagsakProsessTask
         final var aktørId = new AktørId(prosessTaskData.getAktørId());
         final var fom = LocalDate.parse(prosessTaskData.getPropertyValue(PERIODE_FOM), DateTimeFormatter.ISO_LOCAL_DATE);
         final var tom = LocalDate.parse(prosessTaskData.getPropertyValue(PERIODE_TOM), DateTimeFormatter.ISO_LOCAL_DATE);
-        if (prosessTaskData.getSaksnummer() != null) {
-            LOG.info("Oppretter oppgave for inntektsrapportering for aktør knyttet til saksnummer={} for periode={}", prosessTaskData.getSaksnummer(), new Periode(fom,tom));
-        }
+        LOG.info("Oppretter oppgave for inntektsrapportering for periode={}", new Periode(fom, tom));
         PersonIdent deltakerIdent = personinfoAdapter.hentIdentForAktørId(aktørId).orElseThrow(() -> new IllegalStateException("Fant ikke ident for aktørId"));
         ungOppgaveKlient.opprettInntektrapporteringOppgave(new InntektsrapporteringOppgaveDTO(
             deltakerIdent.getIdent(),
@@ -83,7 +75,7 @@ public class OpprettOppgaveForInntektsrapporteringTask extends FagsakProsessTask
         final var fom = LocalDate.parse(prosessTaskData.getPropertyValue(PERIODE_FOM), DateTimeFormatter.ISO_LOCAL_DATE);
         final var tom = LocalDate.parse(prosessTaskData.getPropertyValue(PERIODE_TOM), DateTimeFormatter.ISO_LOCAL_DATE);
         if (prosessTaskData.getSaksnummer() != null) {
-            LOG.info("Oppretter oppgave for inntektsrapportering for aktør knyttet til saksnummer={} for periode={}", prosessTaskData.getSaksnummer(), new Periode(fom,tom));
+            LOG.info("Oppretter oppgave for inntektsrapportering for aktør knyttet til saksnummer={} for periode={}", prosessTaskData.getSaksnummer(), new Periode(fom, tom));
         }
         PersonIdent deltakerIdent = personinfoAdapter.hentIdentForAktørId(aktørId).orElseThrow(() -> new IllegalStateException("Fant ikke ident for aktørId"));
         ungOppgaveKlient.opprettInntektrapporteringOppgave(new InntektsrapporteringOppgaveDTO(
