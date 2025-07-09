aae53490ccf2c252d2b22d5ae73218751217f37a
Ramin Esfandiari <ramin.esfandiari@nav.no>
2025-07-09 14:08:27 +0200

    Fikser extractClass
    Signed-off-by: Ramin Esfandiari <ramin.esfandiari@nav.no>

--- domenetjenester/datavarehus/src/main/java/no/nav/ung/sak/metrikker/bigquery/BigQueryStatistikkRepository.java
+++ domenetjenester/datavarehus/src/main/java/no/nav/ung/sak/metrikker/bigquery/BigQueryStatistikkRepository.java
@@ -9,6 +9,7 @@ import no.nav.k9.felles.util.Tuple;
 import no.nav.k9.prosesstask.api.ProsessTask;
 import no.nav.k9.prosesstask.api.ProsessTaskHandler;
 import no.nav.ung.kodeverk.behandling.FagsakYtelseType;
+import org.jboss.weld.interceptor.util.proxy.TargetInstanceProxy;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -40,8 +41,12 @@ public class BigQueryStatistikkRepository {
                 .collect(Collectors.toSet());
     }
 
-    private Class<?> extractClass(ProsessTaskHandler handler) {
-        return handler.getClass();
+    private Class<?> extractClass(ProsessTaskHandler bean) {
+        if (!bean.getClass().isAnnotationPresent(ProsessTask.class) && bean instanceof TargetInstanceProxy<?> tip) {
+            return tip.weld_getTargetInstance().getClass();
+        } else {
+            return bean.getClass();
+        }
     }
 
     public Map<BigQueryTable, JSONObject> hentHyppigRapporterte() {
