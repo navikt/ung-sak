0edb9c760a49c8f6be770b62b69bf608d71c8ed3
Espen Velsvik <espen.velsvik@nav.no>
2025-08-27 08:55:56 +0200

    Legger til taskgruppe

--- migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_051__oppretter_crontask_for_daglig_bigquery_metrikk_task.sql
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_051__oppretter_crontask_for_daglig_bigquery_metrikk_task.sql
@@ -1,3 +1,3 @@
-insert into prosess_task (id, task_type)
-select nextval('SEQ_PROSESS_TASK'), 'bigquery.metrikk.daglig.task'
+insert into prosess_task (id, task_type, task_gruppe)
+select nextval('SEQ_PROSESS_TASK'), 'bigquery.metrikk.daglig.task', nextval('SEQ_PROSESS_TASK_GRUPPE')
 where not exists (select 1 from prosess_task where task_type = 'bigquery.metrikk.daglig.task'  and status IN ('KLAR', 'FEILET'));
