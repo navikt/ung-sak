dcf9c28e034476e603775e21eb9513bc2677dcfe
Qadeer Khan <qadeer.ahmad.khan@nav.no>
2025-09-08 12:30:17 +0200

    test for periode i mellom uten reduksjon pga rapportert inntekt

--- formidling/src/test/java/no/nav/ung/sak/formidling/EndringRapportertInntektTest.java
+++ formidling/src/test/java/no/nav/ung/sak/formidling/EndringRapportertInntektTest.java
@@ -8,6 +8,7 @@ import no.nav.ung.sak.formidling.scenarioer.EndringInntektScenarioer;
 import no.nav.ung.sak.test.util.UngTestRepositories;
 import no.nav.ung.sak.test.util.behandling.TestScenarioBuilder;
 import no.nav.ung.sak.test.util.behandling.UngTestScenario;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -31,12 +32,7 @@ class EndringRapportertInntektTest extends AbstractVedtaksbrevInnholdByggerTest 
             "Vi har endret ungdomsprogramytelsen din " +
                 "Du får 8 326 kroner i ungdomsprogramytelse for perioden fra 1. januar 2025 til 31. januar 2025. " +
                 "Det er fordi du har hatt en inntekt på 10 000 kroner i denne perioden. " +
-                "Pengene får du utbetalt før den 10. denne måneden. " +
-                "Når du har en inntekt, får du mindre penger i ungdomsprogramytelse. " +
-                "Vi regner ut hva 66 prosent av inntekten din er hver måned, og så trekker vi dette beløpet fra pengene du får i ungdomsprogramytelsen for den måneden. " +
-                "Likevel får du til sammen mer penger når du både har en inntekt og får ungdomsprogramytelse, enn hvis du bare hadde fått penger gjennom ungdomsprogramytelsen. " +
-                "Se eksempel på hvordan vi regner ut ungdomsprogramytelsen basert på inntekt i Ungdomsportalen. " +
-                "Vedtaket er gjort etter arbeidsmarkedsloven §§ 12 tredje ledd og 13 fjerde ledd og forskrift om forsøk med ungdomsprogram og ungdomsprogramytelse § 8 jf. § 11. ");
+                standardTekstEndringInntekt());
 
         var behandling = lagScenario(ungTestGrunnlag);
 
@@ -64,12 +60,7 @@ class EndringRapportertInntektTest extends AbstractVedtaksbrevInnholdByggerTest 
                 "Det er fordi du har hatt en inntekt på 10 000 kroner i denne perioden. " +
                 "Du får 6 380 kroner i ungdomsprogramytelse for perioden fra 1. februar 2025 til 28. februar 2025. " +
                 "Det er fordi du har hatt en inntekt på 10 000 kroner i denne perioden. " +
-                "Pengene får du utbetalt før den 10. denne måneden. " +
-                "Når du har en inntekt, får du mindre penger i ungdomsprogramytelse. " +
-                "Vi regner ut hva 66 prosent av inntekten din er hver måned, og så trekker vi dette beløpet fra pengene du får i ungdomsprogramytelsen for den måneden. " +
-                "Likevel får du til sammen mer penger når du både har en inntekt og får ungdomsprogramytelse, enn hvis du bare hadde fått penger gjennom ungdomsprogramytelsen. " +
-                "Se eksempel på hvordan vi regner ut ungdomsprogramytelsen basert på inntekt i Ungdomsportalen. " +
-                "Vedtaket er gjort etter arbeidsmarkedsloven §§ 12 tredje ledd og 13 fjerde ledd og forskrift om forsøk med ungdomsprogram og ungdomsprogramytelse § 8 jf. § 11. ");
+                standardTekstEndringInntekt());
 
         var behandling = lagScenario(ungTestGrunnlag);
 
@@ -83,6 +74,45 @@ class EndringRapportertInntektTest extends AbstractVedtaksbrevInnholdByggerTest 
             .containsHtmlSubSequenceOnce(
                 "<h1>Vi har endret ungdomsprogramytelsen din</h1>"
             );
+    }
+
+    @DisplayName("Endringsbrev med flere perioder med ingen reduksjon i midten")
+    @Test
+    void melder_inntekt_for_flere_mnd_med_0_inntekt_i_midten() {
+        LocalDate fom = LocalDate.of(2024, 12, 1);
+        var ungTestGrunnlag = EndringInntektScenarioer.endringMedInntektPå10k_utenom_mnd_2(fom);
+        var forventet = VedtaksbrevVerifikasjon.medHeaderOgFooter(fnr,
+            "Vi har endret ungdomsprogramytelsen din " +
+                "Du får 8 326 kroner i ungdomsprogramytelse for perioden fra 1. januar 2025 til 31. januar 2025. " +
+                "Det er fordi du har hatt en inntekt på 10 000 kroner i denne perioden. " +
+                "Du får 7 035 kroner i ungdomsprogramytelse for perioden fra 1. mars 2025 til 31. mars 2025. " +
+                "Det er fordi du har hatt en inntekt på 10 000 kroner i denne perioden. " +
+                standardTekstEndringInntekt());
+
+        var behandling = lagScenario(ungTestGrunnlag);
+
+        GenerertBrev generertBrev = genererVedtaksbrev(behandling.getId());
+        assertThat(generertBrev.templateType()).isEqualTo(TemplateType.ENDRING_INNTEKT);
+
+        var brevtekst = generertBrev.dokument().html();
+
+        assertThatHtml(brevtekst)
+            .asPlainTextIsEqualTo(forventet)
+            .containsHtmlSubSequenceOnce(
+                "<h1>Vi har endret ungdomsprogramytelsen din</h1>"
+            );
+    }
+
+    @NotNull
+    private static String standardTekstEndringInntekt() {
+        return """
+            Pengene får du utbetalt før den 10. denne måneden. \
+            Når du har en inntekt, får du mindre penger i ungdomsprogramytelse. \
+            Vi regner ut hva 66 prosent av inntekten din er hver måned, og så trekker vi dette beløpet fra pengene du får i ungdomsprogramytelsen for den måneden. \
+            Likevel får du til sammen mer penger når du både har en inntekt og får ungdomsprogramytelse, enn hvis du bare hadde fått penger gjennom ungdomsprogramytelsen. \
+            Se eksempel på hvordan vi regner ut ungdomsprogramytelsen basert på inntekt i Ungdomsportalen. \
+            Vedtaket er gjort etter arbeidsmarkedsloven §§ 12 tredje ledd og 13 fjerde ledd og forskrift om forsøk med ungdomsprogram og ungdomsprogramytelse § 8 jf. § 11. \
+            """;
     }
 
 
