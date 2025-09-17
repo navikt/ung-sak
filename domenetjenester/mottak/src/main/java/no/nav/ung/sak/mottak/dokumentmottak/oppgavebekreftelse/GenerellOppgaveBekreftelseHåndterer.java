8eedb24b7bad4a4a584fbd1f34d65ba79548975d
Eirik Fløtre Rørstad <eirik.flotre.rorstad@nav.no>
2025-09-17 11:51:58 +0200

    Legg til lagring av uttalelse i ny struktur

--- domenetjenester/mottak/src/main/java/no/nav/ung/sak/mottak/dokumentmottak/oppgavebekreftelse/GenerellOppgaveBekreftelseHåndterer.java
+++ domenetjenester/mottak/src/main/java/no/nav/ung/sak/mottak/dokumentmottak/oppgavebekreftelse/GenerellOppgaveBekreftelseHåndterer.java
@@ -5,11 +5,16 @@ import jakarta.inject.Inject;
 import no.nav.k9.oppgave.bekreftelse.Bekreftelse;
 import no.nav.ung.kodeverk.behandling.BehandlingÅrsakType;
 import no.nav.ung.kodeverk.dokument.DokumentStatus;
+import no.nav.ung.kodeverk.varsel.EndringType;
 import no.nav.ung.kodeverk.varsel.EtterlysningStatus;
+import no.nav.ung.kodeverk.varsel.EtterlysningType;
 import no.nav.ung.sak.behandlingslager.behandling.motattdokument.MottatteDokumentRepository;
 import no.nav.ung.sak.behandlingslager.etterlysning.Etterlysning;
 import no.nav.ung.sak.behandlingslager.etterlysning.EtterlysningRepository;
+import no.nav.ung.sak.behandlingslager.uttalelse.UttalelseRepository;
+import no.nav.ung.sak.behandlingslager.uttalelse.UttalelseV2;
 import no.nav.ung.sak.mottak.dokumentmottak.Trigger;
+import no.nav.ung.sak.registerendringer.Endringstype;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,11 +32,13 @@ public class GenerellOppgaveBekreftelseHåndterer implements BekreftelseHåndter
     private static final Logger log = LoggerFactory.getLogger(GenerellOppgaveBekreftelseHåndterer.class);
     private final MottatteDokumentRepository mottatteDokumentRepository;
     private final EtterlysningRepository etterlysningRepository;
+    private final UttalelseRepository uttalelseRepository;
 
     @Inject
-    public GenerellOppgaveBekreftelseHåndterer(MottatteDokumentRepository mottatteDokumentRepository, EtterlysningRepository etterlysningRepository) {
+    public GenerellOppgaveBekreftelseHåndterer(MottatteDokumentRepository mottatteDokumentRepository, EtterlysningRepository etterlysningRepository, UttalelseRepository uttalelseRepository) {
         this.mottatteDokumentRepository = mottatteDokumentRepository;
         this.etterlysningRepository = etterlysningRepository;
+        this.uttalelseRepository = uttalelseRepository;
     }
 
     @Override
@@ -60,6 +67,17 @@ public class GenerellOppgaveBekreftelseHåndterer implements BekreftelseHåndter
                 bekreftelse.getUttalelseFraBruker()
             );
             etterlysningRepository.lagre(etterlysning);
+
+            UttalelseV2 nyUttalelse = new UttalelseV2(
+                bekreftelse.harUttalelse(),
+                bekreftelse.getUttalelseFraBruker(),
+                etterlysning.getPeriode(),
+                oppgaveBekreftelse.mottattDokument().getJournalpostId(),
+                mapTilEndringsType(etterlysning.getType()),
+                etterlysning.getGrunnlagsreferanse()
+            );
+            uttalelseRepository.lagre(etterlysning.getBehandlingId(), List.of(nyUttalelse));
+
         }
 
         mottatteDokumentRepository.oppdaterStatus(List.of(oppgaveBekreftelse.mottattDokument()), DokumentStatus.GYLDIG);
@@ -70,6 +88,14 @@ public class GenerellOppgaveBekreftelseHåndterer implements BekreftelseHåndter
     public Optional<Trigger> utledTrigger(UUID oppgaveId) {
         Etterlysning etterlysning = etterlysningRepository.hentEtterlysningForEksternReferanse(oppgaveId);
         return Optional.of(new Trigger(etterlysning.getPeriode(), BehandlingÅrsakType.UTTALELSE_FRA_BRUKER));
+    }
+
+    private EndringType mapTilEndringsType(EtterlysningType etterlysningType) {
+        return switch (etterlysningType) {
+            case UTTALELSE_KONTROLL_INNTEKT -> EndringType.ENDRET_INNTEKT;
+            case UTTALELSE_ENDRET_STARTDATO -> EndringType.ENDRET_STARTDATO;
+            case UTTALELSE_ENDRET_SLUTTDATO -> EndringType.ENDRET_SLUTTDATO;
+        };
     }
 
 }
