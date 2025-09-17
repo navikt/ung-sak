b75976554db2decadcbc69700042d8ed08509ead
EdemEdem <62141992+EdemEdem@users.noreply.github.com>
2025-09-17 10:15:13 +0200

    Opprett forhåndsvarsel grunnlag (#686)
    * La til ForhåndsvarselGrunnlag.java, Forhåndsvarsler.java og ForhåndsvarslerRepository.java
    
    * Legg på annoteringer til ForhåndsVarselGrunnlag og Forhåndsvarsler
    
    * Operetta UttalelseV2, Uttalelser, UttalelseGrunnlag, og EndringType. Lagde migrerings fil for uttalelser.
    Endra navn på etterlysning kodeverket til varsel.
    
    * ung-sak.openapi.json updated by build pipeline 
    
    
    skip-checks:true
    
    * Byttet til riktig kolonne navn, og fjernet grunllagsreferanse som UUID in UttalelseGrunnlag
    
    * La til en index på migration filen
    
    * Satt grunnlag_ref inn i tabellen uttalelse_v2 og byttet til Long datatype i javakoden, satt kollonenavn til uttalelser_id i UttalelseGrunnlag, byttet til et forståelig indexnavn,
    
    * La til entiteter og sekvenser in ORM.-filen til etterlysning
    
    * Endringer i grunnlag
    
    * Fikset stavefeil
    
    * Endre grunnlag_ref til UUID type
    
    * Legg til hashcode og equals metoder i UtallelseV2
    
    * Endre hashcode og equals metoder i UtallelseV2
    
    ---------
    
    Co-authored-by: Eirik Fløtre Rørstad <eirik.flotre.rorstad@nav.no>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Eirik Fløtre Rørstad <62816788+eirroer@users.noreply.github.com>

--- behandlingsprosess/src/main/java/no/nav/ung/sak/domene/behandling/steg/ungdomsprogramkontroll/EndretUngdomsprogramEtterlysningInput.java
+++ behandlingsprosess/src/main/java/no/nav/ung/sak/domene/behandling/steg/ungdomsprogramkontroll/EndretUngdomsprogramEtterlysningInput.java
@@ -1,12 +1,10 @@
 package no.nav.ung.sak.domene.behandling.steg.ungdomsprogramkontroll;
 
-import no.nav.ung.kodeverk.etterlysning.EtterlysningType;
+import no.nav.ung.kodeverk.varsel.EtterlysningType;
 import no.nav.ung.sak.behandlingslager.behandling.startdato.UngdomsytelseStartdatoGrunnlag;
 import no.nav.ung.sak.behandlingslager.perioder.UngdomsprogramPeriodeGrunnlag;
 
-import java.util.Map;
 import java.util.Optional;
-import java.util.UUID;
 
 public record EndretUngdomsprogramEtterlysningInput(
         EtterlysningType etterlysningType,
