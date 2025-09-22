7dc7abfe7684e363c5c8576aabd6a15e82feff09
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2025-09-22 18:30:28 +0200

    Fix: fjern duplikat klasse

--- kodeverk/src/main/java/no/nav/ung/abac/AppAbacAttributtType.java
+++ kodeverk/src/main/java/no/nav/ung/abac/AppAbacAttributtType.java
@@ -1,7 +1,6 @@
 package no.nav.ung.abac;
 
 import no.nav.k9.felles.sikkerhet.abac.AbacAttributtType;
-import no.nav.k9.felles.sikkerhet.abac.StandardAbacAttributtType;
 
 /**
  * AbacAttributtTyper brukes i applikasjonen for å utlede hva som er relevant å sende til PDP for tilgangskontroll
@@ -13,28 +12,7 @@ public enum AppAbacAttributtType implements AbacAttributtType {
      * egen-definert oppgaveId i Gsak.
      */
     OPPGAVE_ID,
-    SAKER_MED_FNR,
-    ABAC_ANSVALIG_SAKSBEHANDLER,
-    ABAC_BEHANDLING_STATUS,
-    ABAC_SAK_STATUS,
-    ABAC_AKSJONSPUNKT_TYPE,
-    ;
-
-    public static AbacAttributtType AKSJONSPUNKT_KODE = StandardAbacAttributtType.AKSJONSPUNKT_KODE;
-
-    public static AbacAttributtType AKTØR_ID = StandardAbacAttributtType.AKTØR_ID;
-
-    public static AbacAttributtType BEHANDLING_ID = StandardAbacAttributtType.BEHANDLING_ID;
-
-    public static AbacAttributtType BEHANDLING_UUID = StandardAbacAttributtType.BEHANDLING_UUID;
-
-    public static AbacAttributtType FAGSAK_ID = StandardAbacAttributtType.FAGSAK_ID;
-
-    public static AbacAttributtType FNR = StandardAbacAttributtType.FNR;
-
-    public static AbacAttributtType JOURNALPOST_ID = StandardAbacAttributtType.JOURNALPOST_ID;
-
-    public static AbacAttributtType SAKSNUMMER = StandardAbacAttributtType.SAKSNUMMER;
+    SAKER_MED_FNR(true);
 
     private final boolean maskerOutput;
 
