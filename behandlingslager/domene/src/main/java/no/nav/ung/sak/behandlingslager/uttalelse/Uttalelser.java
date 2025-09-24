43d1963e2a0441283283c82db3fe21ffc974427d
Espen Velsvik <espen.velsvik@nav.no>
2025-09-24 16:54:46 +0200

    FIX: Legger til kopi-konstruktør for uttalelseV2 for å unngå å endre på uttalelser fra et annet grunnlag

--- behandlingslager/domene/src/main/java/no/nav/ung/sak/behandlingslager/uttalelse/Uttalelser.java
+++ behandlingslager/domene/src/main/java/no/nav/ung/sak/behandlingslager/uttalelse/Uttalelser.java
@@ -8,6 +8,7 @@ import org.hibernate.annotations.Immutable;
 
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity(name = "Uttalelser")
 @Table(name = "UTTALELSER")
@@ -27,7 +28,9 @@ public class Uttalelser extends BaseEntitet {
     }
 
     public Uttalelser(Set<UttalelseV2> uttalelser) {
-        this.uttalelser = uttalelser;
+        this.uttalelser = uttalelser.stream()
+            .map(UttalelseV2::new)
+            .collect(Collectors.toSet());
     }
 
     public Set<UttalelseV2> getUttalelser() {
