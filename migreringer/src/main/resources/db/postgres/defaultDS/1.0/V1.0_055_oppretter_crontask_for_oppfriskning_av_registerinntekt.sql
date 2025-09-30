64ce99d92fd0f01d760a85ed5e043fe509b14e77
Eirik Fløtre Rørstad <62816788+eirroer@users.noreply.github.com>
2025-09-30 11:30:28 +0200

    Oppretter migreringsskript for batch task som oppfrisker registerinntekt (#726)
    * Nytt tasknavn til batchtask
    
    * Legg til migreringsskript for cron-task
    
    * Fiks taskname
    
    * Setter flagg for kjøring av task til true i test
    
    * Setter flagg for kjøring av task til false i prod
    
    * Endres batchtask for OppfriskInntektForBehandlingerPåVentBatchTask til å kjøre hver time istedenfor hver dag kl. 12
    
    * Setter konfig verdi i test, oppdaterer registerdata ved innhenting dersom de er mer enn 30 min gamle
    
    * Update migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_055_oppretter_crontask_for_oppfriskning_av_registerinntekt.sql
    
    Co-authored-by: Espen Velsvik <espenjv@users.noreply.github.com>
    
    * Fikser cron-utrykk
    
    * Fikser cron-utrykk
    
    ---------
    
    Co-authored-by: Espen Velsvik <espenjv@users.noreply.github.com>

---
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_055_oppretter_crontask_for_oppfriskning_av_registerinntekt.sql
@@ -0,0 +1,3 @@
+insert into prosess_task (id, task_type, task_gruppe)
+select nextval('SEQ_PROSESS_TASK'), 'batch.oppfriskInntektPåVent', nextval('SEQ_PROSESS_TASK_GRUPPE')
+where not exists (select 1 from prosess_task where task_type = 'batch.oppfriskInntektPåVent'  and status IN ('KLAR', 'FEILET'));
