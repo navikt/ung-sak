29a66621326726781be1591e0c8a67b9caf787a4
Eirik Fløtre Rørstad <62816788+eirroer@users.noreply.github.com>
2025-10-02 13:02:54 +0200

    TSFF-2091: Swaggerendepunkt for å vise innhold i papirsøknaden (#740)
    * oppretta branch. oppadetrete vtp fila
    
    * Legger til REST-tjeneste for henting av papirsøknad med tilgangskontroll
    
    * ung-sak.openapi.json updated by build pipeline 
    
    
    skip-checks:true
    
    * ung-sak.openapi.json updated by build pipeline 
    
    
    skip-checks:true
    
    * Legger til HentPapirSøknadRequestDto for å strukturere forespørsel om papirsøknad og oppdaterer REST-tjeneste for å bruke denne DTO-en.
    
    Fjerner koden for tilgangskontroll, da dette gjøres gjennom kall til SafTjeneste.
    
    * ung-sak.openapi.json updated by build pipeline 
    
    
    skip-checks:true
    
    * Oppdaterer HentPapirSøknadRequestDto for å bruke AppAbacAttributt
    
    * Fjerner unødvendige import-setninger i PapirSøknadRestTjeneste
    
    * Legger til PapirSøknadRestTjeneste i RestImplementationClasses
    
    Signed-off-by: Ramin Esfandiari <ramin.esfandiari@nav.no>
    
    * Setter på påkrevde annotasjoner for String.
    
    Signed-off-by: Ramin Esfandiari <ramin.esfandiari@nav.no>
    
    * Lager eget dto for DokumenttId.
    
    Signed-off-by: Ramin Esfandiari <ramin.esfandiari@nav.no>
    
    * Ttypo
    
    Signed-off-by: Ramin Esfandiari <ramin.esfandiari@nav.no>
    
    * ung-sak.openapi.json updated by build pipeline 
    
    
    skip-checks:true
    
    ---------
    
    Signed-off-by: Ramin Esfandiari <ramin.esfandiari@nav.no>
    Co-authored-by: Edem.Schaanning.Agbo <edem.schaanning.agbo@nav.no>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Ramin Esfandiari <ramin.esfandiari@nav.no>

---
+++ web/src/main/java/no/nav/ung/sak/web/app/tjenester/fordeling/PapirSøknadRestTjeneste.java
@@ -0,0 +1,57 @@
+package no.nav.ung.sak.web.app.tjenester.fordeling;
+
+
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import no.nav.k9.felles.sikkerhet.abac.*;
+import no.nav.ung.sak.dokument.arkiv.DokumentArkivTjeneste;
+import no.nav.ung.sak.kontrakt.søknad.HentPapirSøknadRequestDto;
+import no.nav.ung.sak.web.server.abac.AbacAttributtSupplier;
+import no.nav.ung.sak.web.server.abac.SifAbacPdpRestKlient;
+
+import static no.nav.ung.sak.web.app.tjenester.fordeling.PapirSøknadRestTjeneste.BASE_PATH;
+
+@Path(BASE_PATH)
+@ApplicationScoped
+@Transactional
+public class PapirSøknadRestTjeneste {
+    static final String BASE_PATH = "/papir";
+
+    private DokumentArkivTjeneste dokumentArkivTjeneste;
+
+    public PapirSøknadRestTjeneste() {// For Rest-CDI
+    }
+
+    @Inject
+    public PapirSøknadRestTjeneste(DokumentArkivTjeneste dokumentArkivTjeneste) {
+        this.dokumentArkivTjeneste = dokumentArkivTjeneste;
+    }
+
+    @POST
+    @Path("/hentPapirSøknad")
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    @Operation(description = "Henter og viser papirsøknad", summary = ("Henter og viser papirsøknad"), tags = "fordel")
+    @BeskyttetRessurs(action = BeskyttetRessursActionType.READ, resource = BeskyttetRessursResourceType.FAGSAK)
+    public Response hentPapirSøknad(@NotNull @Valid @TilpassetAbacAttributt(supplierClass = AbacAttributtSupplier.class) HentPapirSøknadRequestDto hentPapirSøknadRequestDto) {
+
+        // SafTjeneste gjør tilgangskontroll på journalpostId internt gjennom kall til SAF
+        byte[] dokument = dokumentArkivTjeneste.hentDokumnet(hentPapirSøknadRequestDto.journalpostId(), hentPapirSøknadRequestDto.dokumentId().getVerdi());
+        String filnavn = "søknadsdokument-" + hentPapirSøknadRequestDto.dokumentId() + ".pdf";
+
+        try {
+            return Response.ok(dokument).type("application/pdf").header("Content-Disposition", "inline; filename=\"" + filnavn + "\"").build();
+        } catch (Exception e) {
+            return Response.serverError().entity("Klarte ikke å generere PDF: " + e.getMessage()).build();
+        }
+    }
+
+}
