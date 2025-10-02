7c22f2a04740bacfde0c826f5d2d5e40acf4314c
Espen Velsvik <espen.velsvik@nav.no>
2025-10-02 15:22:33 +0200

    Legge til kommentar

--- migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_060__bumpe_saksnummer_sekvense.sql
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_060__bumpe_saksnummer_sekvense.sql
@@ -1,1 +1,2 @@
+-- Tilfeldig valgt startverdi for sekvensen som ligger langt foran sekvensen for k9-sak for å unngå kollisjon med eksisterende saksnummer k9-sak
 ALTER SEQUENCE seq_saksnummer RESTART WITH 35038750;
