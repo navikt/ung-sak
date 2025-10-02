9bbc655911bd1b0f4e450abf9ecbd39bb7c76780
Eirik Fløtre Rørstad <eirik.flotre.rorstad@nav.no>
2025-10-02 11:03:44 +0200

    Oppdaterer HentPapirSøknadRequestDto for å bruke AppAbacAttributt

--- kontrakt/src/main/java/no/nav/ung/sak/kontrakt/søknad/HentPapirSøknadRequestDto.java
+++ kontrakt/src/main/java/no/nav/ung/sak/kontrakt/søknad/HentPapirSøknadRequestDto.java
@@ -1,20 +1,15 @@
 package no.nav.ung.sak.kontrakt.søknad;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import no.nav.k9.felles.sikkerhet.abac.StandardAbacAttributtType;
+import no.nav.ung.abac.AppAbacAttributt;
+import no.nav.ung.abac.AppAbacAttributtType;
 import no.nav.ung.abac.StandardAbacAttributt;
 import no.nav.ung.sak.typer.JournalpostId;
-import no.nav.ung.sak.typer.PersonIdent;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
-@JsonFormat(shape = JsonFormat.Shape.OBJECT)
-@JsonAutoDetect(getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE, fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public record HentPapirSøknadRequestDto(
 
     @StandardAbacAttributt(StandardAbacAttributtType.JOURNALPOST_ID)
@@ -23,7 +18,7 @@ public record HentPapirSøknadRequestDto(
     @Valid
     JournalpostId journalpostId,
 
-    @StandardAbacAttributt(StandardAbacAttributtType.DOKUMENT_DATA_ID)
+    @AppAbacAttributt(AppAbacAttributtType.DOKUMENT_ID)
     @JsonProperty(value = "dokumentId", required = true)
     @NotBlank
     String dokumentId
