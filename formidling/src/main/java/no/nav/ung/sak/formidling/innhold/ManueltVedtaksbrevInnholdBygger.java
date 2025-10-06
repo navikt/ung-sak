ddfe503fa9d141adfbe587f7c6b8d22855c716f1
Qadeer Khan <qadeer.ahmad.khan@nav.no>
2025-10-06 09:56:42 +0200

    Tsff 1205 feilhandtering (#764)
    * feiler behandling hvis brev ikke implementert.
    
    * fjerner BrevfeilHåndterer da den ikke er nødvendig
    
    * fjerner toggle for feil i VurderVedtaksbrevTask og har det istedet i foreslå vedtak-
    
    * Endrer navn så de passer bedre i frontend
    
    * håndterer avslag for seg selv. Kun avslag kan få tom redigerbar brev. Ukjente brev vil feile.
     Flytter utledning av alle brevegenskaper til strategiene
    
    * refactor
    
    * litt javadoc

--- formidling/src/main/java/no/nav/ung/sak/formidling/innhold/ManueltVedtaksbrevInnholdBygger.java
+++ formidling/src/main/java/no/nav/ung/sak/formidling/innhold/ManueltVedtaksbrevInnholdBygger.java
@@ -6,6 +6,9 @@ import no.nav.ung.kodeverk.formidling.TemplateType;
 import no.nav.ung.sak.formidling.template.dto.ManuellVedtaksbrevDto;
 
 @Dependent
+/**
+ * Bruker for alle redigerte vedtaksbrev
+ */
 public class ManueltVedtaksbrevInnholdBygger {
 
     @Inject
--- formidling/src/test/java/no/nav/ung/sak/formidling/TomVedtaksbrevMalTest.java
+++ formidling/src/test/java/no/nav/ung/sak/formidling/TomVedtaksbrevMalTest.java
@@ -3,9 +3,13 @@ package no.nav.ung.sak.formidling;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import no.nav.k9.felles.testutilities.cdi.CdiAwareExtension;
+import no.nav.ung.kodeverk.behandling.BehandlingType;
 import no.nav.ung.sak.db.util.JpaExtension;
-import no.nav.ung.sak.formidling.scenarioer.EndringInntektScenarioer;
+import no.nav.ung.sak.formidling.scenarioer.AvslagScenarioer;
 import no.nav.ung.sak.formidling.vedtak.VedtaksbrevTjeneste;
+import no.nav.ung.sak.test.util.UngTestRepositories;
+import no.nav.ung.sak.test.util.behandling.TestScenarioBuilder;
+import no.nav.ung.sak.test.util.behandling.UngTestScenario;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
@@ -32,13 +36,14 @@ class TomVedtaksbrevMalTest {
     @DisplayName("Tom vedtaksbrev")
     @Test
     void tomBrevMal(TestInfo testInfo) {
-        LocalDate fom = LocalDate.of(2024, 12, 1);
+        UngTestRepositories ungTestRepositories = BrevTestUtils.lagAlleUngTestRepositories(entityManager);
+        LocalDate fom = LocalDate.of(2025, 8, 1);
+        UngTestScenario ungTestGrunnlag = AvslagScenarioer.avslagAlder(fom);
+        var behandling = TestScenarioBuilder.builderMedSøknad()
+            .medBehandlingType(BehandlingType.FØRSTEGANGSSØKNAD)
+            .medUngTestGrunnlag(ungTestGrunnlag)
+            .buildOgLagreMedUng(ungTestRepositories);
 
-        var behandling = EndringInntektScenarioer
-            .lagBehandlingMedAksjonspunktKontrollerInntekt(
-                EndringInntektScenarioer.endring0KrInntekt_19år(fom),
-                BrevTestUtils.lagAlleUngTestRepositories(entityManager));
-        behandling.avsluttBehandling();
 
         var forventetMal = VedtaksbrevVerifikasjon.medHeaderOgFooterManuell(pdlKlientFake.fnr(),
             "<Fyll inn overskrift...> " +
