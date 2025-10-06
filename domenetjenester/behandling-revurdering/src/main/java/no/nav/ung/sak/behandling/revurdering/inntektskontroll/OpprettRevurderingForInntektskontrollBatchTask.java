319c2a4780e5e7af80160c890373257581d05774
Espen Velsvik <espen.velsvik@nav.no>
2025-10-06 11:21:10 +0200

    Hindrer opprettelse av task for kontroll av inntekt dersom det finnes en som er klar, feilet eller veto for samme periode.

--- domenetjenester/behandling-revurdering/src/main/java/no/nav/ung/sak/behandling/revurdering/inntektskontroll/OpprettRevurderingForInntektskontrollBatchTask.java
+++ domenetjenester/behandling-revurdering/src/main/java/no/nav/ung/sak/behandling/revurdering/inntektskontroll/OpprettRevurderingForInntektskontrollBatchTask.java
@@ -6,7 +6,9 @@ import no.nav.k9.prosesstask.api.*;
 import no.nav.k9.prosesstask.impl.cron.CronExpression;
 import no.nav.ung.kodeverk.behandling.BehandlingÅrsakType;
 import no.nav.ung.sak.behandling.revurdering.OpprettRevurderingEllerOpprettDiffTask;
+import no.nav.ung.sak.behandling.revurdering.sats.OpprettRevurderingHøySatsTask;
 import no.nav.ung.sak.behandlingslager.fagsak.Fagsak;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,6 +16,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAdjusters;
 import java.util.List;
+import java.util.function.Predicate;
 
 import static no.nav.ung.sak.behandling.revurdering.OpprettRevurderingEllerOpprettDiffTask.*;
 
@@ -28,6 +31,9 @@ import static no.nav.ung.sak.behandling.revurdering.OpprettRevurderingEllerOppre
 public class OpprettRevurderingForInntektskontrollBatchTask implements BatchProsessTaskHandler {
 
     public static final String TASKNAME = "batch.opprettRevurderingForInntektskontrollBatch";
+
+    private static final Logger log = LoggerFactory.getLogger(OpprettRevurderingForInntektskontrollBatchTask.class);
+
     private ProsessTaskTjeneste prosessTaskTjeneste;
 
     OpprettRevurderingForInntektskontrollBatchTask() {
@@ -47,10 +53,26 @@ public class OpprettRevurderingForInntektskontrollBatchTask implements BatchPros
     public void doTask(ProsessTaskData prosessTaskData) {
         var fom = LocalDate.now().minusMonths(1).withDayOfMonth(1);
         var tom = LocalDate.now().minusMonths(1).with(TemporalAdjusters.lastDayOfMonth());
+
+
+        List<ProsessTaskData> feiletTask = prosessTaskTjeneste.finnAlle(OpprettRevurderingHøySatsTask.TASKNAME, ProsessTaskStatus.FEILET).stream().filter(gjelderSammePeriode(fom)).toList();
+        List<ProsessTaskData> klarTask = prosessTaskTjeneste.finnAlle(OpprettRevurderingHøySatsTask.TASKNAME, ProsessTaskStatus.KLAR).stream().filter(gjelderSammePeriode(fom)).toList();
+        List<ProsessTaskData> vetoTask = prosessTaskTjeneste.finnAlle(OpprettRevurderingHøySatsTask.TASKNAME, ProsessTaskStatus.VETO).stream().filter(gjelderSammePeriode(fom)).toList();
+        if (!feiletTask.isEmpty() || !klarTask.isEmpty() || !vetoTask.isEmpty()) {
+            // Hvis det finnes noen task i noen av disse statusene, så betyr det at de enten kjører, eller skal kjøres.
+            // Vi ønsker ikke å opprette duplikater av disse.
+            log.info("Kontroll av inntekt for perioden {} - {} er allerede opprettet som task. Feilet: {}, Klar: {}, Veto: {}. Oppretter ikke duplikat", fom, tom, feiletTask.size(), klarTask.size(), vetoTask.size());
+            return;
+        }
+
         ProsessTaskData kontrollTask = ProsessTaskData.forProsessTask(OpprettRevurderingForInntektskontrollTask.class);
         kontrollTask.setProperty(PERIODE_FOM, fom.format(DateTimeFormatter.ISO_LOCAL_DATE));
         kontrollTask.setProperty(PERIODE_TOM, tom.format(DateTimeFormatter.ISO_LOCAL_DATE));
         prosessTaskTjeneste.lagre(kontrollTask);
+    }
+
+    private static Predicate<ProsessTaskData> gjelderSammePeriode(LocalDate fom) {
+        return it -> it.getPropertyValue(PERIODE_FOM).equals(fom.format(DateTimeFormatter.ISO_LOCAL_DATE));
     }
 
 }
