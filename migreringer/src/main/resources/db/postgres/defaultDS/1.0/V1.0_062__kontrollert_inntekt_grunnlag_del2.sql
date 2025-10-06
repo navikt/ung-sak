0983dee1b7c618c8746a15a9dce6ca198df88316
Espen Velsvik <espen.velsvik@nav.no>
2025-10-06 12:54:54 +0200

    Fjerner inaktive aggregat og fjerner behandling_id og aktiv-flagg

--- migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_062__kontrollert_inntekt_grunnlag_del2.sql
+++ migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_062__kontrollert_inntekt_grunnlag_del2.sql
@@ -9,12 +9,6 @@ DELETE FROM KONTROLLERT_INNTEKT_PERIODER perioder where
 and not exists (Select 1 from GR_KONTROLLERT_INNTEKT gk where gk.KONTROLLERT_INNTEKT_PERIODER_ID = perioder.id);
 
 drop index idx_kontrollert_inntekt_perioder_behandling_id;
-
-create unique index idx_kontrollert_inntekt_perioder_aktiv_behandling_id
-    on kontrollert_inntekt_perioder (behandling_id)
-    where aktiv = true;
-
--- Drop the unique index before dropping the column
 drop index idx_kontrollert_inntekt_perioder_aktiv_behandling_id;
 
 ALTER table KONTROLLERT_INNTEKT_PERIODER
