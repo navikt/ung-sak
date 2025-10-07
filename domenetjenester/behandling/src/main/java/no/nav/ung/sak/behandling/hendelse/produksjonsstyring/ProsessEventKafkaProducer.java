16278cab0abc8d988bc82ff76088e0544bf1a7c3
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2025-10-07 14:14:54 +0200

    Fjerner støtte for gammel vtp-kafka-løsning, fiks lokal kjøring mot ny vtp-kafka-løsning (#757)

--- domenetjenester/behandling/src/main/java/no/nav/ung/sak/behandling/hendelse/produksjonsstyring/ProsessEventKafkaProducer.java
+++ domenetjenester/behandling/src/main/java/no/nav/ung/sak/behandling/hendelse/produksjonsstyring/ProsessEventKafkaProducer.java
@@ -4,7 +4,6 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import no.nav.k9.felles.integrasjon.kafka.GenerellKafkaProducer;
 import no.nav.k9.felles.integrasjon.kafka.KafkaPropertiesBuilder;
-import no.nav.k9.felles.konfigurasjon.env.Environment;
 import no.nav.k9.felles.konfigurasjon.konfig.KonfigVerdi;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
@@ -17,48 +16,33 @@ public class ProsessEventKafkaProducer {
     private static final Logger logger = LoggerFactory.getLogger(ProsessEventKafkaProducer.class);
 
     private GenerellKafkaProducer producer;
-    String topic;
+    private String topic;
 
     public ProsessEventKafkaProducer() {
         // for CDI proxy
     }
 
     @Inject
-    public ProsessEventKafkaProducer(@KonfigVerdi("kafka.aksjonspunkthendelse.topic") String topic,
-                                     @KonfigVerdi("kafka.aksjonspunkthendelse.aiven.topic") String topicV2,
-                                     @KonfigVerdi(value = "KAFKA_BROKERS") String kafkaBrokers,
-                                     @KonfigVerdi(value = "KAFKA_TRUSTSTORE_PATH", required = false) String trustStorePath,
-                                     @KonfigVerdi(value = "KAFKA_CREDSTORE_PASSWORD", required = false) String trustStorePassword,
-                                     @KonfigVerdi(value = "KAFKA_KEYSTORE_PATH", required = false) String keyStoreLocation,
-                                     @KonfigVerdi(value = "KAFKA_CREDSTORE_PASSWORD", required = false) String keyStorePassword,
-                                     @KonfigVerdi(value = "KAFKA_BRUK_AIVEN_PROPERTY_LOKALT", required = false, defaultVerdi = "false") boolean brukAivenPropertyLokalt,
-                                     @KonfigVerdi("systembruker.username") String username,
-                                     @KonfigVerdi("systembruker.password") String password) {
+    public ProsessEventKafkaProducer(
+        @KonfigVerdi("kafka.aksjonspunkthendelse.aiven.topic") String topicName,
+        @KonfigVerdi(value = "KAFKA_BROKERS") String bootstrapServers,
+        @KonfigVerdi(value = "KAFKA_TRUSTSTORE_PATH", required = false) String trustStorePath,
+        @KonfigVerdi(value = "KAFKA_CREDSTORE_PASSWORD", required = false) String trustStorePassword,
+        @KonfigVerdi(value = "KAFKA_KEYSTORE_PATH", required = false) String keyStoreLocation,
+        @KonfigVerdi(value = "KAFKA_CREDSTORE_PASSWORD", required = false) String keyStorePassword) {
 
 
-        boolean aivenEnabled = !Environment.current().isLocal() || brukAivenPropertyLokalt;
-        String _topicName = aivenEnabled ? topicV2 : topic;
-        String _bootstrapServer = kafkaBrokers;
+        var props = new KafkaPropertiesBuilder()
+            .clientId("KP-" + topicName)
+            .bootstrapServers(bootstrapServers)
+            .truststorePath(trustStorePath)
+            .truststorePassword(trustStorePassword)
+            .keystorePath(keyStoreLocation)
+            .keystorePassword(keyStorePassword)
+            .buildForProducerAiven();
 
-        var builder = new KafkaPropertiesBuilder()
-            .clientId("KP-" + _topicName).bootstrapServers(_bootstrapServer);
-
-        var props = aivenEnabled ?
-            builder
-                .truststorePath(trustStorePath)
-                .truststorePassword(trustStorePassword)
-                .keystorePath(keyStoreLocation)
-                .keystorePassword(keyStorePassword)
-                .buildForProducerAiven() :
-            builder
-                .username(username)
-                .password(password)
-                .buildForProducerJaas();
-
-
-        producer = new GenerellKafkaProducer(_topicName, props);
-
-        this.topic = _topicName;
+        producer = new GenerellKafkaProducer(topicName, props);
+        this.topic = topicName;
 
     }
 
