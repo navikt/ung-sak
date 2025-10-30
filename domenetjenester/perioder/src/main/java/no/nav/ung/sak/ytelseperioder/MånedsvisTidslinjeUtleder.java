311ffb4a79a8844422048e91e9dbdd8f306179ab
Espen Velsvik <espenjv@users.noreply.github.com>
2025-10-30 18:25:36 +0100

    TSFF-2199: Kontroll av inntekt gjøres på deler av inntekt i tilfelle kontroll i siste måned (#874)
    * TSFF-2199: Kontroll av inntekt gjøres på deler av inntekt i tilfelle kontroll i siste måned
    
    * Sender med informasjon om det gjelder siste måned til ung-deltakelse-opplyser
    
    * Bumper ung-deltakelse-opplyser

--- domenetjenester/perioder/src/main/java/no/nav/ung/sak/ytelseperioder/MånedsvisTidslinjeUtleder.java
+++ domenetjenester/perioder/src/main/java/no/nav/ung/sak/ytelseperioder/MånedsvisTidslinjeUtleder.java
@@ -4,14 +4,11 @@ import jakarta.enterprise.context.Dependent;
 import jakarta.inject.Inject;
 import no.nav.fpsak.tidsserie.LocalDateSegment;
 import no.nav.fpsak.tidsserie.LocalDateTimeline;
-import no.nav.fpsak.tidsserie.StandardCombinators;
-import no.nav.k9.felles.konfigurasjon.konfig.KonfigVerdi;
 import no.nav.ung.sak.behandlingslager.behandling.repository.BehandlingRepository;
 import no.nav.ung.sak.ungdomsprogram.UngdomsprogramPeriodeTjeneste;
 
 import java.time.Period;
 import java.time.YearMonth;
-import java.time.temporal.TemporalAdjusters;
 import java.util.List;
 
 @Dependent
@@ -19,14 +16,12 @@ public class MånedsvisTidslinjeUtleder {
 
     private final UngdomsprogramPeriodeTjeneste ungdomsprogramPeriodeTjeneste;
     private final BehandlingRepository behandlingRepository;
-    private final boolean kontrollAvSisteMndEnabled;
 
 
     @Inject
-    public MånedsvisTidslinjeUtleder(UngdomsprogramPeriodeTjeneste ungdomsprogramPeriodeTjeneste, BehandlingRepository behandlingRepository, @KonfigVerdi(value = "KONTROLL_SISTE_MND_ENABLED", defaultVerdi = "false") boolean kontrollAvSisteMndEnabled) {
+    public MånedsvisTidslinjeUtleder(UngdomsprogramPeriodeTjeneste ungdomsprogramPeriodeTjeneste, BehandlingRepository behandlingRepository) {
         this.ungdomsprogramPeriodeTjeneste = ungdomsprogramPeriodeTjeneste;
         this.behandlingRepository = behandlingRepository;
-        this.kontrollAvSisteMndEnabled = kontrollAvSisteMndEnabled;
     }
 
 
@@ -42,14 +37,6 @@ public class MånedsvisTidslinjeUtleder {
         final var fagsakPeriode = fagsak.getPeriode();
         LocalDateTimeline<Boolean> programOgFagsakTidslinje = ungdomsprogramperioder.intersection(new LocalDateTimeline<>(fagsakPeriode.getFomDato(), fagsakPeriode.getTomDato(), true))
             .compress();
-        if (kontrollAvSisteMndEnabled) {
-            var mappedSegments = programOgFagsakTidslinje
-                .toSegments()
-                .stream()
-                .map(it -> new LocalDateSegment<>(it.getFom(), it.getTom().with(TemporalAdjusters.lastDayOfMonth()), it.getValue()))
-                .toList(); // Mapper segmenter til å dekke hele måneder
-            programOgFagsakTidslinje = new LocalDateTimeline<>(mappedSegments, StandardCombinators::alwaysTrueForMatch).compress();
-        }
         return programOgFagsakTidslinje
             .splitAtRegular(ungdomsprogramperioder.getMinLocalDate().withDayOfMonth(1), fagsakPeriode.getTomDato(), Period.ofMonths(1))
             .map(it -> List.of(new LocalDateSegment<>(it.getLocalDateInterval(), YearMonth.of(it.getFom().getYear(), it.getFom().getMonthValue()))));
