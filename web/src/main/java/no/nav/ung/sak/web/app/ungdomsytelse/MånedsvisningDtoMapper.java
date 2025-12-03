7bf43f946891b8a7700083d47f3575fef847b6ed
Espen Velsvik <espen.velsvik@nav.no>
2025-12-03 15:12:23 +0100

    Reduksjonsgrunnlag som hele kroner

--- web/src/main/java/no/nav/ung/sak/web/app/ungdomsytelse/MånedsvisningDtoMapper.java
+++ web/src/main/java/no/nav/ung/sak/web/app/ungdomsytelse/MånedsvisningDtoMapper.java
@@ -53,8 +53,8 @@ public class MånedsvisningDtoMapper {
 
     private static Optional<BigDecimal> finnReduksjonsgrunnlag(LocalDateSegment<YearMonth> måned, Optional<BigDecimal> rapportertInntekt, Integer antallYtelsesdagerIMåned) {
         final var totaltAntallDagerIMåned = måned.getTom().lengthOfMonth();
-        final var reduksjonsgrunnlag = rapportertInntekt.map(it -> BigDecimal.valueOf(antallYtelsesdagerIMåned).divide(BigDecimal.valueOf(totaltAntallDagerIMåned), 0, RoundingMode.HALF_UP)
-            .multiply(it));
+        final var reduksjonsgrunnlag = rapportertInntekt.map(it -> BigDecimal.valueOf(antallYtelsesdagerIMåned).divide(BigDecimal.valueOf(totaltAntallDagerIMåned), 10, RoundingMode.HALF_UP)
+            .multiply(it).setScale(0, RoundingMode.HALF_UP));
         return reduksjonsgrunnlag;
     }
 
