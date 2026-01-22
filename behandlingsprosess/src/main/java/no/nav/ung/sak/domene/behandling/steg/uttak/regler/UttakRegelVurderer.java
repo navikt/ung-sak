8bfa937200aa72c5ebb84b87c8c679a04c9a581c
Espen Velsvik <espenjv@users.noreply.github.com>
2026-01-22 09:30:14 +0100

    FIX: Starter på en ytelsesspesifikk modul for ungdomsprogramytelsen (#1061)
    ### **Behov / Bakgrunn**
    Skiller det spesifikke ved denne ytelsen fra resten av applikasjonen

--- behandlingsprosess/src/main/java/no/nav/ung/sak/domene/behandling/steg/uttak/regler/UttakRegelVurderer.java
+++
@@ -1,9 +0,0 @@
-package no.nav.ung.sak.domene.behandling.steg.uttak.regler;
-
-import no.nav.fpsak.tidsserie.LocalDateTimeline;
-
-public interface UttakRegelVurderer {
-
-    UttakDelResultat vurder(LocalDateTimeline<Boolean> tidslinjeTilVurdering);
-
-}
