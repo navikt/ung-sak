02e3008cf759e72d1227bfa526f0ec89c9f12d11
Espen Velsvik <espen.velsvik@nav.no>
2026-02-12 16:19:37 +0100

    FIX: Fikser toggling av consumer

--- brukerdialog-oppgave/tjeneste/src/main/java/no/nav/ung/sak/oppgave/kafka/BrukerdialogKafkaServiceHandler.java
+++ brukerdialog-oppgave/tjeneste/src/main/java/no/nav/ung/sak/oppgave/kafka/BrukerdialogKafkaServiceHandler.java
@@ -15,7 +15,6 @@ public class BrukerdialogKafkaServiceHandler implements AppServiceHandler {
 
     private static final Logger LOG = LoggerFactory.getLogger(BrukerdialogKafkaServiceHandler.class);
 
-    private boolean oppgaverIUngsakEnabled;
     private KafkaConsumerManager<String, String> kcm;
 
     public BrukerdialogKafkaServiceHandler() {
@@ -25,24 +24,21 @@ public class BrukerdialogKafkaServiceHandler implements AppServiceHandler {
     public BrukerdialogKafkaServiceHandler(
         @KonfigVerdi(value = "OPPGAVER_I_UNGSAK_ENABLED", defaultVerdi = "true") boolean oppgaverIUngsakEnabled,
         SvarPåVarselHendelseHåndterer svarPåVarselHendelseHåndterer) {
-        this.oppgaverIUngsakEnabled = oppgaverIUngsakEnabled;
-        this.kcm = new KafkaConsumerManager<>(List.of(svarPåVarselHendelseHåndterer));
+        this.kcm = oppgaverIUngsakEnabled ?
+            new KafkaConsumerManager<>(List.of(svarPåVarselHendelseHåndterer)) :
+            new KafkaConsumerManager<>(List.of());
     }
 
     @Override
     public void start() {
-        if (oppgaverIUngsakEnabled) {
-            LOG.info("Starter konsumering av topics={}", kcm.topicNames());
-            kcm.start((t, e) -> LOG.error("{} :: Caught exception in stream, exiting", t, e));
-        }
+        LOG.info("Starter konsumering av topics={}", kcm.topicNames());
+        kcm.start((t, e) -> LOG.error("{} :: Caught exception in stream, exiting", t, e));
     }
 
     @Override
     public void stop() {
-        if (oppgaverIUngsakEnabled) {
-            LOG.info("Starter shutdown av topics={} med 10 sekunder timeout", kcm.topicNames());
-            kcm.stop();
-        }
+        LOG.info("Starter shutdown av topics={} med 10 sekunder timeout", kcm.topicNames());
+        kcm.stop();
     }
 
     public boolean allRunning() {
@@ -52,6 +48,5 @@ public class BrukerdialogKafkaServiceHandler implements AppServiceHandler {
     public String getTopicNames() {
         return kcm.topicNames();
     }
-
 
 }
