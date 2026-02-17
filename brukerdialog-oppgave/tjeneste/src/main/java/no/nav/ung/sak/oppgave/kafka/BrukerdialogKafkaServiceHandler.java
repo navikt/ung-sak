26d481cf1486893a626d82486e592236765f42d9
Qadeer Khan <qadeer.ahmad.khan@nav.no>
2026-02-17 14:15:05 +0100

    Revert "FIX: Prøver å flytte toggle til handler, gir mulighet for toggling av kvar enkelt consumer (#1128)"
    This reverts commit fc902f17faa50dceaa0effc8a5ce864e89dc5e60.

--- brukerdialog-oppgave/tjeneste/src/main/java/no/nav/ung/sak/oppgave/kafka/BrukerdialogKafkaServiceHandler.java
+++ brukerdialog-oppgave/tjeneste/src/main/java/no/nav/ung/sak/oppgave/kafka/BrukerdialogKafkaServiceHandler.java
@@ -3,6 +3,7 @@ package no.nav.ung.sak.oppgave.kafka;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import no.nav.k9.felles.apptjeneste.AppServiceHandler;
+import no.nav.k9.felles.konfigurasjon.konfig.KonfigVerdi;
 import no.nav.ung.sak.oppgave.typer.varsel.kafka.SvarPåVarselHendelseHåndterer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,8 +21,12 @@ public class BrukerdialogKafkaServiceHandler implements AppServiceHandler {
     }
 
     @Inject
-    public BrukerdialogKafkaServiceHandler(SvarPåVarselHendelseHåndterer svarPåVarselHendelseHåndterer) {
-        this.kcm = new KafkaConsumerManager<>(List.of(svarPåVarselHendelseHåndterer));
+    public BrukerdialogKafkaServiceHandler(
+        @KonfigVerdi(value = "OPPGAVER_I_UNGSAK_ENABLED", defaultVerdi = "true") boolean oppgaverIUngsakEnabled,
+        SvarPåVarselHendelseHåndterer svarPåVarselHendelseHåndterer) {
+        this.kcm = oppgaverIUngsakEnabled ?
+            new KafkaConsumerManager<>(List.of(svarPåVarselHendelseHåndterer)) :
+            new KafkaConsumerManager<>(List.of());
     }
 
     @Override
