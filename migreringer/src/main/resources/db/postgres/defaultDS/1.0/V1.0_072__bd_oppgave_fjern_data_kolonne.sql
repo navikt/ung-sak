4fdec929a85132b7ef3bba76cc4bda284605d15e
Espen Velsvik <espen.velsvik@nav.no>
2026-02-23 15:07:05 +0100

    Fjerner duplikat script

--- migreringer/src/main/resources/db/postgres/defaultDS/1.0/V1.0_072__bd_oppgave_fjern_data_kolonne.sql
+++
@@ -1,7 +0,0 @@
--- Fjerner jsonb data-kolonne fra BD_OPPGAVE.
--- Strukturert oppgavedata lagres nå i egne BD_OPPGAVE_DATA_*-tabeller (se V1.0_071),
--- og referansen holdes via oppgave_data_id / type-kolonnen (@Any-mapping).
-
-alter table BD_OPPGAVE
-    drop column data;
-
