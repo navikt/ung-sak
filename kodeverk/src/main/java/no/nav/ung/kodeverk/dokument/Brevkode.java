8b50fa6cd945ca7d1975a5c139093dfa9b610662
Qadeer Khan <qadeer.ahmad.khan@nav.no>
2026-03-06 08:48:19 +0100

    legger inn midleritidig brevkode for søknad

--- kodeverk/src/main/java/no/nav/ung/kodeverk/dokument/Brevkode.java
+++ kodeverk/src/main/java/no/nav/ung/kodeverk/dokument/Brevkode.java
@@ -36,10 +36,10 @@ public class Brevkode implements Kodeverdi {
     public static final Brevkode KLAGE = new Brevkode(KLAGE_KODE, "NAV 90-00.08 K", VEDLEGG_RANGERING);
 
     /**
-     * Ung brevkoder.
+     * Aktivitetspenger brevkoder.
      */
     public static final String AKTIVITETSPENGER_SOKNAD_KODE = "AKTIVITETSPENGER_SOKNAD";
-    public static final Brevkode AKTIVITETSPENGER_SOKNAD = new Brevkode(AKTIVITETSPENGER_SOKNAD_KODE, "FIXME_AKTIVITETSPENGER_BREVKODE", SØKNAD_RANGERING);
+    public static final Brevkode AKTIVITETSPENGER_SOKNAD = new Brevkode(AKTIVITETSPENGER_SOKNAD_KODE, "NAV Aktivitetspenger", SØKNAD_RANGERING);
 
     // Default
     public static final Brevkode UDEFINERT = new Brevkode("-", null, VEDLEGG_RANGERING);
