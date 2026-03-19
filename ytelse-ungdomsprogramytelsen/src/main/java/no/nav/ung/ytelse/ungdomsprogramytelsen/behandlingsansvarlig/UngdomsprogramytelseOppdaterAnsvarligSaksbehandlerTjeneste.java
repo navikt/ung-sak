5c6fe0f80b45ce3771d4630d2e9db18010c0aacd
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2026-03-19 11:12:33 +0100

    whitespace

--- ytelse-ungdomsprogramytelsen/src/main/java/no/nav/ung/ytelse/ungdomsprogramytelsen/behandlingsansvarlig/UngdomsprogramytelseOppdaterAnsvarligSaksbehandlerTjeneste.java
+++ ytelse-ungdomsprogramytelsen/src/main/java/no/nav/ung/ytelse/ungdomsprogramytelsen/behandlingsansvarlig/UngdomsprogramytelseOppdaterAnsvarligSaksbehandlerTjeneste.java
@@ -4,7 +4,8 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import no.nav.k9.sikkerhet.context.SubjectHandler;
 import no.nav.ung.kodeverk.behandling.FagsakYtelseType;
-import no.nav.ung.kodeverk.behandling.aksjonspunkt.AksjonspunktDefinisjon;import no.nav.ung.sak.behandlingskontroll.FagsakYtelseTypeRef;
+import no.nav.ung.kodeverk.behandling.aksjonspunkt.AksjonspunktDefinisjon;
+import no.nav.ung.sak.behandlingskontroll.FagsakYtelseTypeRef;
 import no.nav.ung.sak.behandlingslager.behandling.repository.BehandlingAnsvarligRepository;
 import no.nav.ung.sak.domene.vedtak.OppdaterAnsvarligSaksbehandlerTjeneste;
 import no.nav.ung.sak.kontrakt.aksjonspunkt.BekreftetAksjonspunktDto;
@@ -28,7 +29,7 @@ public class UngdomsprogramytelseOppdaterAnsvarligSaksbehandlerTjeneste implemen
     }
 
     @Override
-    public void oppdaterAnsvarligSaksbehandler(Collection<BekreftetAksjonspunktDto> bekreftedeAksjonspunktDtoer, Long behandlingId){
+    public void oppdaterAnsvarligSaksbehandler(Collection<BekreftetAksjonspunktDto> bekreftedeAksjonspunktDtoer, Long behandlingId) {
         if (bekreftedeAksjonspunktDtoer.stream().anyMatch(dto -> dto instanceof FatterVedtakAksjonspunktDto)) {
             return;
         }
@@ -40,4 +41,5 @@ public class UngdomsprogramytelseOppdaterAnsvarligSaksbehandlerTjeneste implemen
     public void oppdaterAnsvarligBeslutter(AksjonspunktDefinisjon fatteVedtakAksjonspunktDefinisjon, Long behandlingId) {
         String saksbehandlerIdent = SubjectHandler.getSubjectHandler().getUid();
         behandlingAnsvarligRepository.setAnsvarligBeslutter(behandlingId, saksbehandlerIdent);
-    }}
+    }
+}
