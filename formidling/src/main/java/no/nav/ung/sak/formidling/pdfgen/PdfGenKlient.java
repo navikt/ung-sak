a9ccdacb3fd45b013e51b440f5eb165d14d258f5
Qadeer Khan <qadeer.ahmad.khan@nav.no>
2026-03-27 16:03:25 +0100

    endrer rekkefølge på hvordan pdfgen klient plukker ressurser da det ikke funka fra IDE. Fikser Navlogo.svg til å være en som er PDF/A compliant.

--- formidling/src/main/java/no/nav/ung/sak/formidling/pdfgen/PdfGenKlient.java
+++ formidling/src/main/java/no/nav/ung/sak/formidling/pdfgen/PdfGenKlient.java
@@ -58,24 +58,27 @@ public class PdfGenKlient {
             return path;
         }
 
-        // 2. Sjekk target/pdfgen/  - brukes av maven tester
+        // 2. sjekk classpath - brukes av IDE
+        URL resource = getClass().getClassLoader().getResource(classpathSti);
+        if (resource != null) {
+            try {
+                log.info("Fant ikke pdfgen-ressurser på {}. Prøver classpath. " +
+                    "Bør bare skje for test via IDE.", classpathSti);
+                return Path.of(resource.toURI());
+            } catch (URISyntaxException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+
+        // 3. Sjekk target/pdfgen/  - brukes av maven tester
         Path targetPath = Path.of("target", classpathSti);
         if (Files.exists(targetPath)) {
             return targetPath;
         }
 
-        // 3. Fallback til classpath - brukes av IDE
-        log.info("Fant ikke pdfgen-ressurser på {} eller {}. Prøver classpath. " +
-            "Bør bare skje for test via IDE.", classpathSti, targetPath);
+        throw new IllegalArgumentException("Fant ikke pdfgen ressurser på filsystem eller classpath");
 
-        URL resource = getClass().getClassLoader().getResource(classpathSti);
-        Objects.requireNonNull(resource, "Fant ingen pdfgen-ressurs på filsystem eller classpath: " + classpathSti);
-
-        try {
-            return Path.of(resource.toURI());
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     @WithSpan
