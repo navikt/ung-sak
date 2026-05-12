c3611f779db156bdd59eb8f20efe7d637d3a201e
Ramin Esfandiari <ramin.esfandiari@nav.no>
2026-05-12 17:55:35 +0200

    fix: update mapping for maximum date to use 'forlengetPeriodeMaksDato'
    Signed-off-by: Ramin Esfandiari <ramin.esfandiari@nav.no>

--- ytelse-ungdomsprogramytelsen/src/main/java/no/nav/ung/ytelse/ungdomsprogramytelsen/hendelsehåndtering/UngdomsprogramOpphørFagsakTilVurderingUtleder.java
+++ ytelse-ungdomsprogramytelsen/src/main/java/no/nav/ung/ytelse/ungdomsprogramytelsen/hendelsehåndtering/UngdomsprogramOpphørFagsakTilVurderingUtleder.java
@@ -145,7 +145,7 @@ public class UngdomsprogramOpphørFagsakTilVurderingUtleder implements FagsakerT
         try {
             var registerOpplysninger = ungdomsprogramRegisterKlient.hentForAktørId(aktør.getAktørId());
             var maksdato = registerOpplysninger.opplysninger().stream()
-                .map(UngdomsprogramRegisterKlient.DeltakerProgramOpplysningDTO::kvoteMaksDato)
+                .map(UngdomsprogramRegisterKlient.DeltakerProgramOpplysningDTO::forlengetPeriodeMaksDato)
                 .filter(Objects::nonNull)
                 .filter(opphørsdato::equals)
                 .findFirst();
