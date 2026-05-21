0ebcb23f730687eb4324c174c7710a08cf69d4f1
Espen Velsvik <espen.velsvik@nav.no>
2026-05-21 14:15:19 +0200

    TSFF-2696: Legger inn og toggler nytt G-beløp for testing av G-justering

--- domenetjenester/grunnbelop/src/main/java/no/nav/ung/sak/grunnbeløp/GrunnbeløpTidslinje.java
+++ domenetjenester/grunnbelop/src/main/java/no/nav/ung/sak/grunnbeløp/GrunnbeløpTidslinje.java
@@ -2,21 +2,32 @@ package no.nav.ung.sak.grunnbeløp;
 
 import no.nav.fpsak.tidsserie.LocalDateSegment;
 import no.nav.fpsak.tidsserie.LocalDateTimeline;
+import no.nav.k9.felles.konfigurasjon.env.Environment;
+import no.nav.k9.felles.konfigurasjon.konfig.KonfigVerdi;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
 public class GrunnbeløpTidslinje {
+
     /**
      * Tidslinje for grunnbeløpsatser
      */
-    private static final LocalDateTimeline<Grunnbeløp> GRUNNBELØP_TIDSLINJE = new LocalDateTimeline<>(
-        List.of(
-            new LocalDateSegment<>(LocalDate.of(2025, 5, 1), LocalDate.of(2099, 12, 31), new Grunnbeløp(BigDecimal.valueOf(130160))),
-            new LocalDateSegment<>(LocalDate.of(2024, 5, 1), LocalDate.of(2025, 4, 30), new Grunnbeløp(BigDecimal.valueOf(124028))),
-            new LocalDateSegment<>(LocalDate.of(2023, 5, 1), LocalDate.of(2024, 4,30), new Grunnbeløp(BigDecimal.valueOf(118620)))
-        ));
+    private static final LocalDateTimeline<Grunnbeløp> GRUNNBELØP_TIDSLINJE = Environment.current().getProperty("G_BELOP_2026_VEDTATT", Boolean.class, false) ?
+        new LocalDateTimeline<>(
+            List.of(
+                new LocalDateSegment<>(LocalDate.of(2026, 5, 1), LocalDate.of(2099, 12, 31), new Grunnbeløp(BigDecimal.valueOf(136549))),
+                new LocalDateSegment<>(LocalDate.of(2025, 5, 1), LocalDate.of(2026, 4, 30), new Grunnbeløp(BigDecimal.valueOf(130160))),
+                new LocalDateSegment<>(LocalDate.of(2024, 5, 1), LocalDate.of(2025, 4, 30), new Grunnbeløp(BigDecimal.valueOf(124028))),
+                new LocalDateSegment<>(LocalDate.of(2023, 5, 1), LocalDate.of(2024, 4, 30), new Grunnbeløp(BigDecimal.valueOf(118620)))
+            )) :
+        new LocalDateTimeline<>(
+            List.of(
+                new LocalDateSegment<>(LocalDate.of(2025, 5, 1), LocalDate.of(2099, 12, 31), new Grunnbeløp(BigDecimal.valueOf(130160))),
+                new LocalDateSegment<>(LocalDate.of(2024, 5, 1), LocalDate.of(2025, 4, 30), new Grunnbeløp(BigDecimal.valueOf(124028))),
+                new LocalDateSegment<>(LocalDate.of(2023, 5, 1), LocalDate.of(2024, 4, 30), new Grunnbeløp(BigDecimal.valueOf(118620)))
+            ));
 
 
     public static LocalDateTimeline<Grunnbeløp> hentTidslinje() {
