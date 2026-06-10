04dd11a9c66cba9f258912ec739d606aefe9aff9
Qadeer Khan <qadeer.ahmad.khan@nav.no>
2026-06-10 11:18:05 +0200

    Skal ikke sende brev ved g-regulering

---
+++ ytelse-ungdomsprogramytelsen/src/main/java/no/nav/ung/ytelse/ungdomsprogramytelsen/formidling/vedtak/regler/strategy/SatsReguleringStrategy.java
@@ -0,0 +1,37 @@
+package no.nav.ung.ytelse.ungdomsprogramytelsen.formidling.vedtak.regler.strategy;
+
+import jakarta.enterprise.context.Dependent;
+import jakarta.inject.Inject;
+import no.nav.fpsak.tidsserie.LocalDateTimeline;
+import no.nav.ung.kodeverk.behandling.FagsakYtelseType;
+import no.nav.ung.sak.behandlingskontroll.FagsakYtelseTypeRef;
+import no.nav.ung.sak.behandlingslager.behandling.Behandling;
+import no.nav.ung.sak.formidling.vedtak.regler.IngenBrevÅrsakType;
+import no.nav.ung.sak.formidling.vedtak.regler.strategy.VedtaksbrevInnholdbyggerStrategy;
+import no.nav.ung.sak.formidling.vedtak.regler.strategy.VedtaksbrevStrategyResultat;
+import no.nav.ung.sak.formidling.vedtak.resultat.DetaljertResultat;
+import no.nav.ung.sak.formidling.vedtak.resultat.DetaljertResultatType;
+import no.nav.ung.sak.formidling.vedtak.resultat.ResultatHelper;
+
+@Dependent
+@FagsakYtelseTypeRef(FagsakYtelseType.UNGDOMSYTELSE)
+public final class SatsReguleringStrategy implements VedtaksbrevInnholdbyggerStrategy {
+
+
+    @Inject
+    public SatsReguleringStrategy() {
+    }
+
+    @Override
+    public VedtaksbrevStrategyResultat evaluer(Behandling behandling, LocalDateTimeline<DetaljertResultat> detaljertResultat) {
+        return VedtaksbrevStrategyResultat.utenBrev(IngenBrevÅrsakType.IKKE_RELEVANT, "Ingen brev ved G-regulering");
+    }
+
+    @Override
+    public boolean skalEvaluere(Behandling behandling, LocalDateTimeline<DetaljertResultat> detaljertResultat) {
+        var resultatInfo = VedtaksbrevInnholdbyggerStrategy.tilResultatInfo(detaljertResultat);
+        var resultater = new ResultatHelper(resultatInfo);
+        return resultater.innholderBare(DetaljertResultatType.SATS_REGULERING);
+    }
+
+}
