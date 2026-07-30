37d20d772624afe67e7695048ac84f505eee503c
Qadeer Khan <qadeer.ahmad.khan@nav.no>
2026-07-30 20:16:58 +0200

    bytter rekkefølge for enklere lesing

--- formidling/src/main/java/no/nav/ung/sak/formidling/vedtak/regler/BehandlingVedtaksbrevResultat.java
+++ formidling/src/main/java/no/nav/ung/sak/formidling/vedtak/regler/BehandlingVedtaksbrevResultat.java
@@ -1,6 +1,5 @@
 package no.nav.ung.sak.formidling.vedtak.regler;
 
-import no.nav.fpsak.tidsserie.LocalDateTimeline;
 import no.nav.ung.kodeverk.dokument.DokumentMalType;
 import no.nav.ung.sak.formidling.vedtak.resultat.DetaljertResultatTidslinje;
 
@@ -55,9 +54,9 @@ public record BehandlingVedtaksbrevResultat(
     public String safePrint() {
         return "BehandlingVedtaksbrevResultat{" +
             "harBrev=" + harBrev +
-            ", detaljertResultatTimeline=[" + detaljertResultatTimeline + "]" +
             ", vedtaksbrevResultater=" + vedtaksbrevResultater +
             ", ingenBrevResultater=" + ingenBrevResultater +
+            ", detaljertResultatTimeline=[" + detaljertResultatTimeline + "]" +
             '}';
     }
 }
