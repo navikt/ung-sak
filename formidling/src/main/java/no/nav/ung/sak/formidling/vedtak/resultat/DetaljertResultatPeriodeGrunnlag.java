3dbc48bd0e110d41a6e301241c9e4c7a4d20ba30
Qadeer Khan <qadeer.ahmad.khan@nav.no>
2026-08-25 17:09:01 +0200

    ignorerer avkortede vilkår

--- formidling/src/main/java/no/nav/ung/sak/formidling/vedtak/resultat/DetaljertResultatPeriodeGrunnlag.java
+++ formidling/src/main/java/no/nav/ung/sak/formidling/vedtak/resultat/DetaljertResultatPeriodeGrunnlag.java
@@ -1,6 +1,7 @@
 package no.nav.ung.sak.formidling.vedtak.resultat;
 
 import no.nav.ung.kodeverk.behandling.BehandlingÅrsakType;
+import no.nav.ung.kodeverk.vilkår.Avslagsårsak;
 import no.nav.ung.kodeverk.vilkår.Utfall;
 
 import java.util.List;
@@ -14,6 +15,7 @@ public record DetaljertResultatPeriodeGrunnlag(List<DetaljertVilkårResultat> vi
     public Set<DetaljertVilkårResultat> avslåtteVilkår() {
         return vilkårsresultater.stream()
             .filter(it -> it.utfall() == Utfall.IKKE_OPPFYLT)
+            .filter(it -> it.avslagsårsak() != Avslagsårsak.AVKORTET)
             .collect(Collectors.toSet());
     }
 
