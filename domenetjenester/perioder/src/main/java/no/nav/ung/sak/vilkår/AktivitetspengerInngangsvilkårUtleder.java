c1eeb32377df6fbceb7c25399400b60b4804a18c
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2026-09-07 12:55:09 +0200

    Skrur på aktivitetsvilkåret for aktivitetspenger

--- domenetjenester/perioder/src/main/java/no/nav/ung/sak/vilkår/AktivitetspengerInngangsvilkårUtleder.java
+++ domenetjenester/perioder/src/main/java/no/nav/ung/sak/vilkår/AktivitetspengerInngangsvilkårUtleder.java
@@ -1,8 +1,6 @@
 package no.nav.ung.sak.vilkår;
 
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
-import no.nav.k9.felles.konfigurasjon.konfig.KonfigVerdi;
 import no.nav.ung.kodeverk.behandling.BehandlingType;
 import no.nav.ung.kodeverk.behandling.FagsakYtelseType;
 import no.nav.ung.kodeverk.vilkår.VilkårType;
@@ -17,7 +15,7 @@ import static no.nav.ung.kodeverk.vilkår.VilkårType.*;
 
 @ApplicationScoped
 @FagsakYtelseTypeRef(FagsakYtelseType.AKTIVITETSPENGER)
-@BehandlingTypeRef(BehandlingType.FØRSTEGANGSSØKNAD )
+@BehandlingTypeRef(BehandlingType.FØRSTEGANGSSØKNAD)
 public class AktivitetspengerInngangsvilkårUtleder implements VilkårUtleder {
 
     private static final List<VilkårType> YTELSE_VILKÅR = asList(
@@ -26,33 +24,18 @@ public class AktivitetspengerInngangsvilkårUtleder implements VilkårUtleder {
         BOSTEDSVILKÅR,
         ANDRE_LIVSOPPHOLDSYTELSER_VILKÅR,
         BISTANDSVILKÅR,
-        FORUTGÅENDE_MEDLEMSKAPSVILKÅRET
-    );
-
-    private static final List<VilkårType> YTELSE_VILKÅR_2 = asList(
-        SØKNADSFRIST,
-        ALDERSVILKÅR,
-        BOSTEDSVILKÅR,
-        ANDRE_LIVSOPPHOLDSYTELSER_VILKÅR,
-        BISTANDSVILKÅR,
         AKTIVITETSVILKÅR,
         FORUTGÅENDE_MEDLEMSKAPSVILKÅRET
     );
-    private boolean aktivitetsvilkårEnabled;
 
     public AktivitetspengerInngangsvilkårUtleder() {
         //for CDI proxy
     }
 
-    @Inject
-    public AktivitetspengerInngangsvilkårUtleder(@KonfigVerdi(value = "AKTIVITETSVILKAAR_ENABLED", required = false, defaultVerdi = "true") boolean aktivitetsvilkårEnabled) {
-        this.aktivitetsvilkårEnabled = aktivitetsvilkårEnabled;
-    }
-
 
     @Override
     public UtledeteVilkår utledVilkår(BehandlingReferanse referanse) {
-        return new UtledeteVilkår(null, aktivitetsvilkårEnabled ? YTELSE_VILKÅR_2 : YTELSE_VILKÅR);
+        return new UtledeteVilkår(null, YTELSE_VILKÅR);
     }
 
 }
