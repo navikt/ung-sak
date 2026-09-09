46c48f0a2fe2734b5d60685e5bbd4ecd8d4ef666
Hallvard Andreas Stark <hallvard@scel.to>
2026-09-09 11:05:13 +0200

    Beholder gamle kodeverdi i tillegg

--- kodeverk/src/main/java/no/nav/ung/kodeverk/behandling/aksjonspunkt/SkjermlenkeType.java
+++ kodeverk/src/main/java/no/nav/ung/kodeverk/behandling/aksjonspunkt/SkjermlenkeType.java
@@ -16,6 +16,7 @@ public enum SkjermlenkeType implements Kodeverdi {
     AKTIVITETSVILKÅR("AKTIVITETSVILKÅR", "Aktivitet"),
     VURDER_ANDRE_LIVSOPPHOLDSYTELSER("VURDER_ANDRE_LIVSOPPHOLDSYTELSER", "Vurder andre livsoppholdsytelser"),
     UDEFINERT("-", "Ikke definert"),
+    FAKTA_OM_MEDLEMSKAP("FAKTA_OM_MEDLEMSKAP", "Medlemskap"),
     FORUTGÅENDE_MEDLEMSKAP("FORUTGÅENDE_MEDLEMSKAP", "Medlemskap"),
     OPPLYSNINGSPLIKT("OPPLYSNINGSPLIKT", "Opplysningsplikt"),
     TILKJENT_YTELSE("TILKJENT_YTELSE", "Tilkjent ytelse"),
