45c0910c175eeec8268b17c24c36aae18ce27205
Bjornar Askevold <bjornar.askevold@nav.no>
2026-09-10 13:17:30 +0200

    Begrenser endring til vilkåravklaring og varsel

--- ytelse-aktivitetspenger/src/main/java/no/nav/ung/ytelse/aktivitetspenger/vilkår/avklaring/VilkårsavklaringUtfall.java
+++
@@ -1,7 +0,0 @@
-package no.nav.ung.ytelse.aktivitetspenger.vilkår.avklaring;
-
-public enum VilkårsavklaringUtfall {
-    VENTER_PÅ_UTTALELSE_FRA_BRUKER,
-    VILKÅR_VURDERES_MANUELT,
-    AVSLÅS_AUTOMATISK;
-}
