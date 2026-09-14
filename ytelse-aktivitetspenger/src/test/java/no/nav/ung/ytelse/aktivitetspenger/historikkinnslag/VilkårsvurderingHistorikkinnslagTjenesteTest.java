63cbacae19187c80876ee9a891d37278dd534869
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2026-09-14 11:18:44 +0200

    Flere tester

--- ytelse-aktivitetspenger/src/test/java/no/nav/ung/ytelse/aktivitetspenger/historikkinnslag/VilkårsvurderingHistorikkinnslagTjenesteTest.java
+++ ytelse-aktivitetspenger/src/test/java/no/nav/ung/ytelse/aktivitetspenger/historikkinnslag/VilkårsvurderingHistorikkinnslagTjenesteTest.java
@@ -167,4 +167,154 @@ class VilkårsvurderingHistorikkinnslagTjenesteTest {
 
 
     }
+
+    @Test
+    void skal_lage_historikkinnslag_når_saksbehandler_ikke_setter_opphørsdato_i_en_opphørsbehandling() {
+        LocalDate innvilgetFom = LocalDate.of(2026, 9, 11);
+        LocalDate innvilgetTom = innvilgetFom.plusWeeks(4).minusDays(1);
+
+        LocalDateTimeline<HistorikkinnslagData> vilkårtidslinjeFør = new LocalDateTimeline<>(innvilgetFom, innvilgetTom, HistorikkinnslagData.oppfylt());
+        LocalDateTimeline<HistorikkinnslagData> vilkårtidslinjeNå = vilkårtidslinjeFør;
+
+        HistorikkinnslagInput input = new HistorikkinnslagInput()
+            .setVilkårType(VilkårType.BOSTEDSVILKÅR)
+            .setBehandlingId(behanldingId)
+            .setEksisterendeVurderinger(vilkårtidslinjeFør)
+            .setGjelderOpphør(true)
+            .setNyeVurderinger(vilkårtidslinjeNå)
+            .setHistorikkAktør(HistorikkAktør.LOKALKONTOR_SAKSBEHANDLER)
+            .setSaksbehandlerIdent("Z000000");
+        List<Historikkinnslag> historikkinnslag = tjeneste.lagHistorikkinnslag(fagsakId, input);
+        assertThat(historikkinnslag).hasSize(1);
+        assertThat(historikkinnslag.getFirst().getLinjer()).containsOnly(
+            HistorikkinnslagLinje.tekst("Det ble ikke satt en opphørsdato", 0)
+        );
+    }
+
+    @Test
+    void skal_lage_historikkinnslag_når_saksbehandler_fjerner_et_tidligere_opphør() {
+        //dette kan for eksempel skje om beslutter er uenig og saksbehandler må endre vurdering
+
+        LocalDate innvilgetFom = LocalDate.of(2026, 9, 11);
+        LocalDate innvilgetTom = innvilgetFom.plusWeeks(52).minusDays(1);
+        LocalDate opphørFom = LocalDate.of(2026, 9, 30);
+
+        LocalDateTimeline<HistorikkinnslagData> heltInnvilget = new LocalDateTimeline<>(innvilgetFom, innvilgetTom, HistorikkinnslagData.oppfylt());
+        LocalDateTimeline<HistorikkinnslagData> opphør = new LocalDateTimeline<>(opphørFom, innvilgetTom, HistorikkinnslagData.avslått(Avslagsårsak.YTELSE_IKKE_TILGJENGELIG_PÅ_BOSTED));
+        LocalDateTimeline<HistorikkinnslagData> vilkårtidslinjeFør = heltInnvilget.crossJoin(opphør, StandardCombinators::coalesceRightHandSide);
+
+        HistorikkinnslagInput input = new HistorikkinnslagInput()
+            .setVilkårType(VilkårType.BOSTEDSVILKÅR)
+            .setBehandlingId(behanldingId)
+            .setEksisterendeVurderinger(vilkårtidslinjeFør)
+            .setGjelderOpphør(true)
+            .setNyeVurderinger(heltInnvilget)
+            .setHistorikkAktør(HistorikkAktør.LOKALKONTOR_SAKSBEHANDLER)
+            .setSaksbehandlerIdent("Z000000");
+        List<Historikkinnslag> historikkinnslag = tjeneste.lagHistorikkinnslag(fagsakId, input);
+        assertThat(historikkinnslag).hasSize(1);
+        assertThat(historikkinnslag.getFirst().getLinjer()).containsOnly(
+            HistorikkinnslagLinje.tekst("Opphør ble fjernet", 0)
+        );
+    }
+
+    @Test
+    void skal_lage_historikkinnslag_når_saksbehandler_endrer_en_tidligere_opphørsdato() {
+        LocalDate innvilgetFom = LocalDate.of(2026, 9, 11);
+        LocalDate innvilgetTom = innvilgetFom.plusWeeks(52).minusDays(1);
+        LocalDate opphørFomFør = LocalDate.of(2026, 9, 30);
+        LocalDate opphørFomNå = LocalDate.of(2026, 10, 15);
+
+        LocalDateTimeline<HistorikkinnslagData> heltInnvilget = new LocalDateTimeline<>(innvilgetFom, innvilgetTom, HistorikkinnslagData.oppfylt());
+        LocalDateTimeline<HistorikkinnslagData> opphørFør = new LocalDateTimeline<>(opphørFomFør, innvilgetTom, HistorikkinnslagData.avslått(Avslagsårsak.YTELSE_IKKE_TILGJENGELIG_PÅ_BOSTED));
+        LocalDateTimeline<HistorikkinnslagData> opphørNå = new LocalDateTimeline<>(opphørFomNå, innvilgetTom, HistorikkinnslagData.avslått(Avslagsårsak.YTELSE_IKKE_TILGJENGELIG_PÅ_BOSTED));
+        LocalDateTimeline<HistorikkinnslagData> vilkårtidslinjeFør = heltInnvilget.crossJoin(opphørFør, StandardCombinators::coalesceRightHandSide);
+        LocalDateTimeline<HistorikkinnslagData> vilkårtidslinjeNå = heltInnvilget.crossJoin(opphørNå, StandardCombinators::coalesceRightHandSide);
+
+        HistorikkinnslagInput input = new HistorikkinnslagInput()
+            .setVilkårType(VilkårType.BOSTEDSVILKÅR)
+            .setBehandlingId(behanldingId)
+            .setEksisterendeVurderinger(vilkårtidslinjeFør)
+            .setGjelderOpphør(true)
+            .setNyeVurderinger(vilkårtidslinjeNå)
+            .setHistorikkAktør(HistorikkAktør.LOKALKONTOR_SAKSBEHANDLER)
+            .setSaksbehandlerIdent("Z000000");
+        List<Historikkinnslag> historikkinnslag = tjeneste.lagHistorikkinnslag(fagsakId, input);
+        assertThat(historikkinnslag).hasSize(1);
+        assertThat(historikkinnslag.getFirst().getLinjer()).containsOnly(
+            HistorikkinnslagLinje.tekst("Opphørsdato endret til 15.10.2026. Søker bor et sted som ikke er forenelig med ytelsen.", 0)
+        );
+    }
+
+    @Test
+    void skal_lage_historikkinnslag_når_saksbehandler_endrer_utfall_fra_oppfylt_til_avslått_for_en_periode() {
+        LocalDate innvilgetFom = LocalDate.of(2026, 9, 11);
+        LocalDate innvilgetTom = innvilgetFom.plusWeeks(52).minusDays(1);
+        LocalDate avslagFom = LocalDate.of(2026, 9, 30);
+
+        LocalDateTimeline<HistorikkinnslagData> vilkårtidslinjeFør = new LocalDateTimeline<>(innvilgetFom, innvilgetTom, HistorikkinnslagData.oppfylt());
+        LocalDateTimeline<HistorikkinnslagData> avslag = new LocalDateTimeline<>(avslagFom, innvilgetTom, HistorikkinnslagData.avslått(Avslagsårsak.YTELSE_IKKE_TILGJENGELIG_PÅ_BOSTED));
+        LocalDateTimeline<HistorikkinnslagData> vilkårtidslinjeNå = vilkårtidslinjeFør.crossJoin(avslag, StandardCombinators::coalesceRightHandSide);
+
+        HistorikkinnslagInput input = new HistorikkinnslagInput()
+            .setVilkårType(VilkårType.BOSTEDSVILKÅR)
+            .setBehandlingId(behanldingId)
+            .setEksisterendeVurderinger(vilkårtidslinjeFør)
+            .setGjelderOpphør(false)
+            .setNyeVurderinger(vilkårtidslinjeNå)
+            .setHistorikkAktør(HistorikkAktør.LOKALKONTOR_SAKSBEHANDLER)
+            .setSaksbehandlerIdent("Z000000");
+        List<Historikkinnslag> historikkinnslag = tjeneste.lagHistorikkinnslag(fagsakId, input);
+        assertThat(historikkinnslag).hasSize(1);
+        assertThat(historikkinnslag.getFirst().getLinjer()).containsOnly(
+            HistorikkinnslagLinje.tekst("Vurdering ble endret for perioden 30.09.2026 - 09.09.2027 til Ikke oppfylt. Søker bor et sted som ikke er forenelig med ytelsen.", 0)
+        );
+    }
+
+    @Test
+    void skal_lage_historikkinnslag_når_saksbehandler_kun_endrer_avslagsårsak() {
+        LocalDate innvilgetFom = LocalDate.of(2026, 9, 11);
+        LocalDate innvilgetTom = innvilgetFom.plusWeeks(52).minusDays(1);
+
+        LocalDateTimeline<HistorikkinnslagData> vilkårtidslinjeFør = new LocalDateTimeline<>(innvilgetFom, innvilgetTom, HistorikkinnslagData.avslått(Avslagsårsak.YTELSE_IKKE_TILGJENGELIG_PÅ_BOSTED));
+        LocalDateTimeline<HistorikkinnslagData> vilkårtidslinjeNå = new LocalDateTimeline<>(innvilgetFom, innvilgetTom, HistorikkinnslagData.avslått(Avslagsårsak.SØKER_HAR_ANNEN_LIVSOPPHOLDSYTELSE));
+
+        HistorikkinnslagInput input = new HistorikkinnslagInput()
+            .setVilkårType(VilkårType.BOSTEDSVILKÅR)
+            .setBehandlingId(behanldingId)
+            .setEksisterendeVurderinger(vilkårtidslinjeFør)
+            .setGjelderOpphør(false)
+            .setNyeVurderinger(vilkårtidslinjeNå)
+            .setHistorikkAktør(HistorikkAktør.LOKALKONTOR_SAKSBEHANDLER)
+            .setSaksbehandlerIdent("Z000000");
+        List<Historikkinnslag> historikkinnslag = tjeneste.lagHistorikkinnslag(fagsakId, input);
+        assertThat(historikkinnslag).hasSize(1);
+        assertThat(historikkinnslag.getFirst().getLinjer()).containsOnly(
+            HistorikkinnslagLinje.tekst("Avslagsårsak ble endret for perioden 11.09.2026 - 09.09.2027 til Søker har livsoppholdsytelse som ikke er forenelig med ytelsen.", 0)
+        );
+    }
+
+    @Test
+    void skal_lage_historikkinnslag_når_en_periode_ikke_lenger_har_en_vurdering() {
+        LocalDate innvilgetFom = LocalDate.of(2026, 9, 11);
+        LocalDate innvilgetTom1 = innvilgetFom.plusWeeks(52).minusDays(1);
+        LocalDate innvilgetTom2 = innvilgetTom1.minusDays(7);
+
+        LocalDateTimeline<HistorikkinnslagData> vilkårtidslinjeFør = new LocalDateTimeline<>(innvilgetFom, innvilgetTom1, HistorikkinnslagData.oppfylt());
+        LocalDateTimeline<HistorikkinnslagData> vilkårtidslinjeNå = new LocalDateTimeline<>(innvilgetFom, innvilgetTom2, HistorikkinnslagData.oppfylt());
+
+        HistorikkinnslagInput input = new HistorikkinnslagInput()
+            .setVilkårType(VilkårType.BOSTEDSVILKÅR)
+            .setBehandlingId(behanldingId)
+            .setEksisterendeVurderinger(vilkårtidslinjeFør)
+            .setGjelderOpphør(false)
+            .setNyeVurderinger(vilkårtidslinjeNå)
+            .setHistorikkAktør(HistorikkAktør.LOKALKONTOR_SAKSBEHANDLER)
+            .setSaksbehandlerIdent("Z000000");
+        List<Historikkinnslag> historikkinnslag = tjeneste.lagHistorikkinnslag(fagsakId, input);
+        assertThat(historikkinnslag).hasSize(1);
+        assertThat(historikkinnslag.getFirst().getLinjer()).containsOnly(
+            HistorikkinnslagLinje.tekst("Perioden 03.09.2027 - 09.09.2027 har ikke lenger en vurdering", 0)
+        );
+    }
 }
