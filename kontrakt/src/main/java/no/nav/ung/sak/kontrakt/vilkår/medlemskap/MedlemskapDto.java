b4fe015b7357674c04c59c048560b598bcd699b5
Qadeer Khan <qadeer.ahmad.khan@nav.no>
2026-09-20 17:46:41 +0200

    setter annotasjoner nødvendig for at typescript klienten ikke lager nullable typer av felter som ikke kan være null
    Signed-off-by: Qadeer Khan <qadeer.ahmad.khan@nav.no>

--- kontrakt/src/main/java/no/nav/ung/sak/kontrakt/vilkår/medlemskap/MedlemskapDto.java
+++ kontrakt/src/main/java/no/nav/ung/sak/kontrakt/vilkår/medlemskap/MedlemskapDto.java
@@ -1,16 +1,12 @@
 package no.nav.ung.sak.kontrakt.vilkår.medlemskap;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
 import no.nav.ung.sak.typer.Periode;
 
 import java.util.List;
 
 public record MedlemskapDto(
     @JsonProperty(required = true)
-    @NotNull
-    @Valid
     Periode forutgåendePeriode,
 
     @JsonProperty(required = true)
@@ -21,11 +17,9 @@ public record MedlemskapDto(
     Boolean harJobbetUtenforNorge,
 
     @JsonProperty(required = true)
-    @NotNull
     String journalpostId,
 
     @JsonProperty(required = true)
-    @NotNull
     List<UtenlandsoppholdDto> utenlandsopphold
 ) {
 }
--- kontrakt/src/main/java/no/nav/ung/sak/kontrakt/vilkår/medlemskap/MedlemskapPeriodeInfoDto.java
+++ kontrakt/src/main/java/no/nav/ung/sak/kontrakt/vilkår/medlemskap/MedlemskapPeriodeInfoDto.java
@@ -1,24 +1,17 @@
 package no.nav.ung.sak.kontrakt.vilkår.medlemskap;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
 import no.nav.ung.kodeverk.vilkår.Utfall;
 import no.nav.ung.sak.kontrakt.aktivitetspenger.medlemskap.MedlemskapAvslagsÅrsakType;
 import no.nav.ung.sak.typer.Periode;
 
 public record MedlemskapPeriodeInfoDto(
     @JsonProperty(required = true)
-    @NotNull
-    @Valid
     Periode periode,
 
     @JsonProperty(required = true)
-    @NotNull
-    @Valid
     Utfall utfall,
 
-    @Valid
     MedlemskapAvslagsÅrsakType avslagsårsak,
 
     String begrunnelse,
@@ -29,7 +22,6 @@ public record MedlemskapPeriodeInfoDto(
     @JsonProperty(required = true)
     boolean erManueltVurdert,
 
-    @Valid
     MedlemskapDto medlemskapFraBruker
 ) {
 }
