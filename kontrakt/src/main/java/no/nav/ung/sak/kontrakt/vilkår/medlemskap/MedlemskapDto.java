b249ddba47dace0d03742cf290c1c47bcc8f2073
Qadeer Khan <qadeer.ahmad.khan@nav.no>
2026-09-21 15:02:19 +0200

    tidslinje blir feil hvis starter på laveste startdato (#1611)
    ### Behov / Bakgrunn
    Startdato tidslinje blir feil hvis starter på laveste startdato. 
    Kontrakten til frontend får alle felter markert som nullable 
    Fortsettelse av TSFF-3050
    
    ### Løsning
    Sorterer startdato etter høyste startdato før opprettelse og merging av
    tidslinjer.
    Legger på annotasjoner som fikser kontraktproblemer mot frontend.
    
    ---
    
    ### Sjekkliste for godkjenner
    
    - [ ] Ingen uhensiktsmessig spesialbehandling av saker (hardkodede
    aktørId-er, saksnumre, org.nr som styrer forretningslogikk)
    - [ ] Flyway-migrasjoner er vurdert (destruktive endringer, korrekt
    versjonering, påvirkning på økonomiske data)
    - [ ] Sporbarhet: lenke til Jira, Slack-tråd, eller en tydelig
    beskrivelse av **hvorfor** endringen gjøres
    
    ---------
    
    Signed-off-by: Qadeer Khan <qadeer.ahmad.khan@nav.no>
    Co-authored-by: sif-openapi-json-writer[bot] <254754783+sif-openapi-json-writer[bot]@users.noreply.github.com>

--- kontrakt/src/main/java/no/nav/ung/sak/kontrakt/vilkår/medlemskap/MedlemskapDto.java
+++ kontrakt/src/main/java/no/nav/ung/sak/kontrakt/vilkår/medlemskap/MedlemskapDto.java
@@ -1,15 +1,25 @@
 package no.nav.ung.sak.kontrakt.vilkår.medlemskap;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import no.nav.ung.sak.typer.Periode;
 
 import java.util.List;
 
 public record MedlemskapDto(
+    @JsonProperty(required = true)
     Periode forutgåendePeriode,
+
+    @JsonProperty(required = true)
     boolean harBoddINorge,
+
     Boolean harJobbetINorge,
+
     Boolean harJobbetUtenforNorge,
+
+    @JsonProperty(required = true)
     String journalpostId,
+
+    @JsonProperty(required = true)
     List<UtenlandsoppholdDto> utenlandsopphold
 ) {
 }
--- kontrakt/src/main/java/no/nav/ung/sak/kontrakt/vilkår/medlemskap/MedlemskapPeriodeInfoDto.java
+++ kontrakt/src/main/java/no/nav/ung/sak/kontrakt/vilkår/medlemskap/MedlemskapPeriodeInfoDto.java
@@ -1,16 +1,27 @@
 package no.nav.ung.sak.kontrakt.vilkår.medlemskap;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import no.nav.ung.kodeverk.vilkår.Utfall;
 import no.nav.ung.sak.kontrakt.aktivitetspenger.medlemskap.MedlemskapAvslagsÅrsakType;
 import no.nav.ung.sak.typer.Periode;
 
 public record MedlemskapPeriodeInfoDto(
+    @JsonProperty(required = true)
     Periode periode,
+
+    @JsonProperty(required = true)
     Utfall utfall,
+
     MedlemskapAvslagsÅrsakType avslagsårsak,
+
     String begrunnelse,
+
+    @JsonProperty(required = true)
     boolean vurderesIBehandlingen,
+
+    @JsonProperty(required = true)
     boolean erManueltVurdert,
+
     MedlemskapDto medlemskapFraBruker
 ) {
 }
